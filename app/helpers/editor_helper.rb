1985a36c30cbafcbf2f09f7bddb476e129cdba96
tany <una@kimiyure.com>
2014-08-08 19:15:21 +0900

    CKEditor: content template

--- app/helpers/editor_helper.rb
+++ app/helpers/editor_helper.rb
@@ -7,7 +7,7 @@ module EditorHelper
       extname = "javascript" if extname == "js"
       mode = extname if File.exists?("#{Rails.public_path}/assets/js/ace/mode-#{extname}.js")
     end
-    
+
     h  = []
     h << %Q[<script data-turbolinks-track="true" src="/assets/js/ace/mode-#{mode}.js"></script>] if mode
     h <<  coffee do
@@ -15,7 +15,7 @@ module EditorHelper
       j << %Q[$ ->]
       j << %Q[  editor = $("#{elem}").ace({ theme: "chrome", lang: "#{mode}" })]
       j << %Q[  ace = editor.data("ace").editor.ace]
-      
+
       if opts[:readonly]
         j << %Q[  ace.setReadOnly(true)]
         j << %Q[  h = ace.getSession().getScreenLength() * 16 + ace.renderer.scrollBar.getWidth()]
@@ -23,13 +23,13 @@ module EditorHelper
         j << %Q[  $(ace["container"]).height(h + "px")]
         j << %Q[  $(ace["container"]).find(".ace_scrollbar").hide()]
       end
-      
+
       j.join("\n").html_safe
     end
-    
+
     h.join("\n").html_safe
   end
-  
+
   def html_editor(elem, opts = {})
     if SS.config.cms.html_editor == "ckeditor"
       html_editor_ckeditor(elem, opts)
@@ -39,10 +39,10 @@ module EditorHelper
       html_editor_wiki(elem, opts)
     end
   end
-  
+
   def html_editor_ckeditor(elem, opts = {})
     opts = { extraPlugins: "", removePlugins: "" }.merge(opts)
-    
+
     if opts[:readonly]
       opts[:removePlugins] << ",toolbar"
       #opts[:removePlugins] << ",resize"
@@ -50,23 +50,48 @@ module EditorHelper
       opts[:readOnly] = true
       opts.delete :readonly
     end
+    opts[:extraPlugins]  << ",templates"
     opts[:enterMode] = 2 #BR
     opts[:shiftEnterMode] = 1 #P
     opts[:allowedContent] = true
     opts[:height] ||= "360px"
-    
+
     h  = []
     h <<  coffee do
       j = []
       j << %Q[$ ->]
       j << %Q[  $("#{elem}").ckeditor #{opts.to_json}]
-      
+
+      j << %Q[  CKEDITOR.on 'dialogDefinition', (ev) -> ]
+      j << %Q[    name = ev.data.name]
+      j << %Q[    def  = ev.data.definition]
+      j << %Q[    if name == 'table']
+      j << %Q[      info = def.getContents('info')]
+      j << %Q[      text = info.get('txtWidth')]
+      j << %Q[      text['default'] = ""]
+      j << %Q[      text = info.get('txtCellSpace')]
+      j << %Q[      text['controlStyle'] = "display: none"]
+      j << %Q[      text['label'] = ""]
+      j << %Q[      text['default'] = ""]
+      j << %Q[      text = info.get('txtCellPad')]
+      j << %Q[      text['controlStyle'] = "display: none"]
+      j << %Q[      text['label'] = ""]
+      j << %Q[      text['default'] = ""]
+      j << %Q[      text = info.get('txtBorder')]
+      j << %Q[      text['controlStyle'] = "display: none"]
+      j << %Q[      text['label'] = ""]
+      j << %Q[      text['default'] = ""]
+      j << %Q[      text = info.get('txtSummary')]
+      j << %Q[      text['controlStyle'] = "display: none"]
+      j << %Q[      text['label'] = ""]
+      j << %Q[      text['default'] = ""]
+
       j.join("\n").html_safe
     end
-    
+
     h.join("\n").html_safe
   end
-  
+
   def html_editor_tinymce(elem, opts = {})
     h  = []
     h <<  coffee do
@@ -75,7 +100,7 @@ module EditorHelper
       j << %Q[  tinymce.init]
       j << %Q[    selector: "#{elem}"]
       j << %Q[    language: "ja"]
-      
+
       if opts[:readonly]
       j << %Q[    readonly: true]
       j << %Q[    plugins: \[\]]
@@ -91,13 +116,13 @@ module EditorHelper
       j << %Q[    toolbar: "insertfile undo redo | styleselect | bold italic | forecolor backcolor" +]
       j << %Q[      " | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media"]
       end
-      
+
       j.join("\n").html_safe
     end
-    
+
     h.join("\n").html_safe
   end
-  
+
   def html_editor_wiki(elem, opts = {})
   end
 end
---
+++ bin/unicorn.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+# chkconfig: - 85 35
+# description: Unicorn
+
+# rvm wrapper 2.1.2 start unicorn => /usr/local/rvm/bin/start_unicorn
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
+
+APPS=()
+APPS=("${APPS[@]}" "/var/www/ss")
+
+for APP in ${APPS[@]}; do
+  NAME="Unicorn"
+  ENV="production"
+  PID="${APP}/tmp/pids/unicorn.pid"
+  CONF="${APP}/config/unicorn.rb"
+  
+  start()
+  {
+    if [ -e $PID ]; then
+      echo "$NAME already started"
+      exit 1
+    fi
+    echo "start $NAME"
+    cd $APP
+    /usr/local/rvm/bin/start_unicorn -c ${CONF} -E ${ENV} -D
+  }
+  
+  stop()
+  {
+    if [ ! -e $PID ]; then
+      echo "$NAME not started"
+      exit 1
+    fi
+    echo "stop $NAME"
+    kill -QUIT `cat ${PID}`
+  }
+  
+  force_stop()
+  {
+    if [ ! -e $PID ]; then
+      echo "$NAME not started"
+      exit 1
+    fi
+    echo "stop $NAME"
+    kill -INT `cat ${PID}`
+  }
+  
+  reload()
+  {
+    if [ ! -e $PID ]; then
+      echo "$NAME not started"
+      start
+      exit 0
+    fi
+    echo "reload $NAME"
+    kill -HUP `cat ${PID}`
+  }
+  
+  restart()
+  {
+    stop
+    sleep 3
+    start
+  }
+  
+  case "$1" in
+    start)
+      start
+      ;;
+    stop)
+      stop
+      ;;
+    force-stop)
+      force_stop
+      ;;
+    reload)
+      reload
+      ;;
+    restart)
+      restart
+      ;;
+    *)
+      echo "Syntax Error: release [start|stop|force-stop|reload|restart]"
+      ;;
+  esac
+done
