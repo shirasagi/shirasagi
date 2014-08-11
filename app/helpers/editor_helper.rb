90ed144a1e2f2d7333d1c72e7624ab3de755d71f
itowtips <ito@web-tips.co.jp>
2014-08-11 10:46:58 +0900

    CKEditor: justify

--- app/helpers/editor_helper.rb
+++ app/helpers/editor_helper.rb
@@ -50,7 +50,7 @@ module EditorHelper
       opts[:readOnly] = true
       opts.delete :readonly
     end
-    opts[:extraPlugins]  << ",templates"
+    opts[:extraPlugins]  << ",templates,justify"
     opts[:enterMode] = 2 #BR
     opts[:shiftEnterMode] = 1 #P
     opts[:allowedContent] = true
