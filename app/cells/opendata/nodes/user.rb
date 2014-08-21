e0306f655a4c0def60716f225ca5dce4203ada27
209030 <209030@TPC0596.tec-inf.local>
2014-08-21 11:24:30 +0900

    登録に必要なユーザIDを取得するためのログイン機能を作成

--- app/cells/opendata/nodes/user.rb
+++ app/cells/opendata/nodes/user.rb
@@ -15,99 +15,45 @@ module Opendata::Nodes::User
 
     public
       def index
+        @item = SS::User.new
         render
       end
 
       def login
-        @item = SS::User.new
-        @item.email    = params[:email]
-        @item.password = params[:password]
-        return if !request.post?
-
-        @item.attributes = get_params
+        @item = SS::User.new get_params
         user = SS::User.where(email: @item.email, password: SS::Crypt.crypt(@item.password)).first
-        return if !user
 
         if user
-          reset_session
-          session[:user] = SS::Crypt.encrypt("#{user._id},#{remote_addr},#{request.user_agent}")
+          set_user user
+          controller.redirect_to "#{@cur_node.url}#{user._id}/dataset/"
         else
-          flash.now[:referer] = params[:referer]
-          @error = "Error"
-          render "index"
+          unset_user
+          controller.redirect_to "#{@cur_node.url}"
         end
-
-=begin
-        if params[:ref].blank? || [node_opendata_path, node_opendata_path].index(params[:ref])
-          return set_user user, session: true, redirect: true
-        end
-
-        set_user user, session: true
-        render file: "views/sns/login/redirect"
-=end
       end
 
       def logout
-        reset_session
-        redirect_to "/"
-=begin
-        unset_user redirect: true
-=end
+        session[:user] = nil
+        controller.redirect_to "#{@cur_node.url}"
       end
 
     private
-#login_controllerより
       def get_params
         params.require(:item).permit(:email, :password, :ref)
       end
-#SS::BaseFilterより
-      def logged_in?
 
-        if session[:user]
-          begin
-            u = SS::Crypt.decrypt(session[:user]).to_s.split(",", 3)
-          rescue ActiveRecord::RecordNotFound
-            reset_session
-          end
-        end
-
-        unless u
-          #flash[:referer] = request.fullpath
-          ref = request.env["REQUEST_URI"]
-          ref = (ref == node_opendata_path) ? "" : "?ref=" + CGI.escape(ref.to_s)
-          redirect_to "#{node_opendata_path}#{ref}"
-        end
-=begin
-        return @cur_user if @cur_user
-
-        if session[:user]
-          u = SS::Crypt.decrypt(session[:user]).to_s.split(",", 3)
-          return unset_user redirect: true if u[1] != remote_addr
-          return unset_user redirect: true if u[2] != request.user_agent
-          @cur_user = SS::User.find u[0].to_i rescue nil
-        end
-
-        return @cur_user if @cur_user
-        unset_user
-
-        ref = request.env["REQUEST_URI"]
-        ref = (ref == sns_mypage_path) ? "" : "?ref=" + CGI.escape(ref.to_s)
-        redirect_to "#{sns_login_path}#{ref}"
-=end
-      end
-
-      def set_user(user, opt = {})
-        if opt[:session]
-          session[:user] = SS::Crypt.encrypt("#{user._id},#{remote_addr},#{request.user_agent}")
-        end
-        redirect_to sns_mypage_path if opt[:redirect]
+      def set_user(user)
+        session[:user] = SS::Crypt.encrypt("#{user._id},#{remote_addr},#{request.user_agent}")
         @cur_user = user
       end
 
-      def unset_user(opt = {})
+      def unset_user
         session[:user] = nil
-        redirect_to sns_login_path if opt[:redirect]
         @cur_user = nil
+      end
+
+      def remote_addr
+        request.env["HTTP_X_REAL_IP"] || request.remote_addr
       end
   end
 end
