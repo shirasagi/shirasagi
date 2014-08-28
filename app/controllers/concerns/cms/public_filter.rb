33f5bf3715cca879b941aa0c9a0f16976cdb92c5
tany <una@kimiyure.com>
2014-08-28 17:27:00 +0900

    fix: public filter

--- app/controllers/concerns/cms/public_filter.rb
+++ app/controllers/concerns/cms/public_filter.rb
@@ -123,7 +123,7 @@ module Cms::PublicFilter
     end
 
     def render_node(node, path = @path)
-      rest = path.sub(/^#{node.filename}/, "")
+      rest = path.sub(/^#{node.filename}/, "").sub(/\/index\.html$/, "")
       cell = recognize_path "/.#{@cur_site.host}/nodes/#{node.route}#{rest}"
       return unless cell
 
