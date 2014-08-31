cf5fcb2e18e4859fd0c37f389b8716b06b164fec
gouf <innocent.zero@gmail.com>
2014-09-01 04:35:32 +0900

    create spec/lib/ss/debug_spec.rb

---
+++ spec/lib/ss/debug_spec.rb
@@ -0,0 +1,8 @@
+module SS; end
+
+require_relative '../../../lib/ss/debug'
+
+describe SS::Debug do
+  # SS::Debug.dump(data, lev = 1)
+  # data will accept type of String, Hash, Array and Fixnum
+end
