656a5e60aa83eb559a6d8195711c6959ba7675e2
gouf <innocent.zero@gmail.com>
2014-09-01 04:43:18 +0900

    update debug_spec.rb setup test cases

--- spec/lib/ss/debug_spec.rb
+++ spec/lib/ss/debug_spec.rb
@@ -5,4 +5,15 @@ require_relative '../../../lib/ss/debug'
 describe SS::Debug do
   # SS::Debug.dump(data, lev = 1)
   # data will accept type of String, Hash, Array and Fixnum
+  subject { SS::Debug }
+
+  describe '.debug' do
+    it 'receives String "test", returns "test <String>"'
+
+    it 'receives Array data, returns "<Array> [...]" format string'
+
+    it 'receives Hash data, returns "<Hash> {...}" format string'
+
+    it 'receives 1, returns "1 <Fixnum>"'
+  end
 end
