61701961caecc6730420d064e7c938163dbd2499
itowtips <ito@web-tips.co.jp>
2014-09-01 17:25:31 +0900

    CKEditor: format_tags add h4

--- public/assets/js/ckeditor/config.js
+++ public/assets/js/ckeditor/config.js
@@ -31,7 +31,7 @@ CKEDITOR.editorConfig = function( config ) {
 	config.removeButtons = 'Underline,Subscript,Superscript';
 
 	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
+	config.format_tags = 'p;h1;h2;h3;h4;pre';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
