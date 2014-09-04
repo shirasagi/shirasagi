d4ad6570eec5a2d5219d4609ed10a73b9d09b4c8
snkmr <nakamura@web-tips.co.jp>
2014-09-04 16:38:49 +0900

    update workflow

--- app/models/workflow/initializer.rb
+++ app/models/workflow/initializer.rb
@@ -2,6 +2,8 @@
 module Workflow
   class Initializer
     Cms::Page.addon "workflow/approver"
+    Article::Page.addon "workflow/approver"
+    Faq::Page.addon "workflow/approver"
 
     Cms::Role.permission :release_other_article_pages
     Cms::Role.permission :release_private_article_pages
