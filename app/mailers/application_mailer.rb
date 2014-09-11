85cb41bed1cdf9c38db346e832b20d11fc8f386c
gouf <innocent.zero@gmail.com>
2014-09-11 11:27:33 +0900

    method split out to smtp and sendmail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -14,22 +14,30 @@ class ApplicationMailer
         ActionMailer::Base.delivery_method = mail['delivery_method']
         ActionMailer::Base.default from: mail['default_from'], charset: mail['default_charset']
         if  mail['delivery_method'] == :smtp
-          ActionMailer::Base.smtp_settings = {
-            address: mail['address'],
-            port: mail['port'],
-            domain: mail['domain'],
-            user_name: mail['user_name'],
-            password: mail['password'],
-            authentication: mail['authentication'],
-            enable_starttls_auto: true
-          }
+          smtp(mail)
         elsif mail['delivery_method'] == :sendmail
-          ActionMailer::Base.sendmail_settings = {
-            location: mail['location'],
-            arguments:  mail['arguments']
-          }
+          sendmail(mail)
         end
       end
+    end
+
+    def smtp(mail)
+      ActionMailer::Base.smtp_settings = {
+        address: mail['address'],
+        port: mail['port'],
+        domain: mail['domain'],
+        user_name: mail['user_name'],
+        password: mail['password'],
+        authentication: mail['authentication'],
+        enable_starttls_auto: true
+      }
+    end
+
+    def sendmail(mail)
+      ActionMailer::Base.sendmail_settings = {
+        location: mail['location'],
+        arguments:  mail['arguments']
+      }
     end
   end
 end
