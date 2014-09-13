1dee7c70a8b5e30cf1c1adc38fa1b677efe7507f
gouf <innocent.zero@gmail.com>
2014-09-14 05:58:00 +0900

    create event_helper_spec.rb
    setup test for "within_one_year?" method

---
+++ spec/helpers/event/event_helper_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'timecop'
+
+include Event::EventHelper
+
+describe Event::EventHelper do
+
+  before { Timecop.freeze(date_when(year: 2014, month: 9, day: 14)) }
+
+  subject { Event::EventHelper }
+
+  context '#within_one_year?' do
+    it 'given current date returns true'
+
+    # start_date
+    it 'given 2013/12/31 returns true'
+    it 'given 2012/12/30 returns false'
+
+    # close_date
+    it 'given 2015/09/01 returns true'
+    it 'given 2015/09/02 returns false'
+
+  end
+
+  after { Timecop.return }
+end
