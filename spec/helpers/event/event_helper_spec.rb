2b40c98ebd3fde71426170f410b7d88fd655a65a
gouf <innocent.zero@gmail.com>
2014-09-14 06:04:56 +0900

    update event_helper_spec.rb
    add some cases for "within_one_year?" method

--- spec/helpers/event/event_helper_spec.rb
+++ spec/helpers/event/event_helper_spec.rb
@@ -15,16 +15,32 @@ describe Event::EventHelper do
   subject { Event::EventHelper }
 
   context '#within_one_year?' do
-    it 'given current date returns true'
+    it 'given current date returns true' do
+      date = Date.current # 2014/09/14
+      expect(subject.within_one_year?(date)).to eq true
+    end
 
     # start_date
-    it 'given 2013/12/31 returns true'
-    it 'given 2012/12/30 returns false'
+    it 'given 2013/12/31 returns true' do
+      date = date_when(year: 2013, month: 12, day: 31)
+      expect(subject.within_one_year?(date)).to eq true
+    end
+
+    it 'given 2012/12/30 returns false' do
+      date = date_when(year: 2012, month: 12, day: 30)
+      expect(subject.within_one_year?(date)).to eq false
+    end
 
     # close_date
-    it 'given 2015/09/01 returns true'
-    it 'given 2015/09/02 returns false'
+    it 'given 2015/09/01 returns true' do
+      date = date_when(year: 2015, month: 9, day: 1)
+      expect(subject.within_one_year?(date)).to eq true
+    end
 
+    it 'given 2015/09/02 returns false' do
+      date = date_when(year: 2015, month: 10, day: 2)
+      expect(subject.within_one_year?(date)).to eq false
+    end
   end
 
   after { Timecop.return }
