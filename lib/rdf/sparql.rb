1ae19e38855ff63113c549bb27ec306611b788b8
tecoda <y.oda@tec-inf.co.jp>
2014-09-30 11:15:10 +0900

    fix: use constant

--- lib/rdf/sparql.rb
+++ lib/rdf/sparql.rb
@@ -48,27 +48,27 @@ module Rdf::Sparql
         if format == "HTML"
           type = "text/html"
           ext = "html"
-          encoding = "utf-8"
+          encoding = Encoding::UTF_8
           data = results.to_html
         elsif format == "JSON"
           type = "application/json"
           ext = "json"
-          encoding = "utf-8"
+          encoding = Encoding::UTF_8
           data = results.to_json
         elsif format == "CSV"
           type = "text/csv"
           ext = "csv"
-          encoding = "sjis"
+          encoding = Encoding::SJIS
           data = results.to_csv
         elsif format == "TSV"
           type = "text/plain"
           ext = "txt"
-          encoding = "sjis"
+          encoding = Encoding::SJIS
           data = results.to_tsv
         elsif format == "XML"
           type = "application/xml"
           ext = "xml"
-          encoding = "utf-8"
+          encoding = Encoding::UTF_8
           data = results.to_xml
         end
 
