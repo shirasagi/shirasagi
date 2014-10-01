b2d0e889511a155682b0818262711d8d998213c8
tany <una@kimiyure.com>
2014-10-01 19:16:06 +0900

    fix: uplaoder

---
+++ app/cells/uploader/nodes/file/view_cell.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+module Uploader::Nodes::File
+  class ViewCell < Cell::Rails
+    include Cms::NodeFilter::ViewCell
+    helper Cms::ListHelper
+
+    public
+      def index
+        controller.render text: "."
+      end
+  end
+end
