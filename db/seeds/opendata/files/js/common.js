6037ac206859945c27897070fc7bc88523519092
ayano <a.yoshikawa@tec-inf.co.jp>
2014-10-02 20:37:56 +0900

    change: site design

---
+++ db/seeds/opendata/files/js/common.js
@@ -0,0 +1,22 @@
+$(function(){
+    $("#gnav li").each(function(){
+        var menu = $(this).find("a");
+        var $href = menu.attr("href");
+        if(location.href.match($href)) {
+        $(this).addClass('current');
+        } else {
+        $(this).removeClass('current');
+        }
+    });
+});
+
+$(function(){
+    $("#mypage-tabs a").each(function(){
+        var $href = $(this).attr("href");
+        if(location.href.match($href)) {
+        $(this).addClass('current');
+        } else {
+        $(this).removeClass('current');
+        }
+    });
+});
