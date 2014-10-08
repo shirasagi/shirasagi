c5c6eed76fad3544e6142e7d071d1e93c6f3860d
tecoda <y.oda@tec-inf.co.jp>
2014-10-08 14:35:46 +0900

    fix: sparql endpoint html

--- app/cells/opendata/nodes/sparql/view_cell.rb
+++ app/cells/opendata/nodes/sparql/view_cell.rb
@@ -14,12 +14,12 @@ module Opendata::Nodes::Sparql
           html_page =  "<html>\n"
           html_page += "<head>\n"
           html_page += "<meta http-equiv='content-type' content='text/html; charset=utf-8'>\n"
-          html_page += "<style type='text/css'>\n"
-          html_page += " table, td, th { border: 1px silver solid ; border-collapse: collapse;}>\n"
+          html_page += "<style type='text/css'>"
+          html_page += " table, td, th { border: 1px silver solid ; border-collapse: collapse;}"
           html_page += "</style>\n"
-          html_page += "<title>SPARQL Results</title>"
+          html_page += "<title>SPARQL Results</title>\n"
           html_page += "</head>\n"
-          html_page += "<body>\n#{result[:data]}\n</body>\n"
+          html_page += "<body>\n<h3>SPARQL Results</h3>\n#{result[:data]}</body>\n"
           html_page += "</html>\n"
           controller.render inline: html_page
         else
