9c31d9e5d0b42dfb1e9c2adf1e600dd788aec607
tany <una@kimiyure.com>
2014-10-08 21:01:23 +0900

    resources dependent :destroy

--- app/models/opendata/addon/resource.rb
+++ app/models/opendata/addon/resource.rb
@@ -7,5 +7,10 @@ module Opendata::Addon::Resource
 
   included do
     embeds_many :resources, class_name: "Opendata::Resource"
+    before_destroy :destroy_resources
+  end
+
+  def destroy_resources
+    resources.destroy
   end
 end
