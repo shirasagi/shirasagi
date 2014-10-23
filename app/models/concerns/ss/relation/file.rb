95435070e260aaa5312ae039ce45f6ee9725f7bc
tany <una@kimiyure.com>
2014-10-24 01:51:16 +0900

    fix: SS::Relation::File
    no grid_fs

--- app/models/concerns/ss/relation/file.rb
+++ app/models/concerns/ss/relation/file.rb
@@ -5,30 +5,23 @@ module SS::Relation::File
   module ClassMethods
     def belongs_to_file(name, opts = {})
       store = opts[:store_as] || "#{name.to_s.singularize}_id"
-      field store, type: (opts[:type] || BSON::ObjectId)
+      belongs_to name, foreign_key: store, class_name: "SS::File", dependent: :destroy
 
       attr_accessor "in_#{name}"
       permit_params "in_#{name}"
 
-      define_method(name) {
-        Mongoid::GridFs.find _id: send(store)
-      }
-
       before_save "save_#{name}", if: ->{ send("in_#{name}").present? }
+
       define_method("save_#{name}") {
         file = send("in_#{name}")
-        if file_id = send(store)
-          Mongoid::GridFs.delete(file_id) rescue nil
-          fs = Mongoid::GridFs.put file, filename: file.original_filename, _id: file_id
-        else
-          fs = Mongoid::GridFs.put file, filename: file.original_filename
-          send("#{store}=", fs.id)
-        end
-      }
 
-      before_destroy "remove_#{name}", if: ->{ send(store).present? }
-      define_method("remove_#{name}") {
-        Mongoid::GridFs.delete send(store) rescue nil
+        ss_file = SS::File.new
+        ss_file.in_file = file
+        ss_file.model = self.class.to_s.underscore
+        ss_file.filename = file.original_filename
+        ss_file.save
+
+        send("#{store}=", ss_file.id)
       }
     end
   end
