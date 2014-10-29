c4ac3263c4fb7574ded98a9c8997ccec3d97a1dc
itowtips <ito@web-tips.co.jp>
2014-10-29 19:37:43 +0900

    facility remove unused view

--- app/controllers/facility/agents/addons/attribute/edit_controller.rb
+++
@@ -1,5 +0,0 @@
-module Facility::Agents::Addons::Attribute
-  class EditController < ApplicationController
-    include SS::AddonFilter::Edit
-  end
-end
--- app/controllers/facility/agents/nodes/attribute/edit_controller.rb
+++
@@ -1,6 +0,0 @@
-module Facility::Agents::Nodes::Attribute
-  class EditController < ApplicationController
-    include Cms::NodeFilter::Edit
-    model Facility::Node::Attribute
-  end
-end
