b695441c05425c2478ad6bfa70c6bef9bb0699b8
tany <una@kimiyure.com>
2014-10-29 20:20:25 +0900

    fix api for CORS

--- app/controllers/opendata/agents/nodes/api/view_controller.rb
+++ app/controllers/opendata/agents/nodes/api/view_controller.rb
@@ -2,6 +2,8 @@ module Opendata::Agents::Nodes::Api
   class ViewController < ApplicationController
     include Cms::NodeFilter::View
 
+    before_action :accept_cors_request
+
     public
       def index
         render
