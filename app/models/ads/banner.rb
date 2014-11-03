529122ef15ed837799857433918671c103725240
tany <tanizawa@web-tips.co.jp>
2014-11-04 03:33:51 +0900

    fix ads: set state with release_date

--- app/models/ads/banner.rb
+++ app/models/ads/banner.rb
@@ -7,10 +7,12 @@ class Ads::Banner
 
   belongs_to_file :file
 
-  before_save :seq_filename, if: ->{ basename.blank? }
-
   validates :link_url, presence: true
   #validates :file_id, presence: true
+
+  validate :validate_release_state
+
+  before_save :seq_filename, if: ->{ basename.blank? }
 
   permit_params :link_url
 
@@ -26,6 +28,12 @@ class Ads::Banner
     end
 
   private
+    def validate_release_state
+      self.state = "public"
+      self.state = "ready" if release_date && release_date > Time.now
+      self.state = "closed" if close_date && close_date <= Time.now
+    end
+
     def validate_filename
       @basename.blank? ? nil : super
     end
