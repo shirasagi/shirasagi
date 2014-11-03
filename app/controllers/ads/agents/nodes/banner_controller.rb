82f97113d8c308fbb105acd55913f8e74941d404
tany <tanizawa@web-tips.co.jp>
2014-11-04 02:33:00 +0900

    fix ads: access log with img

--- app/controllers/ads/agents/nodes/banner_controller.rb
+++ app/controllers/ads/agents/nodes/banner_controller.rb
@@ -26,6 +26,10 @@ class Ads::Agents::Nodes::BannerController < ApplicationController
       })
       log.inc count: 1
 
+      response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+      response.headers["Pragma"] = "no-cache"
+      response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+
       render nothing: true
     end
 end
