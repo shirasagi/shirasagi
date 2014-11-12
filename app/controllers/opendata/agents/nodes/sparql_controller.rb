7f3e378cce7ff9ab4eecf46896ba37970a71da21
tecoda <y.oda@tec-inf.co.jp>
2014-11-12 14:04:25 +0900

    fix: String format

--- app/controllers/opendata/agents/nodes/sparql_controller.rb
+++ app/controllers/opendata/agents/nodes/sparql_controller.rb
@@ -20,27 +20,29 @@ class Opendata::Agents::Nodes::SparqlController < ApplicationController
       if file_format == "HTML"
 
         html_result = result[:data]
-        if html_result.include?("<td>")
-          html_page =  %Q{<html>\n<head><meta charset="utf-8"></head>\n<body>\n}
-          html_page += html_result.gsub(/<table class="sparql">/, %Q{<table class="sparql" border="1">} )
-          html_page += "</body>\n</html>\n"
+        if html_result.include?(%(<td>))
+          html_page =  %(<html>\n<head><meta charset="utf-8"></head>\n<body>\n)
+          html_page += html_result.gsub(/<table class="sparql">/, %(<table class="sparql" border="1">) )
+          html_page += %(</body>\n</html>\n)
         else
-          html_page = "<h1>No Data</h1>"
+          html_page = %(<h1>No Data</h1>)
         end
 
         @cur_node.layout_id = nil
         send_data html_page, type: result[:type], disposition: :inline
+
       elsif file_format == "HTML_TABLE"
 
         html_result = result[:data]
-        if html_result.include?("<td>")
-          html_page = html_result.gsub(/<table class="sparql">/, %Q{<table class="sparql" border="1">} )
+        if html_result.include?(%(<td>))
+          html_page = html_result.gsub(/<table class="sparql">/, %(<table class="sparql" border="1">) )
         else
-          html_page = "<h1>No Data</h1>"
+          html_page = %(<h1>No Data</h1>)
         end
 
         @cur_node.layout_id = nil
         send_data html_page, type: result[:type], disposition: :inline
+
       else
         send_data result[:data], type: result[:type], filename: "sparql.#{result[:ext]}",
           disposition: :attachment
