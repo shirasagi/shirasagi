d0f60cbb3518412212c9c738c61052678c8a4863
itowtips <ito@web-tips.co.jp>
2014-11-19 09:41:59 +0900

    remove unused routing

--- config/routes/facility/routes.rb
+++ config/routes/facility/routes.rb
@@ -38,11 +38,8 @@ SS::Application.routes.draw do
 
   namespace "facility", path: ".:site/facility" do
     get "/search_categories" => "search_categories#index"
-    post "/search_categories" => "search_categories#search"
     get "/search_locations" => "search_locations#index"
-    post "/search_locations" => "search_locations#search"
     get "/search_services" => "search_services#index"
-    post "/search_services" => "search_services#search"
   end
 
   namespace "facility", path: ".u:user/facility", module: "facility", servicer: /\d+/ do
