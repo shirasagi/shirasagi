521bf3379d5285bbf059e6f1a934b44b9d948187
tany <tanizawa@web-tips.co.jp>
2014-11-26 19:53:59 +0900

    fix ads banner parts

--- app/controllers/ads/agents/parts/banner_controller.rb
+++ app/controllers/ads/agents/parts/banner_controller.rb
@@ -9,7 +9,7 @@ class Ads::Agents::Parts::BannerController < ApplicationController
       sort = @node.becomes_with_route.sort_hash
       @random = sort[:random]
 
-      @items = Ads::Banner.site(@cur_site).public(@cur_date).order_by(sort)
+      @items = Ads::Banner.site(@cur_site).node(@node).public(@cur_date).order_by(sort)
       #@items = @items.shuffle if @random
     end
 end
