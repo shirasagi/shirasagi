f44d66ce2c65822c357152a653fa62453f0cc688
ayano <a.yoshikawa@tec-inf.co.jp>
2014-12-11 20:37:51 +0900

    add: terms of use

--- db/seeds/opendata/load.rb
+++ db/seeds/opendata/load.rb
@@ -56,6 +56,7 @@ end
 save_node filename: "css", name: "CSS", route: "uploader/file"
 save_node filename: "js", name: "Javascript", route: "uploader/file"
 save_node filename: "img", name: "画像", route: "uploader/file"
+save_node filename: "termsofuse", name: "利用規約", route: "uploader/file"
 
 save_node filename: "info", name: "お知らせ", route: "article/page", shortcut: "show",
   layout_id: layouts["portal-info"].id
