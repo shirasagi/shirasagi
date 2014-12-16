32126714d216c8f9ba3c2e5dd6be6e6b150fd134
tany <tanizawa@web-tips.co.jp>
2014-12-17 04:04:45 +0900

    fix dataset_group.part

--- app/controllers/opendata/agents/parts/dataset_group_controller.rb
+++ app/controllers/opendata/agents/parts/dataset_group_controller.rb
@@ -4,10 +4,24 @@ class Opendata::Agents::Parts::DatasetGroupController < ApplicationController
 
   public
     def index
+      cond = {}
+      if @cate = category
+        cond[:category_ids] = @cate.id
+      end
+
       @items = Opendata::DatasetGroup.site(@cur_site).public.
-        order_by(name: 1).
+        where(cond).
+        order_by(created: -1).
         limit(10)
 
       render
+    end
+
+  private
+    def category
+      return nil unless @cur_node = cur_node
+      return nil if @cur_node.route != "opendata/dataset_category"
+      name = File.basename(File.dirname(@cur_path))
+      Opendata::Node::Category.site(@cur_site).public.where(filename: /\/#{name}$/).first
     end
 end
