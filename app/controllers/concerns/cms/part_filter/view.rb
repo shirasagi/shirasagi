50ac3cd3ad15a265c5bd11098970e4acc760fd40
tany <tanizawa@web-tips.co.jp>
2014-12-17 03:59:34 +0900

    add method: cur_node on parts

--- app/controllers/concerns/cms/part_filter/view.rb
+++ app/controllers/concerns/cms/part_filter/view.rb
@@ -11,4 +11,11 @@ module Cms::PartFilter::View
     def prepend_current_view_path
       prepend_view_path "app/views/" + self.class.to_s.underscore.sub(/_\w+$/, "")
     end
+
+  public
+    def cur_node
+      node = Cms::Node.site(@cur_site).in_path(@cur_path).sort(depth: -1).first
+      return unless node
+      @preview || node.public? ? node.becomes_with_route : nil
+    end
 end
