1f1a2f5367b57b006926f6044aee9d105da7a163
ayano <a.yoshikawa@tec-inf.co.jp>
2015-01-14 08:51:23 +0900

    fix: opendata-datasets html

--- app/helpers/opendata/list_helper.rb
+++ app/helpers/opendata/list_helper.rb
@@ -16,8 +16,7 @@ module Opendata::ListHelper
           ih << '<article class="item-#{class} #{new} #{current}">'
           ih << '  <header>'
           ih << '    <time datetime="#{date.iso}">#{date.long}</time>'
-          ih << '    <h2><a href="#{url}">#{name}</a></h2>'
-          ih << '    <span class="point">#{point}</span>'
+          ih << '    <h2><a href="#{url}">#{name}</a><span class="point">#{point}</span></h2>'
           ih << '  </header>'
           ih << '</article>'
           ih = cur_item.render_loop_html(item, html: ih.join("\n"))
