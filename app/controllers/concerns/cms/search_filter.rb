29c2ad3ee679fe4a551087f27168944bb29fab07
itowtips <ito@web-tips.co.jp>
2015-02-02 14:06:15 +0900

    fix: pagination in related page modal

--- app/controllers/concerns/cms/search_filter.rb
+++ app/controllers/concerns/cms/search_filter.rb
@@ -8,6 +8,7 @@ module Cms::SearchFilter
     def index
       @items = @model.site(@cur_site).
         search(params[:s]).
-        order_by(_id: -1)
+        order_by(_id: -1).
+        page(params[:page]).per(50)
     end
 end
