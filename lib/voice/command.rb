34a28abf77dcfdb94b777218075cc725a19991f1
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-02-17 18:01:09 +0900

    [#197] use capture3 instead of popen3.

--- lib/voice/command.rb
+++ lib/voice/command.rb
@@ -4,22 +4,10 @@ class Voice::Command
       def run_with_logging(cmd, prompt)
         require "open3"
         Rails.logger.debug("popen3: #{cmd}")
-        Open3.popen3(cmd) do |i, o, e, t|
-          Thread.new do
-            o.each_line do |line|
-              Rails.logger.debug("[#{prompt}] #{line}")
-            end
-          end
-          Thread.new do
-            e.each_line do |line|
-              Rails.logger.info("[#{prompt}] #{line}")
-            end
-          end
-          i.write ''
-          i.close
-          t.join
-          t.value
-        end
+        stdout, stderr, status = Open3.capture3(cmd)
+        Rails.logger.debug("[#{prompt} stdout] #{stdout}") if stdout.present?
+        Rails.logger.info("[#{prompt} stderr] #{stderr}") if stderr.present?
+        status
       end
   end
 end
