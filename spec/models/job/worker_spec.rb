b2dd075b28857ee28f007f39ec0aaf6798c2a0ac
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-03-04 11:50:16 +0900

    fix test failure.

--- spec/models/job/worker_spec.rb
+++ spec/models/job/worker_spec.rb
@@ -9,15 +9,17 @@ describe Job::Worker do
     end
   end
 
+  jobs = []
+
   describe ".call_async" do
     it "creates Job::Model" do
-      expect { model.call_async }.to change { Job::Model.count }.by(1)
+      expect { jobs << model.call_async }.to change { Job::Model.count }.by(1)
     end
   end
 
   describe ".call_in" do
     it "creates Job::Model" do
-      expect { model.call_in 2.minutes.from_now }.to change { Job::Model.count }.by(1)
+      expect { jobs << model.call_in(2.minutes.from_now) }.to change { Job::Model.count }.by(1)
     end
   end
 
@@ -26,5 +28,9 @@ describe Job::Worker do
       expect(model.job_options).not_to be_nil
       expect(model.job_options).to be_a(Hash)
     end
+  end
+
+  after(:all) do
+    jobs.each(&:delete)
   end
 end
