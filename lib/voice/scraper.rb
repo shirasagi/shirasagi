2298db63ba20a904a45bcef84aa93ccb9a46a63d
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-03-11 17:57:58 +0900

    [#264] fix issue.

--- lib/voice/scraper.rb
+++ lib/voice/scraper.rb
@@ -18,6 +18,7 @@ class Voice::Scraper
       if html =~ /<!--[^>]*?\s#{@skip_marks[0]}\s[^>]*?-->/i
         html.gsub!(/<!--[^>]*?\s#{@skip_marks[0]}\s[^>]*?-->(.*?)<!--[^>]*?\s#{@skip_marks[1]}\s[^>]*?-->/im, '')
       end
+      html.gsub!(/<!--[\s\S]*?-->/m, '')
       html.gsub!(/<\s*(#{@delete_tags.join("|")})(\s|>).*?<\/\s*\1\s*>/im, '')
 
       # <img> tag's special case
