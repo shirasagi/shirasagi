9940f586581db9870aa03742d37f6ab0b0ab9aea
tsutsuitec <m.tsutsui@tec-inf.co.jp>
2015-03-16 15:17:37 +0900

    change: app name, add: apps dsselect

---
+++ app/controllers/opendata/dataset_selects_controller.rb
@@ -0,0 +1,19 @@
+class Opendata::DatasetSelectsController < ApplicationController
+  include SS::AjaxFilter
+
+  before_action :set_site
+
+  private
+    def set_site
+      host = request.env["HTTP_X_FORWARDED_HOST"] || request.env["HTTP_HOST"]
+      @cur_site ||= SS::Site.find_by_domain host
+
+
+      @model = Opendata::Dataset
+    end
+
+  public
+    def index
+      @items = @model.site(@cur_site).search(params[:s]).order_by(_id: -1)
+    end
+end
