78b9ef94b89a18e548b24d8bceee54e48c49aff0
itowtips <ito@web-tips.co.jp>
2015-03-16 14:55:51 +0900

    fix: workflow routing

--- config/routes/workflow/routes.rb
+++ config/routes/workflow/routes.rb
@@ -4,12 +4,12 @@ SS::Application.routes.draw do
 
   concern :deletion do
     get :delete, on: :member
-  end
-
-  concern :workflow do
     post :request_update, on: :member
     post :approve_update, on: :member
     post :remand_update, on: :member
+  end
+
+  concern :branch do
     post :branch_create, on: :member
   end
 
@@ -24,13 +24,13 @@ SS::Application.routes.draw do
 
   namespace "workflow", path: ".:site/workflow" do
     get "/" => "main#index"
-    resources :pages, concerns: [:deletion, :workflow]
+    resources :pages, concerns: [:deletion, :branch]
     get "/search_approvers" => "search_approvers#index"
     resources :routes, concerns: :deletion
-    # get "/wizard/:id/approver_setting" => "wizard#approver_setting"
-    # post "/wizard/:id/approver_setting" => "wizard#approver_setting"
-    # get "/wizard/:id" => "wizard#index"
-    # post "/wizard/:id" => "wizard#index"
+    get "/wizard/:id/approver_setting" => "wizard#approver_setting"
+    post "/wizard/:id/approver_setting" => "wizard#approver_setting"
+    get "/wizard/:id" => "wizard#index"
+    post "/wizard/:id" => "wizard#index"
   end
 
 end
