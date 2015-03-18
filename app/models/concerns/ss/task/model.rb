c1bee2023fcbfa3eb49d677d7421dd6b8be0e224
tany <tanizawa@web-tips.co.jp>
2015-03-18 20:10:13 +0900

    GC.start on tasks

--- app/models/concerns/ss/task/model.rb
+++ app/models/concerns/ss/task/model.rb
@@ -58,6 +58,7 @@ module SS::Task::Model
         save
         interrupt = self.class.find_by(id: id, select: interrupt).interrupt
         raise Interrupt, "interrupted: stop" if interrupt.to_s == "stop"
+        GC.start
       end
       self
     end
