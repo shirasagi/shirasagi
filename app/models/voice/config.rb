e3739ddffdb5ca5e356916da0c42efd51e88e870
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-03-25 19:24:21 +0900

    [#298] add default configuration value.

--- app/models/voice/config.rb
+++ app/models/voice/config.rb
@@ -13,6 +13,39 @@ class Voice::Config
     {
       disable: false,
       root: DEFAULT_ROOT,
+      download: {
+        "max_attempts" => 3,
+        "initial_wait" => 3,
+        "timeout_sec" => 10,
+      },
+      controller: {
+        "location" => "/.voice",
+        "retry_after" => 5
+      },
+      resource: {
+        "loading" => "/assets/voice/voice-loading.mp3",
+        "disabled" => "/assets/voice/voice-disabled.mp3",
+        "overload" => "/assets/voice/voice-overload.mp3"
+      },
+      scraper: {
+        "voice-marks" => [ "read-voice", "end-read-voice" ],
+        "skip-marks" => [ "skip-voice", "end-skip-voice" ],
+        "delete-tags" => %w(style script noscript iframe rb rp),
+        "kuten-tags" => %w(h1 h2 h3 h4 h5 p br div pre blockquote ul ol table)
+      },
+      type: "openjtalk",
+      openjtalk: {
+        "bin" => "/usr/local/bin/open_jtalk",
+        "dic" => "/usr/local/dic",
+        "voice" => "config/voices/mei_normal/mei_normal.htsvoice",
+        "opts" => "-s 48000 -p 200 -u 0.5 -jm 0.5 -jf 0.5",
+        "max_length" => 1024,
+        "sox" => "/usr/local/bin/sox"
+      },
+      lame: {
+        "bin" => "/usr/local/bin/lame",
+        "opts" => "--scale 5 --silent"
+      },
     }
   end
 end
