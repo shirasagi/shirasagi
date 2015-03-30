6111ceeef5a3972be05fd950d9ede179ddc9858f
ka <ka.kaosf@gmail.com>
2015-03-30 16:35:51 +0900

    Remove the warning of "already initialized constant SS::Migration::DIR"
    ref.
      http://docs.ruby-lang.org/ja/2.2.0/method/Module/i/remove_const.html
      http://docs.ruby-lang.org/ja/2.2.0/class/Module.html#I_CLASS_EVAL

--- spec/models/ss/migration_spec.rb
+++ spec/models/ss/migration_spec.rb
@@ -30,6 +30,10 @@ RSpec.describe SS::Migration, type: :model, dbscope: :example do
       touch 'tmp/lib/migrations/mod1/20150324000001_a.rb'
       touch 'tmp/lib/migrations/mod1/20150324000002_a.rb'
       touch 'tmp/lib/migrations/mod2/20150324000003_a.rb'
+      # ref.
+      #   http://docs.ruby-lang.org/ja/2.2.0/method/Module/i/remove_const.html
+      #   http://docs.ruby-lang.org/ja/2.2.0/class/Module.html#I_CLASS_EVAL
+      SS::Migration.class_eval { remove_const :DIR }
       SS::Migration::DIR = Rails.root.join 'tmp/lib/migrations'
     end
 
