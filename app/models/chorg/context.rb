e149b1a2f2fda1a20b853cf00303d6e230f89b64
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-03-30 19:16:04 +0900

    refactor chorg/runner.rb with rubocop

---
+++ app/models/chorg/context.rb
@@ -0,0 +1,19 @@
+module Chorg::Context
+  attr_reader :cur_site, :cur_user, :adds_group_to_site, :item
+  attr_reader :results, :substituter, :validation_substituter, :delete_group_ids
+
+  def init_context
+    @results = { "add" => { "success" => 0, "failed" => 0 },
+                 "move" => { "success" => 0, "failed" => 0 },
+                 "unify" => { "success" => 0, "failed" => 0 },
+                 "division" => { "success" => 0, "failed" => 0 },
+                 "delete" => { "success" => 0, "failed" => 0 } }
+    @substituter = Chorg::Substituter.new
+    @validation_substituter = Chorg::Substituter.new
+    @delete_group_ids = []
+  end
+
+  def inc_counter(method, type)
+    @results["#{method}"]["#{type}"] += 1
+  end
+end
