e9964e044753d207b744f0f252696d4703c0856e
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-03-31 13:01:55 +0900

    add workflow js test.

--- spec/features/workflow/routes_spec.rb
+++ spec/features/workflow/routes_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe "workflow_routes" do
-  subject(:site) { cms_site }
-  subject(:user) { cms_user }
-  subject(:group) { cms_group }
-  subject(:index_path) { workflow_routes_path site.host }
-  subject(:new_path) { new_workflow_route_path site.host }
+describe "workflow_routes", dbscope: :example do
+  let!(:site) { cms_site }
+  let!(:user) { cms_user }
+  let!(:group) { cms_group }
+  let(:index_path) { workflow_routes_path site.host }
+  let(:new_path) { new_workflow_route_path site.host }
 
   it "without login" do
     visit index_path
@@ -21,29 +21,41 @@ describe "workflow_routes" do
   context "with auth" do
     before { login_cms_user }
 
-    it "#index" do
-      visit index_path
-      expect(current_path).not_to eq sns_login_path
+    describe "#index" do
+      it do
+        visit index_path
+        expect(current_path).not_to eq sns_login_path
+      end
     end
 
-    # it "#new" do
-    #   visit new_path
-    #   within "form#item-form" do
-    #     fill_in "item[name]", with: "sample"
-    #   end
-    #   click_link "グループを選択する"
-    #   click_link group.name
-    #   save_and_open_page
-    #   print page.html
-    #   click_link "承認者を選択する"
-    #   click_link user.name
-    #   within "form#item-form" do
-    #     click_button "保存"
-    #   end
-    #   expect(status_code).to eq 200
-    #   expect(current_path).not_to eq new_path
-    #   expect(page).not_to have_css("form#item-form")
-    # end
-  end
+    describe "#new", js: true do
+      it do
+        visit new_path
+        within "form#item-form" do
+          fill_in "item[name]", with: "sample"
+        end
 
+        click_link "グループを選択する"
+        wait_for_cbox
+        within "div#ajax-box table.index" do
+          click_link group.name
+        end
+
+        within "dl.workflow-level-1" do
+          click_link "承認者を選択する"
+        end
+        # wait a while to load contents of dialog
+        wait_for_cbox
+        within "div#ajax-box table.index" do
+          click_link user.name
+        end
+
+        within "form#item-form" do
+          click_button "保存"
+        end
+        expect(status_code).to eq 200
+        expect(page).not_to have_css("form#item-form")
+      end
+    end
+  end
 end
