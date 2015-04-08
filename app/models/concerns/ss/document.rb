e0af15056bbfa1fe9c273e72ddd76d8dbf5cb3d1
tany <una@kimiyure.com>
2015-04-08 18:59:31 +0900

    fix tooltip design

--- app/models/concerns/ss/document.rb
+++ app/models/concerns/ss/document.rb
@@ -52,7 +52,7 @@ module SS::Document
       end
       return msg if msg.blank? || !html_wrap
       msg = [msg] if msg.class.to_s == "String"
-      list = msg.map {|d| "<li>" + d.gsub(/\r\n|\n/, "<br />") + "</li>"}
+      list = msg.map {|d| "<li>" + d.to_s.gsub(/\r\n|\n/, "<br />") + "<br /></li>"}
 
       h  = []
       h << %(<div class="tooltip">?)
