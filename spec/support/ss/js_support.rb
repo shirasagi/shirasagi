308741340bd7253edd1a833f05463ee599d01d09
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-04-10 20:04:57 +0900

    refactor with rubocop

--- spec/support/ss/js_support.rb
+++ spec/support/ss/js_support.rb
@@ -2,7 +2,7 @@ module SS
   module JsSupport
     def wait_for_ajax(selector, timeout = 10)
       start_at = Time.now.to_i
-      while !page.has_selector?(selector) && (Time.now.to_i - start_at) < timeout do
+      while !page.has_selector?(selector) && (Time.now.to_i - start_at) < timeout
         sleep 0.1
       end
     end
