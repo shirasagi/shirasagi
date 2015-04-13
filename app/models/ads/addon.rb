bf410bd45b855e76dca23512947153c02eb9e619
tany <una@kimiyure.com>
2015-04-13 18:55:22 +0900

    add ads options #373

--- app/models/ads/addon.rb
+++ app/models/ads/addon.rb
@@ -6,13 +6,23 @@ module Ads::Addon
     set_order 200
 
     included do
+      field :link_action, type: String
+      field :link_target, type: String
       field :sort, type: String
       field :upper_html, type: String
       field :lower_html, type: String
-      permit_params :sort, :upper_html, :lower_html
+      permit_params :link_action, :link_target, :sort, :upper_html, :lower_html
     end
 
     public
+      def link_action_options
+        [ ["クッションページ", ""], ["直リンク", "direct"] ]
+      end
+
+      def link_target_options
+        [ ["同じタブで表示", ""], ["別のタブで表示", "_blank"] ]
+      end
+
       def sort_options
         [ ["指定順", "order"], ["ランダム", "random"] ]
       end
