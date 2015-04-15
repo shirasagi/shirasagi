65b04111cab1e24d2a0a6ae046711173e2d6ed3d
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-04-15 14:44:09 +0900

    refactor helper rspec by specifying `type: :helper` which enables rspec-rails's extension.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,17 +1,14 @@
 require 'spec_helper'
 
-describe ApplicationHelper do
-  include ActionView::Helpers
-  include ApplicationHelper
-
+describe ApplicationHelper, type: :helper do
   describe ".mail_to_entity" do
     context "when only email address is given" do
-      subject { mail_to_entity("test@example.jp") }
+      subject { helper.mail_to_entity("test@example.jp") }
       it { is_expected.to eq '<a href="mailto:test&#64;example&#46;jp">test&#64;example&#46;jp</a>' }
     end
 
     context "when email address and name is given" do
-      subject { mail_to_entity("test@example.jp", "テスト") }
+      subject { helper.mail_to_entity("test@example.jp", "テスト") }
       it { is_expected.to eq '<a href="mailto:test&#64;example&#46;jp">テスト</a>' }
     end
   end
