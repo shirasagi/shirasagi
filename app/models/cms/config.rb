1ac4c90981779711ef10b4f7e3cd48ffecc3577b
tany <una@kimiyure.com>
2015-04-22 18:15:38 +0900

    fix map.center #390

--- app/models/cms/config.rb
+++ app/models/cms/config.rb
@@ -2,7 +2,8 @@ class Cms::Config
   cattr_reader(:default_values) do
     {
       serve_static_pages: true,
-      html_editor: "ckeditor"
+      html_editor: "ckeditor",
+      map_center: [36.204824, 138.252924],
     }
   end
 end
