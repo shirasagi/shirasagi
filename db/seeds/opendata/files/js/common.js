1c515be547a0264fead8a568d2bfc970c388f241
ayano <a.yoshikawa@tec-inf.co.jp>
2015-04-22 16:32:37 +0900

    update:portal-about

--- db/seeds/opendata/files/js/common.js
+++ db/seeds/opendata/files/js/common.js
@@ -31,3 +31,16 @@ $(function(){
         }
     });
 });
+
+function showPlagin(idno){
+    pc = ('PlagClose' + (idno));
+    po = ('PlagOpen' + (idno));
+    if( document.getElementById(pc).style.display == "none" ) {
+      document.getElementById(pc).style.display = "block";
+      document.getElementById(po).style.display = "none";
+    }
+    else {
+      document.getElementById(pc).style.display = "none";
+      document.getElementById(po).style.display = "block";
+    }
+}
