18524d33efdf6dada7da0a31d4e9ab0d0e6a75a9
tany <tanizawa@web-tips.co.jp>
2015-04-28 00:38:41 +0900

    disable config/rb_config.rb #398

--- config/initializers/ss/rb_config.rb
+++ config/initializers/ss/rb_config.rb
@@ -1,3 +1,3 @@
 #> Use RbConfig instead of obsolete and deprecated Config.
-Object.send :remove_const, :Config if defined? Object::Config
-Config = RbConfig
+#Object.send :remove_const, :Config if defined? Object::Config
+#Config = RbConfig
