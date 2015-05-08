fe36935cc9a9ec003c7c948e5ab9e2d21a78f3fd
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-05-08 18:35:27 +0900

    add Opendata::App spec.

--- spec/factories/opendata/pages.rb
+++ spec/factories/opendata/pages.rb
@@ -9,6 +9,16 @@ FactoryGirl.define do
     dataset_group_ids [1]
   end
 
+  factory :opendata_app, class: Opendata::App, traits: [:cms_page] do
+    filename { "dir/#{unique_id}" }
+    route "opendata/app"
+    text "aaaa\naaaa"
+    tags ["aaa", "bbb"]
+    category_ids [1]
+    area_ids [1]
+    license { unique_id }
+  end
+
   factory :opendata_resource, class: Opendata::Resource do
     name { "#{unique_id}" }
     text "bbbb\nbbbb"
---
+++ spec/models/opendata/app_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+
+describe Opendata::App, dbscope: :example do
+  context "check attributes with typical url resource" do
+    subject { create(:opendata_app) }
+    its(:becomes_with_route) { is_expected.not_to be_nil }
+    its(:dirname) { is_expected.to eq "dir" }
+    its(:basename) { is_expected.to eq subject.filename.split('/').last }
+    its(:path) { is_expected.to end_with  "/#{subject.dirname}/#{subject.basename}" }
+    its(:url) { is_expected.to eq "/#{subject.dirname}/#{subject.basename}" }
+    its(:full_url) { is_expected.to eq "http://#{cms_site.domain}/#{subject.dirname}/#{subject.basename}" }
+    its(:parent) { is_expected.to eq nil }
+    its(:point_url) { is_expected.to eq "#{subject.url}/point/show.html" }
+    its(:point_add_url) { is_expected.to eq "#{subject.url}/point/add.html" }
+    its(:point_members_url) { is_expected.to eq "#{subject.url}/point/members.html" }
+    its(:app_ideas_url) { is_expected.to eq "#{subject.url}/ideas/show.html" }
+    its(:zip_url) { is_expected.to eq "#{subject.url}/zip" }
+    its(:executed_show_url) { is_expected.to eq "#{subject.url}/executed/show.html" }
+    its(:executed_add_url) { is_expected.to eq "#{subject.url}/executed/add.html" }
+    its(:contact_present?) { is_expected.to be_falsey }
+  end
+
+  describe ".sort_options" do
+    it { expect(described_class.sort_options).to include %w(新着順 released) }
+  end
+
+  describe ".sort_hash" do
+    it { expect(described_class.sort_hash("released")).to include(released: -1).and include(_id: -1) }
+    it { expect(described_class.sort_hash("popular")).to include(point: -1).and include(_id: -1) }
+    it { expect(described_class.sort_hash("attention")).to include(executed: -1).and include(_id: -1) }
+    it { expect(described_class.sort_hash("")).to include(released: -1) }
+    it { expect(described_class.sort_hash("foobar")).to include("foobar" => 1) }
+  end
+
+  describe ".aggregate_field" do
+    it { expect(described_class.aggregate_field(:license, limit: 10)).to be_empty }
+  end
+
+  describe ".aggregate_array" do
+    it { expect(described_class.aggregate_array(:tags, limit: 10)).to be_empty }
+  end
+
+  describe ".get_tag_list" do
+    it { expect(described_class.get_tag_list(nil)).to be_empty }
+  end
+
+  describe ".get_tag" do
+    it { expect(described_class.get_tag("タグ")).to be_empty }
+  end
+
+  describe ".search" do
+    let(:name_keyword_matcher) do
+      include("$and" => include("$or" => include("name" => /\Qキーワード\E/i).and(include("text" => /\Qキーワード\E/i))))
+    end
+    it { expect(described_class.search({}).selector.to_h).to include("route" => "opendata/app") }
+    it { expect(described_class.search(keyword: "キーワード").selector.to_h).to include("$and") }
+    it { expect(described_class.search(name: true, keyword: "キーワード").selector.to_h).to name_keyword_matcher }
+    it { expect(described_class.search(tag: "タグ").selector.to_h).to include("tags" => "タグ") }
+    it { expect(described_class.search(area_id: "43").selector.to_h).to include("area_ids" => 43) }
+    it { expect(described_class.search(category_id: "56").selector.to_h).to include("category_ids" => 56) }
+    it { expect(described_class.search(license: "ライセンス").selector.to_h).to include("license" => "ライセンス") }
+  end
+end
