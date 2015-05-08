837393efa1db208fa304172ce27610695bf946c3
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-05-08 17:31:59 +0900

    add Opendata::License spec.

---
+++ spec/models/opendata/license_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Opendata::License, dbscope: :example do
+  context "check attributes with typical url resource" do
+    let(:site) { cms_site }
+    let(:license_logo_file_path) { Rails.root.join("spec", "fixtures", "ss", "logo.png") }
+    let(:license_logo_file) { Fs::UploadedFile.create_from_file(license_logo_file_path, basename: "spec") }
+    subject { create(:opendata_license, site: site, file: license_logo_file) }
+    its(:state_options) { is_expected.to include %w(非公開 closed) }
+  end
+
+  describe ".public" do
+    it { expect(described_class.public.selector.to_h).to include("state" => "public") }
+  end
+
+  describe ".search" do
+    it { expect(described_class.search(nil).selector.to_h).to be_empty }
+    it do
+      expect(described_class.search(keyword: "キーワード").selector.to_h).to \
+        include("$and" => include("$or" => include("name" => /\Qキーワード\E/i)))
+    end
+  end
+end
