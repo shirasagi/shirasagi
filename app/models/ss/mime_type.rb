a8d90fb70b046d382b77f0383acf9eb41817801f
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-05-18 11:28:15 +0900

    [#434] add mime type configuration.

--- app/models/ss/mime_type.rb
+++ app/models/ss/mime_type.rb
@@ -3,10 +3,12 @@ require "mime/types"
 class SS::MimeType
   class << self
     def find(name, default)
-      mime_types = ::MIME::Types.type_for(name)
-      ret ||= mime_types.first.content_type rescue nil if mime_types.present?
-      ret ||= default
-      ret
+      ret = SS.config.env.mime_type_map[name.sub(/.*\./, "")]
+      if ret.blank?
+        mime_types = ::MIME::Types.type_for(name)
+        ret = mime_types.first.content_type rescue nil if mime_types.present?
+      end
+      ret || default
     end
   end
 end
