9be89b74929ad541a8de1d4a43d77c63498c0577
tsutsuitec <m.tsutsui@tec-inf.co.jp>
2015-05-19 21:59:15 +0900

    add: rspec my_apps

---
+++ spec/features/opendata/my_apps_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "opendata_my_apps" do
+  let(:site) { cms_site }
+  let(:node) { create_once :opendata_node_my_app, name: "opendata_my_app" }
+
+  subject(:index_path) { opendata_my_apps_path site.host, node }
+
+  context "my_apps" do
+    before { login_cms_user }
+
+    it "#index" do
+      visit index_path
+      expect(current_path).not_to eq sns_login_path
+    end
+  end
+end
