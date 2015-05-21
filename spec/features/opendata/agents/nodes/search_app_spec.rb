b31d19ca4c04409438b4c51c3726a6f2e3e38da3
tsutsuitec <m.tsutsui@tec-inf.co.jp>
2015-05-21 19:31:56 +0900

    add: rspec

---
+++ spec/features/opendata/agents/nodes/search_app_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe "opendata_search_apps", dbscope: :example do
+  let(:site) { cms_site }
+  let(:node) { create_once :opendata_node_search_app, name: "opendata_search_apps" }
+
+  let(:index_path) { "#{node.url}index.html" }
+  let(:rss_path) { "#{node.url}rss.xml" }
+
+  context "search_app" do
+    it "#index" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+        session.env("REQUEST_PATH", index_path)
+        visit index_path
+        expect(current_path).to eq index_path
+      end
+    end
+
+    it "#index released" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+        session.env("REQUEST_PATH", index_path)
+        visit "#{index_path}?&sort=released"
+        expect(current_path).to eq index_path
+      end
+    end
+
+    it "#index popular" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+        session.env("REQUEST_PATH", index_path)
+        visit "#{index_path}?&sort=popular"
+        expect(current_path).to eq index_path
+      end
+    end
+
+    it "#index attention" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+        session.env("REQUEST_PATH", index_path)
+        visit "#{index_path}?&sort=attention"
+        expect(current_path).to eq index_path
+      end
+    end
+
+    it "#rss" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+        session.env("REQUEST_PATH", rss_path)
+        visit rss_path
+        expect(current_path).to eq rss_path
+      end
+    end
+  end
+end
---
+++ spec/features/opendata/search_apps_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "opendata_search_apps", dbscope: :example do
+  let(:site) { cms_site }
+  let(:node) { create_once :opendata_node_search_app, name: "opendata_search_apps" }
+
+  let(:index_path) { opendata_search_apps_path site.host, node }
+
+  context "search_app" do
+    before { login_cms_user }
+
+    it "#index" do
+      visit index_path
+      expect(current_path).not_to eq sns_login_path
+    end
+  end
+end
