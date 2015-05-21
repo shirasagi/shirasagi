9f94a338a0f2e9217259815d573c90f519f12353
tsutsuitec <m.tsutsui@tec-inf.co.jp>
2015-05-22 08:12:52 +0900

    add: rspec

---
+++ spec/features/opendata/dataset_selects_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "opendata_dataset_selects" do
+  #before do
+  #  ss_site
+  #end
+  let(:site) { ss_site }
+  let(:node) { create_once :opendata_node_dataset, name: "opendata_dataset_selects" }
+  let(:dataset) { create(:opendata_dataset, node: node) }
+
+  let(:index_path) { "/datasets/select"}
+
+  it "#index" do
+    page.driver.browser.with_session("public") do |session|
+      session.env("HTTP_X_FORWARDED_HOST", site.domain)
+      session.env("REQUEST_PATH", index_path)
+      visit index_path
+      expect(current_path).to eq index_path
+    end
+  end
+end
