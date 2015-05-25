233a1f24bdb9dfb334c6752eccf1da893431f557
tecoda <y.oda@tec-inf.co.jp>
2015-05-25 10:24:50 +0900

    update: idea_spec.rb

--- spec/features/opendata/agents/nodes/idea_spec.rb
+++ spec/features/opendata/agents/nodes/idea_spec.rb
@@ -13,6 +13,9 @@ describe "opendata_agents_nodes_idea", dbscope: :example do
   let(:index_path) { "#{node.url}index.html" }
   let(:show_point_path) { "#{node.url}#{idea.id}/point.html" }
   let(:add_point_path) { "#{node.url}#{idea.id}/point.html" }
+  let(:show_comment_path) { "#{node.url}#{idea.id}/comment/show.html" }
+  let(:show_dataset_path) { "#{node.url}#{idea.id}/dataset/show.html" }
+  let(:show_app_path) { "#{node.url}#{idea.id}/app/show.html" }
   let(:rss_path) { "#{node.url}rss.xml" }
   let(:index_areas_path) { "#{node.url}areas.html" }
   let(:index_tags_path) { "#{node.url}tags.html" }
@@ -30,7 +33,7 @@ describe "opendata_agents_nodes_idea", dbscope: :example do
     page.driver.browser.with_session("public") do |session|
       session.env("HTTP_X_FORWARDED_HOST", site.domain)
       session.env("REQUEST_PATH", show_point_path)
-      session.env("method", "POST")
+#      session.env("method", "POST")
       visit show_point_path
       expect(current_path).to eq show_point_path
     end
@@ -42,6 +45,33 @@ describe "opendata_agents_nodes_idea", dbscope: :example do
       session.env("REQUEST_PATH", rss_path)
       visit rss_path
       expect(current_path).to eq rss_path
+    end
+  end
+
+  it "#show_comment" do
+    page.driver.browser.with_session("public") do |session|
+      session.env("HTTP_X_FORWARDED_HOST", site.domain)
+      session.env("REQUEST_PATH", show_comment_path)
+      visit show_comment_path
+      expect(current_path).to eq show_comment_path
+    end
+  end
+
+  it "#show_dataset" do
+    page.driver.browser.with_session("public") do |session|
+      session.env("HTTP_X_FORWARDED_HOST", site.domain)
+      session.env("REQUEST_PATH", show_dataset_path)
+      visit show_dataset_path
+      expect(current_path).to eq show_dataset_path
+    end
+  end
+
+  it "#show_app" do
+    page.driver.browser.with_session("public") do |session|
+      session.env("HTTP_X_FORWARDED_HOST", site.domain)
+      session.env("REQUEST_PATH", show_app_path)
+      visit show_app_path
+      expect(current_path).to eq show_app_path
     end
   end
 
