05f0c2d46d8ec65079fab32f41b1be9513abe13f
tecoda <y.oda@tec-inf.co.jp>
2015-05-25 15:16:47 +0900

    update: search_idea_spec.rb

--- spec/features/opendata/agents/nodes/search_idea_spec.rb
+++ spec/features/opendata/agents/nodes/search_idea_spec.rb
@@ -44,6 +44,56 @@ describe "opendata_search_ideas", dbscope: :example do
       end
     end
 
+    it "#keyword_input" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+        session.env("REQUEST_PATH", index_path)
+        visit index_path
+        fill_in "s_keyword", with: "アイデア"
+        click_button "検索"
+
+        expect(status_code).to eq 200
+      end
+    end
+
+    it "#category_select" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+        session.env("REQUEST_PATH", index_path)
+        visit index_path
+        # select "くらし", from: "s_category_id"
+        select "", from: "s_category_id"
+        click_button "検索"
+
+        expect(status_code).to eq 200
+      end
+    end
+
+    it "#area_select" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+        session.env("REQUEST_PATH", index_path)
+        visit index_path
+        # select "徳島県", from: "s_area_id"
+        select "", from: "s_area_id"
+        click_button "検索"
+
+        expect(status_code).to eq 200
+      end
+    end
+
+    it "#tag_input" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+        session.env("REQUEST_PATH", index_path)
+        visit index_path
+        fill_in "s_tag", with: "テスト"
+        click_button "検索"
+
+        expect(status_code).to eq 200
+      end
+    end
+
     it "#rss" do
       page.driver.browser.with_session("public") do |session|
         session.env("HTTP_X_FORWARDED_HOST", site.domain)
