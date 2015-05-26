5bf62bb6f4ac62424f4ad75a670ca9a80fe904de
tecoda <y.oda@tec-inf.co.jp>
2015-05-26 17:04:29 +0900

    fix: sort

--- app/controllers/opendata/agents/nodes/search_idea_controller.rb
+++ app/controllers/opendata/agents/nodes/search_idea_controller.rb
@@ -15,7 +15,7 @@ class Opendata::Agents::Nodes::SearchIdeaController < ApplicationController
       when "popular"
         sort = { point: -1 }
       when "attention"
-        sort = { downloaded: -1 }
+        sort = { commented: -1 }
       else
         sort = { released: -1 }
       end
