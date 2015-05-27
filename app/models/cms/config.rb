180d1b2e486ebaee7891d9ca742b62e1ea8e9a58
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-05-27 20:41:57 +0900

    use '-' instead of '_' for img filename.

--- app/models/cms/config.rb
+++ app/models/cms/config.rb
@@ -4,7 +4,7 @@ class Cms::Config
       serve_static_pages: true,
       html_editor: "ckeditor",
       map_center: [36.204824, 138.252924],
-      editor_template_thumb: '/assets/img/editor_template.png',
+      editor_template_thumb: '/assets/img/editor-template.png',
       map_max_point_form: 10,
     }
   end
