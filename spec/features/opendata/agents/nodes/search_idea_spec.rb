bed34660c43080688686d598dd7addb3c398a8bc
tecoda <y.oda@tec-inf.co.jp>
2015-05-27 10:08:59 +0900

    update: agents/nodes/search_idea_spec.rb

--- spec/features/opendata/agents/nodes/search_idea_spec.rb
+++ spec/features/opendata/agents/nodes/search_idea_spec.rb
@@ -6,12 +6,16 @@ describe "opendata_search_ideas", dbscope: :example do
 
   let(:index_path) { "#{node.url}index.html" }
   let(:rss_path) { "#{node.url}rss.xml" }
+  let!(:node_category) { create_once :opendata_node_category, name: 'カテゴリー０１' }
+  let!(:node_area) { create_once :opendata_node_area, name: '地域Ａ' }
 
   context "search_idea" do
+
     it "#index" do
       page.driver.browser.with_session("public") do |session|
         session.env("HTTP_X_FORWARDED_HOST", site.domain)
         session.env("REQUEST_PATH", index_path)
+
         visit index_path
         expect(current_path).to eq index_path
       end
@@ -21,6 +25,7 @@ describe "opendata_search_ideas", dbscope: :example do
       page.driver.browser.with_session("public") do |session|
         session.env("HTTP_X_FORWARDED_HOST", site.domain)
         session.env("REQUEST_PATH", index_path)
+
         visit "#{index_path}?&sort=released"
         expect(current_path).to eq index_path
       end
@@ -30,6 +35,7 @@ describe "opendata_search_ideas", dbscope: :example do
       page.driver.browser.with_session("public") do |session|
         session.env("HTTP_X_FORWARDED_HOST", site.domain)
         session.env("REQUEST_PATH", index_path)
+
         visit "#{index_path}?&sort=popular"
         expect(current_path).to eq index_path
       end
@@ -39,6 +45,7 @@ describe "opendata_search_ideas", dbscope: :example do
       page.driver.browser.with_session("public") do |session|
         session.env("HTTP_X_FORWARDED_HOST", site.domain)
         session.env("REQUEST_PATH", index_path)
+
         visit "#{index_path}?&sort=attention"
         expect(current_path).to eq index_path
       end
@@ -48,10 +55,10 @@ describe "opendata_search_ideas", dbscope: :example do
       page.driver.browser.with_session("public") do |session|
         session.env("HTTP_X_FORWARDED_HOST", site.domain)
         session.env("REQUEST_PATH", index_path)
+
         visit index_path
         fill_in "s_keyword", with: "アイデア"
         click_button "検索"
-
         expect(status_code).to eq 200
       end
     end
@@ -60,11 +67,10 @@ describe "opendata_search_ideas", dbscope: :example do
       page.driver.browser.with_session("public") do |session|
         session.env("HTTP_X_FORWARDED_HOST", site.domain)
         session.env("REQUEST_PATH", index_path)
-        visit index_path
-        # select "くらし", from: "s_category_id"
-        select "", from: "s_category_id"
-        click_button "検索"
 
+        visit index_path
+        select node_category.name
+        click_button "検索"
         expect(status_code).to eq 200
       end
     end
@@ -73,11 +79,10 @@ describe "opendata_search_ideas", dbscope: :example do
       page.driver.browser.with_session("public") do |session|
         session.env("HTTP_X_FORWARDED_HOST", site.domain)
         session.env("REQUEST_PATH", index_path)
-        visit index_path
-        # select "徳島県", from: "s_area_id"
-        select "", from: "s_area_id"
-        click_button "検索"
 
+        visit index_path
+        select node_area.name
+        click_button "検索"
         expect(status_code).to eq 200
       end
     end
@@ -86,10 +91,10 @@ describe "opendata_search_ideas", dbscope: :example do
       page.driver.browser.with_session("public") do |session|
         session.env("HTTP_X_FORWARDED_HOST", site.domain)
         session.env("REQUEST_PATH", index_path)
+
         visit index_path
         fill_in "s_tag", with: "テスト"
         click_button "検索"
-
         expect(status_code).to eq 200
       end
     end
@@ -98,6 +103,7 @@ describe "opendata_search_ideas", dbscope: :example do
       page.driver.browser.with_session("public") do |session|
         session.env("HTTP_X_FORWARDED_HOST", site.domain)
         session.env("REQUEST_PATH", rss_path)
+
         visit rss_path
         expect(current_path).to eq rss_path
       end
