859b91cbbac898d181bb531d57ac105d3d0e6573
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-05-28 11:46:00 +0900

    add opendata/agents/parts/dataset_group spec

--- spec/factories/opendata/dataset_groups.rb
+++ spec/factories/opendata/dataset_groups.rb
@@ -7,8 +7,8 @@ FactoryGirl.define do
     end
 
     name { "#{unique_id}" }
-    site_id { site.present? ? site.id : nil }
+    site_id { site.present? ? site.id : cms_site.id }
     user_id { user.present? ? user.id : nil }
-    category_ids { categories.present? ? categories.map(&:_id) : nil }
+    category_ids { categories.present? ? categories.map(&:_id).to_a : nil }
   end
 end
---
+++ spec/features/opendata/agents/parts/dataset_group_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "opendata_agents_nodes_dataset_group", dbscope: :example do
+  let(:site) { cms_site }
+  let!(:category) { create :opendata_node_category, basename: "bunya/kurashi" }
+  let!(:dataset_group) { create :opendata_dataset_group, categories: [ category ] }
+  let(:part) { create :opendata_part_dataset_group }
+  before do
+    create :opendata_node_search_dataset, basename: "dataset/search"
+    create :opendata_node_search_dataset_group
+  end
+
+  let(:index_url) { ::URI.parse "http://#{site.domain}#{part.url}" }
+
+  describe "#index" do
+    it do
+      visit index_url
+      expect(status_code).to eq 200
+      expect(current_path).to eq index_url.path
+      expect(page).to have_link dataset_group.name
+    end
+  end
+end
