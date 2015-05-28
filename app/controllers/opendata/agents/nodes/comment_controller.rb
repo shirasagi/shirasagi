5d82f4201d8e4ccbc462241376de9b5d72413478
tecoda <y.oda@tec-inf.co.jp>
2015-05-28 10:51:35 +0900

    change: comment routing

--- app/controllers/opendata/agents/nodes/comment_controller.rb
+++ app/controllers/opendata/agents/nodes/comment_controller.rb
@@ -1,24 +1,104 @@
 class Opendata::Agents::Nodes::CommentController < ApplicationController
   include Cms::NodeFilter::View
   include Opendata::UrlHelper
+  include Opendata::MypageFilter
 
   before_action :accept_cors_request
-  before_action :set_idea
+  #before_action :set_idea
+  before_action :set_idea_comment, only: [:show_comment, :add_comment, :delete_comment]
+
+  skip_filter :logged_in?
 
   private
-    def set_idea
-      @idea_path = @cur_path.sub(/\/comment\/.*/, ".html")
+#    def set_idea
+#      @idea_path = @cur_path.sub(/\/comment\/.*/, ".html")
+#
+#      @idea = Opendata::Idea.site(@cur_site).public.
+#        filename(@idea_path).
+#        first
+#
+#      raise "404" unless @idea
+#    end
 
-      @idea = Opendata::Idea.site(@cur_site).public.
-        filename(@idea_path).
+    def set_idea_comment
+      @idea_comment_path = Opendata::Idea.to_idea_path(@cur_path)
+
+      @idea_comment = Opendata::Idea.site(@cur_site).public.
+        filename(@idea_comment_path).
         first
 
-      raise "404" unless @idea
+      cond = { site_id: @cur_site.id, idea_id: @idea_comment.id }
+      @comments = Opendata::IdeaComment.where(cond).order_by(:created.asc)
+
+      @comment_mode = logged_in?(redirect: false)
+
+      raise "404" unless @idea_comment
+    end
+
+    def update_commented_count(member_ids, count)
+      member_ids.each do |member_id|
+        notice = Opendata::MemberNotice.where({site_id: @cur_site.id, member_id: member_id}).first
+        if notice
+          commented_count = notice.commented_count || 0
+          notice.commented_count = notice.commented_count + count
+          notice.save
+        else
+          notice_new = { site_id: @cur_site.id, member_id: member_id, commented_count: 1 }
+          Opendata::MemberNotice.new(notice_new).save
+        end
+      end
     end
 
   public
-    def index
-      redirect_to @idea_path
+#    def index
+#      redirect_to @idea_path
+#    end
+
+    def show_comment
+      @cur_node.layout = nil
+    end
+
+    def add_comment
+      @cur_node.layout = nil
+      raise "403" unless logged_in?(redirect: false)
+
+      idea_id = @idea_comment.id
+      idea = Opendata::Idea.site(@cur_site).find(idea_id)
+
+      new_comment = { site_id: @cur_site.id, member_id: @cur_member.id, idea_id: idea_id, text: params[:comment_body]}
+      Opendata::IdeaComment.new(new_comment).save
+
+      idea.commented = Time.zone.now
+      idea.total_comment += 1
+      idea.save
+
+      member_ids = []
+      other_comments = Opendata::IdeaComment.where({idea_id: @idea_comment.id})
+      other_comments = other_comments.not_in({member_id: [@cur_member.id]})
+      other_comments = other_comments.not_in({member_id: [@idea_comment.member_id]}) if @idea_comment.member_id.present?
+      other_comments.each do |other_comment|
+        member_ids << other_comment.member_id
+      end
+
+      if @idea_comment.member_id.present? && @idea_comment.member_id != @cur_member.id
+        member_ids << @idea_comment.member_id
+      end
+
+      update_commented_count(member_ids.uniq, 1)
+
+      render :show_comment
+    end
+
+    def delete_comment
+      @cur_node.layout = nil
+
+      comment = Opendata::IdeaComment.find params[:comment]
+      if comment
+        comment.comment_deleted = Time.zone.now
+        comment.save
+      end
+
+      render :show_comment
     end
 
 end
--- app/controllers/opendata/agents/nodes/idea_controller.rb
+++ app/controllers/opendata/agents/nodes/idea_controller.rb
@@ -5,7 +5,6 @@ class Opendata::Agents::Nodes::IdeaController < ApplicationController
   include Opendata::IdeaFilter
 
   before_action :set_idea, only: [:show_point, :add_point, :point_members]
-  before_action :set_idea_comment, only: [:show_comment, :add_comment, :delete_comment]
   skip_filter :logged_in?
 
   private
@@ -16,35 +15,6 @@ class Opendata::Agents::Nodes::IdeaController < ApplicationController
         first
 
       raise "404" unless @idea
-    end
-
-    def set_idea_comment
-      @idea_comment_path = Opendata::Idea.to_idea_path(@cur_path)
-
-      @idea_comment = Opendata::Idea.site(@cur_site).public.
-        filename(@idea_comment_path).
-        first
-
-      cond = { site_id: @cur_site.id, idea_id: @idea_comment.id }
-      @comments = Opendata::IdeaComment.where(cond).order_by(:created.asc)
-
-      @comment_mode = logged_in?(redirect: false)
-
-      raise "404" unless @idea_comment
-    end
-
-    def update_commented_count(member_ids, count)
-      member_ids.each do |member_id|
-        notice = Opendata::MemberNotice.where({site_id: @cur_site.id, member_id: member_id}).first
-        if notice
-          commented_count = notice.commented_count || 0
-          notice.commented_count = notice.commented_count + count
-          notice.save
-        else
-          notice_new = { site_id: @cur_site.id, member_id: member_id, commented_count: 1 }
-          Opendata::MemberNotice.new(notice_new).save
-        end
-      end
     end
 
   public
@@ -114,53 +84,6 @@ class Opendata::Agents::Nodes::IdeaController < ApplicationController
     def point_members
       @cur_node.layout = nil
       @items = Opendata::IdeaPoint.where(site_id: @cur_site.id, idea_id: @idea.id)
-    end
-
-    def show_comment
-      @cur_node.layout = nil
-    end
-
-    def add_comment
-      @cur_node.layout = nil
-      raise "403" unless logged_in?(redirect: false)
-
-      idea_id = @idea_comment.id
-      idea = Opendata::Idea.site(@cur_site).find(idea_id)
-
-      new_comment = { site_id: @cur_site.id, member_id: @cur_member.id, idea_id: idea_id, text: params[:comment_body]}
-      Opendata::IdeaComment.new(new_comment).save
-
-      idea.commented = Time.zone.now
-      idea.total_comment += 1
-      idea.save
-
-      member_ids = []
-      other_comments = Opendata::IdeaComment.where({idea_id: @idea_comment.id})
-      other_comments = other_comments.not_in({member_id: [@cur_member.id]})
-      other_comments = other_comments.not_in({member_id: [@idea_comment.member_id]}) if @idea_comment.member_id.present?
-      other_comments.each do |other_comment|
-        member_ids << other_comment.member_id
-      end
-
-      if @idea_comment.member_id.present? && @idea_comment.member_id != @cur_member.id
-        member_ids << @idea_comment.member_id
-      end
-
-      update_commented_count(member_ids.uniq, 1)
-
-      render :show_comment
-    end
-
-    def delete_comment
-      @cur_node.layout = nil
-
-      comment = Opendata::IdeaComment.find params[:comment]
-      if comment
-        comment.comment_deleted = Time.zone.now
-        comment.save
-      end
-
-      render :show_comment
     end
 
     def show_dataset
