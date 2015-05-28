30366d450d1bd06af008e96e23f5ce4b39acf15d
tecoda <y.oda@tec-inf.co.jp>
2015-05-28 15:09:40 +0900

    update: agents/nodes/idea_spec.rb

--- spec/features/opendata/agents/nodes/idea_spec.rb
+++ spec/features/opendata/agents/nodes/idea_spec.rb
@@ -9,6 +9,8 @@ describe "opendata_agents_nodes_idea", dbscope: :example do
   let(:site) { cms_site }
   let(:area) { create_once :opendata_node_area, basename: "opendata_area_1" }
   let(:node_idea) { create_once :opendata_node_idea }
+  let!(:node_mypage) { create_once :opendata_node_mypage, filename: "mypage" }
+
   let!(:node_area) { create :opendata_node_area }
 
   let(:page_idea) { create_once :opendata_idea, filename: "#{node_idea.filename}/1.html", area_ids: [ area.id ] }
@@ -23,88 +25,131 @@ describe "opendata_agents_nodes_idea", dbscope: :example do
   let(:areas_path) { "#{node_idea.url}areas.html" }
   let(:tags_path) { "#{node_idea.url}tags.html" }
 
-  it "#index" do
-    page.driver.browser.with_session("public") do |session|
-      session.env("HTTP_X_FORWARDED_HOST", site.domain)
-      session.env("REQUEST_PATH", index_path)
-      visit index_path
-      expect(current_path).to eq index_path
+  context "without auth" do
+
+    it "#index" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+
+        visit index_path
+        expect(current_path).to eq index_path
+      end
     end
+
+    it "#ajax_path" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+
+        visit show_point_path
+        expect(current_path).to eq show_point_path
+
+        visit point_members_path
+        expect(current_path).to eq point_members_path
+
+        visit show_comment_path
+        expect(current_path).to eq show_comment_path
+
+        visit show_dataset_path
+        expect(current_path).to eq show_dataset_path
+
+        visit show_app_path
+        expect(current_path).to eq show_app_path
+
+      end
+    end
+
+    it "#rss" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+
+        visit rss_path
+        expect(current_path).to eq rss_path
+      end
+    end
+
+    it "#areas" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+
+        visit areas_path
+        expect(current_path).to eq areas_path
+      end
+    end
+
+    it "#tags" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+
+        visit tags_path
+        expect(current_path).to eq tags_path
+      end
+    end
+
   end
 
-  it "#show_point" do
-    page.driver.browser.with_session("public") do |session|
-      session.env("HTTP_X_FORWARDED_HOST", site.domain)
-      session.env("REQUEST_PATH", show_point_path)
-      visit show_point_path
-      expect(current_path).to eq show_point_path
+  context "with auth" do
+    before do
+      login_opendata_member(site, node_mypage)
     end
-  end
 
-  it "#rss" do
-    page.driver.browser.with_session("public") do |session|
-      session.env("HTTP_X_FORWARDED_HOST", site.domain)
-      session.env("REQUEST_PATH", rss_path)
-      visit rss_path
-      expect(current_path).to eq rss_path
-    end
-  end
+    it "#index" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
 
-  it "#areas" do
-    page.driver.browser.with_session("public") do |session|
-      session.env("HTTP_X_FORWARDED_HOST", site.domain)
-      session.env("REQUEST_PATH", areas_path)
-      visit areas_path
-      expect(current_path).to eq areas_path
-      #expect(page).to have_content(node_area.name)
+        visit index_path
+        expect(current_path).to eq index_path
+      end
     end
-  end
 
-  it "#tags" do
-    page.driver.browser.with_session("public") do |session|
-      session.env("HTTP_X_FORWARDED_HOST", site.domain)
-      session.env("REQUEST_PATH", tags_path)
-      visit tags_path
-      expect(current_path).to eq tags_path
-      #expect(page).to have_content(page_idea.tags[0])
-      #expect(page).to have_content(page_idea.tags[1])
-    end
-  end
+    it "#ajax_path" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
 
-  it "#point_members" do
-    page.driver.browser.with_session("public") do |session|
-      session.env("HTTP_X_FORWARDED_HOST", site.domain)
-      session.env("REQUEST_PATH", point_members_path)
-      visit point_members_path
-      expect(current_path).to eq point_members_path
-    end
-  end
+        visit show_point_path
+        expect(current_path).to eq show_point_path
 
-  it "#show_comment" do
-    page.driver.browser.with_session("public") do |session|
-      session.env("HTTP_X_FORWARDED_HOST", site.domain)
-      session.env("REQUEST_PATH", show_comment_path)
-      visit show_comment_path
-      expect(current_path).to eq show_comment_path
-    end
-  end
+        visit point_members_path
+        expect(current_path).to eq point_members_path
 
-  it "#show_dataset" do
-    page.driver.browser.with_session("public") do |session|
-      session.env("HTTP_X_FORWARDED_HOST", site.domain)
-      session.env("REQUEST_PATH", show_dataset_path)
-      visit show_dataset_path
-      expect(current_path).to eq show_dataset_path
-    end
-  end
+        visit show_comment_path
+        expect(current_path).to eq show_comment_path
 
-  it "#show_app" do
-    page.driver.browser.with_session("public") do |session|
-      session.env("HTTP_X_FORWARDED_HOST", site.domain)
-      session.env("REQUEST_PATH", show_app_path)
-      visit show_app_path
-      expect(current_path).to eq show_app_path
+        visit show_dataset_path
+        expect(current_path).to eq show_dataset_path
+
+        visit show_app_path
+        expect(current_path).to eq show_app_path
+
+      end
     end
+
+    it "#rss" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+
+        visit rss_path
+        expect(current_path).to eq rss_path
+      end
+    end
+
+    it "#areas" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+
+        visit areas_path
+        expect(current_path).to eq areas_path
+      end
+    end
+
+    it "#tags" do
+      page.driver.browser.with_session("public") do |session|
+        session.env("HTTP_X_FORWARDED_HOST", site.domain)
+
+        visit tags_path
+        expect(current_path).to eq tags_path
+      end
+    end
+
   end
 
 end
