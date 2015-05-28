5e3364b0a76efb069cc2043c28975c086fa4dc23
tany <una@kimiyure.com>
2015-05-28 19:44:44 +0900

    show invalid login message

--- app/controllers/sns/login_controller.rb
+++ app/controllers/sns/login_controller.rb
@@ -8,25 +8,27 @@ class Sns::LoginController < ApplicationController
 
   private
     def get_params
-      params.require(:item).permit(:email, :password)
+      params.require(:item).permit(:uid, :email, :password)
     end
 
   public
     def login
       if !request.post?
         # retrieve parameters from get parameter. this is bookmark support.
-        @item = SS::User.new
-        @item.email = params[:email]
-        @item.password = params[:password]
+        @item = SS::User.new email: params[:email]
         return
       end
 
-      safe_params = get_params
-      email_or_uid = safe_params[:email]
-      password = safe_params[:password]
+      safe_params  = get_params
+      email_or_uid = safe_params[:email].presence || safe_params[:uid]
+      password     = safe_params[:password]
 
       @item = SS::User.authenticate(email_or_uid, password)
-      return if !@item
+      unless @item
+        @item  = SS::User.new email: email_or_uid
+        @error = t "sns.errors.invalid_login"
+        return
+      end
 
       if params[:ref].blank? || [sns_login_path, sns_mypage_path].index(params[:ref])
         return set_user @item, session: true, redirect: true, password: password
