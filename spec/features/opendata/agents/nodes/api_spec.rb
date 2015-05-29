60802eaa491270710074c9185cce8946b8acab7e
tecoda <y.oda@tec-inf.co.jp>
2015-05-29 14:57:09 +0900

    update: agents/nodes/api_spec.rb

--- spec/features/opendata/agents/nodes/api_spec.rb
+++ spec/features/opendata/agents/nodes/api_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
 
-describe "opendata_api", dbscope: :example do
+describe "opendata_agents_nodes_api", dbscope: :example do
 
-  let(:node) { create_once :opendata_node_api, name: "opendata_api" }
-  let(:node_dataset) { create_once :opendata_node_dataset }
-  let(:node_area) { create :opendata_node_area }
-  let(:index_path) { "#{node.url}" }
+  let!(:node) { create_once :opendata_node_api, name: "opendata_api" }
+  let!(:node_area) { create :opendata_node_area }
+
   let(:package_list_path) { "#{node.url}1/package_list" }
   let(:group_list_path) { "#{node.url}1/group_list" }
   let(:tag_list_path) { "#{node.url}1/tag_list" }
@@ -13,8 +12,17 @@ describe "opendata_api", dbscope: :example do
   let(:group_show_path) { "#{node.url}1/group_show" }
   let(:tag_show_path) { "#{node.url}1/tag_show" }
 
-#  let!(:dataset_01) { create(:opendata_dataset, node: node_dataset, area_ids: [ node_area.id ]) }
-#  let!(:dataset_02) { create(:opendata_dataset, node: node_dataset, area_ids: [ node_area.id ]) }
+  let!(:node_dataset) { create_once :opendata_node_dataset }
+  let!(:node_dataset_group_01) { create(:opendata_dataset_group, site: cms_site, categories: [ OpenStruct.new({ _id: 1 }) ]) }
+  let!(:node_dataset_group_02) { create(:opendata_dataset_group, site: cms_site, categories: [ OpenStruct.new({ _id: 2 }) ]) }
+  let!(:node_search_dataset) { create_once :opendata_node_search_dataset, basename: "dataset/search" }
+  let!(:page_dataset_01) {
+    create(:opendata_dataset, node: node_dataset, dataset_group_ids: [node_dataset_group_01.id],
+                              area_ids: [ node_area.id ], tags: ["TEST_1"]) }
+  let!(:page_dataset_02) {
+    create(:opendata_dataset, node: node_dataset, dataset_group_ids: [node_dataset_group_02.id],
+                              area_ids: [ node_area.id ], tags: ["TEST_2"])
+  }
 
   context "api" do
 
@@ -68,8 +76,10 @@ describe "opendata_api", dbscope: :example do
         visit tag_list_path
         expect(status_code).to eq 200
 
-        visit "#{tag_list_path}?query=test"
+        visit "#{tag_list_path}?query=#{page_dataset_01.tags[0]}"
         expect(status_code).to eq 200
+
+        visit "#{tag_list_path}?query=NO_TAG"
 
       end
     end
@@ -78,11 +88,11 @@ describe "opendata_api", dbscope: :example do
       page.driver.browser.with_session("public") do |session|
         session.env("HTTP_X_FORWARDED_HOST", cms_site.domain)
 
-        visit "#{package_show_path}?id=test"
+        visit "#{package_show_path}?id=#{page_dataset_01.name}"
         expect(status_code).to eq 200
 
+        visit "#{package_show_path}?id=NO_DATASET"
         visit package_show_path
-        #expect(status_code).to eq 200
 
       end
     end
@@ -91,11 +101,11 @@ describe "opendata_api", dbscope: :example do
       page.driver.browser.with_session("public") do |session|
         session.env("HTTP_X_FORWARDED_HOST", cms_site.domain)
 
-        visit "#{tag_show_path}?id=test"
+        visit "#{tag_show_path}?id=#{page_dataset_01.tags[0]}"
         expect(status_code).to eq 200
 
+        visit "#{tag_show_path}?id=NO_TAG"
         visit tag_show_path
-        #expect(status_code).to eq 200
 
       end
     end
@@ -104,11 +114,11 @@ describe "opendata_api", dbscope: :example do
       page.driver.browser.with_session("public") do |session|
         session.env("HTTP_X_FORWARDED_HOST", cms_site.domain)
 
-        visit "#{group_show_path}?id=test"
+        visit "#{group_show_path}?id=#{node_dataset_group_01.id}"
         expect(status_code).to eq 200
 
+        visit "#{group_show_path}?id=NO_DATASET_GROUP"
         visit group_show_path
-        #expect(status_code).to eq 200
 
       end
     end
