896ce62341ce2c30802e7033bdf3fa273bf6bbea
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-05-29 19:44:22 +0900

    * add sparql disable mode support to sparql_controller. * fix sparql_spec failure.

--- app/controllers/opendata/agents/nodes/sparql_controller.rb
+++ app/controllers/opendata/agents/nodes/sparql_controller.rb
@@ -17,8 +17,9 @@ class Opendata::Agents::Nodes::SparqlController < ApplicationController
       file_format = params[:format]
       result = Opendata::Sparql.select(params[:query], file_format)
 
-      if file_format == "HTML"
-
+      if result == true
+        send_data "Disabled", type: "text/html", disposition: :inline
+      elsif file_format == "HTML"
         html_result = result[:data]
         if html_result.include?(%(<td>))
           html_page =  %(<html>\n<head><meta charset="utf-8"></head>\n<body>\n)
@@ -30,9 +31,7 @@ class Opendata::Agents::Nodes::SparqlController < ApplicationController
 
         @cur_node.layout_id = nil
         send_data html_page, type: result[:type], disposition: :inline
-
       elsif file_format == "HTML_TABLE"
-
         html_result = result[:data]
         if html_result.include?(%(<td>))
           html_page = html_result.gsub(/<table class="sparql">/, %(<table class="sparql" border="1">) )
@@ -42,12 +41,9 @@ class Opendata::Agents::Nodes::SparqlController < ApplicationController
 
         @cur_node.layout_id = nil
         send_data html_page, type: result[:type], disposition: :inline
-
       else
         send_data result[:data], type: result[:type], filename: "sparql.#{result[:ext]}",
           disposition: :attachment
       end
-
     end
-
 end
