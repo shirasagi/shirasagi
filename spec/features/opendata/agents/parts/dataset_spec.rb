1a9feb2356a65a85a00545744afccacf9fa05b0f
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-06-02 16:04:19 +0900

    add opendata/agents/parts/dataset feature spec

---
+++ spec/features/opendata/agents/parts/dataset_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe "opendata_agents_nodes_dataset", dbscope: :example do
+  let(:site) { cms_site }
+  let!(:node_category) { create :opendata_node_category, basename: "bunya/kurashi" }
+  let!(:node_search_dataset) { create :opendata_node_search_dataset, basename: "dataset/search" }
+  let!(:node_search_dataset_group) { create :opendata_node_search_dataset_group }
+  let!(:node_dataset) { create(:opendata_node_dataset) }
+  let!(:page_dataset) { create(:opendata_dataset, node: node_dataset) }
+  let(:part) { create :opendata_part_dataset, sort: 'attention' }
+  let(:index_url) { ::URI.parse "http://#{site.domain}#{part.url}" }
+
+  describe "#index" do
+    it do
+      visit "#{index_url}"
+      expect(status_code).to eq 200
+      expect(current_path).to eq index_url.path
+      within "div.pages" do
+        expect(page).to have_content(page_dataset.name)
+      end
+      within "nav.feed" do
+        expect(page).to have_content("もっと見る")
+        expect(page).to have_content("RSS")
+      end
+    end
+  end
+end
