8880b79d6719854721927b8ae5cce92124ea0b96
tecoda <y.oda@tec-inf.co.jp>
2015-06-04 16:22:46 +0900

    fix: tag_show_filter.rb

--- app/controllers/concerns/opendata/api/tag_show_filter.rb
+++ app/controllers/concerns/opendata/api/tag_show_filter.rb
@@ -5,17 +5,17 @@ module Opendata::Api::TagShowFilter
     def tag_show
       help = SS.config.opendata.api["tag_show_help"]
       id = params[:id]
-      id = URI.decode(id) if !id.nil?
+      id = URI.decode(id) if id
 
       if id.blank?
         error = {__type: "Validation Error", id: "Missing value"}
         render json: {help: help, success: false, error: error} and return
       end
 
-      @tags = Opendata::Dataset.site(@cur_site).public.get_tag(id)
+      datasets = Opendata::Dataset.site(@cur_site).public.search({tag: id})
 
-      if @tags.count > 0
-        res = {help: help, success: true, result: [@tags[0]["name"]]}
+      if datasets.count > 0
+        res = {help: help, success: true, result: datasets}
       else
         res = {help: help, success: false}
         res[:error] = {message: "Not found", __type: "Not Found Error"}
