bb22a05228664e5803e17ce83746bacfdbdf2238
tsutsuitec <m.tsutsui@tec-inf.co.jp>
2015-06-04 10:45:32 +0900

    kyogo

--- config/routes/opendata/routes.rb
+++ config/routes/opendata/routes.rb
@@ -2,6 +2,11 @@ SS::Application.routes.draw do
 
   Opendata::Initializer
 
+  get "/app/:app/application/*filename" => "opendata/appscripts#index"
+  get "/text/:app/appfile/*filename" => "opendata/appscripts#text"
+  get "/app/:app/full" => "opendata/appscripts#full"
+  get "/datasets/select(.:format)" => "opendata/dataset_selects#index"
+
   concern :deletion do
     get :delete, on: :member
   end
@@ -96,9 +101,6 @@ SS::Application.routes.draw do
     get "dataset/:dataset/apps/show.:format" => "public#show_apps", cell: "nodes/dataset", format: false
     get "dataset/:dataset/ideas/show.:format" => "public#show_ideas", cell: "nodes/dataset", format: false
 
-    get "dataset/datasets/search(.:format)" => "public#datasets_search", cell: "nodes/dataset"
-    post "dataset/datasets/search(.:format)" => "public#datasets_search", cell: "nodes/dataset"
-
     match "search_dataset_group/(index.:format)" => "public#index", cell: "nodes/search_dataset_group", via: [:get, :post]
     match "search_dataset/(index.:format)" => "public#index", cell: "nodes/search_dataset", via: [:get, :post]
     get "search_dataset/rss.xml" => "public#rss", cell: "nodes/search_dataset"
@@ -120,17 +122,13 @@ SS::Application.routes.draw do
     get "app/:app/point/members.html" => "public#point_members", cell: "nodes/app", format: false
     get "app/:app/ideas/show.:format" => "public#show_ideas", cell: "nodes/app", format: false
     get "app/:app/executed/show.:format" => "public#show_executed", cell: "nodes/app", format: false
-    post "app/:app/executed/add.:format" => "public#add_executed", cell: "nodes/app", format: false
+    match "app/:app/executed/add.:format" => "public#add_executed", cell: "nodes/app", via: [:get, :post]
 
     get "app/:app/zip" => "public#download", cell: "nodes/app", format: false
     get "app/:app/appfile/:id/" => "public#index", cell: "nodes/appfile"
     get "app/:app/appfile/:id/content.html" => "public#content", cell: "nodes/appfile", format: false
     get "app/:app/appfile/:id/json.html" => "public#json", cell: "nodes/appfile", format: false
     get "app/:app/appfile/:id/*filename" => "public#download", cell: "nodes/appfile", format: false
-
-    get "app/:app/full" => "public#full", cell: "nodes/app", format: false
-    get "app/:app/file_index/(*filename)" => "public#app_index", cell: "nodes/app", format: false
-    get "app/:app/file_text/(*filename)" => "public#text", cell: "nodes/app", format: false
 
     match "search_app/(index.:format)" => "public#index", cell: "nodes/search_app", via: [:get, :post]
     get "search_app/rss.xml" => "public#rss", cell: "nodes/search_app"
@@ -149,8 +147,8 @@ SS::Application.routes.draw do
     post "idea/:idea/point.:format" => "public#add_point", cell: "nodes/idea", format: false
     get "idea/:idea/point/members.html" => "public#point_members", cell: "nodes/idea", format: false
     get "idea/:idea/comment/show.:format" => "public#index", cell: "nodes/comment", format: false
-    post "idea/:idea/comment/add.:format" => "public#add", cell: "nodes/comment", format: false
-    post "idea/:idea/comment/delete.:format" => "public#delete", cell: "nodes/comment", format: false
+    match "idea/:idea/comment/add.:format" => "public#add", cell: "nodes/comment", via: [:get, :post]
+    match "idea/:idea/comment/delete.:format" => "public#delete", cell: "nodes/comment", via: [:get, :post]
     get "idea/:idea/dataset/show.:format" => "public#show_dataset", cell: "nodes/idea", format: false
     get "idea/:idea/app/show.:format" => "public#show_app", cell: "nodes/idea", format: false
 
