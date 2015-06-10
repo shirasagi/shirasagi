ba0c174fcfa02bc3d67bad2e40e5c0e7be7f4277
tecoda <y.oda@tec-inf.co.jp>
2015-06-10 14:30:07 +0900

    add: url_resource

--- lib/opendata/api.rb
+++ lib/opendata/api.rb
@@ -58,8 +58,10 @@ module Opendata::Api
       package[:author] = author
       package[:author_email] = author_email
       package[:type] = "dataset"
-      package[:resources] = convert_resources(dataset[:resources])
-      package[:num_resources] = dataset.resources.size
+      package[:resources] = convert_resources(dataset[:resources]) +
+                            convert_url_resources(dataset[:url_resources])
+      package[:num_resources] = get_num_resources(dataset[:resources]) +
+                                get_num_url_resources(dataset[:url_resources])
       package[:tags] = convert_tags(dataset)
       package[:groups] = convert_dataset_groups(dataset[:dataset_group_ids])
       package[:license_id] = dataset[:license]
@@ -85,12 +87,17 @@ module Opendata::Api
       return package
     end
 
+    def get_num_resources(dataset_resources)
+      return dataset_resources ? dataset_resources.size : 0
+    end
+
     def convert_resources(dataset_resources)
       package_resources = []
 
       resources = dataset_resources || []
       resources.each do |resource|
         package_resources << convert_resource(resource)
+
       end
 
       return package_resources
@@ -104,7 +111,6 @@ module Opendata::Api
       package_resource[:description] = resource[:text]
       package_resource[:format] = resource[:format]
       package_resource[:name] = resource[:name]
-      package_resource[:filename] = resource[:filename]
       package_resource[:created] = resource[:created]
       package_resource[:filename] = resource[:filename]
       package_resource[:license_id] = resource[:license_id]
@@ -112,6 +118,42 @@ module Opendata::Api
       package_resource[:rdf_error] = resource[:rdf_error]
 
       return package_resource
+    end
+
+    def get_num_url_resources(dataset_url_resources)
+      return dataset_url_resources ? dataset_url_resources.size : 0
+    end
+
+    def convert_url_resources(dataset_url_resources)
+      package_url_resources = []
+
+      url_resources = dataset_url_resources || []
+      url_resources.each do |url_resource|
+        package_url_resources << convert_url_resource(url_resource)
+      end
+
+      return package_url_resources
+    end
+
+    def convert_url_resource(url_resource)
+      package_url_resource = {}
+
+      package_url_resource[:id] = url_resource[:file_id]
+      package_url_resource[:last_modified] = url_resource[:updated]
+      package_url_resource[:description] = url_resource[:text]
+      package_url_resource[:format] = url_resource[:format]
+      package_url_resource[:name] = url_resource[:name]
+      package_url_resource[:created] = url_resource[:created]
+      package_url_resource[:filename] = url_resource[:filename]
+      package_url_resource[:license_id] = url_resource[:license_id]
+      package_url_resource[:rdf_iri] = url_resource[:rdf_iri]
+      package_url_resource[:rdf_error] = url_resource[:rdf_error]
+      package_url_resource[:originai_url] = url_resource[:original_url]
+      package_url_resource[:original_updated] = url_resource[:original_updated]
+      package_url_resource[:crawl_state] = url_resource[:crawl_state]
+      package_url_resource[:crawl_update] = url_resource[:crawl_update]
+
+      return package_url_resource
     end
 
     def convert_tags(dataset)
