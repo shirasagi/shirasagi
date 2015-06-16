456208739febb6b4851298947d4dac2191d07579
itowtips <ito@web-tips.co.jp>
2015-06-16 19:39:51 +0900

    fix oauth spec: prefix_path configuration was removed

--- spec/factories/member/nodes.rb
+++ spec/factories/member/nodes.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
 
     site_id { site.present? ? site.id : cms_site.id }
     route "member/login"
-    filename { SS.config.oauth.prefix_path.sub(/^\//, '') || "auth" }
+    filename { "auth" }
     twitter_oauth "enabled"
     twitter_client_id "#{unique_id}"
     twitter_client_secret "#{unique_id}"
