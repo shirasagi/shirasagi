83cc433f43cc92faaeec6a01691b061063badc97
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-06-18 21:20:51 +0900

    fix insufficient dataset group search link

--- app/controllers/opendata/agents/parts/dataset/dataset_controller.rb
+++ app/controllers/opendata/agents/parts/dataset/dataset_controller.rb
@@ -5,7 +5,7 @@ class Opendata::Agents::Parts::Dataset::DatasetController < ApplicationControlle
 
   public
     def index
-      @node_url = "#{search_datasets_path}?sort=#{@cur_part.sort}"
+      @node_url = search_datasets_path("sort" => @cur_part.sort)
       default_options = { "sort" => "#{@cur_part.sort}" }
       @rss_path = ->(options = {}) { build_path("#{search_datasets_path}rss.xml", default_options.merge(options)) }
       @items = Opendata::Dataset.site(@cur_site).public.
