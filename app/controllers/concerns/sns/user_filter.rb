16cf683cf834f4beb0442d317cbf3997f198676f
tany <tanizawa@web-tips.co.jp>
2015-06-26 02:46:40 +0900

    fix filter: set_sns_user

--- app/controllers/concerns/sns/user_filter.rb
+++ app/controllers/concerns/sns/user_filter.rb
@@ -3,15 +3,14 @@ module Sns::UserFilter
   include Sns::BaseFilter
 
   included do
-    before_action :set_sns_user #, if: ->{ request.env["REQUEST_PATH"] =~ /^\/\.u\d+\// }
+    before_action :set_sns_user
     before_action :set_crumbs
     navi_view "sns/user/main/navi"
   end
 
   private
     def set_sns_user
-      uid = request.env["REQUEST_PATH"].sub(/^\/\.u(\d+)\/.*/, '\\1')
-      @sns_user = SS::User.find uid
+      @sns_user = SS::User.find params[:user]
       @crumbs <<  [@sns_user.name, sns_user_path(@sns_user)]
     end
 
