0f472c8be707736b6523bdf7130b26449438ef76
tany <una@kimiyure.com>
2015-07-16 17:21:13 +0900

    [renew] outline

--- app/helpers/cms/node_helper.rb
+++ app/helpers/cms/node_helper.rb
@@ -9,14 +9,28 @@ module Cms::NodeHelper
 
   def node_navi(opts = {}, &block)
     h  = []
+    h << render(partial: "cms/node/main/node_navi")
 
-    if block_given?
-      h << capture(&block)
+    h << %(<nav class="mod-navi">).html_safe
+    h << capture(&block) if block_given?
+    h << %(</nav>).html_safe
+
+    #h << render(partial: "cms/main/navi")
+    safe_join(h)
+  end
+
+  def mod_navi(&block)
+    mods = Cms::Node.modules.map do |name, key|
+      key = "node" if key == "cms" #TODO:
+      %(<li>#{link_to name, send("#{key}_main_path")}</li>).html_safe
     end
 
-    h << render(partial: "cms/node/main/node_navi")
-    h << render(partial: "cms/node/main/modules")
-    #h << render(partial: "cms/main/navi")
-    h.join.html_safe
+    h  = []
+    h << %(<div class="pulldown-menu">).html_safe
+    h << capture(&block) if block_given?
+    h << %(<ul>#{safe_join(mods)}</ul>).html_safe
+    h << %(</div>).html_safe
+
+    safe_join(h)
   end
 end
