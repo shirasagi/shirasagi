74d41b35e7cd1018fefced24d3087adfc071f322
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-07-24 12:59:45 +0900

    [modify] fix illegal boolean field normalization

--- spec/models/ss/document_spec.rb
+++ spec/models/ss/document_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe SS::Document, type: :model, dbscope: :example do
     include SS::Document
 
     field :name, type: String
+    field :bool1, type: Boolean
+    field :bool2, type: Boolean, default: false
+    field :bool3, type: Boolean, default: -> { false }
+    field :str1, type: String
+    field :str2, type: String, default: ""
+    field :str3, type: String, default: -> { "" }, metadata: { normalize: false }
   end
 
   describe "#created and #updated" do
@@ -172,6 +178,101 @@ RSpec.describe SS::Document, type: :model, dbscope: :example do
           check[r.name] = true
         end
         expect(total_count).to eq Klass.count
+      end
+    end
+  end
+
+  describe "boolean field normalization" do
+    before do
+      @name = unique_id
+      Klass.create!(name: @name, bool1: false, bool2: false, bool3: false)
+    end
+
+    it do
+      # bool1 field was not removed because bool1 is boolean.
+      expect(Klass.where(bool1: false).count).to eq 1
+    end
+
+    it do
+      # bool2 field was not removed because bool2 has default.
+      expect(Klass.where(bool2: false).count).to eq 1
+    end
+
+    it do
+      # bool3 field was not removed because bool3 has default.
+      expect(Klass.where(bool3: false).count).to eq 1
+    end
+  end
+
+  describe "string field normalization" do
+    context "when blank string is given" do
+      before do
+        @name = unique_id
+        Klass.create!(name: @name, str1: "", str2: "", str3: "")
+      end
+
+      it do
+        # str1 field was removed by SS::Fields::Normalizer#remove_blank_fields
+        expect(Klass.where(str1: "").count).to eq 0
+        expect(Klass.where(:str1.exists => false).count).to eq 1
+      end
+
+      it do
+        # str2 field was not removed because str2 has default.
+        expect(Klass.where(str2: "").count).to eq 1
+      end
+
+      it do
+        # str3 field was not removed because str3 has default.
+        expect(Klass.where(str3: "").count).to eq 1
+      end
+    end
+
+    context "when 1-space-string is given" do
+      before do
+        @name = unique_id
+        Klass.create!(name: @name, str1: " ", str2: " ", str3: " ")
+      end
+
+      it do
+        # str1 field was not stripped because str1 length is 1
+        expect(Klass.where(str1: /^ $/).count).to eq 1
+      end
+
+      it do
+        # str2 field was not stripped because str2 length is 1
+        expect(Klass.where(str2: /^ $/).count).to eq 1
+      end
+
+      it do
+        # str3 field was not stripped because str3 length is 1
+        expect(Klass.where(str3: /^ $/).count).to eq 1
+      end
+    end
+
+    context "when leading spaces/trailing spaces is given" do
+      before do
+        @name = unique_id
+        @str = " \r\n\t aaa \r\n\t "
+        Klass.create!(name: @name, str1: @str, str2: @str, str3: @str)
+      end
+
+      it do
+        # str1 field was stripped
+        expect(Klass.where(str1: /^#{Regexp.escape(@str)}$/).count).to eq 0
+        expect(Klass.where(str1: /^#{Regexp.escape(@str.strip)}$/).count).to eq 1
+      end
+
+      it do
+        # str2 field was stripped
+        expect(Klass.where(str2: /^#{Regexp.escape(@str)}$/).count).to eq 0
+        expect(Klass.where(str2: /^#{Regexp.escape(@str.strip)}$/).count).to eq 1
+      end
+
+      it do
+        # str3 field was not stripped because str3 has metadata: { normalize: false }
+        expect(Klass.where(str3: /^#{Regexp.escape(@str)}$/).count).to eq 1
+        expect(Klass.where(str3: /^#{Regexp.escape(@str.strip)}$/).count).to eq 0
       end
     end
   end
