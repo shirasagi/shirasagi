779e151cb79f9a01607b910da02d9b2acffc70f7
tany <una@kimiyure.com>
2015-08-04 18:34:02 +0900

    [add] unicorn config

--- bin/unicorn-centos6.sh
+++ bin/unicorn-centos6.sh
@@ -2,7 +2,7 @@
 # chkconfig: - 85 35
 # description: Unicorn
 
-# rvm wrapper 2.1.2 start unicorn => /usr/local/rvm/bin/start_unicorn
+# rvm wrapper 2.2.2 start unicorn
 export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
 
 APPS=()
