29fd2734e2582e17edc636874dc29a5c932c0d25
itowtips <ito@web-tips.co.jp>
2015-08-05 21:10:39 +0900

    [hotfix] disable find_each

--- app/controllers/cms/agents/tasks/pages_controller.rb
+++ app/controllers/cms/agents/tasks/pages_controller.rb
@@ -17,7 +17,8 @@ class Cms::Agents::Tasks::PagesController < ApplicationController
       pages = pages.node(@node) if @node
       @task.total_count = pages.size
 
-      pages.order_by(id: 1).find_each(batch_size: PER_BATCH) do |page|
+      #pages.order_by(id: 1).find_each(batch_size: PER_BATCH) do |page|
+      pages.each do |page|
         @task.count
         page.serve_static_relation_files = @attachments
         @task.log page.url if page.becomes_with_route.generate_file
@@ -30,7 +31,8 @@ class Cms::Agents::Tasks::PagesController < ApplicationController
       pages = Cms::Page.site(@site).public
       pages = pages.node(@node) if @node
 
-      pages.order_by(id: 1).find_each(batch_size: PER_BATCH) do |page|
+      #pages.order_by(id: 1).find_each(batch_size: PER_BATCH) do |page|
+      pages.each do |page|
         page = page.becomes_with_route
         if !page.update
           @task.log page.url
@@ -51,7 +53,8 @@ class Cms::Agents::Tasks::PagesController < ApplicationController
       pages = Cms::Page.site(@site).or(cond)
       @task.total_count = pages.size
 
-      pages.order_by(id: 1).find_each(batch_size: PER_BATCH) do |page|
+      #pages.order_by(id: 1).find_each(batch_size: PER_BATCH) do |page|
+      pages.each do |page|
         @task.count
         @task.log page.full_url
         release_page page.becomes_with_route
@@ -78,7 +81,8 @@ class Cms::Agents::Tasks::PagesController < ApplicationController
       pages = Cms::Page.site(@site)
       @task.total_count = pages.size
 
-      pages.order_by(id: 1).find_each(batch_size: PER_BATCH) do |page|
+      #pages.order_by(id: 1).find_each(batch_size: PER_BATCH) do |page|
+      pages.each do |page|
         @task.count
         @task.log page.path if Fs.rm_rf page.path
       end
