a5708b54e1761419c0d10ba1d76cca35f91e20c0
tany <una@kimiyure.com>
2015-08-25 16:57:37 +0900

    [modify] css / .list-items

--- spec/features/chorg/revisions_spec.rb
+++ spec/features/chorg/revisions_spec.rb
@@ -23,7 +23,7 @@ describe "chorg_revisions", dbscope: :example do
         visit index_path
         expect(status_code).to eq 200
         expect(current_path).to eq index_path
-        expect(page).not_to have_selector("ul.list-items li.list-item nav.list-item-tap")
+        expect(page).not_to have_selector("ul.list-items li.list-item nav.tap-menu")
       end
     end
 
@@ -37,7 +37,7 @@ describe "chorg_revisions", dbscope: :example do
         visit index_path
         expect(status_code).to eq 200
         expect(current_path).to eq index_path
-        expect(page).to have_selector("ul.list-items li.list-item nav.list-item-tap")
+        expect(page).to have_selector("ul.list-items li.list-item nav.tap-menu")
       end
     end
   end
