580bc787539602cdd2c90b78012ab209a0f94032
tany <una@kimiyure.com>
2015-10-01 19:41:01 +0900

    [modify] gws/board
    update_parent_descendants_updated on save
    => update_topic_descendants_updated on create

--- app/models/gws/board/post.rb
+++ app/models/gws/board/post.rb
@@ -10,37 +10,29 @@ class Gws::Board::Post
   field :name, type: String
   field :mode, type: String, default: 'thread'
   field :permit_comment, type: String, default: 'allow'
-  field :descendants_created, type: DateTime
   field :descendants_updated, type: DateTime
-
-  permit_params :name, :mode, :permit_comment
 
   belongs_to :topic, class_name: "Gws::Board::Post", inverse_of: :descendants
   belongs_to :parent, class_name: "Gws::Board::Post", inverse_of: :children
 
-  has_many :descendants, class_name: "Gws::Board::Post", dependent: :destroy, inverse_of: :topic,
-    order: { created: 1 }
   has_many :children, class_name: "Gws::Board::Post", dependent: :destroy, inverse_of: :parent,
     order: { created: 1 }
+  has_many :descendants, class_name: "Gws::Board::Post", dependent: :destroy, inverse_of: :topic,
+    order: { created: 1 }
 
-  validates :name, presence: true
-  validates :text, presence: true
+  permit_params :name, :mode, :permit_comment
+
+  before_validation :set_topic_id, if: :comment?
+
+  validates :name, presence: true, length: { maximum: 80 }
   validates :mode, inclusion: {in: %w(thread tree)}, unless: :comment?
   validates :permit_comment, inclusion: {in: %w(allow deny)}, unless: :comment?
 
-  # Can't create a comment if its topic "permit_comment?" returns false.
-  validate -> do
-    unless topic.permit_comment?
-      errors.add :base, I18n.t("gws/board.errors.denied_comment")
-      # FIXME 排他制御出来ないためにこんなコード書いてる
-      # FIXME (例:コメント本文入力中にトピックがコメント許可しないに変更)
-      # FIXME バリデーションエラーメッセージを何処に入れれば良いのだろう？
-    end
-  end, if: :comment?
+  validate :validate_comment, if: :comment?
 
-  before_validation :set_topic_id, if: :comment?
-  before_save :set_descendants_updated
-  after_save :update_parent_descendants_updated
+  before_save :set_descendants_updated, if: -> { topic_id.blank? }
+
+  after_save :update_topic_descendants_updated, if: -> { topic_id.present? }
 
   scope :topic, ->{ exists parent_id: false }
   scope :comment, ->{ exists parent_id: true }
@@ -53,47 +45,59 @@ class Gws::Board::Post
     criteria
   }
 
-  def set_descendants_updated
-    self.descendants_updated = updated
-  end
-
-  # Update parent's "descendants_updated" field recursively.
-  def update_parent_descendants_updated(time = nil)
-    if parent.present?
-      time ||= descendants_updated
-      # Call low level "set" API instead of "update" to skip callbacks.
-      parent.set descendants_updated: time
-      parent.update_parent_descendants_updated time
+  public
+    # Returns the topic.
+    def root_post
+      parent.nil? ? self : parent.root_post
     end
-  end
 
-  def root_post
-    parent.nil? ? self : parent.root_post
-  end
+    # is comment?
+    def comment?
+      parent_id.present?
+    end
 
-  def comment?
-    parent.present?
-  end
+    def permit_comment?
+      permit_comment == 'allow'
+    end
 
-  def set_topic_id
-    self.topic_id = root_post.id
-  end
+    def mode_options
+      [
+        [I18n.t('gws/board.options.mode.thread'), 'thread'],
+        [I18n.t('gws/board.options.mode.tree'), 'tree']
+      ]
+    end
 
-  def permit_comment?
-    permit_comment == 'allow'
-  end
+    def permit_comment_options
+      [
+        [I18n.t('gws/board.options.permit_comment.allow'), 'allow'],
+        [I18n.t('gws/board.options.permit_comment.deny'), 'deny']
+      ]
+    end
 
-  def mode_options
-    [
-      [I18n.t('gws/board.options.mode.thread'), 'thread'],
-      [I18n.t('gws/board.options.mode.tree'), 'tree']
-    ]
-  end
+  private
+    # topic(root_post)を設定
+    def set_topic_id
+      self.topic_id = root_post.id
+    end
 
-  def permit_comment_options
-    [
-      [I18n.t('gws/board.options.permit_comment.allow'), 'allow'],
-      [I18n.t('gws/board.options.permit_comment.deny'), 'deny']
-    ]
-  end
+    # 最新レス投稿日時の初期値をトピックのみ設定
+    # 明示的に age るケースが発生するかも
+    def set_descendants_updated
+      return unless new_record?
+      self.descendants_updated = updated
+    end
+
+    # 最新レス投稿日時をトピックに設定
+    # 明示的に age るケースが発生するかも
+    def update_topic_descendants_updated
+      return unless topic
+      return unless _id_changed?
+      topic.set descendants_updated: updated
+    end
+
+    # コメントを許可しているか検証
+    def validate_comment
+      return if topic.permit_comment?
+      errors.add :base, I18n.t("gws/board.errors.denied_comment")
+    end
 end
