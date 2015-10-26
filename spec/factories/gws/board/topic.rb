cb5d5a4862adda9c65d9b91dceab9be53dc52165
tany <una@kimiyure.com>
2015-10-26 18:39:34 +0900

    [add] spec gws/board

--- spec/factories/gws/board/topic.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :gws_board_topic, class: Gws::Board::Post do
-    cur_site { gws_site }
-    cur_user { gws_user }
-
-    name { "name-#{unique_id}" }
-    text { "text-#{unique_id}" }
-  end
-end
