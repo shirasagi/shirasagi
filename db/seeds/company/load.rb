29b3b128ce5b585d69ac72f497875e93f022121a
tany <una@kimiyure.com>
2015-10-27 17:43:44 +0900

    [fix] seed message

--- db/seeds/company/load.rb
+++ db/seeds/company/load.rb
@@ -1,7 +1,7 @@
 ## -------------------------------------
 # Require
 
-puts "Please input site_name: site=[group_name]" or exit if ENV['site'].blank?
+puts "Please input site_name: site=[site_host]" or exit if ENV['site'].blank?
 
 @site = SS::Site.where(host: ENV['site']).first
 puts "Site not found: #{ENV['site']}" or exit unless @site
