17962a0bbb98b275c220d48d5eef8277a8e77561
itowtips <ito@web-tips.co.jp>
2015-10-29 20:55:45 +0900

    [fix] seed demo

--- db/seeds/demo/load.rb
+++ db/seeds/demo/load.rb
@@ -887,6 +887,37 @@ editor_template_html = File.read("editor_templates/clear.html") rescue nil
 save_editor_template name: "回り込み解除", description: "回り込みを解除します",
   html: editor_template_html, order: 30, site_id: @site.id
 
+puts "# board"
+def save_board_post(data)
+  puts data[:name]
+  cond = { site_id: data[:site_id], node_id: data[:node_id], name: data[:name], poster: data[:poster] }
+  item = Board::Post.where(cond).first || Board::Post.new
+  item.attributes = data
+  item.save
+
+  item
+end
+
+node = save_node route: "board/post", filename: "board", name: "災害掲示板", layout_id: layouts["one"].id,
+  mode: "tree", file_limit: 1, text_size_limit: 400, captcha: "enabled", deletable_post: "enabled",
+  deny_url: "deny", file_size_limit: 2097152, file_scan: "disabled", show_email: "enabled", show_url: "enabled"
+topic1 = save_board_post name: "テスト投稿", text: "テスト投稿です。", site_id: @site.id, node_id: node.id,
+  poster: "白鷺　太郎", delete_key: 1234
+comment1 = save_board_post name: "Re:テスト投稿", text: "返信します。", site_id: @site.id, node_id: node.id,
+  poster: "鷺　智子", delete_key: 1234, parent_id: topic1.id, topic_id: topic1.id
+comment2 = save_board_post name: "Re:テスト投稿", text: "返信します。", site_id: @site.id, node_id: node.id,
+  poster: "黒鷺　次郎", delete_key: 1234, parent_id: topic1.id, topic_id: topic1.id
+topic2 = save_board_post name: "タイトル", text: "投稿します。", site_id: @site.id, node_id: node.id,
+  poster: "白鷺　太郎", delete_key: 1234
+
+user = Cms::User.first
+if user
+  file = save_ss_files "ss_files/article/pdf_file.pdf", filename: "file.pdf", model: "board/post", site_id: @site.id
+  file.set(state: "public")
+  topic3 = save_board_post name: "管理画面から", text: "管理画面からの投稿です。", site_id: @site.id, node_id: node.id,
+    user_id: user.id, poster: "管理者", delete_key: 1234, poster_url: " http://demo.ss-proj.org/", file_ids: [file.id]
+end
+
 puts "# cms pages"
 save_page route: "cms/page", filename: "index.html", name: "自治体サンプル", layout_id: layouts["top"].id
 save_page route: "cms/page", filename: "mobile.html", name: "スマートフォン・携帯サイト", layout_id: layouts["pages"].id
