94525a8f139e6b70ea67d0163050c876a85f27d1
ka <ka.kaosf@gmail.com>
2016-01-19 16:06:19 +0900

    [fix] values method definition place

--- app/models/ckan/node.rb
+++ app/models/ckan/node.rb
@@ -10,26 +10,5 @@ module Ckan::Node
     include History::Addon::Backup
 
     default_scope ->{ where(route: "ckan/page") }
-
-    public
-      def values
-        uri = URI.parse "#{ckan_url}/api/3/action/package_search?rows=#{ckan_max_docs}&sort=metadata_modified+desc"
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true if uri.scheme == 'https'
-        req = Net::HTTP::Get.new(uri.path + '?' + uri.query)
-        res = http.request(req)
-        if res.code != '200'
-          # HTTP Error
-          []
-        else
-          h = JSON.parse(res.body)
-          if h['success']
-            h['result']['results']
-          else
-            # Failure
-            []
-          end
-        end
-      end
   end
 end
--- app/models/concerns/ckan/addon/server.rb
+++ app/models/concerns/ckan/addon/server.rb
@@ -8,5 +8,26 @@ module Ckan::Addon
       field :ckan_max_docs, type: Integer
       permit_params :ckan_url, :ckan_max_docs
     end
+
+    public
+      def values
+        uri = URI.parse "#{ckan_url}/api/3/action/package_search?rows=#{ckan_max_docs}&sort=metadata_modified+desc"
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true if uri.scheme == 'https'
+        req = Net::HTTP::Get.new(uri.path + '?' + uri.query)
+        res = http.request(req)
+        if res.code != '200'
+          # HTTP Error
+          []
+        else
+          h = JSON.parse(res.body)
+          if h['success']
+            h['result']['results']
+          else
+            # Failure
+            []
+          end
+        end
+      end
   end
 end
