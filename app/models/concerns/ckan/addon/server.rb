0ddaba39adefa75cc68cd46bcc6ab107e95e6779
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-02-16 11:36:08 +0900

    [add] basic auth support

--- app/models/concerns/ckan/addon/server.rb
+++ app/models/concerns/ckan/addon/server.rb
@@ -5,19 +5,41 @@ module Ckan::Addon
 
     included do
       field :ckan_url, type: String
+      field :ckan_basicauth_state, type: String
+      field :ckan_basicauth_username, type: String
+      field :ckan_basicauth_password, type: String
       field :ckan_max_docs, type: Integer
+      attr_accessor :in_ckan_basicauth_password
       permit_params :ckan_url, :ckan_max_docs
+      permit_params :ckan_basicauth_state, :ckan_basicauth_username, :in_ckan_basicauth_password
 
+      before_validation :set_ckan_basicauth_password, if: ->{ in_ckan_basicauth_password.present? }
       validates :ckan_url, format: /\Ahttps?:\/\//
       validates :ckan_max_docs, numericality: { greater_than_or_equal_to: 0 }
     end
 
+    private
+      def set_ckan_basicauth_password
+        self.ckan_basicauth_password = SS::Crypt.encrypt(in_ckan_basicauth_password)
+      end
+
     public
+      def ckan_basicauth_state_options
+        %w(enabled disabled).map { |m| [ I18n.t("ckan.options.ckan_basicauth_state.#{m}"), m ] }.to_a
+      end
+
+      def ckan_basicauth_enabled?
+        ckan_basicauth_state == 'enabled'
+      end
+
       def values
         uri = URI.parse "#{ckan_url}/api/3/action/package_search?rows=#{ckan_max_docs}&sort=metadata_modified+desc"
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true if uri.scheme == 'https'
         req = Net::HTTP::Get.new(uri.path + '?' + uri.query)
+        if ckan_basicauth_enabled?
+          req.basic_auth(ckan_basicauth_username, SS::Crypt.decrypt(ckan_basicauth_password))
+        end
         res = http.request(req)
         if res.code != '200'
           # HTTP Error
--- app/models/concerns/ckan/addon/status.rb
+++ app/models/concerns/ckan/addon/status.rb
@@ -5,16 +5,36 @@ module Ckan::Addon
 
     included do
       field :ckan_url, type: String
+      field :ckan_basicauth_state, type: String
+      field :ckan_basicauth_username, type: String
+      field :ckan_basicauth_password, type: String
       field :ckan_status, type: String
-      permit_params :ckan_url, :ckan_status
+      attr_accessor :in_ckan_basicauth_password
+      permit_params :ckan_url, :ckan_basicauth_state, :ckan_basicauth_username, :in_ckan_basicauth_password
+      permit_params :ckan_status
 
+      before_validation :set_ckan_basicauth_password, if: ->{ in_ckan_basicauth_password.present? }
       validates :ckan_url, format: /\Ahttps?:\/\//
+      validates :ckan_basicauth_state, inclusion: { in: %w(enabled disabled) }
       validates :ckan_status, inclusion: { in: %w(dataset tag group related_item) }
     end
+
+    private
+      def set_ckan_basicauth_password
+        self.ckan_basicauth_password = SS::Crypt.encrypt(in_ckan_basicauth_password)
+      end
 
     public
       def ckan_status_options
         %w(dataset tag group related_item).map { |m| [ I18n.t("ckan.options.ckan_status.#{m}"), m ] }.to_a
+      end
+
+      def ckan_basicauth_state_options
+        %w(enabled disabled).map { |m| [ I18n.t("ckan.options.ckan_basicauth_state.#{m}"), m ] }.to_a
+      end
+
+      def ckan_basicauth_enabled?
+        ckan_basicauth_state == 'enabled'
       end
 
       def value
@@ -22,6 +42,9 @@ module Ckan::Addon
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true if uri.scheme == 'https'
         req = Net::HTTP::Get.new(uri.path)
+        if ckan_basicauth_enabled?
+          req.basic_auth(ckan_basicauth_username, SS::Crypt.decrypt(ckan_basicauth_password))
+        end
         res = http.request(req)
         if res.code != '200'
           # HTTP Error
