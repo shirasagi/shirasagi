bfcbbf8bbcb475fbdb453e3cc4184c9b701d2ad9
ka <ka.kaosf@gmail.com>
2016-02-18 00:32:10 +0900

    [add] tests for ckan_values_cache

--- spec/models/ckan/node_spec.rb
+++ spec/models/ckan/node_spec.rb
@@ -54,18 +54,52 @@ RSpec.describe Ckan::Node::Page, type: :model, dbscope: :example do
       let(:status) { 200 }
       let(:body) { "{\"success\":true,\"result\":{\"results\":[1,2,3,4,5]}}" }
       it { is_expected.to eq [1, 2, 3, 4, 5] }
+
+      describe "ckan_values_cache update" do
+        # Marshal.dump([1,2,3,4]) #=> "\x04\b[\ti\x06i\ai\bi\t"
+        let(:page) do
+          create :ckan_node_page, ckan_values_cache: "\x04\b[\ti\x06i\ai\bi\t"
+        end
+
+        # Marshal.dump([1,2,3,4,5]) #=> "\x04\b[\ni\x06i\ai\bi\ti\n"
+        it "updates ckan_value_cache" do
+          expect { subject }.to change {
+            described_class.find(page.id).ckan_values_cache
+          }.from("\x04\b[\ti\x06i\ai\bi\t").to("\x04\b[\ni\x06i\ai\bi\ti\n")
+        end
+      end
     end
 
     context "HTTP error" do
       let(:status) { 500 }
       let(:body) { "" }
-      it { is_expected.to eq [] }
+
+      context "no cache values are stored" do
+        before { page.ckan_values_cache = nil }
+        it { is_expected.to eq [] }
+      end
+
+      context "cache values are stored" do
+        # Marshal.dump([1]) #=> "\x04\b[\x06i\x06"
+        before { page.ckan_values_cache = "\x04\b[\x06i\x06" }
+        it { is_expected.to eq [1] }
+      end
     end
 
     context "failure" do
       let(:status) { 200 }
       let(:body) { "{\"success\":false}" }
-      it { is_expected.to eq [] }
+
+      context "no cache values are stored" do
+        before { page.ckan_values_cache = nil }
+        it { is_expected.to eq [] }
+      end
+
+      context "cache values are stored" do
+        # Marshal.dump([1]) #=> "\x04\b[\x06i\x06"
+        before { page.ckan_values_cache = "\x04\b[\x06i\x06" }
+        it { is_expected.to eq [1] }
+      end
     end
 
     after(:all) { WebMock.disable! }
