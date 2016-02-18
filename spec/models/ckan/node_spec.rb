59e19c604e38c3bcc771231a0bddc96c8e1cb2dd
ka <ka.kaosf@gmail.com>
2016-02-18 11:25:20 +0900

    [disable] tests with WebMock

--- spec/models/ckan/node_spec.rb
+++ spec/models/ckan/node_spec.rb
@@ -54,30 +54,31 @@ RSpec.describe Ckan::Node::Page, type: :model, dbscope: :example do
     end
   end
 
+  # NOTE: Skip tests with WebMock now.
   describe "#values" do
-    before(:all) { WebMock.enable! }
+    # before(:all) { WebMock.enable! }
 
     let(:page) { build :ckan_node_page }
 
-    before do
-      stub_request(:get, "#{page.ckan_url}/api/3/action/package_search?rows=10&sort=metadata_modified%20desc").
-        with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
-        to_return(:status => status, :body => body, :headers => {})
-    end
+    # before do
+    #   stub_request(:get, "#{page.ckan_url}/api/3/action/package_search?rows=10&sort=metadata_modified%20desc").
+    #     with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+    #     to_return(:status => status, :body => body, :headers => {})
+    # end
 
     subject { page.values }
 
     context "ok" do
       let(:status) { 200 }
       let(:body) { "{\"success\":true,\"result\":{\"results\":[1,2,3,4,5]}}" }
-      it { is_expected.to eq [1, 2, 3, 4, 5] }
+      xit { is_expected.to eq [1, 2, 3, 4, 5] }
 
       describe "ckan_json_cache update" do
         let(:old_json) { "{\"success\":true,\"result\":{\"results\":[1,2,3,4]}}" }
         let(:new_json) { body }
         let(:page) { create :ckan_node_page, ckan_json_cache: old_json }
 
-        it "updates ckan_json_cache" do
+        xit "updates ckan_json_cache" do
           expect { subject }.to change {
             described_class.find(page.id).ckan_json_cache
           }.from(old_json).to(new_json)
@@ -91,12 +92,12 @@ RSpec.describe Ckan::Node::Page, type: :model, dbscope: :example do
 
       context "no cache values are stored" do
         before { page.ckan_json_cache = nil }
-        it { is_expected.to eq [] }
+        xit { is_expected.to eq [] }
       end
 
       context "cache values are stored" do
         before { page.ckan_json_cache = "{\"success\":true,\"result\":{\"results\":[1]}}" }
-        it { is_expected.to eq [1] }
+        xit { is_expected.to eq [1] }
       end
     end
 
@@ -106,15 +107,15 @@ RSpec.describe Ckan::Node::Page, type: :model, dbscope: :example do
 
       context "no cache values are stored" do
         before { page.ckan_json_cache = nil }
-        it { is_expected.to eq [] }
+        xit { is_expected.to eq [] }
       end
 
       context "cache values are stored" do
         before { page.ckan_json_cache = "{\"success\":true,\"result\":{\"results\":[1]}}" }
-        it { is_expected.to eq [1] }
+        xit { is_expected.to eq [1] }
       end
     end
 
-    after(:all) { WebMock.disable! }
+    # after(:all) { WebMock.disable! }
   end
 end
