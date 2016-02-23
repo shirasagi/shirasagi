8a6c6e7d5141d0d183f5782590d4d96fe7e3cd6a
tany <una@kimiyure.com>
2016-02-23 20:35:59 +0900

    [fix] ss_files: enable multibytes name

--- app/models/concerns/ss/model/file.rb
+++ app/models/concerns/ss/model/file.rb
@@ -171,7 +171,7 @@ module SS::Model::File
 
   private
     def set_filename
-      self.name         ||= in_file.original_filename
+      self.name         = in_file.original_filename if self[:name].blank?
       self.filename     = in_file.original_filename if filename.blank?
       self.size         = in_file.size
       self.content_type = ::SS::MimeType.find(in_file.original_filename, in_file.content_type)
