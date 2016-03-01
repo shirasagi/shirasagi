9ac8d19e0488f5d67a754f460949f8b19baee9ca
itowtips <ito@web-tips.co.jp>
2016-03-01 19:10:35 +0900

    [update] db seed

--- db/seeds/demo/load.rb
+++ db/seeds/demo/load.rb
@@ -919,6 +919,25 @@ if user
     user_id: user.id, poster: "管理者", delete_key: 1234, poster_url: " http://demo.ss-proj.org/", file_ids: [file.id]
 end
 
+puts "# board"
+def save_body_layouts(data)
+  puts data[:name]
+  cond = { site_id: data[:site_id], node_id: data[:node_id], name: data[:name], poster: data[:poster] }
+  item = Cms::BodyLayout.where(cond).first || Cms::BodyLayout.new
+  item.attributes = data
+  item.save
+
+  item
+end
+body_layout_html = File.read("body_layouts/layout.layout.html") rescue nil
+body_layout = save_body_layouts name: "本文レイアウト",
+  html: body_layout_html,
+  parts: %W(本文1 本文2 本文3),
+  site_id: @site.id
+save_page route: "article/page", filename: "docs/body_layout.html", name: "本文レイアウト",
+  layout_id: layouts["pages"].id, body_layout_id: body_layout.id, body_parts: %W(本文1 本文2 本文3),
+  contact_group_id: contact_group_id, contact_email: contact_email, contact_tel: contact_tel, contact_fax: contact_fax
+
 puts "# cms pages"
 save_page route: "cms/page", filename: "index.html", name: "自治体サンプル", layout_id: layouts["top"].id
 save_page route: "cms/page", filename: "mobile.html", name: "スマートフォン・携帯サイト", layout_id: layouts["pages"].id
