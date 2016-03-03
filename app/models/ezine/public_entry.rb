fb8e6139022b1fbbd1905a5440c2ea60af5adec3
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-03-03 10:04:54 +0900

    use client instead of session

--- app/models/ezine/public_entry.rb
+++ app/models/ezine/public_entry.rb
@@ -2,5 +2,5 @@ class Ezine::PublicEntry
   include Ezine::Entryable
   include Ezine::Addon::Data
 
-  store_in session: :public, collection: :ezine_entries
+  store_in client: :public, collection: :ezine_entries
 end
