75ad7631788f44bf11f1591659c6a12669fc4075
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-03-22 12:08:15 +0900

    [add] inquiry validation specs

--- spec/factories/inquiry/columns.rb
+++ spec/factories/inquiry/columns.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :inquiry_column1, class: Inquiry::Column do
+  factory :inquiry_column_name, class: Inquiry::Column do
     name "お名前"
     input_type "text_field"
     required "required"
@@ -7,7 +7,7 @@ FactoryGirl.define do
     order 10
   end
 
-  factory :inquiry_column2, class: Inquiry::Column do
+  factory :inquiry_column_optional, class: Inquiry::Column do
     name "企業・団体名"
     input_type "text_field"
     required "optional"
@@ -15,11 +15,39 @@ FactoryGirl.define do
     order 20
   end
 
-  factory :inquiry_column3, class: Inquiry::Column do
+  factory :inquiry_column_email, class: Inquiry::Column do
     name "メールアドレス"
     input_type "email_field"
     required "required"
+    input_confirm "enabled"
     html "<p>半角 英数字記号で入力してください。<br />\nお問い合わせへの返信に利用させていただきます。</p>"
     order 30
+  end
+
+  factory :inquiry_column_radio, class: Inquiry::Column do
+    name "性別"
+    input_type "radio_button"
+    required "optional"
+    html "<p>性別を選択して ください。</p>"
+    select_options ["男性", "女性"]
+    order 40
+  end
+
+  factory :inquiry_column_select, class: Inquiry::Column do
+    name "年齢"
+    input_type "select"
+    required "optional"
+    html "<p>年齢を選択してください。</p>"
+    select_options ["10代", "20代", "30代", "40代", "50代", "60代", "70代", "80代"]
+    order 50
+  end
+
+  factory :inquiry_column_check, class: Inquiry::Column do
+    name "お問い合わせ区分"
+    input_type "check_box"
+    required "optional"
+    html "<p>お問い合わせ内容の区分を選択してください。</p>"
+    select_options ["市政について", "ご意見・ご要望", "申請について", "その他"]
+    order 60
   end
 end
--- spec/features/inquiry/agents/nodes/form_spec.rb
+++ spec/features/inquiry/agents/nodes/form_spec.rb
@@ -5,9 +5,12 @@ describe "inquiry_agents_nodes_form", dbscope: :example do
   let(:node) { create :inquiry_node_form, site: site, inquiry_captcha: 'disabled' }
 
   before do
-    node.columns.create! attributes_for(:inquiry_column1).reverse_merge({cur_site: site})
-    node.columns.create! attributes_for(:inquiry_column2).reverse_merge({cur_site: site})
-    node.columns.create! attributes_for(:inquiry_column3).reverse_merge({cur_site: site})
+    node.columns.create! attributes_for(:inquiry_column_name).reverse_merge({cur_site: site})
+    node.columns.create! attributes_for(:inquiry_column_optional).reverse_merge({cur_site: site})
+    node.columns.create! attributes_for(:inquiry_column_email).reverse_merge({cur_site: site})
+    node.columns.create! attributes_for(:inquiry_column_radio).reverse_merge({cur_site: site})
+    node.columns.create! attributes_for(:inquiry_column_select).reverse_merge({cur_site: site})
+    node.columns.create! attributes_for(:inquiry_column_check).reverse_merge({cur_site: site})
     node.reload
   end
 
@@ -22,6 +25,10 @@ describe "inquiry_agents_nodes_form", dbscope: :example do
           fill_in "item[1]", with: "シラサギ太郎"
           fill_in "item[2]", with: "株式会社シラサギ"
           fill_in "item[3]", with: "shirasagi@example.jp"
+          fill_in "item[3_confirm]", with: "shirasagi@example.jp"
+          choose "item_4_0"
+          select "50代", from: "item[5]"
+          check "item[6][2]"
         end
         click_button "確認画面へ"
       end
@@ -32,6 +39,9 @@ describe "inquiry_agents_nodes_form", dbscope: :example do
           expect(find('#item_1')['value']).to eq 'シラサギ太郎'
           expect(find('#item_2')['value']).to eq '株式会社シラサギ'
           expect(find('#item_3')['value']).to eq 'shirasagi@example.jp'
+          expect(find('#item_4')['value']).to eq '男性'
+          expect(find('#item_5')['value']).to eq '50代'
+          expect(find('#item_6_2')['value']).to eq '申請について'
         end
         # within 'div.simple-captcha' do
         #   fill_in "answer[captcha]", with: "xxxx"
@@ -59,6 +69,10 @@ describe "inquiry_agents_nodes_form", dbscope: :example do
           fill_in "item[1]", with: "シラサギ太郎"
           fill_in "item[2]", with: "株式会社シラサギ"
           fill_in "item[3]", with: "shirasagi@example.jp"
+          fill_in "item[3_confirm]", with: "shirasagi@example.jp"
+          choose "item_4_0"
+          select "50代", from: "item[5]"
+          check "item[6][2]"
         end
         click_button "確認画面へ"
       end
@@ -73,6 +87,9 @@ describe "inquiry_agents_nodes_form", dbscope: :example do
           expect(find('#item_1')['value']).to eq 'シラサギ太郎'
           expect(find('#item_2')['value']).to eq '株式会社シラサギ'
           expect(find('#item_3')['value']).to eq 'shirasagi@example.jp'
+          expect(find('#item_4')['value']).to eq '男性'
+          expect(find('#item_5')['value']).to eq '50代'
+          expect(find('#item_6_2')['value']).to eq '申請について'
         end
         # mobile モードの場合 <footer> タグが <div> タグに置換されているはず
         within 'div.tag-footer' do
@@ -83,6 +100,46 @@ describe "inquiry_agents_nodes_form", dbscope: :example do
       expect(status_code).to eq 200
       # mobile モードの場合、/mobile/ で始まるはず
       expect(current_path).to start_with "#{site.mobile_location}/#{node.filename}/"
+      expect(find('div.inquiry-sent').text).to eq node.inquiry_sent_html.gsub(/<.*?>/, '')
+    end
+  end
+
+  context "when input non-email to email field" do
+    let(:index_url) { ::URI.parse "http://#{site.domain}/#{node.filename}/"}
+
+    it do
+      visit index_url
+      expect(status_code).to eq 200
+
+      puts page.html
+
+      within 'div.inquiry-form' do
+        within 'div.columns' do
+          fill_in "item[1]", with: "シラサギ太郎"
+          fill_in "item[2]", with: "株式会社シラサギ"
+          fill_in "item[3]", with: "<script>alert(\"hello\");</script>"
+        end
+        click_button "確認画面へ"
+      end
+
+      expect(status_code).to eq 200
+      within 'div.inquiry-form' do
+        expect(page).to have_css(".errorExplanation li", text: "メールアドレスを入力してください。")
+
+        within 'div.columns' do
+          expect(find('#item_1')['value']).to eq 'シラサギ太郎'
+          expect(find('#item_2')['value']).to eq '株式会社シラサギ'
+          expect(find('#item_3')['value']).to eq 'shirasagi@example.jp'
+        end
+        # within 'div.simple-captcha' do
+        #   fill_in "answer[captcha]", with: "xxxx"
+        # end
+        within 'footer.send' do
+          click_button "送信する"
+        end
+      end
+
+      expect(status_code).to eq 200
       expect(find('div.inquiry-sent').text).to eq node.inquiry_sent_html.gsub(/<.*?>/, '')
     end
   end
