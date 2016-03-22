6330331de7d4015428a62f1a618d4a10b7bbc52f
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-03-22 10:33:24 +0900

    [modify] use db session instead of cookie session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ss_session'
+# Rails.application.config.session_store :cookie_store, key: '_ss_session'
+Rails.application.config.session_store :mongoid_store
+Rails.application.config.session_options = { cookie_only: false, key: '_ss_session' }
+
+if defined?(MongoidStore::Session)
+  class MongoidStore::Session
+    # set TTL index
+    index({ updated_at: 1 }, { expire_after_seconds: 1.hour })
+  end
+end
