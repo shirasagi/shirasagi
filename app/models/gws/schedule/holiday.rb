4d7a370ffa324088a9e2f4088c7fe1cd3a148f30
tany <una@kimiyure.com>
2016-03-25 15:50:21 +0900

    [add] gws/schedule/holiday: colorize

--- app/models/gws/schedule/holiday.rb
+++ app/models/gws/schedule/holiday.rb
@@ -2,17 +2,32 @@ class Gws::Schedule::Holiday
   include SS::Document
   include Gws::Reference::User
   include Gws::Reference::Site
+  include Gws::Schedule::Colorize
   include Gws::Schedule::Planable
   include Gws::SitePermission
 
   set_permission_name "gws_schedule_holidays", :edit
+
+  field :color, type: String, default: "#99dd66"
+
+  permit_params :color
+
+  validates :color, presence: true
 
   def allday?
     true
   end
 
   def calendar_format
-     { className: 'fc-holiday', title: "  #{name}",
-       start: start_at, end: (end_at + 1.day).to_date, allDay: allday?, editable: false }
+    {
+      className: 'fc-holiday',
+      title: "  #{name}",
+      start: start_at,
+      end: (end_at + 1.day).to_date,
+      allDay: allday?,
+      backgroundColor: color,
+      textColor: text_color,
+      editable: false
+    }
   end
 end
