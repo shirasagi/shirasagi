4ca7a7063b358866a6788b98301b22bfa2a35438
ka <ka.kaosf@gmail.com>
2016-03-31 19:52:21 +0900

    [implement] Cms::Content#name_for_index
    Add "index_name" filed.

--- spec/models/cms/page_spec.rb
+++ spec/models/cms/page_spec.rb
@@ -24,4 +24,18 @@ describe Cms::Page do
     subject { create(:cms_page, route: "article/page") }
     it { expect(subject.becomes_with_route).to be_kind_of(Article::Page) }
   end
+
+  describe "#name_for_index" do
+    let(:item) { model.last }
+    subject { item.name_for_index }
+
+    context "the value is set" do
+      before { item.index_name = "Name for index" }
+      it { is_expected.to eq "Name for index" }
+    end
+
+    context "the value isn't set" do
+      it { is_expected.to eq item.name }
+    end
+  end
 end
