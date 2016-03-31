e6c275e0b7c2a33d9bba3af497b400f8b88abe13
tany <tanizawa@web-tips.co.jp>
2016-04-01 07:06:49 +0900

    [add] gws/schedule: double booking validation

--- app/models/concerns/gws/addon/schedule/facility.rb
+++ app/models/concerns/gws/addon/schedule/facility.rb
@@ -7,6 +7,20 @@ module Gws::Addon::Schedule::Facility
 
     permit_params facility_ids: []
 
+    validate :validate_facility_double_booking, if: ->{ facilities.present? }
+
     scope :facility, ->(item) { where facility_ids: item.id }
   end
+
+  private
+    def validate_facility_double_booking
+      return unless self.class.ne(id: id).
+        where(site_id: site_id).
+        #between_dates(start_at, end_at).
+        where(:end_at.gt => start_at, :start_at.lt => end_at).
+        any_in(facility_ids: facility_ids).
+        exists?
+
+      errors.add :facility_ids, :duplicate
+    end
 end
