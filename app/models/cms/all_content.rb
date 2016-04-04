c0840ba49bf81421743b51abcedf18f63c30ab77
ka <ka.kaosf@gmail.com>
2016-04-04 20:56:00 +0900

    [add] all contents csv exporting feature
    For #753.

---
+++ app/models/cms/all_content.rb
@@ -0,0 +1,45 @@
+require "csv"
+
+class Cms::AllContent
+  class << self
+    def csv(encode: nil)
+      CSV.generate do |data|
+        data << header
+        (Cms::Page.all + Cms::Node.all).each do |content|
+          data << row(content)
+        end
+      end
+    end
+
+    private
+      def header
+        %w(url group_ids group_names id file_size name created released updated close_date status route category_ids use_map have_files)
+          .map { |e| I18n.t("all_content.#{e}") }
+      end
+
+      def row(content)
+        [
+          content.url,
+          content.group_ids.join(","),
+          Cms::Group.where(:id.in => content.group_ids).pluck(:name).join(","),
+          content.id,
+          file_size(content.path),
+          content.name,
+          content.created,
+          content.released,
+          content.updated,
+          content.try(:close_date),
+          content.status,
+          content.route,
+          Category::Node::Base.where(:id.in => content.category_ids).pluck(:name).join(","),
+          content.try(:map_points).present?,
+          content.try(:files).present?,
+        ]
+      end
+
+      def file_size(file_path)
+        File.exist?(file_path) ? File.stat(file_path).size : nil
+      end
+  end
+end
+
