f3c1f54c1869e4b5ffa21e4b91830694a11a5585
ka <ka.kaosf@gmail.com>
2016-04-05 16:23:39 +0900

    [add] tests

---
+++ spec/models/cms/all_content_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Cms::AllContent, type: :model, dbscope: :example  do
+  describe ".csv" do
+    let(:csv) { described_class.csv }
+
+    describe "header line" do
+      let(:header) { csv.split("\n")[0].split(",") }
+      it { expect(header[0]).to eq(I18n.t("all_content.url")) }
+      it { expect(header[1]).to eq(I18n.t("all_content.group_ids")) }
+      it { expect(header[14]).to eq(I18n.t("all_content.have_files")) }
+    end
+
+    describe "contents" do
+      before do
+        @page = create :cms_page
+        @node = create :cms_node
+      end
+
+      let(:line1) { csv.split("\n")[1].split(",") }
+      let(:line2) { csv.split("\n")[2].split(",") }
+
+      it { expect(line1[0]).to eq @page.url }
+      it { expect(line2[0]).to eq @node.url }
+    end
+  end
+end
