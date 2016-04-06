25c326d5788a253bf929a79c32d27f00912d6c15
ka <ka.kaosf@gmail.com>
2016-04-07 02:49:00 +0900

    [fix] filename creation process
    Remove dirname (e.g. "docs" for "docs/page27.html") from "master.filename".

--- app/models/concerns/workflow/addon/branch.rb
+++ app/models/concerns/workflow/addon/branch.rb
@@ -121,7 +121,8 @@ module Workflow::Addon
       end
 
       def seq_clone_filename_with_master
-        filename_backup = master.filename.gsub(File.extname(master.filename), '')
+        filename_backup = File.basename(master.filename)
+        filename_backup = filename_backup.gsub(File.extname(master.filename), '')
         last_branch = master.branches.order(created_at: -1).first
         if last_branch.nil?
           number = '01'
