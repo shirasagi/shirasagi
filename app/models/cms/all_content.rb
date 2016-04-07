d9f5cd4150e85fd3accd71d5a084ecdb5da2ccd3
ka <ka.kaosf@gmail.com>
2016-04-07 17:42:40 +0900

    [fix] AllContent.csv #753
    Modify from .url to .full_url.
    Modify ja locale of name.
    Add index_name.
    
    For the comment of #753:
    
    https://github.com/shirasagi/shirasagi/issues/753#issuecomment-206192848

--- app/models/cms/all_content.rb
+++ app/models/cms/all_content.rb
@@ -13,18 +13,19 @@ class Cms::AllContent
 
     private
       def header
-        %w(url group_ids group_names id file_size name created released updated close_date status route category_ids use_map have_files)
+        %w(url group_ids group_names id file_size name index_name created released updated close_date status route category_ids use_map have_files)
           .map { |e| I18n.t("all_content.#{e}") }
       end
 
       def row(content)
         [
-          content.url,
+          content.full_url,
           content.group_ids.join(","),
           Cms::Group.where(:id.in => content.group_ids).pluck(:name).join(","),
           content.id,
           file_size(content.path),
           content.name,
+          content.index_name,
           content.created.strftime("%F %T"),
           content.released.try(:strftime, "%F %T"),
           content.updated.strftime("%F %T"),
