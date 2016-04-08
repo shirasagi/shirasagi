7fa986f0919e048286d3755debe6341b64ebe00f
tany <tanizawa@web-tips.co.jp>
2016-04-09 07:23:23 +0900

    [modify] gws/schedul: faciity: minimum reservation time = 5.min

--- app/models/concerns/gws/addon/schedule/facility.rb
+++ app/models/concerns/gws/addon/schedule/facility.rb
@@ -7,12 +7,20 @@ module Gws::Addon::Schedule::Facility
 
     permit_params facility_ids: []
 
+    validate :validate_facility_time, if: ->{ facilities.present? }
     validate :validate_facility_double_booking, if: ->{ facilities.present? }
 
     scope :facility, ->(item) { where facility_ids: item.id }
   end
 
   private
+    def validate_facility_time
+      min_time = 5
+      if (end_at.to_i - start_at.to_i) < 60 * min_time
+        errors.add :base, I18n.t("gws/schedule.errors.faciliy_time_gte", count: min_time)
+      end
+    end
+
     def validate_facility_double_booking
       return unless self.class.ne(id: id).
         where(site_id: site_id).
