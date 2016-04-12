44706c58a01a7b0c2a7a4c0ef42dfbadd2e23ee5
ka <ka.kaosf@gmail.com>
2016-04-12 23:46:21 +0900

    [implement] 2 helper methods of gravatar_image_url and gravatar_image_tag

--- app/helpers/gravatar_helper.rb
+++ app/helpers/gravatar_helper.rb
@@ -1,2 +1,9 @@
 module GravatarHelper
+  def gravatar_image_url(email, size = 150)
+    "https://gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}?s=#{size}"
+  end
+
+  def gravatar_image_tag(email, size = 150, options = {})
+    tag(:img, options.merge({ src: gravatar_image_url(email, size) }))
+  end
 end
