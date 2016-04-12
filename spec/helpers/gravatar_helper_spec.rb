fbda5be677769be348453cd1ece1c4f2b9b36818
ka <ka.kaosf@gmail.com>
2016-04-12 23:46:21 +0900

    [implement] 2 helper methods of gravatar_image_url and gravatar_image_tag

--- spec/helpers/gravatar_helper_spec.rb
+++ spec/helpers/gravatar_helper_spec.rb
@@ -1,15 +1,32 @@
-require 'rails_helper'
+require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the GravatarHelper. For example:
-#
-# describe GravatarHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe GravatarHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email) { "user@example.jp" }
+  let(:md5sum) { "12b75d826c225ac1e313d05d44cce941" }
+
+  describe "assertion" do
+    it { expect(Digest::MD5.hexdigest(email)).to eq md5sum }
+  end
+
+  describe ".gravatar_image_url" do
+    it "returns Gravatar's image URL" do
+      expect(helper.gravatar_image_url(email, 100)).to eq "https://gravatar.com/avatar/#{md5sum}?s=100"
+    end
+
+    describe "default \"size\"" do
+      it("is 150") { expect(helper.gravatar_image_url(email)).to eq  "https://gravatar.com/avatar/#{md5sum}?s=150" }
+    end
+  end
+
+  describe ".gravatar_image_tag" do
+    it "returns an img element which has \"image_url\" as a \"src\"" do
+      expect(helper.gravatar_image_tag(email, 100)).to eq "<img src=\"https://gravatar.com/avatar/#{md5sum}?s=100\" />"
+    end
+
+    it "returns an img element which has some attributes" do
+      expect(helper.gravatar_image_tag(email, 100, alt: "alt", title: "title")).to eq(
+          "<img alt=\"alt\" title=\"title\" src=\"https://gravatar.com/avatar/#{md5sum}?s=100\" />"
+        )
+    end
+  end
 end
