9a850188cb252fd2dc5004bb09ac23fbea1376c9
ka <ka.kaosf@gmail.com>
2016-04-13 21:46:34 +0900

    [enable] to specify "d" option as a default image URL

--- app/helpers/gravatar_helper.rb
+++ app/helpers/gravatar_helper.rb
@@ -1,9 +1,11 @@
 module GravatarHelper
-  def gravatar_image_url(email, size = 150)
-    "https://gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}?s=#{size}"
+  def gravatar_image_url(email, size = 150, default: nil)
+    url = "https://gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}?s=#{size}"
+    default.present? ? "#{url}&d=#{CGI.escape(default)}" : url
   end
 
   def gravatar_image_tag(email, size = 150, options = {})
-    tag(:img, options.merge({ src: gravatar_image_url(email, size) }))
+    default = options.delete :default
+    tag(:img, options.merge({ src: gravatar_image_url(email, size, default: default) }))
   end
 end
