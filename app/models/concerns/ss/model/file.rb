a165292026a0bbba8f61bd05e675498b6de2d7f5
itowtips <ito@web-tips.co.jp>
2016-04-14 17:28:04 +0900

    [modify] delete attached image's exif

--- app/models/concerns/ss/model/file.rb
+++ app/models/concerns/ss/model/file.rb
@@ -186,14 +186,19 @@ module SS::Model::File
       return false if errors.present?
       return if in_file.blank?
 
-      if image? && resizing
-        width, height = resizing
+      if image?
         image = Magick::Image.from_blob(in_file.read).shift
-        image = image.resize_to_fit width, height if image.columns > width || image.rows > height
+        image.auto_orient!
+        image.strip!
+        if resizing
+          width, height = resizing
+          image = image.resize_to_fit width, height if image.columns > width || image.rows > height
+        end
         binary = image.to_blob
       else
         binary = in_file.read
       end
+      in_file.rewind
 
       dir = ::File.dirname(path)
       Fs.mkdir_p(dir) unless Fs.exists?(dir)
