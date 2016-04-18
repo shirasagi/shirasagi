9de382a2f10fb673fb0fa13bd07a72537ecfbe73
tany <una@kimiyure.com>
2016-04-18 22:33:35 +0900

    [fix] spec: gws/share/file

--- spec/factories/gws/share/file.rb
+++ spec/factories/gws/share/file.rb
@@ -5,5 +5,6 @@ FactoryGirl.define do
 
     name { "name-#{unique_id}" }
     in_file Fs::UploadedFile.create_from_file "#{Rails.root}/spec/fixtures/ss/logo.png", content_type: 'image/png'
+    category_ids [1]
   end
 end
