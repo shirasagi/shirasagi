01c8ace3605c91d6dd60c7d40e1a5bbc3c4925d3
tany <una@kimiyure.com>
2016-04-20 17:28:42 +0900

    [fix] spec support: create_gws_users

--- spec/support/gws/user.rb
+++ spec/support/gws/user.rb
@@ -1,27 +1,17 @@
 def create_gws_users
-  return if SS::Group.all.present?
+  return if Gws::Group.where(name: 'gws-sys').exists?
 
   g00 = SS::Group.create name: "シラサギ市", order: 10
   g10 = SS::Group.create name: "シラサギ市/企画政策部", order: 20
   g11 = SS::Group.create name: "シラサギ市/企画政策部/政策課", order: 30
-  #g12 = SS::Group.create name: "シラサギ市/企画政策部/広報課", order: 40
-  #g20 = SS::Group.create name: "シラサギ市/危機管理部", order: 50
-  #g21 = SS::Group.create name: "シラサギ市/危機管理部/管理課", order: 60
-  #g22 = SS::Group.create name: "シラサギ市/危機管理部/防災課", order: 70
 
   role = Gws::Role.create name: I18n.t('gws.roles.admin'), site_id: g00.id,
     permissions: Gws::Role.permission_names, permission_level: 3
 
-  sys = Gws::User.create name: "システム管理者", uid: "sys", email: "sys@example.jp", in_password: "pass",
+  sys = Gws::User.create name: "gws-sys", uid: "sys", email: "sys@example.jp", in_password: "pass",
     group_ids: [g11.id], gws_role_ids: [role.id]
-  adm = Gws::User.create name: "サイト管理者", uid: "admin", email: "admin@example.jp", in_password: "pass",
+  adm = Gws::User.create name: "gw-admin", uid: "admin", email: "admin@example.jp", in_password: "pass",
     group_ids: [g11.id], gws_role_ids: [role.id]
-  #u01 = Gws::User.create name: "一般ユーザー1", uid: "user1", email: "user1@example.jp", in_password: "pass",
-  #  group_ids: [g11.id], gws_role_ids: [role.id]
-  #u02 = Gws::User.create name: "一般ユーザー2", uid: "user2", email: "user2@example.jp", in_password: "pass",
-  #  group_ids: [g21.id], gws_role_ids: [role.id]
-  #u03 = Gws::User.create name: "一般ユーザー3", uid: "user3", email: "user3@example.jp", in_password: "pass",
-  #  group_ids: [g12.id, g22.id], gws_role_ids: [role.id]
 end
 
 def gws_site
