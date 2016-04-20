16e505c4ef79f1e7fbedf2e9985f88f491ec2c1e
tany <una@kimiyure.com>
2016-04-20 19:51:00 +0900

    [modify] add gws/history feature to board and link

--- app/models/gws/board/post.rb
+++ app/models/gws/board/post.rb
@@ -1,8 +1,10 @@
 # "Post" class for BBS. It represents "comment" models.
 class Gws::Board::Post
+  include Gws::Referenceable
   include Gws::Board::Postable
   include SS::Addon::Markdown
   include Gws::Addon::File
   include Gws::Board::DescendantsFileInfo
   include Gws::Addon::GroupPermission
+  include Gws::Addon::History
 end
