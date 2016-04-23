43cfabae4bf6fac69a3a943666a36503d8da62dd
tany <tanizawa@web-tips.co.jp>
2016-04-24 00:28:12 +0900

    [add] gws/schedule: search plans

---
+++ app/controllers/gws/schedule/search_plans_controller.rb
@@ -0,0 +1,21 @@
+class Gws::Schedule::SearchPlansController < ApplicationController
+  include Gws::BaseFilter
+  #include Gws::CrudFilter
+  include Gws::Schedule::PlanFilter
+
+  private
+
+  public
+    def index
+      if params[:s].blank?
+        @items = []
+        return
+      end
+
+      @items = Gws::User.site(@cur_site).
+        active.
+        search(params[:s]).
+        order_by_title(@cur_site).
+        limit(10)
+    end
+end
