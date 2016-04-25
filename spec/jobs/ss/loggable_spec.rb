d83e64c6b40fb71a75c52a84468817648e9ab0d7
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-04-25 23:33:47 +0900

    [add] job/ss/loggable spec

---
+++ spec/jobs/ss/loggable_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+describe Job::SS::Loggable, dbscope: :example do
+  # let(:job_class) do
+  #   m = Module.new do
+  #     class TestJob < ActiveJob::Base
+  #       include Job::SS::Loggable
+  #
+  #       def perform
+  #         raise Thread.current['ss.exception']
+  #       end
+  #     end
+  #   end
+  #
+  #   m.const_get(:TestJob)
+  # end
+  let(:job_class) do
+    Class.new(ActiveJob::Base) do
+      include Job::SS::Loggable
+
+      def self.name
+        unique_id
+      end
+
+      def perform
+        raise Thread.current['ss.exception']
+      end
+    end
+  end
+
+  context "with standard exception" do
+    before do
+      Thread.current['ss.exception'] = ArgumentError
+    end
+
+    it do
+      expect { job_class.perform_now }.not_to raise_error
+
+      expect(Job::Log.count).to eq 1
+      log = Job::Log.first
+      expect(log.logs).to include(include("INFO -- : Started Job"))
+      expect(log.logs).not_to include(include("INFO -- : Completed Job"))
+      expect(log.logs).to include(include("FATAL -- : Failed Job"))
+      expect(log.state).to eq Job::Log::STATE_FAILED
+    end
+  end
+
+  context "with system exception" do
+    before do
+      Thread.current['ss.exception'] = NoMemoryError
+    end
+
+    it do
+      expect { job_class.perform_now }.to raise_error NoMemoryError
+
+      expect(Job::Log.count).to eq 1
+      log = Job::Log.first
+      expect(log.logs).to include(include("INFO -- : Started Job"))
+      expect(log.logs).not_to include(include("INFO -- : Completed Job"))
+      expect(log.logs).to include(include("FATAL -- : Failed Job"))
+      expect(log.state).to eq Job::Log::STATE_FAILED
+    end
+  end
+end
