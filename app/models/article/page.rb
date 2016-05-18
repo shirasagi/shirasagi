d2905d72bcaf29e664314d9129d96c3a0862959f
ka <ka.kaosf@gmail.com>
2016-05-18 19:50:04 +0900

    [impelement] Gravatar::Addon::Gravatar module

--- app/models/article/page.rb
+++ app/models/article/page.rb
@@ -5,6 +5,7 @@ class Article::Page
   include Workflow::Addon::Branch
   include Workflow::Addon::Approver
   include Cms::Addon::Meta
+  include Gravatar::Addon::Gravatar
   include Cms::Addon::Body
   include Cms::Addon::BodyPart
   include Cms::Addon::File
