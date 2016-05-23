8ab9e5a623c5b537d9214b2f8f0b4d0062e93d27
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-05-23 19:20:38 +0900

    [modify] public controller always doen't care about csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token unless SS.config.env.protect_csrf
+  skip_before_action :verify_authenticity_token, if: ->{ !protect_csrf? }
 
   #before_action -> { FileUtils.touch "#{Rails.root}/Gemfile" } if Rails.env.to_s == "development"
   before_action :set_cache_buster
@@ -29,6 +29,10 @@ class ApplicationController < ActionController::Base
   end
 
   private
+    def protect_csrf?
+      SS.config.env.protect_csrf
+    end
+
     def remote_addr
       request.env["HTTP_X_REAL_IP"] || request.remote_addr
     end
