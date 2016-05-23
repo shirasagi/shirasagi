ca2e0714f5c7ac005004c598f562d5e085fed987
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-05-23 13:42:35 +0900

    [add] gravatar helper

--- app/controllers/concerns/cms/page_filter/view.rb
+++ app/controllers/concerns/cms/page_filter/view.rb
@@ -2,6 +2,10 @@ module Cms::PageFilter::View
   extend ActiveSupport::Concern
   include SS::AgentFilter
 
+  included do
+    helper Gravatar::GravatarHelper
+  end
+
   def index
     render
   end
