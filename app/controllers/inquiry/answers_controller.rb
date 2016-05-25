712cb7d8bfe5bbca5d530a1dcc8f97154c6a45c3
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-05-25 11:11:26 +0900

    [add] source_url and source_name to answer download

--- app/controllers/inquiry/answers_controller.rb
+++ app/controllers/inquiry/answers_controller.rb
@@ -12,15 +12,22 @@ class Inquiry::AnswersController < ApplicationController
       require "csv"
 
       columns = @cur_node.becomes_with_route("inquiry/form").columns.pluck(:name)
+      headers = %w(id)
+      headers += columns
+      headers += %w(created source_url source_name)
       csv = CSV.generate do |data|
-        data << columns
+        data << headers
         items.each do |item|
           values = item.data.map{|d| [d.column.name, d.value]}.to_h
 
           row = []
+          row << item.id
           columns.each do |col|
             row << values[col]
           end
+          row << item.updated.strftime("%Y/%m/%d %H:%M")
+          row << item.source_url
+          row << item.source_name
 
           data << row
         end
@@ -56,6 +63,11 @@ class Inquiry::AnswersController < ApplicationController
     end
 
     def download
-      send_csv @cur_node.becomes_with_route("inquiry/form").answers
+      raise "403" unless @cur_node.allowed?(:read, @cur_user, site: @cur_site)
+      @items = @model.site(@cur_site).
+        where(node_id: @cur_node.id).
+        search(params[:s]).
+        order_by(updated: -1)
+      send_csv @items
     end
 end
