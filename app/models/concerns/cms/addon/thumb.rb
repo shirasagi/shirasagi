dcf5ae299ff92ed074df9ea83310a40b3dd1e542
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-05-30 18:33:29 +0900

    [fix] unable to generate thumbnail file

--- app/models/concerns/cms/addon/thumb.rb
+++ app/models/concerns/cms/addon/thumb.rb
@@ -9,12 +9,25 @@ module Cms::Addon
       belongs_to_file :thumb
       permit_params :in_thumb
       validate :validate_thumb, if: ->{ in_thumb.present? }
+
+      after_generate_file :generate_thumb_public_file, if: ->{ serve_static_relation_files? } if respond_to?(:after_generate_file)
+      after_remove_file :remove_thumb_public_file if respond_to?(:after_remove_file)
     end
 
     private
       def validate_thumb
         file = relation_file(:thumb)
         errors.add :thumb_id, :thums_is_not_an_image unless file.image?
+      end
+
+      def generate_thumb_public_file
+        return if thumb.blank?
+        thumb.generate_public_file
+      end
+
+      def remove_thumb_public_file
+        return if thumb.blank?
+        thumb.remove_public_file
       end
   end
 end
