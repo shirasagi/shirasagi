6841f6782fc85c807379621bbd3608563d040bc9
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-06-07 21:16:10 +0900

    [fix] key_visual translation errors on cms/roles show/edit (#988)

--- app/models/key_visual/initializer.rb
+++ app/models/key_visual/initializer.rb
@@ -3,11 +3,11 @@ module KeyVisual
     Cms::Node.plugin "key_visual/image"
     Cms::Part.plugin "key_visual/slide"
 
-    Cms::Role.permission :read_other_key_visual_images
-    Cms::Role.permission :read_private_key_visual_images
-    Cms::Role.permission :edit_other_key_visual_images
-    Cms::Role.permission :edit_private_key_visual_images
-    Cms::Role.permission :delete_other_key_visual_images
-    Cms::Role.permission :delete_private_key_visual_images
+    Cms::Role.permission :read_other_key_visual_images, module_name: "key_visual"
+    Cms::Role.permission :read_private_key_visual_images, module_name: "key_visual"
+    Cms::Role.permission :edit_other_key_visual_images, module_name: "key_visual"
+    Cms::Role.permission :edit_private_key_visual_images, module_name: "key_visual"
+    Cms::Role.permission :delete_other_key_visual_images, module_name: "key_visual"
+    Cms::Role.permission :delete_private_key_visual_images, module_name: "key_visual"
   end
 end
