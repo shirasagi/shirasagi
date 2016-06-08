9dfc43dacc5b0ce3073d2ebc34b871a0b0973969
itowtips <ito@web-tips.co.jp>
2016-06-09 08:58:17 +0900

    [modify] cms all_content (#995)
    * [modify] cms all_content
    
    * [fix] spec failure

--- app/controllers/cms/all_contents_controller.rb
+++ app/controllers/cms/all_contents_controller.rb
@@ -1,10 +1,12 @@
 class Cms::AllContentsController < ApplicationController
   include Cms::BaseFilter
 
+  navi_view "cms/main/navi"
+
   def index
     respond_to do |format|
       format.html
-      format.csv { send_data Cms::AllContent.csv.encode("SJIS", invalid: :replace, undef: :replace), filename: "all_contents.csv" }
+      format.csv { send_data Cms::AllContent.csv(@cur_site).encode("SJIS", invalid: :replace, undef: :replace), filename: "all_contents.csv" }
     end
   end
 end
--- app/models/cms/all_content.rb
+++ app/models/cms/all_content.rb
@@ -1,11 +1,15 @@
 require "csv"
 
 class Cms::AllContent
+  cattr_accessor :site
+
   class << self
-    def csv(encode: nil)
+    def csv(site, encode: nil)
+      self.site = site
+
       CSV.generate do |data|
         data << header
-        (Cms::Page.all + Cms::Node.all).each do |content|
+        (Cms::Page.site(site).all + Cms::Node.site(site).all).each do |content|
           data << row(content)
         end
       end
@@ -13,34 +17,51 @@ class Cms::AllContent
 
     private
       def header
-        fields = %w(url group_ids group_names id file_size name index_name created released updated close_date
-                    status route category_ids use_map have_files)
+        fields = %w(page_id node_id route name index_name url category_ids files file_urls
+          use_map group_ids group_names close_date created released updated status file_size)
         fields.map { |e| I18n.t("all_content.#{e}") }
       end
 
       def row(content)
+        content.site ||= site
+        page_id    = (content.class == Cms::Page) ? content.id : ""
+        node_id    = (content.class == Cms::Node) ? content.id : ""
+        files      = content.files.map(&:name).join("\n") rescue ""
+        file_urls  = content.files.map(&:url).join("\n") rescue ""
+        map_points = content.map_points.map { |point| point[:loc].join(",") }.join("\n") rescue ""
+
         [
-          content.full_url,
-          content.group_ids.join(","),
-          Cms::Group.where(:id.in => content.group_ids).pluck(:name).join(","),
-          content.id,
-          file_size(content.path),
+          page_id,
+          node_id,
+          content.route,
           content.name,
           content.index_name,
+          content.full_url,
+          Category::Node::Base.where(:id.in => content.category_ids).pluck(:name).join(","),
+          files,
+          file_urls,
+          map_points,
+          content.group_ids.join(","),
+          Cms::Group.where(:id.in => content.group_ids).pluck(:name).join(","),
+          content.try(:close_date).try(:strftime, "%F %T"),
           content.created.strftime("%F %T"),
           content.released.try(:strftime, "%F %T"),
           content.updated.strftime("%F %T"),
-          content.try(:close_date).try(:strftime, "%F %T"),
-          content.status,
-          content.route,
-          Category::Node::Base.where(:id.in => content.category_ids).pluck(:name).join(","),
-          content.try(:map_points).present?,
-          content.try(:files).present?,
+          content.label(:state),
+          file_size(content),
         ]
       end
 
-      def file_size(file_path)
-        File.exist?(file_path) ? File.stat(file_path).size : nil
+      def file_size(content)
+        return nil unless File.exist?(content.path)
+
+        size = File.stat(content.path).size
+        return size unless content.respond_to?(:files)
+
+        content.files.each do |file|
+          size += File.exist?(file.public_path) ? File.stat(file.public_path).size : nil
+        end
+        size
       end
   end
 end
