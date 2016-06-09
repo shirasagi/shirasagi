06a1c6420ee35a74a998891536ada56ec62b7e41
itowtips <ito@web-tips.co.jp>
2016-06-09 14:10:18 +0900

    [fix] remove unused validation

--- app/models/concerns/member/addon/additional_attributes.rb
+++ app/models/concerns/member/addon/additional_attributes.rb
@@ -13,8 +13,6 @@ module Member::Addon
       field :sex, type: String
       field :birthday, type: Date
 
-      attr_accessor :kana_required, :organization_name_required, :job_required, :tel_required, :postal_code_required
-      attr_accessor :addr_required, :sex_required, :birthday_required
       attr_accessor :in_birth
 
       permit_params :kana, :organization_name, :job, :tel, :postal_code, :addr, :sex, :birthday
@@ -24,21 +22,13 @@ module Member::Addon
       before_validation :normalize_in_birth
 
       validates :kana, length: { maximum: 40 }
-      validates :kana, presence: true, if: ->{ kana_required }
       validates :organization_name, length: { maximum: 40 }
-      validates :organization_name, presence: true, if: ->{ organization_name_required }
       validates :job, length: { maximum: 40 }
-      validates :job, presence: true, if: ->{ job_required }
       validates :tel, length: { maximum: 40 }
-      validates :tel, presence: true, if: ->{ tel_required }
       validates :postal_code, length: { maximum: 40 }
-      validates :postal_code, presence: true, if: ->{ postal_code_required }
       validates :addr, length: { maximum: 80 }
-      validates :addr, presence: true, if: ->{ addr_required }
       validates :sex, inclusion: { in: %w(male female), allow_blank: true }
-      validates :sex, presence: true, if: ->{ sex_required }
       validates_with Member::BirthValidator, attributes: :in_birth, if: ->{ in_birth.present? }
-      validates :in_birth, presence: true, if: ->{ birthday_required }
 
       before_save :set_birthday, if: ->{ in_birth.present? }
     end
