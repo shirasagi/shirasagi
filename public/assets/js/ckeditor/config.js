18b8f6e45f0f496a675f2dc0a361a2036a4c8b79
itowtips <ito@web-tips.co.jp>
2016-06-27 15:50:37 +0900

    CKEditor 4.5.9
    [add] CKEditor colorbutton & font plugin

--- public/assets/js/ckeditor/config.js
+++ public/assets/js/ckeditor/config.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -28,15 +28,26 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+	config.removeButtons = 'Underline,Subscript,Superscript,Font,BGColor';
 
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;h4;pre';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
-	
+
 	config.image_previewText = ' ';
-	
+
 	config.removePlugins = 'iframe';
+
+	config.fontSize_style = {
+		element: 'strong',
+		styles: { 'font-size': '#(size)' },
+		overrides: [ { element: 'font', attributes: { 'size': null } } ]
+	};
+
+	config.colorButton_foreStyle = {
+		element: 'strong',
+		styles: { color: '#(color)' }
+	};
 };
