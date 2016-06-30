dac970414a1b64ef88a82127a8b1eb02805ee6f4
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-06-30 17:26:40 +0900

    [fix] unable to delete history logs

--- spec/features/history/cms/logs_spec.rb
+++ spec/features/history/cms/logs_spec.rb
@@ -16,11 +16,37 @@ describe "history_cms_logs" do
   end
 
   context "with auth" do
-    before { login_cms_user }
+    before do
+      login_cms_user
+
+      create(:history_log, site_id: cms_site.id, user_id: cms_user.id, url: "/path/to/#{unique_id}")
+      create(:history_log, site_id: cms_site.id, user_id: cms_user.id, url: "/path/to/#{unique_id}")
+      create(:history_log, site_id: cms_site.id, user_id: cms_user.id, url: "/path/to/#{unique_id}")
+      create(:history_log, site_id: cms_site.id, user_id: cms_user.id, url: "/path/to/#{unique_id}")
+      create(:history_log, site_id: cms_site.id, user_id: cms_user.id, url: "/path/to/#{unique_id}")
+    end
 
     it "#index" do
       visit index_path
-      expect(current_path).not_to eq sns_login_path
+      expect(current_path).to eq index_path
+      expect(page).to have_css('.list-item', count: 5)
+
+      click_on 'ダウンロード'
+      click_on 'ダウンロード'
+
+      expect(page).to have_content('Date,User,Target,Action,URL')
+      expect(page).to have_content(',create,/path/to/')
+
+      visit index_path
+      click_on '削除する'
+      click_on '削除'
+      expect(page).to have_css('.list-item', count: 6)
+
+      visit index_path
+      click_on '削除する'
+      select 'すべて削除', from: 'item[save_term]'
+      click_on '削除'
+      expect(page).to have_css('.list-item', count: 1)
     end
   end
 end
--- spec/features/history/sys/logs_spec.rb
+++ spec/features/history/sys/logs_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "history_sys_logs" do
+describe "history_sys_logs", dbscope: :example do
   subject(:index_path) { history_sys_logs_path }
 
   it "without login" do
@@ -15,11 +15,40 @@ describe "history_sys_logs" do
   end
 
   context "with auth" do
-    before { login_cms_user }
+    before do
+      role = create(:sys_role_admin, name: unique_id)
+      user = create(:sys_user_sample, sys_role_ids: [role.id])
+      login_user user
+
+      create(:history_log, site_id: nil, user_id: user.id, url: "/path/to/#{unique_id}")
+      create(:history_log, site_id: nil, user_id: user.id, url: "/path/to/#{unique_id}")
+      create(:history_log, site_id: nil, user_id: user.id, url: "/path/to/#{unique_id}")
+      create(:history_log, site_id: nil, user_id: user.id, url: "/path/to/#{unique_id}")
+      create(:history_log, site_id: nil, user_id: user.id, url: "/path/to/#{unique_id}")
+    end
 
     it "#index" do
       visit index_path
-      expect(current_path).not_to eq sns_login_path
+      expect(current_path).to eq index_path
+      expect(page).to have_css('.list-item', count: 6)
+
+      click_on 'ダウンロード'
+      click_on 'ダウンロード'
+
+      expect(page).to have_content('Date,User,Target,Action,URL')
+      expect(page).to have_content(',login,/.mypage/login')
+      expect(page).to have_content(',create,/path/to/')
+
+      visit index_path
+      click_on '削除する'
+      click_on '削除'
+      expect(page).to have_css('.list-item', count: 7)
+
+      visit index_path
+      click_on '削除する'
+      select 'すべて削除', from: 'item[save_term]'
+      click_on '削除'
+      expect(page).to have_css('.list-item', count: 1)
     end
   end
 end
