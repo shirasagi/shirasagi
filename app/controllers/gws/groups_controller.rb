b285d5a9d97b632c1faac4ad97273a19dc272814
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-07-01 20:06:41 +0900

    [fix] group list when group is searched.

--- app/controllers/gws/groups_controller.rb
+++ app/controllers/gws/groups_controller.rb
@@ -24,10 +24,19 @@ class Gws::GroupsController < ApplicationController
     def index
       raise "403" unless @model.allowed?(:read, @cur_user, site: @cur_site, node: @cur_node)
 
+      @search_params = params[:s]
+      @search_params = @search_params.except(:state) if @search_params
+      @search_params = @search_params.presence
+
       @items = @model.site(@cur_site).
         state(params.dig(:s, :state)).
-        allow(:read, @cur_user, site: @cur_site).
-        search(params[:s]).tree_sort
+        allow(:read, @cur_user, site: @cur_site)
+
+      if @search_params
+        @items = @items.search(@search_params).page(params[:page]).per(50)
+      else
+        @items = @items.tree_sort
+      end
     end
 
     def destroy
