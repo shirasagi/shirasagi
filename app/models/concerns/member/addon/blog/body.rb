0d1b6bc10e5f074f028f00a76f3d2395a5183dfe
itowtips <ito@web-tips.co.jp>
2016-07-04 14:50:54 +0900

    [fix] member blog & photo img src handler (#1061)

--- app/models/concerns/member/addon/blog/body.rb
+++ app/models/concerns/member/addon/blog/body.rb
@@ -7,12 +7,31 @@ module Member::Addon::Blog
       field :html, type: String, metadata: { unicode: :nfc }
       permit_params :html
       validates :html, presence: true
+
+      if respond_to?(:template_variable_handler)
+        template_variable_handler('img.src', :template_variable_handler_img_src)
+      end
     end
 
     def summary
       #return summary_html if summary_html.present?
       return "" unless html.present?
       ApplicationController.helpers.sanitize(html, tags: []).squish.truncate(120)
+    end
+
+    def template_variable_handler_img_src(name, issuer)
+      dummy_source = ERB::Util.html_escape("/assets/img/dummy.png")
+
+      return dummy_source unless html =~ /\<\s*?img\s+[^>]*\/?>/i
+
+      img_tag = $&
+      return dummy_source unless img_tag =~ /src\s*=\s*(['"]?[^'"]+['"]?)/
+
+      img_source = $1
+      img_source = img_source[1..-1] if img_source.start_with?("'", '"')
+      img_source = img_source[0..-2] if img_source.end_with?("'", '"')
+      img_source = img_source.strip
+      img_source
     end
   end
 end
