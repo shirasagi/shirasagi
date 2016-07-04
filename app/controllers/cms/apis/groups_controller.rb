fa64d9b7ba620bd3d5879a0840d6dd996a52a3f8
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-07-04 16:03:03 +0900

    [modify] apply cms group order to all group views

--- app/controllers/cms/apis/groups_controller.rb
+++ app/controllers/cms/apis/groups_controller.rb
@@ -6,9 +6,17 @@ class Cms::Apis::GroupsController < ApplicationController
   def index
     @single = params[:single].present?
     @multi = !@single
-    @items = @model.site(@cur_site).
-      active.
-      search(params[:s]).
-      page(params[:page]).per(50)
+
+    @search_params = params[:s]
+    @search_params = @search_params.except(:state).delete_if { |k, v| v.blank? } if @search_params
+    @search_params = @search_params.presence if @search_params
+
+    @items = @model.site(@cur_site).active
+    if @search_params
+      @items = @items.search(@search_params).
+        page(params[:page]).per(50)
+    else
+      @items = @items.tree_sort
+    end
   end
 end
