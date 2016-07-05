e089b778091fee037ca0e5cfcea7d28eda1364c6
tany <tanizawa@web-tips.co.jp>
2016-07-06 00:13:22 +0900

    [gws] enable no-category search (#1072)
    * [modify] gws/schedule: enable no-category search
    
    * [refactor] return nil

--- app/controllers/gws/schedule/facilities_controller.rb
+++ app/controllers/gws/schedule/facilities_controller.rb
@@ -18,15 +18,18 @@ class Gws::Schedule::FacilitiesController < ApplicationController
     def set_facility_category
       @facility_categories = facility_category_criteria.tree_sort
 
+      if params[:s].blank?
+        @facility_category ||= begin
+          c = @facility_categories.find { |c| c.id.present? }
+          c = facility_category_criteria.find(c.id) rescue nil
+          c
+        end
+        return
+      end
+
       facility_category_id = params.dig(:s, :facility_category_id)
       if facility_category_id.present?
         @facility_category = facility_category_criteria.find(facility_category_id) rescue nil
-      end
-
-      @facility_category ||= begin
-        c = @facility_categories.find { |c| c.id.present? }
-        c = facility_category_criteria.find(c.id) rescue nil
-        c
       end
     end
 
@@ -40,7 +43,7 @@ class Gws::Schedule::FacilitiesController < ApplicationController
     end
 
     def category_ids
-      return if @facility_category.blank?
+      return nil if @facility_category.blank?
       ids = facility_category_criteria.where(name: /^#{Regexp.escape(@facility_category.name)}\//).pluck(:id)
       ids << @facility_category.id
     end
@@ -50,6 +53,6 @@ class Gws::Schedule::FacilitiesController < ApplicationController
       @items = Gws::Facility::Item.site(@cur_site).
         readable(@cur_user, @cur_site).
         active
-      @items = @items.in(category_id: category_ids) if @facility_category.present?
+      @items = @items.in(category_id: category_ids)
     end
 end
