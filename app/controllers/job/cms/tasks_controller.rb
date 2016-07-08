3663ece8ff294817e4eca972093bca140812bd2f
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-07-08 13:25:48 +0900

    [add] cms_task crud to cms conf

--- app/controllers/job/cms/tasks_controller.rb
+++ app/controllers/job/cms/tasks_controller.rb
@@ -1,12 +1,17 @@
-class Job::Cms::LogsController < ApplicationController
+class Job::Cms::TasksController < ApplicationController
   include Cms::BaseFilter
   include Cms::CrudFilter
-  include Job::LogsFilter
+  include Job::TasksFilter
 
+  model Cms::Task
   navi_view "job/cms/main/navi"
 
   private
     def filter_permission
       raise "403" unless Cms::Tool.allowed?(:edit, @cur_user, site: @cur_site)
+    end
+
+    def item_criteria
+      @model.site(@cur_site)
     end
 end
--- config/routes/job/routes.rb
+++ config/routes/job/routes.rb
@@ -23,14 +23,17 @@ SS::Application.routes.draw do
   end
 
   cms "job" do
-    get "logs" => "logs#index"
+    get "/" => redirect { |p, req| "#{req.path}/logs" }, as: :main
 
-    get "logs/batch_destroy" => "logs#batch_destroy", as: :batch_destroy
-    post "logs/batch_destroy" => "logs#batch_destroy"
+    resources :logs, only: [:index, :show] do
+      get :batch_destroy, on: :collection
+      post :batch_destroy, on: :collection
+      get :download, on: :collection
+      post :download, on: :collection
+    end
 
-    get "logs/download" => "logs#download", as: :download
-    post "logs/download" => "logs#download"
-
-    get "logs/:id" => "logs#show"
+    resources :tasks, only: [:index, :show, :destroy], concerns: [:deletion] do
+      post :reset_state, on: :member
+    end
   end
 end
