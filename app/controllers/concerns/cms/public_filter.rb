11d2196281d431cfb587fb9220e057a9215f4d6e
itowtips <ito@web-tips.co.jp>
2016-07-13 15:41:51 +0900

    [fix] before_action :x_sendfile condition for #1070 (#1073)
    * [fix] restore before_action :x_sendfile condition for #1070
    
    * [fix] x_sendfile condition
    
    * [fix] space

--- app/controllers/concerns/cms/public_filter.rb
+++ app/controllers/concerns/cms/public_filter.rb
@@ -11,7 +11,7 @@ module Cms::PublicFilter
     before_action :deny_path
     before_action :parse_path
     before_action :compile_scss
-    before_action :x_sendfile, if: ->{ !filters.include?(:mobile) }
+    before_action :x_sendfile, unless: ->{ filters.include?(:mobile) || filters.include?(:kana) }
   end
 
   def index
