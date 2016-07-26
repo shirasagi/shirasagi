22ddf546b9372f3e34a4491c09eec22c9d6e4da7
itowtips <ito@web-tips.co.jp>
2016-07-26 09:10:40 +0900

    [fix] inquiry #1120 (#1121)
    * [fix] inquiry #1120
    
    * [add] inquiry_forms spec

---
+++ spec/features/inquiry/form_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+
+describe "inquiry_forms", dbscope: :example do
+  let(:site) { cms_site }
+  let(:node) { create :cms_node }
+  let(:item) { create :inquiry_node_form, filename: "#{node.filename}/name" }
+  let(:index_path)  { node_nodes_path site.id, node }
+  let(:new_path)    { "#{index_path}/new" }
+  let(:show_path)   { "#{index_path}/#{item.id}" }
+  let(:edit_path)   { "#{index_path}/#{item.id}/edit" }
+  let(:delete_path) { "#{index_path}/#{item.id}/delete" }
+
+  it "without login" do
+    visit index_path
+    expect(current_path).to eq sns_login_path
+  end
+
+  it "without auth" do
+    login_ss_user
+    visit index_path
+    expect(status_code).to eq 403
+  end
+
+  context "with auth" do
+    before { login_cms_user }
+
+    it "#index" do
+      visit index_path
+      expect(current_path).not_to eq sns_login_path
+    end
+
+    it "#new" do
+      visit new_path
+      within "form#item-form" do
+        fill_in "item[name]", with: "sample"
+        fill_in "item[basename]", with: "sample"
+        click_button "保存"
+      end
+      expect(status_code).to eq 200
+      expect(current_path).not_to eq new_path
+      expect(page).not_to have_css("form#item-form")
+    end
+
+    it "#show" do
+      visit show_path
+      expect(status_code).to eq 200
+      expect(current_path).not_to eq sns_login_path
+    end
+
+    it "#edit" do
+      visit edit_path
+      within "form#item-form" do
+        fill_in "item[name]", with: "modify"
+        click_button "保存"
+      end
+      expect(current_path).not_to eq sns_login_path
+      expect(page).not_to have_css("form#item-form")
+    end
+
+    it "#delete" do
+      visit delete_path
+      within "form" do
+        click_button "削除"
+      end
+      expect(current_path).to eq index_path
+    end
+  end
+end
