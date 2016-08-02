a676ee6f3479e00e4a5129b4c53caa79b8796a10
itowtips <ito@web-tips.co.jp>
2016-08-02 16:46:55 +0900

    [fix] spec failure

--- app/controllers/concerns/category/integration_filter.rb
+++ app/controllers/concerns/category/integration_filter.rb
@@ -1,36 +1,25 @@
 module Category::IntegrationFilter
   extend ActiveSupport::Concern
 
-  included do
-    before_action :set_item
-    before_action :set_model
+  def split
+    @item = @cur_node.becomes_with_route
+    @model = @item.class
+    #raise "403" unless @item.allowed?(:edit, @cur_user)
+
+    return if request.get?
+    @item.attributes = get_params
+    #@item.in_updated = params[:_updated] if @item.respond_to?(:in_updated)
+    render_create @item.split, location: { action: :index }, render: { file: :split }
   end
 
-  private
-    def set_item
-      @item = @cur_node.becomes_with_route
-    end
+  def integrate
+    @item = @cur_node.becomes_with_route
+    @model = @item.class
+    #raise "403" unless @item.allowed?(:edit, @cur_user)
 
-    def set_model
-      @model = @item.class
-    end
-
-  public
-    def split
-      #raise "403" unless @item.allowed?(:edit, @cur_user)
-
-      return if request.get?
-      @item.attributes = get_params
-      #@item.in_updated = params[:_updated] if @item.respond_to?(:in_updated)
-      render_create @item.split, location: { action: :index }, render: { file: :split }
-    end
-
-    def integrate
-      #raise "403" unless @item.allowed?(:edit, @cur_user)
-
-      return if request.get?
-      @item.attributes = get_params
-      #@item.in_updated = params[:_updated] if @item.respond_to?(:in_updated)
-      render_create @item.integrate, location: { action: :index }, render: { file: :integrate }
-    end
+    return if request.get?
+    @item.attributes = get_params
+    #@item.in_updated = params[:_updated] if @item.respond_to?(:in_updated)
+    render_create @item.integrate, location: { action: :index }, render: { file: :integrate }
+  end
 end
