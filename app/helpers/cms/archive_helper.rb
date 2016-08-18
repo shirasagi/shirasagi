87b58b88ed0e7430591054a33646336de2fe675d
masakiooba <masaki.ooba@gmail.com>
2016-08-18 14:16:55 +0900

    add archive navigation (calendar version)

---
+++ app/helpers/cms/archive_helper.rb
@@ -0,0 +1,16 @@
+module Cms::ArchiveHelper
+  def link_to_archive_daily(date, opts = {})
+    year  = date.year
+    month = date.month
+    day   = date.day
+    name = opts[:name].present? ? opts[:name] : "#{day}#{t_date('day')}"
+    path = opts[:path].present? ? opts[:path] : @cur_node.try(:url).to_s
+    enable = (opts[:enable] != nil) ? opts[:enable] : true
+
+    if enable && within_one_year?(date)
+      link_to name, sprintf("#{path}%04d%02d%02d", year, month, day)
+    else
+      name
+    end
+  end
+end
---
+++ spec/features/cms/agents/parts/calendar_nav_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "event_agents_parts_calendar", type: :feature, dbscope: :example do
+  let(:site)   { cms_site }
+  let(:layout) { create_cms_layout [part] }
+  let(:node) { create :cms_node_archive, cur_site: site, layout_id: layout.id, filename: "node" }
+  let(:part)   { create :cms_part_calendar_nav, filename: "node/part" }
+
+  context "public" do
+    let!(:item) { create :article_page, filename: "node/item" }
+
+    before do
+      Capybara.app_host = "http://#{site.domain}"
+    end
+
+    it "#index" do
+      visit node.url
+      expect(status_code).to eq 200
+      expect(page).to have_css(".event-calendar")
+    end
+  end
+end
