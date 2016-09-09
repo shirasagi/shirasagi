81115b8e5f0c23e8452a9a470927a4e478e07fd4
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-09-09 19:34:51 +0900

    [fix][#1167] #{summary} loop html variable (#1168)

--- app/models/concerns/cms/addon/meta.rb
+++ app/models/concerns/cms/addon/meta.rb
@@ -21,7 +21,7 @@ module Cms::Addon
     def summary
       return summary_html if summary_html.present?
       return nil unless respond_to?(:html)
-      ApplicationController.helpers.sanitize(html, tags: []).squish.truncate(120)
+      ApplicationController.helpers.sanitize(html.presence || '', tags: []).squish.truncate(120)
     end
 
     def meta_present?
