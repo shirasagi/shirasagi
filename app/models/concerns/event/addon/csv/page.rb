e1a10b1b6f19d28b6b5f904511a146ad6a989f19
itowtips <ito@web-tips.co.jp>
2016-10-24 13:57:26 +0900

    [fix] Rubocop: C: Assignment Branch Condition size for to_csv is too high.

--- app/models/concerns/event/addon/csv/page.rb
+++ app/models/concerns/event/addon/csv/page.rb
@@ -26,59 +26,63 @@ module Event::Addon::Csv
         CSV.generate do |data|
           data << csv_headers.map { |k| t k }
           criteria.each do |item|
-            line = []
-
-            # basic
-            line << item.basename
-            line << item.name
-            line << item.index_name
-            line << item.layout.try(:name)
-            line << item.order
-
-            # meta
-            line << item.keywords
-            line << item.description
-            line << item.summary_html
-
-            # body
-            line << item.html
-
-            # event body
-            line << item.schedule
-            line << item.venue
-            line << item.content
-            line << item.cost
-            line << item.related_url
-            line << item.contact
-
-            # category
-            line << item.categories.pluck(:name).join("\n")
-
-            # event
-            line << item.event_name
-            line << item.event_dates
-
-            # related pages
-            line << item.related_pages.pluck(:filename).join("\n")
-
-            # crumb
-            line << item.parent_crumb_urls
-
-            # released
-            line << item.released.try(:strftime, "%Y/%m/%d %H:%M")
-            line << item.release_date.try(:strftime, "%Y/%m/%d %H:%M")
-            line << item.close_date.try(:strftime, "%Y/%m/%d %H:%M")
-
-            # groups
-            line << item.groups.pluck(:name).join("\n")
-            line << item.permission_level
-
-            # state
-            line << item.label(:state)
-
-            data << line
+            data << csv_line(item)
           end
         end
+      end
+
+      def csv_line(item)
+        line = []
+
+        # basic
+        line << item.basename
+        line << item.name
+        line << item.index_name
+        line << item.layout.try(:name)
+        line << item.order
+
+        # meta
+        line << item.keywords
+        line << item.description
+        line << item.summary_html
+
+        # body
+        line << item.html
+
+        # event body
+        line << item.schedule
+        line << item.venue
+        line << item.content
+        line << item.cost
+        line << item.related_url
+        line << item.contact
+
+        # category
+        line << item.categories.pluck(:name).join("\n")
+
+        # event
+        line << item.event_name
+        line << item.event_dates
+
+        # related pages
+        line << item.related_pages.pluck(:filename).join("\n")
+
+        # crumb
+        line << item.parent_crumb_urls
+
+        # released
+        line << item.released.try(:strftime, "%Y/%m/%d %H:%M")
+        line << item.release_date.try(:strftime, "%Y/%m/%d %H:%M")
+        line << item.close_date.try(:strftime, "%Y/%m/%d %H:%M")
+
+        # groups
+        line << item.groups.pluck(:name).join("\n")
+        line << item.permission_level
+
+        # state
+        line << item.label(:state)
+
+        line
       end
     end
   end
