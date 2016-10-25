7b28d7f0cf69ce4aa00907fe93d1b5cdce4c37f5
itowtips <ito@web-tips.co.jp>
2016-10-25 12:19:56 +0900

    [modify] recommend history part (#1233)

--- app/controllers/recommend/history/receiver_controller.rb
+++ app/controllers/recommend/history/receiver_controller.rb
@@ -2,23 +2,26 @@ class Recommend::History::ReceiverController < ApplicationController
 
   before_action :set_site
 
-  private
-    def set_site
-      @cur_site = Cms::Site.find id: params[:site]
-    end
-
   public
     def index
       token = cookies["_ss_recommend"]
       path = params[:path]
-      url = params[:url]
+      access_url = params[:access_url]
+      target_id = params[:target_id]
+      target_class = params[:target_class]
+      remote_addr = request.env["HTTP_X_REAL_IP"] || request.remote_ip
+      user_agent = request.user_agent
 
       if token && !Recommend::History::Log.site(@cur_site).where(token: token).first
         token = nil
       end
 
-      url = params[:url]
-      log = Recommend::History::Log.new(path: path, url: url, token: token, site: @cur_site)
+      log = Recommend::History::Log.new(
+        token: token, site: @cur_site,
+        path: path, access_url: access_url,
+        target_id: target_id, target_class: target_class,
+        remote_addr: remote_addr, user_agent: user_agent,
+      )
       log.save
 
       cookies.permanent["_ss_recommend"] = log.token
@@ -26,5 +29,10 @@ class Recommend::History::ReceiverController < ApplicationController
       respond_to do |format|
         format.json { render json: log.attributes.to_json }
       end
+    end
+
+  private
+    def set_site
+      @cur_site = Cms::Site.find id: params[:site]
     end
 end
