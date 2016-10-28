d4d774d8d5d763267ef4ab547d430b939bf8b968
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-10-28 11:41:07 +0900

    [fix][#1180] XSS valunability

--- app/controllers/gws/public_links_controller.rb
+++ app/controllers/gws/public_links_controller.rb
@@ -18,7 +18,8 @@ class Gws::PublicLinksController < ApplicationController
     end
 
     def show
-      raise "403" unless @model.site(@cur_site).and_public.readable(@cur_user, @cur_site).find(@item.id)
+      @links = @model.site(@cur_site).and_public.readable(@cur_user, @cur_site)
+      raise "403" unless @links.find(@item.id)
       render
     end
 end
