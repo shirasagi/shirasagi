feaab2faccbe674a2e14c3cbe10713db66ec35bf
hotmix <hotmix.blog@gmail.com>
2016-11-06 11:28:12 +0900

    [modify] mobile_size file.thumb.size

--- app/controllers/cms/mobile_size_check_controller.rb
+++ app/controllers/cms/mobile_size_check_controller.rb
@@ -1,6 +1,3 @@
-require "timeout"
-require "open-uri"
-
 class Cms::MobileSizeCheckController < ApplicationController
   protect_from_forgery except: :check
   skip_before_action :verify_authenticity_token unless SS.config.env.csrf_protect
@@ -10,6 +7,7 @@ class Cms::MobileSizeCheckController < ApplicationController
     result = {}
     imgs_ids = params[:img_ids]
     mobile_size = params[:mobile_size]
+    is_thumb = params[:is_thumb]
 
     raise "400" if imgs_ids.blank?
     imgs_ids = imgs_ids.values if imgs_ids.is_a?(Hash)
@@ -17,15 +15,37 @@ class Cms::MobileSizeCheckController < ApplicationController
 
     img_files = SS::File.where(:id.in => ids)
     size = 0
-    result[:file_name] = []
+    result[:file_id] = []
     result[:errors] = []
     img_files.each do |file|
-      next if result[:file_name].include?(file.name)
-      size += file.size
-      if size > mobile_size
-        result[:errors] << I18n.t("errors.messages.too_bigsize")
+      next if result[:file_id].include?(file.id)
+
+      if is_thumb[file.id.to_s]
+        if file.thumb.size > 0
+          result[:errors] << I18n.t(
+            "errors.messages.too_bigfile",
+            filename: file.name,
+            filesize: view_context.number_to_human_size(file.thumb.size)
+          )
+        end
+
+        size += file.thumb.size
+      else
+        if file.size > 0
+          result[:errors] << I18n.t(
+            "errors.messages.too_bigfile",
+            filename: file.name,
+            filesize: view_context.number_to_human_size(file.size)
+          )
+        end
+
+        size += file.size
+        result[:file_id] << file.id
       end
-      result[:file_name] << file.name
+
+    end
+    if size > mobile_size
+      result[:errors] << I18n.t("errors.messages.too_bigsize")
     end
     result[:size] = size
 
