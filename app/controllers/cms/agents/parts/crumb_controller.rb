66c54e4a03504fa762117fcf727028a1bccc9681
itowtips <ito@web-tips.co.jp>
2016-11-09 17:06:18 +0900

    [modify] multiple crumbs

--- app/controllers/cms/agents/parts/crumb_controller.rb
+++ app/controllers/cms/agents/parts/crumb_controller.rb
@@ -3,36 +3,42 @@ class Cms::Agents::Parts::CrumbController < ApplicationController
 
   def index
     @cur_node = @cur_part.parent
-    root_node = @cur_node || @cur_site
+    @root_node = @cur_node || @cur_site
 
     @items = []
-    return if @cur_path !~ /^#{root_node.url}/
-
-    @items << [@cur_part.home_label, root_node.url]
+    return if @cur_path !~ /^#{@root_node.url}/
 
     path = @cur_path.sub(/^#{@cur_site.url}/, "")
-
-    if @cur_item.try(:parent_crumb_urls).try(:first).present?
-      find_node @cur_item.parent_crumb_urls.first
-    else
-      find_node path
-    end
-
-    @items << [@preview_page.name, @preview_page.url] if @preview_page
-    page = Cms::Page.site(@cur_site).filename(path).first
-    return unless page
-
-    last_item = @items.last
-    return if last_item[0] == page.name && page.url.end_with?("/index.html")
-
-    @items << [page.name, page.url]
+    parent_crumb_urls = @cur_item.parent_crumb_urls.select(&:present?) rescue nil
+    set_items(path, parent_crumb_urls)
   end
 
   private
-    def find_node(path)
+    def set_items(path, urls = nil)
+      page = Cms::Page.site(@cur_site).filename(path).first
+      urls = [path] if urls.blank?
+
+      urls.each do |url|
+        item = []
+        item << [@cur_part.home_label, @root_node.url]
+        find_node(item, url)
+
+        item << [@preview_page.name, @preview_page.url] if @preview_page
+        if page
+          last_item = item.last
+          unless last_item[0] == page.name && page.url.end_with?("/index.html")
+            item << [page.name, page.url]
+          end
+        end
+
+        @items << item
+      end
+    end
+
+    def find_node(item, path)
       Cms::Node.site(@cur_site).in_path(path).order(depth: -1).reverse_each do |node|
         next if @cur_node && @cur_node.depth >= node.depth
-        @items << [node.name, node.url]
+        item << [node.name, node.url]
       end
     end
 end
