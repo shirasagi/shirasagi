29a19efb36d5701f82f7d0c880cca63409c2f0f2
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-11-09 17:39:55 +0900

    [add] tag addon to cms::page

--- app/models/cms/page.rb
+++ app/models/cms/page.rb
@@ -14,6 +14,7 @@ class Cms::Page
   include Map::Addon::Page
   include Cms::Addon::RelatedPage
   include Contact::Addon::Page
+  include Cms::Addon::Tag
   include Cms::Addon::Release
   include Cms::Addon::ReleasePlan
   include Cms::Addon::GroupPermission
