801163cbaffdbda26aa5e8285db349d52be063d4
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-11-11 17:10:10 +0900

    [add] filter show / new / edit

--- app/models/rss/weather_xml/filter.rb
+++ app/models/rss/weather_xml/filter.rb
@@ -4,13 +4,27 @@ class Rss::WeatherXml::Filter
   embedded_in :node, class_name: "Rss::Node::WeatherXml", inverse_of: :filter
   field :name, type: String
   field :state, type: String
-  belongs_to :trigger, class_name: "Rss::WeatherXml::Trigger::Base"
-  belongs_to :action, class_name: "Rss::WeatherXml::Action::Base"
-  permit_params :name, :state, :trigger_id, :action_id
+  embeds_ids :triggers, class_name: "Rss::WeatherXml::Trigger::Base"
+  embeds_ids :actions, class_name: "Rss::WeatherXml::Action::Base"
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :state, presence: true, inclusion: { in: %w(enabled disabled), allow_blank: true }
+  validates :trigger_ids, presence: true
+  validates :action_ids, presence: true
+  permit_params :name, :state
+  permit_params trigger_ids: [], action_ids: []
 
   class << self
     def search(params = {})
-      self.all
+      criteria = self.all
+      return criteria if params.blank?
+
+      if params[:name].present?
+        criteria = criteria.search_text params[:name]
+      end
+      if params[:keyword].present?
+        criteria = criteria.keyword_in params[:keyword], :name
+      end
+      criteria
     end
 
     delegate :allowed?, to: 'Rss::Node::WeatherXml'
@@ -22,5 +36,13 @@ class Rss::WeatherXml::Filter
     %w(enabled disabled).map do |v|
       [ I18n.t("views.options.state.#{v}"), v ]
     end
+  end
+
+  def trigger_options
+    Rss::WeatherXml::Trigger::Base.site(node.site).pluck(:name, :id)
+  end
+
+  def action_options
+    Rss::WeatherXml::Action::Base.site(node.site).pluck(:name, :id)
   end
 end
