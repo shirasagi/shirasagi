51bc12357616e7077ab7af81daa7202d3c0d09cc
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-11-11 18:55:20 +0900

    [modify] implement tsunami alert trigger

--- app/models/rss/weather_xml/trigger/tsunami_alert.rb
+++ app/models/rss/weather_xml/trigger/tsunami_alert.rb
@@ -1,2 +1,4 @@
 class Rss::WeatherXml::Trigger::TsunamiAlert < Rss::WeatherXml::Trigger::Base
+  embeds_ids :target_regions, class_name: "Rss::WeatherXml::TsunamiRegion"
+  permit_params target_region_ids: []
 end
