7e14c9b576159f2ab74edab235443132555ae75c
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-11-14 16:24:29 +0900

    [modify] add AdditionalInfo addon to article page and expand it in the loop html

--- app/models/concerns/cms/addon/additional_info.rb
+++ app/models/concerns/cms/addon/additional_info.rb
@@ -7,6 +7,19 @@ module Cms::Addon
       field :additional_info, type: Cms::Extensions::AdditionalInfo
 
       permit_params additional_info: [ :field, :value ]
+
+      template_variable_handler /^add_info\.\S+$/, :template_variable_handler_add_info if respond_to?(:template_variable_handler)
+    end
+
+    def template_variable_handler_add_info(name, issuer)
+      name =~ /^add_info\.(\S+)$/
+      value_key = $1
+      return if value_key.blank?
+
+      h = additional_info.find { |h| h[:field] == value_key }
+      return if h.blank?
+
+      h[:value]
     end
   end
 end
