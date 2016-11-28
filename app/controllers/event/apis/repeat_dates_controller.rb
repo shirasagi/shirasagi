f3dc1b95583e34b022fddeea563b1c0daa01148e
itowtips <ito@web-tips.co.jp>
2016-11-28 17:30:16 +0900

    [add] event repeat dates api

---
+++ app/controllers/event/apis/repeat_dates_controller.rb
@@ -0,0 +1,61 @@
+class Event::Apis::RepeatDatesController < ApplicationController
+  include Cms::ApiFilter
+
+  public
+    def index
+      #
+    end
+
+    def create
+      @repeat_start = params[:repeat_start]
+      @repeat_end = params[:repeat_end]
+      @days = params[:days]
+      @wdays = params[:wdays]
+      @errors = []
+
+      begin
+        @repeat_start = Date.parse(@repeat_start)
+      rescue
+        @errors << I18n.t("event.apis.repeat_dates.start_blank")
+      end
+
+      begin
+        @repeat_end = Date.parse(@repeat_end)
+      rescue
+        @errors << I18n.t("event.apis.repeat_dates.end_blank")
+      end
+
+      if @errors.present?
+        render json: @errors, status: 422
+        return
+      end
+
+      dates = []
+      range = []
+      repeat_dates.each do |d|
+        if range.present? && range.last.tomorrow != d
+          dates << range
+          range = []
+        end
+        range << d
+      end
+      dates << range if range.present?
+      @dates = dates.map { |range| [range.first, range.last] }
+
+      @errors << I18n.t("event.apis.repeat_dates.not_found_dates") if @dates.blank?
+      if @errors.present?
+        render json: @errors, status: 422
+        return
+      end
+    end
+
+  private
+    def repeat_dates
+      dates = []
+      @repeat_start.step(@repeat_end, 1) do |d|
+        dates << d if @days.include?(d.day.to_s)
+        dates << d if @wdays.include?(d.wday.to_s)
+      end
+      dates
+    end
+end
