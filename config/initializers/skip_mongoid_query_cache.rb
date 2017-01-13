00fa11ceab5b00f70cde8e7a33664d2e8928c01c
tany <tanizawa@web-tips.co.jp>
2017-01-13 21:08:25 +0900

    [modify] disable SkipMongoidQueryCache

--- config/initializers/skip_mongoid_query_cache.rb
+++ config/initializers/skip_mongoid_query_cache.rb
@@ -1,14 +1,14 @@
-module SkipMongoidQueryCache
-  private
-  def system_collection?
-    return true if cache_skip?
-    super
-  end
+# module SkipMongoidQueryCache
+#   private
+#   def system_collection?
+#     return true if cache_skip?
+#     super
+#   end
+#
+#   def cache_skip?
+#     return true if selector.reject { |k, v| k == '_id' }.present?
+#     return true if options.reject { |k, v| k == 'limit' }.present?
+#   end
+# end
 
-  def cache_skip?
-    return true if selector.reject { |k, v| k == '_id' }.present?
-    return true if options.reject { |k, v| k == 'limit' }.present?
-  end
-end
-
-Mongo::Collection::View.__send__(:include, SkipMongoidQueryCache)
+# Mongo::Collection::View.__send__(:include, SkipMongoidQueryCache)
