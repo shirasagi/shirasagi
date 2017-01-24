05c4758e8e0f03743b3cb505b7d3107bebf94a89
itowtips <ito@web-tips.co.jp>
2017-01-24 14:59:11 +0900

    [add] rss generation in some tasks (#1350)

--- app/controllers/category/agents/tasks/node/pages_controller.rb
+++ app/controllers/category/agents/tasks/node/pages_controller.rb
@@ -1,7 +1,16 @@
 class Category::Agents::Tasks::Node::PagesController < ApplicationController
   include Cms::PublicFilter::Node
+  include Cms::GeneratorFilter::Rss
 
   def generate
-    generate_node_with_pagination @node
+    written = generate_node_with_pagination @node
+
+    # initialize context before generating rss
+    init_context
+    if generate_node_rss @node
+      @task.log "#{@node.url}rss.xml" if @task
+    end
+
+    written
   end
 end
