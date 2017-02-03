d8992a9df1173d1e771e7f8041de8bc6f7c0f001
s.kudo <s.kudo@fmbee.com>
2017-02-03 15:44:33 +0900

    could delete my post from facebookgit add -A

--- app/models/concerns/cms/addon/sns_poster.rb
+++ app/models/concerns/cms/addon/sns_poster.rb
@@ -3,6 +3,8 @@ module Cms::Addon
     extend ActiveSupport::Concern
     extend SS::Addon
 
+   require 'twitter'
+
     included do
       field :fbauto,     type: String, default: 'expired'
       field :twauto,     type: String, default: 'expired'
@@ -51,11 +53,10 @@ module Cms::Addon
       def post_sns
         site_name = "自治体サンプル"
         tweet_url = "http://localhost:3000#{url}"
+        snskeys = SS.config.cms.sns_poster
 
         # tweet
         if twauto === "active"
-          require 'twitter'
-          snskeys = SS.config.cms.sns_poster
           client = Twitter::REST::Client.new do |config|
             config.consumer_key        = snskeys["consumer_key"]
             config.consumer_secret     = snskeys["consumer_secret"]
@@ -65,8 +66,7 @@ module Cms::Addon
           tweet = "#{name}｜#{tweet_url}"
           twitter_param = client.update(tweet)
           twitter_id = twitter_param.id
-          self.update(twid: twitter_id)
-
+          self.set(twid: twitter_id)
         end
 
         # facebook
@@ -82,14 +82,35 @@ module Cms::Addon
             # "link" => "#{tweet_url}",
             # "description" => "#{name}"
           })
-          facebook_param = facebook_param['id'].inspect
-          self.update(fbid: facebook_param)
+          facebook_param = facebook_param['id'].to_s
+          self.set(fbid: facebook_param)
+          logger.error "fb_putwall=>"
           logger.error fbid
         end
       end
 
       def delete_sns
+        snskeys = SS.config.cms.sns_poster
         if deleteauto === "active"
+          if twid.present?
+            client = Twitter::REST::Client.new do |config|
+              config.consumer_key        = snskeys["consumer_key"]
+              config.consumer_secret     = snskeys["consumer_secret"]
+              config.access_token        = snskeys["access_token"]
+              config.access_token_secret = snskeys["access_token_secret"]
+            end
+            client.destroy_status(twid)
+            logger.error "tw_delete=>"
+            logger.error twid
+          end
+          if fbid.present?
+            require 'koala'
+            access_token = snskeys["access_token_f"]
+            graph = Koala::Facebook::API.new(access_token)
+            graph.delete_object(fbid)
+            logger.error "fb_delete=>"
+            logger.error fbid
+          end
         end
       end
   end
