e3e389c7a7390b97938e1b8eb75dbaf420cf7827
NAKANO Hideo <pinarello.marvel@gmail.com>
2017-03-31 20:40:36 +0900

    [add] decode to sys mail log

--- app/controllers/sys/mail_logs_controller.rb
+++ app/controllers/sys/mail_logs_controller.rb
@@ -1,3 +1,5 @@
+require 'nkf'
+
 class Sys::MailLogsController < ApplicationController
   include Sys::BaseFilter
   include Sys::CrudFilter
@@ -26,6 +28,25 @@ class Sys::MailLogsController < ApplicationController
   def destroy
     raise "403" unless SS::User.allowed?(:delete, @cur_user)
     render_destroy @item.destroy
+  end
+
+  def decode
+    set_item
+
+    if @item.subject =~ /ISO-2022-JP/i
+      @item.subject = NKF.nkf("-w", @item.subject)
+    end
+
+    if @item.mail =~ /ISO-2022-JP/i
+      @item.mail = NKF.nkf("-w", @item.mail)
+    end
+  end
+
+  def commit_decode
+    raise "403" unless SS::User.allowed?(:edit, @cur_user)
+    set_item
+    @item.set(params.require(:item).permit(:subject, :mail))
+    render_update true
   end
 
   private
