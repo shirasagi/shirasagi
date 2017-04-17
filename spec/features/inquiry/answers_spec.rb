a62b3ad3c1603f92e61dced6b893aabb5446d4cd
NAKANO Hideo <pinarello.marvel@gmail.com>
2017-04-17 20:26:47 +0900

    [fix] 500 error on downloading inquiry answers (#1399)
    * [fix] 500 error on downloading inquiry answers
    
    * [fixup] 500 error on downloading inquiry answers
    
    * [modify] mark spec as fragile

--- spec/features/inquiry/answers_spec.rb
+++ spec/features/inquiry/answers_spec.rb
@@ -16,6 +16,7 @@ describe "inquiry_answers", dbscope: :example do
   let(:select) { select_column.select_options.sample }
   let(:check) { Hash[check_column.select_options.map.with_index { |val, i| [i.to_s, val] }.sample(2)] }
   let(:name_column) { node.columns[0] }
+  let(:company_column) { node.columns[1] }
   let(:email_column) { node.columns[2] }
   let(:radio_column) { node.columns[3] }
   let(:select_column) { node.columns[4] }
@@ -46,58 +47,182 @@ describe "inquiry_answers", dbscope: :example do
   context "basic crud" do
     before { login_cms_user }
 
-    it do
-      visit index_path
-      expect(page).to have_css(".list-item a", text: answer.data_summary)
-      click_on answer.data_summary
+    context "usual case" do
+      it do
+        visit index_path
+        expect(page).to have_css(".list-item a", text: answer.data_summary)
+        click_on answer.data_summary
 
-      expect(page).to have_css("#addon-basic dt", text: name_column.name)
-      expect(page).to have_css("#addon-basic dd", text: name)
-      expect(page).to have_css("#addon-basic dt", text: email_column.name)
-      expect(page).to have_css("#addon-basic dd", text: email)
-      expect(page).to have_css("#addon-basic dt", text: radio_column.name)
-      expect(page).to have_css("#addon-basic dd", text: radio)
-      expect(page).to have_css("#addon-basic dt", text: select_column.name)
-      expect(page).to have_css("#addon-basic dd", text: select)
+        expect(page).to have_css("#addon-basic dt", text: name_column.name)
+        expect(page).to have_css("#addon-basic dd", text: name)
+        expect(page).to have_css("#addon-basic dt", text: email_column.name)
+        expect(page).to have_css("#addon-basic dd", text: email)
+        expect(page).to have_css("#addon-basic dt", text: radio_column.name)
+        expect(page).to have_css("#addon-basic dd", text: radio)
+        expect(page).to have_css("#addon-basic dt", text: select_column.name)
+        expect(page).to have_css("#addon-basic dd", text: select)
 
-      expect(page).to have_css("#addon-basic dd", text: remote_addr)
-      expect(page).to have_css("#addon-basic dd", text: user_agent)
+        expect(page).to have_css("#addon-basic dd", text: remote_addr)
+        expect(page).to have_css("#addon-basic dd", text: user_agent)
 
-      click_on "削除する"
-      click_on "削除"
+        click_on "削除する"
+        click_on "削除"
 
-      expect(page).to have_no_css(".list-item a", text: answer.data_summary)
-      expect(Inquiry::Answer.count).to eq 0
+        expect(page).to have_no_css(".list-item a", text: answer.data_summary)
+        expect(Inquiry::Answer.count).to eq 0
+      end
+    end
+
+    context "when a column was destroyed after answers ware committed" do
+      before { email_column.destroy }
+
+      it do
+        visit index_path
+        expect(page).to have_css(".list-item a", text: answer.data_summary)
+        click_on answer.data_summary
+
+        expect(page).to have_css("#addon-basic dt", text: name_column.name)
+        expect(page).to have_css("#addon-basic dd", text: name)
+        expect(page).to have_css("#addon-basic dt", text: radio_column.name)
+        expect(page).to have_css("#addon-basic dd", text: radio)
+        expect(page).to have_css("#addon-basic dt", text: select_column.name)
+        expect(page).to have_css("#addon-basic dd", text: select)
+
+        expect(page).to have_css("#addon-basic dd", text: remote_addr)
+        expect(page).to have_css("#addon-basic dd", text: user_agent)
+
+        click_on "削除する"
+        click_on "削除"
+
+        expect(page).to have_no_css(".list-item a", text: answer.data_summary)
+        expect(Inquiry::Answer.count).to eq 0
+      end
     end
   end
 
   context "search" do
     before { login_cms_user }
 
-    it do
-      visit index_path
-      expect(page).to have_css(".list-item a", text: answer.data_summary)
-      click_on "検索"
-      click_on answer.data_summary
+    context "usual case" do
+      it do
+        visit index_path
+        expect(page).to have_css(".list-item a", text: answer.data_summary)
+        click_on "検索"
+        click_on answer.data_summary
 
-      expect(page).to have_css("#addon-basic dt", text: name_column.name)
-      expect(page).to have_css("#addon-basic dd", text: name)
-      expect(page).to have_css("#addon-basic dt", text: email_column.name)
-      expect(page).to have_css("#addon-basic dd", text: email)
-      expect(page).to have_css("#addon-basic dt", text: radio_column.name)
-      expect(page).to have_css("#addon-basic dd", text: radio)
-      expect(page).to have_css("#addon-basic dt", text: select_column.name)
-      expect(page).to have_css("#addon-basic dd", text: select)
+        expect(page).to have_css("#addon-basic dt", text: name_column.name)
+        expect(page).to have_css("#addon-basic dd", text: name)
+        expect(page).to have_css("#addon-basic dt", text: email_column.name)
+        expect(page).to have_css("#addon-basic dd", text: email)
+        expect(page).to have_css("#addon-basic dt", text: radio_column.name)
+        expect(page).to have_css("#addon-basic dd", text: radio)
+        expect(page).to have_css("#addon-basic dt", text: select_column.name)
+        expect(page).to have_css("#addon-basic dd", text: select)
 
-      expect(page).to have_css("#addon-basic dd", text: remote_addr)
-      expect(page).to have_css("#addon-basic dd", text: user_agent)
+        expect(page).to have_css("#addon-basic dd", text: remote_addr)
+        expect(page).to have_css("#addon-basic dd", text: user_agent)
 
-      click_on "削除する"
-      click_on "削除"
+        click_on "削除する"
+        click_on "削除"
 
-      expect(page).to have_no_css(".list-item a", text: answer.data_summary)
-      expect(Inquiry::Answer.count).to eq 0
+        expect(page).to have_no_css(".list-item a", text: answer.data_summary)
+        expect(Inquiry::Answer.count).to eq 0
+      end
+    end
+
+    context "when a column was destroyed after answers ware committed" do
+      before { email_column.destroy }
+
+      it do
+        visit index_path
+        expect(page).to have_css(".list-item a", text: answer.data_summary)
+        click_on "検索"
+        click_on answer.data_summary
+
+        expect(page).to have_css("#addon-basic dt", text: name_column.name)
+        expect(page).to have_css("#addon-basic dd", text: name)
+        expect(page).to have_css("#addon-basic dt", text: radio_column.name)
+        expect(page).to have_css("#addon-basic dd", text: radio)
+        expect(page).to have_css("#addon-basic dt", text: select_column.name)
+        expect(page).to have_css("#addon-basic dd", text: select)
+
+        expect(page).to have_css("#addon-basic dd", text: remote_addr)
+        expect(page).to have_css("#addon-basic dd", text: user_agent)
+
+        click_on "削除する"
+        click_on "削除"
+
+        expect(page).to have_no_css(".list-item a", text: answer.data_summary)
+        expect(Inquiry::Answer.count).to eq 0
+      end
     end
   end
 
+  context "download" do
+    before { login_cms_user }
+
+    context "usual case" do
+      it do
+        visit index_path
+        expect(page).to have_css(".list-item a", text: answer.data_summary)
+        click_on I18n.t('views.links.download')
+        expect(status_code).to eq 200
+
+        csv_lines = CSV.parse(page.html.encode("UTF-8"))
+        expect(csv_lines.length).to eq 2
+        expect(csv_lines[0][0]).to eq 'id'
+        expect(csv_lines[0][1]).to eq name_column.name
+        expect(csv_lines[0][2]).to eq company_column.name
+        expect(csv_lines[0][3]).to eq email_column.name
+        expect(csv_lines[0][4]).to eq radio_column.name
+        expect(csv_lines[0][5]).to eq select_column.name
+        expect(csv_lines[0][6]).to eq check_column.name
+        expect(csv_lines[0][7]).to eq 'created'
+        expect(csv_lines[0][8]).to eq 'source_url'
+        expect(csv_lines[0][9]).to eq 'source_name'
+        expect(csv_lines[1][0]).to eq answer.id.to_s
+        expect(csv_lines[1][1]).to eq name
+        expect(csv_lines[1][2]).to be_nil
+        expect(csv_lines[1][3]).to eq email
+        expect(csv_lines[1][4]).to eq radio
+        expect(csv_lines[1][5]).to eq select
+        expect(csv_lines[1][6]).to eq check.values.join("\n")
+        expect(csv_lines[1][7]).to eq answer.created.strftime('%Y/%m/%d %H:%M')
+        expect(csv_lines[1][8]).to be_nil
+        expect(csv_lines[1][9]).to be_nil
+      end
+    end
+
+    context "when a column was destroyed after answers ware committed" do
+      before { email_column.destroy }
+
+      it do
+        visit index_path
+        expect(page).to have_css(".list-item a", text: answer.data_summary)
+        click_on I18n.t('views.links.download')
+        expect(status_code).to eq 200
+
+        csv_lines = CSV.parse(page.html.encode("UTF-8"))
+        expect(csv_lines.length).to eq 2
+        expect(csv_lines[0][0]).to eq 'id'
+        expect(csv_lines[0][1]).to eq name_column.name
+        expect(csv_lines[0][2]).to eq company_column.name
+        expect(csv_lines[0][3]).to eq radio_column.name
+        expect(csv_lines[0][4]).to eq select_column.name
+        expect(csv_lines[0][5]).to eq check_column.name
+        expect(csv_lines[0][6]).to eq 'created'
+        expect(csv_lines[0][7]).to eq 'source_url'
+        expect(csv_lines[0][8]).to eq 'source_name'
+        expect(csv_lines[1][0]).to eq answer.id.to_s
+        expect(csv_lines[1][1]).to eq name
+        expect(csv_lines[1][2]).to be_nil
+        expect(csv_lines[1][3]).to eq radio
+        expect(csv_lines[1][4]).to eq select
+        expect(csv_lines[1][5]).to eq check.values.join("\n")
+        expect(csv_lines[1][6]).to eq answer.created.strftime('%Y/%m/%d %H:%M')
+        expect(csv_lines[1][7]).to be_nil
+        expect(csv_lines[1][8]).to be_nil
+      end
+    end
+  end
 end
