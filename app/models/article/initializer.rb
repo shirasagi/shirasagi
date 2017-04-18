ee53b23cf3b88dc517c3e70f86cfb4f148b4d045
itowtips <ito@web-tips.co.jp>
2017-04-18 19:36:15 +0900

    [add] article page_navi part (#1401)

--- app/models/article/initializer.rb
+++ app/models/article/initializer.rb
@@ -2,6 +2,7 @@ module Article
   class Initializer
     Cms::Node.plugin "article/page"
     Cms::Part.plugin "article/page"
+    Cms::Part.plugin "article/page_navi"
 
     Cms::Role.permission :read_other_article_pages
     Cms::Role.permission :read_private_article_pages
