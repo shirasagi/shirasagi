e12f77742ab33f806c605c4a744a3b45ee9030ca
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2017-04-19 17:39:20 +0900

    [Fix]Features/sns_post

--- app/models/concerns/ss/addon/twitter_setting.rb
+++ app/models/concerns/ss/addon/twitter_setting.rb
@@ -6,8 +6,14 @@ module SS::Addon::TwitterSetting
     field :twitter_card, type: String
     field :twitter_username, type: String
     field :twitter_default_image_url, type: String
+    field :twitter_consumer_key, type: String
+    field :twitter_consumer_secret, type: String
+    field :twitter_access_token, type: String
+    field :twitter_access_token_secret, type: String
     validates :twitter_card, inclusion: { in: %w(none summary summary_large_image), allow_blank: true }
     permit_params :twitter_card, :twitter_username, :twitter_default_image_url
+    permit_params :twitter_consumer_key, :twitter_consumer_secret
+    permit_params :twitter_access_token, :twitter_access_token_secret
   end
 
   def twitter_card_options
