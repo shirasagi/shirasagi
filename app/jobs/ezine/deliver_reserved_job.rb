ef6a181355ad5a3e7acc4fe697329e8e610564cf
NAKANO Hideo <pinarello.marvel@gmail.com>
2017-04-21 15:41:01 +0900

    [fixup] convert ezine deliver job to active job (#1417)

--- app/jobs/ezine/deliver_reserved_job.rb
+++ app/jobs/ezine/deliver_reserved_job.rb
@@ -6,7 +6,9 @@ class Ezine::DeliverReservedJob < Cms::ApplicationJob
   # 1ページをメールとして送信する。
   # 配信予約日時が設定されている場合、条件を満たせば送信される。
   #
-  def perform(now = Time.zone.now)
+  def perform(opts = {})
+    now = opts[:now]
+    now ||= Time.zone.now
     Ezine::Page.site(site).where(:completed => false, :deliver_date.ne => nil, :deliver_date.lte => now).each do |page|
       Ezine::Task.ready(site_id: site.id, page_id: page.id, name: 'ezine:deliver') do |task|
         deliver_one_page(page, task)
