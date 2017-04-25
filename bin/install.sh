9cc97b2d5ee42c421f964e1473d62f7513d25a35
snkmr <nakamura@web-tips.co.jp>
2017-04-25 15:15:15 +0900

    [fix] install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -2,12 +2,12 @@ SS_HOSTNAME=${1:-"example.jp"}
 SS_USER=${2:-"$USER"}
 SS_DIR=/var/www/shirasagi
 
-cat <<EOS | sudo tee -a /etc/yum.repos.d/CentOS-Base.repo
+cat <<EOS | sudo tee -a /etc/yum.repos.d/mongodb-org-3.4.repo
 [mongodb-org-3.4]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/3.4/x86_64/
 gpgcheck=1
-enabled=1
+enabled=0
 gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc
 EOS
 
