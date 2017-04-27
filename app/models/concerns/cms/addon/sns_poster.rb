3f7b52a17ab3ea4d7b3a3188dd5bcc7cd81b77f9
itowtips <ito@web-tips.co.jp>
2017-04-27 11:22:42 +0900

    edit_auto_post を調整

--- app/models/concerns/cms/addon/sns_poster.rb
+++ app/models/concerns/cms/addon/sns_poster.rb
@@ -14,6 +14,8 @@ module Cms::Addon
       field :twitter_post_id,    type: String
       field :facebook_user_id,   type: String
       field :facebook_post_id,   type: String
+      field :twitter_posted,     type: Array, default: []
+      field :facebook_posted,    type: Array, default: []
 
       permit_params :facebook_auto_post,
                     :twitter_auto_post,
@@ -69,6 +71,38 @@ module Cms::Addon
       facebook_auto_post == "active"
     end
 
+    def sns_auto_delete_enabled?
+      sns_auto_delete == "active"
+    end
+
+    def edit_auto_post_enabled?
+      edit_auto_post == "active"
+    end
+
+    def twitter_post_enabled?
+      return false unless use_twitter_post?
+
+      if edit_auto_post_enabled?
+        true
+      elsif twitter_posted.present?
+        false
+      else
+        true
+      end
+    end
+
+    def facebook_post_enabled?
+      return false unless use_facebook_post?
+
+      if edit_auto_post_enabled?
+        true
+      elsif facebook_posted.present?
+        false
+      else
+        true
+      end
+    end
+
     def twitter_url
       "https://twitter.com/#{twitter_user_id}/status/#{twitter_post_id}" if \
         use_twitter_post? && twitter_user_id.present? && twitter_post_id.present?
@@ -95,12 +129,12 @@ module Cms::Addon
     private
     def post_sns
       # tweet
-      if use_twitter_post?
+      if twitter_post_enabled?
         post_to_twitter
       end
 
       # facebook
-      if use_facebook_post?
+      if facebook_post_enabled?
         post_to_facebook
       end
     end
@@ -129,15 +163,14 @@ module Cms::Addon
       # URLを表示するためにスクリーンネームを取得し、DBに保存
       user_screen_id = client.user.screen_name
       self.set(twitter_post_id: twitter_id, twitter_user_id: user_screen_id)
+      self.add_to_set(twitter_posted: { twitter_post_id: twitter_id, twitter_user_id: user_screen_id })
     rescue => e
       Rails.logger.fatal("post_to_twitter failed: #{e.backtrace.join("\n  ")}")
     end
 
     def post_to_facebook
       message = message_format(html)
-      if file_ids.present?
-        image_path = files.first.full_url
-      end
+      file_ids.present? ? image_path = files.first.full_url : nil
       access_token = self.site.facebook_access_token
       graph = Koala::Facebook::API.new(access_token)
       # facebokに投稿し、戻り値を取得
@@ -153,12 +186,13 @@ module Cms::Addon
       # 戻り値からUID/PID取得し、DBに保存
       facebook_id_array = facebook_id_separator(facebook_param)
       self.set(facebook_user_id: facebook_id_array[0], facebook_post_id: facebook_id_array[1])
+      self.add_to_set(facebook_posted: { facebook_user_id: facebook_id_array[0], facebook_post_id: facebook_id_array[1] })
     rescue => e
       Rails.logger.fatal("post_to_facebook failed: #{e.backtrace.join("\n  ")}")
     end
 
     def delete_sns
-      if sns_auto_delete == "active"
+      if sns_auto_delete_enabled?
         if twitter_post_id.present?
           client = connect_twitter
           client.destroy_status(twitter_post_id)
