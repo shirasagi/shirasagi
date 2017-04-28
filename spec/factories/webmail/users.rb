df7020264aebe55533946d1a25c08409121eb21c
tany <tanizawa@web-tips.co.jp>
2017-04-28 14:31:31 +0900

    [fix] webmail spec

--- spec/factories/webmail/users.rb
+++ spec/factories/webmail/users.rb
@@ -1,9 +1,11 @@
 FactoryGirl.define do
   factory :webmail_user, class: SS::User do
+    conf = SS.config.webmail.test_user || {}
+
     name "user_name"
     in_password "pass"
-    email SS.config.webmail.test_user['email']
-    imap_account SS.config.webmail.test_user['account']
-    in_imap_password SS.config.webmail.test_user['password']
+    email conf['email']
+    imap_account conf['account']
+    in_imap_password conf['password']
   end
 end
