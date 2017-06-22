881e831c43422fe2fdcb031be5c5cdf1caaa3f2f
NAKANO Hideo <pinarello.marvel@gmail.com>
2017-06-22 09:38:38 +0900

    [fix] unable to share page to all social networks under subdir (#1493)
    * [add] sub dir spec
    
    * [fix] unable to share page to all social networks

--- spec/factories/cms/sites.rb
+++ spec/factories/cms/sites.rb
@@ -8,4 +8,12 @@ FactoryGirl.define do
     #group_id 1
   end
 
+  factory :cms_site_subdir, class: Cms::Site do
+    name { unique_id }
+    host { name }
+    domains { (server = Capybara.current_session.server).nil? ? "#{name}.localhost.jp" : "#{server.host}:#{server.port}" }
+    auto_keywords "disabled"
+    auto_description "disabled"
+    subdir { unique_id }
+  end
 end
