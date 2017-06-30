01e25ad8066c1fed49ba0e7555706a6308604457
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2017-06-30 18:41:50 +0900

    body_layout_id

--- app/jobs/article/page/import_job.rb
+++ app/jobs/article/page/import_job.rb
@@ -76,11 +76,11 @@ class Article::Page::ImportJob < Cms::ApplicationJob
   def set_page_attributes(row, item)
     # basic
     layout = Cms::Layout.site(site).where(name: value(row, :layout)).first
-    body_layout = Cms::BodyLayout.site(site).where(name: value(row, :body_layout_id)).pluck(:_id).first
+    body_layout_id = Cms::BodyLayout.site(site).where(name: value(row, :body_layout_id)).pluck(:_id).first
     item.name = value(row, :name)
     item.index_name = value(row, :index_name)
     item.layout = layout
-    item.body_layout_id = body_layout
+    item.body_layout_id = body_layout_id
     item.order = value(row, :order)
 
     # meta
