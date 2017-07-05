3111fb5ce56d0c60999152d92be3e4c6b7509da0
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2017-07-05 13:41:28 +0900

    Add close_confirmation

--- app/models/concerns/ss/addon/approve_setting.rb
+++ app/models/concerns/ss/addon/approve_setting.rb
@@ -1,14 +1,28 @@
 module SS::Addon::ApproveSetting
   extend ActiveSupport::Concern
   extend SS::Addon
+
   included do
     field :forced_update, type: String
-    permit_params :forced_update
+    field :close_confirmation, type: String
+    validates :forced_update, inclusion: { in: %w(enabled disabled), allow_blank: true }
+    validates :close_confirmation, inclusion: { in: %w(enabled disabled), allow_blank: true }
+    permit_params :forced_update, :close_confirmation
   end
+
   def forced_update_options
-    [
-      [I18n.t("views.options.state.enabled"), "enabled"],
-      [I18n.t("views.options.state.disabled"), "disabled"]
-    ]
+    %w(enabled disabled).map do |v|
+      [I18n.t("ss.options.state.#{v}"), v]
+    end
+  end
+
+  def close_confirmation_options
+    %w(enabled disabled).map do |v|
+      [I18n.t("ss.options.state.#{v}"), v]
+    end
+  end
+
+  def close_confirmation_enabled?
+    close_confirmation == 'enabled' || close_confirmation.blank?
   end
 end
