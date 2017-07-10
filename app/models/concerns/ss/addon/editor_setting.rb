8b0c18d3152c4606e491877750d9cfff7ff404c2
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2017-07-10 15:25:45 +0900

    Add syntax_check options

--- app/models/concerns/ss/addon/editor_setting.rb
+++ app/models/concerns/ss/addon/editor_setting.rb
@@ -6,9 +6,11 @@ module SS::Addon::EditorSetting
     field :color_button, type: String
     field :editor_css, type: String
     field :editor_css_path, type: String
+    field :syntax_check, type: String
     validates :color_button, inclusion: { in: %w(enabled disabled), allow_blank: true }
     validates :editor_css, inclusion: { in: %w(enabled disabled), allow_blank: true }
-    permit_params :color_button, :editor_css, :editor_css_path
+    validates :syntax_check, inclusion: { in: %w(enabled disabled), allow_blank: true }
+    permit_params :color_button, :editor_css, :editor_css_path, :syntax_check
   end
 
   def color_button_options
@@ -21,5 +23,16 @@ module SS::Addon::EditorSetting
     %w(enabled disabled).map do |v|
       [I18n.t("ss.options.state.#{v}"), v]
     end
+  end
+
+  def syntax_check_options
+    %w(enabled disabled).map do |v|
+      [I18n.t("ss.options.state.#{v}"), v]
+    end
+  end
+
+  def syntax_check_enabled?
+    return false if Cms::WordDictionary.to_config[:replace_words].blank?
+    syntax_check == 'enabled' || syntax_check.blank?
   end
 end
