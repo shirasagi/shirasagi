880e66ec3621fe3fa0d5041d012d1eb22ed015b2
NAKANO Hideo <nakano@web-tips.co.jp>
2017-07-18 15:54:55 +0900

    [modify] add a few BRs to avoid preview controller overlap some elements

--- spec/support/cms/layout.rb
+++ spec/support/cms/layout.rb
@@ -1,6 +1,6 @@
 def create_cms_layout(parts = [], options = {})
   html = []
-  html << "<html><body>"
+  html << "<html><body><br><br><br>"
   html << parts.map { |m| '{{ part "/' + m.filename.sub(/\..*/, '') + '" }}' }.join("\n")
   html << "{{ yield }}"
   html << "</body></html>"
