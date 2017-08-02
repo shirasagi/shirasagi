aae6bcc3b3c5549667f2e2689ebc18c79db3eb34
itowtips <ito@web-tips.co.jp>
2017-08-02 18:08:47 +0900

    [add] open_uri_proxy setting

---
+++ config/initializers/open_uri_proxy.rb
@@ -0,0 +1,22 @@
+module OpenURI
+  class << self
+    alias_method :open_uri_without_proxy, :open_uri
+  end
+
+  def self.open_uri(name, *rest, &block)
+    if !SS.config.open_uri_proxy.disable
+      proxy_uri = SS.config.open_uri_proxy.proxy_http_basic_authentication["proxy_uri"]
+      proxy_username = SS.config.open_uri_proxy.proxy_http_basic_authentication["proxy_username"]
+      proxy_password = SS.config.open_uri_proxy.proxy_http_basic_authentication["proxy_password"]
+
+      options = rest.first || {}
+      if options.kind_of?(Hash)
+        options.symbolize_keys!
+        options.delete(:proxy)
+        options[:proxy_http_basic_authentication] = [proxy_uri, proxy_username, proxy_password]
+        rest = [options]
+      end
+    end
+    open_uri_without_proxy name, *rest, &block
+  end
+end
