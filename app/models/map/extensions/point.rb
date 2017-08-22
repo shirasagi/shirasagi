bed3f18c13d769172163f689391594886c9fda35
itowtips <ito@web-tips.co.jp>
2017-08-22 11:54:48 +0900

    [fix] unable save anpi_post's point

--- app/models/map/extensions/point.rb
+++ app/models/map/extensions/point.rb
@@ -4,28 +4,28 @@ class Map::Extensions::Point < Hash
     loc = self.loc
     return {} if loc.nil?
 
-    ret = { 'loc' => loc.mongoize }
-    ret['zoom_level'] = self[:zoom_level] if self[:zoom_level].present?
+    ret = { "loc" => loc.mongoize }
+    ret["zoom_level"] = zoom_level if zoom_level.present?
     ret
   end
 
   def loc
-    value = self[:loc]
+    value = self["loc"].presence || self[:loc]
     return nil if value.nil?
 
     unless value.is_a?(Map::Extensions::Loc)
-      self[:loc] = value = Map::Extensions::Loc.demongoize(value)
+      value = Map::Extensions::Loc.demongoize(value)
     end
     value
   end
 
   def zoom_level
-    self[:zoom_level]
+    self["zoom_level"].presence || self[:zoom_level]
   end
 
   def empty?
     return true if super
-    loc.empty?
+    loc.blank?
   end
   alias blank? empty?
 
@@ -33,7 +33,9 @@ class Map::Extensions::Point < Hash
     # convert mongoid native type to its custom type(this class)
     def demongoize(object)
       return self.new if object.nil?
-      self[object.to_h.symbolize_keys]
+      ret = self.new
+      ret.merge!(object.to_h)
+      ret
     end
 
     # convert any possible object to mongoid native type
@@ -43,8 +45,14 @@ class Map::Extensions::Point < Hash
         object.mongoize
       when Hash then
         object.deep_stringify_keys!
+        return self.new.mongoize if object["loc"].blank?
+
+        object["loc"] = Map::Extensions::Loc.mongoize(object["loc"])
+        if object["zoom_level"].present?
+          object["zoom_level"] = Integer(object["zoom_level"]) rescue nil
+        end
         ret = self.new
-        ret.merge!(object) if object["loc"].present?
+        ret.merge!(object)
         ret.mongoize
       else object
       end
