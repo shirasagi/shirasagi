b66a5ad3a83618043576d9d51a73523d42a9bfb2
NAKANO Hideo <nakano@web-tips.co.jp>
2017-09-04 18:07:51 +0900

    [add] gws/elasticsearch/search skeletons

---
+++ app/controllers/concerns/gws/elasticsearch/search_filter.rb
@@ -0,0 +1,41 @@
+module Gws::Elasticsearch::SearchFilter
+  extend ActiveSupport::Concern
+
+  included do
+    prepend_view_path 'app/views/gws/elasticsearch/search/main'
+    menu_view nil
+    model nil
+    before_action :set_search_type
+    before_action :set_search_params
+  end
+
+  private
+
+  def set_item
+  end
+
+  def set_crumbs
+  end
+
+  def fix_params
+  end
+
+  def set_search_type
+  end
+
+  def set_search_params
+    @s = OpenStruct.new(params[:s])
+  end
+
+  public
+
+  def show
+    raise '404' unless @cur_site.elasticsearch_enabled?
+
+    if @s.keyword.present?
+      @result = Gws::Elasticsearch::Searcher.search(@cur_site, @search_type, @s.keyword)
+    end
+
+    render
+  end
+end
