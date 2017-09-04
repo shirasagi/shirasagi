e928b44ee8deb00392781c7fe315d876799fd70c
NAKANO Hideo <nakano@web-tips.co.jp>
2017-09-04 20:05:42 +0900

    [modify] stabilize fragile specs to wait for ajax completion explicitly

--- spec/features/gws/schedule/group_plans_spec.rb
+++ spec/features/gws/schedule/group_plans_spec.rb
@@ -15,11 +15,13 @@ describe "gws_schedule_group_plans", type: :feature, dbscope: :example do
 
     it "#index" do
       visit index_path
+      wait_for_ajax
       expect(page).to have_content(item.name)
     end
 
     it "#new" do
       visit new_path
+      wait_for_ajax
       within "form#item-form" do
         fill_in "item[name]", with: "name"
         click_button "保存"
@@ -29,11 +31,13 @@ describe "gws_schedule_group_plans", type: :feature, dbscope: :example do
 
     it "#show" do
       visit show_path
+      wait_for_ajax
       expect(page).to have_content(item.name)
     end
 
     it "#edit" do
       visit edit_path
+      wait_for_ajax
       within "form#item-form" do
         fill_in "item[name]", with: "modify"
         click_button "保存"
@@ -43,6 +47,7 @@ describe "gws_schedule_group_plans", type: :feature, dbscope: :example do
 
     it "#delete" do
       visit delete_path
+      wait_for_ajax
       within "form" do
         click_button "削除"
       end
