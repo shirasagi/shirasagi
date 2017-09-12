7e0c867a71bef844d90696c709005c88d0d586a4
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2017-09-12 16:42:09 +0900

    Add date in <title> (#1516)
    * Add date in <title>
    
    * Add date in <title>
    
    * Add localize format
    
    * Remove some localize format
    
    * Add SS::Site.window_name
    
    * use attr_accessor
    
    * remove redundants
    
    * Add spec in event/agents/nodes/page
    
    * Fix redundant
    
    * modify window_name
    
    * remove site.window_name

--- app/controllers/concerns/cms/public_filter/layout.rb
+++ app/controllers/concerns/cms/public_filter/layout.rb
@@ -63,9 +63,10 @@ module Cms::PublicFilter::Layout
   def render_layout(layout)
     @cur_layout = layout
     @cur_item   = @cur_page || @cur_node
+    @cur_item.window_name ||= @cur_item.name
 
     @window_name = @cur_site.name
-    @window_name = "#{@cur_item.name} - #{@cur_site.name}" if @cur_item.filename != "index.html"
+    @window_name = "#{@cur_item.window_name} - #{@cur_site.name}" if @cur_item.filename != 'index.html'
 
     @cur_layout.keywords    = @cur_item.keywords if @cur_item.respond_to?(:keywords)
     @cur_layout.description = @cur_item.description if @cur_item.respond_to?(:description)
