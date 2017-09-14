a0759b5e890b72ead6c7d93f043331fdabf8a901
jsd_kondo <kondo-yuya@jsdnet.co.jp>
2017-09-14 20:59:56 +0900

    articles state switch

--- app/controllers/gws/monitor/topics_controller.rb
+++ app/controllers/gws/monitor/topics_controller.rb
@@ -4,6 +4,15 @@ class Gws::Monitor::TopicsController < ApplicationController
 
   model Gws::Monitor::Topic
 
+  before_action :set_item, only: [
+    :show, :edit, :update, :delete, :destroy,
+  ]
+
+  before_action :set_selected_items, only: [
+      :destroy_all, :public_all,
+      :preparation_all, :qNA_all
+  ]
+
   private
 
   def fix_params
@@ -11,6 +20,45 @@ class Gws::Monitor::TopicsController < ApplicationController
   end
 
   def set_crumbs
-    @crumbs << ['照会・回答', gws_monitor_topics_path]
+    @crumbs << [t('modules.gws/monitor'), gws_monitor_topics_path]
+  end
+
+  def pre_params
+    super.keep_if {|key| %i(facility_ids).exclude?(key)}
+  end
+
+  public
+
+  def public
+    raise '403' unless @item.allowed?(:edit, @cur_user, site: @cur_site)
+    render_update @item.update(state: 'public')
+  end
+
+  def preparation
+    raise '403' unless @item.allowed?(:edit, @cur_user, site: @cur_site)
+    render_update @item.update(state: 'preparation')
+  end
+
+  def qNA
+    raise '403' unless @item.allowed?(:edit, @cur_user, site: @cur_site)
+    render_update @item.update(state: 'qNA')
+  end
+
+  def public_all
+    raise '403' unless @items.allowed?(:edit, @cur_user, site: @cur_site)
+    @items.update_all(state: 'public')
+    render_destroy_all(false)
+  end
+
+  def preparation_all
+    raise '403' unless @items.allowed?(:edit, @cur_user, site: @cur_site)
+    @items.update_all(state: 'preparation')
+    render_destroy_all(false)
+  end
+
+  def qNA_all
+    raise '403' unless @items.allowed?(:edit, @cur_user, site: @cur_site)
+    @items.update_all(state: 'qNA')
+    render_destroy_all(false)
   end
 end
