cd0a0532d7a549dc49b4224e5ba21a63c4dcf73d
NAKANO Hideo <nakano@web-tips.co.jp>
2017-09-14 18:02:29 +0900

    [add] static permission support

--- app/controllers/gws/elasticsearch/search/alls_controller.rb
+++ app/controllers/gws/elasticsearch/search/alls_controller.rb
@@ -3,6 +3,12 @@ class Gws::Elasticsearch::Search::AllsController < ApplicationController
   include Gws::CrudFilter
   include Gws::Elasticsearch::SearchFilter
 
+  def show
+    set_search_type
+    raise '403' if @search_type.blank?
+    super
+  end
+
   private
 
   def set_crumbs
@@ -10,6 +16,11 @@ class Gws::Elasticsearch::Search::AllsController < ApplicationController
   end
 
   def set_search_type
-    @search_type = nil
+    @search_type ||= begin
+      search_type = []
+      search_type << Gws::Board::Post.collection_name if Gws::Board::Topic.allowed?(:read, @cur_user, site: @cur_site)
+      search_type << 'gws_share_files' if Gws::Share::File.allowed?(:read, @cur_user, site: @cur_site)
+      search_type
+    end
   end
 end
