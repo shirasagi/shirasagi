b3bf624c925b3d348972a3ea7a2d757156a7b1cc
NAKANO Hideo <nakano@web-tips.co.jp>
2017-09-15 15:45:37 +0900

    [add] disable file attachment

--- app/controllers/concerns/gws/schedule/plan_filter.rb
+++ app/controllers/concerns/gws/schedule/plan_filter.rb
@@ -6,6 +6,7 @@ module Gws::Schedule::PlanFilter
     menu_view "gws/schedule/main/menu"
     helper Gws::Schedule::PlanHelper
     model Gws::Schedule::Plan
+    before_action :set_file_addon_state
   end
 
   private
@@ -33,6 +34,10 @@ module Gws::Schedule::PlanFilter
 
   def redirection_url
     url_for(action: :index) + "?calendar[view]=#{redirection_view}&calendar[date]=#{@item.start_at.to_date}"
+  end
+
+  def set_file_addon_state
+    @file_addon_state = 'hide' if @cur_site.schedule_attachment_denied?
   end
 
   public
