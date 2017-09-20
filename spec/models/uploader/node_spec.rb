3c43dc21fa117494e3d9ab8751df20b349ce0162
NAKANO Hideo <pinarello.marvel@gmail.com>
2017-09-20 13:57:45 +0900

    [fix] #1517: remove all files and directories when state was changed to closed. (#1518)
    * [#1517][add] reproduce specs, all files and directories are gone when upload folder state was changed to `closed`
    
    * [#1517][fix] protect from removing uploader's files and directories by adding state validation
    
    * [#1517][fix] protect from removing uploader's files and directories by removing cms/release addon and set default state and released.
    
    * [modify] with rubocop: C: Inconsistent indentation detected.

--- spec/models/uploader/node_spec.rb
+++ spec/models/uploader/node_spec.rb
@@ -5,7 +5,31 @@ describe Uploader::Node::Base, type: :model, dbscope: :example do
   it_behaves_like "cms_node#spec"
 end
 
-describe Uploader::Node::File do
-  let(:item) { create :uploader_node_file }
-  it_behaves_like "cms_node#spec"
+describe Uploader::Node::File, type: :model, dbscope: :example do
+  context 'cms node' do
+    let(:item) { create :uploader_node_file }
+    it_behaves_like "cms_node#spec"
+  end
+
+  context 'all files and directories are gone when state is changed to closed' do
+    let(:item) { create(:uploader_node_file, state: 'public') }
+    let(:uploader_file) { "#{item.path}/logo.png" }
+    let(:sub_dirname) { "#{item.path}/#{unique_id}" }
+    let(:uploader_subdir_file) { "#{sub_dirname}/keyvisual.gif" }
+
+    before do
+      ::FileUtils.mkdir_p(item.path) if !Dir.exist?(item.path)
+      ::FileUtils.cp("#{Rails.root}/spec/fixtures/ss/logo.png", uploader_file)
+      ::FileUtils.mkdir(sub_dirname)
+      ::FileUtils.cp("#{Rails.root}/spec/fixtures/ss/file/keyvisual.gif", uploader_subdir_file)
+    end
+
+    it do
+      item.state = 'closed'
+      expect { item.save! }.to raise_error Mongoid::Errors::Validations
+
+      expect(::File.exist?(uploader_file)).to be_truthy
+      expect(::File.exist?(uploader_subdir_file)).to be_truthy
+    end
+  end
 end
