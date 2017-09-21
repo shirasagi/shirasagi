9e69837eb63b417ecb50f17907110c0050d899d2
NAKANO Hideo <nakano@web-tips.co.jp>
2017-09-21 16:38:17 +0900

    [modify] strict attendance check to owner or managers

--- app/controllers/gws/schedule/attendances_controller.rb
+++ app/controllers/gws/schedule/attendances_controller.rb
@@ -45,15 +45,15 @@ class Gws::Schedule::AttendancesController < ApplicationController
   public
 
   def edit
-    raise "403" unless @item.allowed?(:edit, @cur_user, site: @cur_site)
+    raise "403" unless @cur_schedule.allowed?(:edit, @cur_user, site: @cur_site)
     @item.valid?
     render(layout: 'ss/ajax')
   end
 
   def update
+    raise "403" unless @cur_schedule.allowed?(:edit, @cur_user, site: @cur_site)
     @item.attributes = get_params
     @item.in_updated = params[:_updated] if @item.respond_to?(:in_updated)
-    raise "403" unless @item.allowed?(:edit, @cur_user, site: @cur_site)
 
     render_opts = {
       location: params[:redirect_to]
