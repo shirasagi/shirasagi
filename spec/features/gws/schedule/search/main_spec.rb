5d87f00f7b860b157de1d5f20e2d6521b1b62f29
NAKANO Hideo <nakano@web-tips.co.jp>
2017-09-22 17:58:30 +0900

    [fix] spec failure

--- spec/features/gws/schedule/search/main_spec.rb
+++ spec/features/gws/schedule/search/main_spec.rb
@@ -1,23 +1,30 @@
 require 'spec_helper'
 
-describe "gws_schedule_search", type: :feature, dbscope: :example do
+describe "gws_schedule_search", type: :feature, dbscope: :example, js: true do
   let(:site) { gws_site }
   let(:path) { gws_schedule_search_path site }
 
-  context "with auth", js: true do
+  context "with auth" do
     before { login_gws_user }
 
     it "#index" do
       visit path
-      expect(page).to have_css('.gws-schedule-search-users h2', text: I18n.t('gws/schedule.search_users'))
-      expect(page).to have_css('.gws-schedule-search-times h2', text: I18n.t('gws/schedule.search_times'))
+      click_on I18n.t('gws/schedule.tabs.search/users')
+      expect(page).to have_css('.gws-schedule-search h2', text: I18n.t('gws/schedule.search_users'))
 
-      fill_in 's[keyword]', with: gws_user.name
-      first('.gws-schedule-search-users input[type=submit]').click
+      within 'form.search' do
+        fill_in 's[keyword]', with: gws_user.name
+        click_on I18n.t('ss.buttons.search')
+      end
       expect(page).to have_css('#calendar-controller')
 
       visit path
-      first('.gws-schedule-search-times input[type=submit]').click
+      click_on I18n.t('gws/schedule.tabs.search/times')
+      puts page.html
+      expect(page).to have_css('.gws-schedule-search h2', text: I18n.t('gws/schedule.search_times'))
+      within 'form.search' do
+        click_on I18n.t('ss.buttons.search')
+      end
       expect(page).to have_css('.gws-schedule-search-times-result')
     end
   end
