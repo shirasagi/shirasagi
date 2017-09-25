d19dd19159ada565d83fedbd729f97db9a3dd12c
NAKANO Hideo <nakano@web-tips.co.jp>
2017-09-25 21:04:36 +0900

    [modify] dynamically load appropriate custom field input form

--- app/models/concerns/gws/addon/schedule/facility_custom_field.rb
+++ app/models/concerns/gws/addon/schedule/facility_custom_field.rb
@@ -1,4 +1,8 @@
 module Gws::Addon::Schedule::FacilityCustomField
   extend ActiveSupport::Concern
   extend SS::Addon
+
+  included do
+    belongs_to :main_facility, class_name: 'Gws::Facility::Item'
+  end
 end
