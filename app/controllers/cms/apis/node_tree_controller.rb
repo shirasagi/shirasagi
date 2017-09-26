44eafb415f7103646953acb18725cb1a38df7df0
tany <una@kimiyure.com>
2017-09-26 20:57:46 +0900

    [modify] tree-navi

--- app/controllers/cms/apis/node_tree_controller.rb
+++ app/controllers/cms/apis/node_tree_controller.rb
@@ -5,16 +5,41 @@ class Cms::Apis::NodeTreeController < ApplicationController
   model Cms::Node
 
   def index
+    @limit = 100
     @type = params[:type].presence || 'cms_nodes'
+    @item = @model.find(params[:id]) if params[:id] != '0'
 
-    if params[:id] != '0'
-      @item = @model.find(params[:id])
+    if params[:only_children]
+      items = child_items if @item
+    else
+      items = root_items
+      items += @item.parents.entries + [@item] if @item
+      items += tree_items + child_items if @item
     end
 
-    items = root_items
-    items += tree_items if @item
+    render json: { items: items_hash(items) }.to_json
+  end
 
-    data = items.sort{ |a, b| a.filename <=> b.filename }.map do |item|
+  private
+
+  def root_items
+    @model.site(@cur_site).where(depth: 1).
+      allow(:read, @cur_user, site: @cur_site).limit(@limit)
+  end
+
+  def tree_items
+    @item.parents.map do |item|
+      next unless item.allowed?(:read, @cur_user, site: @cur_site)
+      item.children.allow(:read, @cur_user, site: @cur_site).limit(@limit)
+    end.flatten
+  end
+
+  def child_items
+    @item.children.allow(:read, @cur_user, site: @cur_site).limit(@limit)
+  end
+
+  def items_hash(items)
+    items.map do |item|
       {
         name: item.name,
         filename: item.filename,
@@ -24,39 +49,13 @@ class Cms::Apis::NodeTreeController < ApplicationController
         is_current: (@item.present? && item.id == @item.id),
         is_parent: (@item.present? && @item.filename.start_with?("#{item.filename}\/"))
       }
-    end
-
-    render json: { items: data }.to_json
+    end.compact.uniq.sort{ |a, b| a[:filename] <=> b[:filename] }
   end
-
-  private
 
   def item_url(item)
-    if @type == 'cms/page'
-      node_pages_path(cid: item.id)
-    elsif @type == 'cms/part'
-      node_parts_path(cid: item.id)
-    elsif @type == 'cms/layout'
-      node_layouts_path(cid: item.id)
-    else
-      item.respond_to?(:view_route) ? contents_path(item) : cms_node_nodes_path(cid: item.id)
-    end
-  end
-
-  def root_items
-    @model.site(@cur_site).
-      where(depth: 1).
-      allow(:read, @cur_user, site: @cur_site)
-  end
-
-  def tree_items
-    items = []
-
-    @item.parents.each do |item|
-      next unless item.allowed?(:read, @cur_user, site: @cur_site)
-      items += item.children.allow(:read, @cur_user, site: @cur_site)
-    end
-
-    items + @item.children.allow(:read, @cur_user, site: @cur_site)
+    return node_pages_path(cid: item.id) if @type == 'cms/page'
+    return node_parts_path(cid: item.id) if @type == 'cms/part'
+    return node_layouts_path(cid: item.id) if @type == 'cms/layout'
+    item.respond_to?(:view_route) ? contents_path(item) : cms_node_nodes_path(cid: item.id)
   end
 end
