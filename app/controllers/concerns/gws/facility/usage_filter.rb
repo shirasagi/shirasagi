76cc033a1c269ded76f488ab649ef0df9763deae
NAKANO Hideo <nakano@web-tips.co.jp>
2017-09-28 22:10:47 +0900

    [modify] reduce duplicated codes

---
+++ app/controllers/concerns/gws/facility/usage_filter.rb
@@ -0,0 +1,135 @@
+module Gws::Facility::UsageFilter
+  extend ActiveSupport::Concern
+
+  included do
+    model Gws::Facility::Item
+
+    navi_view 'gws/facility/settings/navi'
+    menu_view 'gws/facility/usage/main/menu'
+
+    helper_method :format_usage_count, :format_usage_hours
+
+    before_action :set_target_time, :set_search_params, :set_years_and_months, :set_items
+  end
+
+  private
+
+  def set_crumbs
+    @crumbs << [t('mongoid.models.gws/facility/group_setting'), gws_facility_items_path]
+    @crumbs << [t('gws/facility.navi.usage'), { action: :index }]
+  end
+
+  def fix_params
+    { cur_user: @cur_user, cur_site: @cur_site }
+  end
+
+  def set_target_time
+    @target_time ||= begin
+      yyyy = params[:yyyy].presence
+      yyyymm = params[:yyyymm].presence
+      raise '403' if yyyy.blank? && yyyymm.blank?
+
+      if yyyy
+        Time.zone.parse("#{yyyy}/01/01")
+      else
+        Time.zone.parse("#{yyyymm[0..3]}/#{yyyymm[4..5]}/01")
+      end
+    end
+  end
+
+  def set_search_params
+    @s ||= begin
+      s = OpenStruct.new(params[:s])
+      s[:year] ||= @target_time.year
+      s[:month] ||= @target_time.month if params[:yyyymm].present?
+      s
+    end
+  end
+
+  def find_usage(facility, *args)
+    year, month, day = args
+    @aggregation.find do |data|
+      next false if data['_id']['facility_id'] != facility.id
+      next false if data['_id']['year'] != year
+      next false if data['_id']['month'] != month
+      next false if day && data['_id']['day'] != day
+      true
+    end
+  end
+
+  def format_usage_count(*args)
+    data = find_usage(*args)
+    return unless data
+    data['count'].to_s(:delimited)
+  end
+
+  def format_usage_hours(*args)
+    data = find_usage(*args)
+    return unless data
+    data['total_usage_hours'].to_s
+  end
+
+  def set_years_and_months
+    sy = Time.zone.today.year - 10
+    ey = Time.zone.today.year
+    @years = (sy..ey).to_a.reverse.map { |d| ["#{d}#{t('datetime.prompts.year')}", d] }
+    @months = (1..12).to_a.map { |d| ["#{d}#{t('datetime.prompts.month')}", d] }
+    sd = @target_time.day
+    ed = @target_time.end_of_month.day
+    @days = (sd..ed).lazy.map { |d| ["#{d}#{t('datetime.prompts.day')}", d] }
+  end
+
+  def set_items
+    @items = @model.site(@cur_site).
+      state(params.dig(:s, :state)).
+      search(params[:s]).
+      allow(:read, @cur_user, site: @cur_site)
+  end
+
+  def target_range
+    raise NotImplementedError
+  end
+
+  def aggregation_ids
+    raise NotImplementedError
+  end
+
+  public
+
+  def index
+    @items = @items.page(params[:page]).per(50)
+
+    criteria = Gws::Schedule::Plan.site(@cur_site)
+    criteria = criteria.in(facility_ids: @items.pluck(:id))
+    criteria = criteria.gte(start_at: @target_time).lt(start_at: @target_time + target_range)
+
+    pipes = []
+    pipes << { '$match' => criteria.selector }
+    pipes << {
+      '$project' => {
+        'usage_hours' => { '$subtract' => [ '$end_at', '$start_at' ] },
+        'facility_ids' => 1,
+        'local_start_at' => { '$add' => [ '$start_at', Time.zone.utc_offset * 1_000 ] },
+        'local_end_at' => { '$add' => [ '$end_at', Time.zone.utc_offset * 1_000 ] }
+      }
+    }
+    pipes << {
+      '$project' => {
+        'usage_hours' => { '$cond' => [ { '$gte' => [ '$usage_hours', 86_399_000 ] }, 24 * 60 * 60 * 1_000, '$usage_hours' ] },
+        'facility_ids' => 1,
+        'local_start_at' => 1,
+        'local_end_at' => 1
+      }
+    }
+    pipes << { '$unwind' => '$facility_ids' }
+    pipes << {
+      '$group' => {
+        '_id' => aggregation_ids,
+        'count' => { '$sum' => 1 },
+        'total_usage_hours' => { '$sum' => { '$divide' => [ '$usage_hours', 60 * 60 * 1_000 ] } }
+      }
+    }
+
+    @aggregation = Gws::Schedule::Plan.collection.aggregate(pipes).to_a.map(&:to_h)
+  end
+end
--- app/controllers/gws/facility/usage/monthly_controller.rb
+++ app/controllers/gws/facility/usage/monthly_controller.rb
@@ -1,120 +1,20 @@
 class Gws::Facility::Usage::MonthlyController < ApplicationController
   include Gws::BaseFilter
   include Gws::CrudFilter
-
-  model Gws::Facility::Item
-
-  navi_view 'gws/facility/settings/navi'
-  menu_view 'gws/facility/usage/main/menu'
-
-  helper_method :format_usage_count, :format_usage_hours
+  include Gws::Facility::UsageFilter
 
   private
 
-  def set_crumbs
-    @crumbs << [t('mongoid.models.gws/facility/group_setting'), gws_facility_items_path]
-    @crumbs << [t('gws/facility.navi.usage'), gws_facility_usage_monthly_index_path]
+  def target_range
+    1.month
   end
 
-  def fix_params
-    { cur_user: @cur_user, cur_site: @cur_site }
-  end
-
-  def set_target_time
-    @target_time ||= begin
-      yyyymm = params[:yyyymm].presence
-      raise '403' if yyyymm.blank?
-
-      Time.zone.parse("#{yyyymm[0..3]}/#{yyyymm[4..5]}/01")
-    end
-  end
-
-  def set_search_params
-    set_target_time
-    @s ||= begin
-      s = OpenStruct.new(params[:s])
-      s[:year]  ||= @target_time.year
-      s[:month] ||= @target_time.month
-      s
-    end
-  end
-
-  def find_usage(facility, year, month, day)
-    @aggregation.find do |data|
-      data['_id']['facility_id'] == facility.id && data['_id']['month'] == month && data['_id']['day'] == day
-    end
-  end
-
-  def format_usage_count(facility, year, month, day)
-    data = find_usage(facility, year, month, day)
-    return unless data
-    count = data['count']
-    count.to_s(:delimited)
-  end
-
-  def format_usage_hours(facility, year, month, day)
-    data = find_usage(facility, year, month, day)
-    return unless data
-    hours = data['total_usage_hours']
-    hours.to_s
-  end
-
-  public
-
-  def index
-    set_target_time
-    set_search_params
-
-    sy = Time.zone.today.year - 10
-    ey = Time.zone.today.year
-    @years = (sy..ey).to_a.reverse.map { |d| ["#{d}#{t('datetime.prompts.year')}", d] }
-    @months = (1..12).to_a.map { |d| ["#{d}#{t('datetime.prompts.month')}", d] }
-    sd = @target_time.day
-    ed = @target_time.end_of_month.day
-    @days = (sd..ed).to_a.map { |d| ["#{d}#{t('datetime.prompts.day')}", d] }
-
-    @items = @model.site(@cur_site).
-      state(params.dig(:s, :state)).
-      search(params[:s]).
-      allow(:read, @cur_user, site: @cur_site).
-      page(params[:page]).per(50)
-
-    criteria = Gws::Schedule::Plan.site(@cur_site)
-    criteria = criteria.in(facility_ids: @items.pluck(:id))
-    criteria = criteria.gte(start_at: @target_time).lt(start_at: @target_time + 1.month)
-
-    pipes = []
-    pipes << { '$match' => criteria.selector }
-    pipes << {
-      '$project' => {
-        'usage_hours' => { '$subtract' => [ '$end_at', '$start_at' ] },
-        'facility_ids' => 1,
-        'start_at' => { '$add' => [ '$start_at', Time.zone.utc_offset * 1_000 ] },
-        'end_at' => { '$add' => [ '$end_at', Time.zone.utc_offset * 1_000 ] }
-      }
+  def aggregation_ids
+    {
+      'facility_id' => '$facility_ids',
+      'year' => { '$year' => '$local_start_at' },
+      'month' => { '$month' => '$local_start_at' },
+      'day' => { '$dayOfMonth' => '$local_start_at' }
     }
-    pipes << {
-      '$project' => {
-        'usage_hours' => { '$cond' => [ { '$gte' => [ '$usage_hours', 86_399_000 ] }, 24 * 60 * 60 * 1_000, '$usage_hours' ] },
-        'facility_ids' => 1,
-        'start_at' => 1,
-        'end_at' => 1
-      }
-    }
-    pipes << { '$unwind' => '$facility_ids' }
-    pipes << {
-      '$group' => {
-        '_id' => {
-          'facility_id' => '$facility_ids',
-          'year' => { '$year' => '$start_at' },
-          'month' => { '$month' => '$start_at' },
-          'day' => { '$dayOfMonth' => '$start_at' }
-        },
-        'count' => { '$sum' => 1 },
-        'total_usage_hours' => { '$sum' => { '$divide' => [ '$usage_hours', 60 * 60 * 1_000 ] } }
-      }
-    }
-
-    @aggregation = Gws::Schedule::Plan.collection.aggregate(pipes).to_a.map(&:to_h)
   end
 end
