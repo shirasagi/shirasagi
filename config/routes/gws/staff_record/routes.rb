9793075e7db0543ce8bc574ccace0f073b9f378a
tany <tanizawa@web-tips.co.jp>
2017-10-03 04:04:24 +0900

    [add] gws/staff_record.seating (#1624)

--- config/routes/gws/staff_record/routes.rb
+++ config/routes/gws/staff_record/routes.rb
@@ -18,10 +18,12 @@ SS::Application.routes.draw do
       get :edit_charge, on: :member
       put :update_charge, on: :member
     end
+    resources :public_seatings, only: [:index]
 
     resource :setting, only: [:show, :edit, :update]
     resources :years, concerns: [:deletion]
     resources :groups, path: ':year/groups', concerns: [:deletion, :export]
     resources :users, path: ':year/users', concerns: [:deletion, :export]
+    resources :seatings, path: ':year/seatings', concerns: [:deletion, :export]
   end
 end
---
+++ spec/factories/gws/staff_record/seating.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :gws_staff_record_seating, class: Gws::StaffRecord::Seating do
+    cur_site { gws_site }
+    cur_user { gws_user }
+
+    name { "name-#{unique_id}" }
+    url { 'http://example.jp' }
+    remark { "remark-#{unique_id}" }
+  end
+end
