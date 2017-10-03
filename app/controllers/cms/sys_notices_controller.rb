3e4036625d25fa9fe488b8831474e0843cc25178
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2017-10-03 16:28:54 +0900

    [Modify]cms/sys_notices_controller.rb

--- app/controllers/cms/sys_notices_controller.rb
+++ app/controllers/cms/sys_notices_controller.rb
@@ -1,12 +1,20 @@
 class Cms::SysNoticesController < ApplicationController
   include Cms::BaseFilter
-  include SS::CrudFilter
+  include Cms::CrudFilter
   include Sns::PublicNoticeFilter
 
   append_view_path 'app/views/sns/public_notices'
   navi_view "cms/main/navi"
 
   private
+
+  def set_item
+    @item = @model.find(params[:id])
+    @item.attributes = fix_params
+  rescue Mongoid::Errors::DocumentNotFound => e
+    return render_destroy(true) if params[:action] == 'destroy'
+    raise e
+  end
 
   def set_crumbs
     @crumbs << [t("mongoid.models.sys/notice"), action: :index]
