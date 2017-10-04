80a2ca734fe4a9140e99b578bafb135ea806a625
tany <tanizawa@web-tips.co.jp>
2017-10-04 09:55:52 +0900

    FAQ and QnA (#1629)

---
+++ app/models/gws/faq/post.rb
@@ -0,0 +1,15 @@
+# "Post" class for BBS. It represents "comment" models.
+class Gws::Faq::Post
+  include Gws::Referenceable
+  include Gws::Faq::Postable
+  include Gws::Addon::Faq::Contributor
+  include SS::Addon::Markdown
+  include Gws::Addon::File
+  include Gws::Faq::DescendantsFileInfo
+  include Gws::Addon::GroupPermission
+  include Gws::Addon::History
+
+  # indexing to elasticsearch via companion object
+  around_save ::Gws::Elasticsearch::Indexer::FaqPostJob.callback
+  around_destroy ::Gws::Elasticsearch::Indexer::FaqPostJob.callback
+end
---
+++ app/models/gws/qna/topic.rb
@@ -0,0 +1,68 @@
+# "Post" class for BBS. It represents "topic" models.
+class Gws::Qna::Topic
+  include Gws::Referenceable
+  include Gws::Qna::Postable
+  include Gws::Addon::Qna::Contributor
+  include SS::Addon::Markdown
+  include Gws::Addon::File
+  include Gws::Qna::DescendantsFileInfo
+  include Gws::Addon::Qna::Category
+  include Gws::Addon::Release
+  include Gws::Addon::ReadableSetting
+  include Gws::Addon::GroupPermission
+  include Gws::Addon::History
+  include Gws::Qna::BrowsingState
+
+  readable_setting_include_custom_groups
+
+  validates :category_ids, presence: true
+  after_validation :set_descendants_updated_with_released, if: -> { released.present? && released_changed? }
+
+  # indexing to elasticsearch via companion object
+  around_save ::Gws::Elasticsearch::Indexer::QnaTopicJob.callback
+  around_destroy ::Gws::Elasticsearch::Indexer::QnaTopicJob.callback
+
+  scope :custom_order, ->(key) {
+    if key.start_with?('created_')
+      where({}).order_by(created: key.end_with?('_asc') ? 1 : -1)
+    elsif key.start_with?('updated_')
+      where({}).order_by(descendants_updated: key.end_with?('_asc') ? 1 : -1)
+    else
+      where({})
+    end
+  }
+
+  def updated?
+    created.to_i != updated.to_i || created.to_i != descendants_updated.to_i
+  end
+
+  def subscribed_users
+    return Gws::User.none if new_record?
+    return Gws::User.none if categories.blank?
+
+    conds = []
+    conds << { id: { '$in' => categories.pluck(:subscribed_member_ids).flatten } }
+    conds << { group_ids: { '$in' => categories.pluck(:subscribed_group_ids).flatten } }
+
+    if Gws::Qna::Category.subscription_setting_included_custom_groups?
+      custom_gropus = Gws::CustomGroup.in(id: categories.pluck(:subscribed_custom_group_ids))
+      conds << { id: { '$in' => custom_gropus.pluck(:member_ids).flatten } }
+    end
+
+    Gws::User.where('$and' => [ { '$or' => conds } ])
+  end
+
+  def sort_options
+    %w(updated_desc updated_asc created_desc created_asc).map { |k| [I18n.t("ss.options.sort.#{k}"), k] }
+  end
+
+  private
+
+  def set_descendants_updated_with_released
+    if descendants_updated.present?
+      self.descendants_updated = released if descendants_updated < released
+    else
+      self.descendants_updated = released
+    end
+  end
+end
