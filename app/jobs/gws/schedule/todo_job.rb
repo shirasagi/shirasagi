561a373c6750e67be64f0849a0695944340e51a4
Yohei Igata <yohei.igata@gmail.com>
2017-10-05 23:40:20 +0900

    [fix] starting pos of deletion to created attr

---
+++ app/jobs/gws/schedule/todo_job.rb
@@ -0,0 +1,10 @@
+class Gws::Schedule::TodoJob < Gws::ApplicationJob
+
+  def perform(opts = {})
+    threshold = (6 + site.todo_delete_threshold * 6).month.ago
+
+    count = Gws::Schedule::Todo.where(:created.lt => threshold).delete_all
+
+    Rails.logger.info "#{threshold}以前のToDoを#{count}件削除しました。"
+  end
+end
