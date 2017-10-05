5f03bb69d2ae8ea3a3f1402ef58b1fa686dfbfa7
NAKANO Hideo <nakano@web-tips.co.jp>
2017-10-05 18:50:04 +0900

    [add] upload file remove support to gws/workflow/file

--- app/models/concerns/gws/addon/workflow/custom_form.rb
+++ app/models/concerns/gws/addon/workflow/custom_form.rb
@@ -14,10 +14,15 @@ module Gws::Addon::Workflow::CustomForm
 
   module ClassMethods
     def build_custom_values(form, hash)
-      values = form.columns.map do |column|
+      values = []
+      form.columns.each do |column|
         column_id = column.id.to_s
         value = Gws::Workflow::Column.to_mongo(column.input_type, hash[column_id])
-        [ column_id, { 'input_type' => column.input_type, 'name' => column.name, 'value' => value } ]
+        if value.is_a?(Hash)
+          values << [ column_id, { 'input_type' => column.input_type, 'name' => column.name }.merge(value) ]
+        else
+          values << [ column_id, { 'input_type' => column.input_type, 'name' => column.name, 'value' => value } ]
+        end
       end
       Hash[values]
     end
@@ -33,12 +38,11 @@ module Gws::Addon::Workflow::CustomForm
     Gws::Workflow::Column.from_mongo(val['input_type'], val['value'])
   end
 
-  def write_custom_value(column, value)
+  def remove_custom_value(column)
     return if custom_values.blank?
 
     column_id = column.respond_to?(:id) ? column.id.to_s : column.to_s
-    custom_values[column_id]['value'] = value
-    nil
+    self.custom_values.delete(column_id)
   end
 
   private
@@ -55,47 +59,97 @@ module Gws::Addon::Workflow::CustomForm
   def before_save_custom_values
     validate_upload_files
     return if errors.present?
+    remove_upload_files
     save_upload_files
-    # remove_upload_files
   end
 
   def validate_upload_files
+    return if custom_values.blank?
+
     self.cur_form.columns.where(input_type: 'upload_file').each do |column|
-      value = read_custom_value(column)
-      if value.is_a?(ActionDispatch::Http::UploadedFile)
-        file = read_relation_file(column)
-        if file.invalid?
-          errors[:base] += file.errors.full_messages
-        end
+      hash = custom_values[column.id.to_s]
+      next if hash.blank?
+
+      uploaded_file = hash['file']
+      next if uploaded_file.blank?
+
+      file = build_file(column, uploaded_file)
+      next if file.valid?
+
+      errors[:base] += file.errors.full_messages
+    end
+  end
+
+  def remove_upload_files
+    return if custom_values.blank?
+
+    self.cur_form.columns.where(input_type: 'upload_file').each do |column|
+      hash = custom_values[column.id.to_s]
+      next if hash.blank?
+
+      do_destroy = false
+
+      rm_flag = hash['rm']
+      do_destroy = true if rm_flag == '1'
+
+      uploaded_file = hash['file']
+      do_destroy = true if uploaded_file.present?
+
+      next unless do_destroy
+
+      file_id = hash['value']
+      if file_id.blank?
+        hash.delete('rm')
+        hash.delete('value')
+        custom_values[column.id.to_s] = hash
+        next
       end
+
+      file = Gws::File.site(site).find(file_id) rescue nil
+      if file.blank?
+        hash.delete('rm')
+        hash.delete('value')
+        custom_values[column.id.to_s] = hash
+        next
+      end
+
+      file.destroy
+      hash.delete('rm')
+      hash.delete('value')
+      custom_values[column.id.to_s] = hash
     end
   end
 
   def save_upload_files
-    self.cur_form.columns.where(input_type: 'upload_file').each do |column|
-      value = read_custom_value(column)
-      if value.is_a?(ActionDispatch::Http::UploadedFile)
-        file = read_relation_file(column)
-        file.save
+    return if custom_values.blank?
 
-        write_custom_value(column, Gws::Workflow::Column.to_mongo(column.input_type, file.id))
-      end
+    self.cur_form.columns.where(input_type: 'upload_file').each do |column|
+      hash = custom_values[column.id.to_s]
+      next if hash.blank?
+
+      uploaded_file = hash['file']
+      next if uploaded_file.blank?
+
+      file = build_file(column, uploaded_file)
+      next if file.invalid?
+
+      file.save!
+
+      hash['value'] = file.id
+      hash.delete('file')
+
+      custom_values[column.id.to_s] = hash
     end
   end
 
-  def read_relation_file(column)
-    value = read_custom_value(column)
-    if value.is_a?(ActionDispatch::Http::UploadedFile)
-      file = Gws::File.new
-      file.in_file = value
-      file.filename = value.original_filename
-      file.site_id = site_id if respond_to?(:site_id)
-      file.user_id = @cur_user.id if @cur_user
-      file.resizing = column.resizing
-      file
-    else
-      Gws::File.find(value) rescue nil
-    end
+  def build_file(column, uploaded_file)
+    file = Gws::File.new
+    file.in_file = uploaded_file
+    file.filename = uploaded_file.original_filename
+    file.site_id = site_id if respond_to?(:site_id)
+    file.user_id = @cur_user.id if @cur_user
+    file.resizing = column.resizing
+    file
   end
 
   def after_save_custom_values
