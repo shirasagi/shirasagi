0910b97f6923f71536a978fb2a7624fffed3232d
tany <una@kimiyure.com>
2017-10-05 13:04:10 +0900

    [add] faq spec

---
+++ spec/factories/gws/faq/post.rb
@@ -0,0 +1,21 @@
+FactoryGirl.define do
+  factory :gws_faq_post, class: Gws::Faq::Post do
+    cur_site { gws_site }
+    cur_user { gws_user }
+
+    name { "name-#{unique_id}" }
+    text { "text-#{unique_id}" }
+
+    factory :gws_faq_topic do
+      parent nil
+    end
+
+    factory :gws_faq_comment do
+      association :parent, factory: :gws_faq_topic
+    end
+
+    factory :gws_faq_comment_to_comment do
+      association :parent, factory: :gws_faq_comment
+    end
+  end
+end
---
+++ spec/features/gws/faq/categories_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe "gws_faq_categories", type: :feature, dbscope: :example do
+  let(:site) { gws_site }
+  let(:index_path) { gws_faq_categories_path site }
+
+  context "basic crud" do
+    let(:name) { unique_id }
+    let(:name2) { unique_id }
+    let(:color) { "#481357" }
+
+    before { login_gws_user }
+
+    it do
+      visit index_path
+      expect(status_code).to eq 200
+      expect(current_path).to eq index_path
+
+      #
+      # create
+      #
+
+      click_on "新規作成"
+
+      within "form#item-form" do
+        fill_in "item[name]", with: name
+        fill_in "item[color]", with: color
+        click_button "保存"
+      end
+
+      category = Gws::Faq::Category.site(site).find_by(name: name)
+      expect(category.name).to eq name
+      expect(category.color).to eq color
+
+      expect(page).to have_css("div.addon-body dd", text: name)
+
+      #
+      # edit
+      #
+      click_link "編集する"
+
+      within "form#item-form" do
+        fill_in "item[name]", with: name2
+        click_button "保存"
+      end
+
+      category.reload
+      expect(category.name).to eq name2
+      expect(category.color).to eq color
+
+      expect(page).to have_css("div.addon-body dd", text: name2)
+
+      #
+      # index
+      #
+      click_link "一覧へ戻る"
+      within "div.info" do
+        expect(page).to have_css("a.title", text: name2)
+        click_link name2
+      end
+
+      #
+      # delete
+      #
+      click_link "削除する"
+      click_button "削除"
+
+      category = Gws::Faq::Category.site(site).where(name: name).first
+      expect(category).to be_nil
+
+      expect(page).to have_no_css("div.info")
+    end
+  end
+end
