3335fa5cd41bb07778be4765cda399fe19d21902
Yohei Igata <yohei.igata@gmail.com>
2017-10-08 18:51:22 +0900

    [modify] change instance method

--- app/models/gws/circular/topic.rb
+++ app/models/gws/circular/topic.rb
@@ -41,7 +41,7 @@ class Gws::Circular::Topic
     return criteria if params.blank?
 
     if sort_num = params[:sort].to_i
-      criteria = criteria.order_by(sort_hash(sort_num))
+      criteria = criteria.order_by(new.sort_hash(sort_num))
     end
 
     if params[:keyword].present?
@@ -87,44 +87,43 @@ class Gws::Circular::Topic
     self
   end
 
+  def mark_type_options
+    [
+        ['通常閲覧', 'normal'],
+        ['簡易閲覧', 'simple']
+    ]
+  end
+
+  def sort_items
+    [
+        {
+            key: :updated,
+            order: -1,
+            name: I18n.t('mongoid.attributes.ss/document.updated')
+        },
+        {
+            key: :created,
+            order: -1,
+            name: I18n.t('mongoid.attributes.ss/document.created')
+        }
+    ]
+  end
+
+  def sort_hash(num=0)
+    result = {}
+    item = sort_items[num]
+    result[item[:key]] = item[:order]
+    result
+  end
+
+  def sort_options
+    sort_items.map.with_index { |item, i| [item[:name], i] }
+  end
+
   class << self
-    def mark_type_options
-      [
-          ['通常閲覧', 'normal'],
-          ['簡易閲覧', 'simple']
-      ]
-    end
-
-    def sort_items
-      [
-          {
-              key: :updated,
-              order: -1,
-              name: I18n.t('mongoid.attributes.ss/document.updated')
-          },
-          {
-              key: :created,
-              order: -1,
-              name: I18n.t('mongoid.attributes.ss/document.created')
-          }
-      ]
-    end
-
-    def sort_hash(num=0)
-      result = {}
-      item = sort_items[num]
-      result[item[:key]] = item[:order]
-      result
-    end
-
-    def sort_options
-      sort_items.map.with_index { |item, i| [item[:name], i] }
-    end
-
     def to_csv
       CSV.generate do |data|
         data << %w(回覧id タイトル 返信id 状態 返信者名 返信欄 返信日時)
-
         each do |item|
           item.members.each do |member|
             post = item.children.where(user_id: member.id).first
