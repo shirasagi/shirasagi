aa6a9597c4e3e023c5fa41a294468e5b587ed900
itowtips <ito@web-tips.co.jp>
2017-10-11 16:25:18 +0900

    [fix] spec failure

--- app/models/webmail/initializer.rb
+++ app/models/webmail/initializer.rb
@@ -2,5 +2,6 @@ module Webmail
   class Initializer
     SS::User.include Webmail::UserExtension
     Cms::User.include Webmail::UserExtension
+    Gws::User.include Webmail::UserExtension
   end
 end
