73a06e83e744b712437845f9f0b1d8d7df76645b
itowtips <ito@web-tips.co.jp>
2017-10-11 14:26:21 +0900

    [fix] spec failure

--- spec/features/webmail/addresses_spec.rb
+++ spec/features/webmail/addresses_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "webmail_addresses", type: :feature, dbscope: :example do
   let!(:item) { create :webmail_address }
-  let(:index_path) { webmail_addresses_path }
+  let(:index_path) { webmail_addresses_path(account: 0) }
 
   context "with auth" do
     before { login_ss_user }
