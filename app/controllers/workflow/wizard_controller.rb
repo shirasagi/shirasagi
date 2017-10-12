82d962c13886b65331080565552e15ff615c0889
NAKANO Hideo <nakano@web-tips.co.jp>
2017-10-12 15:27:03 +0900

    [add] new reroute procedure

--- app/controllers/workflow/wizard_controller.rb
+++ app/controllers/workflow/wizard_controller.rb
@@ -1,8 +1,9 @@
 class Workflow::WizardController < ApplicationController
   include Cms::ApiFilter
+  include Workflow::WizardFilter
 
   before_action :set_route, only: [:approver_setting]
-  before_action :set_item, only: [:approver_setting]
+  before_action :set_item, only: [:approver_setting, :reroute, :do_reroute]
 
   private
 
@@ -44,5 +45,29 @@ class Workflow::WizardController < ApplicationController
     else
       render file: :approver_setting, layout: false
     end
+  end
+
+  def reroute
+    level = Integer(params[:level])
+
+    workflow_approvers = @item.workflow_approvers
+    workflow_approvers = workflow_approvers.select do |workflow_approver|
+      workflow_approver[:level] == level
+    end
+    same_level_user_ids = workflow_approvers.map do |workflow_approver|
+      workflow_approver[:user_id]
+    end
+
+    criteria = @item.approver_user_class.site(@cur_site)
+    criteria = criteria.search(params[:s])
+    criteria = criteria.nin(id: same_level_user_ids + [ @item.workflow_user_id ])
+    criteria = criteria.order_by(filename: 1)
+
+    @items = criteria.select do |user|
+      @item.allowed?(:read, user, site: @cur_site) && @item.allowed?(:approve, user, site: @cur_site)
+    end
+    @items = Kaminari.paginate_array(@items).page(params[:page]).per(50)
+
+    render file: 'reroute', layout: false
   end
 end
