171d0cc8df7b3fc7e92136fa3c6886bb3c659903
Yohei Igata <yohei.igata@gmail.com>
2017-10-13 21:32:30 +0900

    [add] gws/memo/folder

---
+++ app/controllers/gws/memo/folders_controller.rb
@@ -0,0 +1,16 @@
+class Gws::Memo::FoldersController < ApplicationController
+  include Gws::BaseFilter
+  include Gws::CrudFilter
+
+  model Gws::Memo::Folder
+
+  def set_crumbs
+    @crumbs << ['連絡メモ', gws_memo_messages_path ]
+    @crumbs << ['管理', gws_memo_folders_path ]
+  end
+
+  def fix_params
+    { cur_user: @cur_user, cur_site: @cur_site }
+  end
+
+end
