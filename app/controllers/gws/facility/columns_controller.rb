7d90806dce36e0786634ff42f409ff62edaf4137
NAKANO Hideo <nakano@web-tips.co.jp>
2017-10-19 17:57:45 +0900

    [modify] refactor gws/schedule/facility_plan with new gws/column

--- app/controllers/gws/facility/columns_controller.rb
+++ app/controllers/gws/facility/columns_controller.rb
@@ -14,4 +14,17 @@ class Gws::Facility::ColumnsController < ApplicationController
     @crumbs << [t('mongoid.models.gws/facility/group_setting/item'), gws_facility_items_path]
     @crumbs << [@cur_form.name, gws_facility_item_path(id: @cur_form)]
   end
+
+  public
+
+  def input_form
+    set_form
+    raise '403' unless @cur_form.allowed?(:read, @cur_user, site: @cur_site)
+    @items = @cur_form.columns.order_by(order: 1, id: 1)
+
+    render_opts = {}
+    render_opts[:layout] = false if request.xhr?
+    render_opts[:html] = '' if @items.blank?
+    render(render_opts)
+  end
 end
