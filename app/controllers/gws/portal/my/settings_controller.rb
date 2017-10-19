668658a85b0234bdddcd2ccebc710db794dfa8d7
tany <una@kimiyure.com>
2017-10-19 20:34:56 +0900

    [add] gws/portal

---
+++ app/controllers/gws/portal/my/settings_controller.rb
@@ -0,0 +1,35 @@
+class Gws::Portal::My::SettingsController < ApplicationController
+  include Gws::BaseFilter
+  include Gws::CrudFilter
+  include Gws::Portal::PortalFilter
+
+  model Gws::Portal::MySetting
+
+  navi_view 'gws/portal/my/navi'
+  menu_view 'gws/crud/resource_menu'
+
+  before_action :set_portal_setting
+  before_action :save_portal_setting
+
+  private
+
+  def set_crumbs
+    @crumbs << [t("gws/portal.my_portal"), gws_portal_path]
+    @crumbs << [t("gws/portal.links.settings"), action: :show]
+  end
+
+  def fix_params
+    { cur_user: @cur_user, cur_site: @cur_site }
+  end
+
+  def set_item
+    set_portal_setting
+    @item = @portal
+  end
+
+  public
+
+  def show
+    show_setting
+  end
+end
---
+++ app/controllers/gws/portal/setting/groups_controller.rb
@@ -0,0 +1,43 @@
+class Gws::Portal::Setting::GroupsController < ApplicationController
+  include Gws::BaseFilter
+  include Gws::CrudFilter
+
+  model Gws::Group
+
+  navi_view "gws/main/conf_navi"
+
+  private
+
+  def set_crumbs
+    @crumbs << [t("mongoid.models.gws/group"), action: :index]
+  end
+
+  def fix_params
+    { cur_user: @cur_user, cur_site: @cur_site }
+  end
+
+  def set_item
+    super
+    raise "403" unless Gws::Group.site(@cur_site).include?(@item)
+  end
+
+  public
+
+  def index
+    raise "403" unless @model.allowed?(:read, @cur_user, site: @cur_site, node: @cur_node)
+
+    @search_params = params[:s]
+    @search_params = @search_params.except(:state).delete_if { |k, v| v.blank? } if @search_params
+    @search_params = @search_params.presence
+
+    @items = @model.site(@cur_site).
+      state(params.dig(:s, :state)).
+      allow(:read, @cur_user, site: @cur_site)
+
+    if @search_params
+      @items = @items.search(@search_params).page(params[:page]).per(50)
+    else
+      @items = @items.tree_sort
+    end
+  end
+end
