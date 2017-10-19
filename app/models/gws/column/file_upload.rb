46c5d5a7275c2ef1e303c9391778fb541890ef36
NAKANO Hideo <nakano@web-tips.co.jp>
2017-10-19 14:31:02 +0900

    [add] new gws/column framework

---
+++ app/models/gws/column/file_upload.rb
@@ -0,0 +1,24 @@
+class Gws::Column::FileUpload < Gws::Column::Base
+
+  # field :html_tag, type: String
+  # field :html_additional_attr, type: String, default: ''
+
+  # validates :html_tag, inclusion: { in: %w(a+img a img), allow_blank: true }
+  # permit_params :html_tag, :html_additional_attr
+
+  # def html_tag_options
+  #   %w(a+img a img).map do |v|
+  #     [ I18n.t("gws.options.html_tag.#{v}", default: v), v ]
+  #   end
+  # end
+
+  def serialize_value(value)
+    # Gws::Column::Value::FileUpload.new(
+    #   column_id: self.id, name: self.name, order: self.order, html_tag: self.html_tag,
+    #   html_additional_attr: self.html_additional_attr, file_id: value
+    # )
+    Gws::Column::Value::FileUpload.new(
+      column_id: self.id, name: self.name, order: self.order, file_id: value
+    )
+  end
+end
---
+++ app/models/gws/column/text_field.rb
@@ -0,0 +1,33 @@
+class Gws::Column::TextField < Gws::Column::Base
+  include Gws::Addon::Column::TextLike
+
+  field :input_type, type: String
+  permit_params :input_type
+
+  validates :input_type, presence: true, inclusion: { in: %w(text email search tel url), allow_blank: true }
+
+  def input_type_options
+    %w(text email search tel url).map do |v|
+      [ I18n.t("gws.options.column_input_type.#{v}"), v ]
+    end
+  end
+
+  def form_options
+    options = super
+    if input_type == 'date'
+      options['class'] = [ options['class'] ].flatten.compact
+      options['class'] << 'date'
+      options['class'] << 'js-date'
+    elsif input_type.present?
+      options['type'] = input_type
+    end
+    options
+  end
+
+  def serialize_value(value)
+    Gws::Column::Value::TextField.new(
+      column_id: self.id, name: self.name, order: self.order,
+      value: value
+    )
+  end
+end
