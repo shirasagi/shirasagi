dcae49d20c82157b2e160e32861d1c48e1980cc5
NAKANO Hideo <nakano@web-tips.co.jp>
2017-10-23 13:43:51 +0900

    [add] add and remove gws/report/file relation support to gws/schedule/plan

--- app/models/concerns/gws/addon/schedule/reports.rb
+++ app/models/concerns/gws/addon/schedule/reports.rb
@@ -2,7 +2,27 @@ module Gws::Addon::Schedule::Reports
   extend ActiveSupport::Concern
   extend SS::Addon
 
+  included do
+    attr_accessor :in_report_ids, :in_initial_report_ids
+    permit_params in_report_ids: [], in_initial_report_ids: []
+
+    after_save :save_related_reports
+  end
+
   def reports
     Gws::Report::File.site(@cur_site || self.site).schedule(self)
+  end
+
+  private
+
+  def save_related_reports
+    report_ids = Array(in_report_ids)
+    initial_report_ids = Array(in_initial_report_ids)
+
+    # add relation
+    Gws::Report::File.in(id: report_ids - initial_report_ids).add_to_set(schedule_ids: self.id.to_s)
+
+    # remove relation
+    Gws::Report::File.in(id: initial_report_ids - report_ids).pull(schedule_ids: self.id.to_s)
   end
 end
