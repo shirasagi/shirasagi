635dc7898bf712cd27565276aa16cd2051ac55c6
NAKANO Hideo <nakano@web-tips.co.jp>
2017-10-23 20:31:51 +0900

    [add] gws scheme and domain setting

--- app/models/concerns/gws/group_setting/system.rb
+++ app/models/concerns/gws/group_setting/system.rb
@@ -6,8 +6,11 @@ module Gws::GroupSetting::System
 
   included do
     field :sendmail_domains, type: SS::Extensions::Words
+    field :canonical_scheme, type: String, default: 'http'
+    field :canonical_domain, type: String
 
     permit_params :sendmail_domains, allow_email_domains: []
+    permit_params :canonical_scheme, :canonical_domain
   end
 
   def allow_email_domains
@@ -18,5 +21,12 @@ module Gws::GroupSetting::System
     return true if allow_email_domains.blank?
     domain = email.to_s.sub(/^.*@/, '')
     allow_email_domains.include?(domain)
+  end
+
+  def canonical_scheme_options
+    [
+      %w(http http),
+      %w(https https),
+    ]
   end
 end
