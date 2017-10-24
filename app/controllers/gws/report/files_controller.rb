1e5373f46802a27492b3fe355f4bd66edd11ac73
NAKANO Hideo <nakano@web-tips.co.jp>
2017-10-24 11:37:19 +0900

    [add] gws/report/file update support

--- app/controllers/gws/report/files_controller.rb
+++ app/controllers/gws/report/files_controller.rb
@@ -133,53 +133,17 @@ class Gws::Report::FilesController < ApplicationController
     render_create @item.save
   end
 
-  # def edit
-  #   raise '403' unless @item.editable?(@cur_user, site: @cur_site)
-  #   if @item.is_a?(Cms::Addon::EditLock)
-  #     unless @item.acquire_lock
-  #       redirect_to action: :lock
-  #       return
-  #     end
-  #   end
-  #   render
-  # end
-
-  # def update
-  #   @item.attributes = get_params
-  #   @item.in_updated = params[:_updated] if @item.respond_to?(:in_updated)
-  #   if @cur_form.present? && params[:custom].present?
-  #     custom = params.require(:custom)
-  #     new_column_values = @cur_form.build_column_values(custom)
-  #     @item.update_column_values(new_column_values)
-  #   end
-  #   raise '403' unless @item.editable?(@cur_user, site: @cur_site)
-  #   render_update @item.save
-  # end
-
-  # def delete
-  #   raise '403' unless @item.destroyable?(@cur_user, site: @cur_site)
-  #   render
-  # end
-  #
-  # def destroy
-  #   raise '403' unless @item.destroyable?(@cur_user, site: @cur_site)
-  #   render_destroy @item.destroy
-  # end
-  #
-  # def destroy_all
-  #   entries = @items.entries
-  #   @items = []
-  #
-  #   entries.each do |item|
-  #     if item.destroyable?(@cur_user, site: @cur_site)
-  #       next if item.destroy
-  #     else
-  #       item.errors.add :base, :auth_error
-  #     end
-  #     @items << item
-  #   end
-  #   render_destroy_all(entries.size != @items.size)
-  # end
+  def update
+    @item.attributes = get_params
+    @item.in_updated = params[:_updated] if @item.respond_to?(:in_updated)
+    raise '403' unless @item.allowed?(:edit, @cur_user, site: @cur_site)
+    if @cur_form.present? && params[:custom].present?
+      custom = params.require(:custom)
+      new_column_values = @cur_form.build_column_values(custom)
+      @item.update_column_values(new_column_values)
+    end
+    render_update @item.save
+  end
 
   def print
     set_item
