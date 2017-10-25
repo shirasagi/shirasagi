9735ff05b7bb4cc031b5b4ae1aa404103fea5f4e
kkinouchi <kinouchi-kazuhito@jsdnet.co.jp>
2017-10-25 15:58:23 +0900

    [add]Share files folder edit forms

---
+++ app/models/gws/share/folder.rb
@@ -0,0 +1,32 @@
+class Gws::Share::Folder
+  include SS::Document
+  include Gws::Reference::User
+  include Gws::Reference::Site
+  include Gws::Addon::ReadableSetting
+#TODO:include Gws::Addon::GroupPermission
+  include SS::UserPermission
+  include Gws::Addon::File
+  include Gws::Share::DescendantsFileInfo
+
+  store_in collection: :gws_share_folders
+
+  seqid :id
+  field :name, type: String
+  field :order, type: Integer, default: 0
+
+  permit_params :name, :order
+
+  validates :name, presence: true, uniqueness: { scope: :site_id }
+
+  default_scope ->{ order_by order: 1 }
+
+  class << self
+    def search(params)
+      criteria = where({})
+      return criteria if params.blank?
+
+      criteria = criteria.keyword_in params[:keyword], :name if params[:keyword].present?
+      criteria
+    end
+  end
+end
