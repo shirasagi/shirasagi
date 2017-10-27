e633146752933e9efe37e3baf61a2b7374f78d62
tany <una@kimiyure.com>
2017-10-27 16:05:06 +0900

    [modify] gws/faq/group_setting addon

--- app/models/gws/group.rb
+++ app/models/gws/group.rb
@@ -2,6 +2,7 @@ class Gws::Group
   include SS::Model::Group
   include Gws::Referenceable
   include Gws::SitePermission
+  include Gws::Addon::Faq::GroupSetting
   include Gws::Addon::History
 
   set_permission_name "gws_groups", :edit
