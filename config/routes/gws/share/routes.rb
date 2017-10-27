9258b8b7c6e9fd6118d55231f36e8dcd40c4847d
ftakao2007 <ftakao2007@gmail.com>
2017-10-27 22:10:28 +0900

    [add] recover action and views

--- config/routes/gws/share/routes.rb
+++ config/routes/gws/share/routes.rb
@@ -45,6 +45,7 @@ SS::Application.routes.draw do
     namespace "management" do
       resources :files, concerns: [:deletion, :export] do
         get :active, on: :member
+        get :recover, on: :member
         post :active_all, on: :collection
       end
       scope(path: "folder-:folder", as: "folder") do
