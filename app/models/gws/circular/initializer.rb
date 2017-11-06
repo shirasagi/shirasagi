24d7bfc2bf08f5a1811a82bc73a7d7a3b9fd1a97
NAKANO Hideo <nakano@web-tips.co.jp>
2017-11-06 13:05:34 +0900

    [modify] set module_name explicitly

--- app/models/gws/circular/initializer.rb
+++ app/models/gws/circular/initializer.rb
@@ -1,10 +1,10 @@
 module Gws::Circular
   class Initializer
-    Gws::Role.permission :read_other_gws_circular_posts
-    Gws::Role.permission :read_private_gws_circular_posts
-    Gws::Role.permission :edit_other_gws_circular_posts
-    Gws::Role.permission :edit_private_gws_circular_posts
-    Gws::Role.permission :delete_other_gws_circular_posts
-    Gws::Role.permission :delete_private_gws_circular_posts
+    Gws::Role.permission :read_other_gws_circular_posts, module_name: 'gws/circular'
+    Gws::Role.permission :read_private_gws_circular_posts, module_name: 'gws/circular'
+    Gws::Role.permission :edit_other_gws_circular_posts, module_name: 'gws/circular'
+    Gws::Role.permission :edit_private_gws_circular_posts, module_name: 'gws/circular'
+    Gws::Role.permission :delete_other_gws_circular_posts, module_name: 'gws/circular'
+    Gws::Role.permission :delete_private_gws_circular_posts, module_name: 'gws/circular'
   end
 end
