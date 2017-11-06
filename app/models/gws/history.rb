ca2aa6ef4eafe17cea2456aba0921fcc553ce0a0
NAKANO Hideo <nakano@web-tips.co.jp>
2017-11-06 19:05:01 +0900

    [add] session_id and request_id to history

--- app/models/gws/history.rb
+++ app/models/gws/history.rb
@@ -5,6 +5,8 @@ class Gws::History
   include Gws::SitePermission
 
   seqid :id
+  field :session_id, type: String
+  field :request_id, type: String
   field :name, type: String
   field :mode, type: String, default: 'create'
   field :model, type: String
@@ -23,15 +25,21 @@ class Gws::History
   default_scope -> {
     order_by created: -1
   }
-  scope :search, ->(params) {
-    criteria = where({})
-    return criteria if params.blank?
 
-    if params[:keyword].present?
-      criteria = criteria.keyword_in params[:keyword], :name, :model_name, :user_name, :user_group_name
+  class << self
+    def search(params)
+      criteria = all
+      return criteria if params.blank?
+
+      criteria = criteria.search_keyword(params)
+      criteria
     end
-    criteria
-  }
+
+    def search_keyword(params)
+      return all if params[:keyword].blank?
+      all.keyword_in(params[:keyword], :session_id, :request_id, :name, :model_name, :user_name, :user_group_name)
+    end
+  end
 
   def model_name
     self[:model_name] || I18n.t("mongoid.models.#{model}")
