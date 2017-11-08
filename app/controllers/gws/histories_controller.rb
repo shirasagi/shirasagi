50a8a845b0f55e632b2ee513027eab289e30a781
NAKANO Hideo <nakano@web-tips.co.jp>
2017-11-08 14:45:27 +0900

    [add] year and month filter to gws/histories

--- app/controllers/gws/histories_controller.rb
+++ app/controllers/gws/histories_controller.rb
@@ -7,10 +7,24 @@ class Gws::HistoriesController < ApplicationController
   # navi_view "gws/main/conf_navi"
   navi_view 'gws/histories/navi'
 
+  before_action :set_year_month
+
   private
 
   def set_crumbs
     @crumbs << [t("mongoid.models.gws/history"), action: :index]
+  end
+
+  def set_year_month
+    if params[:year].blank?
+      now = Time.zone.now
+      redirect_to gws_monthly_histories_path(year: now.year, month: now.month)
+      return
+    end
+
+    @s = OpenStruct.new(params[:s])
+    @s.year = params[:year]
+    @s.month = params[:month]
   end
 
   public
@@ -19,7 +33,7 @@ class Gws::HistoriesController < ApplicationController
     raise '403' unless Gws::History.allowed?(:read, @cur_user, site: @cur_site)
 
     @items = @model.site(@cur_site).
-      search(params[:s]).
+      search(@s).
       page(params[:page]).per(50)
   end
 end
