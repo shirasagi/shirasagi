6e0b9436c25986b1662e3cad8fde3b309986fe52
ftakao2007 <ftakao2007@gmail.com>
2017-11-08 17:03:18 +0900

    [clean] zip download logic move controller to model

--- app/controllers/gws/share/folders_controller.rb
+++ app/controllers/gws/share/folders_controller.rb
@@ -44,56 +44,16 @@ class Gws::Share::FoldersController < ApplicationController
 
     download_root_dir = "#{Rails.root}/tmp/shirasagi_download"
     download_dir = "#{download_root_dir}" + "/" + "#{@cur_user.id}_#{SecureRandom.hex(4)}"
-
-    Dir.glob("#{download_root_dir}" + "/" + "#{@cur_user.id}_*").each do |tmp_dir|
-      FileUtils.rm_rf(tmp_dir) if File.exists?(tmp_dir)
-    end
-
-    FileUtils.mkdir_p(download_dir) unless FileTest.exist?(download_dir)
+    zipfile = download_dir + "/" + Time.now.strftime("%Y-%m-%d_%H-%M-%S") + ".zip"
 
     filenames = []
     ss_file_items.each {|item| filenames.push(item.name)}
     filename_duplicate_flag = filenames.size == filenames.uniq.size ? 0 : 1
 
-    ss_file_items.each do |item|
-      if  filename_duplicate_flag == 0
-        FileUtils.copy("#{item.path}", "#{download_dir}" + "/" + "#{item.name}") if File.exist?(item.path)
-      elsif filename_duplicate_flag == 1
-        FileUtils.copy("#{item.path}", "#{download_dir}" + "/" + item._id.to_s + "_" + "#{item.name}") if File.exist?(item.path)
-      end
-    end
-
-    @zipfile = download_dir + "/" + Time.now.strftime("%Y-%m-%d_%H-%M-%S") + ".zip"
-
-    Zip::File.open(@zipfile, Zip::File::CREATE) do |zip_file|
-      Dir.glob("#{download_dir}/*").each do |downloadfile|
-        zip_file.add(NKF::nkf('-sx --cp932',File.basename(downloadfile)), downloadfile)
-      end
-    end
-
-    send_file(@zipfile, type: 'application/zip', filename: File.basename(@zipfile), disposition: 'attachment')
-
-    file_body = Class.new do
-      attr_reader :to_path
-
-      def initialize(path)
-        @to_path = path
-      end
-
-      def each
-        File.open(to_path, 'rb') do |file|
-          while chunk = file.read(16384)
-            yield chunk
-          end
-        end
-      end
-
-      def close
-        FileUtils.rm_rf File.dirname(@to_path)
-      end
-    end
-    self.response_body = file_body.new(@zipfile)
-
+    @model.create_temporary_directory(@cur_user.id, download_root_dir, download_dir)
+    @model.create_zip(zipfile, ss_file_items, filename_duplicate_flag)
+    send_file(zipfile, type: 'application/zip', filename: File.basename(zipfile), disposition: 'attachment')
+    self.response_body = @model.delete_temporary_directory(zipfile)
   end
 
 end
