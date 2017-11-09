a451380851b28248f28127325aec1bf34ed5c9a8
NAKANO Hideo <nakano@web-tips.co.jp>
2017-11-09 15:08:15 +0900

    [modify] add text_index to column value and search in text_index

--- app/models/gws/column/date_field.rb
+++ app/models/gws/column/date_field.rb
@@ -1,20 +1,9 @@
 class Gws::Column::DateField < Gws::Column::Base
 
   field :place_holder, type: String
-  # field :html_tag, type: String
-  # field :html_additional_attr, type: String, default: ''
-
-  # validates :html_tag, inclusion: { in: %w(span time), allow_blank: true }
   permit_params :place_holder, :html_tag, :html_additional_attr
 
-  # def html_tag_options
-  #   %w(span time).map do |v|
-  #     [ I18n.t("gws.options.html_tag.#{v}", default: v), v ]
-  #   end
-  # end
-
   def form_options
-    # options = additional_attr_to_h
     options = {}
     options['placeholder'] = place_holder if place_holder.present?
     options['class'] = %w(date js-date)
@@ -22,12 +11,10 @@ class Gws::Column::DateField < Gws::Column::Base
   end
 
   def serialize_value(value)
-    # Gws::Column::Value::DateField.new(
-    #   column_id: self.id, name: self.name, order: self.order, html_tag: self.html_tag,
-    #   html_additional_attr: self.html_additional_attr, date: value
-    # )
-    Gws::Column::Value::DateField.new(
+    ret = Gws::Column::Value::DateField.new(
       column_id: self.id, name: self.name, order: self.order, date: value
     )
+    ret.text_index = ret.value
+    ret
   end
 end
