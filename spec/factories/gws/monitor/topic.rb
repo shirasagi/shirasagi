eba2cd07a5e4a7567410be7ebec02bd23fa64451
ftakao2007 <ftakao2007@gmail.com>
2017-11-10 13:35:41 +0900

    [add] index test when group include or except

--- spec/factories/gws/monitor/topic.rb
+++ spec/factories/gws/monitor/topic.rb
@@ -7,7 +7,57 @@ FactoryGirl.define do
     text { "text-#{unique_id}" }
 
     trait :attend_group_ids do
-      attend_group_ids [1]
+      #attend_group_ids [3]
+      attend_group_ids { gws_user.group_ids }
     end
+
+    trait :attend_group_ids_include_mygroup do
+      attend_group_ids { gws_user.group_ids + [999] }
+      readable_group_ids { gws_user.group_ids + [999] }
+    end
+
+    trait :attend_group_ids_except_mygroup do
+      attend_group_ids [999]
+      readable_group_ids { gws_user.group_ids + [999] }
+    end
+
+    trait :display_only_my_group do
+      spec_config "0"
+    end
+
+    trait :display_all_groups do
+      spec_config "5"
+    end
+
+    trait :state_public do
+      state "public"
+    end
+
+    trait :gws_monitor_topics do
+      attend_group_ids { gws_user.group_ids }
+      readable_group_ids { gws_user.group_ids }
+      state "public"
+    end
+
+    trait :gws_monitor_answers do
+      attend_group_ids { gws_user.group_ids }
+      readable_group_ids { gws_user.group_ids }
+      state_of_the_answers_hash { { gws_user.group_ids.first.to_s => "answered" } }
+      state "public"
+    end
+
+    trait :gws_monitor_admins do
+      attend_group_ids { gws_user.group_ids }
+    end
+
+    trait :gws_monitor_management_topics do
+      attend_group_ids { gws_user.group_ids }
+    end
+
+    trait :gws_monitor_management_trashes do
+      attend_group_ids { gws_user.group_ids }
+      deleted Time.zone.now
+    end
+
   end
 end
