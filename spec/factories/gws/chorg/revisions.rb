52e185bef34c2926d99f500b677141d3549290a6
NAKANO Hideo <nakano@web-tips.co.jp>
2017-11-10 16:14:59 +0900

    [add] gws/chorg/runner specs

---
+++ spec/factories/gws/chorg/revisions.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :gws_revision, class: Gws::Chorg::Revision do
+    name { "組織変更_#{unique_id}" }
+  end
+
+  factory :gws_revision_root_group, parent: :gws_group do
+    name { "組織変更" }
+  end
+
+  factory :gws_revision_new_group, parent: :gws_group do
+    name { "組織変更/グループ#{unique_id}" }
+    ldap_dn { "ou=group,dc=example,dc=jp" }
+  end
+end
