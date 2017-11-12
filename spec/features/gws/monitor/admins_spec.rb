4e253bba8cb768998ea9722eb666d3a45b50891b
ftakao2007 <ftakao2007@gmail.com>
2017-11-13 05:41:37 +0900

    [modify] add monitor controller test

--- spec/features/gws/monitor/admins_spec.rb
+++ spec/features/gws/monitor/admins_spec.rb
@@ -5,6 +5,7 @@ describe "gws_monitor_admins", type: :feature, dbscope: :example do
   let(:item) { create :gws_monitor_topic, :gws_monitor_admins }
   let(:item2) { create :gws_monitor_topic, :gws_monitor_admins_item2 }
   let(:index_path) { gws_monitor_admins_path site, gws_user }
+  let(:new_path) { new_gws_monitor_admin_path site, gws_user }
 
   context "with auth", js: true do
     before { login_gws_user }
@@ -23,87 +24,9 @@ describe "gws_monitor_admins", type: :feature, dbscope: :example do
       expect(page).to have_content('回答状況(1/2)')
     end
 
+    it "#new" do
+      visit new_path
+      wait_for_ajax
+    end
   end
-
-  # it "#popup" do
-  #   visit popup_gws_schedule_todo_path gws_site, item
-  #   expect(page).to have_content(item.name)
-  # end
-  #
-  # it "#finish" do
-  #   visit finish_gws_schedule_todo_path gws_site, item
-  #   wait_for_ajax
-  #   expect(page).to have_content(item.name)
-  # end
-  #
-  # it "#revert" do
-  #   visit revert_gws_schedule_todo_path gws_site, item
-  #   wait_for_ajax
-  #   expect(page).to have_content(item.name)
-  # end
-  #
-  # it "#disable" do
-  #   visit disable_gws_schedule_todo_path gws_site, item
-  #   wait_for_ajax
-  #   expect(page).to have_no_content(item.name)
-  # end
-  #
-  # it "#finish_all" do
-  #   item
-  #   visit gws_schedule_todos_path gws_site
-  #   find('.list-head label.check input').set(true)
-  #   page.accept_confirm do
-  #     find('.finish-all').click
-  #   end
-  #   wait_for_ajax
-  # end
-  #
-  # it "#revert_all" do
-  #   item
-  #   visit gws_schedule_todos_path gws_site
-  #   find('.list-head label.check input').set(true)
-  #   page.accept_confirm do
-  #     find('.revert-all').click
-  #   end
-  #   wait_for_ajax
-  # end
-  #
-  # it "#disable_all" do
-  #   item
-  #   visit gws_schedule_todos_path gws_site
-  #   find('.list-head label.check input').set(true)
-  #   page.accept_confirm do
-  #     find('.disable-all').click
-  #   end
-  #   wait_for_ajax
-  # end
-  #
-  # it "#index" do
-  #   item
-  #   visit gws_schedule_todos_path gws_site
-  #   wait_for_ajax
-  #   expect(page).to have_content(item.name)
-  # end
-  #
-  # # it "#create" do
-  # # end
-  #
-  # it "#new" do
-  #   visit new_gws_schedule_todo_path gws_site
-  #   wait_for_ajax
-  # end
-  #
-  # it "#edit" do
-  #   visit edit_gws_schedule_todo_path gws_site, item
-  #   wait_for_ajax
-  # end
-  #
-  # it "#show" do
-  #   visit gws_schedule_todo_path gws_site, item
-  #   wait_for_ajax
-  #   expect(page).to have_content(item.name)
-  # end
-  #
-  # # it "#update" do
-  # # end
 end
--- spec/features/gws/monitor/management/trashes_spec.rb
+++ spec/features/gws/monitor/management/trashes_spec.rb
@@ -6,6 +6,7 @@ describe "gws_monitor_management_trashes", type: :feature, dbscope: :example do
   let(:item2) { create :gws_monitor_topic, :attend_group_ids }
   let(:item3) { create :gws_monitor_topic, :attend_group_ids, :article_deleted }
   let(:index_path) { gws_monitor_management_trashes_path site, gws_user }
+  let(:new_path) { new_gws_monitor_management_trash_path site, gws_user }
 
   context "with auth", js: true do
     before { login_gws_user }
@@ -30,87 +31,10 @@ describe "gws_monitor_management_trashes", type: :feature, dbscope: :example do
       wait_for_ajax
       expect(page).to have_content(item3.name)
     end
-  end
 
-  # it "#popup" do
-  #   visit popup_gws_schedule_todo_path gws_site, item
-  #   expect(page).to have_content(item.name)
-  # end
-  #
-  # it "#finish" do
-  #   visit finish_gws_schedule_todo_path gws_site, item
-  #   wait_for_ajax
-  #   expect(page).to have_content(item.name)
-  # end
-  #
-  # it "#revert" do
-  #   visit revert_gws_schedule_todo_path gws_site, item
-  #   wait_for_ajax
-  #   expect(page).to have_content(item.name)
-  # end
-  #
-  # it "#disable" do
-  #   visit disable_gws_schedule_todo_path gws_site, item
-  #   wait_for_ajax
-  #   expect(page).to have_no_content(item.name)
-  # end
-  #
-  # it "#finish_all" do
-  #   item
-  #   visit gws_schedule_todos_path gws_site
-  #   find('.list-head label.check input').set(true)
-  #   page.accept_confirm do
-  #     find('.finish-all').click
-  #   end
-  #   wait_for_ajax
-  # end
-  #
-  # it "#revert_all" do
-  #   item
-  #   visit gws_schedule_todos_path gws_site
-  #   find('.list-head label.check input').set(true)
-  #   page.accept_confirm do
-  #     find('.revert-all').click
-  #   end
-  #   wait_for_ajax
-  # end
-  #
-  # it "#disable_all" do
-  #   item
-  #   visit gws_schedule_todos_path gws_site
-  #   find('.list-head label.check input').set(true)
-  #   page.accept_confirm do
-  #     find('.disable-all').click
-  #   end
-  #   wait_for_ajax
-  # end
-  #
-  # it "#index" do
-  #   item
-  #   visit gws_schedule_todos_path gws_site
-  #   wait_for_ajax
-  #   expect(page).to have_content(item.name)
-  # end
-  #
-  # # it "#create" do
-  # # end
-  #
-  # it "#new" do
-  #   visit new_gws_schedule_todo_path gws_site
-  #   wait_for_ajax
-  # end
-  #
-  # it "#edit" do
-  #   visit edit_gws_schedule_todo_path gws_site, item
-  #   wait_for_ajax
-  # end
-  #
-  # it "#show" do
-  #   visit gws_schedule_todo_path gws_site, item
-  #   wait_for_ajax
-  #   expect(page).to have_content(item.name)
-  # end
-  #
-  # # it "#update" do
-  # # end
+    it "#new" do
+      visit new_path
+      wait_for_ajax
+    end
+  end
 end
