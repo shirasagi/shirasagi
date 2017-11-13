0938f2717d586d1298295d20db293374d444ea0a
NAKANO Hideo <nakano@web-tips.co.jp>
2017-11-13 14:13:52 +0900

    [add] edge case

--- spec/jobs/gws/history_archive_job_spec.rb
+++ spec/jobs/gws/history_archive_job_spec.rb
@@ -39,42 +39,100 @@ describe Gws::HistoryArchiveJob, dbscope: :example do
   end
 
   describe '#perform' do
-    let(:now) { Time.zone.parse('2017-11-07T12:00:00+09:00') }
-    let(:threshold_day) { Gws::HistoryArchiveJob.threshold_day(now, site.effective_log_save_days.days) }
+    context 'usual case' do
+      let(:now) { Time.zone.parse('2017-11-07T12:00:00+09:00') }
+      let(:threshold_day) { Gws::HistoryArchiveJob.threshold_day(now, site.effective_log_save_days.days) }
 
-    before do
-      Timecop.freeze(threshold_day - 1.second) do
-        create(:gws_history_model, cur_site: site)
+      before do
+        Timecop.freeze(threshold_day - 1.second) do
+          create(:gws_history_model, cur_site: site)
+        end
+        Timecop.freeze(threshold_day) do
+          create(:gws_history_model, cur_site: site)
+        end
+        Timecop.freeze(threshold_day + 1.second) do
+          create(:gws_history_model, cur_site: site)
+        end
       end
-      Timecop.freeze(threshold_day) do
-        create(:gws_history_model, cur_site: site)
-      end
-      Timecop.freeze(threshold_day + 1.second) do
-        create(:gws_history_model, cur_site: site)
+
+      it do
+        expect(Gws::History.site(site).count).to eq 3
+        Timecop.freeze(now) do
+          described_class.bind(site_id: site).perform_now
+        end
+        expect(Gws::History.site(site).count).to eq 2
+
+        expect(Gws::Job::Log.count).to eq 1
+        Gws::Job::Log.first.tap do |log|
+          expect(log.logs).to include(include('INFO -- : Started Job'))
+          expect(log.logs).to include(include('INFO -- : Completed Job'))
+        end
+
+        expect(Gws::HistoryArchiveFile.site(site).count).to eq 1
+        Gws::HistoryArchiveFile.site(site).first.tap do |archive_file|
+          expect(archive_file.model).to eq 'gws/history_archive_file'
+          expect(archive_file.state).to eq 'closed'
+          expect(archive_file.name).to eq '2017年7月30日〜2017年8月5日'
+          expect(archive_file.filename).to eq '2017-week-30.zip'
+          expect(archive_file.size).to be > 0
+          expect(archive_file.content_type).to eq 'application/zip'
+        end
       end
     end
 
-    it do
-      expect(Gws::History.site(site).count).to eq 3
-      Timecop.freeze(now) do
-        described_class.bind(site_id: site).perform_now
-      end
-      expect(Gws::History.site(site).count).to eq 2
+    context '年をまたぐ場合' do
+      let(:now) { Time.zone.parse('2016-01-01T12:00:00+09:00') + site.effective_log_save_days.days + 7.days }
+      let(:threshold_day) { Gws::HistoryArchiveJob.threshold_day(now, site.effective_log_save_days.days) }
 
-      expect(Gws::Job::Log.count).to eq 1
-      Gws::Job::Log.first.tap do |log|
-        expect(log.logs).to include(include('INFO -- : Started Job'))
-        expect(log.logs).to include(include('INFO -- : Completed Job'))
+      before do
+        # sunday
+        Timecop.freeze('2015-12-27T12:00:00+09:00') { create(:gws_history_model, cur_site: site) }
+        # monday
+        Timecop.freeze('2015-12-28T12:00:00+09:00') { create(:gws_history_model, cur_site: site) }
+        # tuesday
+        Timecop.freeze('2015-12-29T12:00:00+09:00') { create(:gws_history_model, cur_site: site) }
+        # wednesday
+        Timecop.freeze('2015-12-30T12:00:00+09:00') { create(:gws_history_model, cur_site: site) }
+        # thursday
+        Timecop.freeze('2015-12-31T12:00:00+09:00') { create(:gws_history_model, cur_site: site) }
+        # friday
+        Timecop.freeze('2016-01-01T12:00:00+09:00') { create(:gws_history_model, cur_site: site) }
+        # saturday
+        Timecop.freeze('2016-01-02T12:00:00+09:00') { create(:gws_history_model, cur_site: site) }
+        # sunday
+        Timecop.freeze('2016-01-03T12:00:00+09:00') { create(:gws_history_model, cur_site: site) }
       end
 
-      expect(Gws::HistoryArchiveFile.site(site).count).to eq 1
-      Gws::HistoryArchiveFile.site(site).first.tap do |archive_file|
-        expect(archive_file.model).to eq 'gws/history_archive_file'
-        expect(archive_file.state).to eq 'closed'
-        expect(archive_file.name).to eq '2017年7月30日〜2017年8月5日'
-        expect(archive_file.filename).to eq '2017-week-30.zip'
-        expect(archive_file.size).to be > 0
-        expect(archive_file.content_type).to eq 'application/zip'
+      it do
+        expect(Gws::History.site(site).count).to eq 8
+        Timecop.freeze(now) do
+          described_class.bind(site_id: site).perform_now
+        end
+        expect(Gws::History.site(site).count).to eq 1
+
+        expect(Gws::Job::Log.count).to eq 1
+        Gws::Job::Log.first.tap do |log|
+          expect(log.logs).to include(include('INFO -- : Started Job'))
+          expect(log.logs).to include(include('INFO -- : Completed Job'))
+        end
+
+        expect(Gws::HistoryArchiveFile.site(site).count).to eq 2
+        Gws::HistoryArchiveFile.site(site).order_by(id: 1).first.tap do |archive_file|
+          expect(archive_file.model).to eq 'gws/history_archive_file'
+          expect(archive_file.state).to eq 'closed'
+          expect(archive_file.name).to eq '2015年12月27日〜2015年12月31日'
+          expect(archive_file.filename).to eq '2015-week-52.zip'
+          expect(archive_file.size).to be > 0
+          expect(archive_file.content_type).to eq 'application/zip'
+        end
+        Gws::HistoryArchiveFile.site(site).order_by(id: 1).last.tap do |archive_file|
+          expect(archive_file.model).to eq 'gws/history_archive_file'
+          expect(archive_file.state).to eq 'closed'
+          expect(archive_file.name).to eq '2016年1月1日〜2016年1月2日'
+          expect(archive_file.filename).to eq '2016-week-00.zip'
+          expect(archive_file.size).to be > 0
+          expect(archive_file.content_type).to eq 'application/zip'
+        end
       end
     end
   end
