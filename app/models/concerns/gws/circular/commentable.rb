c09b24e9b50b0151e90d580ab1cc8566d293a73a
Yohei Igata <yohei.igata@gmail.com>
2017-11-13 11:30:28 +0900

    [modify] apply comment timezone

--- app/models/concerns/gws/circular/commentable.rb
+++ app/models/concerns/gws/circular/commentable.rb
@@ -39,6 +39,8 @@ module Gws::Circular::Commentable
   def comments
     attributes[:_comments].map.with_index do |comment, idx|
       comment[:id] = idx
+      comment[:created] = comment[:created].localtime
+      comment[:updated] = comment[:updated].localtime
       Gws::Circular::Comment.new comment
     end
   end
