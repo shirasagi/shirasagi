976e4865bfacf00df901ba2a8c92c53292174cdb
jsd_kondo <kondo-yuya@jsdnet.co.jp>
2017-11-15 13:43:23 +0900

    [modify] gws/share remove_zip

--- app/models/gws/share/folder.rb
+++ app/models/gws/share/folder.rb
@@ -30,6 +30,8 @@ class Gws::Share::Folder
   validates :name, presence: true, uniqueness: { scope: :site_id }
   validates :share_max_file_size, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_blank: true }
 
+  after_destroy :remove_zip
+
   default_scope ->{ order_by order: 1 }
 
   class << self
@@ -71,6 +73,10 @@ class Gws::Share::Folder
         end
       end
     end
+  end
+
+  def remove_zip
+    Fs.rm_rf self.zip_path(id) if File.exist?(self.zip_path(id))
   end
 
   private
