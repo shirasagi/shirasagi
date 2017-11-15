e9d0d3ec5249616c363009647d8d9a2bc9736123
ftakao2007 <ftakao2007@gmail.com>
2017-11-15 13:45:37 +0900

    [add] shared_file test base

---
+++ spec/models/gws/share/file_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+RSpec.describe Gws::Share::File, type: :model, dbscope: :example, tmpdir: true do
+  let(:model) { described_class }
+
+  describe "topic" do
+    context "blank params" do
+      subject { Gws::Share::File.new(cur_site: gws_site, cur_user: gws_user).valid? }
+      it { expect(subject).to be_falsey }
+    end
+
+    context "default params" do
+      subject { create(:gws_share_file) }
+      it { expect(subject.errors.size).to eq 0 }
+    end
+
+    context "when in_file is missing" do
+      subject { build(:gws_share_file, in_file: nil).valid? }
+      it { expect(subject).to be_falsey }
+    end
+  end
+end
