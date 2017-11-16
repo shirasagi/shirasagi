089cda8b5d0ce06dcf7c634ffe4288b7d4bed1b4
NAKANO Hideo <nakano@web-tips.co.jp>
2017-11-16 16:10:02 +0900

    [rubocop] C: Space between { and | missing.

--- app/models/concerns/gws/model/file.rb
+++ app/models/concerns/gws/model/file.rb
@@ -253,7 +253,7 @@ module Gws::Model::File
 
   def remove_file
     Fs.rm_rf(path)
-    Dir.glob(path + "_history[0-9]*").each {|file| Fs.rm_rf(file) } if Dir.glob(path + "_history[0-9]*").count > 0
+    Dir.glob(path + "_history[0-9]*").each { |file| Fs.rm_rf(file) } if Dir.glob(path + "_history[0-9]*").count > 0
     remove_public_file
   end
 
