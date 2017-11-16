f8ff5f0eb452ee18f10270dfd340486c8aa9c1dd
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2017-11-16 13:33:22 +0900

    [Add]Import and export cms/role

--- app/controllers/cms/roles_controller.rb
+++ app/controllers/cms/roles_controller.rb
@@ -27,4 +27,34 @@ class Cms::RolesController < ApplicationController
       order_by(name: 1).
       page(params[:page]).per(50)
   end
+
+  def download
+    csv = @model.to_csv(@cur_site).encode("SJIS", invalid: :replace, undef: :replace)
+    filename = @model.to_s.tableize.gsub(/\//, "_")
+    send_data csv, filename: "#{filename}_#{Time.zone.now.to_i}.csv"
+  end
+
+  def import
+    @item = @model.new
+    return if request.get?
+
+    begin
+      file = params[:item].try(:[], :file)
+      raise I18n.t("errors.messages.invalid_csv") if file.nil? || ::File.extname(file.original_filename) != ".csv"
+      CSV.read(file.path, headers: true, encoding: 'SJIS:UTF-8')
+
+      # save csv to use in job
+      ss_file = SS::File.new
+      ss_file.in_file = file
+      ss_file.model = "cms/role"
+      ss_file.save
+
+      # call job
+      Cms::Role::ImportJob.bind(site_id: @cur_site, node_id: @cur_node).perform_later(ss_file.id)
+      flash.now[:notice] = I18n.t("ss.notice.started_import")
+    rescue => e
+      Rails.logger.error("#{e.class} (#{e.message}):\n  #{e.backtrace.join("\n  ")}")
+      @item.errors.add :base, e.to_s
+    end
+  end
 end
