503927a526dd67e10797c8499d7ded5d66358771
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2017-11-17 18:34:33 +0900

    [Modify]Form select in inquiry/column

--- app/models/concerns/inquiry/addon/input_setting.rb
+++ app/models/concerns/inquiry/addon/input_setting.rb
@@ -7,22 +7,26 @@ module Inquiry::Addon
       field :input_type, type: String, default: "text_field"
       field :select_options, type: SS::Extensions::Lines, default: ""
       field :required, type: String, default: "required"
+      field :required_in_select_form, type: Array
       field :additional_attr, type: String, default: ""
       field :input_confirm, type: String, default: ""
       field :max_upload_file_size, type: Integer, default: 0
-      permit_params :input_type, :required, :additional_attr, :select_options, :input_confirm, :max_upload_file_size
+      permit_params :input_type, :required, :additional_attr
+      permit_params :select_options, :input_confirm, :max_upload_file_size
+      permit_params required_in_select_form: []
 
       validates :input_type, presence: true, inclusion: {
-        in: %w(text_field text_area email_field radio_button select check_box upload_file)
+        in: %w(text_field text_area email_field radio_button select check_box upload_file form_select)
       }
       validate :validate_input_type_upload_file
       validate :validate_select_options
       validate :validate_input_confirm_options
       # validate :validate_max_upload_file_size_options
+      validate :validate_form_select
     end
 
     def input_type_options
-      %w(text_field text_area email_field radio_button select check_box upload_file).map do |v|
+      %w(text_field text_area email_field radio_button select check_box upload_file form_select).map do |v|
         label = I18n.t("inquiry.options.input_type.#{v}")
         label += I18n.t("inquiry.cannot_use") if v == "upload_file" && Mongoid::Config.clients[:default_post]
         [ label, v ]
@@ -35,6 +39,7 @@ module Inquiry::Addon
         [I18n.t('inquiry.options.required.optional'), 'optional'],
       ]
     end
+    alias required_in_reply_options required_options
 
     def input_confirm_options
       [
@@ -47,7 +52,8 @@ module Inquiry::Addon
     #   [ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 ]
     # end
 
-    def required?
+    def required?(in_reply)
+      return true if in_reply && required_in_select_form && required_in_select_form.include?(in_reply)
       required == "required"
     end
 
@@ -60,7 +66,7 @@ module Inquiry::Addon
     private
 
     def validate_select_options
-      if input_type =~ /(select|radio_button|check_box)/
+      if input_type =~ /(select|radio_button|check_box|form_select)/
         errors.add :select_options, :blank if select_options.blank?
       end
     end
@@ -81,6 +87,14 @@ module Inquiry::Addon
        if input_type == "upload_file" && Mongoid::Config.clients[:default_post]
          errors.add :input_type, :cannot_use_upload_file
        end
+    end
+
+    def validate_form_select
+      return if input_type != 'form_select'
+      column = node.columns.where(input_type: 'form_select').first
+      if column.present? && column != self
+        errors.add :input_type, :exist_form_select, input_type: label(:input_type)
+      end
     end
   end
 end
