33c2b74876b1f92f773f5553f5b13bac1ca2a6ec
NAKANO Hideo <nakano@web-tips.co.jp>
2017-11-17 14:35:40 +0900

    [modify] set border width 1px

--- app/assets/javascripts/gws/elasticsearch/highlighter.js
+++ app/assets/javascripts/gws/elasticsearch/highlighter.js
@@ -4,7 +4,7 @@ Gws_Elasticsearch_Highlighter = function () {
 Gws_Elasticsearch_Highlighter.prototype = {
   render: function () {
     if (location.hash) {
-      $(location.hash).css('border', '2px solid red');
+      $(location.hash).css('border', '1px solid red');
     }
   }
 }
