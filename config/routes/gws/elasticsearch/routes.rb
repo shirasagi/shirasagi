73588ee390ed005aca27dd03a3704143614e51d0
NAKANO Hideo <nakano@web-tips.co.jp>
2017-11-20 16:38:23 +0900

    [remove] unused controller and route

--- config/routes/gws/elasticsearch/routes.rb
+++ config/routes/gws/elasticsearch/routes.rb
@@ -1,20 +1,10 @@
 SS::Application.routes.draw do
   Gws::Elasticsearch::Initializer
 
-  # concern :deletion do
-  #   get :delete, on: :member
-  #   delete action: :destroy_all, on: :collection
-  # end
-
   gws 'elasticsearch' do
-    resource :setting, only: [:show, :edit, :update]
     namespace :search do
       get '/' => redirect { |p, req| "#{req.path}/all" }, as: :main
       resource :search, path: ':type', only: [:show]
     end
-
-    # namespace "apis" do
-    #   get "categories" => "categories#index"
-    # end
   end
 end
