be963934d02813e7583ed85f72039ee57e96adbc
kkinouchi <kinouchi-kazuhito@jsdnet.co.jp>
2017-11-21 20:55:37 +0900

    [add]Memo file limit setting

--- config/routes/gws/memo/routes.rb
+++ config/routes/gws/memo/routes.rb
@@ -30,5 +30,6 @@ SS::Application.routes.draw do
     resources :folders, concerns: :deletion
     resources :filters, concerns: :deletion
     resources :signatures, concerns: :deletion
+    resource :setting, only: [:show, :edit, :update]
   end
 end
