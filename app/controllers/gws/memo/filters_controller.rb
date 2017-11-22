4405f8ec9585250bdf30df640460cd04b66e503b
NAKANO Hideo <nakano@web-tips.co.jp>
2017-11-22 10:52:49 +0900

    [modify] apply menu label setting to crumb

--- app/controllers/gws/memo/filters_controller.rb
+++ app/controllers/gws/memo/filters_controller.rb
@@ -5,7 +5,7 @@ class Gws::Memo::FiltersController < ApplicationController
   model Gws::Memo::Filter
 
   def set_crumbs
-    @crumbs << [t('mongoid.models.gws/memo/message'), gws_memo_messages_path ]
+    @crumbs << [@cur_site.menu_memo_label || t('mongoid.models.gws/memo/message'), gws_memo_messages_path ]
     @crumbs << [t('mongoid.models.gws/memo/folder'), gws_memo_filters_path ]
   end
 
