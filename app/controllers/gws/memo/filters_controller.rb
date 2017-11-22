5d6898b13f255b555dc8884a42e43a2d33bfac9f
NAKANO Hideo <nakano@web-tips.co.jp>
2017-11-22 10:56:53 +0900

    [fix] gws/memo/filters crumb

--- app/controllers/gws/memo/filters_controller.rb
+++ app/controllers/gws/memo/filters_controller.rb
@@ -6,7 +6,7 @@ class Gws::Memo::FiltersController < ApplicationController
 
   def set_crumbs
     @crumbs << [@cur_site.menu_memo_label || t('mongoid.models.gws/memo/message'), gws_memo_messages_path ]
-    @crumbs << [t('mongoid.models.gws/memo/folder'), gws_memo_filters_path ]
+    @crumbs << [t('mongoid.models.gws/memo/filter'), gws_memo_filters_path ]
   end
 
   def fix_params
