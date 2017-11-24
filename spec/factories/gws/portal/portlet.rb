99f635fbacfedb2d8543cc844e72e7242a979494
tany <una@kimiyure.com>
2017-11-24 21:16:27 +0900

    [add] gws/portal spec

---
+++ spec/factories/gws/portal/portlet.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  trait :gws_portal_portlet_base do
+    cur_site { gws_site }
+    cur_user { gws_user }
+
+    name { "name-#{unique_id}" }
+    limit 1
+  end
+
+  factory :gws_portal_user_portlet, class: Gws::Portal::UserPortlet, traits: [:gws_portal_portlet_base] do
+    portlet_model ''
+  end
+
+  factory :gws_portal_group_portlet, class: Gws::Portal::GroupPortlet, traits: [:gws_portal_portlet_base] do
+    portlet_model ''
+  end
+end
