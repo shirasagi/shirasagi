05b993a50cea274c101c440494da7b0df3e42a3c
ftakao2007 <ftakao2007@gmail.com>
2017-11-29 13:20:48 +0900

    [add] file upload permission all

--- app/models/gws/share/initializer.rb
+++ app/models/gws/share/initializer.rb
@@ -9,6 +9,7 @@ module Gws::Share
     Gws::Role.permission :edit_private_gws_share_files, module_name: 'gws/share'
     Gws::Role.permission :delete_other_gws_share_files, module_name: 'gws/share'
     Gws::Role.permission :delete_private_gws_share_files, module_name: 'gws/share'
+    Gws::Role.permission :write_other_gws_share_files, module_name: 'gws/share'
     Gws::Role.permission :write_private_gws_share_files, module_name: 'gws/share'
 
     Gws::Role.permission :read_other_gws_share_categories, module_name: 'gws/share'
