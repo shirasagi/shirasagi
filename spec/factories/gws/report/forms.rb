452f3521d80360a66b85b03431ffd173bc15975e
NAKANO Hideo <nakano@web-tips.co.jp>
2017-12-01 13:59:58 +0900

    [add] gws/report/file indexer specs

---
+++ spec/factories/gws/report/forms.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :gws_report_form, class: Gws::Report::Form do
+    cur_site { gws_site }
+    cur_user { gws_user }
+
+    name { "name-#{unique_id}" }
+    order { rand(999) }
+    state { %w(public closed).sample }
+  end
+end
