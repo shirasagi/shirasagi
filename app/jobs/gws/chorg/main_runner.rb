395bc4650e6b16c90156eca823bdf2bbe5f235c9
NAKANO Hideo <nakano@web-tips.co.jp>
2017-12-07 19:17:06 +0900

    [add] gws/staff_record creation support

--- app/jobs/gws/chorg/main_runner.rb
+++ app/jobs/gws/chorg/main_runner.rb
@@ -3,4 +3,43 @@ class Gws::Chorg::MainRunner < Gws::Chorg::Runner
   include Job::SS::Binding::Task
 
   self.task_class = Gws::Chorg::Task
+
+  private
+
+  def init_context
+    super
+    create_staff_record
+  end
+
+  def create_staff_record
+    return if @gws_staff_record.blank?
+    # name and code are required
+    return if @gws_staff_record['name'].blank? || @gws_staff_record['code'].blank?
+
+    task.log("==電子職員録==")
+
+    year = create_staff_record_year
+    if year.errors.present?
+      task.log("  次のエラーが発生しました。\n#{year.errors.full_messages.join("\n")}")
+      return
+    end
+
+    job = Gws::StaffRecord::CopySituationJob.bind(site_id: site, user_id: user)
+    job.perform_now(year.id.to_s)
+    task.log("  #{year.name_with_code}: 電子職員録を作成しました。")
+  end
+
+  def create_staff_record_year
+    close_date = Time.zone.now.end_of_month
+    start_date = close_date - 1.year + 1.day
+    start_date = start_date.beginning_of_month
+    ret = Gws::StaffRecord::Year.new(
+      cur_site: site, cur_user: user,
+      name: @gws_staff_record['name'], code: @gws_staff_record['code'],
+      start_date: start_date, close_date: close_date
+    )
+
+    ret.save
+    ret
+  end
 end
---
+++ spec/jobs/gws/chorg/main_runner/staff_record_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Gws::Chorg::MainRunner, dbscope: :example do
+  let(:site) { create(:gws_group) }
+  let(:task) { Gws::Chorg::Task.create!(name: unique_id, group_id: site) }
+  let(:staff_record_name) { unique_id }
+  let(:staff_record_code) { (2010 + rand(10)).to_s }
+  let(:job_opts) { { 'gws_staff_record' => { 'name' => staff_record_name, 'code' => staff_record_code } } }
+
+  context 'with staff record creation' do
+    let(:revision) { create(:gws_revision, site_id: site.id) }
+    let(:changeset) { create(:gws_add_changeset, revision_id: revision.id) }
+
+    it do
+      expect(changeset).not_to be_nil
+
+      # execute
+      job = described_class.bind(site_id: site, task_id: task)
+      expect { job.perform_now(revision.name, job_opts) }.not_to raise_error
+
+      # check for job was succeeded
+      expect(Gws::Job::Log.count).to eq 2
+      Gws::Job::Log.all.each do |log|
+        expect(log.logs).to include(include('INFO -- : Started Job'))
+        expect(log.logs).to include(include('INFO -- : Completed Job'))
+      end
+
+      expect(Gws::StaffRecord::Year.site(site).count).to eq 1
+      Gws::StaffRecord::Year.site(site).find_by(name: staff_record_name).tap do |y|
+        expect(y.name).to eq staff_record_name
+        expect(y.code).to eq staff_record_code
+      end
+    end
+  end
+end
