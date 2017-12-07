7e7feab83dde5512b79332edb685bd6e7632e160
jsd_kondo <kondo-yuya@jsdnet.co.jp>
2017-12-07 20:23:05 +0900

    [modify] forward setting display

--- app/controllers/gws/memo/forwards_controller.rb
+++ app/controllers/gws/memo/forwards_controller.rb
@@ -5,11 +5,18 @@ class Gws::Memo::ForwardsController < ApplicationController
 
   model Gws::Memo::Forward
 
+  before_action :set_item, only: [:show, :edit, :update]
+
   private
 
-  # def set_item
-  #   @item = @cur_site
-  # end
+   def set_item
+     if @model.user(@cur_user).site(@cur_site).present?
+       @item = @model.find_by(user_id: @cur_user.id, site_id: @cur_site.id)
+     else
+       @item = @model.new(user_id: @cur_user.id, site_id: @cur_site.id)
+       @item.save
+     end
+   end
 
   def set_crumbs
     @crumbs << [@cur_site.menu_memo_label || t('mongoid.models.gws/memo/message'), gws_memo_messages_path ]
@@ -19,13 +26,18 @@ class Gws::Memo::ForwardsController < ApplicationController
 
   public
 
-  # def show
-  #   raise "403" unless @item.allowed?(:read, @cur_user, site: @cur_site)
-  #   super
-  # end
-
-  def fix_params
-    { cur_user: @cur_user, cur_site: @cur_site }
+  def show
+    raise "403" unless @item.allowed?(:read, @cur_user, site: @cur_site)
+    super
   end
 
+  def edit
+    raise "403" unless @item.allowed?(:edit, @cur_user, site: @cur_site)
+    super
+  end
+
+  def update
+    raise "403" unless @item.allowed?(:edit, @cur_user, site: @cur_site)
+    super
+  end
 end
