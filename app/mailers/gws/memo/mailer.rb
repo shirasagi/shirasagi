8f8cff48209cd70cea87f4663364acf56d8e6c96
ftakao2007 <ftakao2007@gmail.com>
2017-12-07 12:59:49 +0900

    [modify] forward template and add cur_site

--- app/mailers/gws/memo/mailer.rb
+++ app/mailers/gws/memo/mailer.rb
@@ -1,7 +1,18 @@
 class Gws::Memo::Mailer < ActionMailer::Base
-  def forward_mail(item, user)
+  def forward_mail(item, user, site)
     @item = item
     @cur_user = user
+    @cur_site = site
+    @cc_memo = []
+    @item.member_ids.each do |member_id|
+      @cc_memo << Gws::User.site(@cur_site).where(id: member_id).pluck(:name, :email).first
+    end
+    @cc_memo.map do |item|
+      item[1] = "<#{item[1]}>"
+      item[0] = item.join(" ")
+      item.delete_at(1)
+    end
+    @cc_memo = @cc_memo.flatten.join(", ")
     from = "noreply@example.com"
     to = "jouhou_tesuto_11@dockernet.co.lg.jp"
     subject = "[メッセージ]送信者:#{@cur_user.name}"
