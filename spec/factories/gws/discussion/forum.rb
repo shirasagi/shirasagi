aecc14f6708b1c0d35f9ac00789fa651149e8efa
itowtips <ito@web-tips.co.jp>
2017-12-08 16:10:59 +0900

    [add] basic crud spec

---
+++ spec/factories/gws/discussion/forum.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :gws_discussion_forum, class: Gws::Discussion::Forum do
+    cur_site { gws_site }
+    cur_user { gws_user }
+
+    name { "name-#{unique_id}" }
+  end
+end
---
+++ spec/factories/gws/discussion/topic.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :gws_discussion_topic, class: Gws::Discussion::Topic do
+    cur_site { gws_site }
+    cur_user { gws_user }
+
+    name { "name-#{unique_id}" }
+    text { "text-#{unique_id}" }
+
+    association :parent, factory: :gws_discussion_forum
+    forum { parent }
+  end
+end
