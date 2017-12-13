ed8a1e57aa8e2bd2b47ffb05b3166d4c401d4906
kkinouchi <kinouchi-kazuhito@jsdnet.co.jp>
2017-12-13 16:23:38 +0900

    [add]sort by ss parts

--- app/controllers/gws/schedule/todos_controller.rb
+++ app/controllers/gws/schedule/todos_controller.rb
@@ -36,7 +36,9 @@ class Gws::Schedule::TodosController < ApplicationController
   def index
     @items = @model.site(@cur_site).
       allow(:read, @cur_user, site: @cur_site).active.
-      search(params[:s]).page(params[:page]).per(50)
+      search(params[:s]).
+      custom_order(params.dig(:s, :sort) || 'created_desc').
+      page(params[:page]).per(50)
   end
 
   def create
