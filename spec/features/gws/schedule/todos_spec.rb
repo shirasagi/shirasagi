eaab099d86eb27558b8cd1109c98e2faac2bc4a3
ftakao2007 <ftakao2007@gmail.com>
2017-12-14 19:57:15 +0900

    [add] group plans member links check test

--- spec/features/gws/schedule/todos_spec.rb
+++ spec/features/gws/schedule/todos_spec.rb
@@ -2,8 +2,22 @@ require 'spec_helper'
 
 describe "gws_schedule_todos", type: :feature, dbscope: :example, js: true do
   let(:item) { create :gws_schedule_todo }
+  let(:group_index_path)  { gws_schedule_group_plans_path gws_site, gws_user.groups.first }
 
   before { login_gws_user }
+
+  it "group_plans_member_link_check" do
+    item
+    visit group_index_path
+    wait_for_ajax
+    within("div#cal-#{gws_user.id}.calendar.multiple.fc.fc-ltr.fc-unthemed") do
+      expect(page).to have_content(item.name)
+    end
+    within("div#cal-#{sys_user.id}.calendar.multiple.fc.fc-ltr.fc-unthemed") do
+      expect(page).to have_no_content(item.name)
+    end
+    expect(current_path).not_to eq sns_login_path
+  end
 
   it "#finish" do
     visit finish_gws_schedule_todo_path gws_site, item
