c06a7dce1cefcb5f554eca81a8aa453ee72a4be7
tany <tanizawa@web-tips.co.jp>
2017-12-19 22:52:41 +0900

    [modify] gws/portal: default portlet settings (#1825)

--- spec/features/gws/portal/user_portal_spec.rb
+++ spec/features/gws/portal/user_portal_spec.rb
@@ -4,8 +4,14 @@ describe "gws_portal_user_portal", type: :feature, dbscope: :example do
   let(:site) { gws_site }
   let(:user) { create :gws_user }
   let(:index_path) { gws_portal_user_path(site: site, user: user) }
+  let(:portal) { user.find_portal_setting(cur_user: user, cur_site: site).tap(&:save) }
+  let(:portlets) do
+    Gws::Portal::UserPortlet.new.portlet_model_options.map do |label, val|
+      create(:gws_portal_user_portlet, setting_id: portal.id, portlet_model: val)
+    end
+  end
 
-  context "with auth" do
+  context "without permission" do
     before do
       login_gws_user
 
@@ -15,7 +21,7 @@ describe "gws_portal_user_portal", type: :feature, dbscope: :example do
       role.update_attributes(permissions: permissions)
     end
 
-    it "#index" do
+    it "#index 403" do
       visit index_path
       expect(current_path).to eq index_path
 
@@ -27,6 +33,40 @@ describe "gws_portal_user_portal", type: :feature, dbscope: :example do
 
       visit gws_portal_user_settings_path(site: site, user: user)
       expect(page).not_to have_css('.gws')
+    end
+  end
+
+  context "with auth" do
+    before { login_gws_user }
+
+    it "#index" do
+      # default portlets
+      visit index_path
+      SS.config.gws['portal']['user_portlets'].each do |data|
+        expect(page).to have_css(".portlet-model-#{data['model']}")
+      end
+
+      # all portlets
+      portlets
+      visit index_path
+      portlets.each do |portlet|
+        expect(page).to have_css(".#{portlet.portlet_model_class}")
+      end
+
+      first('.nav-management-menu a').click
+      expect(current_path).to eq gws_portal_user_layouts_path(site: site, user: user)
+
+      first('a', text: I18n.t("gws/portal.links.manage_portlets")).click
+      expect(current_path).to eq gws_portal_user_portlets_path(site: site, user: user)
+
+      first('a', text: I18n.t("gws/portal.links.settings")).click
+      expect(current_path).to eq gws_portal_user_settings_path(site: site, user: user)
+
+      first('#menu a', text: I18n.t("ss.links.edit")).click
+      expect(current_path).to eq edit_gws_portal_user_settings_path(site: site, user: user)
+
+      click_button I18n.t('ss.buttons.save')
+      expect(current_path).to eq gws_portal_user_settings_path(site: site, user: user)
     end
   end
 end
