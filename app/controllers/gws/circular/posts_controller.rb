b90fd78b59b9fdbca4b37032e547223d4b029b5c
NAKANO Hideo <nakano@web-tips.co.jp>
2017-12-19 18:00:47 +0900

    [modify] reduce duplicated codes

--- app/controllers/gws/circular/posts_controller.rb
+++ app/controllers/gws/circular/posts_controller.rb
@@ -1,49 +1,11 @@
 class Gws::Circular::PostsController < ApplicationController
   include Gws::BaseFilter
   include Gws::CrudFilter
-
-  model Gws::Circular::Post
-
-  before_action :set_item, only: [:show, :edit, :update, :delete, :destroy, :set_seen, :unset_seen, :toggle_seen]
-  before_action :set_selected_items, only: [:destroy_all, :set_seen_all, :unset_seen_all,]
-  before_action :set_category
+  include Gws::Circular::PostFilter
 
   private
 
-  def fix_params
-    { cur_user: @cur_user, cur_site: @cur_site }
-  end
-
-  def pre_params
-    { due_date: Time.zone.now + @cur_site.circular_default_due_date.day }
-  end
-
-  def set_crumbs
-    set_category
-    if @category.present?
-      @crumbs << [@cur_site.menu_circular_label || I18n.t('modules.gws/circular'), gws_circular_posts_path]
-      @crumbs << [@category.name, action: :index]
-    else
-      @crumbs << [@cur_site.menu_circular_label || I18n.t('modules.gws/circular'), action: :index]
-    end
-  end
-
-  def set_category
-    @categories ||= Gws::Circular::Category.site(@cur_site).readable(@cur_user, @cur_site).tree_sort
-    if category_id = params[:category].presence
-      @category ||= Gws::Circular::Category.site(@cur_site).readable(@cur_user, @cur_site).where(id: category_id).first
-    end
-  end
-
-  public
-
-  def index
-    if @category.present?
-      params[:s] ||= {}
-      params[:s][:site] = @cur_site
-      params[:s][:category_id] = @category.id
-    end
-
+  def set_items
     @items = @model.site(@cur_site).
       topic.
       without_deleted.
@@ -53,33 +15,7 @@ class Gws::Circular::PostsController < ApplicationController
       page(params[:page]).per(50)
   end
 
-  def create
-    @item = @model.new get_params
-    if params[:commit] == t("ss.buttons.draft_save")
-      @item.state = 'draft'
-    elsif params[:commit] == t("ss.buttons.publish_save")
-      @item.state = 'public'
-    end
-    raise "403" unless @item.allowed?(:edit, @cur_user, site: @cur_site)
-    render_create @item.save
-  end
-
-  def update
-    @item.attributes = get_params
-    @item.in_updated = params[:_updated] if @item.respond_to?(:in_updated)
-    if params[:commit] == t("ss.buttons.draft_save")
-      @item.state = 'draft'
-    elsif params[:commit] == t("ss.buttons.publish_save")
-      @item.state = 'public'
-    end
-    raise '403' unless @item.allowed?(:edit, @cur_user, site: @cur_site)
-    render_update @item.update
-  end
-
-  def disable
-    raise '403' unless @item.allowed?(:delete, @cur_user, site: @cur_site)
-    render_destroy @item.disable, {notice: t('gws/circular.notice.disable')}
-  end
+  public
 
   def show
     raise '404' if !@item.public? || @item.active?
@@ -88,35 +24,5 @@ class Gws::Circular::PostsController < ApplicationController
       @item.set_seen(@cur_user).save
     end
     render
-  end
-
-  def set_seen
-    raise '404' if !@item.public? || @item.active?
-    raise '403' unless @item.member?(@cur_user)
-    raise '404' if @item.seen?(@cur_user)
-    render_update @item.set_seen(@cur_user).update
-  end
-
-  def unset_seen
-    raise '404' if !@item.public? || @item.active?
-    raise '403' unless @item.member?(@cur_user)
-    raise '404' if @item.unseen?(@cur_user)
-    render_update @item.unset_seen(@cur_user).update
-  end
-
-  def toggle_seen
-    raise '404' if !@item.public? || @item.active?
-    raise '403' unless @item.member?(@cur_user)
-    render_update @item.toggle_seen(@cur_user).update
-  end
-
-  def set_seen_all
-    @items.each{ |item| item.set_seen(@cur_user).save if item.unseen?(@cur_user) }
-    render_destroy_all(false)
-  end
-
-  def unset_seen_all
-    @items.each{ |item| item.unset_seen(@cur_user).save if item.seen?(@cur_user) }
-    render_destroy_all(false)
   end
 end
--- app/controllers/gws/circular/trashes_controller.rb
+++ app/controllers/gws/circular/trashes_controller.rb
@@ -1,8 +1,7 @@
 class Gws::Circular::TrashesController < ApplicationController
   include Gws::BaseFilter
   include Gws::CrudFilter
-
-  model Gws::Circular::Post
+  include Gws::Circular::PostFilter
 
   before_action :set_item, only: [:show, :delete, :destroy, :active, :recover]
   before_action :set_selected_items, only: [:active_all, :destroy_all]
@@ -10,73 +9,12 @@ class Gws::Circular::TrashesController < ApplicationController
 
   private
 
-  def fix_params
-    { cur_user: @cur_user, cur_site: @cur_site }
-  end
-
-  def set_crumbs
-    set_category
-    @crumbs << [I18n.t('modules.gws/circular'), gws_circular_posts_path]
-    if @category.present?
-      @crumbs << [@category.name, action: :index]
-    end
-  end
-
-  def set_category
-    @categories = Gws::Circular::Category.site(@cur_site).readable(@cur_user, @cur_site).tree_sort
-    if category_id = params[:category].presence
-      @category ||= Gws::Circular::Category.site(@cur_site).readable(@cur_user, @cur_site).where(id: category_id).first
-    end
-  end
-
-  public
-
-  def index
-    if @category.present?
-      params[:s] ||= {}
-      params[:s][:site] = @cur_site
-      params[:s][:category_id] = @category.id
-    end
-
-    @items = @model.site(@cur_site).
+  def set_items
+    @items ||= @model.site(@cur_site).
       topic.
       only_deleted.
       allow(:read, @cur_user, site: @cur_site).
       search(params[:s]).
       page(params[:page]).per(50)
-  end
-
-  def active
-    raise '403' unless @item.allowed?(:delete, @cur_user, site: @cur_site)
-
-    if request.get?
-      render
-      return
-    end
-
-    render_destroy @item.active, {notice: t('gws/circular.notice.active')}
-  end
-
-  def active_all
-    entries = @items.entries
-    @items = []
-
-    entries.each do |item|
-      if item.allowed?(:delete, @cur_user, site: @cur_site)
-        next if item.active
-      else
-        item.errors.add :base, :auth_error
-      end
-      @items << item
-    end
-
-    location = crud_redirect_url || { action: :index }
-    notice = { notice: t('gws/circular.notice.active') }
-    errors = @items.map { |item| [item.id, item.errors.full_messages] }
-
-    respond_to do |format|
-      format.html { redirect_to location, notice }
-      format.json { head json: errors }
-    end
   end
 end
