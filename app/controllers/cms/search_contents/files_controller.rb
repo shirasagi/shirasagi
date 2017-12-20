69d8867adcd2a455368f14b1becdc26414611b26
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2017-12-20 11:07:18 +0900

    [Add]search content files (#1737)
    * [Add]search content files
    
    * [Modify]Use pluck
    
    * [Remove]unnecessary sort

---
+++ app/controllers/cms/search_contents/files_controller.rb
@@ -0,0 +1,30 @@
+class Cms::SearchContents::FilesController < ApplicationController
+  include Cms::BaseFilter
+  include Cms::ApiFilter::Contents
+  include SS::FileFilter
+
+  model SS::File
+
+  navi_view "cms/search_contents/navi"
+
+  private
+
+  def set_crumbs
+    @crumbs << [t("cms.file"), action: :index]
+  end
+
+  def fix_params
+    { cur_user: @cur_user, cur_site: @cur_site }
+  end
+
+  public
+
+  def index
+    file_ids = Cms::Page.site(@cur_site).pluck(:file_ids).flatten.uniq.compact
+    @items = @model.where(site_id: @cur_site).
+      search(params[:s]).
+      in(id: file_ids).
+      order_by(filename: 1).
+      page(params[:page]).per(50)
+  end
+end
