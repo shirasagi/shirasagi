bca3530cc4c69640a78ffe161c33d32ed915a113
NAKANO Hideo <nakano@web-tips.co.jp>
2017-12-22 11:59:04 +0900

    [add] gws/workflow

--- db/seeds/gws/contents.rb
+++ db/seeds/gws/contents.rb
@@ -579,7 +579,7 @@ end
   create_report_form(name: '出張報告書', order: 20, state: 'public', memo: '出張報告書です。', category_ids: [@rep_cate[1].id])
 ]
 
-@rep_form1_cols = [
+@rep_form0_cols = [
   create_column(:text, name: '打ち合わせ場所', order: 10, required: 'required', tooltips: '打ち合わせ場所をん入力してください。', input_type: 'text', form: @rep_forms[0]),
   create_column(:date, name: '打ち合わせ日', order: 20, required: 'required', tooltips: '打ち合わせ日を入力してください。', form: @rep_forms[0]),
   create_column(:text, name: '打ち合わせ時間', order: 30, required: 'required', tooltips: '打ち合わせ時間を入力してください。', input_type: 'text', form: @rep_forms[0]),
@@ -588,7 +588,7 @@ end
   create_column(:file_upload, name: '添付ファイル', order: 60, required: 'optional', tooltips: '関連資料があればファイルをアップロードしてください。', upload_file_count: 5, form: @rep_forms[0])
 ]
 
-@rep_form2_cols = [
+@rep_form1_cols = [
   create_column(:text, name: '出張先', order: 10, required: 'required', input_type: 'text', form: @rep_forms[1]),
   create_column(:date, name: '出張日', order: 20, required: 'required', form: @rep_forms[1]),
   create_column(:text_area, name: '報告内容', order: 30, required: 'required', form: @rep_forms[1])
@@ -602,27 +602,27 @@ def create_report_file(data)
 end
 
 create_report_file(
-  name: '第1回シラサギ会議打ち合わせ議事録', state: 'public',
+  cur_form: @rep_forms[0], name: '第1回シラサギ会議打ち合わせ議事録', state: 'public',
   member_ids: %w(admin user1 user3).map { |u| u(u).id }, schedule_ids: [@sch_plan1.id.to_s],
   readable_setting_range: 'select', readable_group_ids: %w[政策課 広報課].map { |n| g("シラサギ市/企画政策部/#{n}").id },
   column_values: [
-    @rep_form1_cols[0].serialize_value('会議室101'),
-    @rep_form1_cols[1].serialize_value((@today - 7.days).strftime('%Y/%m/%d')),
-    @rep_form1_cols[2].serialize_value('15:00〜16:00'),
-    @rep_form1_cols[3].serialize_value("広報課　斎藤課長\n政策課　白鷺係長"),
-    @rep_form1_cols[4].serialize_value("シラサギプロジェクトについての会議を行った。\nかれこれしかじか"),
-    @rep_form1_cols[5].serialize_value([])
+    @rep_form0_cols[0].serialize_value('会議室101'),
+    @rep_form0_cols[1].serialize_value((@today - 7.days).strftime('%Y/%m/%d')),
+    @rep_form0_cols[2].serialize_value('15:00〜16:00'),
+    @rep_form0_cols[3].serialize_value("広報課　斎藤課長\n政策課　白鷺係長"),
+    @rep_form0_cols[4].serialize_value("シラサギプロジェクトについての会議を行った。\nかれこれしかじか"),
+    @rep_form0_cols[5].serialize_value([])
   ]
 )
 
 create_report_file(
-  cur_user: u('user1'), name: '東京出張報告', state: 'public',
+  cur_user: u('user1'), cur_form: @rep_forms[1], name: '東京出張報告', state: 'public',
   member_ids: @users.map(&:id), schedule_ids: [@sch_plan2.id.to_s],
   readable_setting_range: 'select', readable_group_ids: [g('シラサギ市/企画政策部/政策課').id],
   column_values: [
-    @rep_form2_cols[0].serialize_value('東京都庁'),
-    @rep_form2_cols[1].serialize_value((@today - 3.days).strftime('%Y/%m/%d')),
-    @rep_form2_cols[2].serialize_value("東京都庁で会議のため、出張しました。\nかれこれしかじか。")
+    @rep_form1_cols[0].serialize_value('東京都庁'),
+    @rep_form1_cols[1].serialize_value((@today - 3.days).strftime('%Y/%m/%d')),
+    @rep_form1_cols[2].serialize_value("東京都庁で会議のため、出張しました。\nかれこれしかじか。")
   ]
 )
 
@@ -749,6 +749,73 @@ u('admin').add_to_set(title_ids: [user_titles[0].id])
 u('user2').add_to_set(title_ids: [user_titles[3].id])
 u('user3').add_to_set(title_ids: [user_titles[1].id])
 u('user4').add_to_set(title_ids: [user_titles[1].id])
+
+## -------------------------------------
+puts "# workflow/form"
+
+def create_workflow_form(data)
+  create_item(Gws::Workflow::Form, data)
+end
+
+@wf_forms = [
+  create_workflow_form(name: '出張申請', order: 10, state: 'public', memo: '出張申請です。'),
+  create_workflow_form(name: '稟議書', order: 20, state: 'public', memo: '稟議書です。')
+]
+
+@wf_form0_cols = [
+  create_column(:text, form: @wf_forms[0], name: '出張期間', order: 10, required: 'required', tooltips: '出張期間を入力してください。', input_type: 'text'),
+  create_column(:text, form: @wf_forms[0], name: '出張先', order: 20, required: 'required', input_type: 'text'),
+  create_column(:text, form: @wf_forms[0], name: '目的', order: 30, required: 'required', input_type: 'text'),
+  create_column(:number, form: @wf_forms[0], name: '必要経費', order: 40, required: 'optional', postfix_label: '円', minus_type: 'normal'),
+  create_column(:text_area, form: @wf_forms[0], name: '詳細', order: 50, required: 'required'),
+]
+
+@wf_form1_cols = [
+  create_column(:text_area, form: @wf_forms[1], name: '起案内容', order: 10, required: 'required', tooltips: '起案内容の詳細説明を入力してください。'),
+  create_column(:text, form: @wf_forms[1], name: '時期', order: 20, required: 'optional', tooltips: '購入、採用時期がある場合は入力してください。', input_type: 'text'),
+  create_column(:text, form: @wf_forms[1], name: '委託行者', order: 30, required: 'optional', tooltips: '購入、採用時期がある場合は入力してください。', input_type: 'text'),
+  create_column(:number, form: @wf_forms[1], name: '金額', order: 40, required: 'optional', postfix_label: '円', minus_type: 'normal'),
+]
+
+## -------------------------------------
+puts "# workflow/file"
+
+def create_workflow_file(data)
+  create_item(Gws::Workflow::File, data)
+end
+
+create_workflow_file(
+  cur_user: u('user1'), cur_form: @wf_forms[0], name: '東京出張申請', state: 'closed',
+  readable_setting_range: 'select', readable_group_ids: %w[政策課].map { |n| g("シラサギ市/企画政策部/#{n}").id },
+  workflow_user_id: u('user1').id, workflow_state: 'request', workflow_comment: '東京出張を申請します。',
+  workflow_approvers: [
+    { 'level' => 1, 'user_id' => u('admin').id, 'editable' => '', 'state' => 'request', 'comment' => ''},
+    { 'level' => 2, 'user_id' => u('sys').id, 'editable' => '', 'state' => 'pending', 'comment' => ''}
+  ], workflow_required_counts: [false, false],
+  column_values: [
+    @wf_form0_cols[0].serialize_value('2017/12/14~2017/12/15'),
+    @wf_form0_cols[1].serialize_value('東京都庁'),
+    @wf_form0_cols[2].serialize_value('業務会議のため'),
+    @wf_form0_cols[3].serialize_value('50000'),
+    @wf_form0_cols[4].serialize_value("会議のため、東京都庁に出張します。\r\n飛行機での移動となります。"),
+  ]
+)
+
+create_workflow_file(
+  cur_user: u('user5'), cur_form: @wf_forms[1], name: 'パソコンの購入', state: 'closed',
+  readable_setting_range: 'select', readable_group_ids: %w[広報課].map { |n| g("シラサギ市/企画政策部/#{n}").id },
+  workflow_user_id: u('user5').id, workflow_state: 'request', workflow_comment: 'パソコン購入の稟議です。',
+  workflow_approvers: [
+    { 'level' => 1, 'user_id' => u('user3').id, 'editable' => '', 'state' => 'request', 'comment' => ''},
+    { 'level' => 2, 'user_id' => u('sys').id, 'editable' => '', 'state' => 'pending', 'comment' => ''}
+  ], workflow_required_counts: [false, false],
+  column_values: [
+    @wf_form1_cols[0].serialize_value('サポート期間が切れるため、新たなパソコン買い替えを行いたいと思います。'),
+    @wf_form1_cols[1].serialize_value('2018年1月'),
+    @wf_form1_cols[2].serialize_value('株式会社シラサギ'),
+    @wf_form1_cols[3].serialize_value('100000'),
+  ]
+)
 
 ## -------------------------------------
 puts "# max file size"
