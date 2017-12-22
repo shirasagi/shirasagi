447ef41c109ea1e591ddfd6ddf8d14c127a552ff
NAKANO Hideo <nakano@web-tips.co.jp>
2017-12-22 21:01:49 +0900

    [fix] gws/monitor/post due_date

--- db/seeds/gws/contents.rb
+++ db/seeds/gws/contents.rb
@@ -513,8 +513,8 @@ def create_monitor_post(data)
 end
 
 create_monitor_post(
-  cur_user: u('admin'), name: 'Re: 新しい公用車の導入',
-  due_date: Time.zone.now.beginning_of_day + 7.days,
+  cur_user: u('admin'), name: 'Re: 新しい公用車の導入', state: 'public',
+  due_date: @mon_topics[1].due_date,
   topic_id: @mon_topics[1].id, parent_id: @mon_topics[1].id,
   text: '車室の広いものを希望します。'
 )
