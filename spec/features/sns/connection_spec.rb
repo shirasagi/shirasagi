3aba939afb242cee9d15edca0fc5dde5c05eebe7
tany <tanizawa@web-tips.co.jp>
2018-01-15 17:06:16 +0900

    [modify] sys/conf (#1877)
    * [modify] css: .main-navi-md
    
    * [modify] spec: sys/conf, sns/connection

--- spec/features/sns/connection_spec.rb
+++ spec/features/sns/connection_spec.rb
@@ -1,163 +1,18 @@
 require 'spec_helper'
 
 describe "sns_connection", type: :feature, dbscope: :example do
-  context "with user which is permitted all" do
-    before do
-      role = create(:sys_role_admin, name: unique_id)
-      user = create(:sys_user_sample, sys_role_ids: [role.id])
-      login_user user
-    end
+  let(:index_path) { sns_connection_path }
+
+  context "with auth" do
+    before { login_sys_user }
 
     it "#index" do
-      visit sns_mypage_path
+      visit index_path
       click_on I18n.t('sns.connection')
-      expect(current_path).to eq sns_connection_path
-      within 'nav.mod-navi' do
-        expect(page).to have_css('h2', text: 'システム設定')
-        expect(page).to have_css('h3 a', text: 'サイト')
-        expect(page).to have_css('h3 a', text: 'グループ')
-        expect(page).to have_css('h3 a', text: 'ユーザー')
-        expect(page).to have_css('h3 a', text: '権限/ロール')
-        expect(page).to have_css('h3 a', text: '認証')
-        expect(page).to have_css('h3 a', text: '最大ファイルサイズ')
-        expect(page).to have_css('h3 a', text: 'サイト複製')
-        expect(page).to have_css('h3 a', text: 'テスト')
-        expect(page).to have_css('h3 a', text: '操作履歴')
-        expect(page).to have_css('h3 a', text: 'ジョブ')
-      end
-    end
-  end
-
-  context "with user which is permitted only edit_sys_groups" do
-    before do
-      role = create(:sys_role, name: unique_id, permissions: %w(edit_sys_groups))
-      user = create(:sys_user_sample, sys_role_ids: [role.id])
-      login_user user
-    end
-
-    it "#index" do
-      visit sns_mypage_path
-      click_on I18n.t('sns.connection')
-      expect(current_path).to eq sns_connection_path
-      within 'nav.mod-navi' do
-        expect(page).to have_css('h2', text: 'システム設定')
-        expect(page).to have_no_css('h3 a', text: 'サイト')
-        expect(page).to have_css('h3 a', text: 'グループ')
-        expect(page).to have_no_css('h3 a', text: 'ユーザー')
-        expect(page).to have_no_css('h3 a', text: '権限/ロール')
-        expect(page).to have_no_css('h3 a', text: '認証')
-        expect(page).to have_no_css('h3 a', text: '最大ファイルサイズ')
-        expect(page).to have_no_css('h3 a', text: 'サイト複製')
-        expect(page).to have_no_css('h3 a', text: 'テスト')
-        expect(page).to have_no_css('h3 a', text: '操作履歴')
-        expect(page).to have_no_css('h3 a', text: 'ジョブ')
-      end
-    end
-  end
-
-  context "with user which is permitted only edit_sys_roles" do
-    before do
-      role = create(:sys_role, name: unique_id, permissions: %w(edit_sys_roles))
-      user = create(:sys_user_sample, sys_role_ids: [role.id])
-      login_user user
-    end
-
-    it "#index" do
-      visit sns_mypage_path
-      click_on I18n.t('sns.connection')
-      expect(current_path).to eq sns_connection_path
-      within 'nav.mod-navi' do
-        expect(page).to have_css('h2', text: 'システム設定')
-        expect(page).to have_no_css('h3 a', text: 'サイト')
-        expect(page).to have_no_css('h3 a', text: 'グループ')
-        expect(page).to have_no_css('h3 a', text: 'ユーザー')
-        expect(page).to have_css('h3 a', text: '権限/ロール')
-        expect(page).to have_no_css('h3 a', text: '認証')
-        expect(page).to have_no_css('h3 a', text: '最大ファイルサイズ')
-        expect(page).to have_no_css('h3 a', text: 'サイト複製')
-        expect(page).to have_no_css('h3 a', text: 'テスト')
-        expect(page).to have_no_css('h3 a', text: '操作履歴')
-        expect(page).to have_no_css('h3 a', text: 'ジョブ')
-      end
-    end
-  end
-
-  context "with user which is permitted only edit_sys_sites" do
-    before do
-      role = create(:sys_role, name: unique_id, permissions: %w(edit_sys_sites))
-      user = create(:sys_user_sample, sys_role_ids: [role.id])
-      login_user user
-    end
-
-    it "#index" do
-      visit sns_mypage_path
-      click_on I18n.t('sns.connection')
-      expect(current_path).to eq sns_connection_path
-      within 'nav.mod-navi' do
-        expect(page).to have_css('h2', text: 'システム設定')
-        expect(page).to have_css('h3 a', text: 'サイト')
-        expect(page).to have_no_css('h3 a', text: 'グループ')
-        expect(page).to have_no_css('h3 a', text: 'ユーザー')
-        expect(page).to have_no_css('h3 a', text: '権限/ロール')
-        expect(page).to have_no_css('h3 a', text: '認証')
-        expect(page).to have_no_css('h3 a', text: '最大ファイルサイズ')
-        expect(page).to have_css('h3 a', text: 'サイト複製')
-        expect(page).to have_no_css('h3 a', text: 'テスト')
-        expect(page).to have_no_css('h3 a', text: '操作履歴')
-        expect(page).to have_no_css('h3 a', text: 'ジョブ')
-      end
-    end
-  end
-
-  context "with user which is permitted only edit_sys_users" do
-    before do
-      role = create(:sys_role, name: unique_id, permissions: %w(edit_sys_users))
-      user = create(:sys_user_sample, sys_role_ids: [role.id])
-      login_user user
-    end
-
-    it "#index" do
-      visit sns_mypage_path
-      click_on I18n.t('sns.connection')
-      expect(current_path).to eq sns_connection_path
-      within 'nav.mod-navi' do
-        expect(page).to have_css('h2', text: 'システム設定')
-        expect(page).to have_no_css('h3 a', text: 'サイト')
-        expect(page).to have_no_css('h3 a', text: 'グループ')
-        expect(page).to have_css('h3 a', text: 'ユーザー')
-        expect(page).to have_no_css('h3 a', text: '権限/ロール')
-        expect(page).to have_css('h3 a', text: '認証')
-        expect(page).to have_no_css('h3 a', text: 'サイト複製')
-        expect(page).to have_css('h3 a', text: 'テスト')
-        expect(page).to have_css('h3 a', text: '操作履歴')
-        expect(page).to have_css('h3 a', text: 'ジョブ')
-      end
-    end
-  end
-
-  context "with user which is permitted nothing" do
-    before do
-      user = create(:sys_user_sample, sys_role_ids: [])
-      login_user user
-    end
-
-    it "#index" do
-      visit sns_mypage_path
-      click_on I18n.t('sns.connection')
-      expect(current_path).to eq sns_connection_path
-      within 'nav.mod-navi' do
-        expect(page).to have_css('h2', text: 'システム設定')
-        expect(page).to have_no_css('h3 a', text: 'サイト')
-        expect(page).to have_no_css('h3 a', text: 'グループ')
-        expect(page).to have_no_css('h3 a', text: 'ユーザー')
-        expect(page).to have_no_css('h3 a', text: '権限/ロール')
-        expect(page).to have_no_css('h3 a', text: '認証')
-        expect(page).to have_no_css('h3 a', text: '最大ファイルサイズ')
-        expect(page).to have_no_css('h3 a', text: 'サイト複製')
-        expect(page).to have_no_css('h3 a', text: 'テスト')
-        expect(page).to have_no_css('h3 a', text: '操作履歴')
-        expect(page).to have_no_css('h3 a', text: 'ジョブ')
-      end
+      expect(current_path).to eq index_path
+      expect(page).to have_css('dt', text: I18n.t("sys.remote_addr"))
+      expect(page).to have_css('dt', text: I18n.t("sys.user_agent"))
+      expect(page).to have_css('dt', text: I18n.t("sys.server_name"))
     end
   end
 end
