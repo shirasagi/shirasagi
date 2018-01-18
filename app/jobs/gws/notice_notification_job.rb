f4184e99265041d783d7e37b241a6845d84e178e
NAKANO Hideo <nakano@web-tips.co.jp>
2018-01-18 15:25:52 +0900

    [add] notice notification job

---
+++ app/jobs/gws/notice_notification_job.rb
@@ -0,0 +1,91 @@
+class Gws::NoticeNotificationJob < Gws::ApplicationJob
+  def perform
+    @now = Time.zone.now
+    select_items
+    send_all_notifications
+  end
+
+  private
+
+  def select_items
+    criteria = Gws::Notice.site(site).and_public
+    criteria = criteria.and('$or' => [{ message_notification: 'enabled' }, { email_notification: 'enabled' }])
+    @items = criteria.exists(notification_noticed: false)
+  end
+
+  def send_all_notifications
+    each_item do |item|
+      next if item.notification_noticed.present?
+
+      criteria = Gws::Notice.where(id: item.id)
+      item = criteria.find_one_and_update({ '$set' => { notification_noticed: @now } }, return_document: :after)
+      next unless item
+
+      send_one_notification(item)
+    end
+  end
+
+  def each_item(batch_size = 20)
+    item_ids = @items.pluck(:id)
+    item_ids.each_slice(batch_size) do |ids|
+      items = @items.in(id: ids).to_a
+      items.each do |item|
+        yield item
+      end
+    end
+  end
+
+  def send_one_notification(notice)
+    if notice.message_notification == 'enabled'
+      send_notification_by_message(notice)
+    end
+
+    if notice.email_notification == 'enabled'
+      send_notification_by_email(notice)
+    end
+  end
+
+  def send_notification_by_message(notice)
+    path = Rails.application.routes.url_helpers.gws_public_notice_url(
+      protocol: site.canonical_scheme, host: site.canonical_domain, site: site, id: notice
+    )
+
+    i18n_key = Gws::Notice.model_name.i18n_key
+    subject = I18n.t("gws_notification.#{i18n_key}.subject", name: notice.name, default: notice.name)
+    text = ApplicationController.helpers.sanitize(notice.html.presence || '', tags: [])
+    text = text.truncate(60)
+    text = I18n.t("gws_notification.#{i18n_key}.text", name: notice.name, text: text, path: path, default: text)
+
+    message = Gws::Memo::Message.new
+    message.cur_site = site
+    message.cur_user = site.sender_user || notice.user
+    message.member_ids = select_recipients(notice).pluck(:id)
+    message.send_date = @now
+    message.state = 'public'
+    message.subject = subject
+    message.format = 'text'
+    message.text = text
+
+    message.save!
+  end
+
+  def send_notification_by_email(notice)
+    select_recipients(notice).pluck(:email).compact.uniq.each do |email|
+      next if email.blank?
+
+      mail = Gws::Notice::Mailer.notify_mail(site, notice, email)
+      next if mail.blank?
+
+      mail.deliver_now
+      Rails.logger.info("#{notice.name}: #{email}へ通知送信")
+    end
+  end
+
+  def select_recipients(notice)
+    user_ids = notice.readable_members.pluck(:id)
+    user_ids += Gws::User.in(group_ids: notice.readable_groups.active.pluck(:id)).active
+    user_ids.uniq!
+
+    Gws::User.in(id: user_ids)
+  end
+end
---
+++ spec/jobs/gws/notice_notification_job_spec.rb
@@ -0,0 +1,122 @@
+require 'spec_helper'
+
+describe Gws::NoticeNotificationJob, dbscope: :example do
+  let(:site) { gws_site }
+  let(:scheme) { %w(http https).sample }
+  let(:domain) { "#{unique_id}.example.jp" }
+  let(:sender) { gws_user }
+  let(:recipient1) { create(:gws_user) }
+  let(:recipient2) { create(:gws_user) }
+
+  before do
+    ActionMailer::Base.deliveries = []
+  end
+
+  after do
+    ActionMailer::Base.deliveries = []
+  end
+
+  before do
+    site.canonical_scheme = scheme
+    site.canonical_domain = domain
+    site.sender_name = unique_id
+    site.sender_email = "#{site.sender_name}@example.jp"
+    site.save!
+  end
+
+  context 'text notice' do
+    let!(:notice) do
+      create(
+        :gws_notice, cur_site: site, cur_user: sender, message_notification: 'enabled', email_notification: 'enabled',
+        readable_member_ids: [recipient1.id, recipient2.id], state: 'public'
+      )
+    end
+
+    it do
+      described_class.bind(site_id: site.id).perform_now
+
+      expect(Gws::Job::Log.count).to eq 1
+      Gws::Job::Log.first.tap do |log|
+        expect(log.logs).to include(include('INFO -- : Started Job'))
+        expect(log.logs).to include(include('INFO -- : Completed Job'))
+      end
+
+      expect(Gws::Memo::Message.count).to eq 1
+      Gws::Memo::Message.first.tap do |message|
+        expect(message.subject).to eq I18n.t('gws_notification.gws/notice.subject', name: notice.name)
+        expect(message.text).to include(notice.name)
+        expect(message.text).to include("#{scheme}://#{domain}/.g#{site.id}/gws/public_notices/#{notice.id}")
+      end
+
+      expect(ActionMailer::Base.deliveries.length).to eq 2
+      ActionMailer::Base.deliveries.first.tap do |notify_mail|
+        expect(notify_mail.from.first).to eq site.sender_email
+        expect(notify_mail.to.first).to eq recipient1.email
+        expect(notify_mail.subject).to eq I18n.t('gws_notification.gws/notice.subject', name: notice.name)
+        expect(notify_mail.body.multipart?).to be_falsey
+        expect(notify_mail.body.raw_source).to include(notice.name)
+        expect(notify_mail.body.raw_source).to include("#{scheme}://#{domain}/.g#{site.id}/gws/public_notices/#{notice.id}")
+      end
+    end
+  end
+
+  context 'markdown notice' do
+    let!(:notice) do
+      create(
+        :gws_notice, cur_site: site, cur_user: sender, message_notification: 'enabled', email_notification: 'enabled',
+        readable_member_ids: [recipient1.id, recipient2.id], state: 'public',
+        text: "# #{unique_id}\n#{unique_id}\n\n* #{unique_id}", text_type: 'markdown'
+      )
+    end
+
+    it do
+      described_class.bind(site_id: site.id).perform_now
+
+      expect(Gws::Job::Log.count).to eq 1
+      Gws::Job::Log.first.tap do |log|
+        expect(log.logs).to include(include('INFO -- : Started Job'))
+        expect(log.logs).to include(include('INFO -- : Completed Job'))
+      end
+
+      expect(Gws::Memo::Message.count).to eq 1
+      Gws::Memo::Message.first.tap do |message|
+        expect(message.subject).to eq I18n.t('gws_notification.gws/notice.subject', name: notice.name)
+        expect(message.text).to include(notice.name)
+        expect(message.text).to include("#{scheme}://#{domain}/.g#{site.id}/gws/public_notices/#{notice.id}")
+      end
+
+      expect(ActionMailer::Base.deliveries.length).to eq 2
+      ActionMailer::Base.deliveries.first.tap do |notify_mail|
+        expect(notify_mail.from.first).to eq site.sender_email
+        expect(notify_mail.to.first).to eq recipient1.email
+        expect(notify_mail.subject).to eq I18n.t('gws_notification.gws/notice.subject', name: notice.name)
+        expect(notify_mail.body.multipart?).to be_falsey
+        expect(notify_mail.body.raw_source).to include(notice.name)
+        expect(notify_mail.body.raw_source).to include("#{scheme}://#{domain}/.g#{site.id}/gws/public_notices/#{notice.id}")
+      end
+    end
+  end
+
+  context 'non-public notice' do
+    let!(:notice) do
+      create(
+        :gws_notice, cur_site: site, cur_user: sender, message_notification: 'enabled', email_notification: 'enabled',
+        readable_member_ids: [recipient1.id, recipient2.id], state: 'closed'
+      )
+    end
+
+    it do
+      described_class.bind(site_id: site.id).perform_now
+
+      expect(Gws::Job::Log.count).to eq 1
+      Gws::Job::Log.first.tap do |log|
+        expect(log.logs).to include(include('INFO -- : Started Job'))
+        expect(log.logs).to include(include('INFO -- : Completed Job'))
+      end
+
+      # no notifications were send
+      expect(Gws::Memo::Message.count).to eq 0
+      expect(ActionMailer::Base.deliveries.length).to eq 0
+    end
+  end
+end
