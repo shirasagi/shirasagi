06b1c15489962e91a0f286abbcf1f44d736365b8
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2018-01-22 09:02:53 +0900

    [Fix]Unnecessary var (#1872)
    * [Fix]Unnecessary var
    
    * [Modify]Use CamelCase

--- app/assets/javascripts/gws/lib/bookmark.js
+++ app/assets/javascripts/gws/lib/bookmark.js
@@ -1,11 +1,11 @@
 function Gws_Bookmark() {
-  this.bookmark_id = null;
-  this.default_name = null;
+  this.bookmarkId = null;
+  this.defaultName = null;
   this.url = null;
   this.model = null;
   this.el = $('.gws-bookmark');
-  this.bookmark_icon = "&#xE866;";
-  this.unbookmark_icon = "&#xE867;";
+  this.bookmarkIcon = "&#xE866;";
+  this.unbookmarkIcon = "&#xE867;";
   this.loading = false;
 }
 
@@ -14,22 +14,23 @@ Gws_Bookmark.prototype.render = function(opts) {
     opts = {};
   }
   var _this = this;
-  this.bookmark_id = opts['id'];
-  this.default_name = opts['default_name'];
+  this.bookmarkId = opts['id'];
+  this.defaultName = opts['default_name'];
   this.url = opts['url'];
   this.model = opts['model'];
 
-  if (this.bookmark_id) {
-    var icon = this.bookmark_icon;
+  var icon;
+  if (this.bookmarkId) {
+    icon = this.bookmarkIcon;
   } else {
-    var icon = this.unbookmark_icon;
+    icon = this.unbookmarkIcon;
   }
-  var bookmark_name = opts['name'] || this.default_name;
+  var bookmarkName = opts['name'] || this.defaultName;
 
   var span = $('<span class="bookmark-icon"></span>').append($('<i class="material-icons"></i>').html(icon));
   var ul = $('<ul class="dropdown-menu"></ul>');
   var li = $('<li></li>');
-  li.append($('<input name="bookmark[name]" id="bookmark_name" class="bookmark-name" type="text">').val(bookmark_name));
+  li.append($('<input name="bookmark[name]" id="bookmark_name" class="bookmark-name" type="text">').val(bookmarkName));
   li.append($('<input name="button" type="button" class="btn update" />').val(opts['save']));
   li.append($('<input name="button" type="button" class="btn delete" />').val(opts['delete']));
   ul.append($('<li><div class="bookmark-notice"></div></li>')).append(li);
@@ -42,7 +43,7 @@ Gws_Bookmark.prototype.render = function(opts) {
       _this.update();
     } else if ($(e.target).hasClass('delete')) {
       _this.delete();
-    } else if (_this.bookmark_id) {
+    } else if (_this.bookmarkId) {
       _this.el.addClass('active');
       _this.el.find('.dropdown-menu').addClass('active');
     } else {
@@ -61,7 +62,7 @@ Gws_Bookmark.prototype.create = function() {
     method: 'POST',
     data: {
       item: {
-        name: this.default_name,
+        name: this.defaultName,
         url: location.pathname + location.search,
         model: this.model
       }
@@ -70,10 +71,10 @@ Gws_Bookmark.prototype.create = function() {
       _this.el.find('.dropdown-menu').html(html);
       _this.el.addClass('active');
       _this.el.find('.dropdown-menu').addClass('active');
-      _this.el.find('.material-icons').html(_this.bookmark_icon);
+      _this.el.find('.material-icons').html(_this.bookmarkIcon);
       _this.el.find('.bookmark-notice').text(data['notice']);
-      _this.el.find('.bookmark-name').val(_this.default_name);
-      _this.bookmark_id = data['bookmark_id'];
+      _this.el.find('.bookmark-name').val(_this.defaultName);
+      _this.bookmarkId = data['bookmark_id'];
       _this.loading = false;
     },
     error: function() {
@@ -85,8 +86,8 @@ Gws_Bookmark.prototype.create = function() {
 Gws_Bookmark.prototype.update = function() {
   this.loading = true;
   var _this = this;
-  var new_name = this.el.find('.bookmark-name').val() || this.default_name;
-  var uri = this.url + '/' + this.bookmark_id;
+  var newName = this.el.find('.bookmark-name').val() || this.defaultName;
+  var uri = this.url + '/' + this.bookmarkId;
   var html = this.el.find('.dropdown-menu').html();
   this.el.find('.dropdown-menu').html(SS.loading);
   this.el.addClass('active');
@@ -97,7 +98,7 @@ Gws_Bookmark.prototype.update = function() {
     data: {
       _method: 'patch',
       item: {
-        name: new_name,
+        name: newName,
         url: location.pathname + location.search,
         model: this.model
       }
@@ -106,10 +107,10 @@ Gws_Bookmark.prototype.update = function() {
       _this.el.find('.dropdown-menu').html(html);
       _this.el.removeClass('active');
       _this.el.find('.dropdown-menu').removeClass('active');
-      _this.el.find('.material-icons').html(_this.bookmark_icon);
+      _this.el.find('.material-icons').html(_this.bookmarkIcon);
       _this.el.find('.bookmark-notice').text(data['notice']);
-      _this.el.find('.bookmark-name').val(new_name);
-      _this.bookmark_id = data['bookmark_id'];
+      _this.el.find('.bookmark-name').val(newName);
+      _this.bookmarkId = data['bookmark_id'];
       _this.loading = false;
     },
     error: function() {
@@ -120,11 +121,11 @@ Gws_Bookmark.prototype.update = function() {
 
 Gws_Bookmark.prototype.delete = function() {
   var _this = this;
-  if (!this.bookmark_id) {
+  if (!this.bookmarkId) {
     return false;
   }
   this.loading = true;
-  var uri = this.url + '/' + this.bookmark_id;
+  var uri = this.url + '/' + this.bookmarkId;
   var html = this.el.find('.dropdown-menu').html();
   this.el.find('.dropdown-menu').html(SS.loading);
   this.el.addClass('active');
@@ -142,8 +143,8 @@ Gws_Bookmark.prototype.delete = function() {
       _this.el.find('.dropdown-menu').html(html);
       _this.el.removeClass('active');
       _this.el.find('.dropdown-menu').removeClass('active');
-      _this.el.find('.material-icons').html(_this.unbookmark_icon);
-      _this.bookmark_id = null;
+      _this.el.find('.material-icons').html(_this.unbookmarkIcon);
+      _this.bookmarkId = null;
       _this.loading = false;
     },
     error: function() {
