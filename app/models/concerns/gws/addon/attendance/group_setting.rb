0ae9124e1f1948e7b95842c764114f2b3bfe629e
NAKANO Hideo <nakano@web-tips.co.jp>
2018-01-22 16:37:35 +0900

    [add] gws/attendance break time settings

--- app/models/concerns/gws/addon/attendance/group_setting.rb
+++ app/models/concerns/gws/addon/attendance/group_setting.rb
@@ -6,8 +6,20 @@ module Gws::Addon::Attendance::GroupSetting
 
   included do
     attr_accessor :in_attendance_time_change_hour
+
     field :attendance_time_changed_at, type: DateTime, default: Time::EPOCH + 3.hours
+    SS.config.gws.attendance['max_break'].times do |i|
+      field "attendance_break_time_state#{i + 1}", type: String
+      field "attendance_break_enter_label#{i + 1}", type: String
+      field "attendance_break_leave_label#{i + 1}", type: String
+      alias_method("attendance_break_time_state#{i + 1}_options", :attendance_break_time_options)
+      permit_params "attendance_break_time_state#{i + 1}"
+      permit_params "attendance_break_enter_label#{i + 1}"
+      permit_params "attendance_break_leave_label#{i + 1}"
+    end
+
     permit_params :in_attendance_time_change_hour
+
     before_validation :set_attendance_time_changed_at
   end
 
@@ -17,13 +29,17 @@ module Gws::Addon::Attendance::GroupSetting
     end
   end
 
+  def attendance_break_time_options
+    %w(hide show).map { |k| [I18n.t("ss.options.state.#{k}"), k] }
+  end
+
   private
 
   def set_attendance_time_changed_at
     if in_attendance_time_change_hour.blank?
       self.attendance_time_changed_at = nil
     else
-      self.attendance_time_changed_at = Time::EPOCH + in_attendance_time_change_hour.hours
+      self.attendance_time_changed_at = Time::EPOCH + Integer(in_attendance_time_change_hour).hours
     end
   end
 end
