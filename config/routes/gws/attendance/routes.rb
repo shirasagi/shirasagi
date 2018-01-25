508ae5153b415b974c710fa675b1902a37830975
NAKANO Hideo <nakano@web-tips.co.jp>
2018-01-25 21:59:36 +0900

    [modify] remove new and create from gws/attendance/management/time_card

--- config/routes/gws/attendance/routes.rb
+++ config/routes/gws/attendance/routes.rb
@@ -20,7 +20,7 @@ SS::Application.routes.draw do
 
     namespace 'management' do
       get '/' => redirect { |p, req| "#{req.path}/time_cards/#{Time.zone.now.strftime('%Y%m')}" }, as: :main
-      resources :time_cards, path: 'time_cards/:year_month', concerns: [:deletion] do
+      resources :time_cards, path: 'time_cards/:year_month', except: %i[new create], concerns: %i[deletion] do
         match :download, on: :collection, via: %i[get post]
         match :lock, on: :collection, via: %i[get post]
         match :unlock, on: :collection, via: %i[get post]
