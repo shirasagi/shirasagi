7ce3795fa1d9e7326a64136442dc0fd61a2695cc
tany <tanizawa@web-tips.co.jp>
2018-01-26 22:36:46 +0900

    [modify] Gws/Share: send message (#1894)
    * [modify] gws/share: send downloader message
    
    * [modify] memo,webmail: auto link message

--- app/jobs/gws/share/compress_job.rb
+++ app/jobs/gws/share/compress_job.rb
@@ -4,6 +4,21 @@ class Gws::Share::CompressJob < Gws::ApplicationJob
 
     Rails.logger.error("Error : Failed to compress share_files.") unless zip.save
 
-    Gws::Share::Mailer.compressed_mail(user, zip).deliver_now
+    #Gws::Share::Mailer.compressed_mail(user, zip).deliver_now
+    item = Gws::Share::Mailer.compressed_mail(zip.user, zip).message
+
+    subject = item.subject
+    subject = NKF.nkf("-w", subject) if subject =~ /ISO-2022-JP/i
+
+    message = Gws::Memo::Message.new
+    message.cur_site      = site
+    message.cur_user      = user
+    message.to_member_ids = [user.id]
+    message.send_date     = Time.zone.now
+    message.state         = 'public'
+    message.subject       = subject
+    message.format        = 'text'
+    message.text          = item.decoded
+    message.save!
   end
 end
