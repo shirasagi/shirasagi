fa0e5024edd824eec8e0b02d50b8d8636896d4e8
tany <tanizawa@web-tips.co.jp>
2018-01-30 20:20:02 +0900

    [add] gws/board: add link addon

--- app/models/concerns/gws/addon/portal/portlet/link.rb
+++ app/models/concerns/gws/addon/portal/portlet/link.rb
@@ -2,23 +2,12 @@ module Gws::Addon::Portal::Portlet
   module Link
     extend ActiveSupport::Concern
     extend SS::Addon
+    include Gws::Addon::Link::Feature
 
     set_addon_type :gws_portlet
 
     included do
-      field :links, type: Array, default: []
-      permit_params links: [:name, :url]
-
       validate :validate_links, if: ->{ portlet_model == 'links' }
-    end
-
-    private
-
-    def validate_links
-      items = links.to_a.select do |item|
-        item.values.join.present?
-      end
-      self.links = items
     end
   end
 end
