91612240cc1ff9e8875667f3b6381b01260135ad
NAKANO Hideo <nakano@web-tips.co.jp>
2018-02-02 13:59:25 +0900

    [add] group as selectable

--- app/controllers/gws/memo/apis/personal_addresses_controller.rb
+++ app/controllers/gws/memo/apis/personal_addresses_controller.rb
@@ -4,11 +4,16 @@ class Gws::Memo::Apis::PersonalAddressesController < ApplicationController
 
   model Webmail::Address
 
+  before_action :set_fragment
   before_action :set_group
   before_action :set_multi
   before_action :set_inherit_params
 
   private
+
+  def set_fragment
+    @fragment = params[:fragment].to_s
+  end
 
   def set_group
     if params[:s].present? && params[:s][:group].present?
@@ -32,11 +37,15 @@ class Gws::Memo::Apis::PersonalAddressesController < ApplicationController
     s_params = params[:s] || {}
     s_params[:address_group_id] = @address_group.id if @address_group.present?
 
+    s_group_params = params[:s_group] || {}
+
     @items = @model.
       user(@cur_user).
       and_has_member.
       search(s_params).
       page(params[:page]).
       per(50)
+
+    @group_items = @groups.search(s_group_params).page(params[:page]).per(50)
   end
 end
