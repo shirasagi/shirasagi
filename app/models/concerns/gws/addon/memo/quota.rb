f94371084b51580f5530807e19548cac68732afc
itowtips <ito@web-tips.co.jp>
2018-02-05 21:21:51 +0900

    [fix] gws/memo/message : usage_bytes aggregate

--- app/models/concerns/gws/addon/memo/quota.rb
+++ app/models/concerns/gws/addon/memo/quota.rb
@@ -24,7 +24,7 @@ module Gws::Addon::Memo::Quota
     return unless @cur_site
     return unless @cur_site.memo_quota.to_i > 0
 
-    if self.class.quota_over?(@cur_user, @cur_site)
+    if quota_over?(@cur_user, @cur_site)
       action = draft? ? I18n.t("ss.buttons.save") : I18n.t("ss.buttons.send")
       errors.add :base, :self_quota_over, action: action
       return
@@ -33,52 +33,54 @@ module Gws::Addon::Memo::Quota
     return if draft?
 
     self.members.each do |member|
-      if self.class.quota_over?(member, @cur_site)
+      if quota_over?(member, @cur_site)
         errors.add :base, :member_quota_over, member: member.long_name
       end
     end
   end
 
-  module ClassMethods
-    def usage_bytes(user, site)
-      self.collection.aggregate([
-        {
-          '$match' => {
-            "site_id" => site.id,
-            "$or" => [
-              { "$and" => [ { "user_id" => user.id }, { "deleted.sent" => { "$exists" => false } } ] },
-              { "$and" => [ { "member_ids" => { "$in" => [ user.id ] } }, { "deleted.#{user.id}" => { "$exists" => false } } ] }
-            ]
-          }
-        },
-        {
-          '$group' => {
-            _id: nil,
-            size: { '$sum' => '$size' }
-          }
+  public
+
+  def usage_bytes(user, site)
+    @usage_bytes ||= {}
+    return @usage_bytes[user.id] if @usage_bytes[user.id]
+    @usage_bytes[user.id] = self.collection.aggregate([
+      {
+        '$match' => {
+          "site_id" => site.id,
+          "$or" => [
+            { "$and" => [ { "user_id" => user.id }, { "deleted.sent" => { "$exists" => false } } ] },
+            { "$and" => [ { "member_ids" => { "$in" => [ user.id ] } }, { "deleted.#{user.id}" => { "$exists" => false } } ] }
+          ]
         }
-      ]).first.try(:[], :size) || 0
-    end
+      },
+      {
+        '$group' => {
+          _id: nil,
+          size: { '$sum' => '$size' }
+        }
+      }
+    ]).first.try(:[], :size) || 0
+  end
 
-    def quota_bytes(site)
-      site.memo_quota.to_i * 1024 * 1024
-    end
+  def quota_bytes(site)
+    site.memo_quota.to_i * 1024 * 1024
+  end
 
-    def quota_label(user, site)
-      h = ApplicationController.helpers
-      usage = quota_over?(user, site) ? quota_bytes(site) : usage_bytes(user, site)
-      "#{h.number_to_human_size(usage)}/#{h.number_to_human_size(quota_bytes(site))}"
-    end
+  def quota_label(user, site)
+    h = ApplicationController.helpers
+    usage = quota_over?(user, site) ? quota_bytes(site) : usage_bytes(user, site)
+    "#{h.number_to_human_size(usage)}/#{h.number_to_human_size(quota_bytes(site))}"
+  end
 
-    def quota_over?(user, site)
-      return false if quota_bytes(site) <= 0
-      usage_bytes(user, site) >= quota_bytes(site)
-    end
+  def quota_over?(user, site)
+    return false if quota_bytes(site) <= 0
+    usage_bytes(user, site) >= quota_bytes(site)
+  end
 
-    def quota_percentage(user, site)
-      return 0 if quota_bytes(site) <= 0
-      percentage = (usage_bytes(user, site).to_f / quota_bytes(site).to_f) * 100
-      percentage > 100 ? 100 : percentage
-    end
+  def quota_percentage(user, site)
+    return 0 if quota_bytes(site) <= 0
+    percentage = (usage_bytes(user, site).to_f / quota_bytes(site).to_f) * 100
+    percentage > 100 ? 100 : percentage
   end
 end
