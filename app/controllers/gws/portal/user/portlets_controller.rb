a6a2bb581c3d941f95fee13bcfa52ce4724a6a36
tany <tanizawa@web-tips.co.jp>
2018-02-14 20:06:10 +0900

    [modify] gws/portal: fix navigation

--- app/controllers/gws/portal/user/portlets_controller.rb
+++ app/controllers/gws/portal/user/portlets_controller.rb
@@ -15,7 +15,9 @@ class Gws::Portal::User::PortletsController < ApplicationController
   private
 
   def set_crumbs
-    @crumbs << [t("gws/portal.user_portal"), gws_portal_user_path]
+    set_portal_setting
+    @crumbs << [@cur_site.menu_portal_label || t("modules.gws/portal"), gws_portal_path]
+    @crumbs << [@portal_user.name, gws_portal_user_path(user: @portal_user)]
     @crumbs << [t("gws/portal.links.manage_portlets"), action: :index]
   end
 end
--- app/controllers/gws/portal/user/settings_controller.rb
+++ app/controllers/gws/portal/user/settings_controller.rb
@@ -15,7 +15,9 @@ class Gws::Portal::User::SettingsController < ApplicationController
   private
 
   def set_crumbs
-    @crumbs << [t("gws/portal.user_portal"), gws_portal_user_path]
+    set_portal_setting
+    @crumbs << [@cur_site.menu_portal_label || t("modules.gws/portal"), gws_portal_path]
+    @crumbs << [@portal_user.name, gws_portal_user_path(user: @portal_user)]
     @crumbs << [t("gws/portal.links.settings"), action: :show]
   end
 
