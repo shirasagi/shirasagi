0d3fbf9720acb64f3b5e736ffa7dd6d61137900b
NAKANO Hideo <nakano@web-tips.co.jp>
2018-02-14 17:39:07 +0900

    [add] clone formed page with file upload support

---
+++ spec/factories/cms/column/file_uploads.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :cms_column_file_upload, class: Cms::Column::FileUpload do
+    cur_site { cms_site }
+
+    name { "name-#{unique_id}" }
+    order { rand(999) }
+    required { %w(required optional).sample }
+    tooltips { "tooltips-#{unique_id}" }
+    prefix_label { "prefix_label-#{unique_id}" }
+    postfix_label { "postfix_label-#{unique_id}" }
+    html_tag { %w(a+img a img).sample }
+  end
+end
