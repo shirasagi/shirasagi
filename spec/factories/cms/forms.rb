2b5ecd3963a7a93d851a4c960669501abff1d99e
NAKANO Hideo <pinarello.marvel@gmail.com>
2018-02-22 11:08:18 +0900

    [add][cms] formed page clone (#1924)
    * [add] clone formed page spec
    
    * [add] basic formed page clone support
    
    * [add] clone formed page with file upload support

---
+++ spec/factories/cms/forms.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :cms_form, class: Cms::Form do
+    cur_user { cms_user }
+    name { unique_id }
+    order { rand(999) }
+    state { %w(public closed).sample }
+    html { unique_id }
+  end
+end
