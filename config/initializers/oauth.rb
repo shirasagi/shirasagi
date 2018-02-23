6b9e3caa1f371ed210e184fbc950ab729808406c
m-tachihara <tachihara.masatoshi@sjc-inc.co.jp>
2018-02-23 10:48:29 +0900

    [add] yahoo oauth v2 provider

--- config/initializers/oauth.rb
+++ config/initializers/oauth.rb
@@ -11,7 +11,17 @@ if SS.config.oauth.prefix_path
   Rails.application.config.middleware.use OmniAuth::Builder do
     provider ::Oauth::Twitter
     provider ::Oauth::Facebook
-    provider ::Oauth::YahooJp, { scope: "openid profile email address" }
+    provider ::Oauth::YahooJp, {
+      name: "yahoojp_v2",
+      scope: "openid profile email address"
+    }
+    provider ::Oauth::YahooJp, {
+      scope: "openid profile email address",
+      client_options: {
+        authorize_url: '/yconnect/v1/authorization',
+        token_url: '/yconnect/v1/token'
+      }
+    }
     provider ::Oauth::GoogleOauth2, { scope: "userinfo.email, userinfo.profile, plus.me" }
     provider ::Oauth::Github
   end
