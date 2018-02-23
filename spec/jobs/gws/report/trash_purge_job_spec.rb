5f2430212542d803a5a44ee485984395fe111dde
NAKANO Hideo <nakano@web-tips.co.jp>
2018-02-23 15:08:23 +0900

    [add] trash purge job to gws/report

---
+++ spec/jobs/gws/report/trash_purge_job_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Gws::Report::TrashPurgeJob, dbscope: :example do
+  let(:site) { gws_site }
+  let(:now) { Time.zone.now.beginning_of_minute }
+  let!(:item) { create(:gws_report_file, cur_site: site, deleted: now - 7.days) }
+
+  describe '#perform' do
+    context 'no reports are purged' do
+      it do
+        expect { described_class.bind(site_id: site).perform_now }.not_to(change { Gws::Report::File.count })
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(include("INFO -- : Started Job"))
+          expect(log.logs).to include(include("INFO -- : Completed Job"))
+        end
+      end
+    end
+
+    context 'reports are purged' do
+      it do
+        expect { described_class.bind(site_id: site).perform_now(threshold: 7) }.to change { Gws::Report::File.count }.by(-1)
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(include("INFO -- : Started Job"))
+          expect(log.logs).to include(include("INFO -- : Completed Job"))
+        end
+      end
+    end
+  end
+end
