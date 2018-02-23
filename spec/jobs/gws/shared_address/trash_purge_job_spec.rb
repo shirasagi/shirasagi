4ffc61600ff69ed3a7105cec4b401b1e5a10ddd4
NAKANO Hideo <nakano@web-tips.co.jp>
2018-02-23 16:43:44 +0900

    [add] trash purge job to gws/shared_address

---
+++ spec/jobs/gws/shared_address/trash_purge_job_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe Gws::SharedAddress::TrashPurgeJob, dbscope: :example do
+  let(:site) { gws_site }
+  let(:now) { Time.zone.now.beginning_of_minute }
+  let!(:item) { create(:gws_shared_address_address, cur_site: site, deleted: now - 7.days) }
+
+  describe '#perform' do
+    context 'no shared_address addresses are purged' do
+      it do
+        expect { described_class.bind(site_id: site).perform_now }.not_to(change { Gws::SharedAddress::Address.count })
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(include("INFO -- : Started Job"))
+          expect(log.logs).to include(include("INFO -- : Completed Job"))
+        end
+      end
+    end
+
+    context 'shared_address addresses are purged' do
+      it do
+        expect { described_class.bind(site_id: site).perform_now(threshold: 7) }.to \
+          change { Gws::SharedAddress::Address.count }.by(-1)
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(include("INFO -- : Started Job"))
+          expect(log.logs).to include(include("INFO -- : Completed Job"))
+        end
+      end
+    end
+  end
+end
