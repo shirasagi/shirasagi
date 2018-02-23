02eb9b59debe3165697e07141dd73cfe442f1303
NAKANO Hideo <nakano@web-tips.co.jp>
2018-02-23 15:12:48 +0900

    [add] trash purge job to gws/workflow

---
+++ spec/jobs/gws/workflow/trash_purge_job_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Gws::Workflow::TrashPurgeJob, dbscope: :example do
+  let(:site) { gws_site }
+  let(:now) { Time.zone.now.beginning_of_minute }
+  let!(:item) { create(:gws_workflow_file, cur_site: site, deleted: now - 7.days) }
+
+  describe '#perform' do
+    context 'no workflow files are purged' do
+      it do
+        expect { described_class.bind(site_id: site).perform_now }.not_to(change { Gws::Workflow::File.count })
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(include("INFO -- : Started Job"))
+          expect(log.logs).to include(include("INFO -- : Completed Job"))
+        end
+      end
+    end
+
+    context 'workflow files are purged' do
+      it do
+        expect { described_class.bind(site_id: site).perform_now(threshold: 7) }.to change { Gws::Workflow::File.count }.by(-1)
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(include("INFO -- : Started Job"))
+          expect(log.logs).to include(include("INFO -- : Completed Job"))
+        end
+      end
+    end
+  end
+end
