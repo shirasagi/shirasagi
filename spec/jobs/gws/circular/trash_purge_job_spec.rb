ce4ae57f5fa62e3543434e705659e9a9012f128c
NAKANO Hideo <nakano@web-tips.co.jp>
2018-02-23 15:19:04 +0900

    [add] trash purge job to gws/circular

---
+++ spec/jobs/gws/circular/trash_purge_job_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Gws::Circular::TrashPurgeJob, dbscope: :example do
+  let(:site) { gws_site }
+  let(:now) { Time.zone.now.beginning_of_minute }
+  let!(:item) { create(:gws_circular_post, :member_ids, :due_date, cur_site: site, deleted: now - 7.days) }
+
+  describe '#perform' do
+    context 'no circular posts are purged' do
+      it do
+        expect { described_class.bind(site_id: site).perform_now }.not_to(change { Gws::Circular::Post.count })
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(include("INFO -- : Started Job"))
+          expect(log.logs).to include(include("INFO -- : Completed Job"))
+        end
+      end
+    end
+
+    context 'circular posts are purged' do
+      it do
+        expect { described_class.bind(site_id: site).perform_now(threshold: 7) }.to change { Gws::Circular::Post.count }.by(-1)
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(include("INFO -- : Started Job"))
+          expect(log.logs).to include(include("INFO -- : Completed Job"))
+        end
+      end
+    end
+  end
+end
