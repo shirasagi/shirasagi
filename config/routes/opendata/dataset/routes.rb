2f0629e6bc83be5e66c983bc604597b898e7e444
m-tachihara <tachihara.masatoshi@sjc-inc.co.jp>
2018-02-26 14:13:25 +0900

    [fix] opendata dataset download report (#1937)
    * [add] opendata/resource_download_history
    
    * [modify] opendata/agents/nodes/dataset/resource: save resource_download_history
    
    * [add] opendata/dataset/download_reports
    
    * [add] opendata/dataset/download_reports: export csv

--- config/routes/opendata/dataset/routes.rb
+++ config/routes/opendata/dataset/routes.rb
@@ -8,6 +8,8 @@ SS::Application.routes.draw do
   end
 
   content "opendata" do
+    get "dataset_download_reports" => "dataset/download_reports#index", as: :dataset_download_reports
+    get "dataset_download_reports/download" => "dataset/download_reports#download", as: :dataset_download_reports_download
     resources :crawls, concerns: :deletion, module: :dataset
     resources :dataset_categories, concerns: :deletion, module: :dataset
     resources :dataset_groups, concerns: :deletion, module: :dataset do
---
+++ spec/factories/opendata/resource_download_history.rb
@@ -0,0 +1,4 @@
+FactoryGirl.define do
+  factory :resource_download_history, class: Opendata::ResourceDownloadHistory do
+  end
+end
