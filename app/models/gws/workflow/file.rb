cee4ab32a6faf8653ff1c34e3eff6990fbc6ec6c
itowtips <ito@web-tips.co.jp>
2018-02-28 13:58:37 +0900

    [add] migration

--- app/models/gws/workflow/file.rb
+++ app/models/gws/workflow/file.rb
@@ -122,7 +122,9 @@ class Gws::Workflow::File
 
   # override Gws::Addon::Reminder#reminder_url
   def reminder_url(*args)
-    ret = super
+    #ret = super
+    name = reference_model.tr('/', '_') + '_readable_path'
+    ret = [name, id: id]
     options = ret.extract_options!
     options[:state] = 'all'
     [ *ret, options ]
