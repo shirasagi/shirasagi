5886e715b0c272b14bca9eeb89658074d9c239e9
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2018-03-05 13:54:46 +0900

    [add] import webmail filter

--- app/controllers/webmail/filters_controller.rb
+++ app/controllers/webmail/filters_controller.rb
@@ -28,6 +28,40 @@ class Webmail::FiltersController < ApplicationController
       per(50)
   end
 
+  def download
+    s_params = params[:s] || {}
+
+    @items = @model.user(@cur_user).
+      imap_setting(@cur_user, @imap_setting).
+      search(s_params)
+
+    send_enum enum_csv, type: 'text/csv; charset=Shift_JIS', filename: "personal_filters_#{Time.zone.now.to_i}.csv"
+  end
+
+  def import
+    return if request.get?
+
+    file = params[:item].try(:[], :in_file)
+    if file.nil? || ::File.extname(file.original_filename) != ".csv"
+      raise I18n.t("errors.messages.invalid_csv")
+    end
+
+    table = CSV.read(file.path, headers: true, encoding: 'SJIS:UTF-8')
+    table.each do |row|
+      conditions = row[@model.t(:conditions)].to_s.split("\n").collect do |value|
+        JSON.parse(value, symbolize_names: true)
+      end
+      item = @model.find_or_initialize_by(conditions: conditions)
+      @model.fields.each_key do |key|
+        next if key == '_id'
+        next if key == 'conditions'
+        item.write_attribute(key, row[@model.t(key)].try(:strip))
+      end
+    end
+
+    render_update true, location: { action: :index }, render: { file: :import }
+  end
+
   def apply
     set_item
 
@@ -45,5 +79,28 @@ class Webmail::FiltersController < ApplicationController
       format.html { redirect_to({ action: :show }, notice: t('webmail.notice.multiple.filtered', count: count)) }
       format.json { head :no_content }
     end
+  end
+
+  private
+
+  def enum_csv
+    fields = @model.fields.sort
+    Enumerator.new do |y|
+      y << encode(fields.collect { |name, field| @model.t(name) })
+      @items.each do |item|
+        row = fields.collect do |name, field|
+          if name == 'conditions'
+            item.send(name).collect(&:to_json).join("\n")
+          else
+            item.send(name)
+          end
+        end
+        y << encode(row)
+      end
+    end
+  end
+
+  def encode(str)
+    str.to_csv.encode('CP932', invalid: :replace, undef: :replace)
   end
 end
