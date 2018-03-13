2ed47dfb82f8dfebe4ffc4bd14c63096c7af5434
itowtips <ito@web-tips.co.jp>
2018-03-13 17:24:19 +0900

    [add] webmail : email address validation

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,6 +1,8 @@
 class EmailValidator < ActiveModel::EachValidator
+  REGEXP = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
   def validate_each(record, attribute, value)
     return if value.blank?
-    record.errors.add(attribute, options[:message] || :email) unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    record.errors.add(attribute, options[:message] || :email) unless value =~ REGEXP
   end
 end
