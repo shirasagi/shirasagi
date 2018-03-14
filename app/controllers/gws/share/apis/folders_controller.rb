e37d1cbf6b7a3018ab049b9dbc084db8ef6cad9d
NAKANO Hideo <nakano@web-tips.co.jp>
2018-03-14 13:26:00 +0900

    [add] except option to exclude specific folders

--- app/controllers/gws/share/apis/folders_controller.rb
+++ app/controllers/gws/share/apis/folders_controller.rb
@@ -4,6 +4,7 @@ class Gws::Share::Apis::FoldersController < ApplicationController
   model Gws::Share::Folder
 
   before_action :set_search_params
+  before_action :set_excepts
   before_action :set_items
 
   private
@@ -12,15 +13,21 @@ class Gws::Share::Apis::FoldersController < ApplicationController
     @s ||= params[:s]
   end
 
+  def set_excepts
+    @excepts ||= Array(params[:except]).flatten.select(&:numeric?).map(&:to_i)
+  end
+
   def set_items
     @items ||= begin
       case params[:mode]
       when 'manageable'
         @model.site(@cur_site).
+          nin(id: @excepts).
           allow(:read, @cur_user, site: @cur_site).
           search(@s)
       when 'readable'
         @model.site(@cur_site).
+          nin(id: @excepts).
           readable(@cur_user, site: @cur_site).
           search(@s)
       else
