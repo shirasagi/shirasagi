d317d7eadfe8313575fff0bc7fdd89983234b03c
NAKANO Hideo <nakano@web-tips.co.jp>
2018-03-28 16:40:16 +0900

    [fix] unable to render category select

--- app/controllers/faq/agents/parts/search_controller.rb
+++ app/controllers/faq/agents/parts/search_controller.rb
@@ -6,6 +6,7 @@ class Faq::Agents::Parts::SearchController < ApplicationController
 
   def index
     @search_node = @cur_part.search_node.present? ? @cur_part.search_node : @cur_part.parent
+    @search_node = @search_node.becomes_with_route if @search_node
     @url = mobile_path? ? "#{@cur_site.mobile_location}#{@search_node.url}" : @search_node.url
     @search_node.blank? ? "" : render
   end
