1fe308691d9afdb369d8cc6cb8145e1ae1174a17
itowtips <ito@web-tips.co.jp>
2018-03-29 11:57:57 +0900

    [modify] todo, attendace view params

--- app/controllers/concerns/gws/schedule/plan_filter.rb
+++ app/controllers/concerns/gws/schedule/plan_filter.rb
@@ -64,8 +64,25 @@ module Gws::Schedule::PlanFilter
     params.dig(:calendar, :viewFormat).presence || 'default'
   end
 
+  def redirection_view_todo
+    params.dig(:calendar, :viewTodo).presence || 'active'
+  end
+
+  def redirection_view_attendance
+    params.dig(:calendar, :viewAttendance).presence || 'inactive'
+  end
+
   def redirection_url
-    url_for(action: :index, calendar: { view: redirection_view, date: @item.start_at.to_date.to_s, viewFormat: redirection_view_format })
+    url_for(
+      action: :index,
+      calendar: {
+        view: redirection_view,
+        date: @item.start_at.to_date.to_s,
+        viewFormat: redirection_view_format,
+        viewTodo: redirection_view_todo,
+        viewAttendance: redirection_view_attendance
+      }
+    )
   end
 
   def set_file_addon_state
