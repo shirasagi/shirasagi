c5a31d5f35a24c086635352c45384ba9ca686ff7
itowtips <ito@web-tips.co.jp>
2018-03-29 18:04:20 +0900

    [fix] gws/memo/notification : destroyed_items (p12) (#2020)

--- app/controllers/concerns/gws/memo/notification_filter.rb
+++ app/controllers/concerns/gws/memo/notification_filter.rb
@@ -2,10 +2,11 @@ module Gws::Memo::NotificationFilter
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_destroyed_item, only: [:destroy, :destroy_all]
+    before_action :set_destroyed_item, only: [:destroy, :soft_delete]
+    before_action :set_destroyed_items, only: [:destroy_all, :soft_delete_all]
 
     after_action :send_update_notification, only: [:create, :update]
-    after_action :send_destroy_notification, only: [:destroy, :destroy_all, :soft_delete]
+    after_action :send_destroy_notification, only: [:destroy, :destroy_all, :soft_delete, :soft_delete_all]
   end
 
   private
@@ -33,7 +34,8 @@ module Gws::Memo::NotificationFilter
     return if response.code !~ /^3/
     return unless @item.try(:notify_enabled?)
 
-    set_destroyed_item # soft deleted
+    @destroyed_items ||= []
+    @destroyed_items << @destroyed_item if @destroyed_item
     return if @destroyed_items.blank?
 
     @destroyed_items.each do |item, users|
@@ -55,17 +57,17 @@ module Gws::Memo::NotificationFilter
 
   def set_destroyed_item
     return if request.get?
-    return if response.code !~ /^3/
-    return unless @item.try(:notify_enabled?)
 
-    users = @item.subscribed_users
-    users = users.nin(id: @cur_user.id) if @cur_user
-
-    @destroyed_items ||= []
     if @item
-      @destroyed_items << [@item.dup, users]
+      @destroyed_item = [@item.dup, @item.subscribed_users]
     end
+  end
+
+  def set_destroyed_items
+    return if request.get?
+
     if @items.present?
+      @destroyed_items ||= []
       @items.each do |item|
         @destroyed_items << [item.dup, users]
       end
