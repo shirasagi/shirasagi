f3c0841aee1a0487c1d008bb3fa36cb7d341df7b
NAKANO Hideo <nakano@web-tips.co.jp>
2018-03-29 16:29:44 +0900

    [modify] move javascript in inuquiry/agents/nodes/form/_form to assets

--- app/assets/javascripts/inquiry/lib/form.js
+++ app/assets/javascripts/inquiry/lib/form.js
@@ -1,15 +1,16 @@
-Inquiry_Form = function () {
+Inquiry_Form = function (options) {
+  this.options = options;
   this.render();
 };
 
 Inquiry_Form.instance = null;
 
-Inquiry_Form.render = function() {
+Inquiry_Form.render = function(options) {
   if (Inquiry_Form.instance) {
     return;
   }
 
-  Inquiry_Form.instance = new Inquiry_Form();
+  Inquiry_Form.instance = new Inquiry_Form(options);
 };
 
 Inquiry_Form.prototype.render = function() {
@@ -18,6 +19,32 @@ Inquiry_Form.prototype.render = function() {
       return false;
     } else {
       return true;
+    }
+  });
+
+  var _this = this;
+  $('.form-select').each(function() {
+    return _this.changeForm($(this));
+  });
+  $(document).on('change', '.form-select', function() {
+    return _this.changeForm($(this));
+  });
+};
+
+Inquiry_Form.prototype.changeForm = function(self) {
+  var _this = this;
+
+  self.find('input').each(function () {
+    if ($(this).prop('checked') || _this.options.confirm) {
+      var value = $(this).val();
+      $(".column").each(function () {
+        $(this).find('span.required').remove();
+        var span;
+        if ($(this).hasClass(value) && $(this).children('span').text() === '') {
+          span = $("<span/>").attr('class', 'required').text(_this.options.requiredLabel);
+          span.appendTo($(this).children('dt'));
+        }
+      });
     }
   });
 };
