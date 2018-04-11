992baa24d12099237b6a1a1a97818463dd2af694
itowtips <ito@web-tips.co.jp>
2018-04-11 13:43:36 +0900

    [refactor] #convert_to_lines

--- app/helpers/ss/mailer_helper.rb
+++ app/helpers/ss/mailer_helper.rb
@@ -1,21 +1,5 @@
 module SS::MailerHelper
-  def convert_to_lines(str, maxlength = 800)
-    str.split(/\R/).map do |s|
-      line = ""
-      lines = []
-      bytes_count = 0
-
-      s.split(//).each do |c|
-        bytes_count += 2
-        #bytes_count += c.bytes.count
-        if bytes_count > maxlength
-          lines << line
-          line = ""
-          bytes_count = 0
-        end
-        line += c
-      end
-      lines.present? ? lines.join("\n") : s
-    end.join("\n")
+  def convert_to_lines(str, maxsize = 400)
+    str.scan(/.{#{maxsize}}|.*\R|.+$/).map { |s| s.chomp }.join("\n")
   end
 end
