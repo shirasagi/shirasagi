1fad8885f514c5bf809fbebeba6e3fde9f257cba
itowtips <ito@web-tips.co.jp>
2018-04-12 16:03:57 +0900

    [fix] gws/schedule : search allday reservations (m25)

--- app/controllers/gws/schedule/search/reservations_controller.rb
+++ app/controllers/gws/schedule/search/reservations_controller.rb
@@ -28,11 +28,37 @@ class Gws::Schedule::Search::ReservationsController < ApplicationController
     @time_search = Gws::Schedule::PlanSearch.new(@s)
     @time_search.valid?
 
+    @start_on = Time.zone.parse(@s[:start_on]) rescue nil
+    @end_on = Time.zone.parse(@s[:end_on]) rescue nil
+
+    @start_on = @start_on.to_date if @start_on && @s[:start_on] !~ /:/
+    @end_on = @end_on.to_date if @end_on && @s[:end_on] !~ /:/
+
     @items = @time_search.search
 
-    min_hour = params.dig(:d, :min_hour).presence || @cur_site.facility_min_hour || 8
-    max_hour = params.dig(:d, :max_hour).presence || @cur_site.facility_max_hour || 22
-    @hour_range = (min_hour.to_i...max_hour.to_i)
+    @hour_range = {}
+    params_min_hour = params.dig(:d, :min_hour).presence
+    params_max_hour = params.dig(:d, :max_hour).presence
+
+    @reservation_valid = true
+    @items.each do |date, hours|
+      min_hour = @cur_site.facility_min_hour || 8
+      max_hour = @cur_site.facility_max_hour || 22
+
+      if (@time_search.start_on == date) && params_min_hour
+        min_hour = params_min_hour
+      end
+
+      if (@time_search.end_on == date) && params_max_hour
+        max_hour = params_max_hour
+      end
+
+      @hour_range[date] = (min_hour.to_i...max_hour.to_i)
+
+      if @reservation_valid
+        @reservation_valid = (@hour_range[date].to_a - hours[1].values.flatten).blank?
+      end
+    end
 
     render layout: false
   end
