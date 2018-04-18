f3e70d8adebdae121ff35852a1b3865a49b5a0c0
NAKANO Hideo <nakano@web-tips.co.jp>
2018-04-18 11:32:23 +0900

    [fix] spec failure: reminders with plans or todos

--- spec/features/gws/apis/reminders_spec.rb
+++ spec/features/gws/apis/reminders_spec.rb
@@ -1,27 +1,62 @@
 require 'spec_helper'
 
-describe "gws_apis_reminders", type: :feature, dbscope: :example do
+describe "gws_apis_reminders", type: :feature, dbscope: :example, js: true do
   let(:site) { gws_site }
   let(:path) { gws_schedule_plan_path site, item }
-  let(:item) { create :gws_schedule_plan }
 
-  context "create", js: true do
-    before { login_gws_user }
+  before { login_gws_user }
 
-    it "create" do
+  context "create" do
+    let(:item) do
+      create(
+        :gws_schedule_plan,
+        start_at: 1.hour.from_now.strftime('%Y/%m/%d %H:%M'),
+        end_at: 2.hours.from_now.strftime('%Y/%m/%d %H:%M')
+      )
+    end
+
+    it do
+      expect(Gws::Reminder.where(item_id: item.id).count).to eq 0
+
       visit path
-      # Capybara::Poltergeist::TimeoutError
-      # click_button "登録"
       within '.gws-addon-reminder' do
-        click_on '解除'
+        within first('.remider-conditions tr') do
+          select I18n.t('gws/reminder.options.notify_state.mail'), from: 'item[in_reminder_conditions][][state]'
+          find('button.action-insert').click
+        end
+        click_on I18n.t('gws/reminder.buttons.register_reminder')
       end
-      expect(page).to have_css('.gws-addon-reminder-label', text: I18n.t("gws.reminder.states.empty"))
+      expect(page).to have_css('#notice', text: I18n.t('gws/reminder.notification.created'))
+      puts Gws::Reminder.all.count
+      expect(Gws::Reminder.where(item_id: item.id).count).to eq 1
+    end
+  end
 
+  context "remove" do
+    let(:reminder_condition) do
+      { 'user_id' => gws_user.id, 'state' => 'mail', 'interval' => 10, 'interval_type' => 'minutes' }
+    end
+    let(:item) do
+      create(
+        :gws_schedule_plan,
+        start_at: 1.hour.from_now.strftime('%Y/%m/%d %H:%M'),
+        end_at: 2.hours.from_now.strftime('%Y/%m/%d %H:%M'),
+        in_reminder_conditions: [ reminder_condition ]
+      )
+    end
+
+    it do
+      expect(Gws::Reminder.where(item_id: item.id).count).to eq 1
+
+      visit path
       within '.gws-addon-reminder' do
-        fill_in 'item_date', with: (Time.zone.now + 2.hours).strftime('%Y/%m/%d %H:%M')
-        click_on '登録'
+        within first('.remider-conditions tr') do
+          find('button.action-remove').click
+        end
+        click_on I18n.t('gws/reminder.buttons.register_reminder')
       end
-      expect(page).to have_css('.gws-addon-reminder-label', text: I18n.t("gws.reminder.states.entry"))
+      expect(page).to have_css('#notice', text: I18n.t('gws/reminder.notification.created'))
+      expect(Gws::Reminder.where(item_id: item.id).count).to eq 0
     end
   end
 end
--- spec/features/gws/schedule/plans/reminder_spec.rb
+++ spec/features/gws/schedule/plans/reminder_spec.rb
@@ -3,7 +3,17 @@ require 'spec_helper'
 describe "gws_schedule_plans", type: :feature, dbscope: :example do
   context "reminder of schedule", js: true do
     let(:site) { gws_site }
-    let(:plan) { create :gws_schedule_plan }
+    let(:reminder_condition) do
+      { 'user_id' => gws_user.id, 'state' => 'mail', 'interval' => 10, 'interval_type' => 'minutes' }
+    end
+    let(:plan) do
+      create(
+        :gws_schedule_plan,
+        start_at: 1.hour.from_now.strftime('%Y/%m/%d %H:%M'),
+        end_at: 2.hours.from_now.strftime('%Y/%m/%d %H:%M'),
+        in_reminder_conditions: [ reminder_condition ]
+      )
+    end
     let(:show_path) { gws_schedule_plan_path site, plan }
 
     before { login_gws_user }
@@ -15,22 +25,24 @@ describe "gws_schedule_plans", type: :feature, dbscope: :example do
       visit show_path
       expect(Gws::Reminder.count).to eq 1
       within ".gws-addon-reminder" do
-        expect(page).to have_css(".gws-addon-reminder-label", text: I18n.t("gws.reminder.states.entry"))
+        within first('.remider-conditions tr') do
+          expect(page).to have_select("item[in_reminder_conditions][][state]", selected: I18n.t('gws/reminder.options.notify_state.mail'))
+        end
       end
 
       #
       # リマインダーを解除
       #
       within ".gws-addon-reminder" do
-        click_button "解除"
+        within first('.remider-conditions tr') do
+          find('button.action-remove').click
+        end
+        click_on I18n.t('gws/reminder.buttons.register_reminder')
       end
 
       # 解除できたか確認
       # リマインダーは非同期で解除される。
-      # capybara は element が存在しない場合、しばらく待機するので、以下の確認は解除を待機する意味もある
-      within ".gws-addon-reminder" do
-        expect(page).to have_css(".gws-addon-reminder-label", text: I18n.t("gws.reminder.states.empty"))
-      end
+      expect(page).to have_css('#notice', text: I18n.t('gws/reminder.notification.created'))
 
       # 解除できたら、ドキュメントは存在しないはず
       expect(Gws::Reminder.count).to eq 0
@@ -39,15 +51,16 @@ describe "gws_schedule_plans", type: :feature, dbscope: :example do
       # リマインダーをもう一度登録する
       #
       within ".gws-addon-reminder" do
-        click_button "登録"
+        within first('.remider-conditions tr') do
+          select I18n.t('gws/reminder.options.notify_state.mail'), from: 'item[in_reminder_conditions][][state]'
+        end
+        click_on I18n.t('gws/reminder.buttons.register_reminder')
       end
 
       # 登録できたか確認
       # リマインダーは非同期で登録される。
       # capybara は element が存在しない場合、しばらく待機するので、以下の確認は登録を待機する意味もある
-      within ".gws-addon-reminder" do
-        expect(page).to have_css(".gws-addon-reminder-label", text: I18n.t("gws.reminder.states.entry"))
-      end
+      expect(page).to have_css('#notice', text: I18n.t('gws/reminder.notification.created'))
 
       expect(Gws::Reminder.count).to eq 1
     end
