6b8c0ce09403bf46b6f43147e3fded03ac3d29c8
itowtips <ito@web-tips.co.jp>
2018-04-19 21:25:38 +0900

    [fix] mail_page : @cur_date

--- app/controllers/mail_page/agents/parts/page_controller.rb
+++ app/controllers/mail_page/agents/parts/page_controller.rb
@@ -3,7 +3,7 @@ class MailPage::Agents::Parts::PageController < ApplicationController
   helper Cms::ListHelper
 
   def index
-    @items = MailPage::Page.site(@cur_site).and_public(@cur_date).and_arrival(Time.zone.now).
+    @items = MailPage::Page.site(@cur_site).and_public(@cur_date).and_arrival(@cur_date).
       where(@cur_part.condition_hash(cur_main_path: @cur_main_path)).
       order_by(@cur_part.sort_hash).
       limit(@cur_part.limit)
