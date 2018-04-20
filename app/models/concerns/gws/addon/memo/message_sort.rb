aee3dea918df84608f7712403daa74cc7a6f6620
itowtips <ito@web-tips.co.jp>
2018-04-20 17:54:16 +0900

    W: Lint/UselessAccessModifier: Useless public access modifier.

--- app/models/concerns/gws/addon/memo/message_sort.rb
+++ app/models/concerns/gws/addon/memo/message_sort.rb
@@ -6,8 +6,6 @@ module Gws::Addon::Memo::MessageSort
     field :gws_memo_message_sort, type: Hash, default: {}
   end
 
-  public
-
   def memo_message_sort_hash(folder, sort, order)
     return @sort_hash if @sort_hash
 
@@ -15,11 +13,11 @@ module Gws::Addon::Memo::MessageSort
     if sort.present? && %w(from_member_name to_member_name subject priority send_date size).include?(sort)
       @sort_hash = { sort => order, "updated" => -1 }
     else
-      @sort_hash = gws_memo_message_sort.dig(folder.site_id.to_s, folder.folder_path.gsub(/\./, "_"))
+      @sort_hash = gws_memo_message_sort.dig(folder.site_id.to_s, folder.folder_path.tr(".", "_"))
       @sort_hash ||= { "send_date" => -1, "updated" => -1 }
     end
     self.gws_memo_message_sort[folder.site_id.to_s] ||= {}
-    self.gws_memo_message_sort[folder.site_id.to_s][folder.folder_path.gsub(/\./, "_")] = @sort_hash
+    self.gws_memo_message_sort[folder.site_id.to_s][folder.folder_path.tr(".", "_")] = @sort_hash
     update
 
     @sort_hash
