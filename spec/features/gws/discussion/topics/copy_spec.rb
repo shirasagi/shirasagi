3165749a10a4417981bb24b4f02e4e9f34a9ff5e
itowtips <ito@web-tips.co.jp>
2018-04-25 11:14:39 +0900

    [modify] gws/discussion : add mode navi (m36)

--- spec/features/gws/discussion/topics/copy_spec.rb
+++ spec/features/gws/discussion/topics/copy_spec.rb
@@ -4,7 +4,7 @@ describe "gws_discussion_topics", type: :feature, dbscope: :example do
   context "copy", js: true do
     let!(:site) { gws_site }
     let!(:item) { create :gws_discussion_topic }
-    let!(:copy_path) { copy_gws_discussion_forum_topic_path(site: site.id, forum_id: item.forum.id, id: item.id) }
+    let!(:copy_path) { copy_gws_discussion_forum_topic_path(mode: '-', site: site.id, forum_id: item.forum.id, id: item.id) }
 
     before { login_gws_user }
 
