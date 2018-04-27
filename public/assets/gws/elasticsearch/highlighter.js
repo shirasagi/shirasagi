f50c7a0d4016eef567b1ef21acca9bbe40c94fd0
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2018-04-27 13:14:38 +0900

    assets precompile

---
+++ public/assets/gws/elasticsearch/highlighter.js
@@ -0,0 +1,1 @@
+Gws_Elasticsearch_Highlighter=function(){},Gws_Elasticsearch_Highlighter.prototype={render:function(){location.hash&&$(location.hash).css("border","1px solid red")}};
---
+++ public/assets/gws/memo/folder.js
@@ -0,0 +1,1 @@
+(function(){this.Gws_Memo_Folder=function(){function t(){}return t.render=function(){return $("#addon-gws-agents-addons-group_permission").hide()},t}()}).call(this);
