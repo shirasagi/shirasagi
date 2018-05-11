218aeff440b2f5eae4bb03f9c2239dffc40b6665
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2018-05-11 14:37:00 +0900

    [add] cms/command sample script

---
+++ config/commands/ss_publish.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+PATH=$PATH:/usr/bin:/bin
+PAR_CMD=${1:-"site"}
+PAR_PATH=${2:-""}
+PAR_PATH=`echo \$PAR_PATH | tr "A-Z" "a-z"`
+cat << EOS
+PAR_CMD=$PAR_CMD
+PAR_PATH=$PAR_PATH
+
+#!/bin/sh
+
+## Env.
+# Shirasagi
+SS_DIR="/var/www/shirasagi"
+# Rsync Remote
+REMOTE_USR="root"
+REMOTE_SVR="0.0.0.0"
+# Rsync/scp option
+SCP_OPT=""
+RSYNC_OPT="-az --delete"
+
+## Param check
+if [ \$# -ne 2 ]; then
+  echo "Usage: ss_publish.sh [site|folder|page] [dir|file]" 1>&2
+  exit 1
+fi
+
+PAR_CMD=\${1:-"site"}
+PAR_PATH=\${2:-""}
+PAR_PATH=\`echo \$PAR_PATH | tr "A-Z" "a-z"\`
+if [ \$PAR_CMD != "site" -a \$PAR_CMD != "folder" -a \$PAR_CMD != "page" ]; then
+  echo "Param1 Error! [site|folder|page]" 1>&2
+  exit 1
+fi
+
+## Make from path
+if [[ \$PAR_PATH =~ ^\$SS_DIR ]]; then
+  SYNC_PATH=\$PAR_PATH
+else
+  SYNC_PATH=\$SS_DIR/\$PAR_PATH
+fi
+
+## Check from path
+if [ \$PAR_CMD == "page" ]; then
+  if [ ! -f \$SYNC_PATH ]; then
+    echo "file not exist! ["\$SYNC_PATH"]" 1>&2
+    exit 1
+  fi
+else
+  SYNC_PATH=\`echo \${SYNC_PATH%/}\`
+  if [ ! -d \$SYNC_PATH ]; then
+    echo "directory not exist! ["\$SYNC_PATH"]" 1>&2
+    exit 1
+  fi
+fi
+
+## Sync to Remote
+if [ \$PAR_CMD == "page" ]; then
+  scp \$SCP_OPT \$SYNC_PATH \$REMOTE_USR@\$REMOTE_SVR:\$SYNC_PATH
+else
+  rsync \$RSYNC_OPT \$SYNC_PATH/ \$REMOTE_USR@\$REMOTE_SVR:\$SYNC_PATH
+fi
+
+exit 0
+EOS
+
+exit 0
