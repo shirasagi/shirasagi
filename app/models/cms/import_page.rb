2980b7a88ee0b2d6a06a778fe0c101d9ac6037d2
itowtips <ito@web-tips.co.jp>
2018-05-15 20:31:24 +0900

    [fix] cms/import_page : use code editor #2162 (#2176)

--- app/models/cms/import_page.rb
+++ app/models/cms/import_page.rb
@@ -1,7 +1,7 @@
 class Cms::ImportPage
   include Cms::Model::Page
-  include Workflow::Addon::Branch
-  include Workflow::Addon::Approver
+  #include Workflow::Addon::Branch
+  #include Workflow::Addon::Approver
   include Cms::Addon::Import::Body
   include Cms::Addon::Release
   include Cms::Addon::GroupPermission
