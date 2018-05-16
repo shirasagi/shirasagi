91148dc839529640b5c77237ca49ea15ab15b288
NAKANO Hideo <pinarello.marvel@gmail.com>
2018-05-16 13:03:19 +0900

    [add] url proxy setting (#2180)
    * [add] open_uri_proxy setting
    
    * [modify] add ssl_verify_mode option to proxy setting
    
    * [modify] fix several issues
    
    * [add] encrypted password sample

---
+++ config/initializers/open_uri_proxy.rb
@@ -0,0 +1,37 @@
+module OpenURI
+  def self.open_uri_with_proxy(name, *rest, &block)
+    proxy_uri = SS.config.proxy.proxy_http_basic_authentication["proxy_uri"]
+    proxy_username = SS.config.proxy.proxy_http_basic_authentication["proxy_username"]
+    proxy_password = SS.config.proxy.proxy_http_basic_authentication["proxy_password"]
+    proxy_password = SS::Crypt.decrypt(proxy_password) || proxy_password if proxy_password.present?
+    ssl_verify_mode = SS.config.proxy.ssl_verify_mode
+
+    options = rest.first || {}
+    if options.is_a?(Hash)
+      options.symbolize_keys!
+      if proxy_uri.present?
+        options.delete(:proxy)
+        options.delete(:proxy_http_basic_authentication)
+
+        if proxy_username.present? || proxy_password.present?
+          options[:proxy_http_basic_authentication] = [ proxy_uri, proxy_username, proxy_password ]
+        else
+          options[:proxy] = proxy_uri
+        end
+      end
+
+      if ssl_verify_mode.present? && OpenSSL::SSL.constants.include?(ssl_verify_mode.to_sym)
+        options[:ssl_verify_mode] = "OpenSSL::SSL::#{ssl_verify_mode}".constantize
+      end
+    end
+
+    open_uri_without_proxy name, options, &block
+  end
+
+  class << self
+    return if SS.config.proxy.disable
+
+    alias open_uri_without_proxy open_uri
+    alias open_uri open_uri_with_proxy
+  end
+end
