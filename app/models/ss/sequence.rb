dc91524e328ded7f776eaa902edbd0f385f5cb03
itowtips <ito@web-tips.co.jp>
2018-06-04 20:41:07 +0900

    SS::Sequence

--- app/models/ss/sequence.rb
+++ app/models/ss/sequence.rb
@@ -7,21 +7,24 @@ class SS::Sequence
   class << self
     def current_sequence(coll, name, options)
       sid = "#{coll}_#{name}"
-      doc = self.with(options[:with] || {}).where(_id: sid).first
-      doc ? doc.value : 0
+      self.with(options[:with] || {}) do |model|
+        doc = model.where(_id: sid).first
+        return doc ? doc.value : 0
+      end
     end
 
     def next_sequence(coll, name, options)
-      model = self.with(options[:with] || {})
+      self.with(options[:with] || {}) do |model|
 
-      sid = "#{coll}_#{name}"
-      doc = model.where(_id: sid).find_one_and_update({"$inc" => { value: 1 }}, return_document: :after, upsert: false)
-      return doc.value if doc
+        sid = "#{coll}_#{name}"
+        doc = model.where(_id: sid).find_one_and_update({"$inc" => { value: 1 }}, return_document: :after, upsert: false)
+        return doc.value if doc
 
-      key = (name == :id) ? :_id : name
-      doc = model.mongo_client[coll].find.sort(key => -1).first
-      val = doc ? doc[key].to_i + 1 : 1
-      model.new(_id: sid, value: val).save ? val : nil
+        key = (name == :id) ? :_id : name
+        doc = model.mongo_client[coll].find.sort(key => -1).first
+        val = doc ? doc[key].to_i + 1 : 1
+        return model.new(_id: sid, value: val).save ? val : nil
+      end
     end
 
     def unset_sequence(coll, name, options)
