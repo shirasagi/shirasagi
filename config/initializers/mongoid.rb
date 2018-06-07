4dd6de88c9656f19dc9bb788a44b7a3f436aa111
itowtips <ito@web-tips.co.jp>
2018-06-07 18:47:12 +0900

    [modify] disable SkipMongoidQueryCache

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -1,18 +1,18 @@
 # Require `belongs_to` associations by default. Previous versions had false.
 Mongoid::Config.belongs_to_required_by_default = false
 
-module SkipMongoidQueryCache
-  private
+#module SkipMongoidQueryCache
+#  private
+#
+#  def system_collection?
+#    return true if cache_skip?
+#    super
+#  end
+#
+#  def cache_skip?
+#    return true if selector.reject { |k, v| k == '_id' }.present?
+#    return true if options.reject { |k, v| k == 'limit' }.present?
+#  end
+#end
 
-  def system_collection?
-    return true if cache_skip?
-    super
-  end
-
-  def cache_skip?
-    return true if selector.reject { |k, v| k == '_id' }.present?
-    return true if options.reject { |k, v| k == 'limit' }.present?
-  end
-end
-
-Mongo::Collection::View.__send__(:include, SkipMongoidQueryCache)
+#Mongo::Collection::View.__send__(:include, SkipMongoidQueryCache)
