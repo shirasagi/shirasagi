f0452fb8080dbd379a1531886b06b6cabb364353
NAKANO Hideo <nakano@web-tips.co.jp>
2018-06-12 13:35:36 +0900

    [add] gws/notice advanced crud spec

--- spec/factories/gws/roles.rb
+++ spec/factories/gws/roles.rb
@@ -19,4 +19,14 @@ FactoryGirl.define do
     permissions { Gws::Role.permission_names.select { |name| name.include?('gws_notice') } }
     permission_level 3
   end
+
+  factory :gws_role_notice_editor, class: Gws::Role, traits: [:gws_role] do
+    permissions { %w(delete_private_gws_notices edit_private_gws_notices read_private_gws_notices) }
+    permission_level 1
+  end
+
+  factory :gws_role_notice_reader, class: Gws::Role, traits: [:gws_role] do
+    permissions { %w(read_private_gws_notices) }
+    permission_level 1
+  end
 end
