40680f8bb91b12133b1f12631f2e048e57ab553f
itowtips <ito@web-tips.co.jp>
2018-06-26 20:40:29 +0900

    [modify] disable datetime_validator with mongoid

--- spec/validators/datetime_validator_spec.rb
+++ spec/validators/datetime_validator_spec.rb
@@ -5,21 +5,33 @@ describe DatetimeValidator, type: :validator do
 
   context 'invalid date' do
     let(:value) { '2016/13/01' }
-    it { is_expected.not_to be_valid }
+    it do
+      is_expected.to be_valid
+      is_expected.expiration_date.to be_nil
+    end
   end
 
   context 'valid date' do
     let(:value) { '2016/01/01' }
-    it { is_expected.to be_valid }
+    it do
+      is_expected.to be_valid
+      is_expected.expiration_date.not_to be_nil
+    end
   end
 
   context 'invalid datetime' do
     let(:value) { '2016/1/01 30:00' }
-    it { is_expected.not_to be_valid }
+    it do
+      is_expected.to be_valid
+      is_expected.expiration_date.to be_nil
+    end
   end
 
   context 'valid datetime' do
     let(:value) { '2016/01/01 00:00' }
-    it { is_expected.to be_valid }
+    it do
+      is_expected.to be_valid
+      is_expected.expiration_date.not_to be_nil
+    end
   end
 end
