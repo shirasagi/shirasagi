a356347ae4889b71ece67cc82018202a1ad9924d
kitarinn1012 <kitamura@web-tips.co.jp>
2018-07-06 19:15:06 +0900

    [fix] mapzoom_level

--- app/models/concerns/map/addon/page.rb
+++ app/models/concerns/map/addon/page.rb
@@ -5,8 +5,9 @@ module Map::Addon
 
     included do
       field :map_points, type: Map::Extensions::Points, default: []
-
+      field :map_zoom_level, type: Integer
       permit_params map_points: [ :name, :loc, :text, :link, :image ]
+      permit_params :map_zoom_level
     end
   end
 end
