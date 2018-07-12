ff60950a81cccc2ea47f7832ebecf26378101b83
NAKANO Hideo <nakano@web-tips.co.jp>
2018-07-12 18:02:06 +0900

    [add] several fields to cms/all_contents download

--- app/models/cms/all_content.rb
+++ app/models/cms/all_content.rb
@@ -1,9 +1,44 @@
 class Cms::AllContent
   class << self
+    FIELDS_DEF = [
+      %w(page_id to_page_id),
+      %w(node_id to_node_id),
+      %w(route),
+      %w(name),
+      %w(index_name),
+      %w(filename),
+      %w(url to_url),
+      %w(layout to_layout),
+      %w(keywords),
+      %w(description),
+      %w(summary_html),
+      %w(conditions),
+      %w(sort),
+      %w(limit),
+      %w(upper_html),
+      %w(loop_setting_id to_loop_setting),
+      %w(loop_html),
+      %w(lower_html),
+      %w(new_days),
+      %w(category_ids to_categories),
+      %w(files to_files),
+      %w(file_urls to_file_urls),
+      %w(use_map to_map_points),
+      %w(group_names to_group_names),
+      %w(released),
+      %w(release_date),
+      %w(close_date),
+      %w(created),
+      %w(updated),
+      %w(status to_label),
+      %w(file_size to_file_size),
+    ].freeze
+
     def enum_csv(site)
       Enumerator.new do |y|
         y << encode_sjis(header.to_csv)
         (Cms::Page.site(site).all + Cms::Node.site(site).all).each do |content|
+          content = content.becomes_with_route rescue content
           content.site ||= site
           y << encode_sjis(row(content).to_csv)
         end
@@ -17,43 +52,75 @@ class Cms::AllContent
     end
 
     def header
-      fields = %w(
-        page_id node_id route name index_name url category_ids files file_urls use_map
-        group_ids group_names close_date created released updated status file_size
-       )
-      fields.map { |e| I18n.t("all_content.#{e}") }
+      FIELDS_DEF.map { |e| I18n.t("all_content.#{e[0]}") }
     end
 
     def row(content)
-      page_id    = (content.class == Cms::Page) ? content.id : ""
-      node_id    = (content.class == Cms::Node) ? content.id : ""
-      files      = content.files.map(&:name).join("\n") rescue ""
-      file_urls  = content.files.map(&:url).join("\n") rescue ""
-      map_points = content.map_points.map { |point| point[:loc].join(",") }.join("\n") rescue ""
+      FIELDS_DEF.map do |e|
+        begin
+          if e.length <= 1
+            val = content.send(e[0])
+          else
+            val = send(e[1], e[0], content)
+          end
+        rescue => e
+          val = nil
+        end
 
-      [
-        page_id,
-        node_id,
-        content.route,
-        content.name,
-        content.index_name,
-        content.full_url,
-        Category::Node::Base.where(:id.in => content.category_ids).pluck(:name).join(","),
-        files,
-        file_urls,
-        map_points,
-        content.group_ids.join(","),
-        Cms::Group.where(:id.in => content.group_ids).pluck(:name).join(","),
-        content.try(:close_date).try(:strftime, "%F %T"),
-        content.created.strftime("%F %T"),
-        content.released.try(:strftime, "%F %T"),
-        content.updated.strftime("%F %T"),
-        content.label(:state),
-        file_size(content),
-      ]
+        val = I18n.l(val) if val.is_a?(Time) || val.is_a?(Date) || val.is_a?(DateTime)
+        val
+      end
     end
 
-    def file_size(content)
+    def to_page_id(key, content)
+      content.is_a?(Cms::Model::Page) ? content.id : nil
+    end
+
+    def to_node_id(key, content)
+      content.is_a?(Cms::Model::Node) ? content.id : nil
+    end
+
+    def to_layout(key, content)
+      content.layout.filename
+    end
+
+    def to_loop_setting(key, content)
+      return nil if !content.respond_to?(:loop_setting_id)
+      content.loop_setting.try(:name) || I18n.t("cms.input_directly")
+    end
+
+    def to_url(key, content)
+      content.full_url
+    end
+
+    def to_categories(key, content)
+      Category::Node::Base.where(:id.in => content.category_ids).
+        pluck(:name, :filename).
+        map { |name, filename| "#{filename}(#{name})" }.
+        join("\n")
+    end
+
+    def to_files(key, content)
+      content.files.map(&:name).join("\n")
+    end
+
+    def to_file_urls(key, content)
+      content.files.map(&:url).join("\n")
+    end
+
+    def to_map_points(key, content)
+      content.map_points.map { |point| point[:loc].join(",") }.join("\n")
+    end
+
+    def to_group_names(key, content)
+      Cms::Group.where(:id.in => content.group_ids).pluck(:name).join(",")
+    end
+
+    def to_label(key, content)
+      content.label(key)
+    end
+
+    def to_file_size(key, content)
       return nil unless File.exist?(content.path)
 
       size = File.stat(content.path).size
--- spec/models/cms/all_content_spec.rb
+++ spec/models/cms/all_content_spec.rb
@@ -9,7 +9,7 @@ describe Cms::AllContent, type: :model, dbscope: :example do
       let(:header) { csv.to_a[0].encode('UTF-8').split(",") }
       it { expect(header[0]).to eq(I18n.t("all_content.page_id")) }
       it { expect(header[1]).to eq(I18n.t("all_content.node_id")) }
-      it { expect(header[15]).to eq(I18n.t("all_content.updated")) }
+      it { expect(header[28]).to eq(I18n.t("all_content.updated")) }
     end
 
     describe "contents" do
@@ -21,8 +21,8 @@ describe Cms::AllContent, type: :model, dbscope: :example do
       let(:line1) { csv.to_a[1].encode('UTF-8').split(",") }
       let(:line2) { csv.to_a[2].encode('UTF-8').split(",") }
 
-      it { expect(line1[5]).to eq @page.full_url }
-      it { expect(line2[5]).to eq @node.full_url }
+      it { expect(line1[6]).to eq @page.full_url }
+      it { expect(line2[6]).to eq @node.full_url }
     end
   end
 end
