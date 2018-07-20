67266647ea4bdbf4a895430e989d6334c32d007b
itowtips <ito@web-tips.co.jp>
2018-07-20 18:44:23 +0900

    [add] feature spec

---
+++ spec/features/gws/presence/users_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe 'gws_presence_users', type: :feature, dbscope: :example do
+  context "basic crud", js: true do
+    let!(:site) { gws_site }
+    let!(:index_path) { gws_presence_users_path site }
+
+    before { login_gws_user }
+
+    it "#index" do
+      visit index_path
+      expect(current_path).not_to eq sns_login_path
+
+      find(".editable-users").click_on gws_user.name
+      find('.editable-users span', text: "在席").click
+
+      find(".editable-users .editicon.presence-plan").click
+      fill_in "presence_plan", with: "modified_plan\n"
+
+      find(".editable-users .editicon.presence-memo").click
+      fill_in "presence_memo", with: "modified_memo\n"
+
+      find(".group-users .list-head-title .editicon").click
+      expect(current_path).to eq index_path
+
+      expect(page).to have_text('modified_plan')
+      expect(page).to have_text('modified_memo')
+    end
+  end
+end
