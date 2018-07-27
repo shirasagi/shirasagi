3deb79e269c3acbc7af73b35ad69454523c29ab7
tany <tanizawa@web-tips.co.jp>
2018-07-27 20:20:04 +0900

    [fix] gws/user_portal_setting: enable has_many

--- app/models/concerns/gws/portal/user_extension.rb
+++ app/models/concerns/gws/portal/user_extension.rb
@@ -2,12 +2,12 @@ module Gws::Portal::UserExtension
   extend ActiveSupport::Concern
 
   included do
-    has_one :portal_setting, class_name: 'Gws::Portal::UserSetting',
+    has_many :portal_setting, class_name: 'Gws::Portal::UserSetting',
       foreign_key: :portal_user_id, dependent: :destroy, inverse_of: :portal_user
   end
 
   def find_portal_setting(params = {})
-    portal = portal_setting || Gws::Portal::UserSetting.new(
+    portal = portal_setting.site(params[:cur_site]).first || Gws::Portal::UserSetting.new(
       {
         site_id: params[:cur_site].id,
         portal_user_id: id,
