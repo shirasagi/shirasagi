6ebe5dffe25469e34b51e6ae65605350b28053f9
tany <tanizawa@web-tips.co.jp>
2018-07-31 19:01:02 +0900

    [modify] gws/group_portal_setting: enable has_many

--- app/models/concerns/gws/portal/group_extension.rb
+++ app/models/concerns/gws/portal/group_extension.rb
@@ -2,12 +2,12 @@ module Gws::Portal::GroupExtension
   extend ActiveSupport::Concern
 
   included do
-    has_one :portal_setting, class_name: 'Gws::Portal::GroupSetting',
+    has_many :portal_setting, class_name: 'Gws::Portal::GroupSetting',
       foreign_key: :portal_group_id, dependent: :destroy, inverse_of: :portal_group
   end
 
   def find_portal_setting(params = {})
-    portal = portal_setting || Gws::Portal::GroupSetting.new(
+    portal = portal_setting.site(params[:cur_site]).first || Gws::Portal::GroupSetting.new(
       {
         site_id: params[:cur_site].id,
         portal_group_id: id,
