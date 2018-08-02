6c553954319efda78e6a5f45b394d748a58bc6dd
NAKANO Hideo <nakano@web-tips.co.jp>
2018-08-02 16:35:38 +0900

    [add] password policy validation

--- app/models/concerns/sys/password_policy.rb
+++ app/models/concerns/sys/password_policy.rb
@@ -46,6 +46,34 @@ module Sys::PasswordPolicy
     permit_params :password_min_symbol_use, :password_min_symbol_length
     permit_params :password_prohibited_char_use, :password_prohibited_char
     permit_params :password_min_change_char_use, :password_min_change_char_count
+
+    validates :password_limit_days, presence: true, if: ->{ password_limit_use == "enabled" }
+    validates :password_limit_days, numericality: { only_integer: true, greater_than: 0, allow_blank: true }
+
+    validates :password_warning_days, presence: true, if: ->{ password_warning_use == "enabled" }
+    validates :password_warning_days, numericality: { only_integer: true, greater_than: 0, allow_blank: true }
+
+    validates :password_min_length, presence: true, if: ->{ password_min_use == "enabled" }
+    validates :password_min_length, numericality: { only_integer: true, greater_than: 0, allow_blank: true }
+
+    validates :password_min_upcase_length, presence: true, if: ->{ password_min_upcase_use == "enabled" }
+    validates :password_min_upcase_length, numericality: { only_integer: true, greater_than: 0, allow_blank: true }
+
+    validates :password_min_downcase_length, presence: true, if: ->{ password_min_downcase_use == "enabled" }
+    validates :password_min_downcase_length, numericality: { only_integer: true, greater_than: 0, allow_blank: true }
+
+    validates :password_min_digit_length, presence: true, if: ->{ password_min_digit_use == "enabled" }
+    validates :password_min_digit_length, numericality: { only_integer: true, greater_than: 0, allow_blank: true }
+
+    validates :password_min_symbol_length, presence: true, if: ->{ password_min_symbol_use == "enabled" }
+    validates :password_min_symbol_length, numericality: { only_integer: true, greater_than: 0, allow_blank: true }
+
+    validates :password_prohibited_char, presence: true, if: ->{ password_prohibited_char_use == "enabled" }
+
+    validates :password_min_change_char_count, presence: true, if: ->{ password_min_change_char_use == "enabled" }
+    validates :password_min_change_char_count, numericality: { only_integer: true, greater_than: 0, allow_blank: true }
+
+    validate :validate_password_min_length_consistency, if: ->{ password_min_use == "enabled" }
   end
 
   module ClassMethods
@@ -85,16 +113,43 @@ module Sys::PasswordPolicy
 
   def password_expired(record, now = Time.zone.now)
     return :ok if password_limit_use != "enabled"
+    return :ok if !password_limit_days.numeric?
 
     timestamp = record.password_changed_at || record.created
-    expiration_at = timestamp + password_limit_days.days
+    expiration_at = timestamp + password_limit_days.to_i.days
     return :expired if now >= expiration_at
 
     return :ok if password_warning_use != "enabled"
+    return :ok if !password_warning_days.numeric?
 
-    warn_at = expiration_at - password_warning_days.days
+    warn_at = expiration_at - password_warning_days.to_i.days
     return :nearly_expired if now >= warn_at
 
     :ok
+  end
+
+  private
+
+  def validate_password_min_length_consistency
+    return if !password_min_length.numeric?
+
+    required_password_length = 0
+
+    if password_min_upcase_use == "enabled" && password_min_upcase_length.numeric?
+      required_password_length += password_min_upcase_length.to_i
+    end
+    if password_min_downcase_use == "enabled" && password_min_downcase_length.numeric?
+      required_password_length += password_min_downcase_length.to_i
+    end
+    if password_min_digit_use == "enabled" && password_min_digit_length.numeric?
+      required_password_length += password_min_digit_length.to_i
+    end
+    if password_min_symbol_use == "enabled" && password_min_symbol_length.numeric?
+      required_password_length += password_min_symbol_length.to_i
+    end
+
+    if required_password_length > password_min_length.to_i
+      errors.add :password_min_length, :password_min_length_short, count: required_password_length
+    end
   end
 end
