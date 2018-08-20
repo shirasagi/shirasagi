d649f535db54ee4d51c5ecc3be089385fef51dd0
NAKANO Hideo <nakano@web-tips.co.jp>
2018-08-20 20:16:52 +0900

    [add] pre-defined columns: tap_menu, column_checkbox and column_update

--- app/models/ss/table_builder.rb
+++ app/models/ss/table_builder.rb
@@ -37,6 +37,42 @@ class SS::TableBuilder
     @column = nil
   end
 
+  def tap_menu(*args, &block)
+    column do
+      th ""
+      td do |item|
+        capture do
+          output_buffer << "<div class=\"dropdown\">".html_safe
+          output_buffer << "<button class=\"btn bmd-btn-icon dropdown-toggle\" type=\"button\" id=\"ex1\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\"><i class=\"material-icons\">more_vert</i></button>".html_safe
+          output_buffer << "<div class=\"dropdown-menu dropdown-menu-left\" aria-labelledby=\"ex1\">".html_safe
+          yield item
+          output_buffer << "</div>".html_safe
+          output_buffer << "</div>".html_safe
+        end
+      end
+    end
+  end
+
+  def column_checkbox
+    column(class: "check") do
+      th "<input type=\"checkbox\" />".html_safe
+      td do |item|
+        "<div class=\"checkbox\"><input type=\"checkbox\" name=\"ids[]\" value=\"#{item.id}\" /></div>".html_safe
+      end
+    end
+  end
+
+  def column_updated
+    column(class: "datetime") do
+      th I18n.t("cms.options.sort.updated_1")
+      td do |item|
+        if item.respond_to?(:updated)
+          item.updated.strftime("%Y/%m/%d %H:%M")
+        end
+      end
+    end
+  end
+
   def th(*args, &block)
     options = args.extract_options!
     options = options.reverse_merge(@column_options)
