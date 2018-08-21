305f67c84cba4450bd1e7b356b6b8f4419e45310
NAKANO Hideo <nakano@web-tips.co.jp>
2018-08-21 21:33:24 +0900

    [add] csv download specs

---
+++ spec/factories/gws/column/file_uploads.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :gws_column_file_upload, class: Gws::Column::FileUpload do
+    cur_site { gws_site }
+
+    name { "name-#{unique_id}" }
+    order { rand(999) }
+    required { %w(required optional).sample }
+    tooltips { "tooltips-#{unique_id}" }
+    prefix_label { "prefix_label-#{unique_id}" }
+    postfix_label { "postfix_label-#{unique_id}" }
+    upload_file_count { rand(1..5) }
+  end
+end
