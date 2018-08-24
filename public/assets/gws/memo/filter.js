56341a3fd2fcf950d461a7eae2743d33eaec8579
itowtips <ito@web-tips.co.jp>
2018-08-24 11:01:46 +0900

    assets:precompile

--- public/assets/gws/memo/filter.js
+++ public/assets/gws/memo/filter.js
@@ -1,1 +1,1 @@
-(function(){this.Gws_Memo_Filter=function(){function n(){}return n.render=function(){return $("#addon-gws-agents-addons-group_permission").hide()},n}()}).call(this);
+function Gws_Memo_Filter(){}Gws_Memo_Filter.render=function(){return $("#addon-gws-agents-addons-group_permission").hide()};
--- public/assets/gws/memo/folder.js
+++ public/assets/gws/memo/folder.js
@@ -1,1 +1,1 @@
-(function(){this.Gws_Memo_Folder=function(){function n(){}return n.render=function(){return $("#addon-gws-agents-addons-group_permission").hide()},n}()}).call(this);
+function Gws_Memo_Folder(){}Gws_Memo_Folder.render=function(){return $("#addon-gws-agents-addons-group_permission").hide()};
