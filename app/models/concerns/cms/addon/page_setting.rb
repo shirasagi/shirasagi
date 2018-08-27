4e9ca14653dbbd64fcdd47fc45689a80d2f77e39
kitarinn1012 <kitamura@web-tips.co.jp>
2018-08-27 16:50:06 +0900

    [add] character limit

--- app/models/concerns/cms/addon/page_setting.rb
+++ app/models/concerns/cms/addon/page_setting.rb
@@ -7,9 +7,10 @@ module Cms::Addon
       field :auto_description, type: String, default: "enabled"
       field :auto_keywords, type: String, default: "enabled"
       field :keywords, type: SS::Extensions::Words
-
+      field :max_name_length, type: Integer, default: 80
       permit_params :auto_keywords, :auto_description
       permit_params :keywords
+      permit_params :max_name_length
     end
 
     def auto_keywords_options
@@ -32,6 +33,12 @@ module Cms::Addon
 
     def auto_description_enabled?
       auto_description == "enabled"
+    end
+
+    def max_name_length_options
+      [ 80, 0 ].map do |v|
+        [ I18n.t("cms.options.max_name_length.#{v}"), v ]
+      end
     end
   end
 end
