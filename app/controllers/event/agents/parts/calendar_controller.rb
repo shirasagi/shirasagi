ca6d7e06c872a07769bac48875736e17c8c694d4
NAKANO Hideo <nakano@web-tips.co.jp>
2018-08-29 16:54:07 +0900

    [fix] invalid year/month title of calendar part if page filename consists of 6 length digits

--- app/controllers/event/agents/parts/calendar_controller.rb
+++ app/controllers/event/agents/parts/calendar_controller.rb
@@ -2,31 +2,9 @@ class Event::Agents::Parts::CalendarController < ApplicationController
   include Cms::PartFilter::View
   helper Event::EventHelper
 
+  before_action :set_year_month
+
   def index
-    y = params[:year]
-    m = params[:month]
-
-    if y.present? && m.present? && Date.valid_date?(y.to_i, m.to_i, 1)
-      @year = y.to_i
-      @month = m.to_i
-      @day = Time.zone.today.day.to_i
-    else
-      @cur_path.sub(/\..+?$/, "").scan(/(\d{4})(\d{2})(\d{2})?$/).each do |y, m, d|
-        d = 1 unless d
-        if Date.valid_date?(y.to_i, m.to_i, d.to_i)
-          @year = y.to_i
-          @month = m.to_i
-          @day = d.to_i
-        end
-      end
-
-      if @year.blank? || @month.blank?
-        @year  = Time.zone.today.year.to_i
-        @month = Time.zone.today.month.to_i
-        @day = Time.zone.today.day.to_i
-      end
-    end
-
     @current_month_date = Date.new(@year, @month, 1)
     @prev_month_date = @current_month_date.change(day: 1).advance(days: -1)
     @next_month_date = @current_month_date.advance(months: 1)
@@ -48,6 +26,32 @@ class Event::Agents::Parts::CalendarController < ApplicationController
   end
 
   private
+
+  def set_year_month
+    y = params[:year]
+    m = params[:month]
+
+    if y.present? && m.present? && Date.valid_date?(y.to_i, m.to_i, 1)
+      @year = y.to_i
+      @month = m.to_i
+      @day = Time.zone.today.day.to_i
+    elsif cur_page.blank?
+      @cur_main_path.sub(/\..+?$/, "").scan(/(\d{4})(\d{2})(\d{2})?$/).each do |y, m, d|
+        d = 1 unless d
+        if Date.valid_date?(y.to_i, m.to_i, d.to_i)
+          @year = y.to_i
+          @month = m.to_i
+          @day = d.to_i
+        end
+      end
+    end
+
+    if @year.blank? || @month.blank?
+      @year  = Time.zone.today.year.to_i
+      @month = Time.zone.today.month.to_i
+      @day = Time.zone.today.day.to_i
+    end
+  end
 
   def events(date)
     @condition_hash = {} unless @condition_hash
