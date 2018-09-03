d2b84ef637eca7acc094f2a552aaa6bbfa5cb951
NAKANO Hideo <nakano@web-tips.co.jp>
2018-09-03 11:17:32 +0900

    [add] webmail/history

---
+++ app/models/concerns/webmail/reference/user.rb
@@ -0,0 +1,44 @@
+module Webmail::Reference::User
+  extend ActiveSupport::Concern
+  extend SS::Translation
+
+  attr_accessor :cur_user
+
+  included do
+    field :user_uid, type: String
+    field :user_name, type: String
+    belongs_to :user, class_name: "SS::User"
+
+    before_validation :set_user_id, if: ->{ @cur_user }
+
+    scope :user, ->(user) { where(user_id: user.id) }
+  end
+
+  def user_uid
+    self[:user_uid] || user.try(:uid)
+  end
+
+  def user_name
+    self[:user_name] || user.try(:name)
+  end
+
+  def user_tel
+    user ? user.try(:tel_label) : nil
+  end
+
+  def user_long_name
+    return "#{user_name} (#{user_uid})" if user_uid.present?
+    return user.long_name if user.present?
+    user_name
+  end
+
+  private
+
+  def set_user_id
+    return if user_id.present?
+
+    self.user_id   = @cur_user.id
+    self.user_uid  = @cur_user.uid unless self[:user_uid]
+    self.user_name = @cur_user.name unless self[:user_name]
+  end
+end
