8ac18666f6a79d5f50de11e4326e7c5773c7d3df
snkmr <nakamura@web-tips.co.jp>
2018-09-04 15:40:10 +0900

    [modify] bin/install.sh (#2312)

--- bin/install.sh
+++ bin/install.sh
@@ -232,6 +232,7 @@ cat <<EOF | sudo tee /etc/nginx/conf.d/header.conf
 proxy_set_header Host \$host;
 proxy_set_header X-Real-IP \$remote_addr;
 proxy_set_header Remote-Addr \$remote_addr;
+proxy_set_header X-Forwarded-Proto \$scheme;
 proxy_set_header X-Forwarded-Host \$http_host;
 proxy_set_header X-Forwarded-Server \$host;
 proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
