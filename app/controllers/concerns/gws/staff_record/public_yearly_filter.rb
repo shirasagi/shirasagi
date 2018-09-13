1140f6df053ecbae23d8b24e7816a5fb549c6478
NAKANO Hideo <nakano@web-tips.co.jp>
2018-09-13 20:52:33 +0900

    [modify] proper year selection

--- app/controllers/concerns/gws/staff_record/public_yearly_filter.rb
+++ app/controllers/concerns/gws/staff_record/public_yearly_filter.rb
@@ -9,8 +9,11 @@ module Gws::StaffRecord::PublicYearlyFilter
 
   def set_year
     year_id = params.dig(:s, :year_id)
-    @cur_year = Gws::StaffRecord::Year.site(@cur_site).where(id: year_id).first if year_id
-    @cur_year ||= Gws::StaffRecord::Year.site(@cur_site).first
+    if year_id.present?
+      @cur_year = Gws::StaffRecord::Year.site(@cur_site).where(id: year_id).first
+    else
+      @cur_year = Gws::StaffRecord::Year.site(@cur_site).first
+    end
 
     render(html: t('gws/staff_record.errors.no_data'), layout: true) unless @cur_year
   end
