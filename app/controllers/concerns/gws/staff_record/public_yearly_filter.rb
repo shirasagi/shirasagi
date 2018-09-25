c0e3cec74075c6885374d0fcc421feb93715dec6
NAKANO Hideo <pinarello.marvel@gmail.com>
2018-09-25 19:40:56 +0900

    [fix][gws] staff record multi tenant issue (#2335)
    * [fix] gws/staff_record multi tenant issue
    
    able to select other site year
    
    * [fix] unable to render with `text` option in rails5
    
    * [modify] proper year selection

--- app/controllers/concerns/gws/staff_record/public_yearly_filter.rb
+++ app/controllers/concerns/gws/staff_record/public_yearly_filter.rb
@@ -9,9 +9,12 @@ module Gws::StaffRecord::PublicYearlyFilter
 
   def set_year
     year_id = params.dig(:s, :year_id)
-    @cur_year = Gws::StaffRecord::Year.where(id: year_id).first if year_id
-    @cur_year ||= Gws::StaffRecord::Year.first
+    if year_id.present?
+      @cur_year = Gws::StaffRecord::Year.site(@cur_site).where(id: year_id).first
+    else
+      @cur_year = Gws::StaffRecord::Year.site(@cur_site).first
+    end
 
-    render(text: I18n.t('gws/staff_record.errors.no_data'), layout: true) unless @cur_year
+    render(html: t('gws/staff_record.errors.no_data'), layout: true) unless @cur_year
   end
 end
