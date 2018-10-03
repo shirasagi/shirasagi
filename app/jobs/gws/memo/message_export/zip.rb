5264fce14644993f25271513ac7f38bffdaba9a3
kanahebi <hebibibibeam1109@gmail.com>
2018-10-03 19:01:11 +0900

    [add][gws] export messages as eml (#2348)
    * [add] message export format is eml
    
    * [fix] gws memo message_export format
    
    * [modify] blank email user support

--- app/jobs/gws/memo/message_export/zip.rb
+++ app/jobs/gws/memo/message_export/zip.rb
@@ -1,7 +1,7 @@
 class Gws::Memo::MessageExport::Zip
   extend ActiveSupport::Concern
 
-  attr_accessor :output_dir
+  attr_accessor :output_dir, :output_format
 
   def initialize(path)
     @path = path
@@ -10,12 +10,23 @@ class Gws::Memo::MessageExport::Zip
   def compress
     FileUtils.rm(@path) if File.exist?(@path)
     Zip::File.open(@path, Zip::File::CREATE) do |zip|
-      add_json(zip)
+      if @output_format == "json"
+        add_json(zip)
+      elsif @output_format == "eml"
+        add_eml(zip)
+      end
     end
   end
 
   def add_json(zip)
     Dir.glob("#{@output_dir}/*.json").each do |file|
+      name = ::File.basename(file)
+      zip.add(name.encode('cp932'), file)
+    end
+  end
+
+  def add_eml(zip)
+    Dir.glob("#{@output_dir}/*.eml").each do |file|
       name = ::File.basename(file)
       zip.add(name.encode('cp932'), file)
     end
