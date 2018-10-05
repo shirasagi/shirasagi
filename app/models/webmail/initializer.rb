cae72ca473c2558de797a4cef7667468548123fb
NAKANO Hideo <nakano@web-tips.co.jp>
2018-10-05 15:57:17 +0900

    [modify] remove unused permissions

--- app/models/webmail/initializer.rb
+++ app/models/webmail/initializer.rb
@@ -11,11 +11,6 @@ module Webmail
 
     # グループ代表メールが使えるか？
     Webmail::Role.permission :use_webmail_group_imap_setting
-    # グループ代表メールを管理できるか？
-    Webmail::Role.permission :edit_webmail_group_imap_setting
-
-    # ユーザーのメールボックス容量を閲覧できるか？
-    Webmail::Role.permission :read_webmail_individual_usage
 
     SS::User.include Webmail::UserExtension
     Cms::User.include Webmail::UserExtension
