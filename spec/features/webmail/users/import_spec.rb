0be11b73d84c39f92ba964dbeb87d31bf7c419a1
NAKANO Hideo <nakano@web-tips.co.jp>
2018-10-10 19:09:00 +0900

    [modify] move `sys/webmail/accounts` specs under webmail

--- spec/features/webmail/users/import_spec.rb
+++ spec/features/webmail/users/import_spec.rb
@@ -3,7 +3,18 @@ require 'spec_helper'
 describe "webmail_users", type: :feature, dbscope: :example do
   before { login_webmail_admin }
 
-  context "import" do
+  context "download template" do
+    it do
+      visit webmail_users_path
+      click_on I18n.t("ss.links.import")
+      click_on I18n.t('ss.links.download_template')
+
+      csv_lines = CSV.parse(page.html.encode("UTF-8"))
+      expect(csv_lines.length).to be > 0
+    end
+  end
+
+  context "import accounts_1.csv" do
     let!(:user1) { create(:ss_user, id: 101, email: "#{unique_id}-1@example.jp") }
     let!(:user2) { create(:ss_user, id: 102, email: "#{unique_id}-2@example.jp") }
     let!(:user3) { create(:ss_user, id: 103, email: "#{unique_id}-3@example.jp") }
@@ -37,6 +48,112 @@ describe "webmail_users", type: :feature, dbscope: :example do
         expect(imap_setting.imap_trash_box).to eq 'INBOX.Trash'
       end
       expect(user1.imap_default_index).to eq 0
+    end
+  end
+
+  context "import old_accounts_1-1.csv" do
+    it do
+      visit webmail_users_path
+      click_on I18n.t("ss.links.import")
+      within "form" do
+        attach_file "item[in_file]", "#{Rails.root}/spec/fixtures/webmail/old_accounts_1-1.csv"
+        click_button I18n.t("ss.import")
+      end
+      expect(page).to have_css("#notice", text: I18n.t("ss.notice.saved"))
+
+      user = SS::User.find(1)
+      sys_account = user.imap_settings[0]
+      expect(sys_account.name).to eq "sys"
+      expect(sys_account.from).to eq "ユーザー1"
+      expect(sys_account.address).to eq "sys@example.jp"
+      expect(sys_account.imap_host).to eq "example.jp"
+      expect(sys_account.imap_auth_type).to eq "CRAM-MD5"
+      expect(sys_account.threshold_mb).to eq 123
+      expect(sys_account.imap_sent_box).to eq "INBOX.Sent"
+      expect(sys_account.imap_draft_box).to eq "INBOX.Draft"
+      expect(sys_account.imap_trash_box).to eq "INBOX.Trash"
+      expect(sys_account.decrypt_imap_password).to eq "pass"
+
+      admin_account = user.imap_settings[1]
+      expect(admin_account.name).to eq "admin"
+      expect(admin_account.from).to eq "ユーザー2"
+      expect(admin_account.address).to eq "admin@example.jp"
+      expect(admin_account.imap_host).to eq "example.jp"
+      expect(admin_account.imap_auth_type).to eq "PLAIN"
+      expect(admin_account.threshold_mb).to eq 234
+      expect(admin_account.imap_sent_box).to eq "INBOX.Sent"
+      expect(admin_account.imap_draft_box).to eq "INBOX.Draft"
+      expect(admin_account.imap_trash_box).to eq "INBOX.Trash"
+      expect(admin_account.decrypt_imap_password).to eq "pass"
+
+      user_account = user.imap_settings[2]
+      expect(user_account.name).to eq "user1"
+      expect(user_account.from).to eq "ユーザー3"
+      expect(user_account.address).to eq "user1@example.jp"
+      expect(user_account.imap_host).to eq "example.jp"
+      expect(user_account.imap_auth_type).to eq ""
+      expect(user_account.threshold_mb).to eq 345
+      expect(user_account.imap_sent_box).to eq "INBOX.Sent"
+      expect(user_account.imap_draft_box).to eq "INBOX.Draft"
+      expect(user_account.imap_trash_box).to eq "INBOX.Trash"
+      expect(user_account.decrypt_imap_password).to eq "pass"
+    end
+  end
+
+  context "import old_accounts_1-1.csv and overwrite with old_accounts_1-2.csv" do
+    it do
+      visit webmail_users_path
+      click_on I18n.t("ss.links.import")
+      within "form" do
+        attach_file "item[in_file]", "#{Rails.root}/spec/fixtures/webmail/old_accounts_1-1.csv"
+        click_button I18n.t("ss.import")
+      end
+      expect(page).to have_css("#notice", text: I18n.t("ss.notice.saved"))
+
+      visit webmail_users_path
+      click_on I18n.t("ss.links.import")
+      within "form" do
+        attach_file "item[in_file]", "#{Rails.root}/spec/fixtures/webmail/old_accounts_1-2.csv"
+        click_button I18n.t("ss.import")
+      end
+      expect(page).to have_css("#notice", text: I18n.t("ss.notice.saved"))
+
+      user = SS::User.find(1)
+      sys_account = user.imap_settings[0]
+      expect(sys_account.name).to eq "edit"
+      expect(sys_account.from).to eq "ユーザー1"
+      expect(sys_account.address).to eq "edit@example.jp"
+      expect(sys_account.imap_host).to eq "example.jp"
+      expect(sys_account.imap_auth_type).to eq ""
+      expect(sys_account.threshold_mb).to eq 123
+      expect(sys_account.imap_sent_box).to eq "INBOX.Sent"
+      expect(sys_account.imap_draft_box).to eq "INBOX.Draft"
+      expect(sys_account.imap_trash_box).to eq "INBOX.Trash"
+      expect(sys_account.decrypt_imap_password).to eq "pass"
+
+      admin_account = user.imap_settings[1]
+      expect(admin_account.name).to eq "admin"
+      expect(admin_account.from).to eq "ユーザー2"
+      expect(admin_account.address).to eq "admin@example.jp"
+      expect(admin_account.imap_host).to eq "example.jp"
+      expect(admin_account.imap_auth_type).to eq "PLAIN"
+      expect(admin_account.threshold_mb).to eq 234
+      expect(admin_account.imap_sent_box).to eq "INBOX.Sent"
+      expect(admin_account.imap_draft_box).to eq "INBOX.Draft"
+      expect(admin_account.imap_trash_box).to eq "INBOX.Trash"
+      expect(admin_account.decrypt_imap_password).to eq "pass"
+
+      user_account = user.imap_settings[2]
+      expect(user_account.name).to eq "user1"
+      expect(user_account.from).to eq "ユーザー3"
+      expect(user_account.address).to eq "user1@example.jp"
+      expect(user_account.imap_host).to eq "example.jp"
+      expect(user_account.imap_auth_type).to eq ""
+      expect(user_account.threshold_mb).to eq 345
+      expect(user_account.imap_sent_box).to eq "INBOX.Sent"
+      expect(user_account.imap_draft_box).to eq "INBOX.Draft"
+      expect(user_account.imap_trash_box).to eq "INBOX.Trash"
+      expect(user_account.decrypt_imap_password).to eq "pass"
     end
   end
 end
