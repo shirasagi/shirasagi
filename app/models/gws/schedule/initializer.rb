e653ae1f0cdaa6123143870b56d7f88a9b064733
NAKANO Hideo <nakano@web-tips.co.jp>
2018-10-15 19:37:57 +0900

    [add] `use_private_gws_schedule_plans`, `use_private_gws-schedule_todos` and `use_private_gws_facility_plans`

--- app/models/gws/schedule/initializer.rb
+++ app/models/gws/schedule/initializer.rb
@@ -12,6 +12,7 @@ module Gws::Schedule
     Gws::Role.permission :delete_private_gws_schedule_plans, module_name: 'gws/schedule'
     Gws::Role.permission :trash_other_gws_schedule_plans, module_name: 'gws/schedule'
     Gws::Role.permission :trash_private_gws_schedule_plans, module_name: 'gws/schedule'
+    Gws::Role.permission :use_private_gws_schedule_plans, module_name: 'gws/schedule'
 
     Gws::Role.permission :read_other_gws_schedule_categories, module_name: 'gws/schedule'
     Gws::Role.permission :read_private_gws_schedule_categories, module_name: 'gws/schedule'
@@ -28,5 +29,6 @@ module Gws::Schedule
     Gws::Role.permission :delete_private_gws_schedule_todos, module_name: 'gws/schedule'
     Gws::Role.permission :trash_other_gws_schedule_todos, module_name: 'gws/schedule'
     Gws::Role.permission :trash_private_gws_schedule_todos, module_name: 'gws/schedule'
+    Gws::Role.permission :use_private_gws_schedule_todos, module_name: 'gws/schedule'
   end
 end
