008f7db34196e3639b093f093ea8df4c3eee1b81
NAKANO Hideo <nakano@web-tips.co.jp>
2018-10-17 14:54:56 +0900

    [add] `gws/module_permission` module

---
+++ app/models/concerns/gws/module_permission.rb
@@ -0,0 +1,28 @@
+module Gws::ModulePermission
+  def self.extended(obj)
+    obj.class_variable_set(:@@_permission_name, nil)
+    obj.class_variable_set(:@@_permission_action, nil)
+  end
+
+  def permission_action
+    class_variable_get(:@@_permission_action)
+  end
+
+  def permission_name
+    class_variable_get(:@@_permission_name) || self.name.underscore.gsub(/\//, "_")
+  end
+
+  def allowed?(action, user, opts = {})
+    site    = opts[:site]
+    action  = self.permission_action || action
+
+    user.gws_role_permit_any?(site, "#{action}_#{self.permission_name}")
+  end
+
+  private
+
+  def set_permission_name(name, fix_action = nil)
+    class_variable_set(:@@_permission_name, name)
+    class_variable_set(:@@_permission_action, fix_action)
+  end
+end
