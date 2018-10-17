c341bd23878f29bb470b0789f5ab34f3ec196b01
NAKANO Hideo <nakano@web-tips.co.jp>
2018-10-17 16:58:06 +0900

    [add] `use_gws_monitor` permission

--- app/models/gws/monitor/initializer.rb
+++ app/models/gws/monitor/initializer.rb
@@ -1,5 +1,7 @@
 module Gws::Monitor
   class Initializer
+    Gws::Role.permission :use_gws_monitor, module_name: 'gws/monitor'
+
     Gws::Role.permission :read_other_gws_monitor_posts, module_name: 'gws/monitor'
     Gws::Role.permission :read_private_gws_monitor_posts, module_name: 'gws/monitor'
     Gws::Role.permission :edit_other_gws_monitor_posts, module_name: 'gws/monitor'
