1ceb78cc53692e3d44aadcf1dbb9cef890a5de77
itowtips <ito@web-tips.co.jp>
2018-10-19 14:04:28 +0900

    [add] mail_page : urgency setting

--- app/models/concerns/urgency/addon/layout.rb
+++ app/models/concerns/urgency/addon/layout.rb
@@ -5,8 +5,8 @@ module Urgency::Addon
 
     included do
       belongs_to :urgency_default_layout, class_name: "Cms::Layout"
-      permit_params :urgency_default_layout_id
-
+      belongs_to :urgency_mail_page_layout, class_name: "Cms::Layout"
+      permit_params :urgency_default_layout_id, :urgency_mail_page_layout_id
       validates :urgency_default_layout_id, presence: true
     end
 
