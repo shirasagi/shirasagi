a33c7e4b2e0de9f650018b0b098862004153d701
NAKANO Hideo <pinarello.marvel@gmail.com>
2018-11-29 13:06:02 +0900

    [fix] member photo slide issue (#2396)
    * [fix] unable to affect `kv_speed` and `kv_pause` settings
    
    * [modify] remove `key_visual/page_list` addon from `member/photo_search` part.
    
    this add on is never used in that part

--- app/models/member/part.rb
+++ app/models/member/part.rb
@@ -42,7 +42,6 @@ module Member::Part
 
   class PhotoSearch
     include Cms::Model::Part
-    include KeyVisual::Addon::PageList
     include Cms::Addon::Release
     include Cms::Addon::GroupPermission
     include History::Addon::Backup
