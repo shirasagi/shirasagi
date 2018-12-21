98198c4ae686e0319e9a0e1f7bbc3070e637932f
NAKANO Hideo <pinarello.marvel@gmail.com>
2018-12-21 13:59:25 +0900

    [gws] launch shirasagi webmail when email address on schedule is clicked (#2453)
    * [modify] launch shirasagi webmail by clicking email on a schedule board
    
    when email address on schedule board is clicked, external MTA is always launched even if webmail is active.
    
    * [modify] refactor with rubocop

---
+++ app/assets/javascripts/gws/schedule/lib/integration.js
@@ -0,0 +1,36 @@
+function Gws_Schedule_Integration() {
+}
+
+Gws_Schedule_Integration.paths = {};
+
+Gws_Schedule_Integration.render = function() {
+  var $el = $(".gws-schedule-box");
+  if (! $el[0]) {
+    return;
+  }
+
+  $el.find(".send-message").each(function() {
+    $(this).on("click", function(ev) {
+      var userId = $(this).closest("[data-user-id]").data("user-id");
+      location.href = Gws_Schedule_Integration.paths.newMemoMessage + "?to%5B%5D=" + userId;
+    });
+  });
+
+  $el.find(".send-email").each(function() {
+    $(this).on("click", function(ev) {
+      var email = $(this).closest("[data-email]").data("email");
+      location.href = Gws_Schedule_Integration.paths.newWebmail + "?item%5Bto%5D=" + encodeURIComponent(email);
+    });
+  });
+
+  $el.find(".copy-email-address").each(function() {
+    $(this).on("click", function(ev) {
+      $(this).closest("[data-email]").find(".clipboard-copy-button").click();
+
+      $(".dropdown, .dropdown-menu").removeClass('active');
+
+      ev.preventDefault();
+      return false;
+    });
+  });
+};
