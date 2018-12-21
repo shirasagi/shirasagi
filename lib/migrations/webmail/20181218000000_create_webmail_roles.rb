1257e779a97c2a8bbca7314c0c255a863ba3fd7b
itowtips <ito@web-tips.co.jp>
2018-12-21 09:45:37 +0900

    [modify] add webmail roles migration (#2450)

---
+++ lib/migrations/webmail/20181218000000_create_webmail_roles.rb
@@ -0,0 +1,18 @@
+class SS::Migration20181218000000
+  def change
+    return if Webmail::Role.count > 0
+
+    webmail_r01 = Webmail::Role.new(
+      name: I18n.t('webmail.roles.admin'), permissions: Webmail::Role.permission_names, permission_level: 3
+    )
+    webmail_r01.save!
+
+    user_ids = Webmail::User.pluck(:id)
+    user_ids.each do |id|
+      user = Webmail::User.find(id) rescue nil
+      next unless user
+
+      user.add_to_set(webmail_role_ids: webmail_r01.id)
+    end
+  end
+end
