9961c8489a778d70a3a9e8193394b8717b34e073
snkmr <nakamura@web-tips.co.jp>
2019-01-10 16:25:17 +0900

    [modify] bin/install.sh README.md

--- bin/install.sh
+++ bin/install.sh
@@ -31,7 +31,7 @@ sudo yum -y install \
 
 for i in $(seq 1 3)
 do
-  curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+  gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
   if [ $? -eq 0 ]; then
     break
   fi
