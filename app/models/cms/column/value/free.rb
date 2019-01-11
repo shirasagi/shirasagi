5963bc3e65e3526c677b6bbcea550ff91857027f
Miyasaka <miyasaka@shiolab.com>
2019-01-11 17:45:51 +0900

    [add] cms column: free

---
+++ app/models/cms/column/value/free.rb
@@ -0,0 +1,27 @@
+class Cms::Column::Value::Free < Cms::Column::Value::Base
+  field :value, type: String
+
+  permit_values :value
+
+  liquidize do
+    export :value
+  end
+
+  private
+
+  def validate_value
+    return if column.blank?
+
+    if column.required? && value.blank?
+      self.errors.add(:base, :blank)
+    end
+
+    return if value.blank?
+
+    if column.max_length.present? && column.max_length > 0
+      if value.length > column.max_length
+        self.errors.add(:value, :less_than_or_equal_to, count: column.max_length)
+      end
+    end
+  end
+end
