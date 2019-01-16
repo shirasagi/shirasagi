2e9f1f83c8f6ca356b8d7a0da78d429f2ddad119
kanahebi <hebibibibeam1109@gmail.com>
2019-01-16 19:42:04 +0900

    [add] cms init column
    * [add] cms column: list
    
    * [add] cms column: youtube
    
    * [add] cms column: free
    
    * [add] cms column: file_upload
    
    * [add] cms init column

---
+++ app/models/concerns/ss/model/init_column.rb
@@ -0,0 +1,34 @@
+module SS::Model::InitColumn
+  extend ActiveSupport::Concern
+  extend SS::Translation
+  include SS::Document
+
+  included do
+    attr_accessor :cur_form
+
+    belongs_to :form, polymorphic: true
+    field :order, type: Integer
+    belongs_to :column, class_name: 'Cms::Column::Base', polymorphic: true
+
+    permit_params :order, :column_id
+
+    before_validation :set_form_id, if: ->{ @cur_form }
+
+    validates :form_id, presence: true
+    validates :column_id, presence: true
+    validates :order, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 999_999, allow_blank: true }
+
+    scope :form, ->(form) { where(form_id: form.id, form_type: form.class.name) }
+  end
+
+  def name
+    column.try(:name)
+  end
+
+  private
+
+  def set_form_id
+    return unless @cur_form
+    self.form = @cur_form
+  end
+end
