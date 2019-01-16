c0639ead905e5da3bd0c070659881d4b75bc942a
NAKANO Hideo <nakano@web-tips.co.jp>
2019-01-16 14:28:34 +0900

    [modify] relax `_type` field treatment

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -1,18 +1,49 @@
 # Require `belongs_to` associations by default. Previous versions had false.
 Mongoid::Config.belongs_to_required_by_default = false
 
-#module SkipMongoidQueryCache
-#  private
-#
-#  def system_collection?
-#    return true if cache_skip?
-#    super
-#  end
-#
-#  def cache_skip?
-#    return true if selector.reject { |k, v| k == '_id' }.present?
-#    return true if options.reject { |k, v| k == 'limit' }.present?
-#  end
-#end
 
-#Mongo::Collection::View.__send__(:include, SkipMongoidQueryCache)
+# monkey patch to Mongid::Factory to relax `_type` field treatment.
+#
+# In a standard Mongid::Factory, exception has occurred and stop processing a request
+# if there are no classes matching with `_type` field.
+#
+# But in usual SHIRASAGI cases (especially development),
+# it can sometimes be met the condition described above.
+#
+# This monkehy patch relaxes `_type` field treatment not to occur any exceptions.
+#
+module Mongoid
+  module Factory
+    alias build_without_shirasagi build
+    alias from_db_without_shirasagi from_db
+
+    def build(klass, attributes = nil)
+      attributes ||= {}
+      type = attributes[TYPE] || attributes[TYPE.to_sym]
+      if type && klass._types.include?(type)
+        effective_kass = type.camelize.constantize rescue klass
+      else
+        effective_kass = klass
+      end
+      effective_kass.new(attributes)
+    end
+
+    def from_db(klass, attributes = nil, criteria = nil)
+      selected_fields = criteria.options[:fields] if criteria
+      attributes ||= {}
+      type = attributes[TYPE] || attributes[TYPE.to_sym]
+      if type && klass._types.include?(type)
+        effective_kass = type.camelize.constantize rescue klass
+      else
+        effective_kass = klass
+      end
+
+      obj = effective_kass.instantiate(attributes, selected_fields)
+      if criteria && criteria.association && criteria.parent_document
+        obj.set_relation(criteria.association.inverse, criteria.parent_document)
+      end
+
+      obj
+    end
+  end
+end
