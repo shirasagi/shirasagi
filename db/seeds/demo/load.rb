1d94d24bd2154c9abebbda3e6b47362e18d30bfd
NAKANO Hideo <nakano@web-tips.co.jp>
2019-01-21 17:41:06 +0900

    [add] db seed

--- db/seeds/demo/load.rb
+++ db/seeds/demo/load.rb
@@ -1600,5 +1600,22 @@ save_source_cleaner_template name: "style", target_type: "attribute", target_val
 puts "# postal code"
 Sys::PostalCode::OfficialCsvImportJob.import_from_zip("postal_code/13tokyo.zip")
 
+## -------------------------------------
+puts "# word dictionary"
+
+def save_word_dictionary(data)
+  puts data[:name]
+  cond = { site_id: @site.id, name: data[:name] }
+
+  body_file = data.delete(:body_file)
+  data[:body] = ::File.read(body_file)
+
+  item = Cms::WordDictionary.find_or_initialize_by cond
+  puts item.errors.full_messages unless item.update data
+  item
+end
+
+save_word_dictionary name: "機種依存文字", body_file: "#{Rails.root}/db/seeds/cms/word_dictionary/dependent_characters.txt"
+
 @site.editor_css_path = '/css/ckeditor_contents.css'
 @site.update!
--- db/seeds/lp/load.rb
+++ db/seeds/lp/load.rb
@@ -310,6 +310,23 @@ save_editor_template name: "回り込み解除",
   description: "回り込みを解除します",
   html: editor_template_html, order: 30, site_id: @site.id
 
+## -------------------------------------
+puts "# word dictionary"
+
+def save_word_dictionary(data)
+  puts data[:name]
+  cond = { site_id: @site.id, name: data[:name] }
+
+  body_file = data.delete(:body_file)
+  data[:body] = ::File.read(body_file)
+
+  item = Cms::WordDictionary.find_or_initialize_by cond
+  puts item.errors.full_messages unless item.update data
+  item
+end
+
+save_word_dictionary name: "機種依存文字", body_file: "#{Rails.root}/db/seeds/cms/word_dictionary/dependent_characters.txt"
+
 ## editor css path
 @site.editor_css_path = '/css/ckeditor_contents.css'
 @site.update
