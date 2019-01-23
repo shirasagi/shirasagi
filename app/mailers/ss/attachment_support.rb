7d05aaab555056228be3f2b276dd049423b71c2e
NAKANO Hideo <nakano@web-tips.co.jp>
2019-01-23 11:32:03 +0900

    [fix] attachment file with unknown ext error

--- app/mailers/ss/attachment_support.rb
+++ app/mailers/ss/attachment_support.rb
@@ -1,9 +1,11 @@
 module SS::AttachmentSupport
   extend ActiveSupport::Concern
 
+  DEFAULT_BINARY_TYPE = "application/octet-stream".freeze
+
   def add_attachment_file(file)
     part = { body: file.read }
-    content_type = ::SS::MimeType.find(file.name, nil)
+    content_type = ::SS::MimeType.find(file.name, DEFAULT_BINARY_TYPE)
     if content_type.start_with?("text/")
       #
       # There are some bugs in standard mail gem.
@@ -11,6 +13,16 @@ module SS::AttachmentSupport
       #
       enc = ::NKF.guess(part[:body])
       part[:mime_type] = "#{content_type}; charset=\"#{enc}\""
+      part[:transfer_encoding] = "base64"
+      part[:body] = ::Base64.encode64(part[:body])
+    else
+      #
+      # There are some inconvenient manner for binary attachment in standard mail gem.
+      # So, attachment is encoded manually
+      #
+      encoded = ::Mail::Encodings.decode_encode(file.name, :encode)
+
+      part[:mime_type] = "#{content_type}; filename=\"#{encoded}\""
       part[:transfer_encoding] = "base64"
       part[:body] = ::Base64.encode64(part[:body])
     end
--- app/models/ss/mime_type.rb
+++ app/models/ss/mime_type.rb
@@ -8,7 +8,7 @@ class SS::MimeType
         mime_types = ::MIME::Types.type_for(name)
         ret = mime_types.first.content_type rescue nil if mime_types.present?
       end
-      ret || default
+      ret.presence || default
     end
   end
 end
