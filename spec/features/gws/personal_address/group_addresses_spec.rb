7780e5203feb3f4a363654a4a96cb7c6169af344
NAKANO Hideo <pinarello.marvel@gmail.com>
2019-01-25 11:05:26 +0900

    [fix][gws] personal address permission (#2504)
    * [fix] improper spec title
    
    * [add] spec about only `edit_gws_personal_address` given
    
    * [fix] improper permission checking

--- spec/features/gws/personal_address/group_addresses_spec.rb
+++ spec/features/gws/personal_address/group_addresses_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "gws_personal_address_management_group_addresses", type: :feature, dbscope: :example do
+describe "gws_personal_address_group_addresses_addresses", type: :feature, dbscope: :example do
   let(:site) { gws_site }
   let(:address_group) { create :webmail_address_group, cur_user: gws_user }
   let!(:item) { create :webmail_address, address_group_id: address_group.id, cur_user: gws_user }
