8e2859cbd650a81d49c47a6162b8156401c81088
NAKANO Hideo <nakano@web-tips.co.jp>
2019-01-30 20:10:27 +0900

    [fix] invalid redirect

--- app/controllers/cms/apis/preview/redirector_controller.rb
+++ app/controllers/cms/apis/preview/redirector_controller.rb
@@ -4,7 +4,7 @@ class Cms::Apis::Preview::RedirectorController < ApplicationController
   def new_page
     if @cur_node.present?
       location = url_for(controller: "/" + @cur_node.route.pluralize, action: :new) rescue nil
-      location ||= new_node_node_path(cid: @cur_node)
+      location ||= new_node_page_path(cid: @cur_node)
     else
       location = new_cms_page_path
     end
