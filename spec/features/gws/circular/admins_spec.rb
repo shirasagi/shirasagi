24c4c9854188cd995a984373a7af32fc5ef417f6
NAKANO Hideo <nakano@web-tips.co.jp>
2019-02-04 16:17:10 +0900

    [modify] use local for japanese text

--- spec/features/gws/circular/admins_spec.rb
+++ spec/features/gws/circular/admins_spec.rb
@@ -15,13 +15,13 @@ describe "gws_circular_admins", type: :feature, dbscope: :example do
 
     it "#new" do
       visit new_gws_circular_admin_path(site)
-      expect(page).to have_content('基本情報')
+      expect(page).to have_content(I18n.t("ss.basic_info"))
     end
 
     it "#edit" do
       item
       visit edit_gws_circular_admin_path(site, item)
-      expect(page).to have_content('基本情報')
+      expect(page).to have_content(I18n.t("ss.basic_info"))
     end
 
     it "#show" do
