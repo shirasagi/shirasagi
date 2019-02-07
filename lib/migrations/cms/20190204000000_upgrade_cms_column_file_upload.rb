ba63fa6c2d7583048fb3f8eb841294ec7ca8d70a
Miyasaka <miyasaka@shiolab.com>
2019-02-07 10:43:37 +0900

    [fix] cms column file_upload: migration

--- lib/migrations/cms/20190204000000_upgrade_cms_column_file_upload.rb
+++ lib/migrations/cms/20190204000000_upgrade_cms_column_file_upload.rb
@@ -5,9 +5,13 @@ class SS::Migration20190204000000
       Cms::Form.in(id: ids).to_a.each do |item|
         item.columns.each do |column|
           next unless column._type == "Cms::Column::FileUpload"
+          next if column.file_type.present?
           case column.html_tag
-          when "img", "a+img"
+          when "a+img"
             column.file_type = "image"
+          when "img"
+            column.file_type = "image"
+            column.layout = '<img src="{{ value.file.url }}" alt="{{ value.image_text | default: value.file.humanized_name }}" />'
           when "a"
             column.file_type = "attachment"
           end
