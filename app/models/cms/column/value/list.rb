6f1037c43db43fe1743fc69b547e1932a4857feb
NAKANO Hideo <pinarello.marvel@gmail.com>
2019-02-08 21:53:32 +0900

    [modify] cms column list (#2570)
    * [modify] simply cms/column/value/list
    
    * [add] `list_type` to liquid template

--- app/models/cms/column/value/list.rb
+++ app/models/cms/column/value/list.rb
@@ -1,16 +1,19 @@
 class Cms::Column::Value::List < Cms::Column::Value::Base
-  field :lists, type: Cms::Extensions::List
+  field :lists, type: SS::Extensions::Lines
 
-  permit_values lists: [ :order, :text ]
+  permit_values lists: []
 
   liquidize do
+    export :list_type do
+      column.list_type
+    end
     export :lists
   end
 
   private
 
   def text_blank?
-    lists.all?{ |list| list[:text].blank? }
+    lists.all? { |list| list.blank? }
   end
 
   def validate_value
@@ -32,7 +35,7 @@ class Cms::Column::Value::List < Cms::Column::Value::Base
   def to_default_html
     return '' if text_blank?
 
-    li = lists.map { |list| ApplicationController.helpers.content_tag(:li, list[:text]) }.join("\n")
+    li = lists.map { |list| ApplicationController.helpers.content_tag(:li, list) }.join("\n")
     ApplicationController.helpers.content_tag(column.list_type.to_sym, li.html_safe)
   end
 end
