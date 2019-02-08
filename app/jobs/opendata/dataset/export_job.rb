8cc5b6d97e73d8933108b33fb4e2cabb85e4fa3e
itowtips <ito@web-tips.co.jp>
2019-02-08 21:56:15 +0900

    [fix] encode csv

--- app/jobs/opendata/dataset/export_job.rb
+++ app/jobs/opendata/dataset/export_job.rb
@@ -23,14 +23,21 @@ class Opendata::Dataset::ExportJob < Cms::ApplicationJob
   end
 
   def export_datasets
-    data = @items.to_csv.encode("cp932", invalid: :replace, undef: :replace)
-    write_csv("datasets", data)
+    csv = @items.to_csv.encode("cp932", invalid: :replace, undef: :replace)
+    path = "#{@output_dir}/datasets.csv"
+
+    ::File.binwrite(path, csv)
   end
 
   def export_resources
     @items.each do |item|
-      data = resources_to_csv(item.resources).encode("cp932", invalid: :replace, undef: :replace)
-      write_resource_csv("resources", data, item.id)
+      csv = resources_to_csv(item.resources).encode("cp932", invalid: :replace, undef: :replace)
+      dir = "#{@output_dir}/#{item.id}"
+      path = "#{dir}/resources.csv"
+
+      ::FileUtils.mkdir_p(dir)
+      ::File.binwrite(path, csv)
+
       item.resources.each do |resource|
         write_file(resource.file.name, resource.file.path, item.id, resource.id) if resource.file.present?
         write_file(resource.tsv.name, resource.tsv.path, item.id, resource.id) if resource.tsv.present?
@@ -44,15 +51,6 @@ class Opendata::Dataset::ExportJob < Cms::ApplicationJob
     args[:t_uid] = user.id
     args[:link] = ::File.join(@root_url, @output_zip.url)
     Opendata::Mailer.export_datasets_mail(args).deliver_now rescue nil
-  end
-
-  def write_csv(name, data)
-    File.write("#{@output_dir}/#{name}.csv", data, encoding: "cp932")
-  end
-
-  def write_resource_csv(name, data, id)
-    FileUtils.mkdir_p("#{@output_dir}/#{id}")
-    File.write("#{@output_dir}/#{id}/#{name}.csv", data, encoding: "cp932")
   end
 
   def write_file(name, path, dataset_id, resource_id)
@@ -69,14 +67,15 @@ class Opendata::Dataset::ExportJob < Cms::ApplicationJob
 
       data << headers
       resources.each do |item|
-        data << item.id
-        data << item.name
-        data << item.format
-        data << item.license.try(:name)
-        data << item.text
-        data << item.file.try(:name)
-        data << item.tsv.try(:name)
-        data << csv_line(item)
+        line = []
+        line << item.id
+        line << item.name
+        line << item.format
+        line << item.license.try(:name)
+        line << item.text
+        line << item.file.try(:name)
+        line << item.tsv.try(:name)
+        data << line
       end
     end
   end
