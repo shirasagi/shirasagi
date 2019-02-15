c5bee134eb9e59fae9058ed70e4165309383c19e
NAKANO Hideo <nakano@web-tips.co.jp>
2019-02-15 11:33:40 +0900

    [modify] apply `dbscope: :example`

--- spec/features/cms/pages/basic_crud_spec.rb
+++ spec/features/cms/pages/basic_crud_spec.rb
@@ -1,94 +1,103 @@
 require 'spec_helper'
 
-describe "cms/pages" do
+describe "cms/pages", dbscope: :example do
   subject(:site) { cms_site }
-  subject(:item) { Cms::Page.last }
   subject(:index_path) { cms_pages_path site.id }
-  subject(:new_path) { new_cms_page_path site.id }
-  subject(:show_path) { cms_page_path site.id, item }
-  subject(:edit_path) { edit_cms_page_path site.id, item }
-  subject(:delete_path) { delete_cms_page_path site.id, item }
-  subject(:move_path) { move_cms_page_path site.id, item }
-  subject(:copy_path) { copy_cms_page_path site.id, item }
-  subject(:contains_urls_path) { contains_urls_cms_page_path site.id, item }
 
-  context "with auth" do
+  describe "basic crud" do
     before { login_cms_user }
 
-    it "#index" do
-      visit index_path
-      expect(current_path).not_to eq sns_login_path
-    end
-
-    it "#new" do
-      visit new_path
+    it do
+      #
+      # new
+      #
+      visit new_cms_page_path(site.id)
       within "form#item-form" do
         fill_in "item[name]", with: "sample"
         fill_in "item[basename]", with: "sample"
-        click_button "保存"
+        click_button I18n.t("ss.buttons.save")
       end
       expect(status_code).to eq 200
-      expect(current_path).not_to eq new_path
-      expect(page).to have_no_css("form#item-form")
-    end
 
-    it "#show" do
-      visit show_path
+      item = Cms::Page.last
+      expect(item.name).to eq "sample"
+      expect(item.filename).to eq "sample.html"
+
+      #
+      # show
+      #
+      visit cms_page_path(site.id, item)
       expect(status_code).to eq 200
-      expect(current_path).not_to eq sns_login_path
-    end
+      expect(page).to have_content("sample.html")
 
-    it "#edit" do
-      visit edit_path
+      #
+      # edit
+      #
+      visit edit_cms_page_path(site.id, item)
       within "form#item-form" do
         fill_in "item[name]", with: "modify"
-        click_button "保存"
+        click_button I18n.t("ss.buttons.save")
       end
-      expect(current_path).not_to eq sns_login_path
-      expect(page).to have_no_css("form#item-form")
-    end
+      expect(status_code).to eq 200
 
-    it "#move" do
-      visit move_path
+      item.reload
+      expect(item.name).to eq "modify"
+      expect(item.filename).to eq "sample.html"
+
+      #
+      # move
+      #
+      visit move_cms_page_path(site.id, item)
       within "form" do
         fill_in "destination", with: "destination"
-        click_button "保存"
+        click_button I18n.t("ss.buttons.save")
       end
       expect(status_code).to eq 200
-      expect(current_path).to eq move_path
       expect(page).to have_css("form#item-form h2", text: "destination.html")
 
+      item.reload
+      expect(item.name).to eq "modify"
+      expect(item.filename).to eq "destination.html"
+
+      visit move_cms_page_path(site.id, item)
       within "form" do
         fill_in "destination", with: "sample"
-        click_button "保存"
+        click_button I18n.t("ss.buttons.save")
       end
       expect(status_code).to eq 200
-      expect(current_path).to eq move_path
       expect(page).to have_css("form#item-form h2", text: "sample.html")
-    end
 
-    it "#copy" do
-      visit copy_path
+      item.reload
+      expect(item.name).to eq "modify"
+      expect(item.filename).to eq "sample.html"
+
+      #
+      # copy
+      #
+      visit copy_cms_page_path(site.id, item)
       within "form" do
-        click_button "保存"
+        click_button I18n.t("ss.buttons.save")
       end
       expect(status_code).to eq 200
-      expect(current_path).to eq index_path
       expect(page).to have_css("a", text: "[複製] modify")
       expect(page).to have_css(".state", text: "非公開")
-    end
 
-    it "#delete" do
-      visit delete_path
+      expect(Cms::Page.count).to eq 2
+
+      #
+      # contains_urls
+      #
+      visit contains_urls_cms_page_path(site.id, item)
+      expect(status_code).to eq 200
+
+      #
+      # delete
+      #
+      visit delete_cms_page_path(site.id, item)
       within "form" do
-        click_button "削除"
+        click_button I18n.t("ss.buttons.delete")
       end
       expect(current_path).to eq index_path
-    end
-
-    it "#contains_urls" do
-      visit contains_urls_path
-      expect(status_code).to eq 200
     end
   end
 end
