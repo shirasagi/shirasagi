d804b508adf8c896599c046e450522108805c57f
NAKANO Hideo <nakano@web-tips.co.jp>
2019-02-15 11:12:51 +0900

    [modify] split specs to improve manageability

---
+++ spec/features/cms/pages/basic_crud_spec.rb
@@ -0,0 +1,94 @@
+require 'spec_helper'
+
+describe "cms/pages" do
+  subject(:site) { cms_site }
+  subject(:item) { Cms::Page.last }
+  subject(:index_path) { cms_pages_path site.id }
+  subject(:new_path) { new_cms_page_path site.id }
+  subject(:show_path) { cms_page_path site.id, item }
+  subject(:edit_path) { edit_cms_page_path site.id, item }
+  subject(:delete_path) { delete_cms_page_path site.id, item }
+  subject(:move_path) { move_cms_page_path site.id, item }
+  subject(:copy_path) { copy_cms_page_path site.id, item }
+  subject(:contains_urls_path) { contains_urls_cms_page_path site.id, item }
+
+  context "with auth" do
+    before { login_cms_user }
+
+    it "#index" do
+      visit index_path
+      expect(current_path).not_to eq sns_login_path
+    end
+
+    it "#new" do
+      visit new_path
+      within "form#item-form" do
+        fill_in "item[name]", with: "sample"
+        fill_in "item[basename]", with: "sample"
+        click_button "保存"
+      end
+      expect(status_code).to eq 200
+      expect(current_path).not_to eq new_path
+      expect(page).to have_no_css("form#item-form")
+    end
+
+    it "#show" do
+      visit show_path
+      expect(status_code).to eq 200
+      expect(current_path).not_to eq sns_login_path
+    end
+
+    it "#edit" do
+      visit edit_path
+      within "form#item-form" do
+        fill_in "item[name]", with: "modify"
+        click_button "保存"
+      end
+      expect(current_path).not_to eq sns_login_path
+      expect(page).to have_no_css("form#item-form")
+    end
+
+    it "#move" do
+      visit move_path
+      within "form" do
+        fill_in "destination", with: "destination"
+        click_button "保存"
+      end
+      expect(status_code).to eq 200
+      expect(current_path).to eq move_path
+      expect(page).to have_css("form#item-form h2", text: "destination.html")
+
+      within "form" do
+        fill_in "destination", with: "sample"
+        click_button "保存"
+      end
+      expect(status_code).to eq 200
+      expect(current_path).to eq move_path
+      expect(page).to have_css("form#item-form h2", text: "sample.html")
+    end
+
+    it "#copy" do
+      visit copy_path
+      within "form" do
+        click_button "保存"
+      end
+      expect(status_code).to eq 200
+      expect(current_path).to eq index_path
+      expect(page).to have_css("a", text: "[複製] modify")
+      expect(page).to have_css(".state", text: "非公開")
+    end
+
+    it "#delete" do
+      visit delete_path
+      within "form" do
+        click_button "削除"
+      end
+      expect(current_path).to eq index_path
+    end
+
+    it "#contains_urls" do
+      visit contains_urls_path
+      expect(status_code).to eq 200
+    end
+  end
+end
