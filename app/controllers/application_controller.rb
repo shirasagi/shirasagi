81338b4d33366e5ac166848cc4ffd8489f51f76e
NAKANO Hideo <pinarello.marvel@gmail.com>
2019-02-19 20:28:37 +0900

    [fix][#2615] unable to input alphabet at login (#2628)
    * [fix][#2615] unable to input alphabet at login
    
    * [modify] disable autofocus on mobile devices
    
    because keyword doesn't be automatically shown at input field with autofocus
    
    * [modify] remove `browser` method, this method is introduced by browser gem itself
    
    * [modify] enable autofocus only on pc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -85,10 +85,13 @@ class ApplicationController < ActionController::Base
     request.env["HTTP_X_REAL_IP"] || request.remote_addr
   end
 
-  def browser
-    require "browser"
-    Browser.new(request.user_agent, accept_language: request.accept_language)
+  def pc_browser?
+    return @is_pc_browser if !@is_pc_browser.nil?
+
+    platform = browser.platform
+    @is_pc_browser = platform.windows? || platform.mac? || platform.linux?
   end
+  helper_method :pc_browser?
 
   # Accepts the request for Cross-Origin Resource Sharing.
   # @return boolean
