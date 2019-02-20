bf843fb74374bcb452c96c949e6cc0498ea5fad1
NAKANO Hideo <pinarello.marvel@gmail.com>
2019-02-20 16:46:11 +0900

    [fix] lock error in preview edit (#2642)

--- app/controllers/cms/apis/preview/inplace_edit/column_values_controller.rb
+++ app/controllers/cms/apis/preview/inplace_edit/column_values_controller.rb
@@ -23,6 +23,7 @@ class Cms::Apis::Preview::InplaceEdit::ColumnValuesController < ApplicationContr
 
   def set_item
     @item = @cur_page = Cms::Page.site(@cur_site).find(params[:page_id]).becomes_with_route
+    @item.attributes = fix_params
     raise "404" if !@item.respond_to?(:form) || !@item.respond_to?(:column_values)
 
     @model = @item.class
