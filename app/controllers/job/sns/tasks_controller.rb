96d7b8d1ec6cc55c2c89cbb34206daf7ff1f5f3b
Miyasaka <miyasaka@shiolab.com>
2019-02-25 15:34:03 +0900

    [add] task manager

---
+++ app/controllers/job/sns/tasks_controller.rb
@@ -0,0 +1,17 @@
+class Job::Sns::TasksController < ApplicationController
+  include Sns::BaseFilter
+  include Sns::CrudFilter
+  include Job::TasksFilter
+
+  model SS::Task
+  navi_view "sns/main/navi"
+
+  private
+
+  def filter_permission
+  end
+
+  def item_criteria
+    @model.where(user_id: @cur_user.id).exists(at: false)
+  end
+end
