c075812af900e37073f748809933300d7c60e35c
NAKANO Hideo <nakano@web-tips.co.jp>
2019-02-26 15:03:28 +0900

    [add] internal managed link support

--- app/models/cms/column/value/url_field2.rb
+++ app/models/cms/column/value/url_field2.rb
@@ -4,12 +4,18 @@ class Cms::Column::Value::UrlField2 < Cms::Column::Value::Base
   field :link_url, type: String
   field :link_label, type: String
   field :link_target, type: String
+  field :link_item_type, type: String
+  field :link_item_id, type: Object
 
-  permit_values :link_url, :link_label, :link_target
+  attr_accessor :in_link_url
+
+  permit_values :in_link_url, :link_label, :link_target
+
+  before_validation :set_link_url
 
   liquidize do
-    export :link_url
-    export :link_label
+    export :effective_link_url, as: :link_url
+    export :effective_link_label, as: :link_label
     export :link_target
   end
 
@@ -29,12 +35,83 @@ class Cms::Column::Value::UrlField2 < Cms::Column::Value::Base
     attrs
   end
 
+  def effective_link_url
+    link_url.presence || link_item.try(:url)
+  end
+
+  def effective_link_label
+    link_label.presence || link_item.try(:name)
+  end
+
   private
+
+  def set_link_url
+    if in_link_url.blank?
+      self.link_url = nil
+      self.link_item_type = nil
+      self.link_item_id = nil
+      remove_instance_variable :@link_item if defined? @link_item
+      return
+    end
+
+    site = _parent.site || _parent.instance_variable_get(:@cur_site)
+
+    u = URI.parse(in_link_url)
+    if u.relative?
+      node = _parent.parent
+      base_url = node ? node.full_url : site.full_url
+      u = URI.join(base_url, in_link_url)
+    end
+
+    searches = []
+    if u.port == 80 || u.port == 443
+      searches << u.host
+      searches << "#{u.host}:#{u.port}"
+    else
+      searches << "#{u.host}:#{u.port}"
+    end
+
+    if site.domains.any? { |domain| searches.include?(domain) }
+      # internal link
+      filename = u.path[1..-1]
+      content = Cms::Page.site(site).where(filename: filename).first
+      content ||= Cms::Node.site(site).where(filename: filename).first
+      if content.present?
+        self.link_url = nil
+        self.link_item_type = content.collection_name.to_s
+        self.link_item_id = content.id
+        remove_instance_variable :@link_item if defined? @link_item
+        return
+      end
+    end
+
+    # external link
+    self.link_url = in_link_url
+    self.link_item_type = nil
+    self.link_item_id = nil
+    remove_instance_variable :@link_item if defined? @link_item
+  end
+
+  def link_item
+    return if link_item_type.blank? || link_item_id.blank?
+    return @link_item if defined? @link_item
+
+    @link_item ||= begin
+      site = _parent.site || _parent.instance_variable_get(:@cur_site)
+
+      case link_item_type
+      when "cms_pages"
+        Cms::Page.site(site).where(id: link_item_id).first.try(:becomes_with_route)
+      when "cms_nodes"
+        Cms::Node.site(site).where(id: link_item_id).first.try(:becomes_with_route)
+      end
+    end
+  end
 
   def validate_value
     return if column.blank?
 
-    if column.required? && link_url.blank?
+    if column.required? && effective_link_url.blank?
       self.errors.add(:link_url, :blank)
     end
 
@@ -61,9 +138,9 @@ class Cms::Column::Value::UrlField2 < Cms::Column::Value::Base
   end
 
   def to_default_html
-    return '' if link_url.blank?
+    return '' if effective_link_url.blank?
 
     options = html_additional_attr_to_h
-    ApplicationController.helpers.link_to(link_label.presence || link_url, link_url, options)
+    ApplicationController.helpers.link_to(effective_link_label.presence || effective_link_url, effective_link_url, options)
   end
 end
