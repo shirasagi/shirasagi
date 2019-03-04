10c4f6d87ebfe88184a0789b7dc1f18d5eef9abe
NAKANO Hideo <nakano@web-tips.co.jp>
2019-03-04 22:56:50 +0900

    [fix] error on viewing gws/memo/list_message file

--- app/models/concerns/gws/model/memo/message.rb
+++ app/models/concerns/gws/model/memo/message.rb
@@ -224,7 +224,7 @@ module Gws::Model
       return false if self.site_id != opts[:site].id
       return true if member?(user)
 
-      if self.user_id == user.id
+      if self.respond_to?(:user_id) && self.user_id == user.id
         if deleted["sent"]
           return false
         else
