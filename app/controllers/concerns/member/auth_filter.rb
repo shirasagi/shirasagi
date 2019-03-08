5501afcc68800f94b8acf4a59a25dc51976dfbb9
NAKANO Hideo <nakano@web-tips.co.jp>
2019-03-08 19:11:31 +0900

    [fix][#2724] store login member by site in session

--- app/controllers/concerns/member/auth_filter.rb
+++ app/controllers/concerns/member/auth_filter.rb
@@ -1,18 +1,24 @@
 module Member::AuthFilter
   extend ActiveSupport::Concern
 
+  def session_member_key
+    "#{@cur_site.host}.member"
+  end
+
   def get_member_by_session
     return nil unless member_session_alives?
 
-    member_id = session[:member]["member_id"]
+    member_id = session[session_member_key]["member_id"]
     Cms::Member.site(@cur_site).and_enabled.find member_id rescue nil
   end
 
   def member_session_alives?(timestamp = Time.zone.now.to_i)
-    session[:member] && timestamp <= session[:member]["last_logged_in"] + SS.config.cms.session_lifetime
+    return false if !@cur_site
+    session[session_member_key] && timestamp <= session[session_member_key]["last_logged_in"] + SS.config.cms.session_lifetime
   end
 
   def set_last_logged_in(timestamp = Time.zone.now.to_i)
-    session[:member]["last_logged_in"] = timestamp if session[:member]
+    return false if !@cur_site
+    session[session_member_key]["last_logged_in"] = timestamp if session[session_member_key]
   end
 end
