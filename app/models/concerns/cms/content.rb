4c773f4b8118c2816be1f4bc462c2d11d9fd8857
NAKANO Hideo <nakano@web-tips.co.jp>
2019-03-14 19:06:26 +0900

    [add] `for_member` support to show image, download file or other operations

--- app/models/concerns/cms/content.rb
+++ app/models/concerns/cms/content.rb
@@ -8,6 +8,7 @@ module Cms::Content
   include Cms::GroupPermission
   include Cms::Addon::CheckLinks
   include SS::Liquidization
+  include Fs::FilePreviewable
 
   attr_accessor :cur_node, :basename
   attr_accessor :serve_static_relation_files
@@ -218,6 +219,21 @@ module Cms::Content
 
   def node_target_options
     %w(current descendant).map { |m| [ I18n.t("cms.options.node_target.#{m}"), m ] }
+  end
+
+  def file_previewable?(file, user:, member:)
+    if state == "public"
+      if parent = self.parent
+        parent = parent.becomes_with_route
+        if parent.try(:for_member_enabled?)
+          return false if member.blank?
+        end
+      end
+
+      return true
+    end
+
+    false
   end
 
   private
