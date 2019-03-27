24d82dc3ff49840293e87a8793524b9d3dfa820b
NAKANO Hideo <nakano@web-tips.co.jp>
2019-03-27 18:25:34 +0900

    [modify] redirect to 'index' after answered survey

--- app/controllers/gws/survey/files_controller.rb
+++ app/controllers/gws/survey/files_controller.rb
@@ -97,10 +97,7 @@ class Gws::Survey::FilesController < ApplicationController
     new_column_values = @cur_form.build_column_values(custom)
     @item.update_column_values(new_column_values)
     @item.in_updated = params[:_updated] if @item.respond_to?(:in_updated)
-    render_opts = { location: { action: :edit } }
-    if !@cur_form.file_editable?
-      render_opts[:location] = { action: :show }
-    end
+    render_opts = { location: gws_survey_readables_path(s: { answered_state: "" }) }
 
     result = @item.save
     if result
