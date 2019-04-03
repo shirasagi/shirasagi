47981d8a9c17d5d221636c5e7e5f0b8b2dde289a
NAKANO Hideo <nakano@web-tips.co.jp>
2019-04-03 17:41:42 +0900

    [add] cms/lib/column_radio_button

---
+++ app/assets/javascripts/cms/lib/column_radio_button.js
@@ -0,0 +1,12 @@
+Cms_Column_RadioButton = function(el) {
+  this.$el = $(el);
+};
+
+Cms_Column_RadioButton.render = function(el) {
+  var instance = new Cms_Column_RadioButton(el);
+  instance.render();
+  return instance;
+};
+
+Cms_Column_RadioButton.prototype.render = function() {
+};
