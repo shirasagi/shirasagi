9cea13dfeab2bebd23653e59a294b1c1364ba957
itowtips <ito@web-tips.co.jp>
2019-04-03 19:01:12 +0900

    [fix] unable import jmaxml regions

--- app/jobs/jmaxml/forecast_region_import_job.rb
+++ app/jobs/jmaxml/forecast_region_import_job.rb
@@ -3,8 +3,12 @@ class Jmaxml::ForecastRegionImportJob < Cms::ApplicationJob
 
   private
 
+  def model
+    Jmaxml::ForecastRegion
+  end
+
   def import_file
-    table = ::CSV.table(@cur_file.path, converters: nil, encoding: 'SJIS:UTF-8')
+    table = ::CSV.read(@cur_file.path, headers: true, encoding: 'SJIS:UTF-8')
     table.each_with_index do |row, i|
       import_row(row, i)
     end
@@ -12,17 +16,17 @@ class Jmaxml::ForecastRegionImportJob < Cms::ApplicationJob
   end
 
   def import_row(row, index)
-    code = row[:code].presence
-    name = row[:name].presence
+    code = row[model.t(:code)].presence
+    name = row[model.t(:name)].presence
     return if code.blank? || name.blank?
 
     item = Jmaxml::ForecastRegion.site(self.site).where(code: code).first_or_create(name: name)
     item.name = name
-    item.yomi = row[:yomi].presence if row[:yomi].present?
-    item.short_name = row[:short_name].presence if row[:short_name].present?
-    item.short_yomi = row[:short_yomi].presence if row[:short_yomi].present?
-    item.order = row[:order].presence if row[:order].present?
-    item.state = row[:state].presence if row[:state].present?
+    item.yomi = row[model.t(:yomi)].presence if row[model.t(:yomi)].present?
+    item.short_name = row[model.t(:short_name)].presence if row[model.t(:short_name)].present?
+    item.short_yomi = row[model.t(:short_yomi)].presence if row[model.t(:short_yomi)].present?
+    item.order = row[model.t(:order)].presence if row[model.t(:order)].present?
+    item.state = row[model.t(:state)].presence if row[model.t(:state)].present?
     item.save!
   end
 end
