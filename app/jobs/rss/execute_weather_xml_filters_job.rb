1d93d0065c67500086bc4124a392a6e0f29e2d95
itowtips <ito@web-tips.co.jp>
2019-04-04 14:28:32 +0900

    [modify] refactor

--- app/jobs/rss/execute_weather_xml_filters_job.rb
+++ app/jobs/rss/execute_weather_xml_filters_job.rb
@@ -7,13 +7,12 @@ class Rss::ExecuteWeatherXmlFiltersJob < Cms::ApplicationJob
   end
 
   def perform(page_ids)
-    weather_node = node.becomes_with_route
     page_ids.each do |page_id|
       page = Rss::WeatherXmlPage.with_repl_master.find(page_id)
       put_log(page.name)
 
-      context = OpenStruct.new(site: site, user: user, node: weather_node)
-      weather_node.execute_weather_xml_filter(page, context)
+      context = OpenStruct.new(site: site, user: user, node: node)
+      node.execute_weather_xml_filter(page, context)
     end
   end
 end
