909a6ffe213622120f12bec14f2b198f41ac3f25
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2019-04-22 17:06:16 +0900

    [modify] gws schedule main controller condition

--- app/controllers/gws/schedule/main_controller.rb
+++ app/controllers/gws/schedule/main_controller.rb
@@ -2,9 +2,11 @@ class Gws::Schedule::MainController < ApplicationController
   include Gws::BaseFilter
 
   def index
-    if Gws::Schedule::Plan.allowed?(:use, @cur_user, site: @cur_site) && Gws::Schedule::Plan.personal_plan_enabled?(@cur_user, site: @cur_site)
-      redirect_to gws_schedule_plans_path
-      return
+    if Gws::Schedule::Plan.allowed?(:use, @cur_user, site: @cur_site)
+      if Gws::Schedule::Plan.personal_plan_enabled?(@cur_user, site: @cur_site)
+        redirect_to gws_schedule_plans_path
+        return
+      end
     end
 
     if @cur_user.gws_role_permit_any?(@cur_site, :use_private_gws_facility_plans)
