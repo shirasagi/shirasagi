c2ba3e8ca765cfa92b195bfab2e967c891205e05
NAKANO Hideo <nakano@web-tips.co.jp>
2019-05-10 17:41:17 +0900

    [modify] implements gws/schedule/todo_comment

--- app/models/concerns/gws/addon/schedule/todo/comment_post.rb
+++ app/models/concerns/gws/addon/schedule/todo/comment_post.rb
@@ -3,6 +3,6 @@ module Gws::Addon::Schedule::Todo::CommentPost
   extend SS::Addon
 
   included do
-    has_many :comments, class_name: 'Gws::Schedule::TodoComment', dependent: :destroy
+    has_many :comments, class_name: 'Gws::Schedule::TodoComment', dependent: :destroy, validate: false
   end
 end
--- app/models/gws/schedule/todo_comment.rb
+++ app/models/gws/schedule/todo_comment.rb
@@ -9,11 +9,13 @@ class Gws::Schedule::TodoComment
   belongs_to :todo, class_name: 'Gws::Schedule::Todo'
   field :achievement_rate, type: Integer
 
+  permit_params :achievement_rate
+
   before_validation :set_todo_id, if: ->{ @cur_todo }
 
   validates :todo_id, presence: true
   validates :achievement_rate, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100, allow_blank: true }
-  # validates :text, presence: true
+  validates :text, presence: true, if: -> { achievement_rate.blank? }
 
   scope :and_todo, ->(todo) { where( todo_id: todo.id ) }
 
