813b8d1e6bdc2fb48cb655b0a83d47983562f0f0
NAKANO Hideo <nakano@web-tips.co.jp>
2019-05-23 15:00:19 +0900

    [fix] '#readable?': improve custom group support

--- spec/models/gws/readable_setting_spec.rb
+++ spec/models/gws/readable_setting_spec.rb
@@ -64,4 +64,88 @@ describe Gws::Addon::ReadableSetting, type: :model, dbscope: :example do
       expect(item.class.readable(user, site: site).present?).to be_truthy
     end
   end
+
+  describe "#readable?" do
+    let(:folder) { create(:gws_notice_folder) }
+    let(:item) { create(:gws_notice_post, folder: folder) }
+    let(:group) { user.groups.first }
+    let!(:group1) { create :gws_group, name: "#{group.name}/group-#{unique_id}" }
+    let!(:group2) { create :gws_group, name: "#{group.name}/group-#{unique_id}" }
+    let!(:user1) { create :gws_user, group_ids: [ group1.id ], gws_role_ids: user.gws_role_ids }
+    let!(:user2) { create :gws_user, group_ids: [ group2.id ], gws_role_ids: user.gws_role_ids }
+    let!(:cg_by_user) { create :gws_custom_group, member_ids: [ user1.id ], member_group_ids: [] }
+    let!(:cg_by_group) { create :gws_custom_group, member_ids: [], member_group_ids: [ group1.id ] }
+
+    it do
+      expect(item.class.readable_setting_included_custom_groups?).to be_truthy
+    end
+
+    context "with user" do
+      before do
+        item.readable_setting_range = "select"
+        item.readable_member_ids = [ user1.id ]
+        item.readable_group_ids = []
+        item.readable_custom_group_ids = []
+        item.save!
+      end
+
+      it do
+        expect(item.readable?(user1, site: site)).to be_truthy
+        expect(item.readable?(user2, site: site)).to be_falsey
+        expect(item.class.readable(user1, site: site)).to be_present
+        expect(item.class.readable(user2, site: site)).to be_blank
+      end
+    end
+
+    context "with group" do
+      before do
+        item.readable_setting_range = "select"
+        item.readable_member_ids = []
+        item.readable_group_ids = [ group1.id ]
+        item.readable_custom_group_ids = []
+        item.save!
+      end
+
+      it do
+        expect(item.readable?(user1, site: site)).to be_truthy
+        expect(item.readable?(user2, site: site)).to be_falsey
+        expect(item.class.readable(user1, site: site)).to be_present
+        expect(item.class.readable(user2, site: site)).to be_blank
+      end
+    end
+
+    context "with custom_group contains user" do
+      before do
+        item.readable_setting_range = "select"
+        item.readable_member_ids = []
+        item.readable_group_ids = []
+        item.readable_custom_group_ids = [ cg_by_user.id ]
+        item.save!
+      end
+
+      it do
+        expect(item.readable?(user1, site: site)).to be_truthy
+        expect(item.readable?(user2, site: site)).to be_falsey
+        expect(item.class.readable(user1, site: site)).to be_present
+        expect(item.class.readable(user2, site: site)).to be_blank
+      end
+    end
+
+    context "with custom_group contains group" do
+      before do
+        item.readable_setting_range = "select"
+        item.readable_member_ids = []
+        item.readable_group_ids = []
+        item.readable_custom_group_ids = [ cg_by_group.id ]
+        item.save!
+      end
+
+      it do
+        expect(item.readable?(user1, site: site)).to be_truthy
+        expect(item.readable?(user2, site: site)).to be_falsey
+        expect(item.class.readable(user1, site: site)).to be_present
+        expect(item.class.readable(user2, site: site)).to be_blank
+      end
+    end
+  end
 end
