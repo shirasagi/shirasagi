96b0a692e67b7b33a441871d0fcf6d48bc54ed95
NAKANO Hideo <nakano@web-tips.co.jp>
2019-05-31 14:14:47 +0900

    [fix] spec failures (#2918)
    * [fix] spec failures: csv import failure
    
    * [fix] spec failure: opendata dataset download aggregation specs
    
    * [fix] spec failure: resource url and full_url
    
    * [fix] typo

--- spec/models/opendata/resource_spec.rb
+++ spec/models/opendata/resource_spec.rb
@@ -154,7 +154,8 @@ describe Opendata::Resource, dbscope: :example do
       ::FileUtils.rm_rf tmpdir
     end
 
-    its(:url) { is_expected.to end_with("/#{URI.escape("index - コピー.html")}") }
-    its(:full_url) { is_expected.to end_with("/#{URI.escape("index - コピー.html")}") }
+    # its(:url) { is_expected.to end_with("/#{URI.escape("index - コピー.html")}") }
+    its(:url) { is_expected.to end_with("/#{SS::FilenameConvertor.convert("index - コピー.html")}") }
+    its(:full_url) { is_expected.to end_with("/#{SS::FilenameConvertor.convert("index - コピー.html")}") }
   end
 end
