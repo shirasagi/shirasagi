a89702be8857844684786b2ff4203eb7f8b57374
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2019-06-04 17:41:31 +0900

    [wip] chat report chart

--- app/assets/javascripts/chat/lib/chart.js
+++ app/assets/javascripts/chat/lib/chart.js
@@ -4,12 +4,46 @@ this.Chat_Chart = (function() {
   Chat_Chart.drawBar = function(selector) {
     $(selector).each(function(){
       var dataColumns = $.parseJSON($(this).attr("data-columns"));
+      var labels = dataColumns[0];
+      var data = dataColumns[1];
+      var borderColor = [dataColumns[2]];
+
+      new Chart(this, {
+        type: 'bar',
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              type: 'line',
+              label: $(this).attr("data-name"),
+              data: data,
+              borderColor : borderColor,
+              fill: false
+             },
+           ],
+        },
+        options: {
+          responsive: true,
+          scales: {
+            yAxes: [{
+              ticks: {
+                stepSize: 1
+              },
+            }],
+          }
+        }
+      });
+    });
+  };
+
+  Chat_Chart.drawPie = function(selector) {
+    $(selector).each(function(){
+      var dataColumns = $.parseJSON($(this).attr("data-columns"));
       var labels = [];
       var data = [];
       var backgroundColor = [];
 
       $.each(dataColumns, function(i,column) {
-        console.log(column);
         labels.push(column[0]);
         data.push(column[1]);
         backgroundColor.push(column[2]);
