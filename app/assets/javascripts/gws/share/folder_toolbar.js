6214c693cd1105452008bf61eeef0fa88dbaa6a4
NAKANO Hideo <nakano@web-tips.co.jp>
2019-06-19 13:02:12 +0900

    [add] folder toolbar implementations

--- app/assets/javascripts/gws/share/folder_toolbar.js
+++ app/assets/javascripts/gws/share/folder_toolbar.js
@@ -11,8 +11,12 @@ Gws_Share_FolderToolbar.prototype.render = function () {
     self.createFolder($(this));
   });
 
+  this.$el.find(".btn-create-root-folder").on("click", function() {
+    self.createFolder($(this));
+  });
+
   this.$el.find(".btn-rename-folder").on("click", function() {
-    self.renameFolder($(this));
+    self.renameFolder($(this), { success: { callback: self.refresh } });
   });
 
   this.$el.find(".btn-delete-folder").on("click", function() {
@@ -20,44 +24,89 @@ Gws_Share_FolderToolbar.prototype.render = function () {
   });
 
   this.$el.find(".btn-edit-folder").on("click", function() {
-    var href = $(this).data("href");
-    if (! href) {
-      return;
-    }
-
-    location.href = href;
+    self.editFolder($(this));
   });
 
   this.$el.find(".btn-refresh-folder").on("click", function() {
-    console.log("on click");
     self.refreshFolder();
   });
 };
 
-Gws_Share_FolderToolbar.prototype.createFolder = function ($button) {
+Gws_Share_FolderToolbar.prototype.createFolder = function ($button, options) {
   var href = $button.data("href");
-  if (! href) {
+  if (href) {
+    location.href = href;
     return;
   }
 
+  var api = $button.data("api");
+  if (! api) {
+    return;
+  }
+
+  var success = $button.data("success");
+  if (! success && options) {
+    success = options.success;
+  }
+  var error = $button.data("error");
+  if (! error && options) {
+    error = options.error;
+  }
+
+  var self = this;
+
   $.colorbox({
-    href: href, open: true, fixed: true, with: "90%", height: "90%",
+    href: api, open: true, fixed: true, with: "90%", height: "90%",
     onComplete: function() {
-      console.log("on complete");
       SS.ajaxForm("#cboxLoadedContent form", {
         success: function(data) {
-          console.log("success");
+          $.colorbox.close();
+          if (success && success.redirect_to) {
+            var id;
+            if (data) {
+              id = data.id;
+              if (! id) {
+                id = data._id;
+              }
+            }
+
+            location.href = success.redirect_to.replace(/:id/, id);
+            return;
+          }
+          if (success && success.reload) {
+            location.reload();
+            return;
+          }
+          if (success && success.callback) {
+            success.callback.call(self, data);
+          }
         },
         error: function(xhr) {
-          console.log("error");
+          $.colorbox.close();
+
+          if (xhr.responseJSON && xhr.responseJSON.length > 0) {
+            alert(xhr.responseJSON.join("\n"));
+          } else if (error && error.message) {
+            alert(error.message);
+          }
         }
       });
     }
   });
 };
 
+Gws_Share_FolderToolbar.prototype.refresh = function (data) {
+  if (! data) {
+    return;
+  }
+
+  this.$el.find(".folder-name").text(data.name);
+  this.refreshFolder();
+};
+
 Gws_Share_FolderToolbar.prototype.renameFolder = Gws_Share_FolderToolbar.prototype.createFolder;
 Gws_Share_FolderToolbar.prototype.deleteFolder = Gws_Share_FolderToolbar.prototype.createFolder;
+Gws_Share_FolderToolbar.prototype.editFolder = Gws_Share_FolderToolbar.prototype.createFolder;
 
 Gws_Share_FolderToolbar.prototype.refreshFolder = function () {
   if (!this.options.treeNavi) {
--- app/controllers/gws/share/apis/folder_crud_controller.rb
+++ app/controllers/gws/share/apis/folder_crud_controller.rb
@@ -7,36 +7,78 @@ class Gws::Share::Apis::FolderCrudController < ApplicationController
   menu_view nil
   layout "ss/ajax"
 
-  def new
-    @addons = []
+  private
+
+  def fix_params
+    { cur_site: @cur_site, cur_user: @cur_user }
+  end
+
+  def set_parent
     if params.key?(:parent_id)
       @parent = @model.find(params[:parent_id])
     end
+  end
+
+  def render_change(result, opts = {})
+    if !result
+      render_update result, opts
+      return
+    end
+
+    location = opts[:location].presence || crud_redirect_url || { action: :show }
+    notice = opts[:notice].presence || t("ss.notice.saved")
+
+    respond_to do |format|
+      format.html { redirect_to location, notice: notice }
+      format.json { render json: @item.to_json, content_type: json_content_type }
+    end
+  end
+
+  public
+
+  def new
+    set_parent
+    if @parent
+      allowed = @parent.allowed?(:edit, @cur_user, site: @cur_site)
+    else
+      allowed = @model.allowed?(:edit, @cur_user, site: @cur_site)
+    end
+    raise "403" unless allowed
+
+    @addons = []
     render file: "new"
   end
 
   def create
-    if params.key?(:parent_id)
-      @parent = @model.find(params[:parent_id])
+    set_parent
+    if @parent
+      allowed = @parent.allowed?(:edit, @cur_user, site: @cur_site)
+    else
+      allowed = @model.allowed?(:edit, @cur_user, site: @cur_site)
     end
+    raise "403" unless allowed
 
     @item = @model.new get_params
-    @item.in_parent = @parent if @parent.present?
+    @item.attributes["action"] = "create"
     @item.group_ids = [ @cur_group.id ]
+    if @parent.present?
+      @item.in_parent = @parent.id
+      @item.share_max_file_size = @parent.share_max_file_size
+      @item.share_max_folder_size = @parent.share_max_folder_size
+      @item.readable_setting_range = @parent.readable_setting_range
+      @item.readable_custom_group_ids = @parent.readable_custom_group_ids
+      @item.readable_group_ids = @parent.readable_group_ids
+      @item.readable_member_ids = @parent.readable_member_ids
+      @item.group_ids = (@item.group_ids + @parent.group_ids).uniq
+      @item.user_ids = (@item.user_ids + @parent.user_ids).uniq
+    end
     render_create @item.save
   end
 
-  # def new_sub
-  #   set_item
-  #   render file: "new"
-  # end
-  #
-  # def create_sub
-  #   set_item
-  # end
-
   def rename
     set_item
+    raise "403" unless @item.allowed?(:edit, @cur_user, site: @cur_site)
+
     @item.in_basename = ::File.basename(@item.name)
     @parent = @item.parent
     @addons = []
@@ -45,18 +87,31 @@ class Gws::Share::Apis::FolderCrudController < ApplicationController
 
   def update
     set_item
+    save_name = @item.name
+    raise "403" unless @item.allowed?(:edit, @cur_user, site: @cur_site)
+
     @item.attributes = get_params
     @item.in_updated = params[:_updated] if @item.respond_to?(:in_updated)
-    return render_update(false) unless @item.allowed?(:edit, @cur_user, site: @cur_site)
-    render_update @item.update
+    @item.in_parent = @item.parent.id if @item.parent.present?
+    # be careful, Gws::Share::Folder has many required magic flags to work properly
+    @item.attributes["before_folder_name"] = save_name
+    @item.attributes["controller"] = "gws/share/folders"
+    @item.attributes["action"] = "update"
+    render_change @item.save
   end
 
   def delete
     set_item
+    raise "403" unless @item.allowed?(:delete, @cur_user, site: @cur_site)
+
     render file: "delete"
   end
 
   def destroy
     set_item
+    raise "403" unless @item.allowed?(:delete, @cur_user, site: @cur_site)
+
+    @item.attributes["action"] = "destroy"
+    render_destroy @item.destroy
   end
 end
