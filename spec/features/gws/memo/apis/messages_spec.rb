0c9622591503cbad1ddb1e5949cc932a24089a2e
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2019-07-02 16:03:53 +0900

    [modify] export INBOX.Trash messages

--- spec/features/gws/memo/apis/messages_spec.rb
+++ spec/features/gws/memo/apis/messages_spec.rb
@@ -5,9 +5,10 @@ describe "gws_memo_apis_messages", type: :feature, dbscope: :example do
   let(:user1) { gws_user }
   let(:user2) { create(:gws_user) }
   let(:path) { gws_memo_apis_messages_path site }
-  let!(:memo) { create(:gws_memo_message, user: user2, site: site, in_to_members: [user1.id.to_s]) }
-  let!(:draft_memo) { create(:gws_memo_message, :with_draft, user: user1, site: site, in_to_members: [user2.id.to_s]) }
-  let!(:sent_memo) { create(:gws_memo_message, user: user1, site: site, in_to_members: [user2.id.to_s]) }
+  let!(:memo) { create(:gws_memo_message, user: user2, site: site) }
+  let!(:draft_memo) { create(:gws_memo_message, :with_draft, site: site, in_to_members: [user2.id.to_s]) }
+  let!(:sent_memo) { create(:gws_memo_message, site: site, in_to_members: [user2.id.to_s]) }
+  let!(:trash_memo) { create(:gws_memo_message, user: user2, site: site, in_path: { user1.id.to_s => 'INBOX.Trash' }) }
 
   context "with auth" do
     before { login_gws_user }
@@ -17,18 +18,28 @@ describe "gws_memo_apis_messages", type: :feature, dbscope: :example do
       expect(page).to have_content(memo.subject)
       expect(page).to have_no_content(draft_memo.subject)
       expect(page).to have_no_content(sent_memo.subject)
+      expect(page).to have_no_content(trash_memo.subject)
 
       select I18n.t('gws/memo/folder.inbox_draft')
       click_button I18n.t('ss.buttons.search')
       expect(page).to have_no_content(memo.subject)
       expect(page).to have_content(draft_memo.subject)
       expect(page).to have_no_content(sent_memo.subject)
+      expect(page).to have_no_content(trash_memo.subject)
 
       select I18n.t('gws/memo/folder.inbox_sent')
       click_button I18n.t('ss.buttons.search')
       expect(page).to have_no_content(memo.subject)
       expect(page).to have_no_content(draft_memo.subject)
       expect(page).to have_content(sent_memo.subject)
+      expect(page).to have_no_content(trash_memo.subject)
+
+      select I18n.t('gws/memo/folder.inbox_trash')
+      click_button I18n.t('ss.buttons.search')
+      expect(page).to have_no_content(memo.subject)
+      expect(page).to have_no_content(draft_memo.subject)
+      expect(page).to have_no_content(sent_memo.subject)
+      expect(page).to have_content(trash_memo.subject)
     end
   end
 end
