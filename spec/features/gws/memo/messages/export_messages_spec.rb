a2a6ed2059e13688157e23dc9a962a1f695711a4
NAKANO Hideo <nakano@web-tips.co.jp>
2019-07-02 20:15:57 +0900

    [add] export all messages feature

--- spec/features/gws/memo/messages/export_messages_spec.rb
+++ spec/features/gws/memo/messages/export_messages_spec.rb
@@ -9,66 +9,67 @@ describe 'gws_memo_messages', type: :feature, dbscope: :example, js: true do
     site.save!
   end
 
-  def export_memo(memo)
-    visit gws_memo_export_messages_path(site)
-    within "form#item-form" do
-      select 'eml', from: 'item_format'
-      click_on I18n.t("ss.links.select")
-    end
-    within "#cboxLoadedContent" do
-      expect(page).to have_css(".list-item", text: memo.subject)
-      click_on memo.subject
-    end
-    within "form#item-form" do
-      perform_enqueued_jobs do
-        click_on I18n.t("ss.export")
-      end
-    end
-
-    expect(page).to have_css("#notice", text: I18n.t("gws/memo/message.notice.start_export"))
-    within '#addon-basic' do
-      expect(page).to have_content(I18n.t("gws/memo/message.export.start_message").split("\n").first)
-    end
-
-    expect(Job::Log.count).to eq 1
-    Job::Log.first.tap do |log|
-      expect(log.logs).to include(include("INFO -- : Started Job"))
-      expect(log.logs).to include(include("INFO -- : Completed Job"))
-    end
-
-    page.execute_script("SS.clearNotice();")
-
-    within "nav.user" do
-      first(".gws-memo-notice.popup-notice-container a").click
-
-      within ".popup-notice-items .list-item.unseen" do
-        click_on I18n.t("gws/memo/message.export.subject")
-      end
-    end
-
-    within ".gws-memo-notices .body" do
-      expect(page).to have_content(I18n.t("gws/memo/message.export.notify_message", link: "").split("\n").first)
-      expect(page).to have_link(href: /\.zip$/)
-      first("a").click
-    end
-
-    wait_for_download
-
-    exported = {}
-    Zip::File.open(downloads.first) do |zip_file|
-      zip_file.each do |entry|
-        name = NKF.nkf("-w", entry.name)
-        mail = ::Mail.read_from_string(entry.get_input_stream.read)
-
-        exported[name] = mail
-      end
-    end
-
-    exported
-  end
-
-  describe 'export' do
+  describe 'export selected message' do
     before { login_gws_user }
+
+    def export_memo(memo)
+      visit gws_memo_export_messages_path(site)
+      within "form#item-form" do
+        select 'eml', from: 'item_format'
+        choose "item_export_filter_selected"
+        click_on I18n.t("ss.links.select")
+      end
+      within "#cboxLoadedContent" do
+        expect(page).to have_css(".list-item", text: memo.subject)
+        click_on memo.subject
+      end
+      within "form#item-form" do
+        perform_enqueued_jobs do
+          click_on I18n.t("ss.export")
+        end
+      end
+
+      expect(page).to have_css("#notice", text: I18n.t("gws/memo/message.notice.start_export"))
+      within '#addon-basic' do
+        expect(page).to have_content(I18n.t("gws/memo/message.export.start_message").split("\n").first)
+      end
+
+      expect(Job::Log.count).to eq 1
+      Job::Log.first.tap do |log|
+        expect(log.logs).to include(include("INFO -- : Started Job"))
+        expect(log.logs).to include(include("INFO -- : Completed Job"))
+      end
+
+      page.execute_script("SS.clearNotice();")
+
+      within "nav.user" do
+        first(".gws-memo-notice.popup-notice-container a").click
+
+        within ".popup-notice-items .list-item.unseen" do
+          click_on I18n.t("gws/memo/message.export.subject")
+        end
+      end
+
+      within ".gws-memo-notices .body" do
+        expect(page).to have_content(I18n.t("gws/memo/message.export.notify_message", link: "").split("\n").first)
+        expect(page).to have_link(href: /\.zip$/)
+        first("a").click
+      end
+
+      wait_for_download
+
+      exported = {}
+      Zip::File.open(downloads.first) do |zip_file|
+        zip_file.each do |entry|
+          name = NKF.nkf("-w", entry.name)
+          mail = ::Mail.read_from_string(entry.get_input_stream.read)
+
+          exported[name] = mail
+        end
+      end
+
+      exported
+    end
 
     context "with unseen message" do
       let!(:memo) do
@@ -223,6 +224,95 @@ describe 'gws_memo_messages', type: :feature, dbscope: :example, js: true do
           body.force_encoding("UTF-8")
           expect(body).to include(memo.html)
         end
+      end
+    end
+  end
+
+  describe 'export all message' do
+    let(:user1) { create :gws_user, gws_role_ids: gws_user.gws_role_ids }
+    let!(:memo1) do
+      create(:gws_memo_message, user: user, site: site, subject: ss_japanese_text, text: ss_japanese_text)
+    end
+    let!(:memo2) do
+      create(:gws_memo_message, user: user, site: site, subject: ss_japanese_text, text: ss_japanese_text)
+    end
+    let!(:memo3) do
+      create(:gws_memo_message, user: user, site: site, subject: ss_japanese_text, text: ss_japanese_text)
+    end
+    let!(:memo4) do
+      create(
+        :gws_memo_message, user: user, site: site, subject: ss_japanese_text, text: ss_japanese_text,
+        user_settings: [{ "user_id" => user1.id, "path" => 'INBOX.Sent' }],
+        in_to_members: [user1.id.to_s]
+      )
+    end
+
+    before { login_gws_user }
+
+    it do
+      visit gws_memo_export_messages_path(site)
+      within "form#item-form" do
+        select 'eml', from: 'item_format'
+        choose "item_export_filter_all"
+        perform_enqueued_jobs do
+          click_on I18n.t("ss.export")
+        end
+      end
+
+      expect(page).to have_css("#notice", text: I18n.t("gws/memo/message.notice.start_export"))
+      within '#addon-basic' do
+        expect(page).to have_content(I18n.t("gws/memo/message.export.start_message").split("\n").first)
+      end
+
+      expect(Job::Log.count).to eq 1
+      Job::Log.first.tap do |log|
+        expect(log.logs).to include(include("INFO -- : Started Job"))
+        expect(log.logs).to include(include("INFO -- : Completed Job"))
+      end
+
+      page.execute_script("SS.clearNotice();")
+
+      within "nav.user" do
+        first(".gws-memo-notice.popup-notice-container a").click
+
+        within ".popup-notice-items .list-item.unseen" do
+          click_on I18n.t("gws/memo/message.export.subject")
+        end
+      end
+
+      within ".gws-memo-notices .body" do
+        expect(page).to have_content(I18n.t("gws/memo/message.export.notify_message", link: "").split("\n").first)
+        expect(page).to have_link(href: /\.zip$/)
+        first("a").click
+      end
+
+      wait_for_download
+
+      exported = {}
+      Zip::File.open(downloads.first) do |zip_file|
+        zip_file.each do |entry|
+          name = NKF.nkf("-w", entry.name)
+          mail = ::Mail.read_from_string(entry.get_input_stream.read)
+
+          exported[name] = mail
+        end
+      end
+
+      expect(exported.keys).to have(3).items
+      memo1.subject.tap do |subject|
+        filename = subject.encode('cp932', invalid: :replace, undef: :replace, replace: "_")
+        filename = filename.encode("UTF-8")
+        expect(exported.keys).to include(include(filename))
+      end
+      memo2.subject.tap do |subject|
+        filename = subject.encode('cp932', invalid: :replace, undef: :replace, replace: "_")
+        filename = filename.encode("UTF-8")
+        expect(exported.keys).to include(include(filename))
+      end
+      memo3.subject.tap do |subject|
+        filename = subject.encode('cp932', invalid: :replace, undef: :replace, replace: "_")
+        filename = filename.encode("UTF-8")
+        expect(exported.keys).to include(include(filename))
       end
     end
   end
