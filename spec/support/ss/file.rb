f0e0b8c2585e042c2de48979f0125d9d13eb6d26
NAKANO Hideo <nakano@web-tips.co.jp>
2019-07-11 16:09:31 +0900

    [modify] set special temp dir to SS::DownloadJobFile#root
    to protect from overwriting download files while specs are executing

--- spec/support/ss/file.rb
+++ spec/support/ss/file.rb
@@ -1,21 +1,26 @@
 def root_dir
-  "#{Rails.root}/tmp/ss_files"
+  "#{Rails.root}/tmp/rspec-#{Process.pid}"
 end
 
 def clear_files
   ::Fs.rm_rf(root_dir)
-  ::Fs.mkdir_p(root_dir)
 end
 
 RSpec.configuration.before(:suite) do
   # load all models
   ::Rails.application.eager_load!
 
-  clear_files
+  ::Fs.mkdir_p(root_dir)
 
-  models = ::Mongoid.models
-  models = models.select { |model| model.ancestors.include?(::SS::Model::File) }
+  all_models = ::Mongoid.models
+  models = all_models.select { |model| model.ancestors.include?(::SS::Model::File) }
   models.each do |model|
-    model.root = root_dir
+    model.root = "#{root_dir}/files"
   end
+
+  SS::DownloadJobFile.root = "#{root_dir}/download"
+end
+
+RSpec.configuration.after(:suite) do
+  clear_files
 end
