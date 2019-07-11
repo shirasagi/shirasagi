7b401e4224a0e800689ad8609bcf73052073cdc8
NAKANO Hideo <nakano@web-tips.co.jp>
2019-07-11 20:51:24 +0900

    [fix] facility_column_values's improper exports
    and also fix `notify_state` is not exported

--- app/models/gws/schedule/plan_csv/exporter.rb
+++ app/models/gws/schedule/plan_csv/exporter.rb
@@ -5,24 +5,6 @@ class Gws::Schedule::PlanCsv::Exporter
   attr_accessor :criteria
 
   class << self
-    def csv_basic_headers
-      headers = %w(
-        id state name start_on end_on start_at end_at allday
-        category_id priority color text_type text
-        attendance_check_state
-        member_ids member_custom_group_ids
-        facility_ids main_facility_id facility_column_values
-        readable_setting_range readable_member_ids readable_group_ids readable_custom_group_ids
-        custom_group_ids group_ids user_ids permission_level
-      )
-      #repeat_plan_id, file_ids
-      headers.map! { |k| Gws::Schedule::Plan.t(k) }
-    end
-
-    def csv_headers(opts = {})
-      new(opts).csv_headers
-    end
-
     def enum_csv(criteria, opts = {})
       opts = opts.dup
       opts[:criteria] = criteria
@@ -34,55 +16,154 @@ class Gws::Schedule::PlanCsv::Exporter
     end
   end
 
-  def csv_headers
-    self.class.csv_basic_headers
-  end
-
   def enum_csv
-    Enumerator.new do |y|
-      y << encode_sjis(csv_headers.to_csv)
-      @criteria.each do |item|
-        next unless item.readable?(user, site: site)
-        y << encode_sjis(item_to_csv(item).to_csv)
-      end
+    facility_ids = self.criteria.pluck(:main_facility_id)
+    facility_ids.uniq!
+    facility_ids.compact!
+    facilities = Gws::Facility::Item.site(site).in(id: facility_ids).order_by(order: 1, id: 1)
+
+    drawer = SS::Csv.draw(:export, context: self) do |drawer|
+      draw_basic(drawer)
+      draw_reminder(drawer)
+      draw_schedule_repeat(drawer)
+      draw_notify_setting(drawer)
+      draw_markdown(drawer)
+      draw_file(drawer)
+      draw_schedule_reports(drawer)
+      draw_member(drawer)
+      draw_schedule_attendance(drawer)
+      draw_schedule_facility(drawer)
+      draw_schedule_facility_column_values(drawer, facilities)
+      draw_schedule_approval(drawer)
+      draw_readable_setting(drawer)
+      draw_group_permission(drawer)
     end
+
+    drawer.enum(self.criteria)
   end
 
   private
 
-  def item_to_csv(item)
-    terms = []
-    terms << item.id
-    terms << item.state
-    terms << item.name
-    terms << item.start_on
-    terms << item.end_on
-    terms << item.start_at
-    terms << item.end_at
-    terms << item.allday
-    terms << item.category_id
-    terms << item.priority
-    terms << item.color
-    terms << item.text_type
-    terms << item.text
-    terms << item.attendance_check_state
-    terms << item.member_ids.to_json
-    terms << item.member_custom_group_ids.to_json
-    terms << item.facility_ids.to_json
-    terms << item.main_facility_id
-    terms << item.facility_column_values.to_json
-    terms << item.readable_setting_range
-    terms << item.readable_member_ids.to_json
-    terms << item.readable_group_ids.to_json
-    terms << item.readable_custom_group_ids.to_json
-    terms << item.custom_group_ids.to_json
-    terms << item.group_ids.to_json
-    terms << item.user_ids.to_json
-    terms << item.permission_level
-    terms
+  def draw_basic(drawer)
+    drawer.column :id
+    drawer.column :name
+    drawer.column :start_on
+    drawer.column :end_on
+    drawer.column :start_at
+    drawer.column :end_at
+    drawer.column :allday, type: :label
+    drawer.column :category_id do
+      drawer.body { |item| Gws::Schedule::Category.where(id: item.category_id).pluck(:name).first }
+    end
+    drawer.column :priority, type: :label
+    drawer.column :color
+    # drawer.column :state do
+    #   drawer.body { |item| I18n.t(item.state, scope: "ss.options.state") }
+    # end
   end
 
-  def encode_sjis(str)
-    str.encode("SJIS", invalid: :replace, undef: :replace)
+  def draw_reminder(drawer)
+  end
+
+  def draw_schedule_repeat(drawer)
+  end
+
+  def draw_notify_setting(drawer)
+    drawer.column :notify_state, type: :label
+  end
+
+  def draw_markdown(drawer)
+    drawer.column :text_type, type: :label
+    drawer.column :text
+  end
+
+  def draw_file(drawer)
+  end
+
+  def draw_schedule_reports(drawer)
+  end
+
+  def draw_member(drawer)
+    drawer.column :member_custom_group_ids do
+      drawer.body { |item| item.custom_groups.pluck(:name).join("\n") }
+    end
+    drawer.column :member_group_ids do
+      drawer.body { |item| item.member_groups.pluck(:name).join("\n") }
+    end
+    drawer.column :member_ids do
+      drawer.body { |item| item.members.map(&:long_name).join("\n") }
+    end
+  end
+
+  def draw_schedule_attendance(drawer)
+    drawer.column :attendance_check_state, type: :label
+  end
+
+  def draw_schedule_facility(drawer)
+    drawer.column :facility_ids do
+      drawer.body { |item| item.facilities.pluck(:name).join("\n") }
+    end
+    drawer.column :main_facility_id do
+      drawer.body { |item| item.main_facility.try(:name) }
+    end
+  end
+
+  def draw_schedule_facility_column_values(drawer, facilities)
+    return if facilities.blank?
+
+    facilities.each do |facility|
+      next if facility.columns.blank?
+
+      facility.columns.order_by(order: 1, name: 1).each do |column|
+        drawer.column "#{facility.name}/#{column.name}" do
+          drawer.head do
+            "#{facility.name}/#{column.name}"
+          end
+          drawer.body do |item|
+            find_facility_column_value(item, facility, column).try do |v|
+              v.value
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def draw_schedule_approval(drawer)
+    drawer.column :approval_member_ids do
+      drawer.body { |item| item.approval_members.map(&:long_name).join("\n") }
+    end
+  end
+
+  def draw_readable_setting(drawer)
+    drawer.column :readable_setting_range, type: :label
+    drawer.column :readable_custom_group_ids do
+      drawer.body { |item| item.readable_custom_groups.pluck(:name).join("\n") }
+    end
+    drawer.column :readable_group_ids do
+      drawer.body { |item| item.readable_groups.pluck(:name).join("\n") }
+    end
+    drawer.column :readable_member_ids do
+      drawer.body { |item| item.readable_members.map(&:long_name).join("\n") }
+    end
+  end
+
+  def draw_group_permission(drawer)
+    drawer.column :custom_group_ids do
+      drawer.body { |item| item.custom_groups.pluck(:name).join("\n") }
+    end
+    drawer.column :group_ids do
+      drawer.body { |item| item.groups.pluck(:name).join("\n") }
+    end
+    drawer.column :user_ids do
+      drawer.body { |item| item.users.map(&:long_name).join("\n") }
+    end
+    drawer.column :permission_level
+  end
+
+  def find_facility_column_value(item, facility, column)
+    return if item.main_facility_id != facility.id
+
+    item.facility_column_values.where(column_id: column.id).first
   end
 end
