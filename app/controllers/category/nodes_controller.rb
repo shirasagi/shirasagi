c67c2c3daf2f040baa77efe9d280eef23bae4abe
itowtips <ito@web-tips.co.jp>
2019-07-16 13:26:43 +0900

    [fix] routing

--- app/controllers/category/nodes_controller.rb
+++ app/controllers/category/nodes_controller.rb
@@ -14,10 +14,6 @@ class Category::NodesController < ApplicationController
     { cur_user: @cur_user, cur_site: @cur_site, cur_node: @cur_node }
   end
 
-  def pre_params
-    { route: "category/node" }
-  end
-
   def redirect_url
     diff = @item.route !~ /^category\//
     diff ? node_node_path(cid: @cur_node, id: @item.id) : { action: :show, id: @item.id }
