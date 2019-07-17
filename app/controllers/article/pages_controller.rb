fe69f1dd6bace3cee4dad8cae7025e3c4e8d5fdc
itowtips <ito@web-tips.co.jp>
2019-07-17 18:32:30 +0900

    [add] article page import task

--- app/controllers/article/pages_controller.rb
+++ app/controllers/article/pages_controller.rb
@@ -14,6 +14,14 @@ class Article::PagesController < ApplicationController
     { cur_user: @cur_user, cur_site: @cur_site, cur_node: @cur_node }
   end
 
+  def set_task
+    @task = Cms::Task.find_or_create_by name: task_name, site_id: @cur_site.id, node_id: @cur_node.id
+  end
+
+  def task_name
+    "article:import_pages"
+  end
+
   public
 
   def download_all
@@ -53,29 +61,42 @@ class Article::PagesController < ApplicationController
   end
 
   def import
+    set_task
+
     @item = @model.new
-    return if request.get?
 
-    begin
-      file = params[:item].try(:[], :file)
-      if file.nil? || ::File.extname(file.original_filename) != ".csv"
-        raise I18n.t("errors.messages.invalid_csv")
+    if request.get?
+      respond_to do |format|
+        format.html { render }
+        format.json { render json: @task.to_json(methods: :head_logs) }
       end
-      if !Article::Page::ImportJob.valid_csv?(file)
-        raise I18n.t("errors.messages.malformed_csv")
+    else
+      begin
+        file = params[:item].try(:[], :file)
+        if file.nil? || ::File.extname(file.original_filename) != ".csv"
+          raise I18n.t("errors.messages.invalid_csv")
+        end
+        if !Article::Page::Importer.valid_csv?(file)
+          raise I18n.t("errors.messages.malformed_csv")
+        end
+
+        # save csv to use in job
+        ss_file = SS::File.new
+        ss_file.in_file = file
+        ss_file.model = "article/import"
+        ss_file.save
+
+        # call job
+        Article::Page::ImportJob.bind(site_id: @cur_site, node_id: @cur_node, user_id: @cur_user).perform_later(file: ss_file.id)
+      rescue => e
+        @item.errors.add :base, e.to_s
       end
 
-      # save csv to use in job
-      ss_file = SS::File.new
-      ss_file.in_file = file
-      ss_file.model = "article/import"
-      ss_file.save
-
-      # call job
-      Article::Page::ImportJob.bind(site_id: @cur_site, node_id: @cur_node, user_id: @cur_user).perform_later(ss_file.id)
-      flash.now[:notice] = I18n.t("ss.notice.started_import")
-    rescue => e
-      @item.errors.add :base, e.to_s
+      if @item.errors.present?
+        render
+      else
+        redirect_to({ action: :import }, { notice: I18n.t("ss.notice.started_import") })
+      end
     end
   end
 end
