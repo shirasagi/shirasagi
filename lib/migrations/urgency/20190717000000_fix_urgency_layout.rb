7b68d16f9577be4ad971e1fc1314a2f7578ebe42
itowtips <ito@web-tips.co.jp>
2019-07-17 10:17:11 +0900

    [modify] migration

--- lib/migrations/urgency/20190717000000_fix_urgency_layout.rb
+++ lib/migrations/urgency/20190717000000_fix_urgency_layout.rb
@@ -1,7 +1,7 @@
 class SS::Migration20190717000000
   def change
     Urgency::Node::Layout.each do |node|
-      next if node.state != "public"
+      next if node.state == "closed"
       node.set(state: "closed")
     end
   end
