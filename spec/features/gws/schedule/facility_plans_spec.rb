3cff4dd152d88291e9917c41983c27b185e049a9
NAKANO Hideo <nakano@web-tips.co.jp>
2019-07-17 19:49:16 +0900

    [add] Gws::Facility::Item#max_days_limit specs

--- spec/features/gws/schedule/facility_plans_spec.rb
+++ spec/features/gws/schedule/facility_plans_spec.rb
@@ -3,14 +3,15 @@ require 'spec_helper'
 describe "gws_schedule_facility_plans", type: :feature, dbscope: :example, js: true do
   let(:site) { gws_site }
   let(:facility) { create :gws_facility_item }
-  let!(:item) { create :gws_schedule_facility_plan, facility_ids: [facility.id] }
-  let(:index_path) { gws_schedule_facility_plans_path site, facility }
-  let(:new_path) { new_gws_schedule_facility_plan_path site, facility }
-  let(:show_path) { gws_schedule_facility_plan_path site, facility, item }
-  let(:edit_path) { edit_gws_schedule_facility_plan_path site, facility, item }
-  let(:delete_path) { soft_delete_gws_schedule_facility_plan_path site, facility, item }
 
   context "with auth" do
+    let!(:item) { create :gws_schedule_facility_plan, facility_ids: [facility.id] }
+    let(:index_path) { gws_schedule_facility_plans_path site, facility }
+    let(:new_path) { new_gws_schedule_facility_plan_path site, facility }
+    let(:show_path) { gws_schedule_facility_plan_path site, facility, item }
+    let(:edit_path) { edit_gws_schedule_facility_plan_path site, facility, item }
+    let(:delete_path) { soft_delete_gws_schedule_facility_plan_path site, facility, item }
+
     before { login_gws_user }
 
     it "#index" do
@@ -77,6 +78,67 @@ describe "gws_schedule_facility_plans", type: :feature, dbscope: :example, js: t
       wait_for_ajax
       expect(current_path).to eq index_path
       expect(page).to have_css('#notice', text: I18n.t('ss.notice.deleted'))
+    end
+  end
+
+  context "with max_days_limit" do
+    let(:now) { Time.zone.now.beginning_of_minute }
+
+    before do
+      facility.max_days_limit = 180
+      facility.save!
+
+      site.schedule_max_years = 10
+      site.save!
+
+      login_gws_user
+    end
+
+    context "when end_at is too far to reserve" do
+      let(:start_at) { end_at - 1.hour }
+      let(:end_at) { now + facility.max_days_limit.days + 1.minute }
+
+      it do
+        Timecop.freeze(now) do
+          visit gws_schedule_facility_plans_path(site: site, facility: facility)
+          click_on I18n.t("gws/schedule.links.add_plan")
+
+          within "#item-form" do
+            fill_in "item[name]", with: unique_id
+            fill_in "item[start_at]", with: I18n.l(start_at, format: :picker)
+            # !!!be cafeful!!!
+            # it is required to input twice
+            fill_in "item[end_at]", with: I18n.l(end_at, format: :picker)
+            fill_in "item[end_at]", with: I18n.l(end_at, format: :picker)
+            click_on I18n.t("ss.buttons.save")
+          end
+          err_msg = I18n.t("gws/schedule.errors.faciliy_day_lte", count: 180)
+          expect(page).to have_css('#errorExplanation', text: err_msg)
+        end
+      end
+    end
+
+    context "when end_at is at the limit" do
+      let(:start_at) { end_at - 1.hour }
+      let(:end_at) { now + facility.max_days_limit.days }
+
+      it do
+        Timecop.freeze(now) do
+          visit gws_schedule_facility_plans_path(site: site, facility: facility)
+          click_on I18n.t("gws/schedule.links.add_plan")
+
+          within "#item-form" do
+            fill_in "item[name]", with: unique_id
+            fill_in "item[start_at]", with: I18n.l(start_at, format: :picker)
+            # !!!be cafeful!!!
+            # it is required to input twice
+            fill_in "item[end_at]", with: I18n.l(end_at, format: :picker)
+            fill_in "item[end_at]", with: I18n.l(end_at, format: :picker)
+            click_on I18n.t("ss.buttons.save")
+          end
+          expect(page).to have_css('#notice', text: I18n.t('ss.notice.saved'))
+        end
+      end
     end
   end
 end
