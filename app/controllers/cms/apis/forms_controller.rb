016d6b13e422af18756bbb383ea8e8cff28b8cb3
itowtips <ito@web-tips.co.jp>
2019-07-18 11:36:08 +0900

    [fix] cms form preview : support column_values (#3014)
    * [fix] cms form preview : support column_values
    
    * [add] spec

--- app/controllers/cms/apis/forms_controller.rb
+++ app/controllers/cms/apis/forms_controller.rb
@@ -43,6 +43,7 @@ class Cms::Apis::FormsController < ApplicationController
 
   def form
     @item = @model.site(@cur_site).find(params[:id])
+    @cur_node = Cms::Node.find(params[:node]).becomes_with_route rescue nil
     @target = Cms::Page.site(@cur_site).find(params[:item_id]).becomes_with_route if params[:item_id].present?
     render layout: false
   end
