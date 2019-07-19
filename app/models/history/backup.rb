eb039bfbbf391f12d21a76ee8d13ea6846f65086
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2019-07-19 19:39:26 +0900

    [wip] restore file when restore cms content

--- app/models/history/backup.rb
+++ app/models/history/backup.rb
@@ -58,9 +58,53 @@ class History::Backup
     end
 
     data.delete("_id")
-    data.delete("file_id")
-    data.delete("file_ids") # TODO: for attachment files
+    # data.delete("file_id")
+    # data.delete("file_ids") # TODO: for attachment files
     data.delete("state")
+
+    ref_class.constantize.relations.each do |k, relation|
+      case relation.class.to_s
+      when Mongoid::Association::Embedded::EmbedsMany.to_s
+        next if data[k].blank?
+        data[k] = data[k].collect do |relation|
+          if relation['_type'].present?
+            relation = relation['_type'].constantize.new(relation)
+          else
+            relation = model.relations[k].class_name.constantize.new(relation)
+          end
+          relation.fields.each do |key, field|
+            if field.type == SS::Extensions::ObjectIds
+              klass = field.options[:metadata][:elem_class].constantize
+              next unless klass.include?(SS::Model::File)
+              relation[key].each_with_index do |file_id, i|
+                relation[key][i] = restore_file(file_id)
+              end
+            else
+              klass = field.association.class_name.constantize rescue nil
+              next if klass.blank?
+              next unless klass.include?(SS::Model::File)
+              relation[key] = restore_file(relation[key])
+            end
+          end
+          relation
+        end
+      end
+    end
+    ref_class.constantize.fields.each do |k, field|
+      next if data[k].blank?
+      if field.type == SS::Extensions::ObjectIds
+        klass = field.options[:metadata][:elem_class].constantize
+        next unless klass.include?(SS::Model::File)
+        data[k] = data[k].collect do |file_id|
+          restore_file(file_id)
+        end
+      else
+        klass = field.association.class_name.constantize rescue nil
+        next if klass.blank?
+        next unless klass.include?(SS::Model::File)
+        data[k] = restore_file(data[k])
+      end
+    end
 
     begin
       query.update_many('$set' => data)
@@ -81,5 +125,19 @@ class History::Backup
       errors.add :base, "error. #{e}"
       return false
     end
+  end
+
+  def restore_file(file_id)
+    file = SS::File.where(id: file_id).first
+    return file.id if file.present?
+    file = History::Trash.where(ref_coll: 'ss_files', 'data._id' => file_id).first
+    file = file.restore!
+    return if file.blank?
+    path = "#{Rails.root}/private/trash/#{file.path.sub(/.*\/(ss_files\/)/, '\\1')}"
+    return unless File.exist?(path)
+    FileUtils.mkdir_p(File.dirname(file.path))
+    FileUtils.cp(path, file.path)
+    FileUtils.rm_rf(File.dirname(path))
+    file._id
   end
 end
