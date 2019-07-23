3c5e69430d17cfe1581fe962792bd0fa66240ca4
itowtips <ito@web-tips.co.jp>
2019-07-23 12:06:36 +0900

    [fix] hidden modal tooltips

--- app/assets/javascripts/ss/lib/tooltips.js
+++ app/assets/javascripts/ss/lib/tooltips.js
@@ -4,15 +4,21 @@ this.SS_Tooltips = (function () {
 
   SS_Tooltips.render = function (ttips) {
     $(document).on("click", ttips, function (ev) {
-      var css, cur, hgt, ofs, style;
+      var css, cur, top, hgt, style, cbox;
       ev.preventDefault();
       ev.stopPropagation();
       ttips = $(ttips);
       ttips.find("ul").hide();
       cur = $(this);
       hgt = cur.find("ul").outerHeight();
-      ofs = cur.offset();
-      if (ofs.top - hgt < 0) {
+      top = cur.offset().top;
+
+      cbox = $(this).closest("#colorbox").offset();
+      if (cbox) {
+        top = top - cbox.top;
+      }
+
+      if (top - hgt < 0) {
         cur.find("ul").css("bottom", (hgt * (-1) - 15) + "px");
         css = "ul:after {border: 8px solid transparent; border-bottom-color:#fff; bottom:" + (hgt - 5) + "px;}";
       } else {
@@ -20,14 +26,15 @@ this.SS_Tooltips = (function () {
         css = "ul:after {border: 8px solid transparent; border-top-color:#fff; bottom:-13px;}";
       }
       style = $("<style>").append(document.createTextNode(css));
-      ttips.find("ul style").remove();
+      $(".tooltip ul").hide();
+      $(".tooltip ul style").remove();
       cur.find("ul").append(style);
-      return cur.find("ul").show();
+      cur.find("ul").show();
     });
-    return $(document).click(function (ev) {
+    $(document).click(function (ev) {
       ttips = $(ttips);
       if (!ttips.is($(ev.target).closest("div,span"))) {
-        return ttips.find("ul").hide();
+        $(".tooltip ul").hide();
       }
     });
   };
