2a9b558141f4144ff18bda7b094adb83fbbc7cc5
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2019-07-29 00:33:08 +0900

    [modify] gws memo messages spec (#3033)

--- spec/models/gws/memo/message_spec.rb
+++ spec/models/gws/memo/message_spec.rb
@@ -9,12 +9,38 @@ RSpec.describe Gws::Memo::Message, type: :model do
     end
 
     context 'when in_path is { gws_user.id.to_s => "INBOX.Trash" }' do
-      let(:memo) { create(:gws_memo_message, in_path: { gws_user.id.to_s => 'INBOX.Trash' }) }
+      let(:user) { Gws::User.find_by(uid: 'sys') }
+      let(:memo) do
+        create(:gws_memo_message, in_to_members: [gws_user.id.to_s, user.id.to_s], in_path: {
+          gws_user.id.to_s => 'INBOX.Trash',
+          user.id.to_s => 'INBOX.Trash'
+        })
+      end
       it { expect(memo.errors.size).to eq 0 }
       it do
         memo.set_seen(gws_user)
         expect(memo.path(gws_user)).to eq 'INBOX.Trash'
+        expect(memo.path(user)).to eq 'INBOX.Trash'
         expect(memo.seen_at(gws_user)).to be_truthy
+        expect(memo.seen_at(user)).to be_falsey
+
+        memo.move(user, 'INBOX').update
+        expect(memo.path(gws_user)).to eq 'INBOX.Trash'
+        expect(memo.path(user)).to eq 'INBOX'
+        expect(memo.seen_at(gws_user)).to be_truthy
+        expect(memo.seen_at(user)).to be_falsey
+
+        memo.set_seen(user)
+        expect(memo.path(gws_user)).to eq 'INBOX.Trash'
+        expect(memo.path(user)).to eq 'INBOX'
+        expect(memo.seen_at(gws_user)).to be_truthy
+        expect(memo.seen_at(user)).to be_truthy
+
+        memo.unset_seen(gws_user)
+        expect(memo.path(gws_user)).to eq 'INBOX.Trash'
+        expect(memo.path(user)).to eq 'INBOX'
+        expect(memo.seen_at(gws_user)).to be_falsey
+        expect(memo.seen_at(user)).to be_truthy
       end
     end
   end
