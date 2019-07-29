fdf016fe1045646c14b8e6ad86eda50ada0f3485
tany <tanizawa@web-tips.co.jp>
2019-07-29 21:17:30 +0900

    [modify] spec: UserSupport

--- spec/support/gws/user.rb
+++ spec/support/gws/user.rb
@@ -1,32 +1,53 @@
-def create_gws_users
-  return if Gws::User.where(uid: 'admin').exists?
+module Gws
+  module UserSupport
+    cattr_accessor :data
 
-  g00 = SS::Group.create name: "シラサギ市", order: 10
-  g10 = SS::Group.create name: "シラサギ市/企画政策部", order: 20
-  g11 = SS::Group.create name: "シラサギ市/企画政策部/政策課", order: 30
+    def self.extended(obj)
+      dbscope = obj.metadata[:dbscope]
+      dbscope ||= RSpec.configuration.default_dbscope
+
+      obj.after(dbscope) do
+        Gws::UserSupport.data = nil
+      end
+    end
+  end
+end
+
+RSpec.configuration.extend(Gws::UserSupport)
+
+def gws_site
+  create_gws_users[:site]
+end
+
+def gws_user
+  create_gws_users[:user]
+end
+
+def login_gws_user
+  login_user(gws_user)
+end
+
+def create_gws_users
+  return Gws::UserSupport.data if Gws::UserSupport.data.present?
+
+  g00 = Gws::Group.create name: "シラサギ市", order: 10
+  g10 = Gws::Group.create name: "シラサギ市/企画政策部", order: 20
+  g11 = Gws::Group.create name: "シラサギ市/企画政策部/政策課", order: 30
 
   role = Gws::Role.create name: I18n.t('gws.roles.admin'), site_id: g00.id,
     permissions: Gws::Role.permission_names, permission_level: 3
 
-  sys = Gws::User.create name: "gws-sys", uid: "sys", email: "sys@example.jp", in_password: "pass",
-    group_ids: [g11.id], gws_role_ids: [role.id]
-  adm = Gws::User.create name: "gw-admin", uid: "admin", email: "admin@example.jp", in_password: "pass",
+  user = Gws::User.create name: "gw-admin", uid: "admin", email: "admin@example.jp", in_password: "pass",
     group_ids: [g11.id], gws_role_ids: [role.id],
     organization_id: g00.id, organization_uid: "org-admin"
-end
 
-def gws_site
-  create_gws_users
-  Gws::Group.find_by name: 'シラサギ市'
-end
+  sys = Gws::User.create name: "gws-sys", uid: "sys", email: "sys@example.jp", in_password: "pass",
+    group_ids: [g11.id], gws_role_ids: [role.id]
 
-def gws_user
-  create_gws_users
-  user = Gws::User.find_by uid: 'admin'
-  user.cur_site = gws_site if user.present?
-  user
-end
+  user.cur_site = g00
 
-def login_gws_user
-  login_user gws_user
+  return Gws::UserSupport.data = {
+    site: g00,
+    user: user
+  }
 end
