c860e0d575d8e31f2d4d9dfbd03d2cdff0791f45
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2019-08-02 10:17:32 +0900

    [modify] chat intent index

--- app/controllers/chat/apis/categories_controller.rb
+++ app/controllers/chat/apis/categories_controller.rb
@@ -9,7 +9,7 @@ class Chat::Apis::CategoriesController < ApplicationController
     @items = @model.site(@cur_site).
       where(node_id: @cur_node.id).
       search(params[:s]).
-      order_by(updated: -1).
+      order_by(order: 1, updated: -1).
       page(params[:page]).per(50)
   end
 end
