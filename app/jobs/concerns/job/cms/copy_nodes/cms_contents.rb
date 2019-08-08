155ab17d790d979722ab8ac0aa1bef3d1ed19469
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2019-08-08 10:04:58 +0900

    [fix] copy article page with checkbox form values

--- app/jobs/concerns/job/cms/copy_nodes/cms_contents.rb
+++ app/jobs/concerns/job/cms/copy_nodes/cms_contents.rb
@@ -57,8 +57,16 @@ module Job::Cms::CopyNodes::CmsContents
       next unless dest_field_names.include?(field_name)
       next if field_value.blank?
 
-      file_url_maps.each do |src_url, dest_url|
-        field_value = field_value.gsub(src_url, dest_url)
+      if field_value.class == Array
+        file_url_maps.each do |src_url, dest_url|
+          field_value = field_value.collect do |value|
+            value.gsub(src_url, dest_url)
+          end
+        end
+      else
+        file_url_maps.each do |src_url, dest_url|
+          field_value = field_value.gsub(src_url, dest_url)
+        end
       end
 
       dest_content[field_name] = field_value
