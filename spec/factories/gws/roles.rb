092651a485ff1076341199c134986cfa0ffbf1c9
tany <tanizawa@web-tips.co.jp>
2019-08-13 06:45:12 +0900

    [spec] gws/role

--- spec/factories/gws/roles.rb
+++ spec/factories/gws/roles.rb
@@ -19,6 +19,26 @@ FactoryBot.define do
     end
   end
 
+  trait :gws_role_notice_editor do
+    after(:build) do |item|
+      item.permissions += %w(
+        use_gws_notice
+        delete_private_gws_notices
+        edit_private_gws_notices
+        read_private_gws_notices
+      )
+    end
+  end
+
+  trait :gws_role_notice_reader do
+    after(:build) do |item|
+      item.permissions += %w(
+        use_gws_notice
+        read_private_gws_notices
+      )
+    end
+  end
+
   trait :gws_role_schedule_plan_editor do
     after(:build) do |item|
       item.permissions += %w(
@@ -27,17 +47,6 @@ FactoryBot.define do
         edit_private_gws_schedule_plans
         delete_private_gws_schedule_plans
         read_private_gws_schedule_categories
-      )
-    end
-  end
-
-  trait :gws_role_notice_editor do
-    after(:build) do |item|
-      item.permissions += %w(
-        use_gws_notice
-        delete_private_gws_notices
-        edit_private_gws_notices
-        read_private_gws_notices
       )
     end
   end
@@ -75,19 +84,15 @@ FactoryBot.define do
     end
   end
 
-  factory :gws_role, class: Gws::Role, traits: [:gws_role] do
-  end
+  factory :gws_role, class: Gws::Role, traits: [:gws_role]
 
   factory :gws_role_admin, class: Gws::Role, traits: [:gws_role, :gws_role_admin]
 
   factory :gws_role_notice_admin, class: Gws::Role, traits: [:gws_role, :gws_role_notice_admin]
 
-  factory :gws_role_notice_editor, class: Gws::Role, traits: [:gws_role] do
-    permissions { nil }
-    permission_level 1
-  end
+  factory :gws_role_notice_editor, class: Gws::Role, traits: [:gws_role, :gws_role_notice_editor]
 
-  factory :gws_role_notice_reader, class: Gws::Role, traits: [:gws_role, :gws_role_notice_editor]
+  factory :gws_role_notice_reader, class: Gws::Role, traits: [:gws_role, :gws_role_notice_reader]
 
   factory :gws_role_schedule_plan_editor, class: Gws::Role, traits: [:gws_role, :gws_role_schedule_plan_editor]
 
