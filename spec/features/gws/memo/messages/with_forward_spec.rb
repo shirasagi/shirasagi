a91fbc8bff0889957a71086f0d973be43cf688eb
NAKANO Hideo <nakano@web-tips.co.jp>
2019-09-03 14:26:35 +0900

    [modify] shorten text length to improve spec performance

--- spec/features/gws/memo/messages/with_forward_spec.rb
+++ spec/features/gws/memo/messages/with_forward_spec.rb
@@ -5,7 +5,7 @@ describe 'gws_memo_messages', type: :feature, dbscope: :example, js: true do
     let(:site) { gws_site }
     let!(:recipient) { create(:gws_user, cur_site: site, group_ids: gws_user.group_ids, gws_role_ids: gws_user.gws_role_ids) }
     let(:subject) { "subject-#{unique_id}" }
-    let(:texts) { Array.new(rand(1..10)) { "text-#{unique_id}" } }
+    let(:texts) { Array.new(rand(2..3)) { "text-#{unique_id}" } }
     let(:text) { texts.join("\r\n") }
     let(:forward_email) { "#{unique_id}@example.jp" }
     let(:forward_subject) { "[#{I18n.t("gws/memo/message.message")}]#{I18n.t("gws/memo/forward.subject")}:#{gws_user.name}" }
