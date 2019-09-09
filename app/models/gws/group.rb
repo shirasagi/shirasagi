44a9fa53445b92f420104db91b35381e426fd553
NAKANO Hideo <nakano@web-tips.co.jp>
2019-09-09 16:38:49 +0900

    [add] new mark to gws/report

--- app/models/gws/group.rb
+++ app/models/gws/group.rb
@@ -6,6 +6,7 @@ class Gws::Group
   include Gws::Addon::Facility::GroupSetting
   include Gws::Addon::Attendance::GroupSetting
   include Gws::Addon::Memo::GroupSetting
+  include Gws::Addon::Report::GroupSetting
   include Gws::Addon::Circular::GroupSetting
   include Gws::Addon::Monitor::GroupSetting
   include Gws::Addon::Survey::GroupSetting
