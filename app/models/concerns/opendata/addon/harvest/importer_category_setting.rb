9674ecdc5c07425e89a4785e4ff26a487f19e06c
itowtips <ito@web-tips.co.jp>
2019-09-19 15:23:15 +0900

    C: Metrics/LineLength

--- app/models/concerns/opendata/addon/harvest/importer_category_setting.rb
+++ app/models/concerns/opendata/addon/harvest/importer_category_setting.rb
@@ -5,7 +5,8 @@ module Opendata::Addon::Harvest
 
     included do
       embeds_ids :default_categories, class_name: 'Opendata::Node::Category'
-      has_many :category_settings, class_name: 'Opendata::Harvest::Importer::CategorySetting', dependent: :destroy, inverse_of: :importer
+      has_many :category_settings, class_name: 'Opendata::Harvest::Importer::CategorySetting',
+        dependent: :destroy, inverse_of: :importer
       permit_params default_category_ids: []
     end
   end
