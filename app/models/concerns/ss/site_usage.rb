3395f67dcd28aeefc7d84bb66a9aa4295f4cfbb4
NAKANO Hideo <nakano@web-tips.co.jp>
2019-09-30 13:27:01 +0900

    [add] ss/site_usage module

---
+++ app/models/concerns/ss/site_usage.rb
@@ -0,0 +1,34 @@
+module SS::SiteUsage
+  extend ActiveSupport::Concern
+  extend SS::Translation
+
+  included do
+    field :usage_node_count, type: Integer
+    field :usage_page_count, type: Integer
+    field :usage_file_count, type: Integer
+    field :usage_db_size, type: Integer
+    field :usage_group_count, type: Integer
+    field :usage_user_count, type: Integer
+    field :usage_calculated_at, type: DateTime
+  end
+
+  def reload_usage!
+    now = Time.zone.now
+
+    self.usage_node_count = Cms::Node.site(self).count
+    self.usage_page_count = Cms::Page.site(self).count
+    self.usage_file_count = SS::File.where(site_id: self.id).count
+    self.usage_db_size = Cms.find_cms_quota_used(self.class.where(id: id))
+
+    group_names = self.root_groups.pluck(:name)
+    conditions = group_names.map { |name| { name: /^#{Regexp.escape(name)}(\/|$)/ } }
+    groups = Cms::Group.all.unscoped.where("$and" => [{ "$or" => conditions }]).active
+    self.usage_group_count = groups.count
+
+    users = Cms::User.all.unscoped.in(group_ids: groups.pluck(:id)).active
+    self.usage_user_count = users.count
+
+    self.usage_calculated_at = now
+    self.save!
+  end
+end
--- app/models/ss/site.rb
+++ app/models/ss/site.rb
@@ -12,4 +12,5 @@ class SS::Site
   include SS::Addon::ApproveSetting
   include SS::Addon::EditorSetting
   include SS::Addon::Elasticsearch::SiteSetting
+  include SS::SiteUsage
 end
