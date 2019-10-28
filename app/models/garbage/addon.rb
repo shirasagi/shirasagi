3b49450a8b5232fbf8e688c0b64cc4d0a2953cf5
tetsu-26 <heiwa326427@gmail.com>
2019-10-28 17:19:15 +0900

    ゴミ品目は#{garbage}で、ゴミの分別区分は#{garbage_category}で、出し方・ワンポイントは#{garbage_category}でループHTMLを表示できるようにした

--- app/models/garbage/addon.rb
+++ app/models/garbage/addon.rb
@@ -8,6 +8,22 @@ module Garbage::Addon
       field :remark, type: String
 
       permit_params :name, :remark
+
+      template_variable_handler(:garbage, :template_variable_handler_garbage)
+      template_variable_handler(:garbage_category, :template_variable_handler_garbage_category)
+      template_variable_handler(:garbage_remark, :template_variable_handler_garbage_remark)
+    end
+
+    def template_variable_handler_garbage(name, issuer)
+      ERB::Util.html_escape self.send(:name)
+    end
+
+    def template_variable_handler_garbage_category(name, issuer)
+      ERB::Util.html_escape self.categories.map(&:name).join
+    end
+
+    def template_variable_handler_garbage_remark(name, issuer)
+      ERB::Util.html_escape self.send(:remark)
     end
   end
 end
