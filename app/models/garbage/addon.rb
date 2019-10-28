d27a5dc861a6838fbdeb006bca4d47dedd2391e2
tetsu-26 <heiwa326427@gmail.com>
2019-10-28 17:19:15 +0900

    [fix] cms garbage list addon

--- app/models/garbage/addon.rb
+++ app/models/garbage/addon.rb
@@ -8,6 +8,13 @@ module Garbage::Addon
       field :remark, type: String
 
       permit_params :name, :remark
+
+      template_variable_handler(:remark, :template_variable_handler_name)
+      template_variable_handler(:categories, :template_variable_handler_categories)
+    end
+
+    def template_variable_handler_categories(name, issuer)
+      ERB::Util.html_escape self.categories.map(&:name).join("\n")
     end
   end
 end
