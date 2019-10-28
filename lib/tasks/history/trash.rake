859984b77bcede1f39ff1db9546fc6c2ef2d8557
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2019-10-28 13:37:08 +0900

    [fix] ss model file create_history_trash (#3228)
    * [fix] ss model file create_history_trash
    
    * [modify] ss model file create_history_trash spec

--- lib/tasks/history/trash.rake
+++ lib/tasks/history/trash.rake
@@ -2,7 +2,7 @@ namespace :history do
   namespace :trash do
     task purge: :environment do
       site = Cms::Site.find_by(host: ENV['site']) if ENV['site'].present?
-      threshold = ENV['threshold']
+      threshold = ENV['purge_threshold'] || ENV['threshold']
       params = []
       params << { threshold: threshold } if threshold.present?
 
--- spec/features/history/cms/trashes_spec.rb
+++ spec/features/history/cms/trashes_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe "history_cms_trashes", type: :feature, dbscope: :example, js: true do
   let(:site) { cms_site }
   let(:node) { create_once :article_node_page, filename: "docs", name: "article" }
-  let(:page_item) { create(:article_page, cur_node: node) }
+  let(:file) { create :cms_file, site: site }
+  let(:page_item) { create(:article_page, cur_node: node, file_ids: [file.id]) }
   let(:index_path) { history_cms_trashes_path(site: site.id) }
   let(:node_path) { article_pages_path(site: site.id, cid: node.id) }
   let(:page_path) { article_page_path(site: site.id, cid: node.id, id: page_item.id) }
@@ -56,6 +57,8 @@ describe "history_cms_trashes", type: :feature, dbscope: :example, js: true do
 
     it "#undo_delete" do
       visit page_path
+      expect(page).to have_css('div.file-view', text: file.name)
+
       click_link I18n.t('ss.links.delete')
       click_button I18n.t('ss.buttons.delete')
       expect(page).to have_no_css('a.title', text: page_item.name)
@@ -73,10 +76,15 @@ describe "history_cms_trashes", type: :feature, dbscope: :example, js: true do
 
       visit node_path
       expect(page).to have_css('a.title', text: page_item.name)
+
+      visit page_path
+      expect(page).to have_css('div.file-view', text: file.name)
     end
 
     it "#undo_delete_all" do
       visit page_path
+      expect(page).to have_css('div.file-view', text: file.name)
+
       click_link I18n.t('ss.links.delete')
       click_button I18n.t('ss.buttons.delete')
       expect(page).to have_no_css('a.title', text: page_item.name)
@@ -95,6 +103,9 @@ describe "history_cms_trashes", type: :feature, dbscope: :example, js: true do
 
       visit node_path
       expect(page).to have_css('a.title', text: page_item.name)
+
+      visit page_path
+      expect(page).to have_css('div.file-view', text: file.name)
     end
   end
 end
