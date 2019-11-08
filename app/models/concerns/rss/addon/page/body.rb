38e60fcf3f22d6a399b314b7a7966e27f017e268
NAKANO Hideo <nakano@web-tips.co.jp>
2019-11-08 15:06:15 +0900

    [modify] use '.reorder' instead of '.order'

--- app/models/concerns/rss/addon/page/body.rb
+++ app/models/concerns/rss/addon/page/body.rb
@@ -20,7 +20,7 @@ module Rss::Addon::Page
         count = criteria.count
         if count > max
           limit = count - max
-          criteria.order(released: 1, _id: 1).limit(limit).each do |item|
+          criteria.reorder(released: 1, id: 1).limit(limit).each do |item|
             item.with(mongo_client_options) do |model|
               model.destroy
             end
