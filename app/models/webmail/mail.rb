1bb00eab4ffb6fec2986300ee81968c27ae9f2f4
NAKANO Hideo <nakano@web-tips.co.jp>
2019-11-13 17:12:36 +0900

    [fix][webmail][#3287] use "select" for appending mail to mailbox (#3288)
    * [fix][webmail][#3287] use "select" for appending mail to mailbox instead of "examine"
    
    * [modify] refactor w/ Rubocop

--- app/models/webmail/mail.rb
+++ app/models/webmail/mail.rb
@@ -130,10 +130,9 @@ class Webmail::Mail
       field.include_in_headers = true if field.respond_to?(:include_in_headers)
     end
 
-    imap.examine(imap.draft_box)
+    imap.select(imap.draft_box)
     imap.conn.append(imap.draft_box, msg.to_s, [:Draft, :Seen], Time.zone.now)
     if draft?
-      imap.select(imap.draft_box)
       imap.uids_delete([uid])
     end
     true
@@ -157,10 +156,9 @@ class Webmail::Mail
       field.include_in_headers = true if field.respond_to?(:include_in_headers)
     end
 
-    imap.examine(imap.sent_box)
+    imap.select(imap.sent_box)
     imap.conn.append(imap.sent_box, msg.to_s, [:Seen], Time.zone.now)
     if draft?
-      imap.select(imap.draft_box)
       imap.uids_delete([uid])
     end
     true
@@ -170,7 +168,7 @@ class Webmail::Mail
     date_time = opts[:date_time] || Time.zone.now
 
     imap.select('INBOX')
-    imap.conn.append('INBOX', msg.to_s, [:Seen], date_time.to_time)
+    imap.conn.append('INBOX', msg.to_s, [:Seen], date_time.in_time_zone)
   end
 
   def requested_mdn?
