76bd8f8a4abe677a47c14d4d37a637d941d18520
NAKANO Hideo <nakano@web-tips.co.jp>
2019-11-13 17:13:09 +0900

    [cms][fix] full_url returns nil on some edge cases (#3296)
    * [fix] full_url returns nil on some edge cases
    
    this PR might fix up #3109.
    
    * [add] rss/page's public side view to support on rss_link is blank

--- app/controllers/rss/agents/pages/page_controller.rb
+++ app/controllers/rss/agents/pages/page_controller.rb
@@ -2,6 +2,6 @@ class Rss::Agents::Pages::PageController < ApplicationController
   include Cms::PageFilter::View
 
   def index
-    redirect_to @cur_page.rss_link
+    redirect_to @cur_page.rss_link if @cur_page.rss_link.present?
   end
 end
--- spec/models/rss/page_spec.rb
+++ spec/models/rss/page_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe Rss::Page, dbscope: :example do
+  let(:site) { cms_site }
+  let(:node) { create :rss_node_page, site: site }
+
   describe "basic attributes" do
-    let(:site) { cms_site }
-    let(:node) { create :rss_node_page, site: site }
     let(:show_path) { Rails.application.routes.url_helpers.rss_page_path(site: subject.site, cid: subject.parent, id: subject) }
     subject { create :rss_page, site: site, node: node }
 
@@ -23,7 +24,33 @@ describe Rss::Page, dbscope: :example do
   describe "validation" do
     it "rss_link" do
       item = build(:rss_page_rss_link_blank)
-      expect(item.invalid?).to be_truthy
+      expect(item).to have(1).errors_on(:rss_link)
+    end
+  end
+
+  describe "serve_static_file?" do
+    subject { create :rss_page, site: site, node: node }
+    its(:serve_static_file?) { is_expected.to be_falsey }
+  end
+
+  describe "url and full_url" do
+    context "with rss_link is given" do
+      subject { create :rss_page, site: site, node: node }
+
+      its(:url) { is_expected.to eq subject.rss_link }
+      its(:full_url) { is_expected.to eq subject.rss_link }
+    end
+
+    context "with rss_link is blank" do
+      subject { create :rss_page, site: site, node: node }
+
+      before do
+        # v1.12.0 より古いバージョンでは rss_link が blank になりうる
+        subject.set(rss_link: nil)
+      end
+
+      its(:url) { is_expected.to eq Cms::Page.find(subject.id).url }
+      its(:full_url) { is_expected.to eq Cms::Page.find(subject.id).full_url }
     end
   end
 end
