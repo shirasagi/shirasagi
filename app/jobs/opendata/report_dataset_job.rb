782d729d82862b1a12c5b86ae77c82bedbb32c0b
NAKANO Hideo <nakano@web-tips.co.jp>
2019-11-15 10:13:25 +0900

    [opendata] remove unused job

--- app/jobs/opendata/report_dataset_job.rb
+++
@@ -1,6 +0,0 @@
-class Opendata::ReportDatasetJob < Cms::ApplicationJob
-  def perform(importer_id)
-    importer = Opendata::DatasetImport::Importer.find(importer_id)
-    importer.report
-  end
-end
