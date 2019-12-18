4126cfc0aa207931d09a4d45d839a061a2e867c2
NAKANO Hideo <nakano@web-tips.co.jp>
2019-12-18 11:12:34 +0900

    [add] search with code

--- app/models/concerns/ss/model/prefecture_code.rb
+++ app/models/concerns/ss/model/prefecture_code.rb
@@ -34,7 +34,7 @@ module SS::Model::PrefectureCode
 
   module ClassMethods
     def search(params = {})
-      all.search_name(params).search_keyword(params)
+      all.search_name(params).search_keyword(params).search_code(params)
     end
 
     def search_name(params = {})
@@ -45,6 +45,17 @@ module SS::Model::PrefectureCode
     def search_keyword(params = {})
       return all if params.blank? || params[:keyword].blank?
       all.keyword_in params[:keyword], :code, :prefecture, :prefecture_kana, :city, :city_kana
+    end
+
+    def search_code(params = {})
+      return all if params.blank?
+
+      code = params[:code]
+      return all if code.blank?
+
+      # normalize postal code
+      code = code.tr('０-９ａ-ｚＡ-Ｚ', '0-9a-zA-Z').gsub(/[^0-9a-zA-Z]/, '')
+      all.where(code: code)
     end
 
     def to_csv
