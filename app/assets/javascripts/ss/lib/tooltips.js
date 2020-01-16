72057aab23e30d3d1c5fc65e41e8a2060be851a9
NAKANO Hideo <nakano@web-tips.co.jp>
2020-01-16 13:30:43 +0900

    [modify] integrate tippy and replace shirasagi tooltip framework with it

--- app/assets/javascripts/ss/lib/tooltips.js
+++ app/assets/javascripts/ss/lib/tooltips.js
@@ -2,48 +2,36 @@ this.SS_Tooltips = (function () {
   function SS_Tooltips() {
   }
 
-  SS_Tooltips.render = function (ttips) {
-    $(document).on("click", ttips, function (ev) {
-      var css, cur, top, hgt, style, cbox;
-      ev.preventDefault();
-      ev.stopPropagation();
-      ttips = $(ttips);
-      ttips.find("ul").hide();
-      cur = $(this);
-      hgt = cur.find("ul").outerHeight();
-      top = cur.offset().top;
+  SS_Tooltips.initialized = false;
 
-      cbox = $(this).closest("#colorbox").offset();
-      if (cbox) {
-        top = top - cbox.top;
-      }
+  SS_Tooltips.initializeOnce = function () {
+    if (SS_Tooltips.initialized) {
+      return;
+    }
 
-      cur.find("ul").removeClass("tooltip-top");
-      cur.find("ul").removeClass("tooltip-bottom");
-      if (top - hgt < 0) {
-        cur.find("ul").css("bottom", (hgt * (-1) - 15) + "px");
-        cur.find("ul").addClass("tooltip-bottom");
-        css = "ul:after {border: 8px solid transparent; border-bottom-color:#fff; bottom:" + (hgt - 5) + "px;}";
-      } else {
-        cur.find("ul").css("bottom", "18px");
-        cur.find("ul").addClass("tooltip-top");
-        css = "ul:after {border: 8px solid transparent; border-top-color:#fff; bottom:-13px;}";
-      }
-      style = $("<style>").append(document.createTextNode(css));
-      $(".tooltip ul").hide();
-      $(".tooltip ul style").remove();
-      cur.find("ul").append(style);
-      cur.find("ul").show();
+    SS_Tooltips.initialized = true;
+
+    // tippy default settings
+    tippy.setDefaultProps({
+      content: function(el) { return el.querySelector("ul"); },
+      trigger: 'click',
+      theme: 'light-border ss-tooltip'
     });
-    $(document).click(function (ev) {
-      ttips = $(ttips);
-      if (!ttips.is($(ev.target).closest("div,span"))) {
-        $(".tooltip ul").hide();
+  };
+
+  SS_Tooltips.render = function (selector) {
+    SS_Tooltips.initializeOnce();
+
+    $(document).on("click", selector, function (ev) {
+      if (ev.target._tippy) {
+        return;
       }
+
+      // lazy tippinize
+      var instance = tippy(ev.target);
+      instance.show();
     });
   };
 
   return SS_Tooltips;
-
 })();
-
