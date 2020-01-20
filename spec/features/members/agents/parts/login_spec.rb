dfdc603309ca4aa92ec04e5df44af0d16b8b04e8
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2020-01-20 10:37:35 +0900

    [fix] SS.renderPart invalid link when 500 error

--- spec/features/members/agents/parts/login_spec.rb
+++ spec/features/members/agents/parts/login_spec.rb
@@ -4,7 +4,7 @@ describe "member_agents_parts_logins", type: :feature, dbscope: :example, js: tr
   let(:site)   { cms_site }
   let(:layout) { create_cms_layout part }
   let(:node)   { create :cms_node, layout_id: layout.id }
-  let(:part)   { create :member_part_login }
+  let(:part)   { create :member_part_login, ajax_view: 'enabled' }
 
   context "public" do
     before do
@@ -15,6 +15,27 @@ describe "member_agents_parts_logins", type: :feature, dbscope: :example, js: tr
       visit node.url
       expect(page).to have_no_css(".ss-part")
       expect(page).to have_css(".login")
+    end
+
+    context "when part state is closed" do
+      before do
+        Cms::Node::GenerateJob.bind(site_id: site.id, node_id: node.id).perform_now
+        part.state = 'closed'
+        part.save!
+      end
+
+      around do |example|
+        Capybara.raise_server_errors = false
+        example.run
+        Capybara.raise_server_errors = true
+      end
+
+      it "#index" do
+        visit node.url
+        expect(page).to have_css('div#main')
+        expect(page).to have_no_css(".ss-part")
+        expect(page).to have_no_css(".login")
+      end
     end
   end
 end
