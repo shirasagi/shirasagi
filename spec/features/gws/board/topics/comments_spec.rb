9e6a2e94ed10f349afdc1d15fe4d4a1fe86f7f36
NAKANO Hideo <nakano@web-tips.co.jp>
2020-02-19 14:06:28 +0900

    [modify][gws][board] clear "browsed" on posting comment (#3176)
    * [add] reproducible specs
    
    * [modify] clear "browsed_users_hash" on posting comment

--- spec/features/gws/board/topics/comments_spec.rb
+++ spec/features/gws/board/topics/comments_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe "gws_board_topics", type: :feature, dbscope: :example do
   context "comments" do
     let(:site) { gws_site }
+    let!(:user1) { create :gws_user, group_ids: gws_user.group_ids, gws_role_ids: gws_user.gws_role_ids }
+    let!(:user2) { create :gws_user, group_ids: gws_user.group_ids, gws_role_ids: gws_user.gws_role_ids }
     let(:show_path) { gws_board_topic_path site, '-', '-', topic }
     let(:name) { unique_id }
     let(:text) { unique_id }
@@ -10,9 +12,20 @@ describe "gws_board_topics", type: :feature, dbscope: :example do
     before { login_gws_user }
 
     context "with thread topic" do
-      let(:topic) { create :gws_board_topic, mode: 'thread' }
+      let(:topic) { create :gws_board_topic, mode: 'thread', readable_member_ids: [ gws_user.id, user1.id, user2.id ] }
+
+      before do
+        topic.set_browsed!(gws_user)
+        topic.set_browsed!(user1)
+        topic.set_browsed!(user2)
+      end
 
       it "#show" do
+        topic.reload
+        expect(topic.browsed?(gws_user)).to be_truthy
+        expect(topic.browsed?(user1)).to be_truthy
+        expect(topic.browsed?(user2)).to be_truthy
+
         visit show_path
         expect(page).to have_css("article.topic .name", text: topic.name)
         expect(page).to have_css("article.topic .body", text: topic.text)
@@ -34,15 +47,32 @@ describe "gws_board_topics", type: :feature, dbscope: :example do
         comment = Gws::Board::Post.where(topic_id: topic.id).first
         expect(comment.name).to eq name
         expect(comment.text).to eq text
+
+        # コメント投稿者の既読状態は維持される
+        topic.reload
+        expect(topic.browsed?(gws_user)).to be_truthy
+        expect(topic.browsed?(user1)).to be_falsey
+        expect(topic.browsed?(user2)).to be_falsey
       end
     end
 
     context "with tree topic" do
-      let(:topic) { create :gws_board_topic, mode: 'tree' }
+      let(:topic) { create :gws_board_topic, mode: 'tree', readable_member_ids: [ gws_user.id, user1.id, user2.id ] }
       let(:name2) { unique_id }
       let(:text2) { unique_id }
 
+      before do
+        topic.set_browsed!(gws_user)
+        topic.set_browsed!(user1)
+        topic.set_browsed!(user2)
+      end
+
       it "#show" do
+        topic.reload
+        expect(topic.browsed?(gws_user)).to be_truthy
+        expect(topic.browsed?(user1)).to be_truthy
+        expect(topic.browsed?(user2)).to be_truthy
+
         visit show_path
         expect(page).to have_css("article.topic .name", text: topic.name)
         expect(page).to have_css("article.topic .body", text: topic.text)
@@ -64,6 +94,12 @@ describe "gws_board_topics", type: :feature, dbscope: :example do
         comment = Gws::Board::Post.where(topic_id: topic.id).first
         expect(comment.name).to eq name
         expect(comment.text).to eq text
+
+        # コメント投稿者の既読状態は維持される
+        topic.reload
+        expect(topic.browsed?(gws_user)).to be_truthy
+        expect(topic.browsed?(user1)).to be_falsey
+        expect(topic.browsed?(user2)).to be_falsey
 
         within "aside.comment div.menu" do
           click_on "返信する"
@@ -84,6 +120,12 @@ describe "gws_board_topics", type: :feature, dbscope: :example do
         comment = Gws::Board::Post.where(topic_id: topic.id).order_by(created: -1).first
         expect(comment.name).to eq name2
         expect(comment.text).to eq text2
+
+        # コメント投稿者の既読状態は維持される
+        topic.reload
+        expect(topic.browsed?(gws_user)).to be_truthy
+        expect(topic.browsed?(user1)).to be_falsey
+        expect(topic.browsed?(user2)).to be_falsey
       end
     end
   end
