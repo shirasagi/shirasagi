3c90a012ab77803f4dc4e5b18edb2a960b79af78
NAKANO Hideo <nakano@web-tips.co.jp>
2020-02-20 11:00:30 +0900

    [add][gws] more specs about reminder on gws/schedule/plans (#3331)
    * [add] extra specs about reminder on gws/schedule/plans
    
    * [fix] typo
    
    * [modify] make more sensitive to send
    
    * [fix] unable to work with from/to options

--- spec/jobs/gws/reminder/notification_job_spec.rb
+++ spec/jobs/gws/reminder/notification_job_spec.rb
@@ -26,7 +26,7 @@ describe Gws::Reminder::NotificationJob, dbscope: :example do
   context 'usual case' do
     it do
       expect(reminder.notifications).to be_present
-      Timecop.travel(reminder.notifications.first.notify_at + 1.minute) do
+      Timecop.travel(reminder.notifications.first.notify_at) do
         described_class.bind(site_id: site.id).perform_now
       end
 
@@ -51,10 +51,10 @@ describe Gws::Reminder::NotificationJob, dbscope: :example do
     end
 
     it 'use notice (internal message)' do
-      reminder.notifications.first.update_attributes(state: 'enabled')
+      reminder.notifications.first.update(state: 'enabled')
 
       expect(reminder.notifications).to be_present
-      Timecop.travel(reminder.notifications.first.notify_at + 1.minute) do
+      Timecop.travel(reminder.notifications.first.notify_at) do
         described_class.bind(site_id: site.id).perform_now
       end
 
@@ -69,7 +69,7 @@ describe Gws::Reminder::NotificationJob, dbscope: :example do
 
   context 'send notification twice' do
     it do
-      Timecop.travel(reminder.notifications.first.notify_at + 1.minute) do
+      Timecop.travel(reminder.notifications.first.notify_at) do
         described_class.bind(site_id: site.id).perform_now
         described_class.bind(site_id: site.id).perform_now
       end
@@ -104,7 +104,7 @@ describe Gws::Reminder::NotificationJob, dbscope: :example do
 
     it do
       expect(reminder.notifications).to be_present
-      Timecop.travel(reminder.notifications.first.notify_at + 1.minute) do
+      Timecop.travel(reminder.notifications.first.notify_at) do
         described_class.bind(site_id: site.id).perform_now
       end
 
@@ -117,6 +117,34 @@ describe Gws::Reminder::NotificationJob, dbscope: :example do
       expect(ActionMailer::Base.deliveries.length).to eq 1
       ActionMailer::Base.deliveries.first.tap do |notify_mail|
         expect(notify_mail.from.first).to eq sender_email
+        expect(notify_mail.to.first).to eq reminder.user.email
+        expect(notify_mail.subject).to eq "[リマインダー] スケジュール - #{schedule.name}"
+        expect(notify_mail.body.multipart?).to be_falsey
+        expect(notify_mail.body.raw_source).to include("[タイトル] #{schedule.name}")
+        expect(notify_mail.body.raw_source).to include("[日時] #{I18n.l(schedule.start_at.to_date, format: :gws_long)}")
+        expect(notify_mail.body.raw_source).to include("[参加ユーザー]\n")
+        expect(notify_mail.body.raw_source).to include(schedule.members.first.long_name)
+      end
+    end
+  end
+
+  context 'with from/to options' do
+    it do
+      expect(reminder.notifications).to be_present
+      from = reminder.notifications.first.notify_at.iso8601
+      to = (reminder.notifications.first.notify_at + 1.second).iso8601
+      described_class.bind(site_id: site.id).perform_now(from: from, to: to)
+
+      expect(Gws::Job::Log.count).to eq 1
+      Gws::Job::Log.first.tap do |log|
+        expect(log.logs).to include(include("INFO -- : Started Job"))
+        expect(log.logs).to include(include("INFO -- : Completed Job"))
+        expect(log.logs).to include(include("リマインダー通知（メール送信）"))
+      end
+
+      expect(ActionMailer::Base.deliveries.length).to eq 1
+      ActionMailer::Base.deliveries.first.tap do |notify_mail|
+        expect(notify_mail.from.first).to eq default_from
         expect(notify_mail.to.first).to eq reminder.user.email
         expect(notify_mail.subject).to eq "[リマインダー] スケジュール - #{schedule.name}"
         expect(notify_mail.body.multipart?).to be_falsey
