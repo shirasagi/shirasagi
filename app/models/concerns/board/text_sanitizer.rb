0dba53d0f78af1125771431d43467332b2b4de90
NAKANO Hideo <nakano@web-tips.co.jp>
2020-02-20 10:45:56 +0900

    [fix] transfer uncertain url through caution page

--- app/models/concerns/board/text_sanitizer.rb
+++ app/models/concerns/board/text_sanitizer.rb
@@ -10,8 +10,22 @@ module Board::TextSanitizer
     ret = helpers.sanitize(ret, tags: [])
     ret = ret.strip
     ret = CGI.escapeHTML(ret)
-    ret = helpers.auto_link(ret, link: :urls)
+    ret = helpers.ss_auto_link(ret, link: :urls, link_to: method(:wrap_link_to))
     ret = helpers.br(ret)
     ret
+  end
+
+  private
+
+  def wrap_link_to(link_text, link_attributes, escape)
+    url_helpers = Rails.application.routes.url_helpers
+
+    href = link_attributes["href"]
+    return link_text if href.blank? || (!href.start_with?("http://") && !href.start_with?("https://"))
+
+    link_attributes["href"] = url_helpers.sns_redirect_path(ref: href)
+    link_attributes["target"] ||= "_blank"
+    link_attributes["class"] ||= "external"
+    ApplicationController.helpers.content_tag(:a, link_text, link_attributes, escape)
   end
 end
