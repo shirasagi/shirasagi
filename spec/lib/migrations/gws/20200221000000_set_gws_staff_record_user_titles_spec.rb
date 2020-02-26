492305b63b303d87621bfdd3042f899e52c87335
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2020-02-26 17:42:53 +0900

    [modify] gws staff record user titles spec

--- spec/lib/migrations/gws/20200221000000_set_gws_staff_record_user_titles_spec.rb
+++ spec/lib/migrations/gws/20200221000000_set_gws_staff_record_user_titles_spec.rb
@@ -10,17 +10,20 @@ RSpec.describe SS::Migration20200221000000, dbscope: :example do
   before do
     sr_user[:title_name] = user_title_name
     sr_user.save!
-    described_class.new.change
   end
 
   context 'without user_title' do
+    before do
+      described_class.new.change
+    end
+
     it 'sr_user should not be have title_ids' do
       sr_user.reload
-      expect(sr_user.title).to be_blank
+      expect(sr_user.title).to be_nil
     end
   end
 
-  context 'with user_title' do
+  context 'with active user_title' do
     let!(:user_title) { create :gws_user_title, name: user_title_name }
 
     before do
@@ -38,7 +41,20 @@ RSpec.describe SS::Migration20200221000000, dbscope: :example do
     end
   end
 
-  context 'with sr_user_title' do
+  context 'with expired user_title' do
+    let!(:user_title) { create :gws_user_title, name: user_title_name, expiration_date: Time.zone.yesterday }
+
+    before do
+      described_class.new.change
+    end
+
+    it 'sr_user should not be have title_ids' do
+      sr_user.reload
+      expect(sr_user.title).to be_nil
+    end
+  end
+
+  context 'with active sr_user_title' do
     let!(:sr_user_title) { create :gws_staff_record_user_title, year_id: sr_year.id, name: user_title_name }
 
     before do
@@ -48,6 +64,21 @@ RSpec.describe SS::Migration20200221000000, dbscope: :example do
     it 'sr_user should be have title_ids' do
       sr_user.reload
       expect(sr_user.title).to eq sr_user_title
+    end
+  end
+
+  context 'with expired sr_user_title' do
+    let!(:sr_user_title) do
+      create :gws_staff_record_user_title, year_id: sr_year.id, name: user_title_name, expiration_date: Time.zone.yesterday
+    end
+
+    before do
+      described_class.new.change
+    end
+
+    it 'sr_user should not be have title_ids' do
+      sr_user.reload
+      expect(sr_user.title).to be_nil
     end
   end
 end
