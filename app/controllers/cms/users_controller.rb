841316185156d76484bea987dc96218c26bf6129
HiraiY <60082177+HiraiY@users.noreply.github.com>
2020-03-09 18:57:54 +0900

    [fix][#2351][cms][gws][webメール] user is destroyed when delete disabled user (#3419)
    * [fix][#2351] show delete button when user enabled
    
    * [fix] cms show delete button when user enabled
    
    * [fix][#2351][cms][gws] user is destroyed when delete desabled user
    
    * [fix][#2351][Webメール] user is destroyed when delete desabled user
    
    * [add][#2351][cms][gws][Webメール] add test
    
    * delete debug code
    
    * [fix][Webメール] fixed webmail test
    
    * fixed webmail test when delete user
    
    * [fix] modify delete user spec

--- app/controllers/cms/users_controller.rb
+++ app/controllers/cms/users_controller.rb
@@ -63,7 +63,7 @@ class Cms::UsersController < ApplicationController
 
   def destroy
     raise "403" unless @item.allowed?(:delete, @cur_user, site: @cur_site)
-    render_destroy @item.disable
+    render_destroy @item.disabled? ? @item.destroy : @item.disable
   end
 
   def destroy_all
--- spec/features/webmail/users/basic_crud_spec.rb
+++ spec/features/webmail/users/basic_crud_spec.rb
@@ -10,6 +10,8 @@ describe "webmail_users", type: :feature, dbscope: :example, js: true do
     let(:password) { unique_id }
     let(:uid2) { unique_id }
     let(:email2) { "#{uid2}@example.jp" }
+    let(:index_path) { webmail_users_path }
+    let(:delete_path) { "#{index_path}/#{webmail_user.id}/delete" }
 
     it do
       visit webmail_users_path
@@ -116,6 +118,20 @@ describe "webmail_users", type: :feature, dbscope: :example, js: true do
       Webmail::User.all.find_by(uid: uid2).tap do |item|
         expect(item.active?).to be_truthy
       end
+
+      visit delete_path
+      within "form" do
+        click_on I18n.t("ss.buttons.delete")
+      end
+      expect(page).to have_css("#notice", text: I18n.t("ss.notice.deleted"))
+      expect { Webmail::User.all.active.find(webmail_user.id) }.to raise_error Mongoid::Errors::DocumentNotFound
+
+      visit delete_path
+      within "form" do
+        click_on I18n.t("ss.buttons.delete")
+      end
+      expect(page).to have_css("#notice", text: I18n.t("ss.notice.deleted"))
+      expect { Webmail::User.all.find(webmail_user.id) }.to raise_error Mongoid::Errors::DocumentNotFound
     end
   end
 end
