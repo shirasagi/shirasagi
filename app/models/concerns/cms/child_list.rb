27a2061257170df647b67fd14322f156616af822
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2020-03-11 09:58:32 +0900

    [modify] child_list order (#3338)
    * [modify] child_list order
    
    * [add] child_list spec
    
    * [modify] child_list spec
    
    * [modify] child_list order
    
    * [modify] add empty line after next
    
    * [modify] cms child_list child_items

--- app/models/concerns/cms/child_list.rb
+++ app/models/concerns/cms/child_list.rb
@@ -49,8 +49,35 @@ module Cms::ChildList
   def child_items
     return category_nodes if self.route == 'category/node'
     return category_pages if self.route == 'category/page'
+    return child_nodes if child_pages.blank?
+    return child_pages if child_nodes.blank?
 
-    child_nodes + child_pages
+    items = []
+    nodes = child_nodes.to_a
+    pages = child_pages.to_a
+    child_list_limit.times do
+      cmp = 0
+      self.sort_hash.each do |k, v|
+        cmp = (nodes.first.send(k) <=> pages.first.send(k)) * v
+        break if cmp.nonzero?
+      end
+      cmp = nodes.first.id <=> pages.first.id if cmp.zero?
+      cmp = nodes.first.collection_name <=> pages.first.collection_name if cmp.zero?
+      if cmp.negative?
+        items << nodes.shift
+        if nodes.blank?
+          items << pages
+          break
+        end
+      else
+        items << pages.shift
+        if pages.blank?
+          items << nodes
+          break
+        end
+      end
+    end
+    items.flatten.take(child_list_limit)
   end
 
   def template_variable_handler_child_items(name, issuer)
--- spec/features/cms/agents/nodes/node_spec.rb
+++ spec/features/cms/agents/nodes/node_spec.rb
@@ -35,4 +35,34 @@ describe "cms_agents_nodes_node", type: :feature, dbscope: :example do
       expect(page).to have_selector("a[href='/mobile/node/item/']")
     end
   end
+
+  context "public with child_item conditions" do
+    let (:upper_html) { '<div>' }
+    let (:loop_html) { '<h2><a href="#{url}">#{index_name}</a></h2>#{child_items}' }
+    let (:lower_html) { '</div>' }
+    let (:child_upper_html) { '<ul>' }
+    let (:child_loop_html) { '<li>#{index_name}</li>' }
+    let (:child_lower_html) { '</ul>' }
+    let(:cms_node) do
+      create :cms_node_node, layout_id: layout.id, filename: "cms-node",
+             upper_html: upper_html, loop_html: loop_html, lower_html: lower_html,
+             child_upper_html: child_upper_html, child_loop_html: child_loop_html, child_lower_html: child_lower_html
+    end
+    let!(:pages) { create :cms_node_page, layout_id: layout.id, filename: "cms-node/pages", sort: 'order' }
+    let!(:docs_node) { create :cms_node_node, layout_id: layout.id, filename: "cms-node/pages/node", order: 20 }
+
+    let!(:item_1) { create :cms_page, filename: "cms-node/pages/item_1.html", group_ids: [cms_group.id], order: 10 }
+    let!(:item_2) { create :article_page, filename: "cms-node/pages/item_2.html", group_ids: [cms_group.id], order: 30 }
+
+    before do
+      Capybara.app_host = "http://#{site.domain}"
+    end
+
+    it "#index" do
+      visit cms_node.url
+      expect(status_code).to eq 200
+      expect(page).to have_link(pages.name)
+      expect(page.body.include?("<li>#{item_1.name}</li><li>#{docs_node.name}</li><li>#{item_2.name}</li>")).to be_truthy
+    end
+  end
 end
