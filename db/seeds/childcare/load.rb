9cba81aedcd2ee6b411dd814a325bb974ee8213c
itowtips <ito@web-tips.co.jp>
2020-03-16 09:51:20 +0900

    [modify] cms translate : import translate lang

--- db/seeds/childcare/load.rb
+++ db/seeds/childcare/load.rb
@@ -795,3 +795,10 @@ if @site.subdir.present?
   ENV["site"]=@site.host
   Rake::Task['cms:set_subdir_url'].invoke
 end
+
+## -------------------------------------
+puts "# translate_lang"
+item = Translate::Lang.new
+item.cur_site = @site
+item.in_file = Fs::UploadedFile.create_from_file("#{Rails.root}/db/seeds/demo/translate/lang.csv")
+item.import_csv
