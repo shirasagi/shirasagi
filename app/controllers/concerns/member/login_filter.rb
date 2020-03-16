e7089898d19aead35b3aa86d12232c29e54022c9
NAKANO Hideo <nakano@web-tips.co.jp>
2020-03-16 19:36:27 +0900

    [fix] potential vulnerability: start new session and destroy old session at login

--- app/controllers/concerns/member/login_filter.rb
+++ app/controllers/concerns/member/login_filter.rb
@@ -35,6 +35,7 @@ module Member::LoginFilter
 
   def set_member(member, timestamp = Time.zone.now.to_i)
     if @cur_site
+      reset_session
       session[session_member_key] = {
         "member_id" => member.id,
         "remote_addr" => remote_addr,
