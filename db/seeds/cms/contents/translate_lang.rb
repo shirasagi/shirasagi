4db76526ea09118777b340e7f5aec8f127ecda1a
itowtips <ito@web-tips.co.jp>
2020-03-16 10:30:16 +0900

    [modify] cms translate mock api (#3502)
    * [modify] cms translate : add loopback option at mock api
    
    * [modify] cms translate : hide ms x-metered-usage
    
    * [modify] cms translate : import translate lang

--- db/seeds/cms/contents/translate_lang.rb
+++ db/seeds/cms/contents/translate_lang.rb
@@ -1,5 +1,5 @@
 puts "# translate_lang"
 item = Translate::Lang.new
 item.cur_site = @site
-item.in_file = Fs::UploadedFile.create_from_file("translate/lang.csv")
+item.in_file = Fs::UploadedFile.create_from_file("#{Rails.root}/db/seeds/demo/translate/lang.csv")
 item.import_csv
