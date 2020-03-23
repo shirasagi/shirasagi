a5deede48855627d60de7b7b990b6ed7f57cab04
HiraiY <hirai@web-tips.co.jp>
2020-03-23 15:01:45 +0900

    [fixed] reverse lat and lng code

--- app/models/map/extensions/points.rb
+++ app/models/map/extensions/points.rb
@@ -20,7 +20,9 @@ class Map::Extensions::Points < Array
           point["loc"] = Map::Extensions::Loc.mongoize(point["loc"])
           lat = point["loc"][0]
           lng = point["loc"][1]
-          point["loc"] = [lng, lat] if lat < lng
+          unless lat.nil? && lng.nil?
+            point["loc"] = [lng, lat] if lat < lng
+          end
           point
         end
         ary = ary.select { |point| point["loc"].present? }
