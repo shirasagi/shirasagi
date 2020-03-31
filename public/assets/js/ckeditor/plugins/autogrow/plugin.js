5c9b09e2dd28c4e19b4983b43b9e3a6e383ebe02
NAKANO Hideo <nakano@web-tips.co.jp>
2020-03-31 16:21:45 +0900

    [modify] version up CKEditor to 4.14.0

--- public/assets/js/ckeditor/plugins/autogrow/plugin.js
+++ public/assets/js/ckeditor/plugins/autogrow/plugin.js
@@ -1,10 +1,10 @@
-﻿/**
- * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+/**
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 /**
- * @fileOverview AutoGrow plugin.
+ * @fileOverview The Auto Grow plugin.
  */
 
 'use strict';
@@ -34,7 +34,7 @@
 			scrollable,
 			marker,
 			configBottomSpace = editor.config.autoGrow_bottomSpace || 0,
-			configMinHeight = editor.config.autoGrow_minHeight != undefined ? editor.config.autoGrow_minHeight : 200,
+			configMinHeight = editor.config.autoGrow_minHeight !== undefined ? editor.config.autoGrow_minHeight : 200,
 			configMaxHeight = editor.config.autoGrow_maxHeight || Infinity,
 			maxHeightIsUnlimited = !editor.config.autoGrow_maxHeight;
 
@@ -69,7 +69,7 @@
 			} );
 		}
 
-		// Coordinate with the "maximize" plugin. (#9311)
+		// Coordinate with the "maximize" plugin. (https://dev.ckeditor.com/ticket/9311)
 		editor.on( 'afterCommandExec', function( evt ) {
 			if ( evt.data.name == 'maximize' && evt.editor.mode == 'wysiwyg' ) {
 				if ( evt.data.command.state == CKEDITOR.TRISTATE_ON )
@@ -82,7 +82,10 @@
 		editor.on( 'contentDom', refreshCache );
 
 		refreshCache();
-		editor.config.autoGrow_onStartup && editor.execCommand( 'autogrow' );
+
+		if ( editor.config.autoGrow_onStartup && editor.editable().isVisible() ) {
+			editor.execCommand( 'autogrow' );
+		}
 
 		function refreshCache() {
 			doc = editor.document;
@@ -90,6 +93,14 @@
 
 			// Quirks mode overflows body, standards overflows document element.
 			scrollable = CKEDITOR.env.quirks ? doc.getBody() : doc.getDocumentElement();
+
+			// Reset scrollable body height and min-height css values.
+			// While set by outside code it may break resizing. (https://dev.ckeditor.com/ticket/14620)
+			var body = CKEDITOR.env.quirks ? scrollable : scrollable.findOne( 'body' );
+			if ( body ) {
+				body.setStyle( 'height', 'auto' );
+				body.setStyle( 'min-height', CKEDITOR.env.safari ? '0%' : 'auto' ); // Safari does not support 'min-height: auto'.
+			}
 
 			marker = CKEDITOR.dom.element.createFromHtml(
 				'<span style="margin:0;padding:0;border:0;clear:both;width:1px;height:1px;display:block;">' +
@@ -103,7 +114,7 @@
 
 			return (
 				!editor.window ||
-				// Disable autogrow when the editor is maximized. (#6339)
+				// Disable autogrow when the editor is maximized. (https://dev.ckeditor.com/ticket/6339)
 				maximizeCommand && maximizeCommand.state == CKEDITOR.TRISTATE_ON
 			);
 		}
@@ -132,7 +143,7 @@
 			newHeight = Math.max( newHeight, configMinHeight );
 			newHeight = Math.min( newHeight, configMaxHeight );
 
-			// #10196 Do not resize editor if new height is equal
+			// https://dev.ckeditor.com/ticket/10196 Do not resize editor if new height is equal
 			// to the one set by previous resizeEditor() call.
 			if ( newHeight != currentHeight && lastHeight != newHeight ) {
 				newHeight = editor.fire( 'autoGrow', { currentHeight: currentHeight, newHeight: newHeight } ).newHeight;
@@ -151,7 +162,11 @@
 } )();
 
 /**
- * The minimum height that the editor can reach using the AutoGrow feature.
+ * The minimum height that the editor can assume when adjusting to content by using the Auto Grow
+ * feature. This option accepts a value in pixels, without the unit (for example: `300`).
+ *
+ * Read more in the [documentation](#!/guide/dev_autogrow)
+ * and see the [SDK sample](https://sdk.ckeditor.com/samples/autogrow.html).
  *
  *		config.autoGrow_minHeight = 300;
  *
@@ -161,7 +176,12 @@
  */
 
 /**
- * The maximum height that the editor can reach using the AutoGrow feature. Zero means unlimited.
+ * The maximum height that the editor can assume when adjusting to content by using the Auto Grow
+ * feature. This option accepts a value in pixels, without the unit (for example: `600`).
+ * Zero (`0`) means that the maximum height is not limited and the editor will expand infinitely.
+ *
+ * Read more in the [documentation](#!/guide/dev_autogrow)
+ * and see the [SDK sample](https://sdk.ckeditor.com/samples/autogrow.html).
  *
  *		config.autoGrow_maxHeight = 400;
  *
@@ -171,7 +191,11 @@
  */
 
 /**
- * Whether to have the auto grow happen on editor creation.
+ * Whether automatic editor height adjustment brought by the Auto Grow feature should happen on
+ * editor creation.
+ *
+ * Read more in the [documentation](#!/guide/dev_autogrow)
+ * and see the [SDK sample](https://sdk.ckeditor.com/samples/autogrow.html).
  *
  *		config.autoGrow_onStartup = true;
  *
@@ -181,7 +205,14 @@
  */
 
 /**
- * Extra height in pixel to leave between the bottom boundary of content with document size when auto resizing.
+ * Extra vertical space to be added between the content and the editor bottom bar when adjusting
+ * editor height to content by using the Auto Grow feature. This option accepts a value in pixels,
+ * without the unit (for example: `50`).
+ *
+ * Read more in the [documentation](#!/guide/dev_autogrow)
+ * and see the [SDK sample](https://sdk.ckeditor.com/samples/autogrow.html).
+ *
+ *		config.autoGrow_bottomSpace = 50;
  *
  * @since 3.6.2
  * @cfg {Number} [autoGrow_bottomSpace=0]
@@ -189,13 +220,13 @@
  */
 
 /**
- * Fired when the AutoGrow plugin is about to change the size of the editor.
+ * Fired when the Auto Grow plugin is about to change the size of the editor.
  *
  * @event autogrow
  * @member CKEDITOR.editor
  * @param {CKEDITOR.editor} editor This editor instance.
  * @param data
- * @param {Number} data.currentHeight The current height of the editor (before resizing).
- * @param {Number} data.newHeight The new height of the editor (after resizing). It can be changed
- * to determine a different height value to be used instead.
+ * @param {Number} data.currentHeight The current editor height (before resizing).
+ * @param {Number} data.newHeight The new editor height (after resizing). It can be changed
+ * to achieve a different height value to be used instead.
  */
