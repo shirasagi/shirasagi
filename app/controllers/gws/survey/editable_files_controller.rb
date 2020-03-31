8501cc0d37107daa13a46f2dfe0a640ebdd1824c
NAKANO Hideo <nakano@web-tips.co.jp>
2020-03-31 18:55:13 +0900

    [modify] arrange order of gws/survey's answer columns

--- app/controllers/gws/survey/editable_files_controller.rb
+++ app/controllers/gws/survey/editable_files_controller.rb
@@ -50,7 +50,11 @@ class Gws::Survey::EditableFilesController < ApplicationController
   public
 
   def index
-    @items = @cur_form.files.search(@s).order_by(updated: -1).page(params[:page]).per(50)
+    criteria = @cur_form.files.search(@s)
+    if !@cur_form.anonymous?
+      criteria = criteria.order_by(user_uid: 1, updated: -1)
+    end
+    @items = criteria.page(params[:page]).per(50)
   end
 
   def summary
@@ -71,7 +75,11 @@ class Gws::Survey::EditableFilesController < ApplicationController
   end
 
   def download_all
-    @items = @cur_form.files.search(@s).order_by(updated: -1)
+    criteria = @cur_form.files.search(@s)
+    if !@cur_form.anonymous?
+      criteria = criteria.order_by(user_uid: 1, updated: -1)
+    end
+    @items = criteria
 
     if request.get?
       render
