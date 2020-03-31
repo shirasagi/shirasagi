65d5c5a909bb81afecf5bd5ccbd8914429270e08
itowtips <ito@web-tips.co.jp>
2020-03-31 17:35:46 +0900

    [fix] Webmail::Mailer : raise suppressed error

--- lib/ss/mailer/rescuable.rb
+++ lib/ss/mailer/rescuable.rb
@@ -2,8 +2,10 @@ module SS::Mailer::Rescuable
   extend ActiveSupport::Concern
 
   included do
-    rescue_from StandardError do |e|
-      Rails.logger.error("#{e.class} (#{e.message}):\n  #{e.backtrace.join("\n  ")}")
-    end
+    rescue_from StandardError, with: :rescue_deliver
+  end
+
+  def rescue_deliver(e)
+    Rails.logger.error("#{e.class} (#{e.message}):\n  #{e.backtrace.join("\n  ")}")
   end
 end
