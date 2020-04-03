8664389390d616355861f2a6f140e5fc7ef628fd
itowtips <ito@web-tips.co.jp>
2020-04-03 09:36:52 +0900

    [modify] cms editor link check : basic_authentication support (#3551)

--- app/controllers/cms/link_check_controller.rb
+++ app/controllers/cms/link_check_controller.rb
@@ -20,8 +20,10 @@ class Cms::LinkCheckController < ApplicationController
 
     url = normalize_url(url)
     proxy = ( url =~ /^https/ ) ? ENV['HTTPS_PROXY'] : ENV['HTTP_PROXY']
+    http_basic_authentication = SS::MessageEncryptor.http_basic_authentication
     opts = {
       proxy: proxy,
+      http_basic_authentication: http_basic_authentication,
       progress_proc: ->(size) do
         progress_data_size = size
         raise "200"
