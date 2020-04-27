e0cff644f020ec6b01da62794417a47643e6459a
itowtips <ito@web-tips.co.jp>
2020-04-27 11:42:26 +0900

    [modify] refactor

--- app/controllers/cms/link_check_controller.rb
+++ app/controllers/cms/link_check_controller.rb
@@ -15,115 +15,19 @@ class Cms::LinkCheckController < ApplicationController
     @cur_user = get_user_by_session
   end
 
-  def check_url(url)
-    @head_request_timeout = SS.config.cms.check_links["head_request_timeout"] rescue 5
-    progress_data_size = nil
-    http_basic_authentication = SS::MessageEncryptor.http_basic_authentication
-
-    redirection = 0
-    max_redirection = SS.config.cms.check_links["max_redirection"].to_i
-
-    begin
-      url = normalize_url(url)
-      proxy = ( url =~ /^https/ ) ? ENV['HTTPS_PROXY'] : ENV['HTTP_PROXY']
-      opts = {
-        proxy: proxy,
-        redirect: false,
-        http_basic_authentication: http_basic_authentication,
-        progress_proc: ->(size) do
-          progress_data_size = size
-          raise "200"
-        end
-      }
-
-      Timeout.timeout(@head_request_timeout) do
-        open(url, opts) { |_f| }
-      end
-
-      return {
-        code: 200,
-        redirection: redirection
-      }
-    rescue OpenURI::HTTPRedirect => e
-      if redirection >= max_redirection
-        return {
-          code: 0,
-          message: I18n.t("errors.messages.link_check_failed_redirection"),
-          redirection: redirection
-        }
-      else
-        redirection += 1
-        url = e.uri
-        retry
-      end
-    rescue Addressable::URI::InvalidURIError
-      return {
-        code: 0,
-        message: I18n.t("errors.messages.link_check_failed_invalid_link"),
-        redirection: redirection
-      }
-    rescue OpenSSL::SSL::SSLError => e
-      return {
-        code: 0,
-        message: I18n.t("errors.messages.link_check_failed_certificate_verify_failed"),
-        redirection: redirection
-      }
-    rescue Timeout::Error
-      return {
-        code: 0,
-        message: I18n.t("errors.messages.link_check_failed_timeout"),
-        redirection: redirection
-      }
-    rescue => e
-      if progress_data_size
-        code = 200
-        message = nil
-      else
-        code = 0
-        if e.to_s == "401 Unauthorized"
-          message = I18n.t("errors.messages.link_check_failed_unauthorized")
-        else
-          message = I18n.t("errors.messages.link_check_failed_not_found")
-        end
-      end
-
-      return {
-        code: code,
-        message: message,
-        redirection: redirection
-      }
-    end
-  end
-
-  def normalize_url(url)
-    uri = ::Addressable::URI.parse(url)
-    url = uri.normalize.to_s
-
-    if @cur_user && @fs_url && url.start_with?(@fs_url)
-      token = SS::AccessToken.new(cur_user: @cur_user)
-      token.create_token
-      if token.save
-        url += uri.query.present? ? "&" : "?"
-        url += "access_token=#{token.token}"
-      end
-    end
-
-    url
-  end
-
   public
 
   def check
     results = {}
     url = params[:url]
-    @root_url = params[:root_url].presence
-    @fs_url = ::File.join(@root_url, "/fs/") if @root_url
+    root_url = params[:root_url]
+    checker = Cms::LinkChecker.new(cur_user: @cur_user, root_url: root_url)
 
     raise "400" if url.blank?
     url = url.values if url.is_a?(Hash)
     url.each do |link|
       next if results[link]
-      results[link] = check_url(link)
+      results[link] = checker.check_url(link)
     end
 
     respond_to do |format|
--- app/models/cms/column/value/base.rb
+++ app/models/cms/column/value/base.rb
@@ -163,10 +163,9 @@ class Cms::Column::Value::Base
 
   def validate_link_check
     @link_errors = {}
-    @root_url = column.form.site.full_root_url
-    @fs_url = ::File.join(@root_url, "/fs/")
-    @head_request_timeout = SS.config.cms.check_links["head_request_timeout"] rescue 5
-    check = {}
+
+    root_url = column.form.site.full_root_url
+    checker = Cms::LinkChecker.new(cur_user: @link_check_user, root_url: root_url)
 
     fields.each_key do |key|
       next if LINK_CHECK_EXCLUSION_FIELDS.include?(key)
@@ -177,114 +176,16 @@ class Cms::Column::Value::Base
         next if url[0] == '#'
 
         if url[0] == "/"
-          url = ::File.join(@root_url, url)
+          url = ::File.join(root_url, url)
         end
 
-        next if check.key?(url)
-        check[url] = true
-
-        @link_errors[url] = check_url(url)
+        next if @link_errors[url]
+        @link_errors[url] = checker.check_url(url)
       end
     end
   end
 
   def find_url(val)
     val.scan(%r!<a.*?href="(.+?)">.+?</a>!).flatten | URI.extract(val, %w(http https))
-  end
-
-  def check_url(url)
-    progress_data_size = nil
-
-    url = normalize_url(url)
-    proxy = ( url =~ /^https/ ) ? ENV['HTTPS_PROXY'] : ENV['HTTP_PROXY']
-    http_basic_authentication = SS::MessageEncryptor.http_basic_authentication
-
-    redirection = 0
-    max_redirection = SS.config.cms.check_links["max_redirection"].to_i
-
-    begin
-      opts = {
-        proxy: proxy,
-        redirect: false,
-        http_basic_authentication: http_basic_authentication,
-        progress_proc: ->(size) do
-          progress_data_size = size
-          raise "200"
-       end
-      }
-
-      Timeout.timeout(@head_request_timeout) do
-        ::OpenURI.open_uri(url, opts) { |_f| }
-      end
-
-      return {
-        code: 200,
-        redirection: redirection
-      }
-    rescue OpenURI::HTTPRedirect => e
-      if redirection >= max_redirection
-        return {
-          code: 0,
-          message: I18n.t("errors.messages.link_check_failed_redirection"),
-          redirection: redirection
-        }
-      else
-        redirection += 1
-        url = e.uri
-        retry
-      end
-    rescue Addressable::URI::InvalidURIError
-      return {
-        code: 0,
-        message: I18n.t("errors.messages.link_check_failed_invalid_link"),
-        redirection: redirection
-      }
-    rescue OpenSSL::SSL::SSLError => e
-      return {
-        code: 0,
-        message: I18n.t("errors.messages.link_check_failed_certificate_verify_failed"),
-        redirection: redirection
-      }
-    rescue Timeout::Error
-      return {
-        code: 0,
-        message: I18n.t("errors.messages.link_check_failed_timeout"),
-        redirection: redirection
-      }
-    rescue => e
-      if progress_data_size
-        code = 200
-        message = nil
-      else
-        code = 0
-        if e.to_s == "401 Unauthorized"
-          message = I18n.t("errors.messages.link_check_failed_unauthorized")
-        else
-          message = I18n.t("errors.messages.link_check_failed_not_found")
-        end
-      end
-
-      return {
-        code: code,
-        message: message,
-        redirection: redirection
-      }
-    end
-  end
-
-  def normalize_url(url)
-    uri = ::Addressable::URI.parse(url)
-    url = uri.normalize.to_s
-
-    if @link_check_user && @fs_url && url.start_with?(@fs_url)
-      token = SS::AccessToken.new(cur_user: @link_check_user)
-      token.create_token
-      if token.save
-        url += uri.query.present? ? "&" : "?"
-        url += "access_token=#{token.token}"
-      end
-    end
-
-    url
   end
 end
