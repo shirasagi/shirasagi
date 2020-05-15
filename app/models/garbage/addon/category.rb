ae6b09a349b4c34e474b814ecf48b993f29bade4
itowtips <ito@web-tips.co.jp>
2020-05-15 13:37:30 +0900

    [fix] garbage loop html (#3587)
    * [fix] cms garbage list addon
    
    * [modify] liquidize export
    
    * [add] spec
    
    * [modify] result html
    
    Co-authored-by: tetsu-26 <heiwa326427@gmail.com>

--- app/models/garbage/addon/category.rb
+++ app/models/garbage/addon/category.rb
@@ -6,6 +6,18 @@ module Garbage::Addon
     included do
       embeds_ids :categories, class_name: "Garbage::Node::Category"
       permit_params category_ids: []
+
+      template_variable_handler(:categories, :template_variable_handler_categories)
+
+      liquidize do
+        export as: :categories do
+          categories.and_public.order_by(order: 1, name: 1)
+        end
+      end
+    end
+
+    def template_variable_handler_categories(name, issuer)
+      ERB::Util.html_escape self.categories.map(&:name).join("\n")
     end
   end
 end
