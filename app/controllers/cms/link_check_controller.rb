1aa0f3b91355f81e92b17c27974b3ee193e17354
itowtips <ito@web-tips.co.jp>
2020-05-25 18:41:15 +0900

    [modify] link check message (#3583)
    * [modify] lick check message
    
    * [modify] refactor
    
    * [modify] message
    
    * [fix] spec failure

--- app/controllers/cms/link_check_controller.rb
+++ app/controllers/cms/link_check_controller.rb
@@ -15,79 +15,23 @@ class Cms::LinkCheckController < ApplicationController
     @cur_user = get_user_by_session
   end
 
-  def check_url(url)
-    @head_request_timeout = SS.config.cms.check_links["head_request_timeout"] rescue 5
-    progress_data_size = nil
-    http_basic_authentication = SS::MessageEncryptor.http_basic_authentication
-
-    redirection = 0
-    max_redirection = SS.config.cms.check_links["max_redirection"].to_i
-
-    begin
-      url = normalize_url(url)
-      proxy = ( url =~ /^https/ ) ? ENV['HTTPS_PROXY'] : ENV['HTTP_PROXY']
-      opts = {
-        proxy: proxy,
-        redirect: false,
-        http_basic_authentication: http_basic_authentication,
-        progress_proc: ->(size) do
-          progress_data_size = size
-          raise "200"
-        end
-      }
-
-      Timeout.timeout(@head_request_timeout) do
-        open(url, opts) { |_f| }
-      end
-
-      200
-    rescue OpenURI::HTTPRedirect => e
-      return 0 if redirection >= max_redirection
-      redirection += 1
-      url = e.uri
-      retry
-    rescue URI::InvalidURIError
-      0
-    rescue Timeout::Error
-      0
-    rescue => _e
-      progress_data_size ? 200 : 0
-    end
-  end
-
-  def normalize_url(url)
-    uri = ::Addressable::URI.parse(url)
-    url = uri.normalize.to_s
-
-    if @cur_user && @fs_url && url.start_with?(@fs_url)
-      token = SS::AccessToken.new(cur_user: @cur_user)
-      token.create_token
-      if token.save
-        url += uri.query.present? ? "&" : "?"
-        url += "access_token=#{token.token}"
-      end
-    end
-
-    url
-  end
-
   public
 
   def check
-    result = {}
+    results = {}
     url = params[:url]
-    @root_url = params[:root_url].presence
-    @fs_url = ::File.join(@root_url, "/fs/") if @root_url
+    root_url = params[:root_url]
+    checker = Cms::LinkChecker.new(cur_user: @cur_user, root_url: root_url)
 
     raise "400" if url.blank?
     url = url.values if url.is_a?(Hash)
     url.each do |link|
-      next if result[link]
-      result[link] = check_url(link)
+      next if results[link]
+      results[link] = checker.check_url(link)
     end
 
     respond_to do |format|
-      format.json { render json: result.to_json }
+      format.json { render json: results.to_json }
     end
   end
 end
