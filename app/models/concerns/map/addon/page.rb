d81a8573fdb132e639e110b5cfe1956daf859402
HiraiY <hirai@web-tips.co.jp>
2020-05-27 11:19:18 +0900

    [add] added map center_position and zoom_level setting form

--- app/models/concerns/map/addon/page.rb
+++ app/models/concerns/map/addon/page.rb
@@ -6,8 +6,12 @@ module Map::Addon
     included do
       field :map_points, type: Map::Extensions::Points, default: []
       field :map_zoom_level, type: Integer
+      field :center_setting, type: String
+      field :set_center_position, type: String
+      field :zoom_setting, type: String
+      field :set_zoom_level, type: Integer
       permit_params map_points: [ :name, :loc, :text, :link, :image ]
-      permit_params :map_zoom_level
+      permit_params :map_zoom_level, :center_setting, :set_center_position, :zoom_setting, :set_zoom_level
 
       if respond_to? :liquidize
         liquidize do
