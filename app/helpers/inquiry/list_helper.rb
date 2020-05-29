6df8675f03f31ee28dd41dd601bc5ff1e0547167
HiraiY <hirai@web-tips.co.jp>
2020-05-29 18:54:01 +0900

    [fix] fixed default html

--- app/helpers/inquiry/list_helper.rb
+++ app/helpers/inquiry/list_helper.rb
@@ -1,8 +1,68 @@
 module Inquiry::ListHelper
   include Cms::ListHelper
 
-  def default_page_loop_html
-    render partial: 'inquiry/agents/nodes/node/default_node_loop_html'
+  def default_table_html
+    ih = []
+    ih << "<table id=\"forms\">"
+    ih << "<caption>#{@cur_node.name}</caption>"
+    ih << "<thead>"
+    ih << "<tr>"
+    ih << "<th>#{@cur_node.class.t :name}</th>"
+    ih << "<th>#{t "inquiry.reception_plan"}</th>"
+    show_aggregation = @items.map(&:aggregation_enabled?).index(true)
+    if show_aggregation
+      ih << "<th>#{t "inquiry.aggregate"}</th>"
+    end
+    ih << "</tr>"
+    ih << "</thead>"
+    ih << "<tbody>"
+    @items.each do |item|
+      ih << "<tr>"
+      ih << "<td>"
+      name = item.index_name.presence || item.name
+      if item.reception_enabled?
+        ih << "#{link_to name, item.url}"
+      else
+        ih << "#{name}"
+      end
+      ih << "</td>"
+      ih << "<td>"
+      if item.reception_start_date.present?
+        ih << "#{item.reception_start_date.strftime(t("date.formats.long"))}"
+        ih << "#{t "inquiry.from"}"
+        ih << "#{item.reception_close_date.strftime(t("date.formats.long"))}"
+      end
+      ih << "</td>"
+      if show_aggregation
+        ih << "<td>"
+        if item.aggregation_enabled?
+          ih << "#{link_to t("inquiry.aggregate"), "#{item.url}results.html"}"
+        else
+          ih << "#{t("inquiry.aggregate")}"
+        end
+        ih << "</td>"
+      end
+      ih << "</tr>"
+    end
+    ih << "</tbody>"
+    ih << "</table>"
+    ih.join("\n").freeze
+  end
+
+  def render_inquiry_list(&block)
+    cur_item = @cur_part || @cur_node
+    if @items.blank? && cur_item.try(:no_items_display_state) == 'hide'
+      return cur_item.substitute_html.to_s.html_safe
+    end
+    cur_item.cur_date = @cur_date
+
+    if cur_item.loop_format_shirasagi?
+      render_list_with_shirasagi(cur_item, default_table_html, &block)
+    else
+      source = cur_item.loop_liquid.presence || default_table_html
+      assigns = { "nodes" => @items.to_a.map(&:becomes_with_route) }
+      render_list_with_liquid(source, assigns)
+    end
   end
 
   private
@@ -36,5 +96,18 @@ module Inquiry::ListHelper
     h << cur_item.lower_html.html_safe if cur_item.lower_html.present?
 
     h.join("\n").html_safe
+  end
+
+  def render_list_with_liquid(source, assigns)
+    template = ::Cms.parse_liquid(source, liquid_registers)
+
+    if @cur_part
+      assigns["part"] = @cur_part
+    end
+    if @cur_node
+      assigns["node"] = @cur_node
+    end
+
+    template.render(assigns).html_safe
   end
 end
--- spec/features/inquiry/agents/nodes/inquiry_node_spec.rb
+++ spec/features/inquiry/agents/nodes/inquiry_node_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "inquiry_agents_nodes_node", type: :feature, dbscope: :example do
+describe "inquiry_agents_nodes_node", type: :feature, dbscope: :example, js: true do
   let(:site) { cms_site }
   let(:layout) { create_cms_layout }
   let(:node) do
@@ -64,22 +64,21 @@ describe "inquiry_agents_nodes_node", type: :feature, dbscope: :example do
   end
 
   context "liquid" do
-    let(:loop_html) { '<div class="#{class}"><header><h2>#{name}</h2></header></div>' }
     let(:node) do
       create(
         :inquiry_node_node,
         cur_site: site,
         layout_id: layout.id,
-        loop_format: "liquid",
-        loop_html: loop_html)
+        loop_format: "liquid")
     end
+
     it do
       visit node.full_url
 
       expect(page).not_to have_content(node.upper_html)
-      expect(page).to have_css("h2 a", text: child_node1.name)
-      expect(page).to have_css("h2 a", text: child_node2.name)
-      expect(page).to have_css("h2 a", text: child_node3.name)
+      expect(page).to have_css("a", text: child_node1.name)
+      expect(page).to have_css("a", text: child_node2.name)
+      expect(page).to have_css("a", text: child_node3.name)
       expect(page).not_to have_content(node.lower_html)
     end
   end
