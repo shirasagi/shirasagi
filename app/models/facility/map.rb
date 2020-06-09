4fb0b4faa7c1e6cdb41d8e59870a73caf21a57ff
HiraiY <hirai@web-tips.co.jp>
2020-06-09 11:14:52 +0900

    [fix] fixed numeric check for latlon

--- app/models/facility/map.rb
+++ app/models/facility/map.rb
@@ -23,7 +23,7 @@ class Facility::Map
     if latlon.length == 2
       lat = latlon[0]
       lon = latlon[1]
-      if !lat.match?(/^([+-]?[1-9]\d*|0)(\.\d+)?$/) || !lon.match?(/^([+-]?[1-9]\d*|0)(\.\d+)?$/)
+      if !lat.numeric? || !lon.numeric?
         self.errors.add :set_center_position, :invalid_latlon
       elsif lat.to_f.floor < -90 || lat.to_f.ceil > 90 || lon.to_f.floor < -180 || lon.to_f.ceil > 180
         self.errors.add :set_center_position, :invalid_latlon
