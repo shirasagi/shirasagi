e3b5f4627d7fbf475f1b30094089e17f7ab92fba
NAKANO Hideo <nakano@web-tips.co.jp>
2020-06-17 11:15:55 +0900

    [add] sys/diag/servers spec

--- app/controllers/sys/diag/servers_controller.rb
+++ app/controllers/sys/diag/servers_controller.rb
@@ -1,6 +1,8 @@
 class Sys::Diag::ServersController < ApplicationController
   include Sys::BaseFilter
 
+  PROC_MEMINFO_FILE_PATH = "/proc/meminfo".freeze
+
   navi_view "sys/diag/main/navi"
   menu_view nil
 
---
+++ spec/features/sys/diag/severs_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe "sys_diag_servers", type: :feature, dbscope: :example do
+  before do
+    @save = Sys::Diag::ServersController::PROC_MEMINFO_FILE_PATH
+    Sys::Diag::ServersController.send(:remove_const, :PROC_MEMINFO_FILE_PATH)
+    Sys::Diag::ServersController.const_set(:PROC_MEMINFO_FILE_PATH, "#{Rails.root}/spec/fixtures/sys/diag/meminfo-1")
+    login_sys_user
+  end
+
+  after do
+    Sys::Diag::ServersController.send(:remove_const, :PROC_MEMINFO_FILE_PATH)
+    Sys::Diag::ServersController.const_set(:PROC_MEMINFO_FILE_PATH, @save)
+  end
+
+  it do
+    visit sys_diag_server_path
+
+    within "#server-info" do
+      expect(page).to have_css("dt", text: "uptime")
+    end
+
+    within "#mem-info" do
+      expect(page).to have_css("dt", text: "MemTotal")
+      expect(page).to have_css("dt", text: "DirectMap2M")
+    end
+
+    within "#http-env-list" do
+      expect(page).to have_css("dt", text: "CONTENT_LENGTH")
+      expect(page).to have_css("dt", text: "SERVER_PORT")
+    end
+
+    within "#rack-env-list" do
+      expect(page).to have_css("dt", text: "rack.errors")
+      expect(page).to have_css("dt", text: "rack.version")
+    end
+
+    within "#rails-env-list" do
+      expect(page).to have_css("dt", text: "action_controller.instance")
+      expect(page).to have_css("dt", text: "action_dispatch.use_authenticated_cookie_encryption")
+    end
+
+    within "#other-env-list" do
+      expect(page).to have_css("dt", text: "http_accept_language.parser")
+    end
+  end
+end
