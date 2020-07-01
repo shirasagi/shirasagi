e8af160634ed878a10169104af20f7594b00b296
NAKANO Hideo <nakano@web-tips.co.jp>
2020-07-01 13:49:23 +0900

    [#3647][fix][cms] edit page which applied a urgency layout (#3649)
    * [fix][#3647] layout is unset if urgency layout is set on page
    
    * [add] reproducible spec
    
    * [modify] refactor spec to check for user experiences more

--- spec/features/urgency/layouts_spec.rb
+++ spec/features/urgency/layouts_spec.rb
@@ -3,29 +3,56 @@ require 'spec_helper'
 describe "urgency_layouts", type: :feature, dbscope: :example do
   let!(:site) { cms_site }
 
-  let!(:node) { create_once :urgency_node_layout, filename: "urgency", urgency_default_layout_id: layout1.id }
+  let!(:node) { create :urgency_node_layout, filename: "urgency", urgency_default_layout_id: layout1.id }
   let!(:layout1) { create :cms_layout, cur_site: site, name: unique_id, filename: "layout1.layout.html" }
   let!(:layout2) { create :cms_layout, cur_site: site, name: unique_id, filename: "urgency/layout2.layout.html" }
 
-  let!(:item) { create_once :cms_page, name: "index", filename: "index.html" }
-  let!(:index_path) { urgency_layouts_path site.id, node }
+  let!(:item) { create :cms_page, name: "index", filename: "index.html" }
 
-  context "with auth" do
+  describe "apply urgency layout and vice versa" do
     before { login_cms_user }
 
-    it "#index" do
-      visit index_path
-      expect(current_path).not_to eq sns_login_path
+    it do
+      visit urgency_layouts_path(site: site, cid: node)
 
       click_on layout2.name
       click_on I18n.t('urgency.switch_layout')
-      expect(current_path).to eq index_path
-      expect(Cms::Page.find(item.id).layout_id).to eq layout2.id
+      expect(page).to have_css("#notice", text: I18n.t("ss.notice.saved"))
+
+      item.reload
+      expect(item.layout_id).to eq layout2.id
 
       click_on layout1.name
       click_on I18n.t('urgency.switch_layout')
-      expect(current_path).to eq index_path
-      expect(Cms::Page.find(item.id).layout_id).to eq layout1.id
+      expect(page).to have_css("#notice", text: I18n.t("ss.notice.saved"))
+
+      item.reload
+      expect(item.layout_id).to eq layout1.id
+    end
+  end
+
+  describe "ss-3647" do
+    before { login_cms_user }
+
+    it do
+      # change to urgency layout
+      visit urgency_layouts_path(site: site, cid: node)
+      click_on layout2.name
+      click_on I18n.t('urgency.switch_layout')
+      expect(page).to have_css("#notice", text: I18n.t("ss.notice.saved"))
+
+      item.reload
+      expect(item.layout_id).to eq layout2.id
+
+      # edit page without any modifications
+      visit cms_page_path(site: site, id: item)
+      click_on I18n.t("ss.links.edit")
+      click_on I18n.t("ss.buttons.save")
+      expect(page).to have_css("#notice", text: I18n.t("ss.notice.saved"))
+
+      # confirm that layout is still set
+      item.reload
+      expect(item.layout_id).to eq layout2.id
     end
   end
 end
