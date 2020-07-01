040a02c9d4b5dd2f91c5c29c0008a47cde6ee99a
NAKANO Hideo <nakano@web-tips.co.jp>
2020-07-01 13:49:01 +0900

    [fix][cms] open redirect vulnerability on member login (#3646)
    * [fix] open redirect vulnerability
    
    * [add] reproducible spec

--- app/controllers/concerns/member/login_filter.rb
+++ app/controllers/concerns/member/login_filter.rb
@@ -68,11 +68,6 @@ module Member::LoginFilter
     "#{member_login_node.url}login.html"
   end
 
-  def redirect_url
-    return "/" unless member_login_node
-    member_login_node.redirect_url || "/"
-  end
-
   def translate_redirect_option(opts)
     case opts[:redirect]
     when true
