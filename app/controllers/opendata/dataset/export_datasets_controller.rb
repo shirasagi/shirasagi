e6486860c6b5e176ff3458cbe874b9eed45bb7cb
itowtips <ito@web-tips.co.jp>
2020-07-14 14:21:18 +0900

    [fix] remove space

--- app/controllers/opendata/dataset/export_datasets_controller.rb
+++ app/controllers/opendata/dataset/export_datasets_controller.rb
@@ -33,7 +33,7 @@ class Opendata::Dataset::ExportDatasetsController < ApplicationController
       return
     end
 
-    job_class =  Opendata::Dataset::ExportJob.bind(site_id: @cur_site.id, user_id: @cur_user, node_id: @cur_node.id)
+    job_class = Opendata::Dataset::ExportJob.bind(site_id: @cur_site.id, user_id: @cur_user, node_id: @cur_node.id)
     job_class.perform_later(root_url: root_url)
     render_create true, location: { action: :start_export }, notice: I18n.t("opendata.notice.start_export")
   end
