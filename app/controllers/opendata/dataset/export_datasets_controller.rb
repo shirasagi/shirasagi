185db4201b0cc610865fd3e7457997be637898ca
itowtips <ito@web-tips.co.jp>
2020-07-15 10:55:45 +0900

    [fix] Opendata::Dataset::ExportJob : use perform_later (#3659)
    * [fix] Opendata::Dataset::ExportJob : use perform_later
    
    * [fix] locale
    
    * [fix] job limit error message
    
    * [fix] remove space

--- app/controllers/opendata/dataset/export_datasets_controller.rb
+++ app/controllers/opendata/dataset/export_datasets_controller.rb
@@ -26,7 +26,15 @@ class Opendata::Dataset::ExportDatasetsController < ApplicationController
 
     root_url = params.dig(:item, :root_url)
 
-    Opendata::Dataset::ExportJob.bind(site_id: @cur_site.id, user_id: @cur_user, node_id: @cur_node.id).perform_now(root_url: root_url)
+    unless Opendata::Dataset::ExportJob.check_size_limit_per_user?(@cur_user.id)
+      @item = @model.new
+      @item.errors.add(:base, t('job.notice.size_limit_exceeded'))
+      render file: :index
+      return
+    end
+
+    job_class = Opendata::Dataset::ExportJob.bind(site_id: @cur_site.id, user_id: @cur_user, node_id: @cur_node.id)
+    job_class.perform_later(root_url: root_url)
     render_create true, location: { action: :start_export }, notice: I18n.t("opendata.notice.start_export")
   end
 
