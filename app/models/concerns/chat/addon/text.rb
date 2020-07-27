1047f86285a6232a9c23a3617005443f427620a1
HiraiY <hirai@web-tips.co.jp>
2020-07-27 14:41:43 +0900

    [fix] fixed to display event

--- app/models/concerns/chat/addon/text.rb
+++ app/models/concerns/chat/addon/text.rb
@@ -11,13 +11,17 @@ module Chat::Addon
       field :question, type: String
       field :chat_success, type: String
       field :chat_retry, type: String
-      field :facility_search, type: String
+      field :set_location, type: String
+      field :radius, type: Integer
 
       has_many :intents, class_name: "Chat::Intent", order: :order.asc, dependent: :destroy
       has_many :chat_categories, class_name: "Chat::Category", order: :order.asc, dependent: :destroy
       has_many :histories, class_name: "Chat::History", dependent: :destroy
 
-      permit_params :first_text, :first_suggest, :exception_text, :response_template, :question, :chat_success, :chat_retry, :facility_search
+      permit_params :first_text, :first_suggest, :exception_text, :response_template, :question, :chat_success, :chat_retry
+      permit_params :set_location, :radius
+
+      validates :radius, :numericality => { :greater_than_or_equal_to => 0 }
     end
   end
 end
