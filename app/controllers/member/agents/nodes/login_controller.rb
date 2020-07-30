298bd29732aec8adddf41c0b052d7baef9d23cc1
NAKANO Hideo <nakano@web-tips.co.jp>
2020-07-30 15:02:22 +0900

    [modify] remove unused method

--- app/controllers/member/agents/nodes/login_controller.rb
+++ app/controllers/member/agents/nodes/login_controller.rb
@@ -29,28 +29,6 @@ class Member::Agents::Nodes::LoginController < ApplicationController
     redirect_to ref
   end
 
-  def make_url_if_trusted(ref)
-    return if ref.blank?
-
-    ref = URI::decode(ref)
-
-    site_url = URI.parse(@cur_site.full_url)
-    full_url = URI.join(site_url, ref) rescue nil
-    return if full_url.blank?
-    return unless %w(http https).include?(full_url.scheme)
-
-    # normalize full url
-    full_url.fragment = nil
-    full_url.query = nil
-
-    # trusted?
-    return if full_url.scheme != site_url.scheme
-    return if full_url.host != site_url.host
-    return if full_url.port != site_url.port
-
-    full_url.to_s
-  end
-
   public
 
   def login
