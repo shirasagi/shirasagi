e4546e76f72ce19d1244efd080300490b3d528a4
NAKANO Hideo <nakano@web-tips.co.jp>
2020-09-15 10:30:55 +0900

    [add] ".site_required" setting

--- app/models/concerns/ss/reference/site.rb
+++ app/models/concerns/ss/reference/site.rb
@@ -1,23 +1,24 @@
-module SS::Reference
-  module Site
-    extend ActiveSupport::Concern
-    extend SS::Translation
+module SS::Reference::Site
+  extend ActiveSupport::Concern
+  extend SS::Translation
+
+  included do
+    cattr_accessor :site_required, instance_accessor: false
+    self.site_required = true
 
     attr_accessor :cur_site
 
-    included do
-      belongs_to :site, class_name: "SS::Site"
+    belongs_to :site, class_name: "SS::Site"
 
-      validates :site_id, presence: true
-      before_validation :set_site_id, if: ->{ @cur_site }
+    validates :site_id, presence: true, if: ->{ self.class.site_required }
+    before_validation :set_site_id, if: ->{ @cur_site }
 
-      scope :site, ->(site) { where(site_id: site.id) }
-    end
+    scope :site, ->(site) { where(site_id: site.id) }
+  end
 
-    private
+  private
 
-    def set_site_id
-      self.site_id ||= @cur_site.id
-    end
+  def set_site_id
+    self.site_id ||= @cur_site.id
   end
 end
