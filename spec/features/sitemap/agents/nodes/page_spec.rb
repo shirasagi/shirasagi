89a13f9ed5d63c7abdff3cb790116ecc6a520edb
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2020-09-17 19:00:28 +0900

    [fix] sitemap addon body load_sitemap_urls

--- spec/features/sitemap/agents/nodes/page_spec.rb
+++ spec/features/sitemap/agents/nodes/page_spec.rb
@@ -4,9 +4,11 @@ describe "sitemap_agents_nodes_page", type: :feature, dbscope: :example do
   let(:site)   { cms_site }
   let(:layout) { create_cms_layout }
   let(:node)   { create :sitemap_node_page, layout_id: layout.id, filename: "node" }
+  let!(:article_node) { create_once :article_node_page }
+  let!(:article_page) { create :article_page, cur_site: site, cur_node: article_node }
 
-  context "public" do
-    let!(:item) { create :cms_page, filename: "node/item" }
+  context "when sitemap_page_state is hide" do
+    let!(:item) { create :sitemap_page, filename: "node/item" }
 
     before do
       Capybara.app_host = "http://#{site.domain}"
@@ -15,6 +17,82 @@ describe "sitemap_agents_nodes_page", type: :feature, dbscope: :example do
     it "#index" do
       visit node.url
       expect(status_code).to eq 200
+      expect(page).to have_selector('a', text: article_node.name)
+      expect(page).to have_no_selector('a', text: article_page.name)
+    end
+
+    it "#xml" do
+      file = Kernel.open(File.join(node.path, 'item.xml'))
+      xml = file.read
+      expect(xml).to include(article_node.full_url)
+      expect(xml).not_to include(article_page.full_url)
+    end
+  end
+
+  context "when sitemap_page_state is show" do
+    let!(:item) { create :sitemap_page, filename: "node/item", sitemap_page_state: 'show' }
+
+    before do
+      Capybara.app_host = "http://#{site.domain}"
+    end
+
+    it "#index" do
+      visit node.url
+      expect(status_code).to eq 200
+      expect(page).to have_selector('a', text: article_node.name)
+      expect(page).to have_selector('a', text: article_page.name)
+    end
+
+    it "#xml" do
+      file = Kernel.open(File.join(node.path, 'item.xml'))
+      xml = file.read
+      expect(xml).to include(article_node.full_url)
+      expect(xml).to include(article_page.full_url)
+      expect(xml).not_to include("#{article_page.url}/")
+    end
+
+    context "with load_sitemap_urls name" do
+      before do
+        item.sitemap_urls = ["#{article_node.url} #article_node", "#{article_page.url} #article_page"]
+        item.save!
+      end
+
+      it "#index" do
+        visit node.url
+        expect(status_code).to eq 200
+        expect(page).to have_selector('a', text: 'article_node')
+        expect(page).to have_selector('a', text: 'article_page')
+      end
+
+      it "#xml" do
+        file = Kernel.open(File.join(node.path, 'item.xml'))
+        xml = file.read
+        expect(xml).to include(article_node.full_url)
+        expect(xml).to include(article_page.full_url)
+        expect(xml).not_to include("#{article_page.url}/")
+      end
+    end
+
+    context "with sitemap_urls slash" do
+      before do
+        item.sitemap_urls = %W[/#{article_node.filename} #{article_page.url}/]
+        item.save!
+      end
+
+      it "#index" do
+        visit node.url
+        expect(status_code).to eq 200
+        expect(page).to have_selector('a', text: article_node.name)
+        expect(page).to have_selector('a', text: article_page.name)
+      end
+
+      it "#xml" do
+        file = Kernel.open(File.join(node.path, 'item.xml'))
+        xml = file.read
+        expect(xml).to include(article_node.full_url)
+        expect(xml).to include(article_page.full_url)
+        expect(xml).not_to include("#{article_page.url}/")
+      end
     end
   end
 end
