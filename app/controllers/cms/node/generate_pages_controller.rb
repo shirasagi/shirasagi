f5271cbf4b6ef4aa3bbef5247ef0763e7b285c76
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2020-11-17 20:05:40 +0900

    [modify] cms generate job filter view

--- app/controllers/cms/node/generate_pages_controller.rb
+++ app/controllers/cms/node/generate_pages_controller.rb
@@ -24,4 +24,13 @@ class Cms::Node::GeneratePagesController < ApplicationController
   def set_item
     @item = Cms::Task.find_or_create_by name: task_name, site_id: @cur_site.id, node_id: @cur_node.id
   end
+
+  public
+
+  def index
+    respond_to do |format|
+      format.html { render file: 'cms/generate_nodes/index' }
+      format.json { render json: @item.to_json(methods: :head_logs) }
+    end
+  end
 end
