c3200dea848ac709f98e8987d6acdd50e945cffb
Uetani <satoshi.0411@icloud.com>
2020-11-17 16:25:43 +0900

    [modified] tryメソッドの追加 twitter_descriptionメソッドの削除  #3448

--- app/controllers/concerns/cms/public_filter/open_graph.rb
+++ app/controllers/concerns/cms/public_filter/open_graph.rb
@@ -8,7 +8,7 @@ module Cms::PublicFilter::OpenGraph
         [ 'og:url', ->() { @cur_item.full_url } ],
         [ 'og:site_name', @cur_site.name ],
         [ 'og:title', ->() { @window_name } ],
-        [ 'og:description', ->() { @cur_item.description } ],
+        [ 'og:description', ->() { @cur_item.try(:description) } ],
         [ 'og:image', ->() { opengraph_image_urls } ],
       ]
     end
