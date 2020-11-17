c4af39e70663daaea2e2b6952f1f261a29d6b682
HiraiY <60082177+HiraiY@users.noreply.github.com>
2020-11-17 11:30:21 +0900

    [add]added to be able to set the center position and zoom level (#3615)
    * [fix][cms][#3597] google map zoom level reset issue
    
    by initializing google map lazily.
    
    * [fix] apply marker bounds to Google Map
    
    * [add] added map center_position and zoom_level setting form
    
    * [fix] fixed to be able to set set_center_position at marker position
    
    * [fix] reflected center_position and zoom_level in google maps
    
    * [fix] fixed center and zoom form style
    
    * [fix] reflected center_position and zoom_level in openlayers
    
    * [fix] fixed google map center and zoom setting
    
    * [fix] fixed google map default zoom
    
    * [fix] fixed default_zoom yml
    
    * [fix] fixed open_street_map helper setting
    
    * [fix] fixed to be able to set set_center_position at marker position in openlayers
    
    * [fix] fixed member phot google_map, openlayers, open_street_map
    
    * [fix] fixed google map default_zoom
    
    * [fix] fixed set_center_position validation
    
    * [add] added tooltip to center and zoom
    
    * [fix] fixed tooltip
    
    * [fix] fixed to display center and zoom setting on the show page
    
    * [fix] fixed center and zoom form
    
    * [add] added zoom level validation
    
    * [add] added center and zoom setting rspec
    
    * [fix] fixed map_points tooltip
    
    * [fix] fixed center_position_validate
    
    * [fix] fixed center position validation rspec
    
    * [fix] fixed numeric check for latlon
    
    * [fix] fixed the way to set center and zoom
    
    Co-authored-by: NAKANO Hideo <nakano@web-tips.co.jp>

--- app/models/facility/map.rb
+++ app/models/facility/map.rb
@@ -9,10 +9,33 @@ class Facility::Map
   include Cms::Addon::GroupPermission
 
   default_scope ->{ where(route: "facility/map") }
+  validate :center_position_validate, if: -> { set_center_position.present? }
+  validate :zoom_level_validate, if: -> { set_zoom_level.present? }
 
   private
 
   def serve_static_file?
     false
+  end
+
+  def center_position_validate
+    latlon = set_center_position.split(',')
+    if latlon.length == 2
+      lat = latlon[0]
+      lon = latlon[1]
+      if !lat.numeric? || !lon.numeric?
+        self.errors.add :set_center_position, :invalid_latlon
+      elsif lat.to_f.floor < -90 || lat.to_f.ceil > 90 || lon.to_f.floor < -180 || lon.to_f.ceil > 180
+        self.errors.add :set_center_position, :invalid_latlon
+      end
+    else
+      self.errors.add :set_center_position, :invalid_latlon
+    end
+  end
+
+  def zoom_level_validate
+    if set_zoom_level <= 0 || set_zoom_level > 21
+      self.errors.add :set_zoom_level, :invalid_zoom_level
+    end
   end
 end
