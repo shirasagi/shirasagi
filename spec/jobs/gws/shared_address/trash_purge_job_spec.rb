c2c0cc7c65cd4d15a8f8302b49bf53b252fa78ba
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2020-11-19 11:59:05 +0900

    [modify] ss addon trash_setting trash_threshold (#3690)

--- spec/jobs/gws/shared_address/trash_purge_job_spec.rb
+++ spec/jobs/gws/shared_address/trash_purge_job_spec.rb
@@ -3,42 +3,75 @@ require 'spec_helper'
 describe Gws::SharedAddress::TrashPurgeJob, dbscope: :example do
   let(:site) { gws_site }
   let(:now) { Time.zone.now.beginning_of_minute }
-  let!(:item) { create(:gws_shared_address_address, cur_site: site, deleted: now - 7.days) }
+  let!(:item1) { create(:gws_shared_address_address, cur_site: site, deleted: now - 7.days) }
+  let!(:item2) { create(:gws_shared_address_address, cur_site: site, deleted: now - 2.years) }
 
   describe '#perform' do
+    context '1 shared_address addresses are purged' do
+      it do
+        expect { described_class.bind(site_id: site).perform_now }.to change { Gws::SharedAddress::Address.count }.by(-1)
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(/INFO -- : .* Started Job/)
+          expect(log.logs).to include(/INFO -- : .* Completed Job/)
+        end
+      end
+    end
+
+    context '2 shared_address addresses are purged' do
+      it do
+        expect { described_class.bind(site_id: site).perform_now(threshold: '7.days') }.to \
+          change { Gws::SharedAddress::Address.count }.by(-2)
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(/INFO -- : .* Started Job/)
+          expect(log.logs).to include(/INFO -- : .* Completed Job/)
+        end
+      end
+    end
+
     context 'no shared_address addresses are purged' do
       it do
-        expect { described_class.bind(site_id: site).perform_now }.not_to(change { Gws::SharedAddress::Address.count })
+        expect { described_class.bind(site_id: site).perform_now(threshold: '3.years') }.to \
+          change { Gws::SharedAddress::Address.count }.by(0)
 
         expect(Job::Log.count).to eq 1
         Job::Log.first.tap do |log|
-          expect(log.logs).to include(/INFO -- : .* Started Job/)
-          expect(log.logs).to include(/INFO -- : .* Completed Job/)
+          expect(log.logs).to include(include("INFO -- : Started Job"))
+          expect(log.logs).to include(include("INFO -- : Completed Job"))
         end
       end
     end
 
-    context 'shared_address addresses are purged' do
-      it do
-        expect { described_class.bind(site_id: site).perform_now(threshold: 7) }.to \
-          change { Gws::SharedAddress::Address.count }.by(-1)
-
-        expect(Job::Log.count).to eq 1
-        Job::Log.first.tap do |log|
-          expect(log.logs).to include(/INFO -- : .* Started Job/)
-          expect(log.logs).to include(/INFO -- : .* Completed Job/)
-        end
-      end
-    end
-
-    context 'when group trash_threshold is 7' do
+    context 'when group trash_threshold is 7.days' do
       before do
         site.set(trash_threshold: 7)
+        site.set(trash_threshold_unit: 'day')
       end
 
       it do
         expect { described_class.bind(site_id: site).perform_now }.to \
-          change { Gws::SharedAddress::Address.count }.by(-1)
+          change { Gws::SharedAddress::Address.count }.by(-2)
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(include("INFO -- : Started Job"))
+          expect(log.logs).to include(include("INFO -- : Completed Job"))
+        end
+      end
+    end
+
+    context 'when group trash_threshold is 3.years' do
+      before do
+        site.set(trash_threshold: 3)
+        site.set(trash_threshold_unit: 'years')
+      end
+
+      it do
+        expect { described_class.bind(site_id: site).perform_now }.to \
+          change { Gws::SharedAddress::Address.count }.by(0)
 
         expect(Job::Log.count).to eq 1
         Job::Log.first.tap do |log|
