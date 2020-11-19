c567f78fb416c77a08a5c359473fc5fe7252827f
NAKANO Hideo <nakano@web-tips.co.jp>
2020-11-19 11:55:28 +0900

    [modify] default log level to debug on development or test (#3679)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Logger
   # config.logger = ActiveSupport::Logger.new("#{Rails.root}/log/development.log")
   config.log_formatter = ::Logger::Formatter.new
-  config.log_level = ENV['DEVELOPMENT_LOG_LEVEL'] || :warn
+  config.log_level = ENV['DEVELOPMENT_LOG_LEVEL'] || :debug
 
   # ActiveJob Queue Adapter
   config.active_job.queue_adapter = :shirasagi
