aada7f2cf004555c362a16fed26349b4bfc0464f
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2020-11-25 11:32:48 +0900

    [fix] cms csv_import_base utf8_file? (#3581)
    * [fix] cms csv_import_base utf8_file?
    
    * [modify] cms csv_import_base non supported encoding
    
    * [modify] article import job spec
    
    * [fix] cms csv import base
    
    * [modify] cms csv_import_base valid_encoding?

--- app/models/concerns/cms/csv_import_base.rb
+++ app/models/concerns/cms/csv_import_base.rb
@@ -20,7 +20,7 @@ module Cms::CsvImportBase
       end
 
       true
-    rescue => e
+    rescue
       false
     ensure
       file.rewind
@@ -28,28 +28,29 @@ module Cms::CsvImportBase
 
     def each_csv(file, &block)
       io = file.to_io
-      if utf8_file?(io)
-        io.seek(3)
-        io.set_encoding('UTF-8')
-      else
-        io.set_encoding('SJIS:UTF-8')
+      if valid_encoding?(io, Encoding::UTF_8)
+        io.set_encoding(Encoding::UTF_8)
+      elsif valid_encoding?(io, Encoding::SJIS)
+        io.set_encoding(Encoding::SJIS, Encoding::UTF_8)
       end
 
       csv = CSV.new(io, { headers: true })
       csv.each(&block)
     ensure
-      io.set_encoding("ASCII-8BIT")
+      io.set_encoding(Encoding::ASCII_8BIT)
     end
 
-    private
-
-    def utf8_file?(file)
+    def valid_encoding?(file, encoding)
       file.rewind
-      bom = file.read(3)
+      if encoding == Encoding::UTF_8
+        bom = file.read(3)
+        bom.force_encoding(encoding)
+        return true if SS::Csv::UTF8_BOM == bom
+        file.rewind
+      end
+      body = file.gets(1000)
       file.rewind
-
-      bom.force_encoding("UTF-8")
-      SS::Csv::UTF8_BOM == bom
+      body.force_encoding(encoding).valid_encoding?
     end
   end
 end
