6917829dc0b13af66b64fb389189c58acd46c53f
NAKANO Hideo <nakano@web-tips.co.jp>
2020-11-25 15:49:51 +0900

    [fixup] cms addon generate_lock (#3580)

--- spec/jobs/cms/node/generate_job/article_spec.rb
+++ spec/jobs/cms/node/generate_job/article_spec.rb
@@ -111,9 +111,9 @@ describe Cms::Node::GenerateJob, dbscope: :example do
 
       expect(Job::Log.count).to eq 1
       Job::Log.first.tap do |log|
-        expect(log.logs).to include(include('INFO -- : Started Job'))
+        expect(log.logs).to include(/INFO -- : .* Started Job/)
         expect(log.logs).to include(include(I18n.t('mongoid.attributes.ss/addon/generate_lock.generate_locked')))
-        expect(log.logs).to include(include('INFO -- : Completed Job'))
+        expect(log.logs).to include(/INFO -- : .* Completed Job/)
       end
     end
   end
