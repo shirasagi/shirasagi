8407234d7cb60b9c1fb1c6e2082b2588c269ea00
itowtips <ito@web-tips.co.jp>
2020-11-26 09:53:14 +0900

    [modify] redirect page : redirect_link url field

--- app/models/concerns/cms/redirect_page.rb
+++ app/models/concerns/cms/redirect_page.rb
@@ -4,9 +4,24 @@ module Cms::RedirectPage
   included do
     field :redirect_link, type: String
     permit_params :redirect_link
+
+    validate :validate_redirect_link, if: ->{ redirect_link.present? }
   end
 
   def view_layout
     redirect_link.present? ? "cms/redirect" : "cms/page"
+  end
+
+  def validate_redirect_link
+    self.redirect_link = redirect_link.strip
+    uri = ::Addressable::URI.parse(redirect_link)
+
+    if uri.scheme.nil?
+      self.redirect_link = ::File.join((@cur_site || site).root_url, redirect_link)
+    elsif uri.scheme !~ /^https?$/
+      raise "invalid scheme"
+    end
+  rescue => e
+    errors.add :redirect_link, :invalid
   end
 end
