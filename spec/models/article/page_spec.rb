8ddcc42221b1331b6406ffef5ddab6b7efbe549b
NAKANO Hideo <nakano@web-tips.co.jp>
2020-11-30 10:54:31 +0900

    [fix][cms] artcile page download error (#3745)
    * [add] reproducible specs
    
    * [fix] article/page download error on nil body_parts

--- spec/models/article/page_spec.rb
+++ spec/models/article/page_spec.rb
@@ -906,4 +906,36 @@ describe Article::Page, dbscope: :example do
       end
     end
   end
+
+  describe ".enum_csv" do
+    let!(:item) { create :article_page, cur_node: node }
+    subject do
+      described_class.all.enum_csv(encoding: "UTF-8", form: nil, cur_user: cms_user, cur_site: cms_site, cur_node: node).to_a
+    end
+
+    context "usual case" do
+      it do
+        expect(subject.length).to eq 2
+        expect(subject[0]).to include(described_class.t(:filename), described_class.t(:name), described_class.t(:body_part))
+        expect(subject[1]).to include(item.basename, item.name)
+      end
+    end
+
+    context "all references or arrays are set null" do
+      before do
+        # some functions are set nil to reference or array fields.
+        # this causes "undefined method" error.
+        item.set(
+          layout_id: nil, body_layout_id: nil, form_id: nil, body_parts: nil, category_ids: nil, event_dates: nil,
+          related_page_ids: nil, related_page_sort: nil, parent_crumb_urls: nil, contact_group_id: nil, group_ids: nil
+        )
+      end
+
+      it do
+        expect(subject.length).to eq 2
+        expect(subject[0]).to include(described_class.t(:filename), described_class.t(:name), described_class.t(:body_part))
+        expect(subject[1]).to include(item.basename, item.name)
+      end
+    end
+  end
 end
