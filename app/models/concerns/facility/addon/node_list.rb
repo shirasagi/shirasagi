e4e46367e4a9c24eb264bcd740fa548c3b7208c3
Uetani <satoshi.0411@icloud.com>
2020-12-01 17:54:35 +0900

    [modified] 施設ガイド投稿画面を新たに作成

---
+++ app/models/concerns/facility/addon/node_list.rb
@@ -0,0 +1,26 @@
+module Facility::Addon
+  module NodeList
+    extend ActiveSupport::Concern
+    extend SS::Addon
+    include Cms::Addon::List::Model
+
+    included do
+      self.use_new_days = false
+    end
+
+    def sort_options
+      [
+        [I18n.t('cms.options.sort.name'), 'name'],
+        [I18n.t('cms.options.sort.filename'), 'filename'],
+        [I18n.t('cms.options.sort.created'), 'created'],
+        [I18n.t('cms.options.sort.updated_1'), 'updated -1'],
+        [I18n.t('cms.options.sort.order'), 'order'],
+      ]
+    end
+
+    def sort_hash
+      return { filename: 1 } if sort.blank?
+      { sort.sub(/ .*/, "") => (/-1$/.match?(sort) ? -1 : 1) }
+    end
+  end
+end
