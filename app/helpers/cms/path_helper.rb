735de73fbd4fd25bd9759d372998b79ebc51bd5b
itowtips <ito@web-tips.co.jp>
2020-12-10 16:18:46 +0900

    [fix] cms mobile preview path (#3768)
    * [fix] cms mobile preview path
    
    * [fix] urgency preview link

--- app/helpers/cms/path_helper.rb
+++
@@ -1,9 +0,0 @@
-module Cms::PathHelper
-  def cms_mobile_preview_path(params = {})
-    if params[:path]
-      params[:path] = ::File.join(@cur_site.mobile_location, params[:path])
-      params[:path].sub!(/^\/+/, "")
-    end
-    cms_preview_path(params)
-  end
-end
