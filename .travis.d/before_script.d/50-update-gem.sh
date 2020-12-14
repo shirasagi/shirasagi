587d54c20b162b0a4514851a6b8e5cfebc47b24b
NAKANO Hideo <nakano@web-tips.co.jp>
2020-12-14 11:42:09 +0900

    [fix] travis failure by removing "50-update-gem.sh"

--- .travis.d/before_script.d/50-update-gem.sh
+++
@@ -1,8 +0,0 @@
-echo "yes | gem update --system --no-document"
-yes | gem update --system --no-document
-
-echo "gem --version"
-gem --version
-
-echo "gem install bundler -v 1.17.3"
-gem install bundler -v 1.17.3
