b196c66b59a569b6b0b7999b25bcee50f52957e4
itowtips <ito@web-tips.co.jp>
2020-12-21 19:03:18 +0900

    [modify] sns post confirmation

--- app/controllers/concerns/cms/base_filter.rb
+++ app/controllers/concerns/cms/base_filter.rb
@@ -8,6 +8,7 @@ module Cms::BaseFilter
     helper Cms::NodeHelper
     helper Cms::FormHelper
     helper Map::MapHelper
+    helper Cms::SnsHelper
     before_action :validate_cms
     before_action :set_cms_assets
     before_action :set_site
---
+++ app/helpers/cms/sns_helper.rb
@@ -0,0 +1,48 @@
+module Cms::SnsHelper
+  def line_post_confirm?
+    return false if !@item.class.include?(Cms::Addon::LinePoster)
+
+    site = @item.site
+    item = (@item.respond_to?(:master) && @item.master) ? @item.master : @item
+    line_posted = item.line_posted.present?
+
+    return false if !site.line_token_enabled?
+    return false if @item.line_auto_post != "active"
+    return false if line_posted && @item.line_edit_auto_post != "active"
+    true
+  end
+
+  def twitter_post_confirm?
+    return false if !@item.class.include?(Cms::Addon::SnsPoster)
+
+    item = (@item.respond_to?(:master) && @item.master) ? @item.master : @item
+    twitter_posted = item.twitter_posted.present?
+
+    return false if !@item.use_twitter_post?
+    return false if twitter_posted && !@item.edit_auto_post_enabled?
+    true
+  end
+
+  def render_sns_post_confirm
+    messages = []
+    if line_post_confirm?
+      messages << t("cms.confirm.line_post_enabled")
+    end
+    if twitter_post_confirm?
+      messages << t("cms.confirm.twitter_post_enabled")
+    end
+
+    return "" if messages.blank?
+
+    h = []
+    h << "<div class=\"sns-post-confirm\">"
+    h << "<h2>#{t("cms.confirm.when_publish")}</h2>"
+    h << "<ul>"
+    messages.each do |message|
+      h << ("<li>" + message + "</li>")
+    end
+    h << "</div>"
+
+    return h.join("\n")
+  end
+end
