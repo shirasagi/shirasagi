c63b75b25f10b0ada97a806a9a4a7b897a5e0e22
NAKANO Hideo <nakano@web-tips.co.jp>
2020-12-22 15:57:22 +0900

    [modify] exit with bundle exit code

--- .travis.d/install.d/90-bundle-install.sh
+++ .travis.d/install.d/90-bundle-install.sh
@@ -3,7 +3,10 @@ for i in $(seq 1 5)
 do
   echo "$i of 5: bundle install --without development --path vendor/bundle"
   bundle install --without development --path vendor/bundle
-  if [ $? -eq 0 ]; then
+  exit_code=$?
+  if [ $exit_code -eq 0 ]; then
     break
   fi
 done
+
+exit $exit_code
