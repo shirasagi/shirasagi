e3f97850facfcd4388006e69f07c5aa555c96139
NAKANO Hideo <nakano@web-tips.co.jp>
2020-12-25 10:44:37 +0900

    [add] .travis.d/install.d/10-docker-pull.sh and invoke it on "install" stage

---
+++ .travis.d/install.d/10-docker-pull.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker pull mongo:4.2
+docker run --name mongo -d -p 27017:27017 mongo:4.2
+docker pull osixia/openldap
+docker pull shirasagi/mail
+docker run --name test_mail -d -p 10143:143 -p 10587:587 shirasagi/mail
+docker ps -a
