a3cd4ff45b6c84c5db50c698329a8caebf0e4819
NAKANO Hideo <nakano@web-tips.co.jp>
2020-12-25 10:50:50 +0900

    [modify] login to docker for avoiding rate limit

--- .travis.d/install.d/10-docker-pull.sh
+++ .travis.d/install.d/10-docker-pull.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ -n "$DOCKER_USERNAME"]; then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+fi
+docker info
 docker pull mongo:4.2
 docker run --name mongo -d -p 27017:27017 mongo:4.2
 docker pull osixia/openldap
