db6cac033314889069feb104b9a4d7af814432ee
NAKANO Hideo <nakano@web-tips.co.jp>
2020-12-25 14:27:23 +0900

    [modify] set trusted url validator

--- spec/models/cms/page_spec.rb
+++ spec/models/cms/page_spec.rb
@@ -88,4 +88,31 @@ describe Cms::Page do
       it { is_expected.to eq item.name }
     end
   end
+
+  describe "#redirect_link" do
+    subject { create(:cms_page, route: "article/page") }
+
+    context "when relative path is given" do
+      it do
+        subject.redirect_link = "a/b/c"
+        expect(subject).to be_invalid
+        expect(subject.errors[:redirect_link].length).to eq 1
+        expect(subject.errors[:redirect_link]).to include(I18n.t("errors.messages.trusted_url"))
+      end
+
+      context "when absolute path is given" do
+        it do
+          subject.redirect_link = "/a/b/c"
+          expect(subject).to be_valid
+        end
+      end
+
+      context "when site url is given" do
+        it do
+          subject.redirect_link = "#{subject.site.full_url}/a/b/c"
+          expect(subject).to be_valid
+        end
+      end
+    end
+  end
 end
