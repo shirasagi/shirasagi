8b7b1b3285fb72a6be0907cbe13a11c9c5671521
NAKANO Hideo <nakano@web-tips.co.jp>
2021-01-07 16:05:27 +0900

    [add] sort descriptions and remove them from tooltips

--- app/models/concerns/cms/addon/related_page.rb
+++ app/models/concerns/cms/addon/related_page.rb
@@ -17,25 +17,22 @@ module Cms::Addon
     end
 
     def related_page_sort_options
-      [
-        [I18n.t('event.options.sort.name'), 'name'],
-        [I18n.t('event.options.sort.filename'), 'filename'],
-        [I18n.t('event.options.sort.created'), 'created'],
-        [I18n.t('event.options.sort.updated_1'), 'updated -1'],
-        [I18n.t('event.options.sort.released_1'), 'released -1'],
-        [I18n.t('event.options.sort.order'), 'order'],
-        [I18n.t('event.options.sort.event_dates'), 'event_dates'],
-        [I18n.t('event.options.sort.unfinished_event_dates'), 'unfinished_event_dates'],
-      ]
+      %w(name filename created updated_desc released_desc order event_dates unfinished_event_dates).map do |k|
+        [
+          I18n.t("event.sort_options.#{k}.title"),
+          k.sub("_desc", " -1"),
+          "data-description" => I18n.t("event.sort_options.#{k}.description", default: nil)
+        ]
+      end
     end
 
     def related_page_sort_hash
       return { released: -1 } if related_page_sort.blank?
 
-      if related_page_sort =~ /event_dates/
+      if related_page_sort.include?("event_dates")
         { "event_dates.0" => 1 }
       else
-        { related_page_sort.sub(/ .*/, "") => (related_page_sort =~ /-1$/ ? -1 : 1) }
+        { related_page_sort.sub(/ .*/, "") => (/-1$/.match?(related_page_sort) ? -1 : 1) }
       end
     end
   end
--- app/models/concerns/event/addon/page_list.rb
+++ app/models/concerns/event/addon/page_list.rb
@@ -5,21 +5,17 @@ module Event::Addon
     include Cms::Addon::List::Model
 
     def sort_options
-      [
-        [I18n.t('event.options.sort.name'), 'name'],
-        [I18n.t('event.options.sort.filename'), 'filename'],
-        [I18n.t('event.options.sort.created'), 'created'],
-        [I18n.t('event.options.sort.updated_1'), 'updated -1'],
-        [I18n.t('event.options.sort.released_1'), 'released -1'],
-        [I18n.t('event.options.sort.order'), 'order'],
-        [I18n.t('event.options.sort.event_dates'), 'event_dates'],
-        [I18n.t('event.options.sort.unfinished_event_dates'), 'unfinished_event_dates'],
-        [I18n.t('event.options.sort.finished_event_dates'), 'finished_event_dates'],
-        [I18n.t('event.options.sort.event_dates_today'), 'event_dates_today'],
-        [I18n.t('event.options.sort.event_dates_tomorrow'), 'event_dates_tomorrow'],
-        [I18n.t('event.options.sort.event_dates_week'), 'event_dates_week'],
-        [I18n.t('event.options.sort.event_deadline'), 'event_deadline']
-      ]
+      %w(
+        name filename created updated_desc released_desc order
+        event_dates unfinished_event_dates finished_event_dates event_dates_today event_dates_tomorrow event_dates_week
+        event_deadline
+      ).map do |k|
+        [
+          I18n.t("event.sort_options.#{k}.title"),
+          k.sub("_desc", " -1"),
+          "data-description" => I18n.t("event.sort_options.#{k}.description", default: nil)
+        ]
+      end
     end
 
     def condition_hash(options = {})
