8d54288ab9125320e8bd4d3fa8daf6ff04f65844
NAKANO Hideo <nakano@web-tips.co.jp>
2021-01-08 12:00:09 +0900

    v1.13.4

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module SS
-  mattr_reader(:version) { "1.13.3" }
+  mattr_reader(:version) { "1.13.4" }
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
