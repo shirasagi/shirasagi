e4d5d3948b26102eb3e2f2ffd6d1699198675b99
NAKANO Hideo <nakano@web-tips.co.jp>
2021-02-02 14:50:00 +0900

    [fix][sys] unable to search sites (#3836)
    * [modify] refactor spec to validate models and UX
    
    * [add] reproducible spec
    
    * [fix] unable search sites with name, host and domains
    
    * [add] spec about destroy all

--- spec/features/sys/sites_spec.rb
+++ spec/features/sys/sites_spec.rb
@@ -1,63 +1,150 @@
 require 'spec_helper'
 
-describe "sys_users", type: :feature, dbscope: :example do
-  let(:item) { create(:sys_site) }
-  let(:index_path) { sys_sites_path }
-  let(:new_path) { new_sys_site_path }
-  let(:show_path) { sys_site_path item }
-  let(:edit_path) { edit_sys_site_path item }
-  let(:delete_path) { delete_sys_site_path item }
-
-  it "without auth" do
-    login_ss_user
-    visit index_path
-    expect(status_code).to eq 403
+describe "sys_sites", type: :feature, dbscope: :example do
+  describe "without auth" do
+    it do
+      login_ss_user
+      visit sys_sites_path
+      expect(status_code).to eq 403
+    end
   end
 
-  context "with auth" do
+  describe "basic crud" do
+    let(:name) { "name-#{unique_id}" }
+    let(:name2) { "modify-#{unique_id}" }
+    let(:host) { unique_id }
+    let(:domain) { unique_domain }
+
     before { login_sys_user }
 
-    it "#index" do
-      visit index_path
+    it do
+      visit sys_sites_path
       expect(status_code).to eq 200
-      expect(current_path).not_to eq sns_login_path
-    end
 
-    it "#new" do
-      visit new_path
+      click_on I18n.t("ss.links.new")
       within "form#item-form" do
-        fill_in "item[name]", with: "sample"
-        fill_in "item[host]", with: "sample"
-        fill_in "item[domains]", with: "example.jp"
-        click_button I18n.t('ss.buttons.save')
+        fill_in "item[name]", with: name
+        fill_in "item[host]", with: host
+        fill_in "item[domains]", with: domain
+        click_on I18n.t('ss.buttons.save')
       end
-      expect(status_code).to eq 200
-      expect(current_path).not_to eq new_path
-      expect(page).to have_no_css("form#item-form")
-    end
+      expect(page).to have_css('#notice', text: I18n.t("ss.notice.saved"))
 
-    it "#show" do
-      visit show_path
-      expect(status_code).to eq 200
-      expect(current_path).not_to eq sns_login_path
-    end
+      expect(SS::Site.all.count).to eq 1
+      SS::Site.all.first.tap do |site|
+        expect(site.name).to eq name
+        expect(site.host).to eq host
+        expect(site.domains).to eq [ domain ]
+      end
 
-    it "#edit" do
-      visit edit_path
+      visit sys_sites_path
+      click_on name
+      expect(status_code).to eq 200
+
+      click_on I18n.t("ss.links.edit")
       within "form#item-form" do
-        fill_in "item[name]", with: "modify"
-        click_button I18n.t('ss.buttons.save')
+        fill_in "item[name]", with: name2
+        click_on I18n.t('ss.buttons.save')
       end
-      expect(current_path).not_to eq sns_login_path
-      expect(page).to have_no_css("form#item-form")
-    end
+      expect(page).to have_css('#notice', text: I18n.t("ss.notice.saved"))
 
-    it "#delete" do
-      visit delete_path
+      SS::Site.all.first.tap do |site|
+        expect(site.name).to eq name2
+      end
+
+      visit sys_sites_path
+      click_on name2
+      click_on I18n.t("ss.links.delete")
       within "form" do
         click_button I18n.t('ss.buttons.delete')
       end
-      expect(current_path).to eq index_path
+      expect(page).to have_css('#notice', text: I18n.t("ss.notice.deleted"))
+
+      expect(SS::Site.all.count).to eq 0
+    end
+  end
+
+  describe "search" do
+    let!(:site1) { create(:sys_site, name: unique_id, host: unique_id, domains: unique_domain) }
+    let!(:site2) { create(:sys_site, name: unique_id, host: unique_id, domains: unique_domain) }
+
+    before { login_sys_user }
+
+    context "by name" do
+      it do
+        visit sys_sites_path
+        expect(page).to have_css('.list-item', count: 2)
+        expect(page).to have_css('.list-item', text: site1.name)
+        expect(page).to have_css('.list-item', text: site2.name)
+
+        within "form.index-search" do
+          fill_in "s[keyword]", with: site1.name
+          click_on I18n.t("ss.buttons.search")
+        end
+
+        expect(page).to have_css('.list-item', count: 1)
+        expect(page).to have_css('.list-item', text: site1.name)
+        expect(page).to have_no_css('.list-item', text: site2.name)
+      end
+    end
+
+    context "by host" do
+      it do
+        visit sys_sites_path
+        expect(page).to have_css('.list-item', count: 2)
+        expect(page).to have_css('.list-item', text: site1.name)
+        expect(page).to have_css('.list-item', text: site2.name)
+
+        within "form.index-search" do
+          fill_in "s[keyword]", with: site2.host
+          click_on I18n.t("ss.buttons.search")
+        end
+
+        expect(page).to have_css('.list-item', count: 1)
+        expect(page).to have_no_css('.list-item', text: site1.name)
+        expect(page).to have_css('.list-item', text: site2.name)
+      end
+    end
+
+    context "by domains" do
+      it do
+        visit sys_sites_path
+        expect(page).to have_css('.list-item', count: 2)
+        expect(page).to have_css('.list-item', text: site1.name)
+        expect(page).to have_css('.list-item', text: site2.name)
+
+        within "form.index-search" do
+          fill_in "s[keyword]", with: site1.domains.first
+          click_on I18n.t("ss.buttons.search")
+        end
+
+        expect(page).to have_css('.list-item', count: 1)
+        expect(page).to have_css('.list-item', text: site1.name)
+        expect(page).to have_no_css('.list-item', text: site2.name)
+      end
+    end
+  end
+
+  describe "destroy all", js: true do
+    let!(:site1) { create(:sys_site, name: unique_id, host: unique_id, domains: unique_domain) }
+    let!(:site2) { create(:sys_site, name: unique_id, host: unique_id, domains: unique_domain) }
+
+    before { login_sys_user }
+
+    it do
+      expect(SS::Site.all.count).to eq 2
+
+      visit sys_sites_path
+
+      within ".list-head" do
+        first("[type='checkbox']").click
+        page.accept_confirm do
+          click_on I18n.t("ss.links.delete")
+        end
+      end
+      expect(page).to have_css('#notice', text: I18n.t("ss.notice.deleted"))
+
+      expect(SS::Site.all.count).to eq 0
     end
   end
 end
