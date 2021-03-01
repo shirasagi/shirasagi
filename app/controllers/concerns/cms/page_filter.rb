6f3467e75d4593b15375827c915660ad45da6efa
NAKANO Hideo <nakano@web-tips.co.jp>
2021-03-01 11:07:30 +0900

    [fixup] error on new action after changed module

--- app/controllers/concerns/cms/page_filter.rb
+++ app/controllers/concerns/cms/page_filter.rb
@@ -26,7 +26,7 @@ module Cms::PageFilter
       params[:layout_id] = layout_id if layout_id.present?
 
       if n.respond_to?(:st_forms) && n.st_form_ids.include?(n.st_form_default_id)
-        if @model.fields.key?(:form_id)
+        if @model.fields.key?("form_id")
           default_form = n.st_form_default
           if default_form.present? && default_form.allowed?(:read, @cur_user, site: @cur_site)
             params[:form_id] = default_form.id
