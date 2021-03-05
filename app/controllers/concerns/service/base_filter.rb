fbda380179e2c381333b2d7bd7dd3db4cde6e762
NAKANO Hideo <nakano@web-tips.co.jp>
2021-03-05 17:17:15 +0900

    [modify] remove '#jquery_migrate_mute' method

--- app/controllers/concerns/service/base_filter.rb
+++ app/controllers/concerns/service/base_filter.rb
@@ -41,18 +41,6 @@ module Service::BaseFilter
     @javascripts << path unless @javascripts.include?(path)
   end
 
-  def jquery_migrate_mute
-    return unless Rails.env.production?
-
-    view_context.javascript_tag do
-      scripts = []
-      scripts << "if ( typeof jQuery.migrateMute === \"undefined\" ) {"
-      scripts << "  jQuery.migrateMute = true;"
-      scripts << "}"
-      scripts.join("\n").html_safe
-    end
-  end
-
   private
 
   def set_model
