ae31b2f02413f85987de22b6ad8934dd5db8373d
Uetani <satoshi.0411@icloud.com>
2021-04-09 16:40:42 +0900

    [add] display captcha.erb

--- app/helpers/ss/captcha_helper.rb
+++ app/helpers/ss/captcha_helper.rb
@@ -1,5 +1,17 @@
 module SS::CaptchaHelper
-  def show_captcha
-    render "captcha/captcha.erb"
+  def show_captcha(f)
+    render(partial: 'captcha/captcha.erb', locals: {f: f})
+  end
+
+  def captcha_options(option, f)
+    if option == "image"
+      @option = "image"
+    elsif option == "field"
+      @option = "field"
+    elsif option == "label"
+      @option = "label"
+    end
+
+    render(partial: 'captcha/option.erb', locals: {f: f})
   end
 end
