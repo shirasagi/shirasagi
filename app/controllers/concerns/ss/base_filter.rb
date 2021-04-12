48ed1c3be5694c4819631186804908625a629049
itowtips <ito@web-tips.co.jp>
2021-04-12 16:34:07 +0900

    [modify] logging in render_exception! (#3881)
    * [modify] logging in render_exception!
    
    * [fixup] logging in render_exception! (#3881)
    
    * [fix] tmpfile is not working
    
    * [modify] always use Rails.logger for test
    
    * [add] specs
    
    Co-authored-by: NAKANO Hideo <nakano@web-tips.co.jp>

--- app/controllers/concerns/ss/base_filter.rb
+++ app/controllers/concerns/ss/base_filter.rb
@@ -2,6 +2,7 @@ module SS::BaseFilter
   extend ActiveSupport::Concern
   include SS::AuthFilter
   include SS::LayoutFilter
+  include SS::ExceptionFilter
   include History::LogFilter
 
   included do
@@ -146,52 +147,5 @@ module SS::BaseFilter
 
   def rescue_action(exception)
     render_exception!(exception)
-  end
-
-  def render_exception!(exception)
-    if exception.is_a?(Job::SizeLimitPerUserExceededError)
-      render_job_size_limit(exception)
-      return
-    end
-
-    backtrace_cleaner = request.get_header("action_dispatch.backtrace_cleaner")
-    wrapper = ::ActionDispatch::ExceptionWrapper.new(backtrace_cleaner, exception)
-    if exception.is_a?(RuntimeError) && exception.message.numeric?
-      status_code = Integer(exception.message)
-    else
-      status_code = wrapper.status_code
-    end
-
-    @ss_rescue = { status: status_code }
-    @wrapper = wrapper if Rails.env.development?
-
-    if @ss_mode == :cms && !@cur_site
-      @ss_mode = nil
-    elsif @ss_mode == :gws && !@cur_site
-      @ss_mode = nil
-    end
-
-    render(
-      file: "ss/rescues/index", layout: @cur_user ? "ss/base" : "ss/login", status: status_code,
-      type: request.xhr? ? "text/plain" : "text/html", formats: request.xhr? ? :text : :html
-    )
-  rescue => e
-    Rails.logger.info("#{e.class} (#{e.message}):\n  #{e.backtrace.join("\n  ")}")
-    raise exception
-  end
-
-  def render_job_size_limit(error)
-    referer_uri = URI.parse(request.referer)
-    begin
-      if @item.present?
-        @item.errors.add(:base, error.to_s)
-        flash[:notice] = error.to_s
-        render(Rails.application.routes.recognize_path(referer_uri.path))
-      else
-        redirect_to(referer_uri.path, notice: error.to_s)
-      end
-    rescue ActionView::MissingTemplate
-      redirect_to(referer_uri.path, notice: error.to_s)
-    end
   end
 end
---
+++ app/controllers/concerns/ss/exception_filter.rb
@@ -0,0 +1,74 @@
+module SS::ExceptionFilter
+  extend ActiveSupport::Concern
+
+  def render_exception!(exception)
+    backtrace_cleaner = request.get_header("action_dispatch.backtrace_cleaner")
+    wrapper = ::ActionDispatch::ExceptionWrapper.new(backtrace_cleaner, exception)
+    log_error(request, wrapper)
+
+    if exception.is_a?(Job::SizeLimitPerUserExceededError)
+      render_job_size_limit(exception)
+      return
+    end
+
+    if exception.is_a?(RuntimeError) && exception.message.numeric?
+      status_code = Integer(exception.message)
+    else
+      status_code = wrapper.status_code
+    end
+
+    @ss_rescue = { status: status_code }
+    @wrapper = wrapper if Rails.env.development?
+
+    if @ss_mode == :cms && !@cur_site
+      @ss_mode = nil
+    elsif @ss_mode == :gws && !@cur_site
+      @ss_mode = nil
+    end
+
+    render(
+      file: "ss/rescues/index", layout: @cur_user ? "ss/base" : "ss/login", status: status_code,
+      type: request.xhr? ? "text/plain" : "text/html", formats: request.xhr? ? :text : :html
+    )
+  rescue => e
+    Rails.logger.info("#{e.class} (#{e.message}):\n  #{e.backtrace.join("\n  ")}")
+    raise exception
+  end
+
+  def log_error(request, wrapper)
+    # logger = request.logger || Rails.logger
+    logger = Rails.logger
+    exception = wrapper.exception
+
+    trace = wrapper.application_trace
+    trace = wrapper.framework_trace if trace.empty?
+
+    separator = "\n"
+    if logger.formatter && logger.formatter.respond_to?(:tags_text) && logger.formatter.tags_text.present?
+      separator << logger.formatter.tags_text
+    end
+
+    ActiveSupport::Deprecation.silence do
+      logger.fatal "  "
+      logger.fatal "#{exception.class} (#{exception.message}):"
+      logger.fatal exception.annoted_source_code.join(separator) if exception.respond_to?(:annoted_source_code)
+      logger.fatal "  "
+      logger.fatal trace.join(separator)
+    end
+  end
+
+  def render_job_size_limit(error)
+    referer_uri = URI.parse(request.referer)
+    begin
+      if @item.present?
+        @item.errors.add(:base, error.to_s)
+        flash[:notice] = error.to_s
+        render(Rails.application.routes.recognize_path(referer_uri.path))
+      else
+        redirect_to(referer_uri.path, notice: error.to_s)
+      end
+    rescue ActionView::MissingTemplate
+      redirect_to(referer_uri.path, notice: error.to_s)
+    end
+  end
+end
