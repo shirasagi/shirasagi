2ca578cda8871954ba9fd2b48ad4e7772ba551a0
itowtips <ito@web-tips.co.jp>
2021-04-13 15:02:15 +0900

    [add] time_card spec

---
+++ spec/features/gws/attendance/time_card/presence_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "gws_attendance_time_card", type: :feature, dbscope: :example, js: true do
+  let(:site) { gws_site }
+  let(:user) { gws_user }
+  let!(:user_presence) { create :gws_user_presence, user: user, site: site, state: "" }
+
+  before { login_gws_user }
+
+  context 'change presence' do
+    before do
+      @save_config = SS.config.gws.dig("presence")
+
+      conf = @save_config.dup
+      conf["sync_timecard"]["disable"] = false
+      SS.config.replace_value_at(:gws, :presence, conf)
+    end
+
+    after do
+      SS.config.replace_value_at(:env, :deley_download, @save_config)
+    end
+
+    it do
+      visit gws_attendance_main_path(site)
+      within '.today .action .enter' do
+        page.accept_confirm do
+          click_on I18n.t('gws/attendance.buttons.punch')
+        end
+      end
+      expect(page).to have_css('#notice', text: I18n.t('gws/attendance.notice.punched'))
+      user_presence.reload
+      expect(user_presence.state).to eq "available"
+
+      visit gws_attendance_main_path(site)
+      within '.today .action .leave' do
+        page.accept_confirm do
+          click_on I18n.t('gws/attendance.buttons.punch')
+        end
+      end
+      expect(page).to have_css('#notice', text: I18n.t('gws/attendance.notice.punched'))
+      user_presence.reload
+      expect(user_presence.state).to eq "leave"
+    end
+  end
+end
