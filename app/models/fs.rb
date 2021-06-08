008ffdc9bbbbd5f41a3b43d41146058e0b53c233
NAKANO Hideo <nakano@web-tips.co.jp>
2021-06-08 12:00:35 +0900

    [modify] refactor "#write_file"

--- app/models/fs.rb
+++ app/models/fs.rb
@@ -4,4 +4,23 @@ module Fs
   else
     include ::Fs::File
   end
+
+  module_function
+
+  def same_data?(path, data)
+    return false unless Fs.exists?(path)
+    return false if Fs.size(path) != data.length
+
+    begin
+      io = Fs.to_io(path)
+      ::FileUtils.compare_stream(io, StringIO.new(data))
+    ensure
+      io.close rescue nil
+    end
+  end
+
+  def write_data_if_modified(path, data)
+    return if Fs.same_data?(path, data)
+    Fs.binwrite(path, data)
+  end
 end
