75187cf968c2c0c900d94a60c9e33739ca35c9f1
NAKANO Hideo <nakano@web-tips.co.jp>
2021-06-09 13:09:43 +0900

    [modify] refactor w/ Rubocop

--- spec/lib/browser_spec.rb
+++ spec/lib/browser_spec.rb
@@ -5,7 +5,9 @@ describe Browser do
   subject { Browser.new(user_agent, accept_language: accept_language) }
 
   context "when Safari's user agent is given" do
-    let(:user_agent) { "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36" }
+    let(:user_agent) do
+      "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36"
+    end
 
     it do
       expect(subject.bot?).to be_falsey
