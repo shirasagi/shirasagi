2d222da9f77c0902e1b0e9e8e989e98b7e1411b0
NAKANO Hideo <nakano@web-tips.co.jp>
2021-06-09 12:01:37 +0900

    [add] bot judgement

---
+++ spec/lib/browser_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Browser do
+  let(:accept_language) { "ja,en-US;q=0.9,en;q=0.8" }
+  subject { Browser.new(user_agent, accept_language: accept_language) }
+
+  context "when Safari's user agent is given" do
+    let(:user_agent) { "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36" }
+
+    it do
+      expect(subject.bot?).to be_falsey
+    end
+  end
+
+  context "when Line Spider's user agent is given" do
+    let(:user_agent) { "Mozilla/5.0 (compatible; Linespider/1.1; +https://lin.ee/4dwXkTH)" }
+
+    it do
+      expect(subject.bot?).to be_truthy
+    end
+  end
+end
