2bb44d3a9588e7a7e98e2ad818b914ae48c98088
itowtips <ito@web-tips.co.jp>
2021-06-22 20:31:46 +0900

    [modify] gws schedule color select form (#3948)
    * スケジュールの表示色について (#1)
    
    * [modify] schedule color select form
    
    * [modify] move color_codes.yml to config/defaults
    
    * [modify] renamed color_codes.yml to minicolors_swatches
    
    * [modify] SS_Color options
    
    Co-authored-by: ko-furuoka <52389720+ko-furuoka@users.noreply.github.com>

---
+++ spec/features/gws/schedule/plans/select_color_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe 'gws_schedule_plans_form', type: :feature, dbscope: :example do
+  let(:site) { gws_site }
+
+  before { login_gws_user }
+
+  describe 'select color', js: true do
+    context 'new' do
+      let(:new_path) { new_gws_schedule_plan_path site }
+
+      before do
+        visit new_path
+      end
+
+      it do
+        find('input[name="item[color]"]').click
+        first('ul.minicolors-swatches li.minicolors-swatch').click
+        expect(find('input[name="item[color]"]').value).to eq '#dc143c'
+
+        within 'form#item-form' do
+          fill_in 'item[name]', with: 'name'
+          click_button I18n.t('ss.buttons.save')
+        end
+        wait_for_ajax
+        expect(Gws::Schedule::Plan.count).to eq 1
+        expect(Gws::Schedule::Plan.first.color).to eq '#dc143c'
+      end
+    end
+
+    context 'edit' do
+      let(:item) { create(:gws_schedule_plan, color: '#dc143c') }
+      let(:edit_path) { edit_gws_schedule_plan_path site, item }
+
+      before do
+        visit edit_path
+      end
+
+      it do
+        find('input[name="item[color]"]').click
+        find('ul.minicolors-swatches li.minicolors-swatch:nth-child(2)').click
+        expect(find('input[name="item[color]"]').value).to eq '#ff0000'
+
+        within 'form#item-form' do
+          click_button I18n.t('ss.buttons.save')
+        end
+        wait_for_ajax
+        expect(item.reload.color).to eq '#ff0000'
+      end
+    end
+
+    context 'copy' do
+      let(:item) { create(:gws_schedule_plan, color: '#dc143c') }
+      let(:copy_path) { copy_gws_schedule_plan_path site, item }
+
+      before do
+        visit copy_path
+      end
+
+      it do
+        find('input[name="item[color]"]').click
+        find('ul.minicolors-swatches li.minicolors-swatch:nth-child(2)').click
+        expect(find('input[name="item[color]"]').value).to eq '#ff0000'
+
+        within 'form#item-form' do
+          fill_in 'item[name]', with: 'copied'
+          click_button I18n.t('ss.buttons.save')
+        end
+        wait_for_ajax
+        expect(Gws::Schedule::Plan.find_by(name: 'copied').color).to eq '#ff0000'
+      end
+    end
+  end
+end
