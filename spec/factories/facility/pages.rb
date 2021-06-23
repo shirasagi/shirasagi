be07acc7a54865dd2b0fa51b3fb968bb186ce96f
itowtips <ito@web-tips.co.jp>
2021-06-23 16:17:11 +0900

    [modify] spec

--- spec/factories/facility/pages.rb
+++ spec/factories/facility/pages.rb
@@ -8,4 +8,8 @@ FactoryBot.define do
     filename { "dir/#{unique_id}" }
     route "facility/map"
   end
+
+  factory :facility_notice, class: Facility::Notice, traits: [:cms_page] do
+    route "facility/notice"
+  end
 end
