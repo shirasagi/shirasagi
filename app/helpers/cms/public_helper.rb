4905d773d7c828692e8d146c27606cbe5d463de9
NAKANO Hideo <nakano@web-tips.co.jp>
2021-06-23 09:10:23 +0900

    [modify][cms] pagination on public (#3936)
    * [modify] directly generate pagination via @cur_path
    
    * [add] specs
    
    * this is empty commit to run travis

--- app/helpers/cms/public_helper.rb
+++ app/helpers/cms/public_helper.rb
@@ -1,10 +1,11 @@
 module Cms::PublicHelper
-  def paginate(*args)
-    super.gsub(/href=".*?"/) do |m|
-      url  = convert_static_url m.sub(/href="(.*?)"/, '\\1')
-      href = %(href="#{url}")
-      m.sub(/href=".*?"/, href)
-    end.html_safe
+  def paginate(*args, **options)
+    if @cur_path.present?
+      options[:paginator_class] = Cms::PublicHelper::Paginator
+      super(*args, **options)
+    else
+      super
+    end
   end
 
   def body_id(path)
@@ -16,29 +17,5 @@ module Cms::PublicHelper
     nodes  = path.to_s.sub(/\/[^\/]+\.html$/, "").sub(/^\//, "").split("/")
     nodes  = nodes.map { |node| prefix = "#{prefix}-" + node.gsub(/[^\w-]+/, "-") }
     nodes.join(" ")
-  end
-
-  private
-
-  def convert_static_url(url)
-    path, query = url.split("?")
-
-    params = Rack::Utils.parse_nested_query(query)
-    params.delete("amp")
-    params.delete("public_path")
-    page = params.delete("page")
-
-    path = @cur_path
-
-    if page
-      path = path.sub(/\/$/, "/index.html").sub(".html", ".p#{page}.html")
-      params[:page] = page if path !~ /\.html/
-    end
-
-    if params.present?
-      path = "#{path}?" + params.to_query
-    end
-
-    path
   end
 end
