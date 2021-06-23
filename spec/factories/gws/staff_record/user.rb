96210ebaa56b18f7e9577f78121dc7ac1becd219
NAKANO Hideo <nakano@web-tips.co.jp>
2021-06-23 09:10:41 +0900

    [fix][gws] gws/staff_record/user export and/or import issue (#3939)
    * [fix] unable to set user titles on importing users
    
    * [modify] remove "permission_level" from export
    
    * [add] gws/staff_record/user export and import specs

--- spec/factories/gws/staff_record/user.rb
+++ spec/factories/gws/staff_record/user.rb
@@ -8,12 +8,15 @@ FactoryBot.define do
 
     name { "name-#{unique_id}" }
     code { "code-#{unique_id}" }
+    order { rand(101..200) }
     kana { "kana-#{unique_id}" }
+    section_name { "section_name-#{unique_id}" }
+    section_order { rand(201..300) }
     tel_ext { "tel_ext-#{unique_id}" }
     charge_name { "charge_name-#{unique_id}" }
     charge_address { "charge_address-#{unique_id}" }
     charge_tel { "charge_tel-#{unique_id}" }
-    divide_duties { "divide_duties-#{unique_id}" }
-    remark { "remark-#{unique_id}" }
+    divide_duties { Array.new(2) { "divide_duties-#{unique_id}" }.join("\n") }
+    remark { Array.new(2) { "remark-#{unique_id}" }.join("\n") }
   end
 end
