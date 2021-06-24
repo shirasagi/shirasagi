14790668a17bde8c1d3e043c2d78aa940ec83ac8
NAKANO Hideo <nakano@web-tips.co.jp>
2021-06-24 15:06:30 +0900

    [fix] gws/staff_record/user export and import

--- spec/models/gws/staff_record/user/export_spec.rb
+++ spec/models/gws/staff_record/user/export_spec.rb
@@ -2,9 +2,26 @@ require 'spec_helper'
 
 describe Gws::StaffRecord::User, type: :model, dbscope: :example do
   let(:site1) { create :gws_group }
+  let!(:group1) { create :gws_group, name: "#{site1.name}/#{unique_id}" }
+  let!(:group2) { create :gws_group, name: "#{site1.name}/#{unique_id}" }
+  let!(:group3) { create :gws_group, name: "#{site1.name}/#{unique_id}" }
+  let!(:group4) { create :gws_group, name: "#{site1.name}/#{unique_id}" }
+  let!(:user1) { create :gws_user, group_ids: [ site1.id ] }
+  let!(:user2) { create :gws_user, group_ids: [ site1.id ] }
+  let!(:user3) { create :gws_user, group_ids: [ site1.id ] }
+  let!(:user4) { create :gws_user, group_ids: [ site1.id ] }
   let!(:year1) { create :gws_staff_record_year, cur_site: site1 }
   let!(:title1) { create :gws_staff_record_user_title, cur_site: site1, year: year1 }
-  let!(:user1) { create :gws_staff_record_user, cur_site: site1, year: year1, title_ids: [ title1.id ] }
+  # let(:readable_setting_range) { %w(public select private).sample }
+  let(:readable_setting_range) { "select" }
+  let!(:staff_record_user1) do
+    create(
+      :gws_staff_record_user, cur_site: site1, year: year1, title_ids: [ title1.id ],
+      readable_setting_range: readable_setting_range, readable_group_ids: [ group1.id, group2.id ],
+      readable_member_ids: [ user1.id, user2.id ], group_ids: [ group3.id, group4.id ], user_ids: [ user3.id, user4.id ],
+      permission_level: rand(1..3)
+    )
+  end
 
   before do
     admin_role = create(:gws_role_admin, cur_site: site1)
@@ -18,7 +35,7 @@ describe Gws::StaffRecord::User, type: :model, dbscope: :example do
     let(:encoding) { "Shift_JIS" }
 
     it do
-      item = Gws::StaffRecord::User.new
+      item = described_class.new
       item.cur_site = site1
       item.cur_user = gws_user
       item.year = year1
@@ -29,29 +46,32 @@ describe Gws::StaffRecord::User, type: :model, dbscope: :example do
       csv = ::CSV.parse(csv, headers: true)
 
       expect(csv.length).to eq 1
-      expect(csv.headers.length).to eq 18
+      expect(csv.headers.length).to eq 21
       expect(csv.headers).to include(*%i[id name code order kana].map { |f| described_class.t(f) })
       expect(csv.headers).not_to include(*%i[section_order permission_level].map { |f| described_class.t(f) })
       csv.first.tap do |row|
-        expect(row.length).to eq 18
-        expect(row[described_class.t(:id)]).to eq user1.id.to_s
-        expect(row[described_class.t(:name)]).to eq user1.name
-        expect(row[described_class.t(:code)]).to eq user1.code
-        expect(row[described_class.t(:order)]).to eq user1.order.to_s
-        expect(row[described_class.t(:kana)]).to eq user1.kana
-        expect(row[described_class.t(:multi_section)]).to eq user1.label(:multi_section)
-        expect(row[described_class.t(:section_name)]).to eq user1.section_name
+        expect(row.length).to eq 21
+        expect(row[described_class.t(:id)]).to eq staff_record_user1.id.to_s
+        expect(row[described_class.t(:name)]).to eq staff_record_user1.name
+        expect(row[described_class.t(:code)]).to eq staff_record_user1.code
+        expect(row[described_class.t(:order)]).to eq staff_record_user1.order.to_s
+        expect(row[described_class.t(:kana)]).to eq staff_record_user1.kana
+        expect(row[described_class.t(:multi_section)]).to eq staff_record_user1.label(:multi_section)
+        expect(row[described_class.t(:section_name)]).to eq staff_record_user1.section_name
         expect(row[described_class.t(:title_ids)]).to eq title1.code
-        expect(row[described_class.t(:tel_ext)]).to eq user1.tel_ext
-        expect(row[described_class.t(:charge_name)]).to eq user1.charge_name
-        expect(row[described_class.t(:charge_address)]).to eq user1.charge_address
-        expect(row[described_class.t(:charge_tel)]).to eq user1.charge_tel
-        expect(row[described_class.t(:divide_duties)]).to eq user1.divide_duties
-        expect(row[described_class.t(:remark)]).to eq user1.remark
-        expect(row[described_class.t(:staff_records_view)]).to eq user1.label(:staff_records_view)
-        expect(row[described_class.t(:divide_duties_view)]).to eq user1.label(:divide_duties_view)
-        expect(row[described_class.t(:group_ids)]).to eq user1.groups.pluck(:name).join("\n")
-        expect(row[described_class.t(:user_ids)]).to eq user1.users.pluck(:uid).join("\n")
+        expect(row[described_class.t(:tel_ext)]).to eq staff_record_user1.tel_ext
+        expect(row[described_class.t(:charge_name)]).to eq staff_record_user1.charge_name
+        expect(row[described_class.t(:charge_address)]).to eq staff_record_user1.charge_address
+        expect(row[described_class.t(:charge_tel)]).to eq staff_record_user1.charge_tel
+        expect(row[described_class.t(:divide_duties)]).to eq staff_record_user1.divide_duties
+        expect(row[described_class.t(:remark)]).to eq staff_record_user1.remark
+        expect(row[described_class.t(:staff_records_view)]).to eq staff_record_user1.label(:staff_records_view)
+        expect(row[described_class.t(:divide_duties_view)]).to eq staff_record_user1.label(:divide_duties_view)
+        expect(row[described_class.t(:readable_setting_range)]).to eq staff_record_user1.label(:readable_setting_range)
+        expect(row[described_class.t(:readable_group_ids)]).to eq staff_record_user1.readable_groups.pluck(:name).join("\n")
+        expect(row[described_class.t(:readable_member_ids)]).to eq staff_record_user1.readable_members.pluck(:uid).join("\n")
+        expect(row[described_class.t(:group_ids)]).to eq staff_record_user1.groups.pluck(:name).join("\n")
+        expect(row[described_class.t(:user_ids)]).to eq staff_record_user1.users.pluck(:uid).join("\n")
       end
     end
   end
@@ -60,7 +80,7 @@ describe Gws::StaffRecord::User, type: :model, dbscope: :example do
     let(:encoding) { "UTF-8" }
 
     it do
-      item = Gws::StaffRecord::User.new
+      item = described_class.new
       item.cur_site = site1
       item.cur_user = gws_user
       item.year = year1
@@ -71,29 +91,32 @@ describe Gws::StaffRecord::User, type: :model, dbscope: :example do
       csv = ::CSV.parse(csv, headers: true)
 
       expect(csv.length).to eq 1
-      expect(csv.headers.length).to eq 18
+      expect(csv.headers.length).to eq 21
       expect(csv.headers).to include(*%i[id name code order kana].map { |f| described_class.t(f) })
       expect(csv.headers).not_to include(*%i[section_order permission_level].map { |f| described_class.t(f) })
       csv.first.tap do |row|
-        expect(row.length).to eq 18
-        expect(row[described_class.t(:id)]).to eq user1.id.to_s
-        expect(row[described_class.t(:name)]).to eq user1.name
-        expect(row[described_class.t(:code)]).to eq user1.code
-        expect(row[described_class.t(:order)]).to eq user1.order.to_s
-        expect(row[described_class.t(:kana)]).to eq user1.kana
-        expect(row[described_class.t(:multi_section)]).to eq user1.label(:multi_section)
-        expect(row[described_class.t(:section_name)]).to eq user1.section_name
+        expect(row.length).to eq 21
+        expect(row[described_class.t(:id)]).to eq staff_record_user1.id.to_s
+        expect(row[described_class.t(:name)]).to eq staff_record_user1.name
+        expect(row[described_class.t(:code)]).to eq staff_record_user1.code
+        expect(row[described_class.t(:order)]).to eq staff_record_user1.order.to_s
+        expect(row[described_class.t(:kana)]).to eq staff_record_user1.kana
+        expect(row[described_class.t(:multi_section)]).to eq staff_record_user1.label(:multi_section)
+        expect(row[described_class.t(:section_name)]).to eq staff_record_user1.section_name
         expect(row[described_class.t(:title_ids)]).to eq title1.code
-        expect(row[described_class.t(:tel_ext)]).to eq user1.tel_ext
-        expect(row[described_class.t(:charge_name)]).to eq user1.charge_name
-        expect(row[described_class.t(:charge_address)]).to eq user1.charge_address
-        expect(row[described_class.t(:charge_tel)]).to eq user1.charge_tel
-        expect(row[described_class.t(:divide_duties)]).to eq user1.divide_duties
-        expect(row[described_class.t(:remark)]).to eq user1.remark
-        expect(row[described_class.t(:staff_records_view)]).to eq user1.label(:staff_records_view)
-        expect(row[described_class.t(:divide_duties_view)]).to eq user1.label(:divide_duties_view)
-        expect(row[described_class.t(:group_ids)]).to eq user1.groups.pluck(:name).join("\n")
-        expect(row[described_class.t(:user_ids)]).to eq user1.users.pluck(:uid).join("\n")
+        expect(row[described_class.t(:tel_ext)]).to eq staff_record_user1.tel_ext
+        expect(row[described_class.t(:charge_name)]).to eq staff_record_user1.charge_name
+        expect(row[described_class.t(:charge_address)]).to eq staff_record_user1.charge_address
+        expect(row[described_class.t(:charge_tel)]).to eq staff_record_user1.charge_tel
+        expect(row[described_class.t(:divide_duties)]).to eq staff_record_user1.divide_duties
+        expect(row[described_class.t(:remark)]).to eq staff_record_user1.remark
+        expect(row[described_class.t(:staff_records_view)]).to eq staff_record_user1.label(:staff_records_view)
+        expect(row[described_class.t(:divide_duties_view)]).to eq staff_record_user1.label(:divide_duties_view)
+        expect(row[described_class.t(:readable_setting_range)]).to eq staff_record_user1.label(:readable_setting_range)
+        expect(row[described_class.t(:readable_group_ids)]).to eq staff_record_user1.readable_groups.pluck(:name).join("\n")
+        expect(row[described_class.t(:readable_member_ids)]).to eq staff_record_user1.readable_members.pluck(:uid).join("\n")
+        expect(row[described_class.t(:group_ids)]).to eq staff_record_user1.groups.pluck(:name).join("\n")
+        expect(row[described_class.t(:user_ids)]).to eq staff_record_user1.users.pluck(:uid).join("\n")
       end
     end
   end
--- spec/models/gws/staff_record/user/import_spec.rb
+++ spec/models/gws/staff_record/user/import_spec.rb
@@ -2,20 +2,38 @@ require 'spec_helper'
 
 describe Gws::StaffRecord::User, type: :model, dbscope: :example do
   let!(:site1) { create :gws_group }
+  let!(:group1) { create :gws_group, name: "#{site1.name}/#{unique_id}" }
+  let!(:group2) { create :gws_group, name: "#{site1.name}/#{unique_id}" }
+  let!(:group3) { create :gws_group, name: "#{site1.name}/#{unique_id}" }
+  let!(:group4) { create :gws_group, name: "#{site1.name}/#{unique_id}" }
+  let!(:user1) { create :gws_user, group_ids: [ site1.id ] }
+  let!(:user2) { create :gws_user, group_ids: [ site1.id ] }
+  let!(:user3) { create :gws_user, group_ids: [ site1.id ] }
+  let!(:user4) { create :gws_user, group_ids: [ site1.id ] }
   let!(:year1) { create :gws_staff_record_year, cur_site: site1 }
   let!(:title1) { create :gws_staff_record_user_title, cur_site: site1, year: year1 }
-  let!(:user1) { create :gws_staff_record_user, cur_site: site1, year: year1, title_ids: [ title1.id ] }
+  # let(:readable_setting_range) { %w(public select private).sample }
+  let(:readable_setting_range) { "select" }
+  let!(:staff_record_user1) do
+    create(
+      :gws_staff_record_user, cur_site: site1, year: year1, title_ids: [ title1.id ],
+      readable_setting_range: readable_setting_range, readable_group_ids: [ group1.id, group2.id ],
+      readable_member_ids: [ user1.id, user2.id ], group_ids: [ group3.id, group4.id ], user_ids: [ user3.id, user4.id ],
+      permission_level: rand(1..3)
+    )
+  end
 
   let(:encoding) { %w(Shift_JIS UTF-8).sample }
   let(:csv) do
-    item = Gws::StaffRecord::User.new
+    item = described_class.new
     item.cur_site = site1
     item.year = year1
     item.in_csv_encoding = encoding
 
     year1.reload
     csv = item.export_csv(year1.yearly_users.site(site1))
-    csv = csv.sub("\n#{user1.id},", "\n,")
+    # clear id to import as new item
+    csv = csv.sub("\n#{staff_record_user1.id},", "\n,")
     csv
   end
   let(:csv_as_file) do
@@ -31,6 +49,11 @@ describe Gws::StaffRecord::User, type: :model, dbscope: :example do
     admin_role = create(:gws_role_admin, cur_site: site2)
     gws_user.add_to_set(group_ids: site2.id)
     gws_user.add_to_set(gws_role_ids: admin_role.id)
+
+    user1.add_to_set(group_ids: site2.id)
+    user2.add_to_set(group_ids: site2.id)
+    user3.add_to_set(group_ids: site2.id)
+    user4.add_to_set(group_ids: site2.id)
   end
 
   context "newly import" do
@@ -45,24 +68,28 @@ describe Gws::StaffRecord::User, type: :model, dbscope: :example do
       expect(item.errors).to be_blank
       expect(Gws::StaffRecord::User.all.site(site2).count).to eq 1
       Gws::StaffRecord::User.all.site(site2).first.tap do |imported_user|
-        expect(imported_user.name).to eq user1.name
-        expect(imported_user.code).to eq user1.code
-        expect(imported_user.order).to eq user1.order
-        expect(imported_user.kana).to eq user1.kana
-        expect(imported_user.multi_section).to eq user1.multi_section
-        expect(imported_user.section_name).to eq user1.section_name
+        expect(imported_user.name).to eq staff_record_user1.name
+        expect(imported_user.code).to eq staff_record_user1.code
+        expect(imported_user.order).to eq staff_record_user1.order
+        expect(imported_user.kana).to eq staff_record_user1.kana
+        expect(imported_user.multi_section).to eq staff_record_user1.multi_section
+        expect(imported_user.section_name).to eq staff_record_user1.section_name
         expect(imported_user.title_ids).to have(1).items
         expect(imported_user.titles.first).to eq title2
-        expect(imported_user.tel_ext).to eq user1.tel_ext
-        expect(imported_user.charge_name).to eq user1.charge_name
-        expect(imported_user.charge_address).to eq user1.charge_address
-        expect(imported_user.charge_tel).to eq user1.charge_tel
-        expect(imported_user.divide_duties).to eq user1.divide_duties
-        expect(imported_user.remark).to eq user1.remark
-        expect(imported_user.staff_records_view).to eq user1.staff_records_view
-        expect(imported_user.divide_duties_view).to eq user1.divide_duties_view
+        expect(imported_user.tel_ext).to eq staff_record_user1.tel_ext
+        expect(imported_user.charge_name).to eq staff_record_user1.charge_name
+        expect(imported_user.charge_address).to eq staff_record_user1.charge_address
+        expect(imported_user.charge_tel).to eq staff_record_user1.charge_tel
+        expect(imported_user.divide_duties).to eq staff_record_user1.divide_duties
+        expect(imported_user.remark).to eq staff_record_user1.remark
+        expect(imported_user.staff_records_view).to eq staff_record_user1.staff_records_view
+        expect(imported_user.divide_duties_view).to eq staff_record_user1.divide_duties_view
+        expect(imported_user.readable_setting_range).to eq staff_record_user1.readable_setting_range
+        expect(imported_user.readable_group_ids).to be_blank
+        expect(imported_user.readable_member_ids).to eq staff_record_user1.readable_member_ids
         expect(imported_user.group_ids).to be_blank
-        expect(imported_user.user_ids).to eq [ gws_user.id ]
+        expect(imported_user.user_ids).to have(3).items
+        expect(imported_user.user_ids).to include(gws_user.id, user3.id, user4.id)
       end
     end
   end
