d3d53b734d1d7c19bd41f460b6dfaae1a5470c07
NAKANO Hideo <nakano@web-tips.co.jp>
2021-06-30 17:35:13 +0900

    [modify] stabilize specs up

--- spec/features/gws/staff_record/public_duties_spec.rb
+++ spec/features/gws/staff_record/public_duties_spec.rb
@@ -4,7 +4,12 @@ describe "gws_staff_record_public_duties", type: :feature, dbscope: :example do
   let(:site) { gws_site }
   let(:year) { create :gws_staff_record_year }
   let(:section) { create :gws_staff_record_group, year_id: year.id }
-  let!(:item) { create :gws_staff_record_user, year_id: year.id, section_name: section.name }
+  let!(:item) do
+    create(
+      :gws_staff_record_user, year_id: year.id, section_name: section.name,
+      staff_records_view: "show", divide_duties_view: "show"
+    )
+  end
   let(:index_path) { gws_staff_record_public_duties_path(site) }
 
   context "with auth", js: true do
