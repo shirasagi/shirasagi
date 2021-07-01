37c9426127ef25f67f05749f762f57056e4e4cea
NAKANO Hideo <nakano@web-tips.co.jp>
2021-07-01 15:05:59 +0900

    [add] tag "<s>" to sanitized_allowed_tags

--- config/initializers/sanitize.rb
+++ config/initializers/sanitize.rb
@@ -1,4 +1,4 @@
 # Sanitize
-ActionView::Base.sanitized_allowed_tags.merge %w(font strike u table thead tbody tr th td)
+ActionView::Base.sanitized_allowed_tags.merge %w(font strike s u table thead tbody tr th td)
 ActionView::Base.sanitized_allowed_attributes.merge %w(style border color face size align valign
                                                        cellspacing cellpadding colspan rowspan)
