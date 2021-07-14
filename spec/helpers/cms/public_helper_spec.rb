6899e82829001049b56292d1d2bc46c5f83cc5d3
NAKANO Hideo <nakano@web-tips.co.jp>
2021-07-14 10:45:14 +0900

    [add] reproducible spec

--- spec/helpers/cms/public_helper_spec.rb
+++ spec/helpers/cms/public_helper_spec.rb
@@ -29,31 +29,50 @@ describe Cms::PublicHelper, type: :helper, dbscope: :example do
     end
 
     context "when @cur_path is given" do
-      it do
+      before do
         @cur_path = "/gyosei/docs/index.html"
-        html = helper.paginate(items)
-        html = Nokogiri::HTML.fragment(html)
-        expect(html.css(".first a")[0]).to be_present
-        expect(html.css(".first a")[0].attr("href")).to eq "/gyosei/docs"
-        expect(html.css(".first a")[0].text()).to eq "«"
+      end
 
-        expect(html.css(".prev a")[0]).to be_present
-        expect(html.css(".prev a")[0].attr("href")).to eq "/gyosei/docs/index.p9.html"
-        expect(html.css(".prev a")[0].text()).to eq I18n.t("views.pagination.previous")
+      context "without query parameters" do
+        it do
+          html = helper.paginate(items)
+          html = Nokogiri::HTML.fragment(html)
+          expect(html.css(".first a")[0]).to be_present
+          expect(html.css(".first a")[0].attr("href")).to eq "/gyosei/docs"
+          expect(html.css(".first a")[0].text()).to eq "«"
 
-        expect(html.css(".page")).to have(11).items
-        expect(html.css(".page.current")[0].text()).to include "10"
+          expect(html.css(".prev a")[0]).to be_present
+          expect(html.css(".prev a")[0].attr("href")).to eq "/gyosei/docs/index.p9.html"
+          expect(html.css(".prev a")[0].text()).to eq I18n.t("views.pagination.previous")
 
-        expect(html.css(".gap")).to have(2).items
+          expect(html.css(".page")).to have(11).items
+          expect(html.css(".page.current")[0].text()).to include "10"
 
-        expect(html.css(".next a")[0]).to be_present
-        expect(html.css(".next a")[0].attr("href")).to eq "/gyosei/docs/index.p11.html"
-        expect(html.css(".next a")[0].text()).to eq I18n.t("views.pagination.next")
+          expect(html.css(".gap")).to have(2).items
 
-        expect(html.css(".last a")[0]).to be_present
-        expect(html.css(".last a")[0].attr("href")).to eq "/gyosei/docs/index.p31.html"
-        # expect(html.css(".last a")[0].text()).to eq CGI.unescapeHTML(I18n.t("views.pagination.last"))
-        expect(html.css(".last a")[0].text()).to eq "»"
+          expect(html.css(".next a")[0]).to be_present
+          expect(html.css(".next a")[0].attr("href")).to eq "/gyosei/docs/index.p11.html"
+          expect(html.css(".next a")[0].text()).to eq I18n.t("views.pagination.next")
+
+          expect(html.css(".last a")[0]).to be_present
+          expect(html.css(".last a")[0].attr("href")).to eq "/gyosei/docs/index.p31.html"
+          # expect(html.css(".last a")[0].text()).to eq CGI.unescapeHTML(I18n.t("views.pagination.last"))
+          expect(html.css(".last a")[0].text()).to eq "»"
+        end
+      end
+
+      context "with query parameters" do
+        before do
+          controller.request.query_string = { keyword: "test" }.to_query
+        end
+
+        it do
+          html = helper.paginate(items)
+          html = Nokogiri::HTML.fragment(html)
+          expect(html.css(".first a")[0]).to be_present
+          expect(html.css(".first a")[0].attr("href")).to eq "/gyosei/docs?keyword=test"
+          expect(html.css(".first a")[0].text()).to eq "«"
+        end
       end
     end
 
