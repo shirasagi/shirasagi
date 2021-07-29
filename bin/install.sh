ceaf4054fcd790688ae82fff6aa4ab5240fa92dd
ttanaka9211 <ttanaka@hatchdogs.net>
2021-07-29 18:45:15 +0900

    [modify]sks-keyservers.net deprecated

--- bin/install.sh
+++ bin/install.sh
@@ -31,8 +31,9 @@ sudo yum -y install \
 
 for i in $(seq 1 3)
 do
-  gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-  if [ $? -eq 0 ]; then
+  curl -sSL https://rvm.io/mpapis.asc | gpg --import - ; RET1=$?
+  curl -sSL https://rvm.io/pkuczynski.asc | gpg --import - ; RET2=$?
+  if [ ${RET1} -eq 0 -o ${RET2} -eq 0 ]; then
     break
   fi
   sleep 5s
