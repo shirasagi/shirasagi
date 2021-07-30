0bdfb97e4f596f999d09fba718d0503aad8fff62
itowtips <ito@web-tips.co.jp>
2021-07-30 13:33:45 +0900

    v1.14.1

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module SS
-  mattr_reader(:version) { "1.14.0" }
+  mattr_reader(:version) { "1.14.1" }
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
