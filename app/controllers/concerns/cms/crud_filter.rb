ade45280274f09f360275ddb013ded13d8876dd7
Uetani <satoshi.0411@icloud.com>
2021-08-30 19:02:59 +0900

    [fix] cms destroy_all: Prevent pages from being deleted when including links to other pages

--- app/controllers/concerns/cms/crud_filter.rb
+++ app/controllers/concerns/cms/crud_filter.rb
@@ -101,7 +101,18 @@ module Cms::CrudFilter
   end
 
   def destroy_all
-    render_destroy_all(destroy_items, location: request.path)
+    raise "400" if @selected_items.blank?
+
+    if params[:destroy_all]
+      @selected_items.destroy_all
+      render_destroy_all true
+      return
+    end
+
+    respond_to do |format|
+      format.html { render "cms/crud/destroy_all" }
+      format.json { head json: errors }
+    end
   end
 
   def disable_all
