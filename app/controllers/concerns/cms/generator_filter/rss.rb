e370f9e835a0795d9372e5f908eac8c35d24c343
NAKANO Hideo <nakano@web-tips.co.jp>
2021-08-30 19:02:09 +0900

    [modify] use ActionDispatch::Response#media_type instead of ActionDispatch::Response#content_type

--- app/controllers/concerns/cms/generator_filter/rss.rb
+++ app/controllers/concerns/cms/generator_filter/rss.rb
@@ -27,7 +27,7 @@ module Cms::GeneratorFilter::Rss
       raise e
     end
 
-    if response.content_type == "text/html" && node.layout
+    if response.media_type == "text/html" && node.layout
       html = render_to_string html: render_layout(node.layout).html_safe, layout: "cms/page"
     else
       html = response.body
--- app/controllers/concerns/cms/public_filter.rb
+++ app/controllers/concerns/cms/public_filter.rb
@@ -213,7 +213,7 @@ module Cms::PublicFilter
     if page.view_layout == "cms/redirect" && !mobile_path?
       @redirect_link = trusted_url!(page.redirect_link)
       render html: "", layout: "cms/redirect"
-    elsif response.content_type == "text/html" && page.layout
+    elsif response.media_type == "text/html" && page.layout
       render html: render_layout(page.layout).html_safe, layout: (request.xhr? ? false : "cms/page")
     else
       @_response_body = response.body
