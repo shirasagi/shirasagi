0f3eecf1035b3ec772aeb7052d1da0327058cddc
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2021-09-30 11:37:13 +0900

    [modify] cms elasticsearch indexer node_close_job rescue

--- app/jobs/cms/elasticsearch/indexer/node_close_job.rb
+++ app/jobs/cms/elasticsearch/indexer/node_close_job.rb
@@ -7,6 +7,8 @@ class Cms::Elasticsearch::Indexer::NodeCloseJob < Cms::ApplicationJob
       criteria.in(id: ids).to_a.each do |item|
         Cms::Elasticsearch::Indexer::PageReleaseJob.bind(site_id: site).
           perform_now(action: 'delete', id: item.id.to_s)
+      rescue => e
+        Rails.logger.warn("#{e.class} (#{e.message}):\n  #{e.backtrace.join("\n  ")}")
       end
     end
     Cms::PageIndexQueue.site(site).in(id: all_ids).where(action: 'release').destroy_all
