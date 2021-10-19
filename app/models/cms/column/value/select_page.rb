e27f28c82c4313bfacc0f28081f64333def56443
itowtips <ito@web-tips.co.jp>
2021-10-19 18:05:59 +0900

    [modify] article import

--- app/models/cms/column/value/select_page.rb
+++ app/models/cms/column/value/select_page.rb
@@ -16,6 +16,23 @@ class Cms::Column::Value::SelectPage < Cms::Column::Value::Base
     ApplicationController.helpers.link_to(page.name, page.url)
   end
 
+  def import_csv(values)
+    values.map do |name, value|
+      case name
+      when self.class.t(:alignment)
+        self.alignment = value.present? ? I18n.t("cms.options.alignment").invert[value] : nil
+      when self.class.t(:page_id)
+        if value.present?
+          id = value.scan(/\((\d+?)\)$/).flatten.first.to_i
+          page = Cms::Page.find(id) rescue nil
+          self.page_id = page.id if page
+        else
+          self.page = nil
+        end
+      end
+    end
+  end
+
   private
 
   def to_default_html
