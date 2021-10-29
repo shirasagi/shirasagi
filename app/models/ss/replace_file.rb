3742b85d1b5799b2f0fb13939c74a6e15a19d4cc
NAKANO Hideo <nakano@web-tips.co.jp>
2021-10-29 14:19:04 +0900

    [modify] include SS::Reference::Site

--- app/models/ss/replace_file.rb
+++ app/models/ss/replace_file.rb
@@ -1,9 +1,13 @@
 class SS::ReplaceFile
   include SS::Model::File
+  include SS::Reference::Site
   include SS::Relation::Thumb
   include SS::Relation::FileBranch
   include SS::Relation::FileHistory
   include SS::Liquidization
+
+  # for backward compatibility
+  self.site_required = false
 
   before_validation :set_source_instance
 
