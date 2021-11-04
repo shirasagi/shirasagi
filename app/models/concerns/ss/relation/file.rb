f853b5a3752cb597bb53c452da56d283e9aaa42e
NAKANO Hideo <nakano@web-tips.co.jp>
2021-11-04 17:58:48 +0900

    [modify] reduce duplicated code of ss/relation/file

--- app/models/concerns/ss/relation/file.rb
+++ app/models/concerns/ss/relation/file.rb
@@ -2,141 +2,123 @@ module SS::Relation::File
   extend ActiveSupport::Concern
   extend SS::Translation
 
+  DEFAULT_FILE_CLASS_NAME = "SS::File".freeze
+
   module ClassMethods
-    def belongs_to_file(name, opts = {})
-      class_name = opts[:class_name].presence || "SS::File"
-      required = opts[:required] || false
+    def belongs_to_file(name, class_name: nil, required: false, static_state: nil, resizing: nil)
+      class_name ||= DEFAULT_FILE_CLASS_NAME
 
       belongs_to name.to_sym, class_name: class_name.to_s
 
       attr_accessor "in_#{name}", "rm_#{name}", "in_#{name}_resizing"
-
-      permit_params "in_#{name}", "rm_#{name}"
+      permit_params "#{name}_id", "in_#{name}", "rm_#{name}"
       permit_params "in_#{name}_resizing" => []
 
-      validate if: ->{ send("in_#{name}").present? } do
-        _validate_relation(name, opts)
+      # 処理の失敗しやすさの順でいうと:
+      #
+      # 1. アップロードファイルの SS::File への保存（ファイルサイズが大きいとタイムアウトや DISK FULL の可能性が高まる）
+      # 2. ページの保存（nil になってはいけないフィールドが　nil になっているなど）
+      # 3. 属性の変更
+      #
+      # この順番を意識して、SS::File への保存を before_validation で、属性の変更を before_save や after_save で実行するように
+      # ハンドラーを登録していく。
+      before_validation if: ->{ send("in_#{name}").present? } do
+        _upload_and_set_file(name, default_resizing: resizing, state: static_state)
       end
-      validate if: ->{ required } do
-        _validate_relation_required(name, opts)
+      if class_name != DEFAULT_FILE_CLASS_NAME
+        # SS::File 以外のファイルクラスでは、そのクラス関連のモデルしか DB から読み込めないが、
+        # シラサギでは登録時に SS::TempFile のインスタンスを用いる。
+        # relation の ID が設定されているにもかかわらず、relation が nil となる。それを防ぐ。
+        before_validation { _ensure_to_have_relation(name) }
       end
-      before_save if: ->{ send("in_#{name}").present? } do
-        _save_relation(name, opts)
+      validate { _validate_relation(name) }
+      if required
+        validate { _validate_relation_required(name) }
       end
-      before_save if: ->{ changes["#{name}_id"].present? } do
-        _replace_relation(name, opts)
+      before_save { _destroy_prev_relation(name) }
+      before_save if: ->{ send("in_#{name}").blank? && send("rm_#{name}").to_s == "1" } do
+        _destroy_relation(name)
       end
-      before_save if: ->{ send("rm_#{name}").to_s == "1" } do
-        _remove_relation(name, opts)
+      after_save do
+        _update_relation_attributes(name, class_name: class_name, default_resizing: resizing, static_state: static_state)
       end
-      after_save if: ->{ send(name).present? } do
-        _update_relation_state(name, opts)
-      end
-      after_save if: ->{ send(name).present? } do
-        _update_relation_owner_item(name, opts)
-      end
-      after_destroy if: ->{ send(name).present? } do
-        _remove_relation(name, opts)
-      end
+      after_destroy { _destroy_relation(name) }
 
-      expose_public_methods(name, opts)
+      expose_public_methods(name, static_state: static_state)
     end
 
-    def belongs_to_file2(name, opts = {})
-      class_name = opts[:class_name].presence || "SS::File"
-
-      belongs_to name.to_sym, class_name: class_name.to_s
-
-      attr_accessor "rm_#{name}", "in_#{name}_resizing"
-
-      permit_params "#{name}_id", "rm_#{name}", "in_#{name}_resizing" => []
-
-      before_save if: ->{ send("#{name}_id").present? } do
-        _transfer_relation_ownership(name, opts)
-      end
-      after_save if: ->{ send(name).present? } do
-        _update_relation_state(name, opts)
-      end
-      after_save if: ->{ send(name).present? } do
-        _update_relation_owner_item(name, opts)
-      end
-      before_save if: ->{ changes["#{name}_id"].present? } do
-        _replace_relation(name, opts)
-      end
-      before_save if: ->{ send("rm_#{name}").to_s == "1" } do
-        _remove_relation(name, opts)
-      end
-      after_destroy if: ->{ send(name).present? } do
-        _remove_relation(name, opts)
-      end
-
-      expose_public_methods(name, opts)
-    end
-
-    def expose_public_methods(name, opts)
+    def expose_public_methods(name, static_state:)
       define_method("#{name}_file_state") do
-        _file_state(name, opts)
+        _file_state(name, static_state: static_state)
       end
 
       define_method("generate_relation_public_#{name}") do
-        _generate_relation_public(name, opts)
+        _generate_relation_public(name)
       end
 
       define_method("remove_relation_public_#{name}") do
-        _remove_relation_public(name, opts)
+        _remove_relation_public(name)
       end
     end
-  end
-
-  def relation_file(name, opts = {})
-    class_name = opts[:class_name] || "SS::File"
-
-    file = send(name) || class_name.constantize.new
-    file.in_file  = send("in_#{name}")
-    file.filename = file.in_file.original_filename
-    # file.model    = class_name.underscore
-    file.model    = self.class.to_s.underscore
-    file.site_id  = site_id if respond_to?(:site_id)
-    file.user_id  = @cur_user.id if @cur_user
-    file.state    = send("#{name}_file_state")
-    file.content_type = ::Fs.content_type(file.filename)
-    file.resizing = send("in_#{name}_resizing").presence || opts[:resizing]
-    file.owner_item = SS::Model.container_of(self) if file.respond_to?(:owner_item=)
-    file
   end
 
   private
 
-  def _validate_relation(name, opts)
-    file = relation_file(name, opts)
-    return true if file.valid?
+  def _upload_and_set_file(name, default_resizing:, static_state:)
+    owner_item = SS::Model.container_of(self)
+    resizing = send("in_#{name}_resizing").presence || default_resizing
 
-    file.errors.full_messages.each do |msg|
-      errors.add :base, msg
+    # SS::File への登録に成功し、その後、何らかの問題で owner_item の保存に失敗したとしても、
+    # ファイルダイアログに表示されることを意図して SS::TempFile として登録する。
+    # ファイルダイアログに表示できさえすれば、なんとかゾンビファイルにならなくてすむ。
+    file = SS::TempFile.create_from_upload!(send("in_#{name}"), resizing: resizing) do |file|
+      file.site_id  = site_id if respond_to?(:site_id)
+      file.user_id  = owner_item.cur_user.id if owner_item.respond_to?(:cur_user) && owner_item.cur_user
+      file.state    = _file_state(name, static_state: static_state)
+      # file の owner_item をセットしない。owner_item は保存に失敗する可能性があるので。
     end
 
+    send("#{name}=", file)
+    send("#{name}_id=", file.id) if file.persisted?
+  end
+
+  def _ensure_to_have_relation(name)
+    return if send(name)
+
+    file_id = send("#{name}_id")
+    return if file_id.blank?
+
+    send("#{name}=", SS::File.where(id: file_id).first)
+  end
+
+  def _validate_relation(name)
+    file = send(name)
+    return true if !file || file.valid?
+
+    SS::Model.copy_errors(file, self)
     false
   end
 
-  def _validate_relation_required(name, _opts)
-    return if send("in_#{name}").present?
-    return if send(name)
+  def _validate_relation_required(name)
+    file = send(name)
+    return if file
+
     errors.add "#{name}_id", :blank
   end
 
-  def _save_relation(name, opts)
-    file = relation_file(name, opts)
-    file.save
-    send("#{name}=", file)
-  end
+  def _destroy_prev_relation(name)
+    return if changes["#{name}_id"].blank?
 
-  def _replace_relation(name, _opts)
-    file = SS::File.where(id: send("#{name}_id_was")).first
+    id_was = send("#{name}_id_was")
+    return if id_was.blank?
+
+    file = SS::File.where(id: id_was).first
     file.destroy if file
   end
 
-  def _remove_relation(name, _opts)
+  def _destroy_relation(name)
     file = send(name)
+
     unset("#{name}_id") rescue nil
     send("#{name}=", nil)
 
@@ -151,30 +133,50 @@ module SS::Relation::File
     file.destroy
   end
 
-  def _file_state(_name, opts)
-    opts[:static_state].presence || try(:state).presence || 'public'
+  def _file_state(_name, static_state: nil)
+    static_state || try(:state).presence || 'public'
   end
 
-  def _update_relation_state(name, _opts)
-    return unless respond_to?(:state)
-
+  def _update_relation_attributes(name, class_name:, default_resizing:, static_state:)
     file = send(name)
-    file_state = send("#{name}_file_state")
-    file.update(state: file_state) if file.state != file_state
-  end
+    return if file.blank?
 
-  def _update_relation_owner_item(name, _opts)
-    file = send(name)
+    attributes = {}
     owner_item = SS::Model.container_of(self)
-    return if SS::File.file_owned?(file, owner_item)
+    if _need_to_change_owner_item?(file, owner_item)
+      attributes[:owner_item] = owner_item
+      attributes[:owner_item_id] = owner_item.id
+      attributes[:owner_item_type] = owner_item.class.name
+    end
+
+    if respond_to?(:state)
+      file_state = _file_state(name, static_state: static_state)
+      attributes[:state] = file_state if file.state != file_state
+    end
+
+    model = class_name == "SS::File" ? self.class.name.underscore : class_name.underscore
+    attributes[:model] = model if file.model != model
+
+    file.update(attributes) if attributes.present?
+
+    if attributes[:model].present?
+      resizing = send("in_#{name}_resizing").presence || default_resizing
+      if resizing
+        file.shrink_image_to(resizing[0].to_i, resizing[1].to_i)
+      end
+    end
+  end
+
+  def _need_to_change_owner_item?(file, owner_item)
+    return false if SS::File.file_owned?(file, owner_item)
 
     # 差し替えページの場合、所有者を差し替え元のままとする
-    return if self.respond_to?(:branch?) && self.branch? && SS::File.file_owned?(file, owner_item.master)
+    return false if try(:branch?) && SS::File.file_owned?(file, owner_item.master)
 
-    file.update(owner_item: owner_item)
+    true
   end
 
-  def _generate_relation_public(name, _opts)
+  def _generate_relation_public(name)
     file = send(name)
     return unless file
 
@@ -188,35 +190,8 @@ module SS::Relation::File
     end
   end
 
-  def _remove_relation_public(name, opts)
+  def _remove_relation_public(name)
     file = send(name)
     file.remove_public_file if file
-  end
-
-  def _transfer_relation_ownership(name, opts)
-    return unless send("#{name}_id_changed?")
-
-    cur_id = send("#{name}_id")
-    prev_id = send("#{name}_id_was")
-
-    file = SS::File.where(id: cur_id).first
-
-    return unless file
-
-    expected_model = opts[:file_model] || (opts[:class_name].presence || "SS::File").to_s.underscore
-    if file.model != expected_model
-      file.update(model: expected_model)
-
-      resizing = send("in_#{name}_resizing").presence || opts[:resizing]
-      if resizing
-        file.shrink_image_to(resizing[0].to_i, resizing[1].to_i)
-      end
-    end
-
-    if prev_id.present?
-      SS::File.where(id: prev_id).destroy_all
-    end
-
-    true
   end
 end
