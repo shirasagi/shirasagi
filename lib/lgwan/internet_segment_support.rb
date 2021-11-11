19b8ad56d7db504f81a1acbc74fa1b98472aaff2
itowtips <ito@web-tips.co.jp>
2021-11-11 18:54:14 +0900

    [fix] blank issues

--- lib/lgwan/internet_segment_support.rb
+++ lib/lgwan/internet_segment_support.rb
@@ -5,6 +5,8 @@ module Lgwan
     class << self
       def pull_private_files(page)
         files = page.owned_files.map { |file| [file, file.thumb] }.flatten.compact
+        return true if files.blank?
+
         stdin_data = files.map(&:path).join("\n") + "\n"
         output, error, status = Open3.capture3(PULL_PRIVATE_FILES_COMMAND, stdin_data: stdin_data)
         Rails.logger.error("Lgwan InternetSegmentSupport pull_private_files : #{error}") if error.present?
