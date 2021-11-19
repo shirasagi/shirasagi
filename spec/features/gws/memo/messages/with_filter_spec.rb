067fcc352fbc4cd667576dbcb73545bc7548ef06
NAKANO Hideo <nakano@web-tips.co.jp>
2021-11-19 14:51:29 +0900

    [add] reproducible spec

--- spec/features/gws/memo/messages/with_filter_spec.rb
+++ spec/features/gws/memo/messages/with_filter_spec.rb
@@ -340,4 +340,47 @@ describe 'gws_memo_messages', type: :feature, dbscope: :example, js: true do
       expect(page).to have_no_content(subject)
     end
   end
+
+  context 'when a folder is not existed' do
+    let!(:folder) { create :gws_memo_folder, cur_user: gws_user }
+    let!(:filter) do
+      create(:gws_memo_filter, cur_user: gws_user, subject: subject, action: "move", folder: folder)
+    end
+
+    before do
+      folder.destroy
+      login_gws_user
+    end
+
+    it do
+      visit gws_memo_messages_path(site)
+      click_on I18n.t('ss.links.new')
+
+      within 'form#item-form' do
+        click_on I18n.t("webmail.links.show_cc_bcc")
+
+        within 'dl.see.to' do
+          click_on I18n.t('gws.organization_addresses')
+        end
+      end
+
+      wait_for_cbox do
+        expect(page).to have_content(gws_user.name)
+        click_on gws_user.name
+      end
+
+      within 'form#item-form' do
+        fill_in 'item[subject]', with: subject
+        fill_in 'item[text]', with: body
+
+        page.accept_confirm do
+          click_on I18n.t("ss.buttons.send")
+        end
+      end
+      expect(page).to have_css('#notice', text: I18n.t("ss.notice.sent"))
+
+      visit gws_memo_messages_path(site)
+      expect(page).to have_content(subject)
+    end
+  end
 end
