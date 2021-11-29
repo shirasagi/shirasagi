6bc05dde07568864f140181a48f36e4708b3a67d
NAKANO Hideo <nakano@web-tips.co.jp>
2021-11-29 17:14:00 +0900

    [modify] clone master branch to test

--- bin/install.sh
+++ bin/install.sh
@@ -59,7 +59,8 @@ bundle --version
 
 if [ ! `which ruby` ]; then exit 1; fi
 
-git clone -b stable --depth 1 https://github.com/shirasagi/shirasagi
+#git clone -b stable --depth 1 https://github.com/shirasagi/shirasagi
+git clone https://github.com/shirasagi/shirasagi
 sudo mkdir -p /var/www
 sudo mv shirasagi $SS_DIR
 
