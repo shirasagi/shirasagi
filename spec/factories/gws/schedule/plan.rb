bf68a4d1321b65359098384184752972eb5d8eef
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2021-12-09 11:29:22 +0900

    [fix] factories gws schedule plan gws_schedule_facility_plan_few_days

--- spec/factories/gws/schedule/plan.rb
+++ spec/factories/gws/schedule/plan.rb
@@ -22,8 +22,8 @@ FactoryBot.define do
 
     factory :gws_schedule_facility_plan_few_days do
       allday { 'allday' }
-      start_at { Time.zone.yesterday.change(hour: 10, minute: 0, second: 0) }
-      end_at { start_at + 2.days + 1.hour }
+      start_on { Time.zone.yesterday.change(hour: 10, minute: 0, second: 0).to_date }
+      end_on { start_at + 2.days }
     end
   end
 end
