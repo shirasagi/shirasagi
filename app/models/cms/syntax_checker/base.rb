5c2c3c939ce0fbfee6e240e1a62c9a883fd4c966
NAKANO Hideo <nakano@web-tips.co.jp>
2021-12-15 11:46:38 +0900

    [add] image_alt_checker implementation

--- app/models/cms/syntax_checker/base.rb
+++ app/models/cms/syntax_checker/base.rb
@@ -1,6 +1,12 @@
 module Cms::SyntaxChecker::Base
   extend ActiveSupport::Concern
 
+  class << self
+    def outer_html_summary(node)
+      node.to_s.gsub(/[\r\n]|&nbsp;/, "")
+    end
+  end
+
   def check(context, id, idx, raw_html, doc)
   end
 
--- app/models/cms/syntax_checker/img_alt_checker.rb
+++ app/models/cms/syntax_checker/img_alt_checker.rb
@@ -2,5 +2,32 @@ class Cms::SyntaxChecker::ImgAltChecker
   include Cms::SyntaxChecker::Base
 
   def check(context, id, idx, raw_html, doc)
+    doc.css('img').each do |img_node|
+      alt = img_node["alt"]
+      alt = alt.strip if alt
+      if alt.blank?
+        context.errors << {
+          id: id,
+          idx: idx,
+          code: Cms::SyntaxChecker::Base.outer_html_summary(img_node),
+          msg: I18n.t('errors.messages.set_img_alt'),
+          detail: I18n.t('errors.messages.syntax_check_detail.set_img_alt')
+        }
+
+        next
+      end
+
+      src = img_node["src"]
+      src = src.strip if src
+      next if src.blank? || !src.downcase.include?(alt.downcase)
+
+      context.errors << {
+        id: id,
+        idx: idx,
+        code: Cms::SyntaxChecker::Base.outer_html_summary(img_node),
+        msg: I18n.t('errors.messages.alt_is_included_in_filename'),
+        detail: I18n.t('errors.messages.syntax_check_detail.alt_is_included_in_filename')
+      }
+    end
   end
 end
