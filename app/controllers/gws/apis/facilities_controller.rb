b9d74453848780955c3c8b9e32e3c19927b37f24
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2021-12-20 14:01:04 +0900

    [modify] gws apis facilities_controller set_time_search

--- app/controllers/gws/apis/facilities_controller.rb
+++ app/controllers/gws/apis/facilities_controller.rb
@@ -4,6 +4,9 @@ class Gws::Apis::FacilitiesController < ApplicationController
   model Gws::Facility::Item
 
   before_action :set_category
+  before_action :set_plan, if: ->{ params[:item].present? }
+  before_action :set_time_search, if: ->{ params[:item].present? }
+  before_action :set_hour_range, if: ->{ @plan && @time_search && @free_times }
 
   private
 
@@ -34,9 +37,7 @@ class Gws::Apis::FacilitiesController < ApplicationController
     { min_hour: @cur_site.facility_min_hour || 8, max_hour: @cur_site.facility_max_hour || 22 }
   end
 
-  def set_time_search
-    return if params[:item].blank?
-
+  def set_plan
     item_params = params[:item].to_unsafe_h
     item_params.delete(:facility_column_values)
     item_id = item_params[:id]
@@ -53,6 +54,10 @@ class Gws::Apis::FacilitiesController < ApplicationController
     @plan.user_id = @cur_user.id
     @plan.site_id = @cur_site.id
 
+    @plan
+  end
+
+  def set_time_search
     @time_search = Gws::Schedule::PlanSearch.new(
       params.require(:s).permit(Gws::Schedule::PlanSearch.permitted_fields).merge(pre_params).merge(fix_params)
     )
@@ -63,71 +68,89 @@ class Gws::Apis::FacilitiesController < ApplicationController
     @time_search.valid?
     @free_times = @time_search.search
 
+    @time_search
+  end
+
+  def set_hour_range
     @hour_range = {}
+    between_days = (@plan.end_at.to_date - @plan.start_at.to_date).to_i
+
+    return set_hour_range_a_day_plan if between_days == 0
+    return set_hour_range_repeat_plan if @plan.repeat?
+    set_hour_range_repeat_plan
+  end
+
+  def set_hour_range_a_day_plan
+    params_min_hour = params.dig(:d, :min_hour).presence
+    params_max_hour = params.dig(:d, :max_hour).presence
+
+    @free_times.each do |date, hours|
+      min_hour = params_min_hour || @cur_site.facility_min_hour || 8
+      max_hour = params_max_hour || @cur_site.facility_max_hour || 22
+      @hour_range[date] = (min_hour.to_i...max_hour.to_i)
+    end
+
+    @hour_range
+  end
+
+  def set_hour_range_repeat_plan
     params_min_hour = params.dig(:d, :min_hour).presence
     params_max_hour = params.dig(:d, :max_hour).presence
     between_days = (@plan.end_at.to_date - @plan.start_at.to_date).to_i
 
-    if between_days == 0
+    @free_times.each do |date, hours|
+      repeat_start = date
+      repeat_end = date.advance(days: between_days)
 
-      @free_times.each do |date, hours|
-        min_hour = params_min_hour || @cur_site.facility_min_hour || 8
-        max_hour = params_max_hour || @cur_site.facility_max_hour || 22
-        @hour_range[date] = (min_hour.to_i...max_hour.to_i)
-      end
+      (repeat_start..repeat_end).each do |d|
+        min_hour = @cur_site.facility_min_hour || 8
+        max_hour = @cur_site.facility_max_hour || 22
 
-    else
-
-      if @plan.repeat?
-        @free_times.each do |date, hours|
-          repeat_start = date
-          repeat_end = date.advance(days: between_days)
-
-          (repeat_start..repeat_end).each do |d|
-            min_hour = @cur_site.facility_min_hour || 8
-            max_hour = @cur_site.facility_max_hour || 22
-
-            if (repeat_start == d) && params_min_hour
-              min_hour = params_min_hour
-            end
-            if (repeat_end == d) && params_max_hour
-              max_hour = params_max_hour
-            end
-
-            if @hour_range[d]
-              exist_min = @hour_range[d].first
-              exist_max = @hour_range[d].last
-
-              min_hour = (exist_min < min_hour.to_i) ? exist_min : min_hour.to_i
-              max_hour = (exist_max > max_hour.to_i) ? exist_max : max_hour.to_i
-            end
-
-            @hour_range[d] = (min_hour.to_i...max_hour.to_i)
-          end
+        if (repeat_start == d) && params_min_hour
+          min_hour = params_min_hour
         end
-      else
-        @free_times.each do |date, hours|
-          min_hour = @cur_site.facility_min_hour || 8
-          max_hour = @cur_site.facility_max_hour || 22
-
-          if (@time_search.start_on == date) && params_min_hour
-            min_hour = params_min_hour
-          end
-          if (@time_search.end_on == date) && params_max_hour
-            max_hour = params_max_hour
-          end
-          @hour_range[date] = (min_hour.to_i...max_hour.to_i)
+        if (repeat_end == d) && params_max_hour
+          max_hour = params_max_hour
         end
-      end
 
+        if @hour_range[d]
+          exist_min = @hour_range[d].first
+          exist_max = @hour_range[d].last
+
+          min_hour = (exist_min < min_hour.to_i) ? exist_min : min_hour.to_i
+          max_hour = (exist_max > max_hour.to_i) ? exist_max : max_hour.to_i
+        end
+
+        @hour_range[d] = (min_hour.to_i...max_hour.to_i)
+      end
     end
+
+    @hour_range
+  end
+
+  def set_hour_range_plan
+    params_min_hour = params.dig(:d, :min_hour).presence
+    params_max_hour = params.dig(:d, :max_hour).presence
+
+    @free_times.each do |date, hours|
+      min_hour = @cur_site.facility_min_hour || 8
+      max_hour = @cur_site.facility_max_hour || 22
+
+      if (@time_search.start_on == date) && params_min_hour
+        min_hour = params_min_hour
+      end
+      if (@time_search.end_on == date) && params_max_hour
+        max_hour = params_max_hour
+      end
+      @hour_range[date] = (min_hour.to_i...max_hour.to_i)
+    end
+
+    @hour_range
   end
 
   public
 
   def index
-    set_time_search
-
     @multi = params[:single].blank?
 
     @items = @model.site(@cur_site).
