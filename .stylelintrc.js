97d7db29e0f1a0240ae08c173d89a3141aa75aa6
NAKANO Hideo <nakano@web-tips.co.jp>
2021-12-20 11:23:07 +0900

    [modify] change configuration file to js to be able to write comments

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,9 @@
-{
+"use strict";
+
+module.exports = {
   "extends": [ "stylelint-config-recommended-scss", "stylelint-config-property-sort-order-smacss" ],
   "rules": {
-    "no-descending-specificity": null,
+  "no-descending-specificity": null,
     "font-family-no-missing-generic-family-keyword": [
       true,
       {
@@ -9,6 +11,7 @@
       }
     ],
     "selector-disallowed-list": [
+      // these selectors could be blocked by ad-blockers, so you shouldn't use.
       "/[.#].*(campaign|splash|adsbygoogle|google_ads).*/",
       "/[.#].*ads[-_].*/", "/[.#].*[-_]ads.*/", "/[.#]ads/"
     ]
