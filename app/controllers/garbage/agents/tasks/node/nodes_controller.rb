39e7110624e61e00d4a01736ebcfb8c875f928d5
itowtips <ito@web-tips.co.jp>
2021-12-20 10:52:00 +0900

    [modify] generate garbage nodes

--- app/controllers/garbage/agents/tasks/node/nodes_controller.rb
+++ app/controllers/garbage/agents/tasks/node/nodes_controller.rb
@@ -2,7 +2,7 @@ class Garbage::Agents::Tasks::Node::NodesController < ApplicationController
   include Cms::PublicFilter::Node
 
   def generate
-    # generate_node @node
+    generate_node_with_pagination @node
 
     exporter = Garbage::K5374::TargetExporter.new(@node, @task)
     exporter.write_csv
