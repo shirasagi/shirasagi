867384e0ccf92a222f917aadde9e7b270ae639d1
NAKANO Hideo <nakano@web-tips.co.jp>
2021-12-24 16:23:07 +0900

    [add] .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true,
+    "jquery": true
+  },
+  "extends": [
+    "eslint:recommended"
+  ],
+  "plugins": [
+  ],
+  "rules": {
+    "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+    "no-undef": "off",
+  }
+};
