293228374ded944ae138eb673be641123645187a
Uetani <satoshi.0411@icloud.com>
2022-01-04 17:34:33 +0900

    [fix][cms] cms_addon_file: enable to change order of uploaded filer #4326

--- app/models/concerns/cms/addon/file.rb
+++ app/models/concerns/cms/addon/file.rb
@@ -2,4 +2,12 @@ module Cms::Addon::File
   extend ActiveSupport::Concern
   extend SS::Addon
   include Cms::Reference::Files
+
+  included do
+    field :file_order, type: String
+  end
+
+  def file_order_options
+    [ [I18n.t("cms.options.file.name"), "name"], [I18n.t("cms.options.file.upload"), "upload"] ]
+  end
 end
