eb204662253348f219fe94a1c006c8b08401d679
NAKANO Hideo <nakano@web-tips.co.jp>
2022-01-17 16:57:59 +0900

    [add] reproducible spec

--- spec/models/jmaxml/trigger/tsunami_alert_spec.rb
+++ spec/models/jmaxml/trigger/tsunami_alert_spec.rb
@@ -51,4 +51,29 @@ describe Jmaxml::Trigger::TsunamiAlert, dbscope: :example do
       expect(flag).to eq 1
     end
   end
+
+  describe 'ss-4354: when category code 72 is occurred' do
+    let(:xml_path1) { Rails.root.join(*%w(spec fixtures jmaxml 20220116050009_0_VTSE41_010000.xml.gz)) }
+    let(:xml1) { Zlib::GzipReader.open(xml_path1) { |gz| gz.read } }
+    let(:xmldoc) { REXML::Document.new(xml1) }
+    let(:report_time) { REXML::XPath.first(context.xmldoc, '/Report/Head/ReportDateTime/text()').to_s.strip }
+    let(:page) { create(:rss_weather_xml_page, in_xml: xml1) }
+    let(:context) { OpenStruct.new(site: site, xmldoc: xmldoc) }
+    subject { create(:jmaxml_trigger_tsunami_alert) }
+
+    before do
+      region_580 = create(:jmaxml_tsunami_region_c580)
+      subject.target_region_ids = [ region_580.id ]
+      subject.save!
+    end
+
+    it do
+      report_datetime = REXML::XPath.first(xmldoc, '/Report/Head/ReportDateTime/text()').to_s.strip
+      Timecop.freeze(report_datetime.in_time_zone + 30.minutes) do
+        expect(subject.verify(page, context)).to be_falsey
+        expect(context.type).to be_blank
+        expect(context.area_codes).to be_blank
+      end
+    end
+  end
 end
