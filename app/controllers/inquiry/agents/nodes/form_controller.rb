14c5037019b28fb56c59184d963d0bb6285ea451
itowtips <ito@web-tips.co.jp>
2022-02-02 11:09:55 +0900

    [modify] inquiry : member support

--- app/controllers/inquiry/agents/nodes/form_controller.rb
+++ app/controllers/inquiry/agents/nodes/form_controller.rb
@@ -66,7 +66,7 @@ class Inquiry::Agents::Nodes::FormController < ApplicationController
     @answer = Inquiry::Answer.new(cur_site: @cur_site, cur_node: @cur_node)
     @answer.remote_addr = remote_addr
     @answer.user_agent = request.user_agent
-    @answer.member = get_member_by_session
+    @answer.member = @cur_member
     @answer.source_url = params[:item].try(:[], :source_url)
     @answer.set_data(@data)
   end
