a12b0769fd44e3391bce0ba3323de52134c63f29
Uetani <satoshi.0411@icloud.com>
2022-02-15 09:29:50 +0900

    [fix] gws: memo import group

--- spec/features/gws/memo/import_messages_spec.rb
+++ spec/features/gws/memo/import_messages_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe 'gws_memo_import_messages', type: :feature, dbscope: :example do
   let!(:site) { gws_site }
+  let!(:shared_address_group1) { create :gws_shared_address_group, name: "企画政策部 政策課" }
+  let!(:shared_address_group2) { create :gws_shared_address_group, name: "企画政策部 広報課" }
+  let!(:shared_address_group3) { create :gws_shared_address_group, name: "危機管理部 管理課" }
+  let!(:shared_address_group4) { create :gws_shared_address_group, name: "危機管理部 防災課" }
 
   before { login_gws_user }
 
@@ -24,6 +28,27 @@ describe 'gws_memo_import_messages', type: :feature, dbscope: :example do
       expect(message.format).to eq "text"
       expect(message.filtered).to include(gws_user.id.to_s)
       expect(message.state).to eq "public"
+    end
+  end
+
+  it do
+    visit gws_memo_import_messages_path(site: site)
+
+    within "form#item-form" do
+      attach_file "item[in_file]", "#{Rails.root}/spec/fixtures/memo/group_messages.zip"
+      click_on I18n.t("ss.import")
+    end
+
+    expect(page).to have_css('#notice', text: I18n.t("gws/memo/message.notice.start_import"))
+
+    expect(Gws::Memo::Message.all.count).to eq 1
+    Gws::Memo::Message.all.first.tap do |message|
+      expect(message.site_id).to eq site.id
+      expect(message.subject).to eq "庁舎一斉停電のお知らせ"
+      expect(message.to_shared_address_group_ids).to include shared_address_group1.id
+      expect(message.to_shared_address_group_ids).to include shared_address_group2.id
+      expect(message.to_shared_address_group_ids).to include shared_address_group3.id
+      expect(message.to_shared_address_group_ids).to include shared_address_group4.id
     end
   end
 end
