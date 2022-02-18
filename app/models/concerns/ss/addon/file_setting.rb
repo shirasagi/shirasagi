9cf13c057756f911fd441c58ee9af1886b3cf885
NAKANO Hideo <nakano@web-tips.co.jp>
2022-02-18 13:22:38 +0900

    [add] /fs access restriction settings

--- app/models/concerns/ss/addon/file_setting.rb
+++ app/models/concerns/ss/addon/file_setting.rb
@@ -4,15 +4,28 @@ module SS::Addon
     extend SS::Addon
 
     included do
-      attr_accessor :in_file_resizing_width, :in_file_resizing_height
+      attr_accessor :in_file_resizing_width, :in_file_resizing_height, :in_file_fs_access_restriction_basic_auth_password
 
       field :file_resizing, type: Array, default: []
       field :multibyte_filename_state, type: String
-      validates :multibyte_filename_state, inclusion: { in: %w(enabled disabled), allow_blank: true }
+      field :file_fs_access_restriction_state, type: String
+      field :file_fs_access_restriction_allowed_ip_addresses, type: SS::Extensions::Lines
+      field :file_fs_access_restriction_basic_auth_id, type: String
+      field :file_fs_access_restriction_basic_auth_password, type: String
+      field :file_fs_access_restriction_env_key, type: String
+      field :file_fs_access_restriction_env_value, type: String
+
       permit_params :in_file_resizing_width, :in_file_resizing_height
       permit_params :multibyte_filename_state
+      permit_params :file_fs_access_restriction_state, :file_fs_access_restriction_allowed_ip_addresses
+      permit_params :file_fs_access_restriction_basic_auth_id, :in_file_fs_access_restriction_basic_auth_password
+      permit_params :file_fs_access_restriction_env_key, :file_fs_access_restriction_env_value
 
       before_validation :set_file_resizing
+      before_validation :encrypt_file_fs_access_restriction_basic_auth_password
+
+      validates :multibyte_filename_state, inclusion: { in: %w(enabled disabled), allow_blank: true }
+      validates :file_fs_access_restriction_state, inclusion: { in: %w(enabled disabled), allow_blank: true }
     end
 
     def set_file_resizing
@@ -29,7 +42,11 @@ module SS::Addon
     end
 
     def multibyte_filename_state_options
-      %w(enabled disabled).map { |m| [ I18n.t("ss.options.multibyte_filename_state.#{m}"), m ] }.to_a
+      %w(enabled disabled).map { |m| [ I18n.t("ss.options.multibyte_filename_state.#{m}"), m ] }
+    end
+
+    def file_fs_access_restriction_state_options
+      %w(enabled disabled).map { |m| [ I18n.t("ss.options.state.#{m}"), m ] }
     end
 
     def multibyte_filename_disabled?
@@ -38,6 +55,17 @@ module SS::Addon
 
     def multibyte_filename_enabled?
       !multibyte_filename_disabled?
+    end
+
+    def file_fs_access_restricted?
+      file_fs_access_restriction_state == "enabled"
+    end
+
+    private
+
+    def encrypt_file_fs_access_restriction_basic_auth_password
+      return if in_file_fs_access_restriction_basic_auth_password.blank?
+      self.file_fs_access_restriction_basic_auth_password = SS::Crypt.encrypt(in_file_fs_access_restriction_basic_auth_password)
     end
   end
 end
