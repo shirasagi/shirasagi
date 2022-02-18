ae6cff936b1ee5e24e9cde18a28b71dbdb8ee0af
NAKANO Hideo <nakano@web-tips.co.jp>
2022-02-18 16:46:37 +0900

    [add] /fs access restriction implementations

---
+++ spec/models/ss/file/basic_auth_matcher_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe SS::Addon::FileSetting::BasicAuthMatcher, dbscope: :example do
+  let(:id) { unique_id }
+  let(:pass) { unique_id }
+  let(:digest) { Base64.encode64("#{id}:#{pass}").chomp }
+
+  it do
+    matcher = described_class.new(id, pass)
+
+    expect(matcher.match?(ActionDispatch::Request.new("HTTP_AUTHORIZATION" => "Basic #{digest}"))).to be_truthy
+    expect(matcher.match?(ActionDispatch::Request.new("X-HTTP_AUTHORIZATION" => "Basic #{digest}"))).to be_truthy
+    expect(matcher.match?(ActionDispatch::Request.new("X_HTTP_AUTHORIZATION" => "Basic #{digest}"))).to be_truthy
+    expect(matcher.match?(ActionDispatch::Request.new("REDIRECT_X_HTTP_AUTHORIZATION" => "Basic #{digest}"))).to be_truthy
+
+    expect(matcher.match?(ActionDispatch::Request.new("HTTP_AUTHORIZATION" => "Digest #{digest}"))).to be_falsey
+    expect(matcher.match?(ActionDispatch::Request.new("HTTP_AUTHORIZATION" => "Basic #{unique_id}"))).to be_falsey
+    expect(matcher.match?(ActionDispatch::Request.new({}))).to be_falsey
+  end
+end
---
+++ spec/models/ss/file/ip_address_matcher_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe SS::Addon::FileSetting::IPAddressMatcher, dbscope: :example do
+  context "with specific addresses" do
+    it do
+      matcher = described_class.new(%w(192.168.10.1 192.168.10.2))
+
+      expect(matcher.match?(OpenStruct.new(env: { "HTTP_X_REAL_IP" => "192.168.10.1" }))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: { "HTTP_X_REAL_IP" => "192.168.10.2" }))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: { "HTTP_X_REAL_IP" => "192.168.10.3" }))).to be_falsey
+
+      expect(matcher.match?(OpenStruct.new(env: {}, remote_addr: "192.168.10.1"))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: {}, remote_addr: "192.168.10.2"))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: {}, remote_addr: "192.168.10.3"))).to be_falsey
+    end
+  end
+
+  context "with network addresses" do
+    it do
+      matcher = described_class.new("192.168.10.0/24")
+
+      expect(matcher.match?(OpenStruct.new(env: { "HTTP_X_REAL_IP" => "192.168.10.1" }))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: { "HTTP_X_REAL_IP" => "192.168.10.2" }))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: { "HTTP_X_REAL_IP" => "192.168.10.255" }))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: { "HTTP_X_REAL_IP" => "192.168.11.1" }))).to be_falsey
+
+      expect(matcher.match?(OpenStruct.new(env: {}, remote_addr: "192.168.10.1"))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: {}, remote_addr: "192.168.10.2"))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: {}, remote_addr: "192.168.10.255"))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: {}, remote_addr: "192.168.11.1"))).to be_falsey
+    end
+  end
+
+  context "with network addresses (bit mask)" do
+    it do
+      matcher = described_class.new("192.168.10.0/255.255.255.0")
+
+      expect(matcher.match?(OpenStruct.new(env: { "HTTP_X_REAL_IP" => "192.168.10.1" }))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: { "HTTP_X_REAL_IP" => "192.168.10.2" }))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: { "HTTP_X_REAL_IP" => "192.168.10.255" }))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: { "HTTP_X_REAL_IP" => "192.168.11.1" }))).to be_falsey
+
+      expect(matcher.match?(OpenStruct.new(env: {}, remote_addr: "192.168.10.1"))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: {}, remote_addr: "192.168.10.2"))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: {}, remote_addr: "192.168.10.255"))).to be_truthy
+      expect(matcher.match?(OpenStruct.new(env: {}, remote_addr: "192.168.11.1"))).to be_falsey
+    end
+  end
+end
