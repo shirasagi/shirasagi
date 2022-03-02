87611df672146469de5f5fe3bd97d67bc83300d6
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2022-03-02 13:18:56 +0900

    [modify] cms site_search categories (#4155)
    * [modify] cms site_search category_id
    
    * [modify] cms elasticsearch searcher aggregate
    
    * [modify] cms elasticsearch searcher category_names
    
    * [fix] cms agents nodes site_search index search target options
    
    * [modify] cms elasticsearch seacher aggregate_size
    
    * Revert "[modify] cms elasticsearch searcher category_names"
    
    This reverts commit 71c8a378
    
    * [modify] cms site search category
    
    * [fix] cms lib site_search class
    
    * [modify] cms agents nodes site_search categories
    
    * [fix] cms agents nodes site_search categories

---
+++ app/assets/javascripts/cms/lib/site_search.js
@@ -0,0 +1,100 @@
+this.Cms_Site_Search = (function () {
+  function Cms_Site_Search() {
+  }
+
+  Cms_Site_Search.render = function () {
+    $(".ajax-box.categories").colorbox({
+      fixed: true,
+      width: "90%",
+      height: "90%"
+    });
+    $(".selected-categories").find(".deselect").on("click", function () {
+      $(this).closest(".selected-item").remove();
+      return false;
+    });
+  }
+
+  Cms_Site_Search.selectItem = function (ele) {
+    var item = $(ele).find(".selected-item").clone(false);
+
+    // remove renderInBox class
+    $(item).find("a").removeClass("cboxElement");
+
+    // deselect button
+    $(item).find(".deselect").on("click", function () {
+      $(this).closest(".selected-item").remove();
+      return false;
+    });
+
+    // append
+    item.show();
+    $(".selected-categories").append(item);
+
+    return item;
+  }
+
+  Cms_Site_Search.toggleSelectButton = function () {
+    if ($(".search-ui .items input:checked").size() > 0) {
+      $(".select-items").parent("div").show();
+    } else {
+      $(".select-items").parent("div").hide();
+    }
+  };
+
+  Cms_Site_Search.modal = function () {
+    // form search event
+    $(".search-ui-form form.search").on("submit", function (e) {
+      $(this).ajaxSubmit({
+        url: $(this).attr("action"),
+        success: function (data) {
+          return $("#cboxLoadedContent").html(data);
+        },
+        error: function (data, status) {
+          return alert("== Error ==");
+        }
+      });
+      return false;
+    });
+
+    // select item event
+    $(".search-ui a.select-item").on("click", function (e) {
+      $(".selected-categories").html('');
+      var tr = $(this).closest("tr");
+      var article = Cms_Site_Search.selectItem(tr);
+
+      $.colorbox.close();
+      $(article).find(".set-graph:first").trigger("click");
+
+      return false;
+    });
+
+    // select items event
+    $(".search-ui-select .select-items").on("click", function (e) {
+        $(".selected-categories").html('');
+        $(".search-ui .items .set-category:checked").each(function () {
+            var tr = $(this).closest("tr");
+            Cms_Site_Search.selectItem(tr);
+        });
+      $.colorbox.close();
+      return false;
+    });
+
+    // list-head checkbox event
+    $(".search-ui .list-head input:checkbox").on("change", function (e) {
+      var chk = $(this).prop('checked');
+      $('.search-ui .list-item').each(function () {
+        $(this).toggleClass('checked', chk);
+        $(this).find('.set-category').prop('checked', chk);
+      });
+    });
+
+      // check selected items in modal
+    $(".selected-categories .selected-item").each(function () {
+      var name = $(this).attr("data-name");
+      var tr = $(".items [data-name='" + name + "']");
+        tr.find("input[type=checkbox]").prop('checked', true);
+    });
+  }
+
+  return Cms_Site_Search;
+})();
--- app/models/cms/elasticsearch/searcher.rb
+++ app/models/cms/elasticsearch/searcher.rb
@@ -4,10 +4,10 @@ class Cms::Elasticsearch::Searcher
 
   DEFAULT_FIELD_NAME = 'text_index'.freeze
 
-  attr_accessor :setting, :keyword
-  attr_writer :index, :type, :field_name, :from, :size
+  attr_accessor :setting, :keyword, :category_name, :category_names
+  attr_writer :index, :type, :field_name, :from, :size, :aggregate_size
 
-  permit_params :keyword
+  permit_params :keyword, :category_name, category_names: []
 
   def index
     @index ||= "s#{setting.cur_site.id}"
@@ -25,6 +25,10 @@ class Cms::Elasticsearch::Searcher
     @size ||= 10
   end
 
+  def aggregate_size
+    @aggregate_size ||= 10
+  end
+
   def type
     @type ||= setting.search_types
   end
@@ -40,14 +44,29 @@ class Cms::Elasticsearch::Searcher
   def search
     query = {}
     query[:bool] = {}
-    query[:bool][:must] = { simple_query_string: { query: keyword, fields: [field_name].flatten, default_operator: 'AND' } }
+    query[:bool][:must] = []
+
+    if keyword.present?
+      query[:bool][:must] << { simple_query_string: { query: keyword, fields: [field_name].flatten, default_operator: 'AND' } }
+    end
+
+    if category_name.present?
+      query[:bool][:must] << { term: { categories: category_name } }
+    end
+
+    if category_names.present?
+      query[:bool][:must] << { terms: { categories: category_names } }
+    end
 
     query[:bool][:filter] = {}
     query[:bool][:filter][:bool] = {}
     query[:bool][:filter][:bool][:minimum_should_match] = 1
     query[:bool][:filter][:bool][:should] = filters
 
-    search_params = { index: index, from: from, size: size, body: { query: query } }
+    aggs = {}
+    aggs[:group_by_categories] = { terms: { field: 'categories', size: aggregate_size } }
+
+    search_params = { index: index, from: from, size: size, body: { query: query, aggs: aggs } }
     #search_params[:type] = type if type.present?
 
     client.search(search_params)
