456c5764625aa89099d6b8d2765664fce3b83301
NAKANO Hideo <nakano@web-tips.co.jp>
2022-03-09 10:32:08 +0900

    [modify] able to disable descendants check feature

--- app/assets/javascripts/ss/lib/tree_ui.js
+++ app/assets/javascripts/ss/lib/tree_ui.js
@@ -49,14 +49,15 @@ this.SS_TreeUI = (function () {
       opts = {}
     }
 
-    this.tree = $(tree);
+    var self = this;
+    self.tree = $(tree);
 
     var root = [];
     var expand_all = opts["expand_all"];
     var collapse_all = opts["collapse_all"];
     var expand_group = opts["expand_group"];
 
-    this.tree.find("tbody tr").each(function () {
+    self.tree.find("tbody tr").each(function () {
       return root.push(parseInt($(this).attr("data-depth")));
     });
     root = Math.min.apply(null, root);
@@ -64,7 +65,7 @@ this.SS_TreeUI = (function () {
     if (isNaN(root) || root < 0) {
       return;
     }
-    this.tree.find("tbody tr").each(function () {
+    self.tree.find("tbody tr").each(function () {
       var d, depth, i, j, ref, ref1, td;
       td = $(this).find(".expandable");
       depth = parseInt($(this).attr("data-depth"));
@@ -83,11 +84,11 @@ this.SS_TreeUI = (function () {
         return i.replaceWith('<span class="padding">');
       }
     });
-    this.tree.find(".toggle").on("mousedown mouseup", function (e) {
+    self.tree.find(".toggle").on("mousedown mouseup", function (e) {
       e.stopPropagation();
       return false;
     });
-    this.tree.find(".toggle").on("click", function (e) {
+    self.tree.find(".toggle").on("click", function (e) {
       var depth, img, tr;
       tr = $(this).closest("tr");
       img = tr.find(".toggle:first");
@@ -111,18 +112,19 @@ this.SS_TreeUI = (function () {
       e.stopPropagation();
       return false;
     });
-    var self = this;
-    this.tree.on("click", "[type='checkbox']", function (ev) {
-      self.checkAllChildren(this, this.checked);
-    });
+    if (opts.descendants_check) {
+      self.tree.on("click", "[type='checkbox']", function (ev) {
+        self.checkAllChildren(this, this.checked);
+      });
+    }
     if (expand_all) {
-      SS_TreeUI.openImage(this.tree.find("tbody tr img"));
+      SS_TreeUI.openImage(self.tree.find("tbody tr img"));
     } else if (collapse_all) {
-      SS_TreeUI.closeImage(this.tree.find("tbody tr img"));
+      SS_TreeUI.closeImage(self.tree.find("tbody tr img"));
     } else if (expand_group && $("tbody tr.current").attr("data-depth") !== "0") {
-      SS_TreeUI.openSelectedGroupsTree(this.tree.find("tbody tr.current"));
+      SS_TreeUI.openSelectedGroupsTree(self.tree.find("tbody tr.current"));
     } else {
-      this.tree.find("tr[data-depth='" + root + "'] img").trigger("click");
+      self.tree.find("tr[data-depth='" + root + "'] img").trigger("click");
     }
   }
 
