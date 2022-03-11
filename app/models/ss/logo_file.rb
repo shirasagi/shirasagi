a57a4e69221a0626299483bb6ad877b685b52950
NAKANO Hideo <nakano@web-tips.co.jp>
2022-03-11 20:07:03 +0900

    [modify] refactor w/ RuboCop

--- app/models/ss/logo_file.rb
+++ app/models/ss/logo_file.rb
@@ -1,13 +1,12 @@
 class SS::LogoFile
   include SS::Model::File
-  # include SS::Relation::Thumb
 
   default_scope ->{ where(model: "ss/logo_file") }
 
-  def previewable?(site: nil, user: nil, member: nil)
-    return false if owner_item.blank?
+  module Private
+    module_function
 
-    if owner_item.is_a?(SS::Model::Site)
+    def cms_previewable?(user, owner_item)
       if user
         names_to_match = owner_item.groups.active.pluck(:name).map { |name| /^#{::Regexp.escape(name)}(\/|$)/ }
         return true if user.groups.active.in(name: names_to_match).present?
@@ -20,11 +19,11 @@ class SS::LogoFile
         return true if owner_item.mypage_domain == request_host
       end
 
-      return false
+      false
     end
 
-    if owner_item.is_a?(SS::Model::Group)
-      return true if user && user.root_groups.any? { |group| group.id == owner_item_id }
+    def gws_previewable?(user, owner_item)
+      return true if user && user.root_groups.any? { |group| group.id == owner_item.id }
 
       request = Rails.application.current_request
       if request
@@ -33,7 +32,19 @@ class SS::LogoFile
         return true if owner_item.canonical_domain == request_host
       end
 
-      return false
+      false
+    end
+  end
+
+  def previewable?(site: nil, user: nil, member: nil)
+    return false if owner_item.blank?
+
+    if owner_item.is_a?(SS::Model::Site)
+      return Private.cms_previewable?(user, owner_item)
+    end
+
+    if owner_item.is_a?(SS::Model::Group)
+      return Private.gws_previewable?(user, owner_item)
     end
 
     # unknown owner_item class
