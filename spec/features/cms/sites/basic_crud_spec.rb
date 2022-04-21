a32e33c3546346c7a34de5e97cf324555f109848
NAKANO Hideo <nakano@web-tips.co.jp>
2022-04-21 14:33:26 +0900

    [add] approve_remind_state and approve_remind_later settings

--- spec/features/cms/sites/basic_crud_spec.rb
+++ spec/features/cms/sites/basic_crud_spec.rb
@@ -22,4 +22,35 @@ describe "cms_sites", type: :feature, dbscope: :example do
       expect(site.name).to eq "modify"
     end
   end
+
+  context "approve_setting" do
+    let(:forced_update) { %w(enabled disabled).sample }
+    let(:forced_update_label) { I18n.t("ss.options.state.#{forced_update}") }
+    let(:close_confirmation) { %w(enabled disabled).sample }
+    let(:close_confirmation_label) { I18n.t("ss.options.state.#{close_confirmation}") }
+    let(:approve_remind_state) { %w(enabled disabled).sample }
+    let(:approve_remind_state_label) { I18n.t("ss.options.state.#{approve_remind_state}") }
+    let(:approve_remind_later) { %w(1.day 2.days 3.days 4.days 5.days 6.days 1.week 2.weeks).sample }
+    let(:approve_remind_state_label) { I18n.t("ss.options.approve_remind_state.#{approve_remind_later}") }
+
+    it do
+      visit index_path
+      click_on I18n.t("ss.links.edit")
+      within "form#item-form" do
+        select forced_update_label, from: "s[forced_update]"
+        select close_confirmation_label, from: "s[close_confirmation]"
+        select approve_remind_state_label, from: "s[approve_remind_state]"
+        select approve_remind_later_label, from: "s[approve_remind_later]"
+
+        click_on I18n.t('ss.buttons.save')
+      end
+      expect(page).to have_css('#notice', text: I18n.t('ss.notice.saved'))
+
+      site.reload
+      expect(site.forced_update).to eq forced_update
+      expect(site.close_confirmation).to eq close_confirmation
+      expect(site.approve_remind_state).to eq approve_remind_state
+      expect(site.approve_remind_later).to eq approve_remind_later
+    end
+  end
 end
