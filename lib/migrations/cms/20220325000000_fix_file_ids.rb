a5458cc2c9b193efc9bed855de101c73aa87059f
NAKANO Hideo <nakano@web-tips.co.jp>
2022-05-12 14:47:41 +0900

    [fixup] cms file repair

--- lib/migrations/cms/20220325000000_fix_file_ids.rb
+++ lib/migrations/cms/20220325000000_fix_file_ids.rb
@@ -1,15 +1,14 @@
 class SS::Migration20220325000000
   include SS::Migration::Base
 
-  depends_on "20211011000000"
+  depends_on "20210825000000"
 
   def change
-    repairer = Cms::FileRepair::Repairer.new
     all_ids = ::Cms::Site.all.pluck(:id)
     all_ids.each_slice(20) do |ids|
       ::Cms::Site.where(:id.in => ids).each do |site|
         puts "\# #{site.name}"
-        repairer.fix_states(site)
+        Cms::FileRepair::Repairer.new.fix_states(site)
       end
     end
   end
