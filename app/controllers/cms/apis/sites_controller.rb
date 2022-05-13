8d5aec906c0cacd1896455fb526ebcd2e4118ba5
NAKANO Hideo <nakano@web-tips.co.jp>
2022-05-13 14:46:26 +0900

    [fix][cms][#4410] able to view non-related sites with current user (#4411)
    * [add] reproducible spec
    
    * [fix][#4410] able to view non-related sites with current user
    
    * [modify] use have_no_content instead of ".not_to have_content"

--- app/controllers/cms/apis/sites_controller.rb
+++ app/controllers/cms/apis/sites_controller.rb
@@ -15,10 +15,14 @@ class Cms::Apis::SitesController < ApplicationController
   public
 
   def index
-    @items = @model.
-      allow(:read, @cur_user, site: @cur_site).
-      search(params[:s]).
-      order_by(_id: -1).
-      page(params[:page]).per(50)
+    @items = @model.all.allow(:read, @cur_user, site: @cur_site)
+    @items = @items.search(params[:s])
+    @items = @items.order_by(_id: -1)
+    @items = @items.page(params[:page]).per(50)
+
+    # see: app/controllers/sns/mypage_controller.rb
+    @items = @items.to_a.select do |site|
+      @cur_user.groups.active.in(name: site.groups.active.pluck(:name).map{ |name| /^#{::Regexp.escape(name)}(\/|$)/ } ).present?
+    end
   end
 end
