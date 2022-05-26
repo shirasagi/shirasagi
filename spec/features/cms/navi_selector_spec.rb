ee5f5feedcd024ed4b064bdf9038063956336070
NAKANO Hideo <nakano@web-tips.co.jp>
2022-05-26 15:53:50 +0900

    fixup! dcfca2266ea4ce4fe1bd42f75e1cc5436bde7f80

--- spec/features/cms/navi_selector_spec.rb
+++ spec/features/cms/navi_selector_spec.rb
@@ -2,104 +2,136 @@ require 'spec_helper'
 
 describe "move_cms_pages", type: :feature, dbscope: :example, js: true do
   let!(:site) { cms_site }
-  let!(:node1) { create :cms_node_page, cur_site: site }
-  let!(:node2) { create :article_node_page, cur_site: site }
-  let!(:node3) { create :event_node_page, cur_site: site }
-  let!(:node4) { create :faq_node_page, cur_site: site }
-  let!(:node5) { create :category_node_page, cur_site: site }
-  let!(:node6) { create :inquiry_node_form, cur_site: site }
 
   before { login_cms_user }
 
-  it do
-    visit cms_main_path(site: site)
-    within first(".main-navi") do
-      click_on I18n.t("cms.part")
+  context "without node" do
+    it do
+      visit cms_main_path(site: site)
+      within first(".main-navi") do
+        click_on I18n.t("cms.part")
+      end
+      within first(".main-navi") do
+        expect(page).to have_css(".current[href='#{cms_parts_path(site: site)}']")
+      end
+      # フォルダーツリーの描画完了まで待機
+      within ".tree-navi" do
+        expect(page).to have_css(".content-navi-refresh", text: "refresh")
+      end
     end
-    within first(".main-navi") do
-      expect(page).to have_css(".current[href='#{cms_parts_path(site: site)}']")
-    end
-    # フォルダーツリーの描画完了まで待機
-    within ".tree-navi" do
-      expect(page).to have_css(".content-navi-refresh", text: "refresh")
-    end
+  end
 
-    # 標準機能/ページリスト
-    visit node_pages_path(site: site, cid: node1)
-    within first(".main-navi") do
-      click_on I18n.t("cms.part")
-    end
-    within first(".main-navi") do
-      expect(page).to have_css(".current[href='#{node_parts_path(site: site, cid: node1)}']")
-    end
-    # フォルダーツリーの描画完了まで待機
-    within ".tree-navi" do
-      expect(page).to have_css(".content-navi-refresh", text: "refresh")
-    end
+  context "with node 'cms/page'" do
+    let!(:node1) { create :cms_node_page, cur_site: site }
 
-    # 記事/記事リスト
-    visit article_pages_path(site: site, cid: node2)
-    within first(".main-navi") do
-      click_on I18n.t("cms.part")
+    it do
+      # 標準機能/ページリスト
+      visit node_pages_path(site: site, cid: node1)
+      within first(".main-navi") do
+        click_on I18n.t("cms.part")
+      end
+      within first(".main-navi") do
+        expect(page).to have_css(".current[href='#{node_parts_path(site: site, cid: node1)}']")
+      end
+      # フォルダーツリーの描画完了まで待機
+      within ".tree-navi" do
+        expect(page).to have_css(".content-navi-refresh", text: "refresh")
+      end
     end
-    within first(".main-navi") do
-      expect(page).to have_css(".current[href='#{node_parts_path(site: site, cid: node2)}']")
-    end
-    # フォルダーツリーの描画完了まで待機
-    within ".tree-navi" do
-      expect(page).to have_css(".content-navi-refresh", text: "refresh")
-    end
+  end
 
-    # イベント/イベントリスト
-    visit event_pages_path(site: site, cid: node3)
-    within first(".main-navi") do
-      click_on I18n.t("cms.part")
-    end
-    within first(".main-navi") do
-      expect(page).to have_css(".current[href='#{node_parts_path(site: site, cid: node3)}']")
-    end
-    # フォルダーツリーの描画完了まで待機
-    within ".tree-navi" do
-      expect(page).to have_css(".content-navi-refresh", text: "refresh")
-    end
+  context "with node 'article/page'" do
+    let!(:node2) { create :article_node_page, cur_site: site }
 
-    # FAQ/FAQ記事リスト
-    visit faq_pages_path(site: site, cid: node4)
-    within first(".main-navi") do
-      click_on I18n.t("cms.part")
+    it do
+      # 記事/記事リスト
+      visit article_pages_path(site: site, cid: node2)
+      within first(".main-navi") do
+        click_on I18n.t("cms.part")
+      end
+      within first(".main-navi") do
+        expect(page).to have_css(".current[href='#{node_parts_path(site: site, cid: node2)}']")
+      end
+      # フォルダーツリーの描画完了まで待機
+      within ".tree-navi" do
+        expect(page).to have_css(".content-navi-refresh", text: "refresh")
+      end
     end
-    within first(".main-navi") do
-      expect(page).to have_css(".current[href='#{node_parts_path(site: site, cid: node4)}']")
-    end
-    # フォルダーツリーの描画完了まで待機
-    within ".tree-navi" do
-      expect(page).to have_css(".content-navi-refresh", text: "refresh")
-    end
+  end
 
-    # カテゴリー/ページリスト
-    visit category_pages_path(site: site, cid: node5)
-    within first(".main-navi") do
-      click_on I18n.t("cms.part")
-    end
-    within first(".main-navi") do
-      expect(page).to have_css(".current[href='#{node_parts_path(site: site, cid: node5)}']")
-    end
-    # フォルダーツリーの描画完了まで待機
-    within ".tree-navi" do
-      expect(page).to have_css(".content-navi-refresh", text: "refresh")
-    end
+  context "with node 'event/page'" do
+    let!(:node3) { create :event_node_page, cur_site: site }
 
-    # メールフォーム/フォーム
-    visit inquiry_forms_path(site: site, cid: node6)
-    within first(".main-navi") do
-      click_on I18n.t("cms.part")
+    it do
+      # イベント/イベントリスト
+      visit event_pages_path(site: site, cid: node3)
+      within first(".main-navi") do
+        click_on I18n.t("cms.part")
+      end
+      within first(".main-navi") do
+        expect(page).to have_css(".current[href='#{node_parts_path(site: site, cid: node3)}']")
+      end
+      # フォルダーツリーの描画完了まで待機
+      within ".tree-navi" do
+        expect(page).to have_css(".content-navi-refresh", text: "refresh")
+      end
     end
-    within first(".main-navi") do
-      expect(page).to have_css(".current[href='#{node_parts_path(site: site, cid: node6)}']")
+  end
+
+  context "with node 'faq/page'" do
+    let!(:node4) { create :faq_node_page, cur_site: site }
+
+    it do
+      # FAQ/FAQ記事リスト
+      visit faq_pages_path(site: site, cid: node4)
+      within first(".main-navi") do
+        click_on I18n.t("cms.part")
+      end
+      within first(".main-navi") do
+        expect(page).to have_css(".current[href='#{node_parts_path(site: site, cid: node4)}']")
+      end
+      # フォルダーツリーの描画完了まで待機
+      within ".tree-navi" do
+        expect(page).to have_css(".content-navi-refresh", text: "refresh")
+      end
     end
-    # フォルダーツリーの描画完了まで待機
-    within ".tree-navi" do
-      expect(page).to have_css(".content-navi-refresh", text: "refresh")
+  end
+
+  context "with node 'category/page'" do
+    let!(:node5) { create :category_node_page, cur_site: site }
+
+    it do
+      # カテゴリー/ページリスト
+      visit category_pages_path(site: site, cid: node5)
+      within first(".main-navi") do
+        click_on I18n.t("cms.part")
+      end
+      within first(".main-navi") do
+        expect(page).to have_css(".current[href='#{node_parts_path(site: site, cid: node5)}']")
+      end
+      # フォルダーツリーの描画完了まで待機
+      within ".tree-navi" do
+        expect(page).to have_css(".content-navi-refresh", text: "refresh")
+      end
+    end
+  end
+
+  context "with node 'inquiry/form'" do
+    let!(:node6) { create :inquiry_node_form, cur_site: site }
+
+    it do
+      # メールフォーム/フォーム
+      visit inquiry_forms_path(site: site, cid: node6)
+      within first(".main-navi") do
+        click_on I18n.t("cms.part")
+      end
+      within first(".main-navi") do
+        expect(page).to have_css(".current[href='#{node_parts_path(site: site, cid: node6)}']")
+      end
+      # フォルダーツリーの描画完了まで待機
+      within ".tree-navi" do
+        expect(page).to have_css(".content-navi-refresh", text: "refresh")
+      end
     end
   end
 end
