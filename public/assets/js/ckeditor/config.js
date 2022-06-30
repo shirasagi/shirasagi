e3cea9c521cca26ff27392d7786cfc0fe8f67f23
NAKANO Hideo <nakano@web-tips.co.jp>
2022-06-30 13:02:56 +0900

    [modify] remove spellchecker(scayt) from ckeditor toolbar (#4539)

--- public/assets/js/ckeditor/config.js
+++ public/assets/js/ckeditor/config.js
@@ -11,7 +11,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
 		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection' ] },
 		{ name: 'links' },
 		{ name: 'insert' },
 		{ name: 'forms' },
