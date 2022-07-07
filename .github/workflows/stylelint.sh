1d50218b38f6dccc47fa21856c369b227fd3a685
NAKANO Hideo <nakano@web-tips.co.jp>
2022-07-07 15:38:05 +0900

    [modify] use stylelint formatter

--- .github/workflows/stylelint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# stylelint を reviewdog に組み込むための実行コマンドが長いのでシェルスクリプトにする。
-#
-# 参考: https://github.com/reviewdog/action-stylelint/blob/master/entrypoint.sh
-/usr/bin/env npx stylelint -f json '{app,db}/**/*.{css,scss,sass}'\
-  | jq -r '.[] | {source: .source, warnings:.warnings[]} | "\(.source):\(.warnings.line):\(.warnings.column):\(.warnings.severity): \(.warnings.text) [\(.warnings.rule)](https://stylelint.io/user-guide/rules/\(.warnings.rule))"'
