fbd8636de27c461da3127a85d0ffa2f920d1cfce
NAKANO Hideo <nakano@web-tips.co.jp>
2022-07-07 15:11:12 +0900

    [fixup] remove rdjson formatter downloads. this formatter is now included in package.json

--- .github/workflows/eslint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -f tmp/eslint-formatter-rdjson.js ]; then
-  echo "formatter is not found" >&2
-  exit 1
-fi
-
-npx eslint --resolve-plugins-relative-to=$(npm root -g) -f tmp/eslint-formatter-rdjson.js 'app/assets/**/*.{js,js.erb}'
