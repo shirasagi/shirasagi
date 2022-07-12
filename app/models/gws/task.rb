4dcc37442d8d83f3a1d22aa205c9a6eeb335d6f0
NAKANO Hideo <nakano@web-tips.co.jp>
2022-07-12 13:12:01 +0900

    [modify] move gws task implementation to gws/model/task

--- app/models/gws/task.rb
+++ app/models/gws/task.rb
@@ -1,17 +1,3 @@
 class Gws::Task
-  include SS::Model::Task
-
-  belongs_to :group, class_name: 'Gws::Group'
-
-  class << self
-    # override site scope
-    def site(site)
-      where(group_id: site.id)
-    end
-
-    def find_or_create_for_model(item, site: nil)
-      task_name = "#{item.collection_name}:#{item.id}"
-      self.all.reorder(id: 1).find_or_create_by(group_id: site.try(:id), name: task_name)
-    end
-  end
+  include Gws::Model::Task
 end
