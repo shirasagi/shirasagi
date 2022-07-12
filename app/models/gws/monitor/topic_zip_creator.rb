5e30834ebbd6dac802787d01c0443ecb0261c49c
NAKANO Hideo <nakano@web-tips.co.jp>
2022-07-12 13:23:53 +0900

    [modify] cache feature

--- app/models/gws/monitor/topic_zip_creator.rb
+++ app/models/gws/monitor/topic_zip_creator.rb
@@ -6,7 +6,7 @@ class Gws::Monitor::TopicZipCreator
   delegate :zip_path, to: :topic
 
   def create_zip
-    synchronized do
+    cached do
       topic.attend_groups.each do |group|
         order = group.order || 0
         basename = "#{order}_#{group.trailing_name}"
@@ -32,26 +32,47 @@ class Gws::Monitor::TopicZipCreator
 
   private
 
+  def task
+    @task ||= Gws::Monitor::TopicZipTask.find_or_create_for_model(topic, site: cur_site)
+  end
+
+  def rejected
+    self.errors.add :base, :other_task_is_running
+  end
+
   def synchronized(&block)
+    task.run_with(rejected: method(:rejected), &block)
+  end
+
+  def cached
     zip_path = topic.zip_path
 
-    Gws::Task.find_or_create_for_model(topic, site: cur_site).tap do |task|
-      rejected = -> { self.errors.add :base, :other_task_is_running }
-      task.run_with(rejected: rejected) do
-        ::File.dirname(zip_path).tap do |zip_dir|
-          ::FileUtils.mkdir_p(zip_dir) unless ::Dir.exist?(zip_dir)
-        end
-
-        ::FileUtils.rm_f zip_path
-        Zip::File.open(zip_path, Zip::File::CREATE) do |zip_file|
-          @zip_file = zip_file
-          yield
-        ensure
-          @zip_file = nil
+    synchronized do
+      all_file_ids = [ topic.file_ids ]
+      topic.attend_groups.each do |group|
+        topic.comment(group.id).each do |post|
+          all_file_ids << post.file_ids
         end
       end
-    end
+      all_file_ids.compact!
+      all_file_ids.flatten!
+      all_file_ids.sort!
 
-    zip_path
+      return if task.zipped_file_ids == all_file_ids
+
+      ::File.dirname(zip_path).tap do |zip_dir|
+        ::FileUtils.mkdir_p(zip_dir) unless ::Dir.exist?(zip_dir)
+      end
+
+      ::FileUtils.rm_f zip_path
+      Zip::File.open(zip_path, Zip::File::CREATE) do |zip_file|
+        @zip_file = zip_file
+        yield
+      ensure
+        @zip_file = nil
+      end
+
+      task.update(zipped_file_ids: all_file_ids)
+    end
   end
 end
