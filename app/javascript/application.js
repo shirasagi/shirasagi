9777b43a09d0338077898657bbc2d47acc731cb6
NAKANO Hideo <pinarello.marvel@gmail.com>
2022-08-11 11:47:54 +0900

    [modify] call SS.doneReady when initialization is completed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,12 @@
 import Initializer from "./ss/initializer"
 
 Initializer.load(require.context("./initializers", true, /\.js$/i))
-Initializer.ready(() => console.log("application.js is ready"))
+Initializer.ready(() => {
+  console.log("application.js is ready")
+  SS.doneReady()
+})
+
+if (SS.readyTimeout) {
+  clearTimeout(SS.readyTimeout)
+  SS.readyTimeout = null
+}
