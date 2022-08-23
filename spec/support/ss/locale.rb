f2ef774e3323bbb88692424bb56782947b68b880
NAKANO Hideo <nakano@web-tips.co.jp>
2022-08-23 18:51:30 +0900

    [modify] able to test with en

---
+++ spec/support/ss/locale.rb
@@ -0,0 +1,51 @@
+module SS
+  module LocaleSupport
+    module_function
+
+    def sample_lang(example)
+      type = example.metadata[:type]
+      return I18n.default_locale if type.blank?
+
+      type = type.to_sym
+      return I18n.default_locale unless %i[feature].include?(type)
+
+      languages = Array(example.metadata[:locale].presence).map(&:to_sym)
+      if languages.blank?
+        languages = I18n.available_locales
+      end
+
+      if ENV["RSPEC_LOCALE"].present?
+        languages &= ENV["RSPEC_LOCALE"].split(",").map(&:to_sym)
+      end
+
+      if languages.present?
+        languages.sample
+      else
+        I18n.default_locale
+      end
+    end
+
+    def current_lang
+      @current_lang
+    end
+
+    def current_lang=(lang)
+      @current_lang = lang
+    end
+
+    module Hooks
+      def self.extended(obj)
+        obj.around(:all) do |example|
+          lang = SS::LocaleSupport.current_lang = SS::LocaleSupport.sample_lang(example)
+          Rails.logger.tagged(lang.to_s) do
+            example.run
+          end
+        ensure
+          SS::LocaleSupport.current_lang = nil
+        end
+      end
+    end
+  end
+end
+
+RSpec.configuration.extend(SS::LocaleSupport::Hooks)
