832b18b0919c647152597d11d4fce61326106f96
NAKANO Hideo <nakano@web-tips.co.jp>
2022-09-07 13:19:37 +0900

    [modify] add spec

---
+++ spec/features/webmail/mails/message_sort_spec.rb
@@ -0,0 +1,103 @@
+require 'spec_helper'
+
+describe "webmail_mails", type: :feature, dbscope: :example, js: true, imap: true do
+  let(:now) { Time.zone.now.change(usec: 0) }
+  let(:user) { webmail_imap }
+  let!(:mail1) { Mail.new(date: now - 4.hours, from: "from-1@example.jp", to: user.email, subject: "subject-1") }
+  let!(:mail2) { Mail.new(date: now - 3.hours, from: "from-2@example.jp", to: user.email, subject: "subject-2") }
+  let!(:mail3) { Mail.new(date: now - 2.hours, from: "from-3@example.jp", to: user.email, subject: "subject-3") }
+
+  before do
+    webmail_import_mail(user, mail1)
+    webmail_import_mail(user, mail2)
+    webmail_import_mail(user, mail3)
+
+    login_user(user)
+  end
+
+  context "message sort" do
+    it do
+      visit webmail_mails_path(account: 0)
+
+      within ".list-items" do
+        expect(page).to have_css(".list-item", count: 3)
+        find_all(".list-item").tap do |list_items|
+          expect(list_items[0]).to have_css(".info", text: mail3.subject)
+          expect(list_items[1]).to have_css(".info", text: mail2.subject)
+          expect(list_items[2]).to have_css(".info", text: mail1.subject)
+        end
+
+        within ".list-item-head" do
+          first(".field.from").click
+        end
+        find_all(".list-item").tap do |list_items|
+          expect(list_items[0]).to have_css(".info", text: mail3.subject)
+          expect(list_items[1]).to have_css(".info", text: mail2.subject)
+          expect(list_items[2]).to have_css(".info", text: mail1.subject)
+        end
+
+        # one more click
+        within ".list-item-head" do
+          first(".field.from").click
+        end
+        find_all(".list-item").tap do |list_items|
+          # mails should be in reverse order
+          expect(list_items[0]).to have_css(".info", text: mail1.subject)
+          expect(list_items[1]).to have_css(".info", text: mail2.subject)
+          expect(list_items[2]).to have_css(".info", text: mail3.subject)
+        end
+
+        within ".list-item-head" do
+          first(".field.title").click
+        end
+        find_all(".list-item").tap do |list_items|
+          expect(list_items[0]).to have_css(".info", text: mail3.subject)
+          expect(list_items[1]).to have_css(".info", text: mail2.subject)
+          expect(list_items[2]).to have_css(".info", text: mail1.subject)
+        end
+
+        # one more click
+        within ".list-item-head" do
+          first(".field.title").click
+        end
+        find_all(".list-item").tap do |list_items|
+          # mails should be in reverse order
+          expect(list_items[0]).to have_css(".info", text: mail1.subject)
+          expect(list_items[1]).to have_css(".info", text: mail2.subject)
+          expect(list_items[2]).to have_css(".info", text: mail3.subject)
+        end
+
+        within ".list-item-head" do
+          first(".field.datetime").click
+        end
+        find_all(".list-item").tap do |list_items|
+          expect(list_items[0]).to have_css(".info", text: mail3.subject)
+          expect(list_items[1]).to have_css(".info", text: mail2.subject)
+          expect(list_items[2]).to have_css(".info", text: mail1.subject)
+        end
+
+        # one more
+        within ".list-item-head" do
+          first(".field.datetime").click
+        end
+        find_all(".list-item").tap do |list_items|
+          # mails should be in reverse order
+          expect(list_items[0]).to have_css(".info", text: mail1.subject)
+          expect(list_items[1]).to have_css(".info", text: mail2.subject)
+          expect(list_items[2]).to have_css(".info", text: mail3.subject)
+        end
+
+        # size は全てのメールが　1kB となり、ソートがうまく動作しないので、2 回クリックするだけにする
+        within ".list-item-head" do
+          first(".field.size").click
+        end
+        expect(page).to have_css(".list-item", text: mail1.subject)
+
+        within ".list-item-head" do
+          first(".field.size").click
+        end
+        expect(page).to have_css(".list-item", text: mail1.subject)
+      end
+    end
+  end
+end
