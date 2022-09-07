bbc0078ff5f57407c7f553f0e300e82d16200954
NAKANO Hideo <nakano@web-tips.co.jp>
2022-09-07 15:20:57 +0900

    [modify^2] refactor w/ RuboCop

--- app/controllers/webmail/mails_controller.rb
+++ app/controllers/webmail/mails_controller.rb
@@ -76,7 +76,8 @@ class Webmail::MailsController < ApplicationController
   def index
     @sys_notices = Sys::Notice.and_public.webmail_admin_notice.reorder(notice_severity: 1, released: -1).page(1).per(5)
 
-    @sort_hash = @cur_user.webmail_message_sort_hash(params[:webmail_mode], params[:account], @mailbox, params[:sort], params[:order])
+    @sort_hash = @cur_user.webmail_message_sort_hash(
+      params[:webmail_mode], params[:account], @mailbox, params[:sort], params[:order])
     @items = @imap.mails.
       mailbox(@mailbox).
       search(params[:s]).
