1bce892509db34b318fdc75ebc5b35243d5eb8fd
itowtips <ito@web-tips.co.jp>
2022-09-07 15:40:40 +0900

    [fix] facility search translation (#4611)
    * [fix] cms map openlayers : toggle sidebar column
    
    * [fix] colorbox j render
    
    * [modify] spec
    
    * [modify] rename
    
    Co-authored-by: NAKANO Hideo <nakano@web-tips.co.jp>

--- spec/factories/member/parts.rb
+++ spec/factories/member/parts.rb
@@ -2,4 +2,8 @@ FactoryBot.define do
   factory :member_part_login, class: Member::Part::Login, traits: [:cms_part] do
     route { "member/login" }
   end
+
+  factory :member_part_photo_search, class: Member::Part::PhotoSearch, traits: [:cms_part] do
+    route { "member/photo_search" }
+  end
 end
