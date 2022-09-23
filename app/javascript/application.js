60422fd9d69b868bd76802623e5c785ffc2b17d9
NAKANO Hideo <pinarello.marvel@gmail.com>
2022-09-23 15:27:09 +0900

    opt-out Turbo Drive

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,8 @@
 import Initializer from "./ss/initializer"
 import moment from "moment/moment"
+import * as Turbo from "@hotwired/turbo"
+
+Turbo.session.drive = false
 
 Initializer.load(require.context("./initializers", true, /\.js$/i))
 Initializer.ready(() => {
