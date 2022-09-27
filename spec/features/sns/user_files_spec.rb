af7bcdc0d8e3670215c1338833c0062eab08bc15
NAKANO Hideo <nakano@web-tips.co.jp>
2022-09-27 21:29:57 +0900

    [add] svg sanitization specs

--- spec/features/sns/user_files_spec.rb
+++ spec/features/sns/user_files_spec.rb
@@ -1,14 +1,6 @@
 require 'spec_helper'
 
 describe "sns_user_files", type: :feature, dbscope: :example, js: true do
-  # let(:user) { ss_user }
-  # let(:item) { tmp_ss_file(contents: "#{Rails.root}/spec/fixtures/ss/logo.png", user: user, model: 'ss/user_file') }
-  # let(:index_path) { sns_cur_user_files_path }
-  # let(:new_path) { new_sns_cur_user_file_path }
-  # let(:show_path) { sns_cur_user_file_path item }
-  # let(:edit_path) { edit_sns_cur_user_file_path item }
-  # let(:delete_path) { delete_sns_cur_user_file_path item }
-
   before { login_ss_user }
 
   context "basic crud" do
@@ -71,26 +63,36 @@ describe "sns_user_files", type: :feature, dbscope: :example, js: true do
     end
   end
 
-  context "with svg file" do
-    before { login_ss_user }
+  context "with regular svg file" do
+    let(:file) do
+      tmpfile(extname: ".svg") do |f|
+        f.write <<~SVG
+          <?xml version="1.0" encoding="utf-8"?>
+          <svg xmlns="http://www.w3.org/2000/svg">
+            <text x="0" y="0">SHIRASAGI</text>
+          </svg>
+        SVG
+      end
+    end
+    let(:name) { ::File.basename(file) }
 
     it do
       visit sns_cur_user_files_path
       click_on I18n.t("ss.links.new")
       within "form#item-form" do
-        attach_file "item[in_files][]", "#{Rails.root}/spec/fixtures/ss/shirasagi.svg"
+        attach_file "item[in_files][]", file
         click_on I18n.t('ss.buttons.save')
       end
       wait_for_notice I18n.t("ss.notice.saved")
 
       expect(SS::UserFile.all.count).to eq 1
       item = SS::UserFile.all.first
-      expect(item.name).to eq "shirasagi.svg"
+      expect(item.name).to eq name
       expect(item.user_id).to eq ss_user.id
 
       visit sns_cur_user_files_path
       click_on item.name
-      expect(page).to have_content("shirasagi.svg")
+      expect(page).to have_content(name)
       click_on I18n.t("ss.links.edit")
       within "form#item-form" do
         fill_in "item[name]", with: "modify"
@@ -110,6 +112,164 @@ describe "sns_user_files", type: :feature, dbscope: :example, js: true do
       wait_for_notice I18n.t("ss.notice.deleted")
 
       expect { item.reload }.to raise_error Mongoid::Errors::DocumentNotFound
+    end
+  end
+
+  context "sanitize: svg file with inline script" do
+    let(:file) do
+      tmpfile(extname: ".svg") do |f|
+        f.write <<~SVG
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <text x="0" y="0">SHIRASAGI</text>
+            <script>alert("xss")</script>
+          </svg>
+        SVG
+      end
+    end
+    let(:name) { ::File.basename(file) }
+
+    it do
+      visit sns_cur_user_files_path
+      click_on I18n.t("ss.links.new")
+      within "form#item-form" do
+        attach_file "item[in_files][]", file
+        click_on I18n.t('ss.buttons.save')
+      end
+      wait_for_notice I18n.t("ss.notice.saved")
+
+      expect(SS::UserFile.all.count).to eq 1
+      item = SS::UserFile.all.first
+      expect(item.name).to eq name
+      expect(item.user_id).to eq ss_user.id
+
+      content = ::File.read(item.path)
+      expect(content).not_to include "script"
+    end
+  end
+
+  context "sanitize: svg file contains onclick" do
+    let(:file) do
+      tmpfile(extname: ".svg") do |f|
+        f.write <<~SVG
+          <?xml version="1.0" encoding="utf-8"?>
+          <svg xmlns="http://www.w3.org/2000/svg">
+            <text x="0" y="0" onclick="alert('xss')">SHIRASAGI</text>
+          </svg>
+        SVG
+      end
+    end
+    let(:name) { ::File.basename(file) }
+
+    it do
+      visit sns_cur_user_files_path
+      click_on I18n.t("ss.links.new")
+      within "form#item-form" do
+        attach_file "item[in_files][]", file
+        click_on I18n.t('ss.buttons.save')
+      end
+      wait_for_notice I18n.t("ss.notice.saved")
+
+      expect(SS::UserFile.all.count).to eq 1
+      item = SS::UserFile.all.first
+      expect(item.name).to eq name
+      expect(item.user_id).to eq ss_user.id
+
+      content = ::File.read(item.path)
+      expect(content).not_to include "onclick"
+    end
+  end
+
+  context "sanitize: svg file with 'javascript' href" do
+    let(:file) do
+      tmpfile(extname: ".svg") do |f|
+        f.write <<~SVG
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <a xlink:href="javascript:alert('xss')"><text x="0" y="0">SHIRASAGI</text></a>
+          </svg>
+        SVG
+      end
+    end
+    let(:name) { ::File.basename(file) }
+
+    it do
+      visit sns_cur_user_files_path
+      click_on I18n.t("ss.links.new")
+      within "form#item-form" do
+        attach_file "item[in_files][]", file
+        click_on I18n.t('ss.buttons.save')
+      end
+      wait_for_notice I18n.t("ss.notice.saved")
+
+      expect(SS::UserFile.all.count).to eq 1
+      item = SS::UserFile.all.first
+      expect(item.name).to eq name
+      expect(item.user_id).to eq ss_user.id
+
+      content = ::File.read(item.path)
+      expect(content).not_to include "href"
+    end
+  end
+
+  context "sanitize: svg file with href to other site" do
+    let(:file) do
+      tmpfile(extname: ".svg") do |f|
+        f.write <<~SVG
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <a xlink:href="https://very.very.danger.com/"><text x="0" y="0">SHIRASAGI</text></a>
+          </svg>
+        SVG
+      end
+    end
+    let(:name) { ::File.basename(file) }
+
+    it do
+      visit sns_cur_user_files_path
+      click_on I18n.t("ss.links.new")
+      within "form#item-form" do
+        attach_file "item[in_files][]", file
+        click_on I18n.t('ss.buttons.save')
+      end
+      wait_for_notice I18n.t("ss.notice.saved")
+
+      expect(SS::UserFile.all.count).to eq 1
+      item = SS::UserFile.all.first
+      expect(item.name).to eq name
+      expect(item.user_id).to eq ss_user.id
+
+      content = ::File.read(item.path)
+      expect(content).not_to include "href"
+    end
+  end
+
+  context "sanitize: svg file with href to myself site" do
+    let(:file) do
+      tmpfile(extname: ".svg") do |f|
+        f.write <<~SVG
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <a xlink:href="/path/to/page.html"><text x="0" y="0">SHIRASAGI</text></a>
+          </svg>
+        SVG
+      end
+    end
+    let(:name) { ::File.basename(file) }
+
+    it do
+      visit sns_cur_user_files_path
+      click_on I18n.t("ss.links.new")
+      within "form#item-form" do
+        attach_file "item[in_files][]", file
+        click_on I18n.t('ss.buttons.save')
+      end
+      wait_for_notice I18n.t("ss.notice.saved")
+
+      expect(SS::UserFile.all.count).to eq 1
+      item = SS::UserFile.all.first
+      expect(item.name).to eq name
+      expect(item.user_id).to eq ss_user.id
+
+      content = ::File.read(item.path)
+      expect(content).to include "href"
+      expect(content).to include "/path/to/page.html"
     end
   end
 end
