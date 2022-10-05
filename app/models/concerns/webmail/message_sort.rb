a3bc5e2dc7e75122a13a17ddf8d1b88231ccc63e
NAKANO Hideo <nakano@web-tips.co.jp>
2022-10-05 10:17:12 +0900

    [fixup][webmail] message sort (#4633) (#4664)

--- app/models/concerns/webmail/message_sort.rb
+++ app/models/concerns/webmail/message_sort.rb
@@ -8,18 +8,19 @@ module Webmail::MessageSort
   def webmail_message_sort_hash(webmail_mode, account, mailbox, sort, order)
     return @sort_hash if @sort_hash
 
+    mailbox = mailbox.to_s.tr(".", "_")
     order = (order == "1") ? 1 : -1
     if sort.present? && %w(from to subject internal_date size).include?(sort)
       @sort_hash = { sort => order }
     else
       # @sort_hash = webmail_message_sort.dig(folder.site_id.to_s, folder.folder_path.tr(".", "_"))
-      @sort_hash = webmail_message_sort.dig(webmail_mode, account, mailbox.to_s)
+      @sort_hash = webmail_message_sort.dig(webmail_mode, account, mailbox)
       @sort_hash ||= { "internal_date" => -1 }
     end
 
     k1 = webmail_mode
     k2 = account
-    k3 = mailbox.to_s
+    k3 = mailbox
     self.webmail_message_sort[k1] ||= {}
     self.webmail_message_sort[k1][k2] ||= {}
     if self.webmail_message_sort[k1][k2][k3] != @sort_hash
