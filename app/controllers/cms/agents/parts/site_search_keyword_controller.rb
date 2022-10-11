c3839be677c016e3df81cbd08be07fb84908fa79
NAKANO Hideo <nakano@web-tips.co.jp>
2022-10-11 13:32:24 +0900

    [modify] select public nodes

--- app/controllers/cms/agents/parts/site_search_keyword_controller.rb
+++ app/controllers/cms/agents/parts/site_search_keyword_controller.rb
@@ -2,6 +2,6 @@ class Cms::Agents::Parts::SiteSearchKeywordController < ApplicationController
   include Cms::PartFilter::View
 
   def index
-    @node = Cms::Node::SiteSearch.site(@cur_site).first
+    @node = Cms::Node::SiteSearch.site(@cur_site).and_public(@cur_date).first
   end
 end
