ea0e823f326ed2ee640c6e8338c5d5e97495d7e4
NAKANO Hideo <nakano@web-tips.co.jp>
2022-10-28 11:10:47 +0900

    [modify] reproducible spec

--- spec/features/webmail/mails/message_sort_spec.rb
+++ spec/features/webmail/mails/message_sort_spec.rb
@@ -5,14 +5,15 @@ describe "webmail_mails", type: :feature, dbscope: :example, js: true, imap: tru
 
   context "webmail_mode is account" do
     let(:user) { webmail_imap }
-    let!(:mail1) { Mail.new(date: now - 4.hours, from: "from-1@example.jp", to: user.email, subject: "subject-1") }
-    let!(:mail2) { Mail.new(date: now - 3.hours, from: "from-2@example.jp", to: user.email, subject: "subject-2") }
-    let!(:mail3) { Mail.new(date: now - 2.hours, from: "from-3@example.jp", to: user.email, subject: "subject-3") }
+    let!(:mail1) { Mail.new(date: now - 25.minutes, from: "from-1@example.jp", to: user.email, subject: "subject-1") }
+    let!(:mail2) { Mail.new(date: now - 20.minutes, from: "from-2@example.jp", to: user.email, subject: "subject-2") }
+    let!(:mail3) { Mail.new(date: now - 15.minutes, from: "from-3@example.jp", to: user.email, subject: "subject-3") }
 
     before do
-      webmail_import_mail(user, mail1)
-      webmail_import_mail(user, mail2)
-      webmail_import_mail(user, mail3)
+      Timecop.freeze(now - 25.minutes) { webmail_import_mail(user, mail1) }
+      # mail2 は無害化処理に時間がかかったため、mail3 より遅れて届いた: date と date.recieved (IMAP INTERNALDATE) とが乖離
+      Timecop.freeze(now - 12.minutes) { webmail_import_mail(user, mail2) }
+      Timecop.freeze(now - 15.minutes) { webmail_import_mail(user, mail3) }
 
       login_user(user)
     end
@@ -24,8 +25,8 @@ describe "webmail_mails", type: :feature, dbscope: :example, js: true, imap: tru
         within ".list-items" do
           expect(page).to have_css(".list-item", count: 3)
           find_all(".list-item").tap do |list_items|
-            expect(list_items[0]).to have_css(".info", text: mail3.subject)
-            expect(list_items[1]).to have_css(".info", text: mail2.subject)
+            expect(list_items[0]).to have_css(".info", text: mail2.subject)
+            expect(list_items[1]).to have_css(".info", text: mail3.subject)
             expect(list_items[2]).to have_css(".info", text: mail1.subject)
           end
 
@@ -73,8 +74,8 @@ describe "webmail_mails", type: :feature, dbscope: :example, js: true, imap: tru
             first(".field.datetime").click
           end
           find_all(".list-item").tap do |list_items|
-            expect(list_items[0]).to have_css(".info", text: mail3.subject)
-            expect(list_items[1]).to have_css(".info", text: mail2.subject)
+            expect(list_items[0]).to have_css(".info", text: mail2.subject)
+            expect(list_items[1]).to have_css(".info", text: mail3.subject)
             expect(list_items[2]).to have_css(".info", text: mail1.subject)
           end
 
@@ -85,8 +86,8 @@ describe "webmail_mails", type: :feature, dbscope: :example, js: true, imap: tru
           find_all(".list-item").tap do |list_items|
             # mails should be in reverse order
             expect(list_items[0]).to have_css(".info", text: mail1.subject)
-            expect(list_items[1]).to have_css(".info", text: mail2.subject)
-            expect(list_items[2]).to have_css(".info", text: mail3.subject)
+            expect(list_items[1]).to have_css(".info", text: mail3.subject)
+            expect(list_items[2]).to have_css(".info", text: mail2.subject)
           end
 
           # size は全てのメールが　1kB となり、ソートがうまく動作しないので、2 回クリックするだけにする
@@ -107,14 +108,15 @@ describe "webmail_mails", type: :feature, dbscope: :example, js: true, imap: tru
   context "webmail_mode is group" do
     let!(:group) { create :webmail_group }
     let!(:user) { webmail_imap }
-    let!(:mail1) { Mail.new(date: now - 4.hours, from: "from-1@example.jp", to: group.contact_email, subject: "subject-1") }
-    let!(:mail2) { Mail.new(date: now - 3.hours, from: "from-2@example.jp", to: group.contact_email, subject: "subject-2") }
-    let!(:mail3) { Mail.new(date: now - 2.hours, from: "from-3@example.jp", to: group.contact_email, subject: "subject-3") }
+    let!(:mail1) { Mail.new(date: now - 25.minutes, from: "from-1@example.jp", to: group.contact_email, subject: "subject-1") }
+    let!(:mail2) { Mail.new(date: now - 20.minutes, from: "from-2@example.jp", to: group.contact_email, subject: "subject-2") }
+    let!(:mail3) { Mail.new(date: now - 15.minutes, from: "from-3@example.jp", to: group.contact_email, subject: "subject-3") }
 
     before do
-      webmail_import_mail(group, mail1)
-      webmail_import_mail(group, mail2)
-      webmail_import_mail(group, mail3)
+      Timecop.freeze(now - 25.minutes) { webmail_import_mail(group, mail1) }
+      # mail2 は無害化処理に時間がかかったため、mail3 より遅れて届いた: date と date.recieved (IMAP INTERNALDATE) とが乖離
+      Timecop.freeze(now - 12.minutes) { webmail_import_mail(group, mail2) }
+      Timecop.freeze(now - 15.minutes) { webmail_import_mail(group, mail3) }
 
       user.add_to_set(group_ids: [ group.id ])
 
@@ -128,8 +130,8 @@ describe "webmail_mails", type: :feature, dbscope: :example, js: true, imap: tru
         within ".list-items" do
           expect(page).to have_css(".list-item", count: 3)
           find_all(".list-item").tap do |list_items|
-            expect(list_items[0]).to have_css(".info", text: mail3.subject)
-            expect(list_items[1]).to have_css(".info", text: mail2.subject)
+            expect(list_items[0]).to have_css(".info", text: mail2.subject)
+            expect(list_items[1]).to have_css(".info", text: mail3.subject)
             expect(list_items[2]).to have_css(".info", text: mail1.subject)
           end
 
@@ -177,8 +179,8 @@ describe "webmail_mails", type: :feature, dbscope: :example, js: true, imap: tru
             first(".field.datetime").click
           end
           find_all(".list-item").tap do |list_items|
-            expect(list_items[0]).to have_css(".info", text: mail3.subject)
-            expect(list_items[1]).to have_css(".info", text: mail2.subject)
+            expect(list_items[0]).to have_css(".info", text: mail2.subject)
+            expect(list_items[1]).to have_css(".info", text: mail3.subject)
             expect(list_items[2]).to have_css(".info", text: mail1.subject)
           end
 
@@ -189,8 +191,8 @@ describe "webmail_mails", type: :feature, dbscope: :example, js: true, imap: tru
           find_all(".list-item").tap do |list_items|
             # mails should be in reverse order
             expect(list_items[0]).to have_css(".info", text: mail1.subject)
-            expect(list_items[1]).to have_css(".info", text: mail2.subject)
-            expect(list_items[2]).to have_css(".info", text: mail3.subject)
+            expect(list_items[1]).to have_css(".info", text: mail3.subject)
+            expect(list_items[2]).to have_css(".info", text: mail2.subject)
           end
 
           # size は全てのメールが　1kB となり、ソートがうまく動作しないので、2 回クリックするだけにする
