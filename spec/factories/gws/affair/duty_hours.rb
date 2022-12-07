196ed4f8e401df1d164ebdd8315096bfa4756c7f
itowtips <ito@web-tips.co.jp>
2022-12-07 11:34:24 +0900

    [modify] gws/affair : refactor spec

--- spec/factories/gws/affair/duty_hours.rb
+++ spec/factories/gws/affair/duty_hours.rb
@@ -3,15 +3,15 @@ FactoryBot.define do
     cur_site { gws_site }
     cur_user { gws_user }
     name { unique_id }
+  end
+
+  factory :gws_affair_duty_hour, class: Gws::Affair::DutyHour, traits: [:duty_hour] do
     affair_start_at_hour { 8 }
     affair_start_at_minute { 30 }
     affair_end_at_hour { 17 }
     affair_end_at_minute { 0 }
     affair_on_duty_working_minute { 360 }
     affair_on_duty_break_minute { 45 }
-  end
-
-  factory :gws_affair_duty_hour, class: Gws::Affair::DutyHour, traits: [:duty_hour] do
   end
 
   factory :gws_affair_duty_hour_short, class: Gws::Affair::DutyHour, traits: [:duty_hour] do
