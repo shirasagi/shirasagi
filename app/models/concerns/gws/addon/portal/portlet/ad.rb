9e0a9008d6f2256eac3f2ad0ffd657519c87dab4
NAKANO Hideo <nakano@web-tips.co.jp>
2022-12-14 16:20:05 +0900

    [fix] next / prev navigation by hiding it if speed or pause is blank

--- app/models/concerns/gws/addon/portal/portlet/ad.rb
+++ app/models/concerns/gws/addon/portal/portlet/ad.rb
@@ -17,14 +17,21 @@ module Gws::Addon::Portal::Portlet
     end
 
     def ad_options
+      ret = { pagination_style: "disc" }
+      # ad_speed と ad_pause の両方が与えられると、自動スライド切り替えモード
+      # ad_speed か ad_pause かのどちらかが省略されると、手動スライド切り替えモード
+      #
       # 注意:
       # speed が未設定の場合、prev / next ナビゲーションが初回のみうまく動作し、2回目以降はうまく動作しない。
       # swiper の内部状態がアニメーション中のままとなるのが原因っぽい。
       # speed を設定すると、pause が未設定の場合、超高速でスライドが切り替わるようになってしまう。
-      # そこで、speed と pause が未設定の場合、適切な初期値をセットしてやる。
-      ret = { autoplay: "started", navigation: "show", pagination_style: "disc" }
-      ret[:speed] = ad_speed && ad_speed > 0 ? ad_speed : Sys::Ad::DEFAULT_SLIDE_SPEED
-      ret[:pause] = ad_pause && ad_pause > 0 ? ad_pause : Sys::Ad::DEFAULT_SLIDE_PAUSE
+      # そこで、speed と pause が未設定の場合、手動スライド切り替えモードとし、prev / next ナビゲーションを非表示とする。
+      if ad_speed && ad_speed > 0 && ad_pause && ad_pause > 0
+        ret[:autoplay] = "started"
+        ret[:navigation] = "show"
+        ret[:speed] = ad_speed
+        ret[:pause] = ad_pause
+      end
       ret
     end
   end
