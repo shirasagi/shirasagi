3523efaf0ed09d7e651c057167af1d2f9461ecb6
NAKANO Hideo <nakano@web-tips.co.jp>
2022-12-14 11:29:01 +0900

    [modify] reduce duplicated codes

--- app/models/concerns/sys/ad.rb
+++ app/models/concerns/sys/ad.rb
@@ -2,12 +2,29 @@ module Sys::Ad
   extend ActiveSupport::Concern
   extend SS::Translation
 
+  DEFAULT_WIDTH = 360
+  DEFAULT_PAUSE = 5000
+
   included do
     include SS::Addon::LinkFile
     field :time, type: Integer
     field :width, type: Integer
     permit_params :time, :width
     after_save :file_state_update
+  end
+
+  def ad_effective_width
+    if width && width > 0
+      width
+    else
+      DEFAULT_WIDTH
+    end
+  end
+
+  def ad_options
+    options = { autoplay: "started", speed: 500, navigation: "show", pagination_style: "disc" }
+    options[:pause] = time && time > 0 ? time * 1000 : DEFAULT_PAUSE
+    options
   end
 
   private
