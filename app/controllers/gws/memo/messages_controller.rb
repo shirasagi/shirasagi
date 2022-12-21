945c8e43787320c43830764cd5233c8f5c97a945
NAKANO Hideo <nakano@web-tips.co.jp>
2022-12-21 10:48:27 +0900

    [add][gws] message next prev action (#4726)
    * [add][gws] message next prev action
    
    * [modify] refactor w/ scss-lint
    
    * [modify] redesign prev / next
    
    * [add] missing english locales
    
    * [add] prev / next navigation spec
    
    * [modify] refactor w/ RuboCop
    
    * [modify] refine styles

--- app/controllers/gws/memo/messages_controller.rb
+++ app/controllers/gws/memo/messages_controller.rb
@@ -14,6 +14,8 @@ class Gws::Memo::MessagesController < ApplicationController
   before_action :set_folders, only: [:index, :recent]
   before_action :set_cur_folder, only: [:index]
 
+  helper_method :move_to_prev_tag, :move_to_next_tag
+
   navi_view "gws/memo/messages/navi"
 
   private
@@ -89,6 +91,34 @@ class Gws::Memo::MessagesController < ApplicationController
     end
   end
 
+  def move_to_prev_tag
+    prev_path = @prev_id ? url_for(action: :show, id: @prev_id) : "#"
+    css_classes = %w(prev)
+    unless @prev_id
+      css_classes << "inactive"
+    end
+
+    view_context.tag.div(class: css_classes) do
+      view_context.link_to(prev_path, title: t('gws/memo/message.links.prev')) do
+        view_context.tag.span("arrow_circle_left", class: "material-icons-outlined")
+      end
+    end
+  end
+
+  def move_to_next_tag
+    next_path = @next_id ? url_for(action: :show, id: @next_id) : "#"
+    css_classes = %w(next)
+    unless @next_id
+      css_classes << "inactive"
+    end
+
+    view_context.tag.div(class: css_classes) do
+      view_context.link_to(next_path, title: t('gws/memo/message.links.next')) do
+        view_context.tag.span("arrow_circle_right", class: "material-icons-outlined")
+      end
+    end
+  end
+
   public
 
   def index
@@ -98,6 +128,21 @@ class Gws::Memo::MessagesController < ApplicationController
       search(params[:s]).
       reorder(@sort_hash).
       page(params[:page]).per(50)
+
+    id_list = []
+    @items.each do |item|
+      id_list << item.id.to_s
+    end
+    gws_memo_id_list_session = session[:gws_memo_id_list]
+    gws_memo_id_list_session ||= {}
+    gws_memo_id_list_session['id_list'] = id_list
+    if params[:s]
+      gws_memo_id_list_session['search'] = params[:s].to_unsafe_h
+    else
+      gws_memo_id_list_session['search'] = nil
+    end
+    gws_memo_id_list_session['page'] = params[:page]
+    session[:gws_memo_id_list] = gws_memo_id_list_session
   end
 
   def recent
@@ -134,7 +179,25 @@ class Gws::Memo::MessagesController < ApplicationController
 
   def show
     @item.set_seen(@cur_user).update if @item.state == "public"
-    render
+    @id_list = session[:gws_memo_id_list]['id_list']
+    @id_index = @id_list.index(@item.id.to_s)
+    if @id_index.blank?
+      @prev_id = nil
+      @next_id = nil
+    else
+      if @id_index == 0
+        @prev_id = nil
+      else
+        @prev_id = @id_list[@id_index - 1]
+      end
+      if @id_index == @id_list.size
+        @next_id = nil
+      else
+        @next_id = @id_list[@id_index + 1]
+      end
+    end
+    @search = session[:gws_memo_id_list]['search']
+    @page = session[:gws_memo_id_list]['page']
   end
 
   def edit
