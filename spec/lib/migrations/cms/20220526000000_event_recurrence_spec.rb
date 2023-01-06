c8d5c5d3839f816c8aeb87ef99b956189add1aa3
NAKANO Hideo <nakano@web-tips.co.jp>
2023-01-06 11:07:23 +0900

    [modify] add test for single event date

--- spec/lib/migrations/cms/20220526000000_event_recurrence_spec.rb
+++ spec/lib/migrations/cms/20220526000000_event_recurrence_spec.rb
@@ -11,18 +11,20 @@ RSpec.describe SS::Migration20220526000000, dbscope: :example do
       event_date.in_time_zone.to_date
     end
   end
+  let!(:page3_single_event_date) { create :cms_page, cur_site: site, cur_node: node }
 
   before do
     page1.set(event_dates: event_dates)
     page2.set(event_dates: event_dates)
+    page3_single_event_date.set(event_dates: [ event_dates[3] ])
 
     described_class.new.change
   end
 
   it do
     page1.reload
-    expect(page1.event_recurrences).to have(2).items
     expect(page1.event_dates).to eq event_dates
+    expect(page1.event_recurrences).to have(2).items
     page1.event_recurrences[0].tap do |event_recurrence|
       expect(event_recurrence.kind).to eq "date"
       expect(event_recurrence.start_at).to eq "2022/05/27".in_time_zone.to_date
@@ -45,8 +47,8 @@ RSpec.describe SS::Migration20220526000000, dbscope: :example do
     end
 
     page2.reload
-    expect(page2.event_recurrences).to have(2).items
     expect(page2.event_dates).to eq event_dates
+    expect(page2.event_recurrences).to have(2).items
     page2.event_recurrences[0].tap do |event_recurrence|
       expect(event_recurrence.kind).to eq "date"
       expect(event_recurrence.start_at).to eq "2022/05/27".in_time_zone.to_date
@@ -62,6 +64,20 @@ RSpec.describe SS::Migration20220526000000, dbscope: :example do
       expect(event_recurrence.start_at).to eq "2022/06/03".in_time_zone.to_date
       expect(event_recurrence.frequency).to eq "daily"
       expect(event_recurrence.until_on).to eq "2022/06/05".in_time_zone.to_date
+      expect(event_recurrence.end_at).to eq "2022/06/04".in_time_zone.to_date
+      expect(event_recurrence.by_days).to be_blank
+      expect(event_recurrence.includes_holiday).to be_falsey
+      expect(event_recurrence.exclude_dates).to be_blank
+    end
+
+    page3_single_event_date.reload
+    expect(page3_single_event_date.event_dates).to eq [ event_dates[3] ]
+    expect(page3_single_event_date.event_recurrences).to have(1).items
+    page3_single_event_date.event_recurrences[0].tap do |event_recurrence|
+      expect(event_recurrence.kind).to eq "date"
+      expect(event_recurrence.start_at).to eq "2022/06/03".in_time_zone.to_date
+      expect(event_recurrence.frequency).to eq "daily"
+      expect(event_recurrence.until_on).to eq "2022/06/03".in_time_zone.to_date
       expect(event_recurrence.end_at).to eq "2022/06/04".in_time_zone.to_date
       expect(event_recurrence.by_days).to be_blank
       expect(event_recurrence.includes_holiday).to be_falsey
