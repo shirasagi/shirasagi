cb8da94bf95e7d3545fb82b734ac09f4757115bb
NAKANO Hideo <nakano@web-tips.co.jp>
2023-01-24 15:26:27 +0900

    [fix] webpack assets path (#4773)
    * [modify] revert webpack assets path to original one
    
    * [modify] move webpack assets under propriate directory
    
    * [add] "app/assets/builds" to .gitignore
    
    * [modify] change locale path
    
    * [add] more assets generated with webpack

--- app/controllers/sns/locales_controller.rb
+++ app/controllers/sns/locales_controller.rb
@@ -1,5 +1,5 @@
 class Sns::LocalesController < ApplicationController
-  LOCALE_PATH = Rails.root.join("public/assets/builds/locales")
+  LOCALE_PATH = Rails.root.join("app/assets/builds/locales")
 
   def default
     safe_params = params.permit(:namespace, :languages)
