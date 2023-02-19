6de07abab2ed0c8e451c663d5ea2ca6f25333311
tany <tanizawa@web-tips.co.jp>
2023-02-20 01:37:43 +0900

    [add] 5. select from under my group

--- app/controllers/contact/search_groups_controller.rb
+++ app/controllers/contact/search_groups_controller.rb
@@ -4,7 +4,16 @@ class Contact::SearchGroupsController < ApplicationController
   model Cms::Group
 
   def index
+    return index_in_group if SS.current_user_group
+
     @items = @model.site(@cur_site).
+      search(params[:s]).
+      page(params[:page]).per(50)
+  end
+
+  def index_in_group
+    @items = @model.site(@cur_site).
+      in_group(SS.current_user_group).
       search(params[:s]).
       page(params[:page]).per(50)
   end
