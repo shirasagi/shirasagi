510a16326cfcd2891f43bca991ca553366f2346b
itowtips <ito@web-tips.co.jp>
2023-02-28 19:30:19 +0900

    [modify] image_map : supplement

--- app/models/concerns/image_map/addon/image_setting.rb
+++ app/models/concerns/image_map/addon/image_setting.rb
@@ -7,14 +7,27 @@ module ImageMap::Addon
     included do
       belongs_to_file :image, presence: true
       field :area_states, type: Array, default: []
+      field :supplement_state, type: String, default: "disabled"
 
       permit_params area_states: [:name, :value]
+      permit_params :supplement_state
 
       validate :validate_area_states
     end
 
     def area_state_options
       area_states.map { |state| [state["name"], state["value"]] }
+    end
+
+    def supplement_state_options
+      [
+        [I18n.t("ss.options.state.enabled"), "enabled"],
+        [I18n.t("ss.options.state.disabled"), "disabled"],
+      ]
+    end
+
+    def supplement_enabled?
+      area_state_options.present? && supplement_state == "enabled"
     end
 
     private
