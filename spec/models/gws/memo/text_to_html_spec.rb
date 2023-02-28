4945d7bdb97f0f93f4f0b2fc7bea363ad14f1420
NAKANO Hideo <nakano@web-tips.co.jp>
2023-02-28 13:08:36 +0900

    [fix] spec failures

--- spec/models/gws/memo/text_to_html_spec.rb
+++ spec/models/gws/memo/text_to_html_spec.rb
@@ -15,53 +15,54 @@ RSpec.describe Gws::Memo, type: :model, dbscope: :example do
     shared_examples "what fragment is" do
       it do
         fragment = Nokogiri::HTML.fragment(subject)
-        expect(fragment.children.count).to eq 11
-        fragment.children.tap do |nodes|
-          nodes[0].tap do |node|
-            expect(node.type).to eq Nokogiri::XML::Node::ELEMENT_NODE
-            expect(node.name).to eq "p"
-            expect(node.inner_text).to be_blank
-          end
-          nodes[1].tap do |node|
-            expect(node.type).to eq Nokogiri::XML::Node::TEXT_NODE
-            expect(node.name).to eq "text"
-            expect(node.inner_text).to eq "-" * 52
-          end
-          nodes[2].tap do |node|
-            expect(node.type).to eq Nokogiri::XML::Node::ELEMENT_NODE
-            expect(node.name).to eq "br"
-            expect(node.inner_text).to be_blank
-          end
-          nodes[3].tap do |node|
-            expect(node.type).to eq Nokogiri::XML::Node::TEXT_NODE
-            expect(node.name).to eq "text"
-            expect(node.inner_text).to eq "シラサギ市　企画政策部　政策課"
-          end
-          nodes[4].tap do |node|
-            expect(node.type).to eq Nokogiri::XML::Node::ELEMENT_NODE
-            expect(node.name).to eq "br"
-            expect(node.inner_text).to be_blank
-          end
+        expect(fragment.children.count).to eq 1
+        fragment.children[0].tap do |root_node|
+          expect(root_node.type).to eq Nokogiri::XML::Node::ELEMENT_NODE
+          expect(root_node.name).to eq "p"
+          expect(root_node.children.count).to eq 10
 
-          nodes[-4].tap do |node|
-            expect(node.type).to eq Nokogiri::XML::Node::TEXT_NODE
-            expect(node.name).to eq "text"
-            expect(node.inner_text).to eq "電話番号：00−0000−0000"
-          end
-          nodes[-3].tap do |node|
-            expect(node.type).to eq Nokogiri::XML::Node::ELEMENT_NODE
-            expect(node.name).to eq "br"
-            expect(node.inner_text).to be_blank
-          end
-          nodes[-2].tap do |node|
-            expect(node.type).to eq Nokogiri::XML::Node::TEXT_NODE
-            expect(node.name).to eq "text"
-            expect(node.inner_text).to eq "-" * 52
-          end
-          nodes[-1].tap do |node|
-            expect(node.type).to eq Nokogiri::XML::Node::ELEMENT_NODE
-            expect(node.name).to eq "br"
-            expect(node.inner_text).to be_blank
+          root_node.children.tap do |nodes|
+            nodes[0].tap do |node|
+              expect(node.type).to eq Nokogiri::XML::Node::TEXT_NODE
+              expect(node.name).to eq "text"
+              expect(node.inner_text).to eq "-" * 52
+            end
+            nodes[1].tap do |node|
+              expect(node.type).to eq Nokogiri::XML::Node::ELEMENT_NODE
+              expect(node.name).to eq "br"
+              expect(node.inner_text).to be_blank
+            end
+            nodes[2].tap do |node|
+              expect(node.type).to eq Nokogiri::XML::Node::TEXT_NODE
+              expect(node.name).to eq "text"
+              expect(node.inner_text).to eq "シラサギ市　企画政策部　政策課"
+            end
+            nodes[3].tap do |node|
+              expect(node.type).to eq Nokogiri::XML::Node::ELEMENT_NODE
+              expect(node.name).to eq "br"
+              expect(node.inner_text).to be_blank
+            end
+
+            nodes[-4].tap do |node|
+              expect(node.type).to eq Nokogiri::XML::Node::TEXT_NODE
+              expect(node.name).to eq "text"
+              expect(node.inner_text).to eq "電話番号：00−0000−0000"
+            end
+            nodes[-3].tap do |node|
+              expect(node.type).to eq Nokogiri::XML::Node::ELEMENT_NODE
+              expect(node.name).to eq "br"
+              expect(node.inner_text).to be_blank
+            end
+            nodes[-2].tap do |node|
+              expect(node.type).to eq Nokogiri::XML::Node::TEXT_NODE
+              expect(node.name).to eq "text"
+              expect(node.inner_text).to eq "-" * 52
+            end
+            nodes[-1].tap do |node|
+              expect(node.type).to eq Nokogiri::XML::Node::ELEMENT_NODE
+              expect(node.name).to eq "br"
+              expect(node.inner_text).to be_blank
+            end
           end
         end
       end
@@ -101,6 +102,15 @@ RSpec.describe Gws::Memo, type: :model, dbscope: :example do
     it do
       sub = Gws::Memo.text_to_html("<script>alert('XSS')</script>")
       expect(sub).to eq "<p>alert('XSS')</p>"
+    end
+  end
+
+  context "with blank" do
+    it do
+      sub = Gws::Memo.text_to_html(nil)
+      expect(sub).to be_nil
+      sub = Gws::Memo.text_to_html("")
+      expect(sub).to be_blank
     end
   end
 end
