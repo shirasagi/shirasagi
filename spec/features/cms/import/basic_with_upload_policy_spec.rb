b467b030224a7af7b20d3cddd24226d020fb6077
NAKANO Hideo <nakano@web-tips.co.jp>
2023-03-02 16:40:45 +0900

    [fix] spec failures to add leap year support

--- spec/features/cms/import/basic_with_upload_policy_spec.rb
+++ spec/features/cms/import/basic_with_upload_policy_spec.rb
@@ -1,34 +1,44 @@
 require 'spec_helper'
 
 describe "cms_import_with_upload_policy", type: :feature, dbscope: :example, js: true do
-  subject(:site) { cms_site }
-  subject(:index_path) { cms_import_path site.id }
+  let(:site) { cms_site }
+  let(:index_path) { cms_import_path site.id }
   let!(:file) { "#{Rails.root}/spec/fixtures/cms/import/site.zip" }
   let!(:name) { File.basename(file, ".*") }
   let!(:now) { Time.zone.now.beginning_of_minute }
-  let!(:job_wait) { 1.year.since.to_f }
+  let!(:job_wait) { (now + 1.year.seconds).to_f }
 
   before { login_cms_user }
   before { upload_policy_before_settings("sanitizer") }
   after { upload_policy_after_settings }
 
   context "sanitizer settings" do
-    context "import_date is presnet" do
+    context "import_date is presented" do
       it do
         visit index_path
 
         # upload
         expectation = expect do
-          within "form#task-form" do
-            attach_file "item[in_file]", file
-            fill_in 'item[import_date]', with: I18n.l(now, format: :long)
-            click_button I18n.t('ss.buttons.import')
+          Timecop.freeze(now) do
+            within "form#task-form" do
+              attach_file "item[in_file]", file
+              fill_in 'item[import_date]', with: I18n.l(now + 1.week, format: :long)
+              click_button I18n.t('ss.buttons.import')
+            end
           end
         end
         expectation.to have_enqueued_job(Cms::ImportFilesJob)
 
+        expect(enqueued_jobs.length).to eq 1
         enqueued_jobs.first.tap do |enqueued_job|
-          expect(enqueued_job[:at]).to be > job_wait
+          expect(enqueued_job[:job]).to eq Cms::ImportFilesJob
+          expect(enqueued_job[:args]).to be_blank
+          expect(enqueued_job[:queue]).to eq "default"
+          # import_date がセットされようがセットされまいが、一律 1 年後がセットされる。
+          # 1年の計算方法に注意が必要。1.year.seconds により1 年後が算出されるので、翌年が閏年の場合、1日ずれる点に注意。
+          # 参照:
+          # https://github.com/rails/rails/blob/v6.1.4.1/activejob/lib/active_job/enqueuing.rb#L49
+          expect(enqueued_job[:at]).to eq job_wait
         end
 
         # sanitize
@@ -44,15 +54,25 @@ describe "cms_import_with_upload_policy", type: :feature, dbscope: :example, js:
 
         # upload
         expectation = expect do
-          within "form#task-form" do
-            attach_file "item[in_file]", file
-            click_button I18n.t('ss.buttons.import')
+          Timecop.freeze(now) do
+            within "form#task-form" do
+              attach_file "item[in_file]", file
+              click_button I18n.t('ss.buttons.import')
+            end
           end
         end
         expectation.to have_enqueued_job(Cms::ImportFilesJob)
 
+        expect(enqueued_jobs.length).to eq 1
         enqueued_jobs.first.tap do |enqueued_job|
-          expect(enqueued_job[:at]).to be > job_wait
+          expect(enqueued_job[:job]).to eq Cms::ImportFilesJob
+          expect(enqueued_job[:args]).to be_blank
+          expect(enqueued_job[:queue]).to eq "default"
+          # import_date がセットされようがセットされまいが、一律 1 年後がセットされる。
+          # 1年の計算方法に注意が必要。1.year.seconds により1 年後が算出されるので、翌年が閏年の場合、1日ずれる点に注意。
+          # 参照:
+          # https://github.com/rails/rails/blob/v6.1.4.1/activejob/lib/active_job/enqueuing.rb#L49
+          expect(enqueued_job[:at]).to eq job_wait
         end
 
         # sanitize
