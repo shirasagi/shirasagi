2e792aee57745d7d8e8af1e64a69757ae7cb7c4c
NAKANO Hideo <nakano@web-tips.co.jp>
2023-03-10 11:06:30 +0900

    [modify] refinement chorg

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,7 @@ import moment from "moment/moment"
 import * as Turbo from "@hotwired/turbo"
 import "./application.scss"
 import Initializer from "./ss/initializer"
+import TurboFullRedirect from "./ss/turbo_full_redirect"
 
 window.moment = moment
 
@@ -10,4 +11,5 @@ Turbo.session.drive = false
 Initializer.load(require.context("./initializers", true, /\.js$/i))
 Initializer.ready(() => {
   SS.doneReady()
+  TurboFullRedirect.start()
 })
