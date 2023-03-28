7858fc74006120d59c1ef4284a0325bb3205e379
NAKANO Hideo <nakano@web-tips.co.jp>
2023-03-28 13:18:55 +0900

    [modify] use ss/color to convert string to RGB

--- app/models/concerns/gws/schedule/colorize.rb
+++ app/models/concerns/gws/schedule/colorize.rb
@@ -5,9 +5,10 @@ module Gws::Schedule::Colorize
   def brightness
     return nil if self.color.blank?
 
-    color = self.color.sub(/^#/, '').sub(/^(.)(.)(.)$/, '\\1\\1\\2\\2\\3\\3')
-    r, g, b = color.scan(/../).map { |c| c.hex }
-    ((r * 299) + (g * 587) + (b * 114)).to_f / 1000
+    rgb = SS::Color.parse(self.color)
+    return nil unless rgb
+
+    ((rgb.red * 299) + (rgb.green * 587) + (rgb.blue * 114)).to_f / 1000
   end
 
   def text_color
