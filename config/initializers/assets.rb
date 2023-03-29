2190396b13c7082cf95df939d1d157118eabdc4e
NAKANO Hideo <nakano@web-tips.co.jp>
2023-03-29 11:52:30 +0900

    Sprockets 4 (#4871)
    * [modify] update sprockets to 4.2
    
    * [modify] use "sass-rails" instead of "sassc-rails"
    
    * [modify] upgrade from Sprockets 3.x to 4.x
    
    * [fix] move assets which is required from others to vendor/assets/
    
    * [modify] rebuild assets with Sprockets 4
    
    * [modify] add "app/assets/images/opendata/icon-user.png"

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,12 +12,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w(colorbox.js colorbox.css)
 
 # Shirasagi
 Rails.application.config.assets.paths << Rails.root.join("app/assets/builds")
 Rails.application.config.assets.paths << Rails.root.join("public/assets/css")
 Rails.application.config.assets.paths << Rails.root.join("public/assets/js")
-Rails.application.config.assets.precompile << proc do |path, fn|
-  fn =~ /#{Rails.root}\/app/ && %w(.js .css).include?(::File.extname(path)) && path !~ /\/lib\// && path !~ /\/_/
-end
