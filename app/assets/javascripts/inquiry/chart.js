93b26f7c58121628fa9c41c74a7a1d20ef5167da
NAKANO Hideo <nakano@web-tips.co.jp>
2023-06-02 16:35:53 +0900

    [modify] move "chart.js" and its dependencies under yarn

--- app/assets/javascripts/inquiry/chart.js
+++ app/assets/javascripts/inquiry/chart.js
@@ -1,6 +1,6 @@
-//= require chart.js/dist/chart.min.js
-//= require chartjs-plugin-datalabels/dist/chartjs-plugin-datalabels.min.js
-//= require chartjs-plugin-colorschemes/dist/chartjs-plugin-colorschemes.js
+//= require chart.js/dist/chart.js
+//= require chartjs-plugin-datalabels/dist/chartjs-plugin-datalabels.js
+//= require chartjs-plugin-colorschemes-v3/dist/chartjs-plugin-colorschemes-v3.js
 
 this.Inquiry_Chart = (function() {
   function Inquiry_Chart() {}
