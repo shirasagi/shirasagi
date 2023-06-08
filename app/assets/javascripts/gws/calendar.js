6de9b77afde41af3e25d657d181ea9f962b4fe64
NAKANO Hideo <nakano@web-tips.co.jp>
2023-06-08 17:44:55 +0900

    [modify] move "fullcalendar" and its dependencies under yarn

--- app/assets/javascripts/gws/calendar.js
+++ app/assets/javascripts/gws/calendar.js
@@ -1,8 +1,8 @@
 //= require gws/schedule/lib/save_jquery3_and_load_jquery1
-//= require fullcalendar
-//= require fullcalendar/gcal
-//= require fullcalendar/lang/ja
-//= require fullcalendar-scheduler/scheduler.min.js
+//= require fullcalendar/dist/fullcalendar.js
+//= require fullcalendar/dist/gcal.js
+//= require fullcalendar/dist/lang/ja.js
+//= require fullcalendar-scheduler/dist/scheduler.js
 //= require gws/schedule/lib/restore_jquery3
 //= require gws/schedule/lib/calendar
 //= require gws/schedule/lib/calendar_basic_hour_view
