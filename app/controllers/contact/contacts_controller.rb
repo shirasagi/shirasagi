3f0cdc46e15829c3f2f285f9a3a17ba57a90b117
NAKANO Hideo <nakano@web-tips.co.jp>
2023-06-09 19:01:02 +0900

    [add] batch delete feature to contact/contacts

--- app/controllers/contact/contacts_controller.rb
+++ app/controllers/contact/contacts_controller.rb
@@ -5,6 +5,7 @@ class Contact::ContactsController < ApplicationController
   model SS::Contact
 
   navi_view "cms/main/group_navi"
+  skip_before_action :set_item
 
   ContactGroupPair = Struct.new(:contact, :group, keyword_init: true) do
     def id
@@ -51,5 +52,34 @@ class Contact::ContactsController < ApplicationController
     end
     total_count = results.first["totalCount"].first["count"]
     @items = Kaminari.paginate_array(@items, limit: limit, offset: offset, total_count: total_count)
+  end
+
+  def destroy
+    group_id, contact_id = params[:id].to_s.split(":")
+    if !group_id.numeric? || !BSON::ObjectId.legal?(contact_id)
+      render json: { title: "Not Found" }, status: :not_found
+      return
+    end
+
+    group_item = Cms::Group.site(@cur_site).find(group_id) rescue nil
+    if !group_item
+      render json: { title: "Not Found" }, status: :not_found
+      return
+    end
+
+    contact_item = group_item.contact_groups.where(id: contact_id).first
+    if !contact_item
+      render json: { title: "Not Found" }, status: :not_found
+      return
+    end
+
+    pages = Cms::Page.all.where(contact_group_id: group_item.id, contact_group_contact_id: contact_item.id)
+    if pages.present?
+      render json: { title: t("mongoid.errors.models.ss/contact.in_use") }, status: :bad_request
+      return
+    end
+
+    contact_item.destroy
+    render json: { title: I18n.t("ss.notice.deleted") }, status: :ok
   end
 end
--- config/routes/contact/routes.rb
+++ config/routes/contact/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   Contact::Initializer
 
   namespace "contact", path: ".s:site/contact" do
-    resources :contacts, only: %i[index]
+    resources :contacts, only: %i[index destroy]
 
     namespace "apis" do
       get "contacts" => "contacts#index"
