dfa8d72dc1402c34545a6a5abc2cc51b0a02a288
itowtips <ito@web-tips.co.jp>
2023-06-12 19:22:33 +0900

    [modify] add cms_page_sns_posts permission

--- app/models/cms/initializer.rb
+++ app/models/cms/initializer.rb
@@ -127,6 +127,7 @@ module Cms
     Cms::Role.permission :use_cms_line_deliver_categories
     Cms::Role.permission :use_cms_line_services
     Cms::Role.permission :use_cms_all_contents
+    Cms::Role.permission :use_cms_page_sns_posts
 
     if SS.config.michecker && SS.config.michecker['disable'].blank?
       Cms::Role.permission :use_cms_michecker
