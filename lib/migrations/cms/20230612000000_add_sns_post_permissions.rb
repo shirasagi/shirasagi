90fb372e2c1c4969387655702da39492f00f8e14
itowtips <ito@web-tips.co.jp>
2023-06-13 16:10:22 +0900

    [modify] permission

--- lib/migrations/cms/20230612000000_add_sns_post_permissions.rb
+++ lib/migrations/cms/20230612000000_add_sns_post_permissions.rb
@@ -5,17 +5,29 @@ class SS::Migration20230612000000
 
   def change
     targets = %w(
-      read_other_article_pages
-      read_private_article_pages
-      read_other_cms_pages
-      read_private_cms_pages
+      edit_other_article_pages
+      edit_private_article_pages
+      edit_other_cms_pages
+      edit_private_cms_pages
     ).compact
 
-    sites = Cms::Site.all.to_a.select { |site| site.twitter_poster_enabled? || site.line_poster_enabled? }
+    permission = "use_cms_page_twitter_posts"
+    sites = Cms::Site.all.to_a.select { |site| site.twitter_poster_enabled? }
     sites.each do |site|
       Cms::Role.site(site).in(:permissions.in => targets).each do |item|
-        next if item.permissions.include?("use_cms_page_sns_posts")
-        item.permissions += %w(use_cms_page_sns_posts)
+        next if item.permissions.include?(permission)
+        item.permissions += [permission]
+        item.update
+        puts "Updated cms_role: #{item.id} #{item.name}"
+      end
+    end
+
+    permission = "use_cms_page_line_posts"
+    sites = Cms::Site.all.to_a.select { |site| site.line_poster_enabled? }
+    sites.each do |site|
+      Cms::Role.site(site).in(:permissions.in => targets).each do |item|
+        next if item.permissions.include?(permission)
+        item.permissions += [permission]
         item.update
         puts "Updated cms_role: #{item.id} #{item.name}"
       end
