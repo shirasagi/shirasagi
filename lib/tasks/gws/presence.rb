eff7b91d49960dbafc4b3d074455f663c704a9ba
NAKANO Hideo <nakano@web-tips.co.jp>
2023-06-30 18:01:36 +0900

    [modify] call present reset job within task

--- lib/tasks/gws/presence.rb
+++ lib/tasks/gws/presence.rb
@@ -5,28 +5,9 @@ module Tasks
 
       class << self
         def reset
-          @reset = ::SS.config.gws.dig("presence", "reset") || {}
-
           each_sites do |site|
-            Rails.logger.info "#{site.name}の在籍ステータスをリセット開始"
-
-            group_ids = [site.id]
-            group_ids += ::Gws::Group.where(name: /^#{site.name}\//).pluck(:id)
-
-            user_ids = ::Gws::User.in(group_ids: group_ids).pluck(:id)
-            user_ids.each do |id|
-              user = ::Gws::User.find(id) rescue nil
-              next if user.nil?
-
-              item = user.user_presence(site)
-              next if item.nil?
-
-              state = @reset[item.state]
-              next if state.nil?
-
-              item.state = state
-              item.save
-            end
+            job_class = Gws::Presence::ResetJob.bind(site_id: site.id)
+            job_class.perform_now
           end
         end
       end
