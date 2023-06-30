d30f3d31006dcea19e6205e80750c4ac206dd340
NAKANO Hideo <nakano@web-tips.co.jp>
2023-06-30 18:01:09 +0900

    [modify] convert present reset task to job

---
+++ app/jobs/gws/presence/reset_job.rb
@@ -0,0 +1,34 @@
+class Gws::Presence::ResetJob < Gws::ApplicationJob
+  def perform
+    Rails.logger.info "#{site.name}の在籍ステータスをリセット開始"
+
+    each_user do |user|
+      presence = user.user_presence(site)
+      next if presence.nil?
+
+      state = reset[presence.state]
+      next if state.nil?
+
+      presence.state = state
+      presence.save
+    end
+
+    Rails.logger.info "#{site.name}の在籍ステータスをリセット完了"
+  end
+
+  private
+
+  def reset
+    @reset ||= ::SS.config.gws.dig("presence", "reset") || {}
+  end
+
+  def each_user(&block)
+    group_ids = [site.id]
+    group_ids += ::Gws::Group.active.where(name: /^#{site.name}\//).pluck(:id)
+
+    all_user_ids = ::Gws::User.active.in(group_ids: group_ids).pluck(:id)
+    all_user_ids.each_slice(100) do |ids|
+      ::Gws::User.all.in(id: ids).to_a.each(&block)
+    end
+  end
+end
