b5868cf7953519ef333b3c55ab49afbc3d951e1a
NAKANO Hideo <nakano@web-tips.co.jp>
2023-07-04 10:57:51 +0900

    [modify] have logs a bit verbose

--- app/jobs/gws/presence/reset_job.rb
+++ app/jobs/gws/presence/reset_job.rb
@@ -1,19 +1,35 @@
 class Gws::Presence::ResetJob < Gws::ApplicationJob
   def perform
-    Rails.logger.info "#{site.name}の在席ステータスをリセット開始"
+    Rails.logger.tagged(site.name) do
+      Rails.logger.info { "在席ステータスをリセット開始" }
 
-    each_user do |user|
-      presence = user.user_presence(site)
-      next if presence.nil?
+      each_user do |user|
+        Rails.logger.tagged("#{user.name}(#{user.uid})") do
+          presence = user.user_presence(site)
+          next if presence.nil?
 
-      state = reset[presence.state]
-      next if state.nil?
+          state = reset[presence.state]
+          next if state.nil?
+          next if presence.state == state
 
-      presence.state = state
-      presence.save
+          presence.state = state
+          result = presence.save
+          if result
+            Rails.logger.info { "在席ステータスをリセットしました。" }
+          else
+            Rails.logger.warn do
+              messages = [ "在席ステータスのリセットに失敗しました。" ]
+              messages += presence.errors.full_messages
+              messages.join("\n")
+            end
+          end
+        rescue => e
+          Rails.logger.warn { "#{e.class} (#{e.message}):\n  #{e.backtrace.join("\n  ")}" }
+        end
+      end
+
+      Rails.logger.info { "在席ステータスをリセット完了" }
     end
-
-    Rails.logger.info "#{site.name}の在席ステータスをリセット完了"
   end
 
   private
