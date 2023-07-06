47eca8e3b5f7e2c3a4a33d15885f6dd7d9e91013
itowtips <ito@web-tips.co.jp>
2023-07-06 15:54:16 +0900

    [J090-127] 通知アイコンの全て既読機能 (#4944)
    * [J090-127] 通知アイコンの全て既読機能 (#139)
    
    * [modify] ss/notification : set_seen_all
    
    * [fix] spec failure
    
    * [fix] reviewdog issues
    
    * [fix] spec failure

---
+++ spec/features/gws/memo/notices/set_seen_all_spec.rb
@@ -0,0 +1,149 @@
+require 'spec_helper'
+
+describe 'gws/memo/notices', type: :feature, dbscope: :example, js: true do
+  let(:site) { gws_site }
+  let(:user) { gws_user }
+  let(:circular_item) { create(:gws_circular_post, :gws_circular_posts) }
+  let(:circular_path) { gws_circular_post_path(site: site, category: '-', id: circular_item) }
+  let!(:item1) do
+    SS::Notification.create!(
+      cur_group: site, cur_user: user,
+      subject: "subject-#{unique_id}", format: "text", text: "text-#{unique_id}" * 10,
+      member_ids: [user.id], state: "public"
+    )
+  end
+  let!(:item2) do
+    SS::Notification.create!(
+      cur_group: site, cur_user: user,
+      subject: "subject-#{unique_id}", format: "text", text: "", url: circular_path,
+      member_ids: [user.id], state: "public"
+    )
+  end
+  let!(:item3) do
+    SS::Notification.create!(
+      cur_group: site, cur_user: user,
+      subject: "subject-#{unique_id}", format: "text", text: "",
+      member_ids: [user.id], state: "public"
+    )
+  end
+
+  before { login_gws_user }
+
+  context "index" do
+    let(:index_path) { gws_memo_notices_path site }
+
+    it do
+      visit index_path
+
+      within ".gws-memo-notices" do
+        within ".list-items" do
+          expect(page).to have_css(".list-item.unseen", text: item1.subject)
+          expect(page).to have_css(".list-item.unseen", text: item2.subject)
+          expect(page).to have_css(".list-item.unseen", text: item3.subject)
+        end
+        within ".list-head" do
+          find('label.check input').set(true)
+          page.accept_confirm(I18n.t("gws/notice.confirm.set_seen")) do
+            click_on I18n.t("gws/notice.links.set_seen")
+          end
+        end
+      end
+      wait_for_notice I18n.t("ss.notice.set_seen")
+
+      within ".gws-memo-notices" do
+        within ".list-items" do
+          expect(page).to have_css(".list-item.seen", text: item1.subject)
+          expect(page).to have_css(".list-item.seen", text: item2.subject)
+          expect(page).to have_css(".list-item.seen", text: item3.subject)
+        end
+      end
+    end
+
+    it do
+      visit index_path
+
+      within ".gws-memo-notices" do
+        within ".list-items" do
+          expect(page).to have_css(".list-item.unseen", text: item1.subject)
+          expect(page).to have_css(".list-item.unseen", text: item2.subject)
+          expect(page).to have_css(".list-item.unseen", text: item3.subject)
+        end
+        within ".list-items" do
+          first('label.check input').set(true)
+        end
+        within ".list-head" do
+          page.accept_confirm(I18n.t("gws/notice.confirm.set_seen")) do
+            click_on I18n.t("gws/notice.links.set_seen")
+          end
+        end
+      end
+      wait_for_notice I18n.t("ss.notice.set_seen")
+
+      within ".gws-memo-notices" do
+        within ".list-items" do
+          expect(page).to have_selector(".list-item.unseen", count: 2)
+          expect(page).to have_selector(".list-item.seen", count: 1)
+        end
+      end
+    end
+  end
+
+  context "popup" do
+    let(:portal_path) { gws_portal_path site }
+
+    it do
+      visit portal_path
+
+      first(".gws-memo-notice.popup-notice-container").click
+      within ".gws-memo-notice.popup-notice-container" do
+        within ".popup-notice-items" do
+          expect(page).to have_css(".list-item", text: item1.subject)
+          expect(page).to have_css(".list-item", text: item2.subject)
+          expect(page).to have_css(".list-item", text: item3.subject)
+        end
+        click_on I18n.t("ss.links.more_all")
+      end
+
+      within ".gws-memo-notices" do
+        within ".list-items" do
+          expect(page).to have_css(".list-item.unseen", text: item1.subject)
+          expect(page).to have_css(".list-item.unseen", text: item2.subject)
+          expect(page).to have_css(".list-item.unseen", text: item3.subject)
+        end
+      end
+    end
+
+    it do
+      visit portal_path
+
+      first(".gws-memo-notice.popup-notice-container").click
+      within ".gws-memo-notice.popup-notice-container" do
+        within ".popup-notice-items" do
+          expect(page).to have_css(".list-item", text: item1.subject)
+          expect(page).to have_css(".list-item", text: item2.subject)
+          expect(page).to have_css(".list-item", text: item3.subject)
+        end
+        page.accept_confirm(I18n.t("gws/notice.confirm.set_seen")) do
+          click_on I18n.t("gws/notice.links.set_seen_all")
+        end
+      end
+      wait_for_notice I18n.t("ss.notice.set_seen")
+
+      first(".gws-memo-notice.popup-notice-container").click
+      within ".gws-memo-notice.popup-notice-container" do
+        within ".popup-notice-items" do
+          expect(page).to have_css(".list-item.empty", text: I18n.t("gws/memo/message.notice.no_recents"))
+        end
+        click_on I18n.t("ss.links.more_all")
+      end
+
+      within ".gws-memo-notices" do
+        within ".list-items" do
+          expect(page).to have_css(".list-item.seen", text: item1.subject)
+          expect(page).to have_css(".list-item.seen", text: item2.subject)
+          expect(page).to have_css(".list-item.seen", text: item3.subject)
+        end
+      end
+    end
+  end
+end
