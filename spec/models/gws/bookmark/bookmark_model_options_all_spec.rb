bfac98356e6d560c9e8cfe60f402ca65036eaf41
itowtips <ito@web-tips.co.jp>
2023-07-07 10:30:39 +0900

    [fix] spec failure

--- spec/models/gws/bookmark/bookmark_model_options_all_spec.rb
+++ spec/models/gws/bookmark/bookmark_model_options_all_spec.rb
@@ -5,16 +5,21 @@ describe Gws::Bookmark, type: :model, dbscope: :example do
 
   describe ".bookmark_model_options_all" do
     context "with all menus available" do
+      let(:private_types) { 1 }
+      let(:public_types) { Gws::Bookmark::BOOKMARK_MODEL_ALL_TYPES.count - private_types }
+
       it do
         public_options, private_options = Gws::Bookmark.bookmark_model_options_all(site)
-        expect(public_options).to have(26).items
-        expect(private_options).to have(1).items
+        expect(public_options).to have(public_types).items
+        expect(private_options).to have(private_types).items
         expect(private_options).to include([ I18n.t("modules.gws/elasticsearch"), "elasticsearch" ])
       end
     end
 
     context "with all menus available" do
       let(:disabled_menu) { (Gws::Bookmark::BOOKMARK_MODEL_TYPES - %w(elasticsearch)).sample }
+      let(:private_types) { 2 }
+      let(:public_types) { Gws::Bookmark::BOOKMARK_MODEL_ALL_TYPES.count - private_types }
 
       before do
         site.update("menu_#{disabled_menu}_state" => "hide")
@@ -22,8 +27,8 @@ describe Gws::Bookmark, type: :model, dbscope: :example do
 
       it do
         public_options, private_options = Gws::Bookmark.bookmark_model_options_all(site)
-        expect(public_options).to have(25).items
-        expect(private_options).to have(2).items
+        expect(public_options).to have(public_types).items
+        expect(private_options).to have(private_types).items
         expect(private_options).to include([ I18n.t("modules.gws/#{disabled_menu}"), disabled_menu ])
       end
     end
