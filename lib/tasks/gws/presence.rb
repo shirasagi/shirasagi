677dfb99b91187053114a3a7e109f28b69112e5d
NAKANO Hideo <nakano@web-tips.co.jp>
2023-07-26 10:38:01 +0900

    [fixup][gws][presence] refactor gws:presence:reset task (#4954)

--- lib/tasks/gws/presence.rb
+++ lib/tasks/gws/presence.rb
@@ -6,7 +6,7 @@ module Tasks
       class << self
         def reset
           each_sites do |site|
-            job_class = Gws::Presence::ResetJob.bind(site_id: site.id)
+            job_class = ::Gws::Presence::ResetJob.bind(site_id: site.id)
             job_class.perform_now
           end
         end
