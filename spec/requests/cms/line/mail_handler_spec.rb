cf64de4953d009c237089507636ea225d4283787
itowtips <ito@web-tips.co.jp>
2023-08-03 15:21:02 +0900

    [add] cms jwt api token : mail_page and cms/line (#4984)
    * [add] cms jwt api token : mail_page and cms/line
    
    * [modify] reviewdog issues
    
    * [modify] expiration_date
    
    * [modify] add curd spec
    
    * [modify] token state
    
    * [modify] login with audience and create history log
    
    * [fix] en locale
    
    * [modify] get_token method

--- spec/requests/cms/line/mail_handler_spec.rb
+++ spec/requests/cms/line/mail_handler_spec.rb
@@ -20,150 +20,176 @@ describe "Cms::Agents::Nodes::LineHubController", type: :request, dbscope: :exam
       Capybara.app_host = "http://#{site.domain}"
     end
 
-    context "deliver handler" do
-      let!(:mail_handler) do
-        create(:cms_line_mail_handler, handle_state: "deliver", deliver_condition_state: "broadcast",
-          from_conditions: from_conditions, to_conditions: to_conditions)
-      end
-      let!(:url) { "http://#{site.domain}/#{node.url}mail/#{mail_handler.filename}" }
+    context "without api_token" do
+      context "deliver handler" do
+        let!(:mail_handler) do
+          create(:cms_line_mail_handler, handle_state: "deliver", deliver_condition_state: "broadcast",
+            from_conditions: from_conditions, to_conditions: to_conditions)
+        end
+        let!(:url) { "http://#{site.domain}/#{node.url}mail/#{mail_handler.filename}" }
 
-      context "post utf-8 mail" do
+        context "post utf-8 mail" do
+          let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/UTF-8.eml") }
+
+          it "#mail" do
+            capture_line_bot_client do |capture|
+              expect { post url, params: { data: data } }.to raise_error "404"
+              expect(Cms::Line::Message.count).to eq 0
+            end
+          end
+        end
+      end
+    end
+
+    context "with api_token" do
+      let!(:api_token) { create :cms_api_token }
+      let!(:headers) { { SS::ApiToken::API_KEY_HEADER => api_token.to_jwt } }
+
+      context "deliver handler" do
+        let!(:mail_handler) do
+          create(:cms_line_mail_handler, handle_state: "deliver", deliver_condition_state: "broadcast",
+            from_conditions: from_conditions, to_conditions: to_conditions)
+        end
+        let!(:url) { "http://#{site.domain}/#{node.url}mail/#{mail_handler.filename}" }
+
+        context "post utf-8 mail" do
+          let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/UTF-8.eml") }
+
+          it "#mail" do
+            capture_line_bot_client do |capture|
+              perform_enqueued_jobs do
+                post url, params: { data: data }, headers: headers
+                expect(Cms::Line::Message.count).to eq 1
+                expect(message.name).to include("[#{mail_handler.name}]")
+                expect(message.templates.first.text).to eq decoded
+              end
+              expect(message.deliver_state).to eq "completed"
+              expect(capture.broadcast.count).to eq 1
+              expect(Cms::SnsPostLog::LineDeliver.count).to eq 1
+            end
+          end
+        end
+
+        context "post iso-2022-jp mail" do
+          let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/ISO-2022-JP.eml") }
+
+          it "#mail" do
+            capture_line_bot_client do |capture|
+              perform_enqueued_jobs do
+                post url, params: { data: data }, headers: headers
+                expect(Cms::Line::Message.count).to eq 1
+                expect(message.name).to include("[#{mail_handler.name}]")
+                expect(message.templates.first.text).to eq decoded
+              end
+              expect(message.deliver_state).to eq "completed"
+              expect(capture.broadcast.count).to eq 1
+              expect(Cms::SnsPostLog::LineDeliver.count).to eq 1
+            end
+          end
+        end
+      end
+
+      context "draft handler" do
+        let!(:mail_handler) do
+          create(:cms_line_mail_handler, handle_state: "draft", deliver_condition_state: "broadcast",
+            from_conditions: from_conditions, to_conditions: to_conditions)
+        end
+        let!(:url) { "http://#{site.domain}/#{node.url}mail/#{mail_handler.filename}" }
+
+        context "post utf-8 mail" do
+          let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/UTF-8.eml") }
+
+          it "#mail" do
+            capture_line_bot_client do |capture|
+              perform_enqueued_jobs do
+                post url, params: { data: data }, headers: headers
+                expect(Cms::Line::Message.count).to eq 1
+                expect(message.name).to include("[#{mail_handler.name}]")
+                expect(message.templates.first.text).to eq decoded
+              end
+              expect(message.deliver_state).to eq "draft"
+              expect(capture.broadcast.count).to eq 0
+              expect(Cms::SnsPostLog::LineDeliver.count).to eq 0
+            end
+          end
+        end
+
+        context "post iso-2022-jp mail" do
+          let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/ISO-2022-JP.eml") }
+
+          it "#mail" do
+            capture_line_bot_client do |capture|
+              perform_enqueued_jobs do
+                post url, params: { data: data }, headers: headers
+                expect(Cms::Line::Message.count).to eq 1
+                expect(message.name).to include("[#{mail_handler.name}]")
+                expect(message.templates.first.text).to eq decoded
+              end
+              expect(message.deliver_state).to eq "draft"
+              expect(capture.broadcast.count).to eq 0
+              expect(Cms::SnsPostLog::LineDeliver.count).to eq 0
+            end
+          end
+        end
+      end
+
+      context "disable handler" do
+        let!(:mail_handler) do
+          create(:cms_line_mail_handler, handle_state: "disabled", deliver_condition_state: "broadcast",
+            from_conditions: from_conditions, to_conditions: to_conditions)
+        end
+        let!(:url) { "http://#{site.domain}/#{node.url}mail/#{mail_handler.filename}" }
+
+        context "post utf-8 mail" do
+          let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/UTF-8.eml") }
+
+          it "#mail" do
+            expect { post(url, params: { data: data }, headers: headers) }.to raise_error "404"
+          end
+        end
+
+        context "post iso-2022-jp mail" do
+          let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/ISO-2022-JP.eml") }
+
+          it "#mail" do
+            expect { post url, params: { data: data }, headers: headers }.to raise_error "404"
+          end
+        end
+      end
+
+      context "no handler" do
+        let!(:url) { "http://#{site.domain}/#{node.url}mail/#{unique_id}" }
         let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/UTF-8.eml") }
 
         it "#mail" do
-          capture_line_bot_client do |capture|
-            perform_enqueued_jobs do
-              post url, params: { data: data }
-              expect(Cms::Line::Message.count).to eq 1
-              expect(message.name).to include("[#{mail_handler.name}]")
-              expect(message.templates.first.text).to eq decoded
-            end
-            expect(message.deliver_state).to eq "completed"
-            expect(capture.broadcast.count).to eq 1
-            expect(Cms::SnsPostLog::LineDeliver.count).to eq 1
-          end
+          expect { post url, params: { data: data }, headers: headers }.to raise_error "404"
         end
       end
 
-      context "post iso-2022-jp mail" do
-        let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/ISO-2022-JP.eml") }
-
-        it "#mail" do
-          capture_line_bot_client do |capture|
-            perform_enqueued_jobs do
-              post url, params: { data: data }
-              expect(Cms::Line::Message.count).to eq 1
-              expect(message.name).to include("[#{mail_handler.name}]")
-              expect(message.templates.first.text).to eq decoded
-            end
-            expect(message.deliver_state).to eq "completed"
-            expect(capture.broadcast.count).to eq 1
-            expect(Cms::SnsPostLog::LineDeliver.count).to eq 1
-          end
+      context "from conditions unmatched" do
+        let!(:mail_handler) do
+          create(:cms_line_mail_handler, handle_state: "draft", deliver_condition_state: "broadcast",
+            to_conditions: to_conditions)
         end
-      end
-    end
-
-    context "draft handler" do
-      let!(:mail_handler) do
-        create(:cms_line_mail_handler, handle_state: "draft", deliver_condition_state: "broadcast",
-          from_conditions: from_conditions, to_conditions: to_conditions)
-      end
-      let!(:url) { "http://#{site.domain}/#{node.url}mail/#{mail_handler.filename}" }
-
-      context "post utf-8 mail" do
+        let!(:url) { "http://#{site.domain}/#{node.url}mail/#{mail_handler.filename}" }
         let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/UTF-8.eml") }
 
         it "#mail" do
-          capture_line_bot_client do |capture|
-            perform_enqueued_jobs do
-              post url, params: { data: data }
-              expect(Cms::Line::Message.count).to eq 1
-              expect(message.name).to include("[#{mail_handler.name}]")
-              expect(message.templates.first.text).to eq decoded
-            end
-            expect(message.deliver_state).to eq "draft"
-            expect(capture.broadcast.count).to eq 0
-            expect(Cms::SnsPostLog::LineDeliver.count).to eq 0
-          end
+          expect { post url, params: { data: data }, headers: headers }.to raise_error "from conditions unmatched"
         end
       end
 
-      context "post iso-2022-jp mail" do
-        let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/ISO-2022-JP.eml") }
-
-        it "#mail" do
-          capture_line_bot_client do |capture|
-            perform_enqueued_jobs do
-              post url, params: { data: data }
-              expect(Cms::Line::Message.count).to eq 1
-              expect(message.name).to include("[#{mail_handler.name}]")
-              expect(message.templates.first.text).to eq decoded
-            end
-            expect(message.deliver_state).to eq "draft"
-            expect(capture.broadcast.count).to eq 0
-            expect(Cms::SnsPostLog::LineDeliver.count).to eq 0
-          end
+      context "to conditions unmatched" do
+        let!(:mail_handler) do
+          create(:cms_line_mail_handler, handle_state: "draft", deliver_condition_state: "broadcast",
+            from_conditions: from_conditions)
         end
-      end
-    end
-
-    context "disable handler" do
-      let!(:mail_handler) do
-        create(:cms_line_mail_handler, handle_state: "disabled", deliver_condition_state: "broadcast",
-          from_conditions: from_conditions, to_conditions: to_conditions)
-      end
-      let!(:url) { "http://#{site.domain}/#{node.url}mail/#{mail_handler.filename}" }
-
-      context "post utf-8 mail" do
+        let!(:url) { "http://#{site.domain}/#{node.url}mail/#{mail_handler.filename}" }
         let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/UTF-8.eml") }
 
         it "#mail" do
-          expect { post url, params: { data: data } }.to raise_error "404"
+          expect { post url, params: { data: data }, headers: headers }.to raise_error "to conditions unmatched"
         end
-      end
-
-      context "post iso-2022-jp mail" do
-        let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/ISO-2022-JP.eml") }
-
-        it "#mail" do
-          expect { post url, params: { data: data } }.to raise_error "404"
-        end
-      end
-    end
-
-    context "no handler" do
-      let!(:url) { "http://#{site.domain}/#{node.url}mail/#{unique_id}" }
-      let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/UTF-8.eml") }
-
-      it "#mail" do
-        expect { post url, params: { data: data } }.to raise_error "404"
-      end
-    end
-
-    context "from conditions unmatched" do
-      let!(:mail_handler) do
-        create(:cms_line_mail_handler, handle_state: "draft", deliver_condition_state: "broadcast",
-          to_conditions: to_conditions)
-      end
-      let!(:url) { "http://#{site.domain}/#{node.url}mail/#{mail_handler.filename}" }
-      let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/UTF-8.eml") }
-
-      it "#mail" do
-        expect { post url, params: { data: data } }.to raise_error "from conditions unmatched"
-      end
-    end
-
-    context "to conditions unmatched" do
-      let!(:mail_handler) do
-        create(:cms_line_mail_handler, handle_state: "draft", deliver_condition_state: "broadcast",
-          from_conditions: from_conditions)
-      end
-      let!(:url) { "http://#{site.domain}/#{node.url}mail/#{mail_handler.filename}" }
-      let(:data) { Fs.binread("#{Rails.root}/spec/fixtures/mail_page/UTF-8.eml") }
-
-      it "#mail" do
-        expect { post url, params: { data: data } }.to raise_error "to conditions unmatched"
       end
     end
   end
