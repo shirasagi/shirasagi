d53898b9ca2c708b104f33d64d7900350e7dcb93
itowtips <ito@web-tips.co.jp>
2023-08-18 11:33:45 +0900

    [fix] cms change_state_all : backup's user

--- spec/features/cms/pages/change_state_all_spec.rb
+++ spec/features/cms/pages/change_state_all_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "cms_page_pages", type: :feature, dbscope: :example do
   let(:site) { cms_site }
+  let(:user) { cms_user }
   let!(:page1) { create(:cms_page) }
   let!(:page2) { create(:cms_page) }
   let!(:page3) { create(:cms_page) }
@@ -31,6 +32,15 @@ describe "cms_page_pages", type: :feature, dbscope: :example do
       expect(page2.state).to eq "closed"
       expect(page3.state).to eq "closed"
 
+      expect(page1.backups.size).to eq 2
+      expect(page1.backups.first.user_id).to eq user.id
+
+      expect(page2.backups.size).to eq 2
+      expect(page2.backups.first.user_id).to eq user.id
+
+      expect(page3.backups.size).to eq 2
+      expect(page3.backups.first.user_id).to eq user.id
+
       wait_event_to_fire("ss:checked-all-list-items") { find('.list-head input[type="checkbox"]').set(true) }
       within ".list-head-action-update" do
         click_button I18n.t('ss.links.make_them_public')
@@ -46,6 +56,15 @@ describe "cms_page_pages", type: :feature, dbscope: :example do
       expect(page1.state).to eq "public"
       expect(page2.state).to eq "public"
       expect(page3.state).to eq "public"
+
+      expect(page1.backups.size).to eq 3
+      expect(page1.backups.first.user_id).to eq user.id
+
+      expect(page2.backups.size).to eq 3
+      expect(page2.backups.first.user_id).to eq user.id
+
+      expect(page3.backups.size).to eq 3
+      expect(page3.backups.first.user_id).to eq user.id
     end
   end
 end
