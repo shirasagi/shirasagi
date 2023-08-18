f80b3a2255658eb4a0170f1264d6bea3f7f974fd
itowtips <ito@web-tips.co.jp>
2023-08-18 09:46:40 +0900

    [fix] hotfix for chartjs-plugin-colorschemes-v3.js (#5002)

--- app/assets/javascripts/inquiry/chart.js
+++ app/assets/javascripts/inquiry/chart.js
@@ -1,6 +1,5 @@
 //= require chart.js/dist/chart.js
 //= require chartjs-plugin-datalabels/dist/chartjs-plugin-datalabels.js
-//= require chartjs-plugin-colorschemes-v3/dist/chartjs-plugin-colorschemes-v3.js
 
 this.Inquiry_Chart = (function() {
   function Inquiry_Chart() {}
