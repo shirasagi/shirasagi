0228267cdbb564520f61efc88d519e473a53689c
itowtips <ito@web-tips.co.jp>
2023-08-18 09:45:47 +0900

    [fix] hotfix for chartjs-plugin-colorschemes-v3.js

--- app/assets/javascripts/ss/chart.js
+++ app/assets/javascripts/ss/chart.js
@@ -1,3 +1,2 @@
 //= require chart.js/dist/chart.js
 //= require chartjs-plugin-datalabels/dist/chartjs-plugin-datalabels.js
-//= require chartjs-plugin-colorschemes-v3/dist/chartjs-plugin-colorschemes-v3.js
