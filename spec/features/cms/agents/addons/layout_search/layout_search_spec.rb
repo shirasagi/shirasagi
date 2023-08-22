b7d089ba140f3261e8b1ec32c3cb28738158bc50
NAKANO Hideo <nakano@web-tips.co.jp>
2023-08-22 16:00:05 +0900

    [modify] refactor specs

--- spec/features/cms/agents/addons/layout_search/layout_search_spec.rb
+++ spec/features/cms/agents/addons/layout_search/layout_search_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 
 describe 'レイアウト検索', type: :feature, dbscope: :example, js: true do
-	
   let(:site) { cms_site }
   let(:node) { create :cms_node }
   let(:user) { create :cms_test_user, group_ids: cms_user.group_ids, cms_role_ids: cms_user.cms_role_ids }
   let(:item) { create :cms_layout, filename: "#{node.filename}/name" }
-  let(:layout_name) { unique_id }
-  subject(:search_index_path)  { cms_search_contents_pages_path node }
-  
+  let(:search_index_path) { cms_search_contents_pages_path(site: site) }
+  let(:layout_show_path) { cms_layout_path(site: site, id: item) }
+
   before do
     dummy_item = create(:cms_layout, filename: "#{node.filename}/dummy_name")
     html1 = "<div>html1</div>"
@@ -19,40 +18,19 @@ describe 'レイアウト検索', type: :feature, dbscope: :example, js: true do
       layout_id: item.id, html: html1)
     create(
       :cms_page, cur_site: site, user: user, name: "[TEST]B", filename: "B.html", state: "public",
-      layout_id: dummy_item.id,  html: html1)
+      layout_id: dummy_item.id,  html: html2)
 
     login_cms_user
   end
 
   context "レイアウト詳細からサイト内検索へ" do
-
-    subject(:layout_index_path)  { cms_layouts_path node }
-    subject(:layout_show_path) { "#{layout_index_path}/#{item.id}" }
-		 
     it do
       visit layout_show_path
-      expect(current_path).to eq layout_show_path
-      expect(page).to have_content ("レイアウト使用ページを検索する")
-
       click_on I18n.t("modules.addons.cms/layout_search/btn")
       expect(current_path).to eq search_index_path
 
-      click_button I18n.t('ss.buttons.search')
-      expect(page).to have_css(".search-count", text: "1 件の検索結果")
-      expect(page).to have_css("div.info a.title", text: "[TEST]A")
-    end
-  end
-
-  context "サイト内検索でレイアウト使用ページを検索" do
-    it "search with layout" do
-      visit search_index_path
-      wait_cbox_open { click_on I18n.t("cms.apis.layouts.index") }
-      wait_for_cbox do
-        click_on item.name
-      end
-      click_button I18n.t('ss.buttons.search')
-
-      expect(page).to have_css(".search-count", text: "1 件の検索結果")
+      click_on I18n.t('ss.buttons.search')
+      expect(page).to have_css(".search-count", text: I18n.t("cms.search_contents_count", count: 1))
       expect(page).to have_css("div.info a.title", text: "[TEST]A")
     end
   end
