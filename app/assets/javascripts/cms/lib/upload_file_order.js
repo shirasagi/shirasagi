53f22c5b7d74291c4a6e5ee3a294c6c7ed39c969
NAKANO Hideo <nakano@web-tips.co.jp>
2023-08-22 11:54:11 +0900

    [#4711][fix][cms] corrupted with multiple uploaders / frees.

--- app/assets/javascripts/cms/lib/upload_file_order.js
+++ app/assets/javascripts/cms/lib/upload_file_order.js
@@ -1,4 +1,5 @@
-Cms_UploadFileOrder = function (addonName, btnClass) {
+Cms_UploadFileOrder = function (el, addonName, btnClass) {
+  this.$el = $(el);
   this.addonName = addonName;
   this.btnClass = btnClass;
   this.render();
@@ -6,19 +7,31 @@ Cms_UploadFileOrder = function (addonName, btnClass) {
 
 Cms_UploadFileOrder.prototype.render = function () {
   var _this = this;
-  $(this.btnClass).on('click', function () {
+  _this.$el.on('click', _this.btnClass, function () {
+    this.disabled = true;
+
     var selectedVal = $(this).val();
     _this.changeFileOrder(selectedVal);
+
+    if (selectedVal === 'upload') {
+      SS.notice(i18next.t('ss.notice.ordered_by_file_upload_order'));
+    } else {
+      // name
+      SS.notice(i18next.t('ss.notice.ordered_by_file_name_order'));
+    }
+
+    this.disabled = false;
   });
 };
 
 Cms_UploadFileOrder.prototype.changeFileOrder = function (selectedVal) {
-  var $filesEl = $('.file-view').sort(function (a, b) {
+  var $filesEl = this.$el.find('.file-view').sort(function (a, b) {
     if (selectedVal === 'upload') {
       if (this.addonName === 'file') {
         a = $(a).attr('id').replace(/[^0-9]/g, '');
         b = $(b).attr('id').replace(/[^0-9]/g, '');
       } else {
+        // columsForm
         a = $(a).attr('data-file-id');
         b = $(b).attr('data-file-id');
       }
@@ -34,8 +47,9 @@ Cms_UploadFileOrder.prototype.changeFileOrder = function (selectedVal) {
 
 Cms_UploadFileOrder.prototype.appendOrderedFiles = function ($filesEl) {
   if (this.addonName === 'file') {
-    $('#selected-files').append($filesEl);
+    this.$el.find('#selected-files').append($filesEl);
   } else {
-    $('.column-value-files').append($filesEl);
+    // columsForm
+    this.$el.find('.column-value-files').append($filesEl);
   }
 };
