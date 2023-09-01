0a9f14e6b4535ec58db8d8f7aa6d63dad738f5b1
user <user@localhost.localdomain>
2023-09-01 11:16:19 +0900

    宛先変更　ファイルをコピーして新規作成　メッセージのみ反応するように

---
+++ app/controllers/gws/memo/apis/shared_multi_checkboxes_controller.rb
@@ -0,0 +1,53 @@
+class Gws::Memo::Apis::SharedMultiCheckboxesController < ApplicationController
+    include Gws::ApiFilter
+
+    MAX_ITEMS_PER_PAGE = 50
+
+    model Gws::SharedAddress::Address
+
+    before_action :set_fragment
+    before_action :set_group
+    before_action :set_multi
+    before_action :set_inherit_params
+
+    private
+
+    def set_fragment
+      @fragment = params[:fragment].to_s
+    end
+
+    def set_group
+      if params[:s].present? && params[:s][:group].present?
+        @address_group = Gws::SharedAddress::Group.find(params[:s][:group]) rescue nil
+      end
+
+      @groups = Gws::SharedAddress::Group.site(@cur_site).readable(@cur_user, site: @cur_site)
+    end
+
+    def set_multi
+      @multi = params[:single].blank?
+    end
+
+    def set_inherit_params
+      @inherit_keys = [:single]
+    end
+
+    public
+
+    def index
+      s_params = params[:s] || {}
+      s_params[:address_group_id] = @address_group.id if @address_group.present?
+
+      s_group_params = params[:s_group] || {}
+
+      @items = @model.site(@cur_site).
+        and_has_member.
+        readable(@cur_user, site: @cur_site).
+        without_deleted.
+        search(s_params).
+        page(params[:page]).
+        per(MAX_ITEMS_PER_PAGE)
+
+      @group_items = @groups.search(s_group_params).page(params[:group_page]).per(MAX_ITEMS_PER_PAGE)
+    end
+  end
