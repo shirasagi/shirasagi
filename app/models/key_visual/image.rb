322389ae01a0e762749bcf3591bac11c359e46d5
itowtips <ito@web-tips.co.jp>
2023-09-15 17:52:50 +0900

    [add] key_visual/image : remark

--- app/models/key_visual/image.rb
+++ app/models/key_visual/image.rb
@@ -11,12 +11,14 @@ class KeyVisual::Image
   set_permission_name "key_visual_images"
 
   field :link_url, type: String
+  field :remark, type: String
 
   belongs_to_file :file
 
   validates :file_id, presence: true
+  validates :remark, length: { maximum: 400 }
 
-  permit_params :link_url
+  permit_params :link_url, :remark
 
   after_generate_file :generate_relation_public_file, if: ->{ public? }
   after_remove_file :remove_relation_public_file
