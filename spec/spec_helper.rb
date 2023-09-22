930be7e805944f7a47d77bdba04594d7dac5963e
root <root@localhost.localdomain>
2023-09-22 18:27:50 +0900

    不要な変更を削除

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,17 +23,6 @@ require 'support/ss/capybara_support'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
-# スクリーンショットを全画面で撮る
-def take_full_page_screenshot(path)
-  width  = Capybara.page.execute_script("return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);")
-  height = Capybara.page.execute_script("return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);")
-
-  window = Capybara.current_session.driver.browser.manage.window
-  window.resize_to(width+100, height+100)
-
-  page.save_screenshot path
-end
-
 def ci?
   ENV["CI"] == "true"
 end
