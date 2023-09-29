fce188b6445fd468f837c53bc8cbc67750974518
NAKANO Hideo <nakano@web-tips.co.jp>
2023-09-29 10:42:35 +0900

    [modify] refactor w/ RuboCop

--- spec/features/gws/monitor/admins_spec.rb
+++ spec/features/gws/monitor/admins_spec.rb
@@ -46,7 +46,7 @@ describe "gws_monitor_admins", type: :feature, dbscope: :example do
       end
       expect(page).to have_css('#notice', text: I18n.t('ss.notice.saved'))
 
-      expect(current_path).not_to eq copy_gws_monitor_admin_path(item1.id,site)
+      expect(current_path).not_to eq copy_gws_monitor_admin_path(item1.id, site)
       expect(page).to have_content("copy_sample")
 
       expect(Gws::Monitor::Topic.all.count).to eq 2
