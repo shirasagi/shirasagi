9bf3f891e5765dcc264bb80c1950abebd60d4eae
itowtips <ito@web-tips.co.jp>
2023-10-16 11:53:18 +0900

    [modify] SS_Emoji improvement

--- app/assets/javascripts/ss/lib/emoji.js
+++ app/assets/javascripts/ss/lib/emoji.js
@@ -13,62 +13,66 @@ this.SS_Emoji = (function () {
 
   SS_Emoji.render = function () {
     $(".js-emoji").each(function () {
-      var inputer = this;
-      var opener = $('<a href="" class="open-emoji-picker btn">絵文字</a>');
-      var picker = $('<div class="emoji-picker" style="display: none;"></div>');
+      var $inputer = $(this);
+      var $opener = $('<a href="" class="open-emoji-picker btn">絵文字</a>');
+      var $picker = $('<div class="emoji-picker" style="display: none;"></div>');
+      var $actions = $('<div class="actions"></div>');
+      var $palettes = $('<div class="palettes"></div>');
+      var $closer = $('<span class="material-icons md-18 close-emoji-picker">close</span>');
+      var selectionStart = null;
 
-      var actions = $('<div class="actions"></div>');
-      var palettes = $('<div class="palettes"></div>');
-      var closer = $('<span class="material-icons md-18 close-emoji-picker">close</span>');
-
-      if ($(inputer).hasClass("js-inline")) {
-        opener.addClass("inline");
+      if ($inputer.hasClass("js-inline")) {
+        $opener.addClass("inline");
       } else {
-        opener.addClass("block");
+        $opener.addClass("block");
       }
 
-      actions.append(closer);
+      $actions.append($closer);
       $.each(SS_Emoji.palettes, function (category, emojis) {
-        var tab = $('<a class="emoji-tab">' + category + '</a>');
-        var palette = $('<div class="emoji-palette"></div>');
+        var $tab = $('<a class="emoji-tab">' + category + '</a>');
+        var $palette = $('<div class="emoji-palette"></div>');
 
-        $(tab).attr("data-category", category);
-        $(palette).attr("data-category", category);
+        $tab.attr("data-category", category);
+        $palette.attr("data-category", category);
 
-        palette.hide();
-        actions.append(tab);
-        palettes.append(palette);
+        $palette.hide();
+        $actions.append($tab);
+        $palettes.append($palette);
 
         $(emojis).each(function (i) {
           var item = $('<div class="emoji-item">').text(this);
           $(item).on("click", function(){
-            var text = $(inputer).val();
-            text += $(this).text();
-            $(inputer).val(text);
+            var text = $inputer.val();
+            var before = text.substr(0, selectionStart);
+            var after = text.substr(selectionStart, text.length);
+            $inputer.val(before + $(this).text() + after);
             return false;
           });
-          $(palette).append(item);
+          $palette.append(item);
         });
       });
 
-      palettes.find(".emoji-palette:first").show();
-      actions.find(".emoji-tab:first").addClass("active");
+      $palettes.find(".emoji-palette:first").show();
+      $actions.find(".emoji-tab:first").addClass("active");
 
-      picker.append(actions);
-      picker.append(palettes);
+      $picker.append($actions);
+      $picker.append($palettes);
 
-      $(inputer).after(opener);
-      $(opener).after(picker);
+      $inputer.after($opener);
+      $opener.after($picker);
 
       // events
-      opener.on("click", function() {
+      $inputer.on("focusout", function(ev) {
+        selectionStart = ev.target.selectionStart;
+      });
+
+      $opener.on("click", function() {
         $(".emoji-picker").hide();
-        picker.show();
+        $picker.show();
 
         var handler = function(e) {
-          var target = $(e.target).closest(".emoji-picker");
-          if (!target.length || !target.is(picker)) {
-            picker.hide();
+          if (!$(e.target).closest(".emoji-picker").length && !$(e.target).is($inputer)) {
+            $picker.hide();
             $("body").unbind('click', handler);
             return false;
           }
@@ -77,16 +81,16 @@ this.SS_Emoji = (function () {
         return false;
       });
 
-      closer.on("click", function() {
+      $closer.on("click", function() {
         $(this).closest(".emoji-picker").hide();
         return false;
       });
 
-      actions.find(".emoji-tab").on("click", function() {
-        actions.find(".emoji-tab").removeClass("active");
+      $actions.find(".emoji-tab").on("click", function() {
+        $actions.find(".emoji-tab").removeClass("active");
         $(this).addClass("active");
-        palettes.find(".emoji-palette").hide();
-        palettes.find('.emoji-palette[data-category="' + $(this).data("category") + '"]').show();
+        $palettes.find(".emoji-palette").hide();
+        $palettes.find('.emoji-palette[data-category="' + $(this).data("category") + '"]').show();
         return false;
       });
     });
