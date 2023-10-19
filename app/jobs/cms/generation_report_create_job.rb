4162677fe250603630318d2051a358cb3437facf
NAKANO Hideo <nakano@web-tips.co.jp>
2023-10-19 11:42:13 +0900

    [add][cms] generation report creator job

---
+++ app/jobs/cms/generation_report_create_job.rb
@@ -0,0 +1,159 @@
+class Cms::GenerationReportCreateJob < Cms::ApplicationJob
+  include Job::SS::Binding::Task
+
+  self.task_class = Cms::Task
+
+  def perform
+    if !::File.exist?(task.perf_log_file_path) || ::File.size(task.perf_log_file_path).zero?
+      Rails.logger.info { "#{task.perf_log_file_path} is not found" }
+      return
+    end
+
+    title
+    Rails.logger.tagged(::File.basename(task.perf_log_file_path)) do
+      Zlib::GzipReader.open(task.perf_log_file_path) do |gz|
+        gz.each_line do |line|
+          line.strip!
+          performance_info = JSON.parse(line)
+          import_performance_info(performance_info)
+        end
+      end
+
+      aggregate_histories
+    rescue => e
+      Rails.logger.error { e.to_s }
+      raise
+    end
+  end
+
+  private
+
+  def generation_type
+    @generation_type ||= begin
+      case task.name
+      when "cms:generate_pages"
+        :page
+      when "cms:generate_nodes"
+        :node
+      end
+    end
+  end
+
+  def title
+    @title ||= create_title!
+  end
+
+  def pending_relation
+    @pending_relation ||= {}
+  end
+
+  def id_part_map
+    @id_part_map ||= Cms::Part.all.site(site).to_a.index_by(&:id)
+  end
+
+  def id_layout_map
+    @id_layout_map ||= Cms::Layout.all.site(site).to_a.index_by(&:id)
+  end
+
+  def id_page_map
+    @id_page_map ||= Cms::Page.all.site(site).to_a.index_by(&:id)
+  end
+
+  def id_node_map
+    @id_node_map ||= Cms::Node.all.site(site).to_a.index_by(&:id)
+  end
+
+  def create_title!
+    title_name = "Performance Report at #{I18n.l(task.started, format: :long)}"
+    if generation_type
+      title_name = "Pages Generation #{title_name}"
+    else
+      title_name = "Nodes Generation #{title_name}"
+    end
+    title = Cms::GenerationReport::Title.new(
+      cur_site: site, name: title_name, task: task, sha256_hash: Cms::GenerationReport.sha256_hash(task.perf_log_file_path))
+    title.save!
+    title
+  end
+
+  def import_performance_info(performance_info)
+    case performance_info["type"]
+    when "header"
+      # ignore
+    when "part"
+      content = id_part_map[performance_info["id"]]
+    when "page"
+      content = id_page_map[performance_info["id"]]
+    when "node"
+      content = id_node_map[performance_info["id"]]
+    when "layout"
+      content = id_layout_map[performance_info["id"]]
+    when "site"
+      if performance_info["id"] == site.id
+        content = site
+      end
+    else
+      Rails.logger.debug { "#{performance_info["type"]}: unknown type" }
+    end
+    return unless content
+
+    history = Cms::GenerationReport::History[title].new(
+      cur_site: site, site: site, task: task, title: title, history_type: performance_info["type"],
+      content: content, content_name: performance_info["name"], content_filename: performance_info["filename"],
+      db: performance_info["db"], view: performance_info["view"], elapsed: performance_info["elapsed"])
+    history.save!
+
+    pending_histories = pending_relation.delete(performance_info.slice("type", "id"))
+    if pending_histories.present?
+      pending_histories.each do |pending_history|
+        pending_history.update(parent: history)
+      end
+      history.update(child_ids: pending_histories.map(&:id))
+    end
+
+    scope = performance_info["scopes"].try(:last)
+    if scope.present?
+      scope = scope.slice("type", "id")
+      pending_relation[scope] ||= []
+      pending_relation[scope] << history
+    end
+  end
+
+  def aggregate_histories
+    stages = []
+    stages << {
+      "$group" => {
+        _id: {
+          history_type: "$history_type",
+          content_id: "$content_id",
+          content_type: "$content_type",
+          content_name: "$content_name",
+          content_filename: "$content_filename",
+        },
+        count: { "$sum" => 1 }, total_db: { "$sum" => "$db" }, total_view: { "$sum" => "$view" }, total_elapsed: { "$sum" => "$elapsed" }
+      }
+    }
+    stages << {
+      "$addFields" => {
+        average_db: { "$divide" => [ "$total_db", "$count" ] },
+        average_view: { "$divide" => [ "$total_view", "$count" ] },
+        average_elapsed: { "$divide" => [ "$total_elapsed", "$count" ] },
+      }
+    }
+
+    results = Cms::GenerationReport::History[title].collection.aggregate(stages)
+    results.to_a.each do |result|
+      # 複数回レンダリングされるパーツとレイアウトは集計する意味があるが、通常1度しかレンダリングされないページとノードは集計する意味がない
+      next unless %(part layout).include?(result["_id"]["history_type"])
+
+      aggregation = Cms::GenerationReport::Aggregation[title].new(
+        cur_site: site, site: site, task: task, title: title, history_type: result["_id"]["history_type"],
+        content_id: result["_id"]["content_id"], content_type: result["_id"]["content_type"],
+        content_name: result["_id"]["content_name"], content_filename: result["_id"]["content_filename"], count: result["count"],
+        total_db: result["total_db"], total_view: result["total_view"], total_elapsed: result["total_elapsed"],
+        average_db: result["average_db"], average_view: result["average_view"], average_elapsed: result["average_elapsed"]
+      )
+      aggregation.save!
+    end
+  end
+end
