87cef7477be0e3b2db3282fbeb2271b6ea7c6857
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2023-11-06 19:46:23 +0900

    [modify] event node search facilities

--- app/controllers/event/agents/parts/search_controller.rb
+++ app/controllers/event/agents/parts/search_controller.rb
@@ -3,6 +3,11 @@ class Event::Agents::Parts::SearchController < ApplicationController
   helper Event::EventHelper
 
   def index
-    @categories = Cms::Node.site(@cur_site).where({:id.in => @cur_part.cate_ids}).sort(filename: 1)
+    @categories = Cms::Node.site(@cur_site).in(id: @cur_part.cate_ids).sort(order: 1)
+    @event_pages = Cms::Page.site(@cur_site).and_public(@cur_date).exists(event_dates: 1)
+
+    facility_ids = @event_pages.pluck(:facility_id, :facility_ids).flatten.compact
+    @facilities = Facility::Node::Page.site(@cur_site).and_public.
+      in(id: facility_ids).order_by(order: 1, kana: 1, name: 1).to_a
   end
 end
