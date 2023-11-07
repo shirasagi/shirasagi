fb6dc06022196a5b76b225e2d24f05f01465bbe5
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2023-11-07 09:52:30 +0900

    [modify] event node search facilities (#5092)

--- app/controllers/event/agents/nodes/search_controller.rb
+++ app/controllers/event/agents/nodes/search_controller.rb
@@ -9,9 +9,18 @@ class Event::Agents::Nodes::SearchController < ApplicationController
   def index
     @categories = []
     @items = []
-    if @cur_node.parent
-      @categories = Cms::Node.site(@cur_site).where({:id.in => @cur_node.parent.st_category_ids}).sort(filename: 1)
+    st_category_ids = @cur_node.parent.try(:st_category_ids)
+    if st_category_ids.present?
+      @categories = Cms::Node.site(@cur_site).in(id: st_category_ids).sort(order: 1)
     end
+    @event_pages = Cms::Page.public_list(site: @cur_site, node: @cur_node, date: @cur_date).
+      where('event_dates.0' => { "$exists" => true })
+    facility_ids = @event_pages.pluck(:facility_id, :facility_ids).flatten.compact
+    @facilities = Facility::Node::Page.site(@cur_site).
+      and_public(@cur_date).
+      in(id: facility_ids).
+      order_by(order: 1, kana: 1, name: 1).
+      to_a
     if @keyword.present? || @category_ids.present? || @start_date.present? || @close_date.present? || @facility_ids.present?
       list_events
     end
@@ -20,7 +29,7 @@ class Event::Agents::Nodes::SearchController < ApplicationController
   private
 
   def set_params
-    safe_params = params.permit(:search_keyword, :facility_name, category_ids: [], event: [ :start_date, :close_date])
+    safe_params = params.permit(:search_keyword, :facility_id, category_ids: [], event: [ :start_date, :close_date])
     @keyword = safe_params[:search_keyword].presence
     @category_ids = safe_params[:category_ids].presence || []
     @category_ids = @category_ids.map(&:to_i)
@@ -30,12 +39,9 @@ class Event::Agents::Nodes::SearchController < ApplicationController
     end
     @start_date = Date.parse(@start_date) if @start_date.present?
     @close_date = Date.parse(@close_date) if @close_date.present?
-    @facility_name = safe_params[:facility_name].presence
-    if @facility_name.present?
-      @facility_ids = Facility::Node::Page.site(@cur_site).
-        where(name: /#{::Regexp.escape(@facility_name)}/).
-        and_public(@cur_date).
-        pluck(:id)
+    @facility_id = safe_params[:facility_id].presence
+    if @facility_id.present?
+      @facility_ids = Facility::Node::Page.site(@cur_site).where(id: @facility_id).and_public.pluck(:id)
     end
   end
 
@@ -44,7 +50,7 @@ class Event::Agents::Nodes::SearchController < ApplicationController
     criteria = criteria.search(keyword: @keyword) if @keyword.present?
     criteria = criteria.where(@cur_node.condition_hash)
     criteria = criteria.in(category_ids: @category_ids) if @category_ids.present?
-    criteria = criteria.in(facility_ids: @facility_ids) if @facility_name.present?
+    criteria = criteria.in(facility_ids: @facility_ids) if @facility_ids.present?
 
     if @start_date.present? && @close_date.present?
       criteria = criteria.search(dates: @start_date..@close_date)
