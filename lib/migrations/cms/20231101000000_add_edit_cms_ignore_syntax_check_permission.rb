cbfda47680e7495640769dc078b1d4b3d1cafb68
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2023-11-07 21:00:51 +0900

    [modify] migration edit_cms_ignore_syntax_check

--- lib/migrations/cms/20231101000000_add_edit_cms_ignore_syntax_check_permission.rb
+++ lib/migrations/cms/20231101000000_add_edit_cms_ignore_syntax_check_permission.rb
@@ -4,10 +4,6 @@ class SS::Migration20231101000000
   depends_on "20230410000004"
 
   def change
-    ::Cms::Role.each do |role|
-      next if role.created > Time.zone.parse("2023/11/01")
-
-      role.add_to_set(permissions: %w(edit_cms_ignore_syntax_check))
-    end
+    ::Cms::Role.all.add_to_set(permissions: 'edit_cms_ignore_syntax_check')
   end
 end
