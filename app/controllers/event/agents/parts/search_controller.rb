7fcb53a6e3c976c87ced949e6acb588ae717e814
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2023-11-10 16:48:33 +0900

    [modify] event node search index criteria (#5099)

--- app/controllers/event/agents/parts/search_controller.rb
+++ app/controllers/event/agents/parts/search_controller.rb
@@ -3,11 +3,17 @@ class Event::Agents::Parts::SearchController < ApplicationController
   helper Event::EventHelper
 
   def index
-    @categories = Cms::Node.site(@cur_site).in(id: @cur_part.cate_ids).sort(order: 1)
-    @event_pages = Cms::Page.site(@cur_site).and_public(@cur_date).exists(event_dates: 1)
+    @categories = Category::Node::Base.public_list(site: @cur_site, date: @cur_date).order_by(order: 1)
+    if @cur_part.cate_ids.present?
+      @categories = @categories.in(id: @cur_part.cate_ids)
+    end
+    @event_pages = Cms::Page.public_list(site: @cur_site, ate: @cur_date).
+      where('event_dates.0' => { "$exists" => true })
 
     facility_ids = @event_pages.pluck(:facility_id, :facility_ids).flatten.compact
-    @facilities = Facility::Node::Page.site(@cur_site).and_public.
-      in(id: facility_ids).order_by(order: 1, kana: 1, name: 1).to_a
+    @facilities = Facility::Node::Page.public_list(site: @cur_site, date: @cur_date).
+      in(id: facility_ids).
+      order_by(order: 1, kana: 1, name: 1).
+      to_a
   end
 end
