940bcdc1d771914559ef9cd84d4c84e48a1807e7
tany <tanizawa@web-tips.co.jp>
2023-12-10 20:26:37 +0900

    [add] rspec

--- spec/features/gws/schedule/search/times_spec.rb
+++ spec/features/gws/schedule/search/times_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 describe "gws_schedule_search_times", type: :feature, dbscope: :example, js: true do
-  let(:site) { gws_site }
-  let(:path) { gws_schedule_search_times_path site }
+  let!(:site) { gws_site }
+  let!(:path) { gws_schedule_search_times_path site }
   let!(:facility) { create(:gws_facility_item) }
+  let!(:facility2) { create(:gws_facility_item) }
+  let!(:facility3) { create(:gws_facility_item) }
+  let!(:count) { 2 }
 
   context "with auth", js: true do
     before { login_gws_user }
@@ -26,6 +29,36 @@ describe "gws_schedule_search_times", type: :feature, dbscope: :example, js: tru
       expect(page).to have_content(gws_user.name)
       expect(page).to have_content(facility.name)
       expect(page).to have_content(gws_user.model_name.human)
+    end
+  end
+
+  context "with auth", js: true do
+    before do
+      login_gws_user
+      @save_config = SS.config.gws.schedule
+      config = { "search_times" => { 'max_facilities' => count } }
+      SS.config.replace_value_at(:gws, :schedule, @save_config.merge(config))
+    end
+
+    after do
+      SS.config.replace_value_at(:gws, :schedule, @save_config)
+    end
+
+    it "over facilities limit" do
+      visit path
+      wait_cbox_open { click_on I18n.t("gws.apis.facilities.index") }
+      wait_for_cbox do
+        find('#colorbox .index .list-head .checkbox input').click
+        find('.select-items').click
+      end
+      within "form.search" do
+        first('input[type=submit]').click
+      end
+
+      within "#errorExplanation" do
+        expect(page).to have_text(I18n.t('gws.errors.plan_search.max_facilities', count: count))
+      end
+      expect(page).to have_selector('.gws-schedule-search-facilities tbody tr', count: count)
     end
   end
 end
--- spec/features/gws/schedule/search/users_spec.rb
+++ spec/features/gws/schedule/search/users_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 describe "gws_schedule_search_users", type: :feature, dbscope: :example do
-  let(:user) { gws_user }
-  let(:site) { gws_site }
-  let(:path) { gws_schedule_search_users_path site }
+  let!(:user) { gws_user }
+  let!(:site) { gws_site }
+  let!(:path) { gws_schedule_search_users_path site }
+  let!(:user2) { create :gws_user, group_ids: gws_user.group_ids }
+  let!(:user3) { create :gws_user, group_ids: gws_user.group_ids }
+  let!(:count) { 2 }
 
   context "with auth", js: true do
     before { login_gws_user }
@@ -19,6 +22,31 @@ describe "gws_schedule_search_users", type: :feature, dbscope: :example do
       within ".calendar-multiple-header" do
         expect(page).to have_content(user.name)
       end
+    end
+  end
+
+  context "with auth", js: true do
+    before do
+      login_gws_user
+      @save_config = SS.config.gws.schedule
+      SS.config.replace_value_at(:gws, :schedule, @save_config.merge({ "search_users" => { 'max_users' => count } }))
+    end
+
+    after do
+      SS.config.replace_value_at(:gws, :schedule, @save_config)
+    end
+
+    it "over users limit" do
+      visit path
+      within "form.search" do
+        fill_in "s[keyword]", with: '@'
+        click_button I18n.t('ss.buttons.search')
+      end
+
+      within "#errorExplanation" do
+        expect(page).to have_text(I18n.t('gws.errors.plan_search.max_users', count: count))
+      end
+      expect(page).to have_selector('.calendar-multiple-header', count: count)
     end
   end
 end
