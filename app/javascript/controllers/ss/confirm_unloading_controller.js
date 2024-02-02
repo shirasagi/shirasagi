ea2758513577f987e56000e5bc5d10abf092abc3
NAKANO Hideo <nakano@web-tips.co.jp>
2024-02-02 11:48:46 +0900

    [modify] SS.confirmUnloading() を何度呼び出しても安全になるように修正 (#5147)
    * [modify] SS.confirmUnloading() を何度呼び出しても安全になるように修正。
    
    * [modify] remove unused method
    
    * [fixup] SS.confirmUnloading() を何度呼び出しても安全になるように修正。

---
+++ app/javascript/controllers/ss/confirm_unloading_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+
+function isInputText(el) {
+  return el.tagName === "INPUT" && el.type === "text";
+}
+
+function isTextArea(el) {
+  return el.tagName === "TEXTAREA";
+}
+
+function isSelect(el) {
+  return el.tagName === "SELECT";
+}
+
+function isSubmit(el) {
+  return (el.tagName === "INPUT" || el.tagName === "BUTTON") && el.type === "submit";
+}
+
+export default class extends Controller {
+  connect() {
+    $(this.element).on("change", (ev) => {
+      if (isInputText(ev.target) || isTextArea(ev.target) || isSelect(ev.target)) {
+        SS.formChanged = new Date().getTime();
+      }
+    }).on("click", (ev) => {
+      if (isSubmit(ev.target)) {
+        SS.formChanged = undefined;
+      }
+    });
+  }
+}
