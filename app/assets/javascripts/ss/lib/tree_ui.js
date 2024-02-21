8732d045a79633374db19886bc3ff87875534451
itowtips <ito@web-tips.co.jp>
2024-02-21 15:08:14 +0900

    [wip][modify] tree_ui

--- app/assets/javascripts/ss/lib/tree_ui.js
+++ app/assets/javascripts/ss/lib/tree_ui.js
@@ -1,37 +1,33 @@
 this.SS_TreeUI = (function () {
-  SS_TreeUI.openImagePath = "/assets/img/tree-open.png";
-
-  SS_TreeUI.closeImagePath = "/assets/img/tree-close.png";
-
   SS_TreeUI.render = function (tree, opts) {
     return new SS_TreeUI(tree, opts);
   };
 
-  SS_TreeUI.toggleImage = function (img) {
-    if (img.attr("src") === SS_TreeUI.openImagePath) {
-      return SS_TreeUI.closeImage(img);
-    } else if (img.attr("src") === SS_TreeUI.closeImagePath) {
-      return SS_TreeUI.openImage(img);
+  SS_TreeUI.toggle = function (el) {
+    if (el.hasClass("opened")) {
+      SS_TreeUI.close(el)
+    } else if (el.hasClass("closed")) {
+      SS_TreeUI.open(el)
     }
   };
 
-  SS_TreeUI.openImage = function (img) {
-    img.attr("src", SS_TreeUI.openImagePath);
-    img.addClass("opened");
-    return img.removeClass("closed");
+  SS_TreeUI.open = function (el) {
+    el.addClass("opened");
+    el.removeClass("closed");
+    el.attr("aria-expanded", "true");
   };
 
-  SS_TreeUI.closeImage = function (img) {
-    img.attr("src", SS_TreeUI.closeImagePath);
-    img.removeClass("opened");
-    return img.addClass("closed");
+  SS_TreeUI.close = function (el) {
+    el.removeClass("opened");
+    el.addClass("closed");
+    el.attr("aria-expanded", "false");
   };
 
   SS_TreeUI.openSelectedGroupsTree = function (current_tr) {
     current_tr.addClass("current");
     for (i = 0; i < parseInt(current_tr.attr("data-depth")); i++) {
       var tr = current_tr.prevAll('tr[data-depth=' + i.toString() + ']:first');
-      var img = tr.find(".toggle:first");
+      var el = tr.find("a.toggle:first");
       tr.nextAll("tr").each(function () {
         var subordinated_depth = parseInt($(this).attr("data-depth"));
         if (i >= subordinated_depth) {
@@ -41,7 +37,7 @@ this.SS_TreeUI = (function () {
           $(this).show();
         }
       });
-      SS_TreeUI.openImage(img);
+      SS_TreeUI.open(el);
     }
   }
 
@@ -59,7 +55,8 @@ this.SS_TreeUI = (function () {
     var expand_group = opts["expand_group"];
 
     self.tree.find("tbody tr").each(function () {
-      return root.push(parseInt($(this).attr("data-depth")));
+      $(this).uniqueId();
+      root.push(parseInt($(this).attr("data-depth")));
     });
     root = Math.min.apply(null, root);
     root = parseInt(root);
@@ -70,7 +67,7 @@ this.SS_TreeUI = (function () {
       var d, depth, i, j, ref, ref1, td;
       td = $(this).find(".expandable");
       depth = parseInt($(this).attr("data-depth"));
-      td.prepend('<img src="' + SS_TreeUI.closeImagePath + '" alt="toggle" class="toggle closed">');
+      td.prepend('<a class="toggle closed" href="#"></a>');
       if (depth !== root) {
         if (!expand_all) {
           $(this).hide();
@@ -89,12 +86,30 @@ this.SS_TreeUI = (function () {
       e.stopPropagation();
       return false;
     });
-    self.tree.find(".toggle").on("click", function (e) {
-      var depth, img, tr;
+
+    // aria
+    self.tree.find("a.toggle").each(function () {
+      var ids = [];
       tr = $(this).closest("tr");
-      img = tr.find(".toggle:first");
       depth = parseInt(tr.attr("data-depth"));
-      SS_TreeUI.toggleImage(img);
+      tr.nextAll("tr").each(function () {
+        var d, i, id;
+        d = parseInt($(this).attr("data-depth"));
+        id = $(this).attr("id");
+        i = $(this).find(".toggle:first");
+        if (depth < d) {
+          ids.push(id)
+        }
+      });
+      $(this).attr("aria-controls", ids.join(" "));
+    });
+
+    self.tree.find(".toggle").on("click", function (e) {
+      var depth, el, tr;
+      tr = $(this).closest("tr");
+      el = tr.find("a.toggle:first");
+      depth = parseInt(tr.attr("data-depth"));
+      SS_TreeUI.toggle(el);
       tr.nextAll("tr").each(function () {
         var d, i;
         d = parseInt($(this).attr("data-depth"));
@@ -104,10 +119,10 @@ this.SS_TreeUI = (function () {
         }
         if ((depth + 1) === d) {
           $(this).toggle();
-          return SS_TreeUI.closeImage(i);
+          SS_TreeUI.close(i);
         } else {
           $(this).hide();
-          return SS_TreeUI.closeImage(i);
+          SS_TreeUI.close(i);
         }
       });
       e.stopPropagation();
@@ -119,23 +134,23 @@ this.SS_TreeUI = (function () {
       });
     }
     if (expand_all) {
-      SS_TreeUI.openImage(self.tree.find("tbody tr img"));
+      SS_TreeUI.open(self.tree.find("tbody tr a.toggle"));
     } else if (collapse_all) {
-      SS_TreeUI.closeImage(self.tree.find("tbody tr img"));
+      SS_TreeUI.close(self.tree.find("tbody tr a.toggle"));
     } else if (expand_group && $("tbody tr.current").attr("data-depth") !== "0") {
       SS_TreeUI.openSelectedGroupsTree(self.tree.find("tbody tr[data-id='" + expand_group + "']"));
     } else {
-      self.tree.find("tr[data-depth='" + root + "'] img").trigger("click");
+      self.tree.find("tr[data-depth='" + root + "'] a.toggle").trigger("click");
     }
   }
 
   SS_TreeUI.prototype.expandAll = function () {
-    return this.tree.find("tr img.toggle.closed").trigger("click");
+    this.tree.find("tr a.toggle.closed").trigger("click");
   };
 
   SS_TreeUI.prototype.collapseAll = function () {
-    return $(this.tree.find("tr img.toggle.opened").get().reverse()).each(function () {
-      return $(this).trigger("click");
+    $(this.tree.find("tr a.toggle.opened").get().reverse()).each(function () {
+      $(this).trigger("click");
     });
   };
 
@@ -166,6 +181,3 @@ this.SS_TreeUI = (function () {
   return SS_TreeUI;
 
 })();
-
-// ---
-// generated by coffee-script 1.9.2
