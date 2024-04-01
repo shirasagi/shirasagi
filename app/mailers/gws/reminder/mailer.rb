b76fe9ce870e35f6b13368867a86f39f2a8ccb47
itowtips <ito@web-tips.co.jp>
2024-04-01 10:31:04 +0900

    [order] (6) 転送先メールアドレス : GMAILや個人のメールに送れないようにする

--- app/mailers/gws/reminder/mailer.rb
+++ app/mailers/gws/reminder/mailer.rb
@@ -8,7 +8,8 @@ class Gws::Reminder::Mailer < ApplicationMailer
 
     from = site.sender_address
     to = format_email(reminder.user)
-    return nil if to.blank?
+    to = site.exclude_disallowed_email(to)
+    return false if to.blank?
 
     subject = I18n.t(
       "gws/reminder.notification.subject",
