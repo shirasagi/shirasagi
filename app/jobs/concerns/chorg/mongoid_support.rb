1160c3824169436b635d5f2073c9c4528dd03be3
NAKANO Hideo <nakano@web-tips.co.jp>
2024-04-12 20:16:46 +0900

    [fix] mongodb cursor error

--- app/jobs/concerns/chorg/mongoid_support.rb
+++ app/jobs/concerns/chorg/mongoid_support.rb
@@ -55,30 +55,35 @@ module Chorg::MongoidSupport
   # rubocop:disable Layout::EmptyLineBetweenDefs
   def with_entities(models, scope = {})
     models.each do |model|
-      model.where(scope).each do |entity|
-        entity = entity.try(:becomes_with_topic) || entity
-        entity.try(:cur_site=, @cur_site)
-        entity.try(:allow_other_user_files)
+      criteria = model.where(scope)
+      all_ids = criteria.pluck(:id)
+      all_ids.each_slice(20) do |ids|
+        entities = criteria.in(id: ids).to_a
+        entities.each do |entity|
+          entity = entity.try(:becomes_with_topic) || entity
+          entity.try(:cur_site=, @cur_site)
+          entity.try(:allow_other_user_files)
 
-        entity_user = (entity.try(:user) || @cur_user)
-        entity_user.try(:cur_site=, @cur_site)
-        entity.try(:cur_user=, entity_user)
+          entity_user = (entity.try(:user) || @cur_user)
+          entity_user.try(:cur_site=, @cur_site)
+          entity.try(:cur_user=, entity_user)
 
-        entity.try(:skip_twitter_post=, true)
-        entity.try(:skip_line_post=, true)
-        def entity.post_to_line(execute: :inline); end
-        def entity.post_to_twitter(execute: :inline); end
+          entity.try(:skip_twitter_post=, true)
+          entity.try(:skip_line_post=, true)
+          def entity.post_to_line(execute: :inline); end
+          def entity.post_to_twitter(execute: :inline); end
 
-        entity.try(:skip_assoc_opendata=, true)
-        def entity.invoke_opendata_job(action); end
+          entity.try(:skip_assoc_opendata=, true)
+          def entity.invoke_opendata_job(action); end
 
-        entity.instance_variable_set(:@base_model, model)
-        def entity.base_model
-          return @base_model
+          entity.instance_variable_set(:@base_model, model)
+          def entity.base_model
+            return @base_model
+          end
+
+          entity.move_changes
+          yield entity
         end
-
-        entity.move_changes
-        yield entity
       end
     end
   end
