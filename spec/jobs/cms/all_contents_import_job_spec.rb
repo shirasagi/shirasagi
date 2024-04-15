3be544f3d87418d5cc32c97afa57d8972f130621
NAKANO Hideo <nakano@web-tips.co.jp>
2024-04-15 10:47:51 +0900

    [fixup] keep timestamps after executed chorg

--- spec/jobs/cms/all_contents_import_job_spec.rb
+++ spec/jobs/cms/all_contents_import_job_spec.rb
@@ -26,6 +26,7 @@ describe Cms::AllContentsImportJob, dbscope: :example do
       Timecop.freeze(now - 2.weeks) do
         node = create(
           :article_node_page, cur_site: site, layout_id: layout.id, category_ids: [ cate.id ], group_ids: [ cms_group.id ])
+        expect(node.backups.count).to eq 1
         Cms::Node.find(node.id)
       end
     end
@@ -60,6 +61,20 @@ describe Cms::AllContentsImportJob, dbscope: :example do
           expect(updated_node.filename).to eq node.filename
           expect(updated_node.layout_id).to eq layout1.id
           expect(updated_node.updated.in_time_zone).to eq node.updated.in_time_zone
+
+          backups = updated_node.backups.to_a
+          expect(backups).to have(2).items
+          backups[0].tap do |backup|
+            expect(backup.state).to eq "current"
+            expect(backup.data["name"]).to eq name
+            expect(backup.data["index_name"]).to eq index_name
+            expect(backup.data["layout_id"]).to eq layout1.id
+            expect(backup.data["updated"].in_time_zone).to eq updated_node.updated.in_time_zone
+          end
+          backups[1].tap do |backup|
+            expect(backup.state).to eq "before"
+            expect(backup.data["updated"].in_time_zone).to eq node.updated.in_time_zone
+          end
         end
       end
     end
@@ -92,6 +107,21 @@ describe Cms::AllContentsImportJob, dbscope: :example do
           expect(updated_node.shortcut).to eq shortcut
           expect(updated_node.view_route).to eq view_route
           expect(updated_node.updated.in_time_zone).to eq node.updated.in_time_zone
+
+          backups = updated_node.backups.to_a
+          expect(backups).to have(2).items
+          backups[0].tap do |backup|
+            expect(backup.state).to eq "current"
+            expect(backup.data["page_layout_id"]).to eq page_layout1.id
+            expect(backup.data["order"]).to eq order
+            expect(backup.data["shortcut"]).to eq shortcut
+            expect(backup.data["view_route"]).to eq view_route
+            expect(backup.data["updated"].in_time_zone).to eq updated_node.updated.in_time_zone
+          end
+          backups[1].tap do |backup|
+            expect(backup.state).to eq "before"
+            expect(backup.data["updated"].in_time_zone).to eq node.updated.in_time_zone
+          end
         end
       end
     end
@@ -121,6 +151,20 @@ describe Cms::AllContentsImportJob, dbscope: :example do
           expect(updated_node.description).to eq description
           expect(updated_node.summary_html).to eq summary_html
           expect(updated_node.updated.in_time_zone).to eq node.updated.in_time_zone
+
+          backups = updated_node.backups.to_a
+          expect(backups).to have(2).items
+          backups[0].tap do |backup|
+            expect(backup.state).to eq "current"
+            expect(backup.data["keywords"]).to eq keywords
+            expect(backup.data["description"]).to eq description
+            expect(backup.data["summary_html"]).to eq summary_html
+            expect(backup.data["updated"].in_time_zone).to eq updated_node.updated.in_time_zone
+          end
+          backups[1].tap do |backup|
+            expect(backup.state).to eq "before"
+            expect(backup.data["updated"].in_time_zone).to eq node.updated.in_time_zone
+          end
         end
       end
     end
@@ -175,6 +219,28 @@ describe Cms::AllContentsImportJob, dbscope: :example do
           expect(updated_node.no_items_display_state).to eq no_items_display_state
           expect(updated_node.substitute_html).to eq substitute_html.join("\r\n")
           expect(updated_node.updated.in_time_zone).to eq node.updated.in_time_zone
+
+          backups = updated_node.backups.to_a
+          expect(backups).to have(2).items
+          backups[0].tap do |backup|
+            expect(backup.state).to eq "current"
+            expect(backup.data["conditions"]).to eq conditions
+            expect(backup.data["sort"]).to eq sort
+            expect(backup.data["limit"]).to eq limit
+            expect(backup.data["new_days"]).to eq new_days
+            expect(backup.data["loop_format"]).to eq loop_format
+            expect(backup.data["upper_html"]).to eq upper_html.join("\r\n")
+            expect(backup.data["loop_html"]).to eq loop_html.join("\r\n")
+            expect(backup.data["lower_html"]).to eq lower_html.join("\r\n")
+            expect(backup.data["loop_liquid"]).to eq loop_liquid.join("\r\n")
+            expect(backup.data["no_items_display_state"]).to eq no_items_display_state
+            expect(backup.data["substitute_html"]).to eq substitute_html.join("\r\n")
+            expect(backup.data["updated"].in_time_zone).to eq updated_node.updated.in_time_zone
+          end
+          backups[1].tap do |backup|
+            expect(backup.state).to eq "before"
+            expect(backup.data["updated"].in_time_zone).to eq node.updated.in_time_zone
+          end
         end
       end
     end
@@ -198,6 +264,18 @@ describe Cms::AllContentsImportJob, dbscope: :example do
         Cms::Node.find(node.id).tap do |updated_node|
           expect(updated_node.st_category_ids).to eq [cate1.id]
           expect(updated_node.updated.in_time_zone).to eq node.updated.in_time_zone
+
+          backups = updated_node.backups.to_a
+          expect(backups).to have(2).items
+          backups[0].tap do |backup|
+            expect(backup.state).to eq "current"
+            expect(backup.data["st_category_ids"]).to eq [cate1.id]
+            expect(backup.data["updated"].in_time_zone).to eq updated_node.updated.in_time_zone
+          end
+          backups[1].tap do |backup|
+            expect(backup.state).to eq "before"
+            expect(backup.data["updated"].in_time_zone).to eq node.updated.in_time_zone
+          end
         end
       end
     end
@@ -229,6 +307,22 @@ describe Cms::AllContentsImportJob, dbscope: :example do
             expect(updated_node.released).to eq released
           end
           expect(updated_node.updated.in_time_zone).to eq node.updated.in_time_zone
+
+          backups = updated_node.backups.to_a
+          expect(backups).to have(2).items
+          backups[0].tap do |backup|
+            expect(backup.state).to eq "current"
+            expect(backup.data["state"]).to eq state
+            expect(backup.data["released_type"]).to eq released_type
+            if released_type == "fixed"
+              expect(backup.data["released"]).to eq released
+            end
+            expect(backup.data["updated"].in_time_zone).to eq updated_node.updated.in_time_zone
+          end
+          backups[1].tap do |backup|
+            expect(backup.state).to eq "before"
+            expect(backup.data["updated"].in_time_zone).to eq node.updated.in_time_zone
+          end
         end
       end
     end
@@ -251,6 +345,18 @@ describe Cms::AllContentsImportJob, dbscope: :example do
         Cms::Node.find(node.id).tap do |updated_node|
           expect(updated_node.group_ids).to eq [group1.id]
           expect(updated_node.updated.in_time_zone).to eq node.updated.in_time_zone
+
+          backups = updated_node.backups.to_a
+          expect(backups).to have(2).items
+          backups[0].tap do |backup|
+            expect(backup.state).to eq "current"
+            expect(backup.data["group_ids"]).to eq [group1.id]
+            expect(backup.data["updated"].in_time_zone).to eq updated_node.updated.in_time_zone
+          end
+          backups[1].tap do |backup|
+            expect(backup.state).to eq "before"
+            expect(backup.data["updated"].in_time_zone).to eq node.updated.in_time_zone
+          end
         end
       end
     end
@@ -321,7 +427,7 @@ describe Cms::AllContentsImportJob, dbscope: :example do
             expect(backup.state).to eq "current"
             expect(backup.data["category_ids"]).to eq [cate1.id]
             expect(backup.data["group_ids"]).to eq [group1.id]
-            expect(backup.data["state"]).to eq "ready"
+            expect(backup.data["state"]).to eq "public"
             expect(backup.data["released"].in_time_zone).to eq released.in_time_zone
             expect(backup.data["release_date"].in_time_zone).to eq release_date.in_time_zone
             expect(backup.data["close_date"].in_time_zone).to eq close_date.in_time_zone
