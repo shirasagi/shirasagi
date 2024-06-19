92a91ebc487092f678cac08736a000c2f23b5102
NAKANO Hideo <nakano@web-tips.co.jp>
2024-06-19 19:01:51 +0900

    add use case

--- spec/features/article/pages/delete_on_master_spec.rb
+++ spec/features/article/pages/delete_on_master_spec.rb
@@ -8,24 +8,57 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
   before { login_cms_user }
 
   context "branches are existed" do
-    it do
-      visit article_pages_path(site: site, cid: node)
-      click_on item.name
-      within '#addon-workflow-agents-addons-branch' do
-        wait_for_turbo_frame "#workflow-branch-frame"
-        wait_event_to_fire "turbo:frame-load" do
-          click_on I18n.t("workflow.create_branch")
+    context "on show view" do
+      it do
+        visit article_pages_path(site: site, cid: node)
+        click_on item.name
+        within '#addon-workflow-agents-addons-branch' do
+          wait_for_turbo_frame "#workflow-branch-frame"
+          wait_event_to_fire "turbo:frame-load" do
+            click_on I18n.t("workflow.create_branch")
+          end
+          expect(page).to have_css('.see.branch', text: I18n.t("workflow.notice.created_branch_page"))
+          expect(page).to have_css('table.branches')
+          expect(page).to have_css('.see.branch', text: item.name)
         end
-        expect(page).to have_css('.see.branch', text: I18n.t("workflow.notice.created_branch_page"))
-        expect(page).to have_css('table.branches')
-        expect(page).to have_css('.see.branch', text: item.name)
-      end
 
-      # visit show_path
-      click_on I18n.t("ss.links.delete")
-      expect(page).to have_css(".addon-head", text: I18n.t('workflow.confirm.unable_to_delete_master_page'))
-      within "form" do
-        expect(page).to have_no_css(".send", text: I18n.t("ss.buttons.delete"))
+        # visit show_path
+        click_on I18n.t("ss.links.delete")
+        expect(page).to have_css(".addon-head", text: I18n.t('workflow.confirm.unable_to_delete_master_page'))
+        within "form" do
+          expect(page).to have_no_css(".send", text: I18n.t("ss.buttons.delete"))
+        end
+      end
+    end
+
+    context "on index view (#5195)" do
+      it do
+        visit article_pages_path(site: site, cid: node)
+        click_on item.name
+        within '#addon-workflow-agents-addons-branch' do
+          wait_for_turbo_frame "#workflow-branch-frame"
+          wait_event_to_fire "turbo:frame-load" do
+            click_on I18n.t("workflow.create_branch")
+          end
+          expect(page).to have_css('.see.branch', text: I18n.t("workflow.notice.created_branch_page"))
+          expect(page).to have_css('table.branches')
+          expect(page).to have_css('.see.branch', text: item.name)
+        end
+
+        visit article_pages_path(site: site, cid: node)
+        within "[data-id='#{item.id}']" do
+          first('[type="checkbox"]').click
+        end
+        wait_for_js_ready
+        within ".list-head-action" do
+          click_on I18n.t("ss.buttons.delete")
+        end
+        within "form" do
+          expect(page).to have_css(".addon-head", text: I18n.t("ss.confirm.target_to_delete"))
+          expect(page).to have_css(".info", text: I18n.t('workflow.confirm.unable_to_delete_master_page'))
+          expect(page).to have_no_css(".check")
+          expect(page).to have_no_css(".send", text: I18n.t("ss.buttons.delete"))
+        end
       end
     end
   end
