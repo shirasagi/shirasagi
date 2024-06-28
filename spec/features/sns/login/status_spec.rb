adc4470c05d671a68e73236ec72b0a88171e36d4
NAKANO Hideo <nakano@web-tips.co.jp>
2024-06-28 12:00:28 +0900

    [add] more status check spec

---
+++ spec/features/sns/login/status_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "sns_login", type: :feature, dbscope: :example, js: true do
+  let(:user) { sys_user }
+
+  it do
+    visit sns_login_path
+    within "form" do
+      fill_in "item[email]", with: user.email
+      fill_in "item[password]", with: user.in_password.presence || "pass"
+      click_on I18n.t("ss.login")
+    end
+    expect(page).to have_css(".user .user-name", text: user.name)
+    expect(page.evaluate_script("document.body.hasAttribute('data-ss-session')")).to be_falsey
+
+    # disable redirecting to login
+    page.execute_script("SS_Login.loginPath = null;")
+
+    wait_for_event_fired("ss:sessionAlive") do
+      page.execute_script("SS_Login.loggedinCheck();")
+    end
+    expect(page.evaluate_script("document.body.getAttribute('data-ss-session')")).to eq "alive"
+
+    Timecop.travel(Time.zone.now + (SS.session_lifetime_of_user(user) + 1).seconds) do
+      page.accept_alert(I18n.t("ss.warning.session_timeout")) do
+        page.execute_script("SS_Login.loggedinCheck();")
+      end
+      expect(page.evaluate_script("document.body.getAttribute('data-ss-session')")).to eq "expired"
+    end
+  end
+end
