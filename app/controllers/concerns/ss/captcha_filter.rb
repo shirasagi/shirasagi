0694d4af4a8d38ec0a4026c619bb577bd05e6bbe
NAKANO Hideo <nakano@web-tips.co.jp>
2024-07-08 10:40:43 +0900

    [modify] refactor w/ RuboCop

--- app/controllers/concerns/ss/captcha_filter.rb
+++ app/controllers/concerns/ss/captcha_filter.rb
@@ -10,7 +10,7 @@ module SS::CaptchaFilter
     session[:captcha_id] = @cur_captcha.id
 
     if @cur_captcha.captcha_error.blank?
-      title = CGI.escape_html(t "simple_captcha.title")
+      title = CGI.escape_html(I18n.t("simple_captcha.title"))
       h = <<~HTML
         <div class="simple-captcha">
           <div class="image">
@@ -20,7 +20,7 @@ module SS::CaptchaFilter
             <input type="text" name="answer[captcha_answer]" id="answer_captcha_answer" pattern="\d{4}" inputmode="numeric" title="#{title}" >
           </div>
           <div class="captcha-label">
-            #{CGI.escape_html(t "simple_captcha.label")}
+            #{CGI.escape_html(I18n.t("simple_captcha.label"))}
           </div>
         </div>
       HTML
@@ -29,13 +29,13 @@ module SS::CaptchaFilter
 
     if show_specific_error
       h = <<~HTML
-        <p>#{CGI.escape_html(t "simple_captcha.captcha_error")}</p>
+        <p>#{CGI.escape_html(I18n.t("simple_captcha.captcha_error"))}</p>
         <p>#{CGI.escape_html(@cur_captcha.captcha_error)}</p>
       HTML
 
       return h.html_safe
     else
-      return "<p>#{CGI.escape_html(t "simple_captcha.captcha_error")}</p>".html_safe
+      return "<p>#{CGI.escape_html(I18n.t("simple_captcha.captcha_error"))}</p>".html_safe
     end
 
     return
