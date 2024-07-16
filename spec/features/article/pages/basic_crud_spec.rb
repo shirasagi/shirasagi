e06f800cbeadae006c40cb02200d289801b8feb1
m-gulsher <110490599+m-gulsher@users.noreply.github.com>
2024-07-16 10:43:38 +0500

    Issue 5195: (#5296)
    * Issue 5195:
    
    -> Stopping user to deleting the pages having replacment pages.
    -> Rspec implementation.
    
    Signed-off-by: Muhammad Gulsher <muhammadgulsher10@gmail.com>
    
    * Issue 5195:
    
    -> logic fixup for article deletion for branch pages.
    
    Signed-off-by: Muhammad Gulsher <muhammadgulsher10@gmail.com>
    
    * Issue 5195:
    
    -> updated use case
    
    Signed-off-by: Muhammad Gulsher <muhammadgulsher10@gmail.com>
    
    * Signed-off-by: Muhammad Gulsher <muhammadgulsher10@gmail.com>
    
    * add specific use case
    
    * Issue 5195:
    
    -> revaluating the logic to fullfill all use case.
    
    Signed-off-by: Muhammad Gulsher <muhammadgulsher10@gmail.com>
    
    * Issue 5195:
    
    -> branch page should have a false checkbopx if delete_alert_enabled
    -> else it should be there with true checked.
    
    Signed-off-by: Muhammad Gulsher <muhammadgulsher10@gmail.com>
    
    * Issue 5195:
    
    -> possibility fixed (destroy_all not permited and contains_urls)
    
    Additional fix:
    
    -> fixed the test failing because of wrong text because it was changed.
    
    Signed-off-by: Muhammad Gulsher <muhammadgulsher10@gmail.com>
    
    * Signed-off-by: Muhammad Gulsher <muhammadgulsher10@gmail.com>
    
    * Issue 5195:
    
    -> Not allowing user to delete for condition !delete_alert_enabled && contains_urls.present?
    
    Signed-off-by: Muhammad Gulsher <muhammadgulsher10@gmail.com>
    
    * improve styling
    
    * [fix] contains_url problem
    
    * [modify] reduce duplicated codes
    
    * [fix] typo
    
    * add some specific use cases
    
    ---------
    
    Signed-off-by: Muhammad Gulsher <muhammadgulsher10@gmail.com>
    Co-authored-by: NAKANO Hideo <nakano@web-tips.co.jp>

--- spec/features/article/pages/basic_crud_spec.rb
+++ spec/features/article/pages/basic_crud_spec.rb
@@ -110,7 +110,7 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
     context "#delete" do
       let(:user) { cms_user }
 
-      it "permited and contains_urls" do
+      it "permitted and contains_urls" do
         visit delete_path2
         expect(page).to have_css(".delete")
         within "form" do
@@ -119,7 +119,7 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
         wait_for_notice I18n.t('ss.notice.deleted')
       end
 
-      it "permited and not contains_urls" do
+      it "permitted and not contains_urls" do
         visit delete_path
         expect(page).to have_css(".delete")
         within "form" do
@@ -128,7 +128,7 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
         wait_for_notice I18n.t('ss.notice.deleted')
       end
 
-      it "not permited and contains_urls" do
+      it "not permitted and contains_urls" do
         role = user.cms_roles[0]
         role.update(permissions: %w(delete_private_article_pages delete_other_article_pages))
         visit delete_path2
@@ -136,7 +136,7 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
         expect(page).to have_css(".addon-head", text: I18n.t('ss.confirm.contains_url_expect'))
       end
 
-      it "not permited and not contains_urls" do
+      it "not permitted and not contains_urls" do
         role = user.cms_roles[0]
         role.update(permissions: %w(delete_private_article_pages delete_other_article_pages))
         visit delete_path
@@ -147,7 +147,7 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
         wait_for_notice I18n.t('ss.notice.deleted')
       end
 
-      it "destroy_all not permited and contains_urls" do
+      it "destroy_all not permitted and contains_urls" do
         role = user.cms_roles[0]
         role_permissions = role.permissions.map do |permission|
           next if permission == "delete_cms_ignore_alert"
@@ -160,8 +160,18 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
         within ".list-head-action" do
           click_button I18n.t('ss.buttons.delete')
         end
-        expect(page).to have_content I18n.t('ss.confirm.contains_links_in_file')
-        expect(page).to have_content I18n.t('ss.confirm.target_to_delete')
+        expect(page).to have_css("h2", text: I18n.t('ss.confirm.target_to_delete'))
+        within "[data-id='#{item2.id}']" do
+          expect(page).to have_no_css("[type='checkbox']")
+          expect(page).to have_content I18n.t('ss.confirm.contains_links_in_file')
+        end
+        within "[data-id='#{item3.id}']" do
+          first("[type='checkbox']").tap do |checkbox|
+            expect(checkbox["checked"]).to eq "true"
+            expect(checkbox["disabled"]).to eq "false"
+          end
+          expect(page).to have_content I18n.t('ss.confirm.not_contains_links_in_file')
+        end
         click_button I18n.t('ss.buttons.delete')
         wait_for_ajax
 
@@ -203,7 +213,7 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
     context "#draft_save" do
       let(:user) { cms_user }
 
-      it "permited and contains_urls" do
+      it "permitted and contains_urls" do
         visit edit_path2
         within "form" do
           click_on I18n.t("ss.buttons.withdraw")
@@ -211,7 +221,7 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
         expect(page).to have_css('.save', text: I18n.t('ss.buttons.ignore_alert'))
       end
 
-      it "permited and not contains_urls" do
+      it "permitted and not contains_urls" do
         visit edit_path
         within "form" do
           click_on I18n.t("ss.buttons.withdraw")
@@ -219,7 +229,7 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
         expect(page).to have_css('.save', text: I18n.t('ss.buttons.ignore_alert'))
       end
 
-      it "not permited and contains_urls" do
+      it "not permitted and contains_urls" do
         role = user.cms_roles[0]
         role.update(permissions: %w(edit_private_article_pages edit_other_article_pages
                                     release_private_article_pages release_other_article_pages
@@ -232,7 +242,7 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
         expect(page).to have_css(".errorExplanation", text: I18n.t('ss.confirm.contains_url_expect'))
       end
 
-      it "not permited and not contains_urls" do
+      it "not permitted and not contains_urls" do
         role = user.cms_roles[0]
         role.update(permissions: %w(edit_private_article_pages edit_other_article_pages
                                     release_private_article_pages release_other_article_pages
