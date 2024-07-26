f35add85cb846d1a8741a0c36aebcd4fa2569b62
itowtips <ito@web-tips.co.jp>
2024-07-26 14:35:01 +0900

    [modify] cms/check_links : mail setting

--- app/mailers/cms/mailer.rb
+++ app/mailers/cms/mailer.rb
@@ -7,7 +7,27 @@ class Cms::Mailer < ApplicationMailer
     mail(
       from: site.sender_address,
       to: "#{group.section_name} <#{group.contact_email}>",
-      subject: site.page_expiration_mail_subject.presence || I18n.t("cms.page_expiration_mail.default_subject")
-    )
+      subject: site.page_expiration_mail_subject.presence || I18n.t("cms.page_expiration_mail.default_subject"))
+  end
+
+  def link_errors(site, to, errors)
+    if site.check_links_message_format == "csv"
+      body = "[#{errors.size} errors]\n"
+      if errors.size > 0
+        body += "error details are in the attached csv\n"
+        attachments["errors.csv"] = errors.to_csv
+      end
+    else
+      body = errors.to_message
+    end
+
+    sender_address = site.sender_address
+    sender_address = site.check_links_default_sender_address if sender_address == SS.config.mail.default_from
+    mail(
+      from: sender_address,
+      to: to,
+      subject: "[#{site.name}] Link Check: #{errors.size} errors",
+      body: body,
+      message_id: Cms.generate_message_id(site))
   end
 end
