ce43359042ffc938776bb1923cbf6474efaea2ca
Muhammad Gulsher <muhammadgulsher10@gmail.com>
2024-08-06 02:42:30 +0500

    Import/Export Nodes:
    -> Implemented export functionality on nodes.
    -> Fixed the import functionality according to feedback.
    
    
    Signed-off-by: Muhammad Gulsher <muhammadgulsher10@gmail.com>

---
+++ app/models/cms/node_exporter.rb
@@ -0,0 +1,63 @@
+require 'csv'
+
+class Cms::NodeExporter
+  include ActiveModel::Model
+
+  attr_accessor :mode, :site, :criteria
+
+  def enum_csv(options = {})
+    drawer = SS::Csv.draw(:export, context: self) do |drawer|
+      #adding header once
+      csv_headers.each do |header|
+        drawer.column drawer_columns["#{header}"] do 
+          drawer.head { header }
+        end
+      end
+    end
+
+    drawer.enum(criteria, options)
+  end
+
+  private
+
+  def csv_headers
+    ['ファイル名', 'フォルダー属性', 'タイトル', '一覧用タイトル', '並び順', 'レイアウト', 'ページレイアウト',
+     'ショートカット', '既定のモジュール', 'キーワード', '概要', 'サマリー', '検索条件(URL)', 
+     'リスト並び順', '表示件数', 'NEWマーク期間', 'ループHTML形式', '上部HTML', 'ループHTML(SHIRASAGI形式)', 
+     '下部HTML', 'ループHTML(Liquid形式)', 'ページ未検出時表示', '代替HTML', 'カテゴリー設定', 
+     '公開日時種別', '公開日時', 'ステータス', '管理グループ']
+  end
+
+  def drawer_columns 
+    {
+      'ファイル名' => :filename,
+      'フォルダー属性' => :route,
+      'タイトル' => :name,
+      '一覧用タイトル' => :index_name,
+      '並び順' => :order,
+      'レイアウト' => :layout_filename,
+      'ページレイアウト' => :page_layout_filename,
+      'ショートカット' => :shortcut,
+      '既定のモジュール' => :view_route,
+      'キーワード' => :keywords,
+      '概要' => :description,
+      'サマリー' => :summary_html,
+      '検索条件(URL)' => :conditions,
+      'リスト並び順' => :sort,
+      '表示件数' => :limit,
+      'NEWマーク期間' => :new_days,
+      'ループHTML形式' => :loop_format,
+      '上部HTML' => :upper_html,
+      'ループHTML(SHIRASAGI形式)' => :loop_html,
+      '下部HTML' => :lower_html,
+      'ループHTML(Liquid形式)' => :loop_liquid,
+      'ページ未検出時表示' => :no_items_display_state,
+      '代替HTML' => :substitute_html,
+      'カテゴリー設定' => :category_ids,
+      '公開日時種別' => :released_type,
+      '公開日時' => :released,
+      'ステータス' => :state,
+      '管理グループ' => :group_ids
+    }
+  end
+end
--- app/models/concerns/cms/node_import_base.rb
+++ app/models/concerns/cms/node_import_base.rb
@@ -42,53 +42,44 @@ module Cms::NodeImportBase
   end
 
   def update_record(row)
-    node = find_or_initialize_node(row)
+    item = find_or_initialize_node(row)
 
-    # TODO and Memo:
-    # variable name of "node" is duplicated in this namespace
-    # this is dangerous
-    #
-    # attr_reader's node... node of import from
-    # node =... node of creating now
+    item.name = row['タイトル'] if item.respond_to?(:name)
+    item.index_name = row['一覧用タイトル'] if item.respond_to?(:index_name)
+    item.order = row['並び順'] if item.respond_to?(:order)
 
-    node.name = row['タイトル'] if node.respond_to?(:name)
-    node.index_name = row['一覧用タイトル'] if node.respond_to?(:index_name)
-    node.order = row['並び順'] if node.respond_to?(:order)
+    update_layout(row, item)
+    update_page_layout(row, item)
 
-    update_layout(row, node)
-    update_page_layout(row, node)
+    item.shortcut = row['ショートカット'] if item.respond_to?(:shortcut)
+    item.view_route = row['既定のモジュール'] if item.respond_to?(:view_route)
+    item.keywords = row['キーワード'].split(',').map(&:strip) if row['キーワード'] && item.respond_to?(:keywords)
+    item.description = row['概要'] if item.respond_to?(:description)
+    item.summary_html = row['サマリー'] if item.respond_to?(:summary_html)
+    item.conditions = row['検索条件(URL)'] if item.respond_to?(:conditions)
+    item.sort = row['リスト並び順'] if item.respond_to?(:sort)
+    item.limit = row['表示件数'] if item.respond_to?(:limit)
+    item.new_days = row['NEWマーク期間'] if item.respond_to?(:new_days)
+    item.loop_format = row['ループHTML形式'] if item.respond_to?(:loop_format)
+    item.upper_html = row['上部HTML'] if item.respond_to?(:upper_html)
+    item.loop_html = row['ループHTML(SHIRASAGI形式)'] if item.respond_to?(:loop_html)
+    item.lower_html = row['下部HTML'] if item.respond_to?(:lower_html)
+    item.loop_liquid = row['ループHTML(Liquid形式)'] if item.respond_to?(:loop_liquid)
+    item.no_items_display_state = row['ページ未検出時表示'] if item.respond_to?(:no_items_display_state)
+    item.substitute_html = row['代替HTML'] if item.respond_to?(:substitute_html)
+    item.category_ids = row['カテゴリー設定'].split(',').map(&:strip) if row['カテゴリー設定'] && item.respond_to?(:category_ids)
+    item.released_type = row['公開日時種別'] if item.respond_to?(:released_type)
+    item.released = row['公開日時'] if item.respond_to?(:released)
+    item.state = row['ステータス'] if item.respond_to?(:state)
 
-    node.shortcut = row['ショートカット'] if node.respond_to?(:shortcut)
-    node.view_route = row['既定のモジュール'] if node.respond_to?(:view_route)
-    node.keywords = row['キーワード'].split(',').map(&:strip) if row['キーワード'] && node.respond_to?(:keywords)
-    node.description = row['概要'] if node.respond_to?(:description)
-    node.summary_html = row['サマリー'] if node.respond_to?(:summary_html)
-    # node.summary = row['サマリー'] if node.respond_to?(:summary)
-    node.conditions = row['検索条件(URL)'] if node.respond_to?(:conditions)
-    node.sort = row['リスト並び順'] if node.respond_to?(:sort)
-    node.limit = row['表示件数'] if node.respond_to?(:limit)
-    node.new_days = row['NEWマーク期間'] if node.respond_to?(:new_days)
-    node.loop_format = row['ループHTML形式'] if node.respond_to?(:loop_format)
-    node.upper_html = row['上部HTML'] if node.respond_to?(:upper_html)
-    node.loop_html = row['ループHTML(SHIRASAGI形式)'] if node.respond_to?(:loop_html)
-    node.lower_html = row['下部HTML'] if node.respond_to?(:lower_html)
-    node.loop_liquid = row['ループHTML(Liquid形式)'] if node.respond_to?(:loop_liquid)
-    node.no_items_display_state = row['ページ未検出時表示'] if node.respond_to?(:no_items_display_state)
-    node.substitute_html = row['代替HTML'] if node.respond_to?(:substitute_html)
-    node.category_ids = row['カテゴリー設定'].split(',').map(&:strip) if row['カテゴリー設定'] && node.respond_to?(:category_ids)
-    node.released_type = row['公開日時種別'] if node.respond_to?(:released_type)
-    node.released = row['公開日時'] if node.respond_to?(:released)
-    node.state = row['ステータス'] if node.respond_to?(:state)
+    update_groups(row, item)
 
-    update_groups(row, node)
+    item.save!
 
-    node.save!
-
-    node
+    item
   end
 
   def find_or_initialize_node(row)
-    # TODO and Memo:
     # "ファイル名" in csv that means basename
     # so actually field filename is "parent node's filename / basename"
     #
@@ -103,83 +94,52 @@ module Cms::NodeImportBase
       filename = basename
     end
 
-    # TODO and Memo:
-    # uniq key is field filename and site_id; user_id is not uniq
-    # Cms::Node.find return node instance that instance of correct class (it's use becomes_with_route internally)
-    # try find node from conditions
-    node = Cms::Node.site(@site).where(filename: filename).first
-    if node.nil?
+    object = Cms::Node.site(@site).where(filename: filename).first
+    if object.nil?
 
       # constantize by becomes_with_route
       route = row['フォルダー属性']
-      node = Cms::Node::Base.new(route: route)
-      node = node.becomes_with_route(route)
+      object = Cms::Node::Base.new(route: route)
+      object = object.becomes_with_route(route)
 
       # stop saving node when unknown route given
-      if node.class == Cms::Node::Base
+      if object.class == Cms::Node::Base
         raise "unknown route given (#{route})"
       end
     end
 
-    # old codes:
-    # node = klass.find_or_initialize_by(filename: filename, site_id: @site.id)
-    #class_name = row['フォルダー属性'].split('/').map(&:camelize).join('::Node::')
-    #klass = class_name.constantize
 
-    node.cur_site = @site
-    node.cur_node = @node if @node
-    node.filename = filename
-    node
+    object.cur_site = @site
+    object.cur_node = @node if @node
+    object.filename = filename
+    object
   end
 
-  def update_layout(row, node)
+  def update_layout(row, item)
     return if row['レイアウト'].blank?
 
-    # TODO and Memo:
-    # layout is relation field
-    # avoid exceptions of caused by find error
-    #
-    # set nil relation when layout not found
-    # and continue saving node
-    # these cases validate by "node.save"
     layout_filename_match = row['レイアウト'].match(/\(([^)]+)\)/)
     layout_filename = layout_filename_match[1] if layout_filename_match
     layout = Cms::Layout.find_by(filename: layout_filename) rescue nil
-    node.layout = layout
+    item.layout = layout
   end
 
-  def update_page_layout(row, node)
+  def update_page_layout(row, item)
     return if row['ページレイアウト'].blank?
 
-    # TODO and Memo:
-    # page_layout is relation field
-    # avoid exceptions of caused by find error
-    #
-    # set nil relation when page_layout not found
-    # and continue saving node
-    # these cases validate by "node.save"
     layout_filename_match = row['ページレイアウト'].match(/\(([^)]+)\)/)
     layout_filename = layout_filename_match[1] if layout_filename_match
     layout = Cms::Layout.find_by(filename: layout_filename) rescue nil
-    node.layout = layout
+    item.layout = layout
   end
 
-  def update_groups(row, node)
+  def update_groups(row, item)
     return if row['管理グループ'].blank?
 
-    # TODO and Memo:
-    # group_ids is relation field
-    # avoid exceptions of caused by find error
-    #
-    # set nil relation when groups not found
-    # and continue saving node
-    # these cases validate by "node.save"
-
-    # find all groups belonging to the site, ref Cms::PageImportBase
     @group_name_group_map ||= Cms::Group.all.site(@site).to_a.index_by(&:name)
 
     names = row['管理グループ'].split("\n").map(&:strip)
     groups = names.filter_map { |name| @group_name_group_map[name] }
-    node.group_ids = groups.map(&:id)
+    item.group_ids = groups.map(&:id)
   end
 end
