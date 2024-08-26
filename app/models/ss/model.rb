fd814e646166dcc8430b5a8024bb32fec55395a9
NAKANO Hideo <nakano@web-tips.co.jp>
2024-08-26 17:32:33 +0900

    [modify] able to set record_timestamps globally

--- app/models/ss/model.rb
+++ app/models/ss/model.rb
@@ -1,4 +1,15 @@
 module SS::Model
+  class Current < ActiveSupport::CurrentAttributes
+    attribute :record_timestamps
+
+    resets { self.record_timestamps = true }
+
+    def initialize
+      super
+      self.record_timestamps = true
+    end
+  end
+
   def self.copy_errors(src, dest, prefix: nil)
     src.errors.full_messages.each do |message|
       message = "#{prefix}#{message}" if prefix
@@ -9,5 +20,17 @@ module SS::Model
   def self.container_of(item)
     return unless item
     item.embedded? ? item._parent : item
+  end
+
+  def self.record_timestamps?
+    SS::Model::Current.record_timestamps
+  end
+
+  def self.without_record_timestamps
+    save_record_timestamps = SS::Model::Current.record_timestamps
+    SS::Model::Current.record_timestamps = false
+    yield
+  ensure
+    SS::Model::Current.record_timestamps = save_record_timestamps
   end
 end
