1874370b0af15bb2bb9e862edda1611e2772bb6d
NAKANO Hideo <nakano@web-tips.co.jp>
2024-08-27 17:05:46 +0900

    check individual permissions and refine styling

--- spec/features/article/pages/publish_and_close_permitted_spec.rb
+++ spec/features/article/pages/publish_and_close_permitted_spec.rb
@@ -2,18 +2,16 @@ require 'spec_helper'
 
 describe "article_pages", type: :feature, dbscope: :example, js: true do
   let(:site) { cms_site }
-  let(:node) { create_once :article_node_page, filename: "docs", name: "article" }
-  let(:item) { create(:article_page, cur_node: node) }
+  let!(:group) { create(:cms_group, name: unique_id) }
+  let!(:user1) { create(:cms_user, name: unique_id, email: "#{unique_id}@example.jp", group_ids: [ group.id ]) }
+  let!(:node) { create :article_node_page, filename: "docs", name: "article" }
+  let!(:item1) { create(:article_page, cur_node: node, cur_user: user1, group_ids: user1.group_ids) }
+  let!(:item2) { create(:article_page, cur_node: node, cur_user: cms_user, group_ids: cms_user.group_ids) }
   let(:index_path) { article_pages_path site.id, node }
-  let(:group) { create(:cms_group, name: unique_id) }
-  let(:user1) { create(:cms_user, name: unique_id, email: "#{unique_id}@example.jp", group_ids: [ group.id ]) }
 
-  context "Manipulate Permissions and check accessibilty" do
-
-    before do 
+  context "Manipulate Permissions and check accessibility" do
+    before do
       login_cms_user
-      item.user = user1
-      item.save
 
       cms_user.cms_roles.each do |role|
         role.permissions = role.permissions - ["close_other_article_pages" , "release_other_article_pages"]
@@ -22,26 +20,32 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
 
     end
 
-    it "check make public if not permitted" do 
+    it "check make public if not permitted" do
       visit index_path
-      find("input[type='checkbox'][name='ids[]'][value='#{item.id}']").set(true)
+      wait_for_event_fired("ss:checked-all-list-items") do
+        find('.list-head input[type="checkbox"]').set(true)
+      end
 
       click_button I18n.t("ss.links.make_them_public")
 
       wait_for_ajax
 
-      expect(page).to have_content(I18n.t("ss.confirm.not_allowed_to_publish"))
+      expect(page).to have_css("[data-id='#{item1.id}']", text: I18n.t("ss.confirm.not_allowed_to_publish"))
+      expect(page).to have_css("[data-id='#{item2.id}'] [type='checkbox']")
     end
 
-    it "check make private if not permitted" do 
+    it "check make private if not permitted" do
       visit index_path
-      find("input[type='checkbox'][name='ids[]'][value='#{item.id}']").set(true)
+      wait_for_event_fired("ss:checked-all-list-items") do
+        find('.list-head input[type="checkbox"]').set(true)
+      end
 
       click_button I18n.t("ss.links.make_them_close")
 
       wait_for_ajax
-      
-      expect(page).to have_content(I18n.t("ss.confirm.not_allowed_to_close"))
+
+      expect(page).to have_css("[data-id='#{item1.id}']", text: I18n.t("ss.confirm.not_allowed_to_close"))
+      expect(page).to have_css("[data-id='#{item2.id}'] [type='checkbox']")
     end
   end
 end
