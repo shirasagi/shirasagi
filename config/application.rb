38dcf98cccc30f90e63d12e71675e0c2f80fc078
NAKANO Hideo <nakano@web-tips.co.jp>
2024-09-04 13:02:29 +0900

    set load_defaults to "7.0"

--- config/application.rb
+++ config/application.rb
@@ -62,7 +62,7 @@ module SS
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
 
     config.middleware.delete ActionDispatch::HostAuthorization
 
