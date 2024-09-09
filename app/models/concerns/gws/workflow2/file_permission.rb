3544418374081646bea03a1cf44f42868abfa759
NAKANO Hideo <nakano@web-tips.co.jp>
2024-09-09 17:47:43 +0900

    [add][gws] reroute permission to gws/workflow2 (#5384)

--- app/models/concerns/gws/workflow2/file_permission.rb
+++ app/models/concerns/gws/workflow2/file_permission.rb
@@ -27,8 +27,15 @@ module Gws::Workflow2::FilePermission
   def allowed?(action, user, opts = {})
     user    = user.gws_user
     site    = opts[:site] || @cur_site
-    return true if user.gws_role_permit_any?(site, "use_gws_workflow2") && owned?(user)
-    return true if user.gws_role_permit_any?(site, "#{action}_other_#{self.class.permission_name}")
+
+    case action
+    when :reroute
+      return true if user.gws_role_permit_any?(site, "#{action}_other_#{self.class.permission_name}")
+      return true if user.gws_role_permit_any?(site, "#{action}_private_#{self.class.permission_name}") && self.editable?(user, site: site)
+    else
+      return true if user.gws_role_permit_any?(site, "use_gws_workflow2") && owned?(user)
+      return true if user.gws_role_permit_any?(site, "#{action}_other_#{self.class.permission_name}")
+    end
 
     errors.add :base, :auth_error if opts.fetch(:adds_error, true)
     false
