72f5522c3c945f8055d0fb153d8733a30dddcd15
itowtips <ito@web-tips.co.jp>
2024-09-12 18:15:06 +0900

    [modify] compatibility of #sanitize before rails 7.1 (#5381)
    * [modify] compatibility of ApplicationController.helpers.sanitize before rails 7.1
    
    * [fixup]

--- config/initializers/sanitize.rb
+++ config/initializers/sanitize.rb
@@ -1,4 +1,8 @@
 # Sanitize
 ActionView::Base.sanitized_allowed_tags.merge %w(font strike s u table thead tbody tr th td)
 ActionView::Base.sanitized_allowed_attributes.merge %w(style border color face size align valign
-                                                       cellspacing cellpadding colspan rowspan)
+  cellspacing cellpadding colspan rowspan)
+
+Rails::HTML5::SafeListSanitizer.allowed_tags.merge %w(font strike s u table thead tbody tr th td)
+Rails::HTML5::SafeListSanitizer.allowed_attributes.merge %w(style border color face size align valign
+  cellspacing cellpadding colspan rowspan)
