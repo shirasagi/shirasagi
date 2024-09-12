8ed768251153293b1f6fef1dae5f207a6d7d6faf
itowtips <ito@web-tips.co.jp>
2024-09-12 16:03:31 +0900

    [fixup]

--- config/initializers/action_view.rb
+++
@@ -1,3 +0,0 @@
-# Action View Sanitize Helpers
-# The default, starting in Rails 7.1, is to use an HTML5 parser for sanitization (if it is available, see NOTE below). If you wish to revert back to the previous HTML4 behavior, you can do so by setting the following in your application configuration:
-Rails.application.config.action_view.sanitizer_vendor = Rails::HTML4::Sanitizer
