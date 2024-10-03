863500e81fe258205b63e93ea333dacedc62fe61
itowtips <ito@web-tips.co.jp>
2024-10-03 18:56:00 +0900

    [fix] cms column value youtube set_youtube_id (#5445)
    Co-authored-by: ShinjiTanimoto <tanimoto@web-tips.co.jp>

--- spec/features/workflow/save_as_branch_spec.rb
+++ spec/features/workflow/save_as_branch_spec.rb
@@ -40,6 +40,156 @@ describe "close_confirmation", type: :feature, dbscope: :example, js: true do
         expect(page).to have_no_css(".branch_save")
         expect(page).to have_css(".publish_save[value='#{I18n.t("ss.buttons.publish_save")}']")
       end
+
+      context "with form" do
+        let!(:file) { create :ss_file, user_id: cms_user.id }
+        let!(:form) { create :cms_form, cur_site: site, state: 'public', sub_type: 'entry' }
+
+        let(:column1) do
+          create(:cms_column_text_field, cur_site: site, cur_form: form, required: "optional", order: 1, input_type: 'text')
+        end
+        let(:column2) do
+          create(:cms_column_date_field, cur_site: site, cur_form: form, required: "optional", order: 2)
+        end
+        let(:column3) do
+          create(:cms_column_url_field2, cur_site: site, cur_form: form, required: "optional", order: 3, html_tag: '')
+        end
+        let(:column4) do
+          create(:cms_column_text_area, cur_site: site, cur_form: form, required: "optional", order: 4)
+        end
+        let(:column5) do
+          create(:cms_column_select, cur_site: site, cur_form: form, required: "optional", order: 5)
+        end
+        let(:column6) do
+          create(:cms_column_radio_button, cur_site: site, cur_form: form, required: "optional", order: 6)
+        end
+        let(:column7) do
+          create(:cms_column_check_box, cur_site: site, cur_form: form, required: "optional", order: 7)
+        end
+        let(:column8) do
+          create(:cms_column_file_upload, cur_site: site, cur_form: form, required: "optional", order: 8, file_type: "image")
+        end
+        let(:column9) do
+          create(:cms_column_free, cur_site: site, cur_form: form, required: "optional", order: 9)
+        end
+        let(:column10) do
+          create(:cms_column_headline, cur_site: site, cur_form: form, required: "optional", order: 10)
+        end
+        let(:column11) do
+          create(:cms_column_list, cur_site: site, cur_form: form, required: "optional", order: 11)
+        end
+        let(:column12) do
+          create(:cms_column_table, cur_site: site, cur_form: form, required: "optional", order: 12)
+        end
+        let(:column13) do
+          create(:cms_column_youtube, cur_site: site, cur_form: form, required: "optional", order: 13)
+        end
+        let(:column14) do
+          create(:cms_column_select_page, cur_site: site, cur_form: form, required: "optional", order: 14, node_ids: [node.id])
+        end
+        let(:column_values1) do
+          [
+            column1.value_type.new(column: column1, value: unique_id),
+            column2.value_type.new(column: column2, date: Time.zone.now),
+            column3.value_type.new(column: column3, link_url: unique_id),
+            column4.value_type.new(column: column4, value: unique_id),
+            column5.value_type.new(column: column5, value: column5.select_options.sample),
+            column6.value_type.new(column: column6, value: column6.select_options.sample),
+            column7.value_type.new(column: column7, values: [column7.select_options.sample]),
+            column8.value_type.new(column: column8, file: file),
+            column9.value_type.new(column: column9, value: unique_id, file_ids: [file.id]),
+            column10.value_type.new(column: column10, text: unique_id),
+            column11.value_type.new(column: column11, lists: [unique_id]),
+            column12.value_type.new(column: column12, value: unique_id),
+            column13.value_type.new(column: column13, url: "https://www.youtube.com/watch?v=CSlLndeDc48"),
+            column14.value_type.new(column: column14, value: [ item2.id ])
+          ]
+        end
+        let(:column_values2) do
+          [
+            column1.value_type.new(column: column1, value: unique_id),
+            column2.value_type.new(column: column2, date: Time.zone.now),
+            column3.value_type.new(column: column3, link_url: unique_id),
+            column4.value_type.new(column: column4, value: unique_id),
+            column5.value_type.new(column: column5, value: column5.select_options.sample),
+            column6.value_type.new(column: column6, value: column6.select_options.sample),
+            column7.value_type.new(column: column7, values: [column7.select_options.sample]),
+            column8.value_type.new(column: column8, file: file),
+            column9.value_type.new(column: column9, value: unique_id, file_ids: [file.id]),
+            column10.value_type.new(column: column10, text: unique_id),
+            column11.value_type.new(column: column11, lists: [unique_id]),
+            column12.value_type.new(column: column12, value: unique_id),
+            column13.value_type.new(column: column13, url: "https://www.youtube.com/watch?v=CSlLndeDc48"),
+            column14.value_type.new(column: column14, value: [ item1.id ])
+          ]
+        end
+
+        before do
+          item1.form = form
+          item1.column_values = column_values1
+          item1.save!
+          item1.reload
+
+          item2.form = form
+          item2.column_values = column_values2
+          item2.save!
+          item2.reload
+        end
+
+        it do
+          visit edit_public_item_path
+          # 保存ボタンの再配置完了を待つ手段がないため、代わりに CKEditor　の完了を待つ
+          within ".column-value-cms-column-free" do
+            wait_ckeditor_ready(find(:fillable_field, "item[column_values][][in_wrap][value]"))
+          end
+
+          expect(page).to have_css(".save[value='#{I18n.t("ss.buttons.withdraw")}']")
+          expect(page).to have_css(".branch_save[value='#{I18n.t("cms.buttons.save_as_branch")}']")
+          expect(page).to have_css(".publish_save[value='#{I18n.t("ss.buttons.publish_save")}']")
+
+          within "#item-form" do
+            fill_in "item[name]", with: unique_id
+            within ".column-value-cms-column-free" do
+              fill_in_ckeditor "item[column_values][][in_wrap][value]", with: unique_id
+            end
+
+            wait_cbox_open do
+              click_on I18n.t('cms.buttons.save_as_branch')
+            end
+          end
+          wait_for_cbox do
+            expect(page).to have_css("#alertExplanation")
+            click_on I18n.t("ss.buttons.ignore_alert")
+          end
+          wait_for_notice I18n.t("workflow.notice.created_branch_page")
+
+          item1.reload
+          expect(item1.master?).to be_truthy
+          expect(item1.branches).to have(1).items
+          item1.branches.first.tap do |branch|
+            expect(branch).to be_present
+            expect(branch.branch?).to be_truthy
+            expect(branch.master_id).to eq item1.id
+          end
+
+          within "#addon-workflow-agents-addons-branch" do
+            expect(page).to have_css(".master .branches", text: item1.name)
+          end
+          expect(page).to have_css("#workflow_route", text: I18n.t("mongoid.attributes.workflow/model/route.my_group"))
+        end
+
+        it do
+          visit edit_closed_item_path
+          # 保存ボタンの再配置完了を待つ手段がないため、代わりに CKEditor　の完了を待つ
+          within ".column-value-cms-column-free" do
+            wait_ckeditor_ready(find(:fillable_field, "item[column_values][][in_wrap][value]"))
+          end
+
+          expect(page).to have_css(".save[value='#{I18n.t("ss.buttons.draft_save")}']")
+          expect(page).to have_no_css(".branch_save")
+          expect(page).to have_css(".publish_save[value='#{I18n.t("ss.buttons.publish_save")}']")
+        end
+      end
     end
 
     context "with user1" do
