df472e2c72d049b144d2061e76c2a083c2145bc2
itowtips <ito@web-tips.co.jp>
2024-10-04 14:29:56 +0900

    v1.19.1

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ require_relative "../app/models/ss/config"
 Bundler.require(*Rails.groups)
 
 module SS
-  mattr_reader(:version) { "1.19.0" }
+  mattr_reader(:version) { "1.19.1" }
 
   class Current < ActiveSupport::CurrentAttributes
     attribute :env, :request
