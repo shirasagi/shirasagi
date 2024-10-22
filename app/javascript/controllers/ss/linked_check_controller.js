e97c412172abedc686b401dcad4e9c94b62d69b9
NAKANO Hideo <nakano@web-tips.co.jp>
2024-10-22 11:55:13 +0900

    [fixup][gws][workflow2] alternate approver

--- app/javascript/controllers/ss/linked_check_controller.js
+++ app/javascript/controllers/ss/linked_check_controller.js
@@ -1,20 +1,35 @@
 import { Controller } from "@hotwired/stimulus";
 import {dispatchEvent} from "../../ss/tool";
 
+const connected = new Set();
+
 export default class extends Controller {
   static values = { checked: String, unchecked: String };
   static targets = [ "source", "target" ];
 
   connect() {
-    if (this.sourceTarget && this.targetTarget) {
-      this.sourceTarget.addEventListener("change", (_ev) => this.#linkValue());
+    if (connected.has(this.element)) {
+      return;
     }
-    this.#linkValue();
+
+    connected.add(this.element);
+    if (this.sourceTarget && this.targetTarget) {
+      this.sourceTargets.forEach((sourceTarget) => {
+        sourceTarget.addEventListener("change", (ev) => this.#linkValue(ev.target));
+      })
+
+      const checkedSourceTarget = this.sourceTargets.find((sourceTarget) => sourceTarget.checked)
+      this.#linkValue(checkedSourceTarget || this.sourceTargets[0]);
+    }
   }
 
-  #linkValue() {
-    if (this.sourceTarget && this.targetTarget) {
-      const newValue = this.sourceTarget.checked ? (this.checkedValue || this.sourceTarget.value) : (this.uncheckedValue || '');
+  disconnect() {
+    connected.delete(this.element);
+  }
+
+  #linkValue(sourceTarget) {
+    if (this.targetTarget) {
+      const newValue = sourceTarget.checked ? (this.checkedValue || sourceTarget.value) : (this.uncheckedValue || '');
       if (this.targetTarget.value !== newValue) {
         this.targetTarget.value = newValue;
         dispatchEvent(this.targetTarget, "ss:change");
