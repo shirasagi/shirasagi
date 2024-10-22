a167c892ae9e9ddad3807b21314781bdff39da63
Muhammad Gulsher <muhammadgulsher10@gmail.com>
2024-10-22 22:32:18 +0500

    Issue 3733:
    -> Developed the logic of initiall opening and highlighting the folder via stimulus:
    -> Found a bug :(clicking on down arrow and the link in front of it both were just opning the folders but i fixed the funnction for link to both open and redirect.)
    
    Signed-off-by: Muhammad Gulsher <muhammadgulsher10@gmail.com>

--- app/javascript/controllers/ss/tree_controller.js
+++ app/javascript/controllers/ss/tree_controller.js
@@ -4,13 +4,37 @@ export default class extends Controller {
   static targets = ["details"];
 
   connect() {
+    this.detailsTargets.forEach(details => {
+      const shouldBeOpen = details.dataset.open === "true";
+      if (shouldBeOpen) {
+        details.open = true;
+      }
+
+      const summary = details.querySelector("summary");
+      const shouldBeHighlighted = summary.dataset.highlight === "true";
+      if (shouldBeHighlighted) {
+        summary.classList.add("highlight");
+      }
+    });
+
+
+    this.element.querySelectorAll("li[data-highlight]").forEach(li => {
+      const shouldBeHighlighted = li.dataset.highlight === "true";
+      if (shouldBeHighlighted) {
+        li.classList.add("highlight");
+      }
+    });
   }
 
   toggle(event) {
     const details = event.currentTarget.closest("details");
+ 
+    if (event.target.closest('a')) {
+      return; 
+    }
 
     if (details.open) {
-      details.open = false;  
+      details.open = false;
     } else {
       details.open = true;
     }
