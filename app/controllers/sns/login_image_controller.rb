adebebd9c2e472179ccc1d5f5f096bc23c7263db
tany <tanizawa@web-tips.co.jp>
2024-10-22 21:56:58 +0900

    [add] login_image page (#5063)
    * [add] sns: login_image
    
    * [add] login_image: format json
    
    * [update] rubocop

---
+++ app/controllers/sns/login_image_controller.rb
@@ -0,0 +1,21 @@
+class Sns::LoginImageController < ApplicationController
+  include HttpAcceptLanguage::AutoLocale
+  include Sns::BaseFilter
+
+  skip_before_action :logged_in?
+
+  layout "ss/login"
+
+  def index
+    @hide_ss_layout_header = true
+
+    respond_to do |format|
+      format.html { render }
+      format.json do
+        @model = Sys::Setting
+        @item = @model.first
+        @url = ::File.dirname(sns_mypage_url)
+      end
+    end
+  end
+end
