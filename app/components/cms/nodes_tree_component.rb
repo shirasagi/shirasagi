1a0572be7f16d2aa53db26d3d0f26d51e6340d3a
NAKANO Hideo <nakano@web-tips.co.jp>
2024-10-29 09:57:47 +0900

    load all nodes at once

--- app/components/cms/nodes_tree_component.rb
+++ app/components/cms/nodes_tree_component.rb
@@ -1,78 +1,54 @@
 class Cms::NodesTreeComponent < ApplicationComponent
+  include ActiveModel::Model
   include Cms::NodeHelper
   include SS::CacheableComponent
 
+  attr_accessor :site, :user
+
   self.cache_key = ->{ [ @site.id, @user.id ] }
 
-  def initialize(site:, user:, item: nil, only_children: false, root_items: nil, type: nil)
-    @type = type.presence || 'cms_nodes'
-    @site = site
-    @user = user
-    @item = item
-    @only_children = only_children
-    @root_items = root_items
-    @folders = []
+  class NodeItem
+    include ActiveModel::Model
+
+    attr_accessor :item, :url, :children
+
+    delegate :id, :name, :filename, :depth, to: :item
+
+    def children?
+      children.present?
+    end
   end
 
-  # Move logic dependent on URL helpers to `before_render`
-  def before_render
-    @folders = fetch_folders
+  def root_items
+    @root_items ||= begin
+      items = folders.select { |node| node.depth == 1 }
+      items.map { |node| NodeItem.new(item: node, url: item_url(node), children: child_items(node)) }
+    end
   end
 
   private
 
-  def fetch_folders
-    items = if @only_children && @item
-              child_items
-            else
-              root_items
-            end
-    items_hash(items)
-  end
-
-  def root_items
-    if @root_items.present?
-      Cms::Node.in(id: @root_items).site(@site).
-        allow(:read, @user, site: @site)
-    else
-      Cms::Node.site(@site).where(depth: 1).
-        allow(:read, @user, site: @site)
+  def folders
+    @folders ||= begin
+      criteria = Cms::Node.all
+      criteria = criteria.site(site)
+      criteria = criteria.allow(:read, user, site: site)
+      criteria = criteria.order_by(filename: 1)
+      criteria = criteria.only(:id, :site_id, :name, :filename, :depth, :route, :view_route, :updated)
+      nodes = criteria.to_a
+      nodes.each { |node| node.site = node.cur_site = site }
+      nodes
     end
   end
 
-  def child_items
-    @item.present? ? @item.children.allow(:read, @user, site: @site) : []
-  end
-
-  def items_hash(items)
-    items.map { |item| build_item_hash(item) }.compact.uniq.sort_by { |item| item[:filename].tr('/', "\0") }
-  end
-
-  def build_item_hash(item)
-    return unless item.allowed?(:read, @user, site: @site)
-
-    item_hash = {
-      id: item.id,
-      name: item.name,
-      filename: item.filename,
-      depth: item.depth,
-      url: item_url(item), # This now works because view context is available
-      is_current: @item.present? && item.id == @item.id,
-      is_parent: @item.present? && @item.filename.start_with?("#{item.filename}/"),
-      has_children: item.children.present?
-    }
-
-    if item.children.present?
-      item_hash[:children] = item.children.allow(:read, @user, site: @site).map { |child| build_item_hash(child) }
-    end
-
-    item_hash
+  def child_items(item)
+    prefix_filename = "#{item.filename}/"
+    folders
+      .select { |node| node.filename.start_with?(prefix_filename) && node.depth == item.depth + 1 }
+      .map { |node| NodeItem.new(item: node, url: item_url(node), children: child_items(node)) }
   end
 
   def item_url(item)
-    return node_pages_path(cid: item.id) if @type == 'cms/page'
-    return node_parts_path(cid: item.id) if @type == 'cms/part'
-    return node_layouts_path(cid: item.id) if @type == 'cms/layout' 
-    item.respond_to?(:view_route) ? contents_path(item) : cms_node_nodes_path(cid: item.id)
+    item.respond_to?(:view_route) ? contents_path(item) : cms_node_nodes_path(cid: item)
   end
 end
