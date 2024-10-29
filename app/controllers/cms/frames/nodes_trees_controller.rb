1b6f1d1d0ca18eec6fbff0c87d1bb0c2d6251504
NAKANO Hideo <nakano@web-tips.co.jp>
2024-10-29 09:29:25 +0900

    reduce unnecessary codes in cms/frames/nodes_trees

--- app/controllers/cms/frames/nodes_trees_controller.rb
+++ app/controllers/cms/frames/nodes_trees_controller.rb
@@ -4,13 +4,6 @@ class Cms::Frames::NodesTreesController < ApplicationController
   model Cms::Node
 
   def index
-    @items = @model.site(@cur_site).
-      allow(:read, @cur_user).
-      where(depth: 1).
-      search(params[:s]).
-      order_by(filename: 1).
-      page(params[:page]).per(50)
-    @item = @model.find(params[:id]) if params[:id].present? && params[:id] != '0'
     render :index, layout: false
   end
 end
