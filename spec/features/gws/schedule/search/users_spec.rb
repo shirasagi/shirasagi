efa05936b862f0178bd41c0ca1c3369f83db40ed
itowtips <ito@web-tips.co.jp>
2024-11-12 10:24:08 +0900

    [fix] gws/schedule : search/users event query (#5468)
    * [fix] gws/schedule : search/users event query
    
    * [modify] spec

--- spec/features/gws/schedule/search/users_spec.rb
+++ spec/features/gws/schedule/search/users_spec.rb
@@ -9,6 +9,10 @@ describe "gws_schedule_search_users", type: :feature, dbscope: :example do
   let!(:count) { 2 }
 
   context "with auth", js: true do
+    let!(:item1) { create :gws_schedule_plan, cur_user: user, member_ids: [user.id] }
+    let!(:item2) { create :gws_schedule_plan, cur_user: user2, member_ids: [user2.id] }
+    let!(:item3) { create :gws_schedule_plan, cur_user: user3, member_ids: [user3.id] }
+
     before { login_gws_user }
 
     it "#index" do
@@ -17,10 +21,45 @@ describe "gws_schedule_search_users", type: :feature, dbscope: :example do
         fill_in "s[keyword]", with: user.name
         click_button I18n.t('ss.buttons.search')
       end
-
       wait_for_js_ready
-      within ".calendar-multiple-header" do
-        expect(page).to have_content(user.name)
+      within ".gws-schedule-search" do
+        expect(page).to have_selector(".calendar-multiple-header", count: 1)
+        within ".calendar-multiple-header" do
+          expect(page).to have_content(user.name)
+        end
+        expect(page).to have_css(".fc-content", text: item1.name)
+        expect(page).to have_no_css(".fc-content", text: item2.name)
+        expect(page).to have_no_css(".fc-content", text: item3.name)
+      end
+
+      within "form.search" do
+        fill_in "s[keyword]", with: user2.name
+        click_button I18n.t('ss.buttons.search')
+      end
+      wait_for_js_ready
+      within ".gws-schedule-search" do
+        expect(page).to have_selector(".calendar-multiple-header", count: 1)
+        within ".calendar-multiple-header" do
+          expect(page).to have_content(user2.name)
+        end
+        expect(page).to have_no_css(".fc-content", text: item1.name)
+        expect(page).to have_css(".fc-content", text: item2.name)
+        expect(page).to have_no_css(".fc-content", text: item3.name)
+      end
+
+      within "form.search" do
+        fill_in "s[keyword]", with: user3.name
+        click_button I18n.t('ss.buttons.search')
+      end
+      wait_for_js_ready
+      within ".gws-schedule-search" do
+        expect(page).to have_selector(".calendar-multiple-header", count: 1)
+        within ".calendar-multiple-header" do
+          expect(page).to have_content(user3.name)
+        end
+        expect(page).to have_no_css(".fc-content", text: item1.name)
+        expect(page).to have_no_css(".fc-content", text: item2.name)
+        expect(page).to have_css(".fc-content", text: item3.name)
       end
     end
   end
