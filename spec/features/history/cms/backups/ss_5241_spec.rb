94d5e2718697f1be4df8d4332ca5af21d0b29cc2
NAKANO Hideo <nakano@web-tips.co.jp>
2024-11-20 10:02:41 +0900

    [fix][cms] mergin expired branch issues (#5481)
    * [add] spec to reproduce issues
    
    * [add] model spec to reproduce issues
    
    * [fix] able to show errors on merging branches
    
    * [fixup] able to show errors on merging branches
    
    * [add] extra use case about workflow
    
    * after_save を before_save に変更するのはダメなケースがあることがわかった。別の方法で修正。
    
    * [fixup] after_save を before_save に変更するのはダメなケースがあることがわかった。別の方法で修正。
    
    * [modify] improve stabilities

--- spec/features/history/cms/backups/ss_5241_spec.rb
+++ spec/features/history/cms/backups/ss_5241_spec.rb
@@ -7,6 +7,7 @@ describe "history_cms_backups", type: :feature, dbscope: :example, js: true do
     create(:cms_column_text_field, cur_site: site, cur_form: form, input_type: 'text', required: 'optional', order: 5)
   end
   let!(:node) { create :article_node_page, site: site, st_form_ids: [ form.id ], st_form_default: form }
+  let(:now) { Time.zone.now.change(sec: 0) }
 
   before { login_cms_user }
 
@@ -17,40 +18,62 @@ describe "history_cms_backups", type: :feature, dbscope: :example, js: true do
       #
       # step1: only fill name and save in private
       #
-      visit article_pages_path(site: site, cid: node)
-      click_on I18n.t("ss.links.new")
-      wait_for_all_ckeditors_ready
-      within "form#item-form" do
-        fill_in "item[name]", with: name
-        click_on I18n.t("ss.buttons.draft_save")
+      # 注意: このテストでは時刻が同じ履歴を二つ作成する
+      # 　　　マイグレーションではタイムスタンプを変更しないようにして更新する。
+      # 　　　別々のユーザーが同時にページを変更する可能性もある。
+      # 　　　このようなケースを想定する。
+      #
+      Timecop.freeze(now) do
+        visit article_pages_path(site: site, cid: node)
+        click_on I18n.t("ss.links.new")
+        wait_for_all_ckeditors_ready
+        within "form#item-form" do
+          fill_in "item[name]", with: name
+          click_on I18n.t("ss.buttons.draft_save")
+        end
+        wait_for_notice I18n.t("ss.notice.saved")
+        wait_for_turbo_frame "#workflow-branch-frame"
+        expect(page).to have_css("#workflow_route", text: I18n.t("mongoid.attributes.workflow/model/route.my_group"))
+
+        expect(Article::Page.all.count).to eq 1
+        item = Article::Page.all.first
+        expect(item.name).to eq name
+        expect(item.column_values.count).to eq 0
+        expect(item.state).to eq "closed"
+        backups = item.backups.to_a
+        expect(backups).to have(1).items
+        expect(backups[0].state).to eq 'current'
+        expect(backups[0].created.in_time_zone).to eq now
+        expect(backups[0].data).to include("_id", "name", "route", "form_id")
+        expect(backups[0].data).not_to include("column_values", "column_values_updated")
+        expect(backups[0].restorable?).to be_falsey
       end
-      wait_for_notice I18n.t("ss.notice.saved")
-      wait_for_turbo_frame "#workflow-branch-frame"
-      expect(page).to have_css("#workflow_route", text: I18n.t("mongoid.attributes.workflow/model/route.my_group"))
 
       #
       # step2: add text column and save in private
       #
-      visit article_pages_path(site: site, cid: node)
-      click_on name
-      click_on I18n.t("ss.links.edit")
-      wait_for_all_ckeditors_ready
-      within "form#item-form" do
-        within ".column-value-palette" do
-          wait_for_event_fired("ss:columnAdded") do
-            click_on col1.name
+      Timecop.freeze(now) do
+        visit article_pages_path(site: site, cid: node)
+        click_on name
+        click_on I18n.t("ss.links.edit")
+        wait_for_all_ckeditors_ready
+        within "form#item-form" do
+          within ".column-value-palette" do
+            wait_for_event_fired("ss:columnAdded") do
+              click_on col1.name
+            end
           end
-        end
 
-        within ".column-value-cms-column-textfield" do
-          fill_in "item[column_values][][in_wrap][value]", with: unique_id
-        end
+          within ".column-value-cms-column-textfield" do
+            fill_in "item[column_values][][in_wrap][value]", with: unique_id
+          end
 
-        click_on I18n.t("ss.buttons.draft_save")
+          click_on I18n.t("ss.buttons.draft_save")
+        end
+        wait_for_notice I18n.t("ss.notice.saved")
+        wait_for_turbo_frame "#workflow-branch-frame"
+        expect(page).to have_css("#workflow_route", text: I18n.t("mongoid.attributes.workflow/model/route.my_group"))
       end
-      wait_for_notice I18n.t("ss.notice.saved")
-      wait_for_turbo_frame "#workflow-branch-frame"
-      expect(page).to have_css("#workflow_route", text: I18n.t("mongoid.attributes.workflow/model/route.my_group"))
 
       expect(Article::Page.all.count).to eq 1
       item = Article::Page.all.first
@@ -59,14 +82,28 @@ describe "history_cms_backups", type: :feature, dbscope: :example, js: true do
       expect(item.state).to eq "closed"
       backups = item.backups.to_a
       expect(backups).to have(2).items
+      expect(backups[0].state).to eq 'current'
+      expect(backups[0].created.in_time_zone).to eq now
+      expect(backups[0].data).to include("_id", "name", "route", "form_id")
+      expect(backups[0].data).to include("column_values", "column_values_updated")
+      expect(backups[0].restorable?).to be_falsey
+      expect(backups[1].state).to eq 'before'
+      expect(backups[1].created.in_time_zone).to eq now
+      expect(backups[1].data).not_to eq backups[0].data
+      expect(backups[1].data).to include("_id", "name", "route", "form_id")
+      expect(backups[1].data).not_to include("column_values", "column_values_updated")
+      expect(backups[1].restorable?).to be_truthy
 
       #
       # step3: restore previous one
       #
       visit article_pages_path(site: site, cid: node)
       click_on name
+      wait_for_turbo_frame "#workflow-branch-frame"
+      expect(page).to have_css("#workflow_route", text: I18n.t("mongoid.attributes.workflow/model/route.my_group"))
+      ensure_addon_opened "#addon-history-agents-addons-backup"
       within "#addon-history-agents-addons-backup" do
-        within "[data-id='#{backups.last.id}']" do
+        within "[data-id='#{backups[1].id}']" do
           click_on I18n.t("ss.links.show")
         end
       end
@@ -75,10 +112,12 @@ describe "history_cms_backups", type: :feature, dbscope: :example, js: true do
         click_on I18n.t('history.restore')
       end
 
-      within "form" do
-        click_on I18n.t('history.buttons.restore')
-      end
-      wait_for_notice I18n.t("history.notice.restored")
+      expect do
+        within "form" do
+          click_on I18n.t('history.buttons.restore')
+        end
+        wait_for_notice I18n.t("history.notice.restored")
+      end.to output.to_stdout
 
       expect(Job::Log.count).to eq 1
       Job::Log.first.tap do |log|
