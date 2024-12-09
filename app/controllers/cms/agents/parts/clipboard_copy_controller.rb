a185e73485dedbf26c26db856e2daecefed6381e
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2024-12-09 18:34:27 +0900

    app/controllers/cms/agents/parts/clipboard_copy_controller.rbの修正/公開画面にclipboard_display_nameが表示されるよう修正

--- app/controllers/cms/agents/parts/clipboard_copy_controller.rb
+++ app/controllers/cms/agents/parts/clipboard_copy_controller.rb
@@ -9,9 +9,12 @@ class Cms::Agents::Parts::ClipboardCopyController < ApplicationController
       selector = @cur_part.clipboard_copy_selector.presence || 'title'
     end
 
+    # display_name が空の場合はパーツ名を使用
+    display_name = @cur_part.clipboard_display_name.presence || @cur_part.name
+
     data = { target: target, selector: selector }
     html = view_context.button_tag(
-      @cur_part.name, type: "button", name: "clipboard-copy", class: "btn-ss-clipboard-copy", data: data)
+      display_name, type: "button", name: "clipboard-copy", class: "btn-ss-clipboard-copy", data: data)
     render html: html
   end
 end
