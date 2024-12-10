5caa0f1afe4007e4f3caed4af585c3770f7d91e8
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2024-12-10 17:49:55 +0900

    app/controllers/cms/agents/parts/print_controller.rbの修正/print_display_nameフィールドの入力値またはパーツ名が表示されるように変更

--- app/controllers/cms/agents/parts/print_controller.rb
+++ app/controllers/cms/agents/parts/print_controller.rb
@@ -1,8 +1,17 @@
 class Cms::Agents::Parts::PrintController < ApplicationController
   include Cms::PartFilter::View
-  helper Cms::ListHelper
+  helper Event::EventHelper
+  helper Cms::ArchiveHelper
 
   def index
-    render html: view_context.button_tag(@cur_part.name, type: "button", name: "print", class: "btn-ss-print")
+    # display_name が空の場合はパーツ名を使用
+    print_parts_display_name = @cur_part.print_display_name.presence || @cur_part.name
+
+    render html: view_context.button_tag(
+      print_parts_display_name, # 変数を使用
+      type: "button",
+      name: "print",
+      class: "btn-ss-print"
+    )
   end
 end
