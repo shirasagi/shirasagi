c03c984eb4de5c70b97a6842dba486a6e840c90c
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2024-12-10 17:53:41 +0900

    app/models/concerns/cms/addon/print.rbの修正

--- app/models/concerns/cms/addon/print.rb
+++ app/models/concerns/cms/addon/print.rb
@@ -1,10 +1,12 @@
-module Cms::Addon::Print
-  extend ActiveSupport::Concern
-  extend SS::Addon
+module Cms::Addon
+  module Print
+    extend ActiveSupport::Concern
+    extend SS::Addon
 
-  included do
-    field :print_display_name, type: String
-    permit_params :print_display_name
-    validates :print_display_name, presence: false
+    included do
+      field :print_display_name, type: String
+      permit_params :print_display_name
+      validates :print_display_name, presence: false
+    end
   end
 end
