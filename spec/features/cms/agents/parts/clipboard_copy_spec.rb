3f74c5a0482229812a9cdb6a4863fba66b92a6a3
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2024-12-12 16:56:58 +0900

    クリップボードコピーのテストコード追加

--- spec/features/cms/agents/parts/clipboard_copy_spec.rb
+++ spec/features/cms/agents/parts/clipboard_copy_spec.rb
@@ -17,3 +17,21 @@ describe "cms_agents_parts_clipboard_copy", type: :feature, dbscope: :example, j
     end
   end
 end
+
+describe "cms_agents_parts_clipboard_copy_has_display_name", type: :feature, dbscope: :example, js: true do
+  let(:site) { cms_site }
+  let(:clipboard_display_name) { "hogehoge1234" } # 定義追加
+
+  context "when clipboard_copy_target is url" do
+    let!(:part) { create :cms_part_clipboard_copy, clipboard_copy_target: 'url', clipboard_display_name: clipboard_display_name }
+    let!(:layout) { create_cms_layout part } # part を定義した後に layout を作成
+    let!(:top_page) { create :cms_page, layout: layout }
+
+    it "displays the clipboard copy button with the correct display name" do
+      visit top_page.full_url + "?a=b"
+      within "#main" do
+        expect(page).to have_css(".btn-ss-clipboard-copy", text: clipboard_display_name)
+      end
+    end
+  end
+end
