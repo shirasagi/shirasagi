d9d4c8c6ee98941515e13e04404e3bd63dbea989
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2024-12-18 13:19:42 +0900

    revert public/assets/map/googlemaps/map.js

--- public/assets/map/googlemaps/map.js
+++ public/assets/map/googlemaps/map.js
@@ -157,13 +157,12 @@ this.Googlemaps_Map = (function () {
       } else if (name || text) {
         markerHtml = '<div class="marker-info">';
         if (name) {
-          markerHtml += '<p class=marker__name>' + name + '</p>';
+          markerHtml += '<p>' + name + '</p>';
         }
         if (text) {
-          markerHtml = '<div class="marker__explanation">';
           $.each(text.split(/[\r\n]+/), function () {
             if (this.match(/^https?:\/\//)) {
-              return markerHtml += '<p class="marker__link"><a href="' + this + '">' + this + '</a></p>';
+              return markerHtml += '<p><a href="' + this + '">' + this + '</a></p>';
             } else {
               return markerHtml += '<p>' + this + '</p>';
             }
