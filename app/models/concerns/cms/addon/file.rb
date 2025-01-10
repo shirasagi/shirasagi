cbdba20b3722e22318c13126d6ba9c14f440f064
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-01-10 18:03:03 +0900

    app/views/cms/agents/addons/file/_show.html.erb修正

--- app/models/concerns/cms/addon/file.rb
+++ app/models/concerns/cms/addon/file.rb
@@ -2,4 +2,21 @@ module Cms::Addon::File
   extend ActiveSupport::Concern
   extend SS::Addon
   include Cms::Reference::Files
+
+  # # ページにのみ適用するため、ページモデルかどうかを判定
+  # included do
+  #   before_save :add_unused_class, if: :page_model?
+  # end
+
+  # private
+
+  # # contains_urlsの値がvalue_contains_urlsと同じかどうかを判定
+  # def in_used?(value_contains_urls)
+  #   contains_urls == value_contains_urls
+  # end
+
+  # # ページモデルかどうかを判定
+  # def page_model?
+  #   is_a?(Cms::Model::Page)
+  # end
 end
