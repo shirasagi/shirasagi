2280a4cea2dc020408300a93775f0517ec8dd27d
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-01-17 20:07:45 +0900

    自由記述のテストコード作成・Selenium::WebDriver::Error::StaleElementReferenceError:

--- spec/features/cms/agents/addons/file/file_view_rendering_spec.rb
+++ spec/features/cms/agents/addons/file/file_view_rendering_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+# 既定フィールドの詳細画面テスト
 describe 'cms_agents_addons_file', type: :feature, dbscope: :example, js: true do
   let(:site) { cms_site }
   let!(:node) { create :article_node_page, cur_site: site, cur_user: cms_user }
@@ -66,5 +67,124 @@ describe 'cms_agents_addons_file', type: :feature, dbscope: :example, js: true d
       )
     end
     let(:button_label) { I18n.t("cms.file") }
+  end
+end
+
+# 自由入力の詳細画面テスト
+describe 'cms_agents_addons_file', type: :feature, dbscope: :example, js: true do
+  let(:site) { cms_site }
+  let!(:node) do
+    create :article_node_page, cur_site: site, cur_user: cms_user, filename: "docs", name: "article", group_ids: [cms_group.id],
+    st_form_ids: [form.id]
+  end
+  let!(:item) { create :article_page, cur_node: node, group_ids: [cms_group.id] }
+  let!(:edit_path) { edit_article_page_path site.id, node, item }
+  let!(:form) { create(:cms_form, cur_site: site, state: 'public', sub_type: 'entry', group_ids: [cms_group.id]) }
+  let!(:column1) do
+    create(:cms_column_file_upload, cur_site: site, cur_form: form, required: "optional", file_type: "video", order: 1)
+  end
+  let!(:column2) { create(:cms_column_free, cur_site: site, cur_form: form, required: "optional", order: 2) }
+  let!(:file) { tmp_ss_file contents: "#{Rails.root}/spec/fixtures/ss/logo.png", user: cms_user, basename: "#{unique_id}.jpg" }
+  let(:button_label) { I18n.t("ss.buttons.upload") }
+  let(:logo_path) { "#{Rails.root}/spec/fixtures/ss/logo.png" }
+  let(:keyvisual_path) { "#{Rails.root}/spec/fixtures/ss/file/keyvisual.jpg" }
+
+  before do
+    login_cms_user
+    visit edit_path
+  end
+  it "allows file uploads in form" do
+    within 'form#item-form' do
+      wait_for_event_fired("ss:formActivated") do
+        page.accept_confirm(I18n.t("cms.confirm.change_form")) do
+          select form.name, from: 'in_form_id'
+        end
+      end
+    end
+
+    within ".column-value-palette" do
+      wait_for_event_fired("ss:columnAdded") do
+        click_on column2.name
+      end
+    end
+    within ".column-value-cms-column-free" do
+      wait_for_cbox_opened do
+        click_on button_label
+      end
+    end
+
+    within_cbox do
+      attach_file 'item[in_files][]', "#{Rails.root}/spec/fixtures/ss/shirasagi.pdf"
+      click_button I18n.t("ss.buttons.save")
+      expect(page).to have_css('.file-view', text: 'shirasagi.pdf')
+      click_on 'shirasagi.pdf'
+
+      wait_for_cbox_closed do
+        click_on I18n.t('ss.buttons.save')
+      end
+    end
+
+    within ".column-value-cms-column-free" do
+      wait_for_cbox_opened do
+        click_on button_label
+      end
+    end
+
+    within_cbox do
+      attach_file "item[in_files][]", "#{Rails.root}/spec/fixtures/ss/logo.png"
+      click_button I18n.t("ss.buttons.save")
+      expect(page).to have_css('.file-view', text: 'logo.png')
+      click_on 'logo.png'
+
+      wait_for_cbox_closed do
+        click_on I18n.t('ss.buttons.save')
+      end
+    end
+
+    within "form#item-form" do
+      within ".column-value-cms-column-free" do
+
+        within '.column-value-files' do
+          expect(page).to have_css('.file-view', text: 'shirasagi.pdf')
+          expect(page).to have_css('.file-view', text: 'logo.png')
+
+          within ".file-view", text: 'logo.png' do
+            find(".action-paste").click
+          end
+        end
+
+      end
+
+      # 定型フォームに動画を添付すると Cms_Form.addSyntaxCheck を呼び出し、アクセシビリティチェックを登録する。
+      # Cms_Form.addSyntaxCheck の呼び出しが完了する前に「公開保存」をクリックしてしまうと、
+      # アクセシビリティチェックが実行されないので、警告ダイアログが表示されず、テストが失敗してしまう。
+      # そこで、苦渋だが  wait_for_js_ready で Cms_Form.addSyntaxCheck の呼び出し完了を待機する。
+      wait_for_js_ready
+
+      click_on I18n.t("ss.buttons.publish_save")
+      click_on I18n.t("ss.buttons.ignore_alert")
+    end
+    wait_for_notice I18n.t('ss.notice.saved')
+
+    context "with article/page" do
+      describe "index" do
+        it do
+          within '#selected-files' do
+            expect(page).to have_css(".file-view", text: 'logo.jpg')
+            expect(page).to have_css(".file-view.unused", text: 'shirasagi.pdf')
+          end
+        end
+      end
+    end
+
+    context "with cms/file" do
+      let!(:file) do
+        tmp_ss_file(
+          Cms::File, model: "cms/file", user: cms_user, site: site, basename: filename,
+          contents: logo_path
+        )
+      end
+      let(:button_label) { I18n.t("cms.file") }
+    end
   end
 end
