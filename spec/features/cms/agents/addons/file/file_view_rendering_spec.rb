e490879fdb20e1aeef10ee1e8bc98fe1e78b3f7f
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-01-17 15:17:50 +0900

    テストコードを修正・既定フィールドのテストパス

--- spec/features/cms/agents/addons/file/file_view_rendering_spec.rb
+++ spec/features/cms/agents/addons/file/file_view_rendering_spec.rb
@@ -4,16 +4,11 @@ describe 'cms_agents_addons_file', type: :feature, dbscope: :example, js: true d
   let(:site){ cms_site }
   let!(:node) { create :article_node_page, cur_site: site, cur_user: cms_user }
   let(:filename) { "#{unique_id}.png" }
-  let!(:file_1) { tmp_ss_file contents: "#{Rails.root}/spec/fixtures/ss/logo.png", user: cms_user, basename: "#{unique_id}.jpg" }
-  let!(:file_2) do
-    tmp_ss_file contents: "#{Rails.root}/spec/fixtures/ss/keyvisual.jpg", user: cms_user, basename: "#{unique_id}.jpg"
-  end
-  let!(:item) { create :article_page, cur_site: site, cur_user: cms_user, cur_node: node, file_ids: [ file_1.id ] }
+  let!(:file) { tmp_ss_file contents: "#{Rails.root}/spec/fixtures/ss/logo.png", user: cms_user, basename: "#{unique_id}.jpg" }
   let(:button_label) { I18n.t("ss.buttons.upload") }
 
   before do
     login_cms_user
-    item.update(contains_urls: [file_1.url])
   end
 
   context "with article/page" do
@@ -33,6 +28,26 @@ describe 'cms_agents_addons_file', type: :feature, dbscope: :example, js: true d
         end
 
         within "#ajax-box" do
+          attach_file "item[in_files][]", "#{Rails.root}/spec/fixtures/ss/logo.png"
+          click_button I18n.t("ss.buttons.save")
+          expect(page).to have_css('.file-view', text: 'logo.png')
+        end
+
+        wait_for_cbox_closed do
+          page.execute_script("SS_AjaxFile.firesEvents = true;")
+          wait_for_event_fired "ss:ajaxFileSelected", selector: "#addon-cms-agents-addons-file .ajax-box" do
+            click_on 'logo.png'
+          end
+        end
+
+        within "#item-form #addon-cms-agents-addons-file" do
+          wait_for_cbox_opened do
+            click_on button_label
+          end
+        end
+
+        within "#ajax-box" do
+          page.execute_script("SS_AjaxFile.firesEvents = true;")
           attach_file "item[in_files][]", "#{Rails.root}/spec/fixtures/ss/file/keyvisual.jpg"
           click_button I18n.t("ss.buttons.save")
           expect(page).to have_css('.file-view', text: 'keyvisual.jpg')
@@ -40,12 +55,6 @@ describe 'cms_agents_addons_file', type: :feature, dbscope: :example, js: true d
             wait_for_event_fired "ss:ajaxFileSelected", selector: "#addon-cms-agents-addons-file .ajax-box" do
               click_on 'keyvisual.jpg'
             end
-          end
-        end
-
-        wait_for_cbox_closed do
-          wait_for_event_fired "ss:ajaxFileSelected", selector: "#addon-cms-agents-addons-file .ajax-box" do
-            click_on 'keyvisual.jpg'
           end
         end
 
@@ -57,22 +66,20 @@ describe 'cms_agents_addons_file', type: :feature, dbscope: :example, js: true d
         click_on I18n.t("ss.buttons.ignore_alert")
 
         within '#selected-files' do
-          expect(page).to have_css('.name', text: file_1.filename)
+          expect(page).to have_css(".file-view", text: 'keyvisual.jpg')
+          expect(page).to have_css(".file-view.unused", text: 'logo.png')
         end
-
-        expect(page).to have_css(".file-view", text: file_1.filename)
-        expect(page).to have_css(".file-view.unused", text: file_2.name)
       end
     end
   end
 
-  context "with cms/temp_file" do
+  context "with cms/file" do
     let!(:file) do
       tmp_ss_file(
-        Cms::TempFile, user: cms_user, site: site, node: node, basename: filename,
+        Cms::File, model: "cms/file", user: cms_user, site: site, basename: filename,
         contents: "#{Rails.root}/spec/fixtures/ss/logo.png"
       )
     end
-    let(:button_label) { I18n.t("ss.buttons.upload") }
+    let(:button_label) { I18n.t("cms.file") }
   end
 end
