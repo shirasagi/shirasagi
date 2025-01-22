0e7e0a5f37fde7ad7e178fb46c9d09645da08088
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-01-22 11:22:25 +0900

    ログの追加、summary_page_id更新処理の追加

--- app/jobs/concerns/sys/site_copy/cms_pages.rb
+++ app/jobs/concerns/sys/site_copy/cms_pages.rb
@@ -4,9 +4,22 @@ module Sys::SiteCopy::CmsPages
   include Sys::SiteCopy::CmsContents
 
   def copy_cms_page(src_page)
+    Rails.logger.info("♦︎ コピー開始: #{src_page.filename}(#{src_page.id}), route: #{src_page.route}")
+
     return nil if (src_page.route != "cms/page") && !@copy_contents.include?('pages')
 
+    Rails.logger.debug("♦︎ copy_cms_page: #{@copy_contents.inspect}") # 📌 copy_contents の状態を確認
+    Rails.logger.debug("♦︎ copy_cms_page: src_page.contact_group_contact_id=#{src_page.contact_group_contact_id}") # 📌 Contact ID 確認
+
     copy_cms_content(:pages, src_page, copy_cms_page_options)
+
+    # サマリーページのコピー後に category_node の summary_page_id を更新
+    if src_page.route == "cms/summary_page"
+      update_summary_page_reference(src_page, dest_page)
+    end
+
+    # dest_page
+
   rescue => e
     @task.log("#{src_page.filename}(#{src_page.id}): ページのコピーに失敗しました。")
     Rails.logger.error("#{e.class} (#{e.message}):\n  #{e.backtrace.join("\n  ")}")
@@ -14,10 +27,38 @@ module Sys::SiteCopy::CmsPages
 
   def copy_cms_pages
     page_ids = Cms::Page.site(@src_site).pluck(:id)
+    Rails.logger.info("♦︎ コピー対象ページ数: #{page_ids.size}") # 📌 ページ数確認
     page_ids.each do |page_id|
       page = Cms::Page.site(@src_site).find(page_id) rescue nil
-      next if page.blank?
+      if page.blank?
+        Rails.logger.warn("♦︎ ページ取得失敗: page_id=#{page_id}")
+        next
+      end
+      # next if page.blank?
+      Rails.logger.info("♦︎ ページコピー開始: #{page.filename} (#{page.id})")
       copy_cms_page(page)
+    end
+  end
+
+  private
+
+  def update_summary_page_reference(src_page, dest_page)
+    if dest_page.nil?
+      Rails.logger.error("♦︎ update_summary_page_reference: dest_page が nil のため更新できません")
+      return
+    end
+
+    category_nodes = Cms::Node.site(@src_site).where(summary_page_id: src_page.id)
+
+    category_nodes.each do |category_node|
+      dest_category_node = Cms::Node.site(@dest_site).find_by(filename: category_node.filename)
+      if dest_category_node.blank?
+        Rails.logger.warn("♦︎ カテゴリーノード未検出: #{category_node.filename}")
+        next
+      end
+
+      Rails.logger.info("♦︎ summary_page_id 更新: #{dest_category_node.filename} -> #{dest_page.id}")
+      dest_category_node.update(summary_page_id: dest_page.id)
     end
   end
 
@@ -29,7 +70,9 @@ module Sys::SiteCopy::CmsPages
         return nil
       end
 
+      Rails.logger.info("♦︎ 参照ページコピー開始: #{src_page.filename} (#{src_page.id})")
       dest_page = copy_cms_page(src_page)
+      Rails.logger.debug("♦︎ コピー後の dest_page: #{dest_page&.id}") # 📌 コピー結果確認
       dest_page.try(:id)
     end
   end
