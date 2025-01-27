15141c1afeec9c27194b36f4f0937650c1381f2b
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-01-27 10:19:02 +0900

    ログの修正

--- app/jobs/concerns/sys/site_copy/cms_pages.rb
+++ app/jobs/concerns/sys/site_copy/cms_pages.rb
@@ -4,12 +4,12 @@ module Sys::SiteCopy::CmsPages
   include Sys::SiteCopy::CmsContents
 
   def copy_cms_page(src_page)
-    Rails.logger.info("♦︎ コピー開始: #{src_page.filename}(#{src_page.id}), route: #{src_page.route}")
-    Rails.logger.debug("♦︎ copy_cms_page: @copy_contents=#{@copy_contents.inspect} (class=#{@copy_contents.class})")
-    return nil if (src_page.route != "cms/page") && !@copy_contents.to_a.include?('pages')
+    Rails.logger.info("♦︎ [copy_cms_page] コピー開始: #{src_page.filename}(#{src_page.id}), route: #{src_page.route}")
+    Rails.logger.debug("♦︎ [copy_cms_page] @copy_contents=#{@copy_contents.inspect} (class=#{@copy_contents.class})")
+    return nil if (src_page.route != "cms/page") && !@copy_contents.include?('pages')
 
-    Rails.logger.debug("♦︎ copy_cms_page: @copy_contents=#{@copy_contents.inspect} (class=#{@copy_contents.class})")
-    Rails.logger.debug("♦︎ copy_cms_page: src_page.contact_group_contact_id=#{src_page.contact_group_contact_id}")
+    Rails.logger.debug("♦︎ [copy_cms_page] @copy_contents=#{@copy_contents.inspect} (class=#{@copy_contents.class})")
+    Rails.logger.debug("♦︎ [copy_cms_page] src_page.contact_group_contact_id=#{src_page.contact_group_contact_id}")
 
     copy_cms_content(:pages, src_page, copy_cms_page_options)
   rescue => e
@@ -19,7 +19,7 @@ module Sys::SiteCopy::CmsPages
 
   def copy_cms_pages
     page_ids = Cms::Page.site(@src_site).pluck(:id)
-    Rails.logger.info("♦︎ コピー対象ページ数: #{page_ids.size}")
+    Rails.logger.info("♦︎ [copy_cms_pages] コピー対象ページ数: #{page_ids.size}")
     page_ids.each do |page_id|
       page = Cms::Page.site(@src_site).find(page_id) rescue nil
       next if page.blank?
@@ -31,12 +31,15 @@ module Sys::SiteCopy::CmsPages
   def resolve_page_reference(id)
     cache(:pages, id) do
       src_page = Cms::Page.site(@src_site).find(id) rescue nil
-      return nil if src_page.blank?
+      if src_page.blank?
+        Rails.logger.warn("♦︎ [resolve_page_reference] #{id}: 参照されているページが存在しません。")
+        return nil
+      end
 
-      Rails.logger.info("♦︎ 参照ページコピー開始: #{src_page.filename} (#{src_page.id})")
+      Rails.logger.info("♦︎ [resolve_page_reference] 参照ページコピー開始: #{src_page.filename} (#{src_page.id})")
       dest_page = copy_cms_page(src_page)
       dest_page.try(:id)
-      Rails.logger.debug("♦︎ コピー後の dest_page: #{dest_page&.id}") # 📌 コピー結果確認
+      Rails.logger.debug("♦︎ [resolve_page_reference] コピー後の dest_page: #{dest_page&.id}") # コピー結果確認
       Rails.logger.debug("♦︎ [resolve_page_reference] 終了時の dest_page: #{dest_page.inspect}")
     end
   end
