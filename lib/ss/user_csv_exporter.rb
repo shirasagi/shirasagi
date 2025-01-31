99e1322f35875c137ece7a29478443405f027c7f
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-01-31 13:11:38 +0900

    refactor lib/ss/user_csv_exporter.rb

--- lib/ss/user_csv_exporter.rb
+++ lib/ss/user_csv_exporter.rb
@@ -1,4 +1,3 @@
-# lib/ss/user_csv_exporter.rb
 require 'csv'
 require 'i18n'
 
@@ -6,8 +5,9 @@ module SS
   module UserCsvExporter
     def self.csv_headers
       headers = %w(
-            id name kana uid organization_uid email password tel tel_ext account_start_date account_expiration_date
-            initial_password_warning organization_id groups last_loggedin)
+        id name kana uid organization_uid email password tel tel_ext account_start_date account_expiration_date
+        initial_password_warning organization_id groups last_loggedin
+      )
       unless Sys::Auth::Setting.instance.mfa_otp_use_none?
         headers << "mfa_otp_enabled_at"
       end
@@ -23,33 +23,35 @@ module SS
           opts[:criteria].each do |item|
             next if item.nil?
 
-            row = csv_headers.map do |header|
-              case header
-              when "password"
-                ""
-              when "mfa_otp_enabled_at"
-                if item.mfa_otp_secret.present?
-                  I18n.t("ss.mfa_otp_enabled_at", time: I18n.l(item.mfa_otp_enabled_at, format: :picker))
-                else
-                  I18n.t("ss.mfa_otp_not_enabled_yet")
-                end
-              when "sys_roles"
-                item.sys_roles.pluck(:name).join("\n")
-              when "account_start_date", "account_expiration_date"
-                item.try(header).present? ? I18n.l(item.try(header)) : nil
-              when "initial_password_warning"
-                item.initial_password_warning.present? ? I18n.t('ss.options.state.enabled') : I18n.t('ss.options.state.disabled')
-              when "organization_id"
-                item.organization&.name
-              when "groups"
-                item.groups.pluck(:name).join("\n")
-              else
-                item.try(header)
-              end
-            end
-
-            csv << row
+            csv << generate_csv_row(item)
           end
+        end
+      end
+    end
+
+    def self.generate_csv_row(item)
+      csv_headers.map do |header|
+        case header
+        when "password"
+          ""
+        when "mfa_otp_enabled_at"
+          if item.mfa_otp_secret.present?
+            I18n.t("ss.mfa_otp_enabled_at", time: I18n.l(item.mfa_otp_enabled_at, format: :picker))
+          else
+            I18n.t("ss.mfa_otp_not_enabled_yet")
+          end
+        when "sys_roles"
+          item.sys_roles.pluck(:name).join("\n")
+        when "account_start_date", "account_expiration_date"
+          item.try(header).present? ? I18n.l(item.try(header)) : nil
+        when "initial_password_warning"
+          item.initial_password_warning.present? ? I18n.t('ss.options.state.enabled') : I18n.t('ss.options.state.disabled')
+        when "organization_id"
+          item.organization&.name
+        when "groups"
+          item.groups.pluck(:name).join("\n")
+        else
+          item.try(header)
         end
       end
     end
