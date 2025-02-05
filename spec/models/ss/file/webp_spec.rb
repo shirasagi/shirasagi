19611672036fe3b4a116464d9865479e32f4331b
NAKANO Hideo <nakano@web-tips.co.jp>
2025-02-05 13:13:22 +0900

    [add] model specs

---
+++ spec/models/ss/file/webp_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe SS::File, dbscope: :example do
+  let(:basename) { "logo-#{unique_id}.webp" }
+  subject! { tmp_ss_file(contents: "#{Rails.root}/spec/fixtures/ss/logo.webp", basename: basename) }
+  let(:thumb_basename) { "#{::File.basename(subject.filename, ".*")}_thumb.#{subject.extname}" }
+
+  its(:valid?) { is_expected.to be_truthy }
+  its(:path) { is_expected.to eq "#{SS::File.root}/ss_files/#{subject.id}/_/#{subject.id}" }
+  its(:url) { is_expected.to eq "/fs/#{subject.id}/_/#{subject.filename}" }
+  its(:thumb_url) { is_expected.to eq "/fs/#{subject.id}/_/#{thumb_basename}" }
+  its(:public?) { is_expected.to be_falsey }
+  its(:public_dir) { is_expected.to be_nil }
+  its(:public_path) { is_expected.to be_nil }
+  its(:full_url) { is_expected.to be_nil }
+  its(:name) { is_expected.to eq basename }
+  its(:humanized_name) { is_expected.to eq "#{::File.basename(basename, ".*")} (WEBP 8.99KB)" }
+  its(:download_filename) { is_expected.to eq basename }
+  its(:basename) { is_expected.to eq basename }
+  its(:extname) { is_expected.to eq 'webp' }
+  its(:content_type) { is_expected.to eq "image/webp" }
+  its(:image?) { is_expected.to be_truthy }
+end
