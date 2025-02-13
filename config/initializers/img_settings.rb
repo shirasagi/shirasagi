0e1b5b60130065b3919bad67ad26ee344c391bce
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-02-13 16:10:45 +0900

    add comments about dir

--- config/initializers/img_settings.rb
+++ config/initializers/img_settings.rb
@@ -1,7 +1,8 @@
 # Image settings configuration
 module ImgSettings
+  # lib/ss/image_converter.rb
   DEFAULT_THUMB_WIDTH = 360 unless defined?(DEFAULT_THUMB_WIDTH)
   DEFAULT_THUMB_HEIGHT = 360 unless defined?(DEFAULT_THUMB_HEIGHT)
-
+  # app/models/member/photo_file.rb
   DEFAULT_THUMB_SIZE = [360, 360].freeze unless defined?(DEFAULT_THUMB_SIZE)
 end
