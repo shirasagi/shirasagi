54e5f3201366440d04a8f0a88a0506362f7c5677
Sayoko <79836227+se1987@users.noreply.github.com>
2025-02-25 14:57:52 +0900

    [fix]sys/reflecting summary page settings in site duplication:issue-5479 (#5526)
    * [#5479] 再現テスト
    
    * サマリーページのツールチップを修正
    
    * [fixup][#5479] 再現テスト
    
    * 複製後のノードにsummary_page_idを設定する処理を追加、summary_page_id=2を期待するテストでsummary_page_id=3となって失敗する
    
    * reference_type に SS::Contact を追加 unknown reference type エラーの対応
    
    * revert  app/jobs/concerns/ss/copy/cms_nodes.rb
    
    * revert
    
    * revert
    
    * revert app/jobs/concerns/ss/copy/cms_pages.rb
    
    * ログの追加、summary_page_id更新処理の追加
    
    * 失敗していたページの複製は成功、ブラウザで確認するとカテゴリーフォルダー内にはサマリーページが表示されていない
    
    * 失敗していたページの複製は成功、ブラウザで確認するとカテゴリーフォルダー内にはサマリーページが表示されていない
    
    # Conflicts:
    #	app/jobs/concerns/sys/site_copy/cms_pages.rb
    
    * フォーマットの修正
    
    * フォーマットの修正
    
    * サイト複製後にサマリーページが表示されていることを確認
    
    * ツールチップの修正
    
    * ツールチップの修正
    
    * ログの追加、テストの修正
    
    * ログの追加
    
    * サイト複製の記事・ページオプションを外した際にsummary_page_idをnilにする
    
    * 不要コードの削除
    
    * 不要コードの削除
    
    * id=trueエラーの対処→意図しないページの複製処理を防止,ログの追加
    
    * ツールチップの修正
    
    * 警告対応:case内にelse追加
    
    * caseにelseがないという警告が出るが、elseを追加すると複製処理が失敗してしまうため、revert
    
    * ログの追加
    
    * ログの追加、テストパス、オプションなしの時にブラウザで確認するとサマリーページが表示されたまま
    
    * ログの追加
    
    * hidden属性の追加
    
    * hidden属性の修正
    
    * app/views/sys/site_copy/_form.html.erb修正
    
    * copy_cms_pagesメソッドの条件分岐を削除
    
    * ubuntuのバージョン固定
    
    * ログの追加
    
    * ログの修正
    
    * ログの追加と修正
    
    * hidden属性を追加
    
    * fix layouts
    
    * ログの修正
    
    * ログの修正
    
    * ログの修正
    
    * id=trueエラーの対処
    
    * 追加したid処理のコードを削除
    
    * テストコードを修正/テストpass
    
    * summary_page_idの参照先をコピー後の記事になるように修正
    
    * ログの追加と修正
    
    * 不要コードの削除
    
    * ログをブロック引数に修正、記号を半角に変換
    
    * サマリーページの注意点を追記
    
    * 特殊文字や全角記号の修正
    
    * サマリーページの注意書きを追記
    
    * ツールチップの内容を表示
    
    * 不要コードの削除、ログの修正
    
    * ログの修正
    
    * ログの修正、コードの修正
    
    * ログの修正
    
    * ログの修正
    
    * ログの修正
    
    * ロケール修正
    
    * revert log level
    
    ---------
    
    Co-authored-by: NAKANO Hideo <nakano@web-tips.co.jp>
    Co-authored-by: 佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>

--- app/jobs/concerns/ss/copy/cms_nodes.rb
+++ app/jobs/concerns/ss/copy/cms_nodes.rb
@@ -3,6 +3,10 @@ module SS::Copy::CmsNodes
   include SS::Copy::Cache
 
   def copy_cms_node(src_node)
+    Rails.logger.debug do
+      "SS::Copy::CmsNodes[copy_cms_node] #{src_node.filename}: " \
+        "コピー処理開始 (summary_page_id=#{src_node.try(:summary_page_id)})"
+    end
     copy_cms_content(:nodes, src_node, copy_cms_node_options)
   rescue => e
     @task.log("#{src_node.filename}(#{src_node.id}): フォルダーのコピーに失敗しました。")
@@ -27,6 +31,8 @@ module SS::Copy::CmsNodes
   end
 
   def after_copy_cms_node(src_node, dest_node)
+    Rails.logger.debug{ "[after_copy_cms_node] #{src_node.filename}: 処理前 summary_page_id=#{src_node.try(:summary_page_id)}" }
+    Rails.logger.debug{ "[after_copy_cms_node] #{src_node.filename}: 処理前 summary_page=#{src_node.try(:summary_page).inspect}" }
     case src_node.route
     when "uploader/file"
       copy_node_files(src_node, dest_node)
@@ -34,11 +40,22 @@ module SS::Copy::CmsNodes
       copy_inquiry_columns(src_node, dest_node)
     when "ezine/page"
       copy_ezine_columns(src_node, dest_node)
+    when "category/page"
+      dest_node.summary_page_id = nil unless @copy_contents.include?('pages')
+      dest_node.save!
     when "rss/weather_xml"
       @task.log("#{src_node.filename}(#{src_node.id}): フォルダーのコピーをスキップします。")
       return
     end
 
     @task.log("#{src_node.filename}(#{src_node.id}): フォルダーをコピーしました。")
+    Rails.logger.debug do
+      "[after_copy_cms_node] #{src_node.filename} → #{dest_node.try(:filename)}:" \
+        "コピー処理完了 (summary_page_id=#{dest_node.try(:summary_page_id)})"
+    end
+    Rails.logger.debug do
+      "[after_copy_cms_node] #{src_node.filename}: → #{dest_node.try(:filename)}:" \
+        "コピー処理完了 (summary_page=#{dest_node.try(:summary_page).inspect}"
+    end
   end
 end
