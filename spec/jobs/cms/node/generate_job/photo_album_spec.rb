63e4bee35dce05c697632128f7f133aa5ea0b45e
NAKANO Hideo <nakano@web-tips.co.jp>
2025-03-03 17:32:09 +0900

    [modify^2] add more detail specs

--- spec/jobs/cms/node/generate_job/photo_album_spec.rb
+++ spec/jobs/cms/node/generate_job/photo_album_spec.rb
@@ -11,19 +11,6 @@ describe Cms::Node::GenerateJob, dbscope: :example do
       :cms_node_photo_album, cur_site: site, cur_node: article_node, layout: layout,
       conditions: article_node.filename)
   end
-  let(:attachment_path) { "#{Rails.root}/spec/fixtures/ss/logo.png" }
-  let!(:page1) do
-    Timecop.freeze(now - 1.hour) do
-      file = tmp_ss_file(site: site, user: user, contents: attachment_path, basename: 'logo.png')
-      create(:article_page, cur_site: site, cur_node: article_node, layout: layout, file_ids: [ file.id ], state: 'public')
-    end
-  end
-  let!(:page2) do
-    Timecop.freeze(now - 2.hours) do
-      file = tmp_ss_file(site: site, user: user, contents: attachment_path, basename: 'logo.png')
-      create(:article_page, cur_site: site, cur_node: article_node, layout: layout, file_ids: [ file.id ], state: 'public')
-    end
-  end
 
   before do
     Cms::Task.create!(site_id: site.id, node_id: nil, name: 'cms:generate_nodes', state: 'ready')
@@ -31,41 +18,98 @@ describe Cms::Node::GenerateJob, dbscope: :example do
 
   describe "#perform" do
     before do
-      described_class.bind(site_id: site.id).perform_now
+      expect do
+        described_class.bind(site_id: site.id).perform_now
+      end.to output(include("/#{photo_album_node.filename}/index.html")).to_stdout
     end
 
-    it do
-      expect(File.exist?("#{photo_album_node.path}/index.html")).to be_truthy
-
-      expect(Cms::Task.count).to eq 1
-      Cms::Task.where(site_id: site.id, node_id: nil, name: 'cms:generate_nodes').first.tap do |task|
-        expect(task.state).to eq 'completed'
-        expect(task.started).not_to be_nil
-        expect(task.closed).not_to be_nil
-        expect(task.total_count).to eq 0
-        expect(task.current_count).to eq 0
-        expect(task.logs).to include(include("#{photo_album_node.url}index.html"))
-        expect(task.node_id).to be_nil
-        # logs are saved in a file
-        expect(::File.exist?(task.log_file_path)).to be_truthy
-        # and there are no `logs` field
-        expect(task[:logs]).to be_nil
-        # performance logs are saved
-        expect(::File.exist?(task.perf_log_file_path)).to be_truthy
+    context "with pages and files" do
+      let(:attachment_path) { "#{Rails.root}/spec/fixtures/ss/logo.png" }
+      let!(:page1) do
+        Timecop.freeze(now - 1.hour) do
+          file = tmp_ss_file(site: site, user: user, contents: attachment_path, basename: 'logo.png')
+          create(:article_page, cur_site: site, cur_node: article_node, layout: layout, file_ids: [ file.id ], state: 'public')
+        end
+      end
+      let!(:page2) do
+        Timecop.freeze(now - 2.hours) do
+          file = tmp_ss_file(site: site, user: user, contents: attachment_path, basename: 'logo.png')
+          create(:article_page, cur_site: site, cur_node: article_node, layout: layout, file_ids: [ file.id ], state: 'public')
+        end
       end
 
-      expect(Job::Log.count).to eq 1
-      Job::Log.first.tap do |log|
-        expect(log.logs).to include(/INFO -- : .* Started Job/)
-        expect(log.logs).to include(/INFO -- : .* Completed Job/)
-      end
+      it do
+        expect(File.exist?("#{photo_album_node.path}/index.html")).to be_truthy
+        # cms/photo には RSS を応答する公開アクションはないので rss.xml が作成されていないことを確認する
+        expect(File.exist?("#{photo_album_node.path}/rss.xml")).to be_falsey
 
-      html = File.read("#{photo_album_node.path}/index.html")
-      html = Nokogiri::HTML5::Document.parse(html)
-      html.css(".photos .photo").tap do |photos|
-        expect(photos).to have(2).items
-        expect(photos[0].css(".title").text.strip).to eq page1.name
-        expect(photos[1].css(".title").text.strip).to eq page2.name
+        expect(Cms::Task.count).to eq 1
+        Cms::Task.where(site_id: site.id, node_id: nil, name: 'cms:generate_nodes').first.tap do |task|
+          expect(task.state).to eq 'completed'
+          expect(task.started).not_to be_nil
+          expect(task.closed).not_to be_nil
+          expect(task.total_count).to eq 0
+          expect(task.current_count).to eq 0
+          expect(task.logs).to include(include("#{photo_album_node.url}index.html"))
+          expect(task.node_id).to be_nil
+          # logs are saved in a file
+          expect(::File.exist?(task.log_file_path)).to be_truthy
+          # and there are no `logs` field
+          expect(task[:logs]).to be_nil
+          # performance logs are saved
+          expect(::File.exist?(task.perf_log_file_path)).to be_truthy
+        end
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(/INFO -- : .* Started Job/)
+          expect(log.logs).to include(/INFO -- : .* Completed Job/)
+        end
+
+        html = File.read("#{photo_album_node.path}/index.html")
+        html = Nokogiri::HTML5::Document.parse(html)
+        html.css(".photos .photo").tap do |photos|
+          expect(photos).to have(2).items
+          expect(photos[0].css(".title").text.strip).to eq page1.name
+          expect(photos[1].css(".title").text.strip).to eq page2.name
+        end
+      end
+    end
+
+    context "without pages and files" do
+      it do
+        expect(File.exist?("#{photo_album_node.path}/index.html")).to be_truthy
+        # cms/photo には RSS を応答する公開アクションはないので rss.xml が作成されていないことを確認する
+        expect(File.exist?("#{photo_album_node.path}/rss.xml")).to be_falsey
+
+        expect(Cms::Task.count).to eq 1
+        Cms::Task.where(site_id: site.id, node_id: nil, name: 'cms:generate_nodes').first.tap do |task|
+          expect(task.state).to eq 'completed'
+          expect(task.started).not_to be_nil
+          expect(task.closed).not_to be_nil
+          expect(task.total_count).to eq 0
+          expect(task.current_count).to eq 0
+          expect(task.logs).to include(include("#{photo_album_node.url}index.html"))
+          expect(task.node_id).to be_nil
+          # logs are saved in a file
+          expect(::File.exist?(task.log_file_path)).to be_truthy
+          # and there are no `logs` field
+          expect(task[:logs]).to be_nil
+          # performance logs are saved
+          expect(::File.exist?(task.perf_log_file_path)).to be_truthy
+        end
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(/INFO -- : .* Started Job/)
+          expect(log.logs).to include(/INFO -- : .* Completed Job/)
+        end
+
+        html = File.read("#{photo_album_node.path}/index.html")
+        html = Nokogiri::HTML5::Document.parse(html)
+        html.css(".photos .photo").tap do |photos|
+          expect(photos).to be_blank
+        end
       end
     end
   end
