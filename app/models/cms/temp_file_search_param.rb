7b307a2dd23780e62d30323a0ba046f298a7f4e4
NAKANO Hideo <nakano@web-tips.co.jp>
2025-03-05 15:38:14 +0900

    [modify] cms/tmep_file/files に検索処理を実装

--- app/models/cms/temp_file_search_param.rb
+++ app/models/cms/temp_file_search_param.rb
@@ -10,9 +10,42 @@ class Cms::TempFileSearchParam < SS::TempFileSearchParam
 
   private
 
-  def validate_type
+  def validate_types
     unless self.types.all? { AVAILABLE_TYPES.include?(_1) }
       errors.add :types, :inclusion
     end
+  end
+
+  def union_all_file_ids(model, base_criteria)
+    all_ids_set = Set.new
+
+    if types && types.include?("temp_file")
+      all_ids_set += search_temp_file(model, base_criteria)
+    end
+    if types && types.include?("user_file")
+      all_ids_set += search_user_file(model, base_criteria)
+    end
+    if types && types.include?("cms_file")
+      all_ids_set += search_cms_file(model, base_criteria)
+    end
+
+    all_ids_set.to_a
+  end
+
+  def search_temp_file(model, base_criteria)
+    criteria = Cms::TempFile.site(cur_site)
+    if node_bound == "current"
+      if cur_node
+        criteria = criteria.node(cur_node)
+      else
+        criteria = criteria.exists(node_id: false)
+      end
+    end
+    criteria = criteria.allow(:read, cur_user)
+    criteria.pluck(:id)
+  end
+
+  def search_cms_file(model, base_criteria)
+    Cms::File.site(cur_site).allow(:read, cur_user).pluck(:id)
   end
 end
