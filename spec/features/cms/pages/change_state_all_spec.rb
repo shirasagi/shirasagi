36e36636ab533d8f318d92ddf6500f73155123d0
NAKANO Hideo <nakano@web-tips.co.jp>
2025-03-10 11:38:50 +0900

    [add][cms] specs about history/backup when pages are all published

--- spec/features/cms/pages/change_state_all_spec.rb
+++ spec/features/cms/pages/change_state_all_spec.rb
@@ -1,33 +1,40 @@
 require 'spec_helper'
 
 describe "cms_page_pages", type: :feature, dbscope: :example, js: true do
+  let(:now) { Time.zone.now.change(usec: 0) }
   let(:site) { cms_site }
   let(:user) { cms_user }
 
-  before { login_cms_user }
-
   context "change state all" do
-    let!(:page1) { create(:cms_page) }
-    let!(:page2) { create(:cms_page) }
-    let!(:page3) { create(:cms_page) }
+    let!(:page1) { Timecop.freeze(now - 4.hours) { create(:cms_page) } }
+    let!(:page2) { Timecop.freeze(now - 3.hours) { create(:cms_page) } }
+    let!(:page3) { Timecop.freeze(now - 2.hours) { create(:cms_page) } }
     let(:index_path) { cms_pages_path(site) }
 
     it do
-      visit index_path
-      wait_for_turbo_frame "#cms-nodes-tree-frame"
       expect(page1.state).to eq "public"
       expect(page2.state).to eq "public"
       expect(page3.state).to eq "public"
+      expect(page1.backups.size).to eq 1
+      expect(page2.backups.size).to eq 1
+      expect(page3.backups.size).to eq 1
 
-      within ".list-head" do
-        wait_for_event_fired("ss:checked-all-list-items") { find('input[type="checkbox"]').set(true) }
-        click_button I18n.t('ss.links.make_them_close')
+      Timecop.freeze(now - 1.hour) do
+        login_cms_user
+
+        visit index_path
+        wait_for_turbo_frame "#cms-nodes-tree-frame"
+
+        within ".list-head" do
+          wait_for_event_fired("ss:checked-all-list-items") { find('input[type="checkbox"]').set(true) }
+          click_button I18n.t('ss.links.make_them_close')
+        end
+
+        wait_for_js_ready
+        click_button I18n.t("ss.buttons.make_them_close")
+        wait_for_notice I18n.t("ss.notice.depublished")
+        wait_for_turbo_frame "#cms-nodes-tree-frame"
       end
-
-      wait_for_js_ready
-      click_button I18n.t("ss.buttons.make_them_close")
-      wait_for_notice I18n.t("ss.notice.depublished")
-      wait_for_turbo_frame "#cms-nodes-tree-frame"
 
       page1.reload
       page2.reload
@@ -37,23 +44,42 @@ describe "cms_page_pages", type: :feature, dbscope: :example, js: true do
       expect(page3.state).to eq "closed"
 
       expect(page1.backups.size).to eq 2
-      expect(page1.backups.first.user_id).to eq user.id
-
-      expect(page2.backups.size).to eq 2
-      expect(page2.backups.first.user_id).to eq user.id
-
-      expect(page3.backups.size).to eq 2
-      expect(page3.backups.first.user_id).to eq user.id
-
-      within ".list-head" do
-        wait_for_event_fired("ss:checked-all-list-items") { find('input[type="checkbox"]').set(true) }
-        click_button I18n.t('ss.links.make_them_public')
+      page1.backups.to_a.tap do |backups|
+        expect(backups[0].created.in_time_zone).to eq now - 1.hour
+        expect(backups[0].user_id).to eq user.id
+        expect(backups[0].data[:state]).to eq "closed"
       end
 
-      wait_for_js_ready
-      click_button I18n.t("ss.buttons.make_them_public")
-      wait_for_notice I18n.t("ss.notice.published")
-      wait_for_turbo_frame "#cms-nodes-tree-frame"
+      expect(page2.backups.size).to eq 2
+      page2.backups.to_a.tap do |backups|
+        expect(backups[0].created.in_time_zone).to eq now - 1.hour
+        expect(backups[0].user_id).to eq user.id
+        expect(backups[0].data[:state]).to eq "closed"
+      end
+
+      expect(page3.backups.size).to eq 2
+      page3.backups.to_a.tap do |backups|
+        expect(backups[0].created.in_time_zone).to eq now - 1.hour
+        expect(backups[0].user_id).to eq user.id
+        expect(backups[0].data[:state]).to eq "closed"
+      end
+
+      Timecop.freeze(now) do
+        login_cms_user
+
+        visit index_path
+        wait_for_turbo_frame "#cms-nodes-tree-frame"
+
+        within ".list-head" do
+          wait_for_event_fired("ss:checked-all-list-items") { find('input[type="checkbox"]').set(true) }
+          click_button I18n.t('ss.links.make_them_public')
+        end
+
+        wait_for_js_ready
+        click_button I18n.t("ss.buttons.make_them_public")
+        wait_for_notice I18n.t("ss.notice.published")
+        wait_for_turbo_frame "#cms-nodes-tree-frame"
+      end
 
       page1.reload
       page2.reload
@@ -63,61 +89,92 @@ describe "cms_page_pages", type: :feature, dbscope: :example, js: true do
       expect(page3.state).to eq "public"
 
       expect(page1.backups.size).to eq 3
-      expect(page1.backups.first.user_id).to eq user.id
+      page1.backups.to_a.tap do |backups|
+        expect(backups[0].created.in_time_zone).to eq now
+        expect(backups[0].user_id).to eq user.id
+        expect(backups[0].data[:state]).to eq "public"
+      end
 
       expect(page2.backups.size).to eq 3
-      expect(page2.backups.first.user_id).to eq user.id
+      page2.backups.to_a.tap do |backups|
+        expect(backups[0].created.in_time_zone).to eq now
+        expect(backups[0].user_id).to eq user.id
+        expect(backups[0].data[:state]).to eq "public"
+      end
 
       expect(page3.backups.size).to eq 3
-      expect(page3.backups.first.user_id).to eq user.id
+      page3.backups.to_a.tap do |backups|
+        expect(backups[0].created.in_time_zone).to eq now
+        expect(backups[0].user_id).to eq user.id
+        expect(backups[0].data[:state]).to eq "public"
+      end
     end
   end
 
   context "branch page" do
     let!(:node) { create :cms_node_page }
-    let!(:page1) { create(:cms_page, cur_site: site, cur_node: node, cur_user: cms_user) }
-    let!(:page2) { create(:cms_page, cur_site: site, cur_node: node, cur_user: cms_user) }
+    let!(:page1) do
+      Timecop.freeze(now - 5.hours) do
+        create(:cms_page, cur_site: site, cur_node: node, cur_user: cms_user)
+      end
+    end
+    let!(:page2) do
+      Timecop.freeze(now - 4.hours) do
+        create(:cms_page, cur_site: site, cur_node: node, cur_user: cms_user)
+      end
+    end
     let!(:page3) do
-      page1.cur_node = node
+      Timecop.freeze(now - 3.hours) do
+        page1.cur_node = node
 
-      copy = page1.new_clone
-      copy.master = page1
-      copy.html = "<s>copy1</s>"
-      copy.save!
+        copy = page1.new_clone
+        copy.master = page1
+        copy.html = "<s>copy1</s>"
+        copy.save!
 
-      page1.reload
-      Cms::Page.find(copy.id)
+        page1.reload
+        Cms::Page.find(copy.id)
+      end
     end
     let!(:page4) do
-      page2.cur_node = node
+      Timecop.freeze(now - 2.hours) do
+        page2.cur_node = node
 
-      copy = page2.new_clone
-      copy.master = page2
-      copy.html = "<s>copy2</s>"
-      copy.save!
+        copy = page2.new_clone
+        copy.master = page2
+        copy.html = "<s>copy2</s>"
+        copy.save!
 
-      page2.reload
-      Cms::Page.find(copy.id)
+        page2.reload
+        Cms::Page.find(copy.id)
+      end
     end
     let(:index_path) { node_pages_path(site, node) }
 
     it do
-      visit index_path
-      wait_for_turbo_frame "#cms-nodes-tree-frame"
-      expect(page1.state).to eq "public"
-      expect(page2.state).to eq "public"
-      expect(page3.state).to eq "closed"
-      expect(page4.state).to eq "closed"
+      expect(page1.backups.size).to eq 1
+      expect(page2.backups.size).to eq 1
 
-      within ".list-head" do
-        wait_for_event_fired("ss:checked-all-list-items") { find('input[type="checkbox"]').set(true) }
-        click_button I18n.t('ss.links.make_them_public')
+      Timecop.freeze(now - 1.hour) do
+        login_cms_user
+
+        visit index_path
+        wait_for_turbo_frame "#cms-nodes-tree-frame"
+        expect(page1.state).to eq "public"
+        expect(page2.state).to eq "public"
+        expect(page3.state).to eq "closed"
+        expect(page4.state).to eq "closed"
+
+        within ".list-head" do
+          wait_for_event_fired("ss:checked-all-list-items") { find('input[type="checkbox"]').set(true) }
+          click_button I18n.t('ss.links.make_them_public')
+        end
+
+        wait_for_js_ready
+        click_button I18n.t("ss.buttons.make_them_public")
+        wait_for_notice I18n.t("ss.notice.published")
+        wait_for_turbo_frame "#cms-nodes-tree-frame"
       end
-
-      wait_for_js_ready
-      click_button I18n.t("ss.buttons.make_them_public")
-      wait_for_notice I18n.t("ss.notice.published")
-      wait_for_turbo_frame "#cms-nodes-tree-frame"
 
       page1.reload
       page2.reload
@@ -128,6 +185,19 @@ describe "cms_page_pages", type: :feature, dbscope: :example, js: true do
       expect(page1.html).to eq "<s>copy1</s>"
       expect(page2.state).to eq "public"
       expect(page2.html).to eq "<s>copy2</s>"
+
+      expect(page1.backups.size).to eq 2
+      page1.backups.to_a.tap do |backups|
+        expect(backups[0].created.in_time_zone).to eq now - 1.hour
+        expect(backups[0].user_id).to eq user.id
+        expect(backups[0].data[:state]).to eq "public"
+      end
+      expect(page2.backups.size).to eq 2
+      page2.backups.to_a.tap do |backups|
+        expect(backups[0].created.in_time_zone).to eq now - 1.hour
+        expect(backups[0].user_id).to eq user.id
+        expect(backups[0].data[:state]).to eq "public"
+      end
     end
   end
 end
