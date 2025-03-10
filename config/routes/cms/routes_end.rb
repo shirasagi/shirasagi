f786bf1505ac31a8f8509fe047cd910332ad4a89
NAKANO Hideo <nakano@web-tips.co.jp>
2025-03-10 16:53:59 +0900

    [fixup][modify] ファイル選択結果のレンダリング処理を実装

--- config/routes/cms/routes_end.rb
+++ config/routes/cms/routes_end.rb
@@ -103,7 +103,9 @@ Rails.application.routes.draw do
         resources :uploads, only: %i[index new create] do
           post :preview, on: :collection
         end
-        resources :files, only: %i[index edit update destroy]
+        resources :files, only: %i[index edit update destroy] do
+          get :select, on: :member
+        end
       end
     end
   end
