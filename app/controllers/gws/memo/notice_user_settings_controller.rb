358a41af3d3080ff1b054e88dd3caace1e25e9b9
itowtips <ito@web-tips.co.jp>
2025-03-11 14:58:21 +0900

    [modify] gws/memo_notice : edit_gws_memo_notice_user_setting permission (#5540)
    * [modify] gws/memo_notice : edit_gws_memo_notice_user_setting permission
    
    * [fix] en locale

--- app/controllers/gws/memo/notice_user_settings_controller.rb
+++ app/controllers/gws/memo/notice_user_settings_controller.rb
@@ -13,7 +13,18 @@ class Gws::Memo::NoticeUserSettingsController < ApplicationController
     fields << :send_notice_mail_addresses
   end
 
+  def show
+    raise "403" if !@cur_user.gws_role_permit_any?(@cur_site, :edit_gws_memo_notice_user_setting)
+    render
+  end
+
+  def edit
+    raise "403" if !@cur_user.gws_role_permit_any?(@cur_site, :edit_gws_memo_notice_user_setting)
+    render
+  end
+
   def update
+    raise "403" if !@cur_user.gws_role_permit_any?(@cur_site, :edit_gws_memo_notice_user_setting)
     @item.attributes = get_params
     render_update @item.save
   end
