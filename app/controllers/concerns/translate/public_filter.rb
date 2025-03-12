22129d9ac334a846048e87d39256ce93810f7d30
itowtips <ito@web-tips.co.jp>
2025-03-12 14:22:53 +0900

    [fixup] chat/bot : cms translate support

--- app/controllers/concerns/translate/public_filter.rb
+++ app/controllers/concerns/translate/public_filter.rb
@@ -52,14 +52,12 @@ module Translate::PublicFilter
     return unless translatable_content_type?
 
     respond_to do |format|
-      format.html { render_translate_html }
-      format.json { render_translate_json }
+      format.html { response.body = translate_convert_html(response.body) }
+      format.json { response.body = translate_convert_json(response.body) }
     end
   end
 
-  def render_translate_html
-    body = response.body
-
+  def translate_convert_html(body)
     converter = Translate::Converter.new(@cur_site, @translate_source, @translate_target)
     body = converter.convert(body)
 
@@ -80,9 +78,15 @@ module Translate::PublicFilter
       HTML
       body.sub!('</html>', h + '</html>')
     end
-    response.body = body
+    body
   end
 
-  def render_translate_json
+  def translate_convert_json(body)
+    # 動的パーツの場合 params[:format] は json だが応答は2パターン存在する
+    # 1. html文字列 : この場合は翻訳を通して応答する
+    # 2. json文字列 : この場合は何もせず応答する
+    body = ActiveSupport::JSON.decode(body)
+    body = translate_convert_html(body) if body.is_a?(String)
+    ActiveSupport::JSON.encode(body)
   end
 end
