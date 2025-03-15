e67fe748563e1d18e272747c96a023bb91e7a32c
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-03-15 23:14:11 +0900

    ジャバスクリプトが実行されない

--- spec/features/theme/ss_theme_spec.rb
+++ spec/features/theme/ss_theme_spec.rb
@@ -1,26 +1,37 @@
 require 'spec_helper'
 
 describe "theme/public_filter", type: :feature, dbscope: :example, js: true do
+  let!(:site) { cms_site }
   shared_examples "theme" do
-    let!(:site) { cms_site }
     let!(:layout) { create_cms_layout part }
     let!(:node) { create :article_node_page, cur_site: site, cur_user: cms_user, layout_id: layout.id }
     let(:page_html) do
-      html = []
-      html << '<div id="content">'
-      html << '<span class="percent-escaped-url">http%3A%2F%2F127.0.0.1%3A3000</span>'
-      html << '<nav class="ss-adobe-reader">'
-      html << '  <div>PDFファイルをご覧いただくためには、Adobe Readerのプラグイン（無償）が必要となります。'
-      html << '  お持ちでない場合は、お使いの機種とスペックに合わせたプラグインをインストールしてください。</div>'
-      html << '  <a href="http://get.adobe.com/jp/reader/">Adobe Readerをダウンロードする</a>'
-      html << '</nav>'
-      html << '</div>'
-      html << '<footer>'
-      html << '  〒000-0000　大鷺県シラサギ市小鷺町1丁目1番地1号'
-      html << '  <small>Copyright © City of Shirasagi All rights Reserved.</small>'
-      html << '</footer>'
-      html.join("\n")
+      # Shirasagi のレイアウト書式をそのまま利用する
+      <<~HTML
+        <html>
+          <head>
+            {{ ss_scripts }}
+          </head>
+          <body>
+            <div id="content">
+              <span class="percent-escaped-url">http%3A%2F%2F127.0.0.1%3A3000</span>
+              <nav class="ss-adobe-reader">
+                <div>
+                  PDFファイルをご覧いただくためには、Adobe Readerのプラグイン（無償）が必要となります。
+                  お持ちでない場合は、お使いの機種とスペックに合わせたプラグインをインストールしてください。
+                </div>
+                <a href="http://get.adobe.com/jp/reader/">Adobe Readerをダウンロードする</a>
+              </nav>
+            </div>
+            <footer>
+              〒000-0000　大鷺県シラサギ市小鷺町1丁目1番地1号
+              <small>Copyright © City of Shirasagi All rights Reserved.</small>
+            </footer>
+          </body>
+        </html>
+      HTML
     end
+
     let(:expected_themes) do
       {
         white: {
@@ -41,9 +52,6 @@ describe "theme/public_filter", type: :feature, dbscope: :example, js: true do
       }
     end
     let!(:item) { create :article_page, cur_site: site, cur_node: node, layout: layout, html: page_html }
-    let!(:part) { create :accessibility_tool, cur_site: site }
-    let!(:part1) { create :accessibility_tool_compat1, cur_site: site }
-    let!(:part2) { create :accessibility_tool_compat2, cur_site: site }
 
     before do
       visit item.full_url
@@ -67,21 +75,6 @@ describe "theme/public_filter", type: :feature, dbscope: :example, js: true do
           }
         };
         SS_Theme.render();
-        // 最新 HTML の button 要素に data-ss-theme を付与
-        document.querySelectorAll('button').forEach(function(btn) {
-          btn.setAttribute('data-ss-theme', btn.getAttribute('name'));
-        });
-        // 旧HTMLの a 要素にも data-ss-theme を付与
-        document.querySelectorAll('a').forEach(function(anchor) {
-          anchor.setAttribute('data-ss-theme', anchor.getAttribute('class'));
-        });
-        document.querySelectorAll('button').forEach(function(btn) {
-          if (btn.getAttribute('name') === 'white') {
-            btn.setAttribute('aria-pressed', 'true');
-          } else {
-            btn.setAttribute('aria-pressed', 'false');
-          }
-        });
       JS
       wait_for_js_ready
     end
@@ -95,9 +88,9 @@ describe "theme/public_filter", type: :feature, dbscope: :example, js: true do
       within ".accessibility__tool-wrap:first-child" do
         expect(page).to have_content(I18n.t("ss.bg_color"))
         if page.has_css?('button[name="white"]')
-          white_button = find('[data-ss-theme="white"]')
-          blue_button  = find('[data-ss-theme="blue"]')
-          black_button = find('[data-ss-theme="black"]')
+          white_button = find('button[name="white"]')
+          blue_button  = find('button[name="blue"]')
+          black_button = find('button[name="black"]')
           expect(white_button['aria-pressed']).to eq("true")
           expect(blue_button['aria-pressed']).to eq("false")
           expect(black_button['aria-pressed']).to eq("false")
