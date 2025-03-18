d5529dba715c58c977e47b3eaf59ca9fcfcb4190
NAKANO Hideo <nakano@web-tips.co.jp>
2025-03-18 17:51:22 +0900

    [modify^2] 重複コードの削除

---
+++ app/components/ss/file_select_box_component.rb
@@ -0,0 +1,69 @@
+#frozen_string_literal: true
+
+class SS::FileSelectBoxComponent < ApplicationComponent
+  include ActiveModel::Model
+
+  attr_accessor :ss_mode, :cur_site, :cur_user, :cur_node, :page, :item, :html_editor_id
+  attr_writer :field_name, :selection_type, :files, :upload_api_path, :file_api_path, :select_api_path, :view_api_path,
+    :show_attach, :show_opendata
+
+  def field_name
+    @field_name ||= "item[file_ids][]"
+  end
+
+  def selection_type
+    @selection_type ||= "append"
+  end
+
+  def files
+    @files ||= item.files.reorder(id: -1)
+  end
+
+  def upload_api_path
+    @upload_api_path ||= begin
+      if @ss_mode == :cms
+        view_context.cms_frames_temp_files_uploads_path(site: cur_site, cid: cur_node || "-")
+      else
+        view_context.sns_frames_temp_files_uploads_path(user: cur_user)
+      end
+    end
+  end
+
+  def file_api_path
+    @file_api_path ||= begin
+      if @ss_mode == :cms
+        view_context.cms_frames_temp_files_files_path(site: cur_site, cid: cur_node || "-")
+      else
+        view_context.sns_frames_temp_files_files_path(site: cur_site, cid: cur_node || "-")
+      end
+    end
+  end
+
+  def select_api_path
+    @select_api_path ||= begin
+      if @ss_mode == :cms
+        view_context.select_cms_frames_temp_files_file_path(site: cur_site, cid: cur_node || "-", id: ':id')
+      else
+        view_context.sns_cms_frames_temp_files_file_path(site: cur_site, cid: cur_node || "-", id: ':id')
+      end
+    end
+  end
+
+  def view_api_path
+    @view_api_path ||= begin
+      if @ss_mode == :cms
+        view_context.view_cms_apis_content_file_path(id: ":id")
+      end
+    end
+  end
+
+  def show_attach
+    return @show_attach if instance_variable_defined?(:@show_attach)
+    true
+  end
+
+  def show_opendata
+    return @show_opendata if instance_variable_defined?(:@show_opendata)
+    true
+  end
+end
