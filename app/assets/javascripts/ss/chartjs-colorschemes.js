2448ed4789c6e07f285c82dc7b1342b2ef6aa491
itowtips <ito@web-tips.co.jp>
2025-03-18 18:39:12 +0900

    [modify] ss/chartjs-colorschemes

--- app/assets/javascripts/ss/chartjs-colorschemes.js
+++ app/assets/javascripts/ss/chartjs-colorschemes.js
@@ -1,4 +1,6 @@
 //= require_self
 //= require chartjs-plugin-colorschemes-v3/dist/chartjs-plugin-colorschemes-v3.js
 
-var helpers = Chart.helpers;
+// chartjs-plugin-colorschemes-v3 に問題があり helpers をグローバル変数として定義しておかないとプラグイン内でエラーが発生する
+// 現状 colorschemes を利用している箇所はオープンデータのグラフプレビューのみ
+window.helpers = Chart.helpers;
