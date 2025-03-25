0219be99fed6731b6a10a30741113619b831db3d
NAKANO Hideo <nakano@web-tips.co.jp>
2025-03-25 10:42:01 +0900

    [add] special use case

--- spec/features/cms/agents/pages/theme_spec.rb
+++ spec/features/cms/agents/pages/theme_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe "theme/public_filter", type: :feature, dbscope: :example, js: true do
   let!(:site) { cms_site }
-  let!(:part1) { create :accessibility_tool, cur_site: site }
-  let!(:part2) { create :accessibility_tool_compat1, cur_site: site }
-  let!(:part3) { create :accessibility_tool_compat2, cur_site: site }
-  let!(:layout) { create_cms_layout part1, part2, part3 }
   let!(:item) do
     page_html = <<~HTML
       <div id="content">
@@ -30,126 +26,213 @@ describe "theme/public_filter", type: :feature, dbscope: :example, js: true do
     )
   end
 
-  it do
-    ::FileUtils.rm_f item.path
+  context "usual case" do
+    let!(:part1) { create :accessibility_tool, cur_site: site }
+    let!(:part2) { create :accessibility_tool_compat1, cur_site: site }
+    let!(:part3) { create :accessibility_tool_compat2, cur_site: site }
+    let!(:layout) { create_cms_layout part1, part2, part3 }
 
-    visit item.full_url
-    wait_for_all_themes_ready
-    expect(page).to have_css(".accessibility__tool-wrap", count: 3)
-    expect(page).to have_css("body[data-ss-theme='#{theme_white.class_name}']")
-    within all(".accessibility__tool-wrap")[0] do
-      expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
-      expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
-      expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+    it do
+      ::FileUtils.rm_f item.path
 
-      expect(page).to have_css(".#{theme_white.class_name}.active")
-      expect(page).to have_no_css(".#{theme_blue.class_name}.active")
-      expect(page).to have_no_css(".#{theme_black.class_name}.active")
+      visit item.full_url
+      wait_for_all_themes_ready
+      expect(page).to have_css(".accessibility__tool-wrap", count: 3)
+      expect(page).to have_css("body[data-ss-theme='#{theme_white.class_name}']")
+      within all(".accessibility__tool-wrap")[0] do
+        expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
+        expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
+        expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+
+        expect(page).to have_css(".#{theme_white.class_name}.active")
+        expect(page).to have_no_css(".#{theme_blue.class_name}.active")
+        expect(page).to have_no_css(".#{theme_black.class_name}.active")
+      end
+      within all(".accessibility__tool-wrap")[1] do
+        expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
+        expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
+        expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+
+        expect(page).to have_css(".#{theme_white.class_name}.active")
+        expect(page).to have_no_css(".#{theme_blue.class_name}.active")
+        expect(page).to have_no_css(".#{theme_black.class_name}.active")
+      end
+      within all(".accessibility__tool-wrap")[2] do
+        expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
+        expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
+        expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+
+        expect(page).to have_css(".#{theme_white.class_name}.active")
+        expect(page).to have_no_css(".#{theme_blue.class_name}.active")
+        expect(page).to have_no_css(".#{theme_black.class_name}.active")
+      end
+
+      within all(".accessibility__tool-wrap")[0] do
+        click_on theme_blue.name
+      end
+
+      expect(page).to have_css("body[data-ss-theme='#{theme_blue.class_name}']")
+      within all(".accessibility__tool-wrap")[0] do
+        expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
+        expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
+        expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+
+        expect(page).to have_no_css(".#{theme_white.class_name}.active")
+        expect(page).to have_css(".#{theme_blue.class_name}.active")
+        expect(page).to have_no_css(".#{theme_black.class_name}.active")
+      end
+      within all(".accessibility__tool-wrap")[1] do
+        expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
+        expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
+        expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+
+        expect(page).to have_no_css(".#{theme_white.class_name}.active")
+        expect(page).to have_css(".#{theme_blue.class_name}.active")
+        expect(page).to have_no_css(".#{theme_black.class_name}.active")
+      end
+      within all(".accessibility__tool-wrap")[2] do
+        expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
+        expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
+        expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+
+        expect(page).to have_no_css(".#{theme_white.class_name}.active")
+        expect(page).to have_css(".#{theme_blue.class_name}.active")
+        expect(page).to have_no_css(".#{theme_black.class_name}.active")
+      end
+
+      # reload したり別ページに遷移した場合、セッションから選択が復元されるはず
+      visit item.full_url
+      wait_for_all_themes_ready
+      expect(page).to have_css(".accessibility__tool-wrap", count: 3)
+      expect(page).to have_css("body[data-ss-theme='#{theme_blue.class_name}']")
+      within all(".accessibility__tool-wrap")[0] do
+        expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
+        expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
+        expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+
+        expect(page).to have_no_css(".#{theme_white.class_name}.active")
+        expect(page).to have_css(".#{theme_blue.class_name}.active")
+        expect(page).to have_no_css(".#{theme_black.class_name}.active")
+      end
+      within all(".accessibility__tool-wrap")[1] do
+        expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
+        expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
+        expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+
+        expect(page).to have_no_css(".#{theme_white.class_name}.active")
+        expect(page).to have_css(".#{theme_blue.class_name}.active")
+        expect(page).to have_no_css(".#{theme_black.class_name}.active")
+      end
+      within all(".accessibility__tool-wrap")[2] do
+        expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
+        expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
+        expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+
+        expect(page).to have_no_css(".#{theme_white.class_name}.active")
+        expect(page).to have_css(".#{theme_blue.class_name}.active")
+        expect(page).to have_no_css(".#{theme_black.class_name}.active")
+      end
+
+      within all(".accessibility__tool-wrap")[1] do
+        click_on theme_black.name
+      end
+
+      expect(page).to have_css("body[data-ss-theme='#{theme_black.class_name}']")
+      within all(".accessibility__tool-wrap")[0] do
+        expect(page).to have_no_css(".#{theme_white.class_name}.active")
+        expect(page).to have_no_css(".#{theme_blue.class_name}.active")
+        expect(page).to have_css(".#{theme_black.class_name}.active")
+      end
+      within all(".accessibility__tool-wrap")[1] do
+        expect(page).to have_no_css(".#{theme_white.class_name}.active")
+        expect(page).to have_no_css(".#{theme_blue.class_name}.active")
+        expect(page).to have_css(".#{theme_black.class_name}.active")
+      end
+      within all(".accessibility__tool-wrap")[2] do
+        expect(page).to have_no_css(".#{theme_white.class_name}.active")
+        expect(page).to have_no_css(".#{theme_blue.class_name}.active")
+        expect(page).to have_css(".#{theme_black.class_name}.active")
+      end
     end
-    within all(".accessibility__tool-wrap")[1] do
-      expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
-      expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
-      expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+  end
 
-      expect(page).to have_css(".#{theme_white.class_name}.active")
-      expect(page).to have_no_css(".#{theme_blue.class_name}.active")
-      expect(page).to have_no_css(".#{theme_black.class_name}.active")
+  context "special case" do
+    let!(:part1) do
+      html = <<~HTML
+        <div class="accessibility__theme">背景色
+          <div data-tool="ss-theme" data-tool-type="button">
+            <div class="on-pc">
+              <button type="button" class="white">#{theme_white.name}</button>
+              <span class="separator">-</span>
+              <button type="button" class="blue">#{theme_blue.name}</button>
+              <span class="separator">-</span>
+              <button type="button" class="black">#{theme_black.name}</button>
+            </div>
+            <div class="on-mobile">
+              <button type="button" class="white">
+                <span class="material-icons-outlined" aria-label="#{theme_white.name}" role="img">palette</span>
+              </button>
+              <button type="button" class="blue">
+                <span class="material-icons-outlined" aria-label="#{theme_blue.name}" role="img">palette</span>
+              </button>
+              <button type="button" class="black">
+                <span class="material-icons-outlined" aria-label="#{theme_black.name}" role="img">palette</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      HTML
+      create :cms_part_free, cur_site: site, html: html
     end
-    within all(".accessibility__tool-wrap")[2] do
-      expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
-      expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
-      expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+    let!(:layout) { create_cms_layout part1 }
 
-      expect(page).to have_css(".#{theme_white.class_name}.active")
-      expect(page).to have_no_css(".#{theme_blue.class_name}.active")
-      expect(page).to have_no_css(".#{theme_black.class_name}.active")
-    end
+    it do
+      ::FileUtils.rm_f item.path
 
-    within all(".accessibility__tool-wrap")[0] do
-      click_on theme_blue.name
-    end
+      visit item.full_url
+      wait_for_all_themes_ready
+      expect(page).to have_css(".accessibility__theme", count: 1)
 
-    expect(page).to have_css("body[data-ss-theme='#{theme_blue.class_name}']")
-    within all(".accessibility__tool-wrap")[0] do
-      expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
-      expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
-      expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+      within ".on-pc" do
+        expect(page).to have_css(".#{theme_white.class_name}.active", text: theme_white.name)
+        expect(page).to have_css(".#{theme_blue.class_name}:not(.active)", text: theme_blue.name)
+        expect(page).to have_css(".#{theme_black.class_name}:not(.active)", text: theme_black.name)
+      end
+      within ".on-mobile" do
+        expect(page).to have_css(".#{theme_white.class_name}.active [aria-label='#{theme_white.name}']")
+        expect(page).to have_css(".#{theme_blue.class_name}:not(.active) [aria-label='#{theme_blue.name}']")
+        expect(page).to have_css(".#{theme_black.class_name}:not(.active) [aria-label='#{theme_black.name}']")
+      end
 
-      expect(page).to have_no_css(".#{theme_white.class_name}.active")
-      expect(page).to have_css(".#{theme_blue.class_name}.active")
-      expect(page).to have_no_css(".#{theme_black.class_name}.active")
-    end
-    within all(".accessibility__tool-wrap")[1] do
-      expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
-      expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
-      expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+      within ".on-pc" do
+        click_on theme_blue.name
+      end
 
-      expect(page).to have_no_css(".#{theme_white.class_name}.active")
-      expect(page).to have_css(".#{theme_blue.class_name}.active")
-      expect(page).to have_no_css(".#{theme_black.class_name}.active")
-    end
-    within all(".accessibility__tool-wrap")[2] do
-      expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
-      expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
-      expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
+      within ".on-pc" do
+        expect(page).to have_css(".#{theme_white.class_name}:not(.active)", text: theme_white.name)
+        expect(page).to have_css(".#{theme_blue.class_name}.active", text: theme_blue.name)
+        expect(page).to have_css(".#{theme_black.class_name}:not(.active)", text: theme_black.name)
+      end
+      within ".on-mobile" do
+        expect(page).to have_css(".#{theme_white.class_name}:not(.active) [aria-label='#{theme_white.name}']")
+        expect(page).to have_css(".#{theme_blue.class_name}.active [aria-label='#{theme_blue.name}']")
+        expect(page).to have_css(".#{theme_black.class_name}:not(.active) [aria-label='#{theme_black.name}']")
+      end
 
-      expect(page).to have_no_css(".#{theme_white.class_name}.active")
-      expect(page).to have_css(".#{theme_blue.class_name}.active")
-      expect(page).to have_no_css(".#{theme_black.class_name}.active")
-    end
+      within ".on-mobile" do
+        click_on theme_black.name
+      end
 
-    # reload したり別ページに遷移した場合、セッションから選択が復元されるはず
-    visit item.full_url
-    wait_for_all_themes_ready
-    expect(page).to have_css(".accessibility__tool-wrap", count: 3)
-    expect(page).to have_css("body[data-ss-theme='#{theme_blue.class_name}']")
-    within all(".accessibility__tool-wrap")[0] do
-      expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
-      expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
-      expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
-
-      expect(page).to have_no_css(".#{theme_white.class_name}.active")
-      expect(page).to have_css(".#{theme_blue.class_name}.active")
-      expect(page).to have_no_css(".#{theme_black.class_name}.active")
-    end
-    within all(".accessibility__tool-wrap")[1] do
-      expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
-      expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
-      expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
-
-      expect(page).to have_no_css(".#{theme_white.class_name}.active")
-      expect(page).to have_css(".#{theme_blue.class_name}.active")
-      expect(page).to have_no_css(".#{theme_black.class_name}.active")
-    end
-    within all(".accessibility__tool-wrap")[2] do
-      expect(page).to have_css(".#{theme_white.class_name}", text: theme_white.name)
-      expect(page).to have_css(".#{theme_blue.class_name}", text: theme_blue.name)
-      expect(page).to have_css(".#{theme_black.class_name}", text: theme_black.name)
-
-      expect(page).to have_no_css(".#{theme_white.class_name}.active")
-      expect(page).to have_css(".#{theme_blue.class_name}.active")
-      expect(page).to have_no_css(".#{theme_black.class_name}.active")
-    end
-
-    within all(".accessibility__tool-wrap")[1] do
-      click_on theme_black.name
-    end
-
-    expect(page).to have_css("body[data-ss-theme='#{theme_black.class_name}']")
-    within all(".accessibility__tool-wrap")[0] do
-      expect(page).to have_no_css(".#{theme_white.class_name}.active")
-      expect(page).to have_no_css(".#{theme_blue.class_name}.active")
-      expect(page).to have_css(".#{theme_black.class_name}.active")
-    end
-    within all(".accessibility__tool-wrap")[1] do
-      expect(page).to have_no_css(".#{theme_white.class_name}.active")
-      expect(page).to have_no_css(".#{theme_blue.class_name}.active")
-      expect(page).to have_css(".#{theme_black.class_name}.active")
-    end
-    within all(".accessibility__tool-wrap")[2] do
-      expect(page).to have_no_css(".#{theme_white.class_name}.active")
-      expect(page).to have_no_css(".#{theme_blue.class_name}.active")
-      expect(page).to have_css(".#{theme_black.class_name}.active")
+      within ".on-pc" do
+        expect(page).to have_css(".#{theme_white.class_name}:not(.active)", text: theme_white.name)
+        expect(page).to have_css(".#{theme_blue.class_name}:not(.active)", text: theme_blue.name)
+        expect(page).to have_css(".#{theme_black.class_name}.active", text: theme_black.name)
+      end
+      within ".on-mobile" do
+        expect(page).to have_css(".#{theme_white.class_name}:not(.active) [aria-label='#{theme_white.name}']")
+        expect(page).to have_css(".#{theme_blue.class_name}:not(.active) [aria-label='#{theme_blue.name}']")
+        expect(page).to have_css(".#{theme_black.class_name}.active [aria-label='#{theme_black.name}']")
+      end
     end
   end
 end
