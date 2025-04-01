e3a023703ab795613ad96b29dbf9f86b3d968eef
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-04-01 19:49:09 +0900

    ステータス、HTML種別追加/画面表示OK

--- app/models/concerns/cms/addon/html.rb
+++ app/models/concerns/cms/addon/html.rb
@@ -5,16 +5,31 @@ module Cms::Addon
 
     included do
       field :html, type: String
-      permit_params :html
+      field :html_format, type: String
+      field :html_format_options, type: Array, default: %w[shirasagi]
 
-      validate :validate_script_tag
+      permit_params :html, :html_format
+
+      validates :html_format, inclusion: { in: %w(shirasagi liquid), allow_blank: true }
     end
 
-    private
+    def html_format_options
+      self.class.use_html_format.map do |v|
+        [ I18n.t("cms.options.loop_format.#{v}"), v ]
+      end
+    end
 
-    def validate_script_tag
-      if html.present?
-        errors.add(:html, "はscriptタグを含めることはできません。") if html.include?("<script")
+    def html_format_shirasagi?
+      html_format == "shirasagi"
+    end
+
+    def html_format_liquid?
+      html_format == "liquid"
+    end
+
+    module ClassMethods
+      def use_html_format
+        %w(shirasagi liquid)
       end
     end
   end
