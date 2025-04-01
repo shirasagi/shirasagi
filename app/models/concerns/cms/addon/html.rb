98dc6b999c26098e11fb781f0929b7ced69961fa
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-04-01 13:57:20 +0900

    scriptタグの入力エラー出力

--- app/models/concerns/cms/addon/html.rb
+++ app/models/concerns/cms/addon/html.rb
@@ -6,6 +6,14 @@ module Cms::Addon
     included do
       field :html, type: String
       permit_params :html
+
+      validate :validate_script_tag
+    end
+
+    def validate_script_tag
+      if html.present?
+        errors.add(:html, "はscriptタグを含めることはできません。") if html.include?("<script")
+      end
     end
   end
 end
