70880e05da4e61c9644a96610bd0a3763924154a
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-04-01 19:49:09 +0900

    ステータス、HTML種別追加/画面表示OK

--- app/models/cms/loop_setting.rb
+++ app/models/cms/loop_setting.rb
@@ -11,6 +11,7 @@ class Cms::LoopSetting
   field :name, type: String
   field :description, type: String
   field :order, type: Integer
+  field :state, type: String, default: "public"
   permit_params :name, :description, :order
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, length: { maximum: 400 }
@@ -29,6 +30,13 @@ class Cms::LoopSetting
         criteria = criteria.keyword_in params[:keyword], :name, :html
       end
       criteria
+    end
+
+    def options_for_state
+      [
+        [I18n.t('ss.options.state.public'), 'public'],
+        [I18n.t('ss.options.state.closed'), 'closed']
+      ]
     end
   end
 end
