c02264146ab47d63217bd59a9cd8892a1c0701d2
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-04-03 17:30:51 +0900

    広告バナーフォルダのlink_urlにフォルダパスURL入力を許可

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -6,14 +6,27 @@ class UrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
 
-    uri = ::Addressable::URI.parse(value)
-
-    allowed_schemes = options[:scheme].try { |scheme| Array[scheme].flatten.map(&:to_s) } || ALLOWED_SCHEMES
-    if !allowed_schemes.include?(uri.scheme)
-      record.errors.add(attribute, options[:message] || :url)
-      return
+    # 関連するファイルが存在する場合、任意のパスを許可
+    if record.respond_to?(:file) && record.file.present?
+      return if value.start_with?("/")
     end
-  rescue
-    record.errors.add(attribute, options[:message] || :url)
+
+    begin
+      uri = ::Addressable::URI.parse(value)
+
+      # 絶対パスの場合の検証
+      if options[:absolute_path] && uri.scheme.blank? && value.start_with?("/")
+        return
+      end
+
+      # URLスキームの検証
+      allowed_schemes = options[:scheme].try { |scheme| Array[scheme].flatten.map(&:to_s) } || ALLOWED_SCHEMES
+      if !allowed_schemes.include?(uri.scheme)
+        record.errors.add(attribute, options[:message] || :url)
+        return
+      end
+    rescue
+      record.errors.add(attribute, options[:message] || :url)
+    end
   end
 end
