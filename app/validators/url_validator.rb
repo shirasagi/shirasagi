4ff2b6e01862538ac0f8ad3abf1ba35bfd097691
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-04-07 15:40:24 +0900

    fix app/validators/url_validator.rb

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -6,22 +6,20 @@ class UrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
 
-    begin
-      uri = ::Addressable::URI.parse(value)
+    uri = ::Addressable::URI.parse(value)
 
-      # 絶対パスの場合の検証
-      if options[:absolute_path] && uri.scheme.blank? && value.start_with?("/")
-        return
-      end
-
-      # URLスキームの検証
-      allowed_schemes = options[:scheme].try { |scheme| Array[scheme].flatten.map(&:to_s) } || ALLOWED_SCHEMES
-      if !allowed_schemes.include?(uri.scheme)
-        record.errors.add(attribute, options[:message] || :url)
-        return
-      end
-    rescue
-      record.errors.add(attribute, options[:message] || :url)
+    # 絶対パスの場合の検証
+    if options[:absolute_path] && uri.scheme.blank? && value.start_with?("/")
+      return
     end
+
+    # URLスキームの検証
+    allowed_schemes = options[:scheme].try { |scheme| Array[scheme].flatten.map(&:to_s) } || ALLOWED_SCHEMES
+    if !allowed_schemes.include?(uri.scheme)
+      record.errors.add(attribute, options[:message] || :url)
+      return
+    end
+  rescue
+    record.errors.add(attribute, options[:message] || :url)
   end
 end
