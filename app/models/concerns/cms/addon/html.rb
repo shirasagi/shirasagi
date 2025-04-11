90ee8b65e73f99ce6e8bdd6669c3c35c8890f4e6
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-04-11 15:26:19 +0900

    不要コード、ファイルの削除

--- app/models/concerns/cms/addon/html.rb
+++ app/models/concerns/cms/addon/html.rb
@@ -7,18 +7,5 @@ module Cms::Addon
       field :html, type: String
       permit_params :html
     end
-
-    def html_format
-      return "shirasagi" unless respond_to?(:loop_setting)
-      loop_setting.try(:html_format) || "shirasagi"
-    end
-
-    def html_format_shirasagi?
-      html_format == "shirasagi"
-    end
-
-    def html_format_liquid?
-      html_format == "liquid"
-    end
   end
 end
