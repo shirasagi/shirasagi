bf51e5c6bb791deb7ccfd2e68d3c985d67f837dd
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-04-15 11:12:48 +0900

    html_customアドオン表示OK,500エラー解消

--- app/models/cms/loop_setting.rb
+++ app/models/cms/loop_setting.rb
@@ -17,8 +17,6 @@ class Cms::LoopSetting
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, length: { maximum: 400 }
 
-  attr_accessor :html_format
-
   default_scope -> { order_by(order: 1, name: 1) }
 
   class << self
@@ -40,6 +38,10 @@ class Cms::LoopSetting
         [I18n.t('ss.options.state.public'), 'public'],
         [I18n.t('ss.options.state.closed'), 'closed']
       ]
+    end
+
+    def html_format_options
+      Cms::Addon::HtmlCustom.html_format_options
     end
   end
 end
