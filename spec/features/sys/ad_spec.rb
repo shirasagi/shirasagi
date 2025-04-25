00ecfe3101eb85b681f6b77b92ce014aafcf7709
NAKANO Hideo <nakano@web-tips.co.jp>
2025-04-25 10:52:00 +0900

    ログイン画面広告 / 広告ポートレットへユーザーファイルを添付するテストを追加

--- spec/features/sys/ad_spec.rb
+++ spec/features/sys/ad_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe "sys_ad", type: :feature, dbscope: :example, js: true do
+  let!(:user) { sys_user }
+
   context "basic crud" do
-    let!(:user) { sys_user }
     let!(:file1) do
       tmp_ss_file(basename: "logo-#{unique_id}.png", contents: "#{Rails.root}/spec/fixtures/ss/logo.png", user: user)
     end
@@ -25,7 +26,6 @@ describe "sys_ad", type: :feature, dbscope: :example, js: true do
       # New
       #
       login_user user, to: sys_ad_path
-      visit sys_ad_path
       click_on I18n.t("ss.links.edit")
 
       within "form#item-form" do
@@ -147,6 +147,100 @@ describe "sys_ad", type: :feature, dbscope: :example, js: true do
             expect(ad_link.state).to eq "show"
           end
         end
+      end
+    end
+  end
+
+  context "when ss/user_file is attached" do
+    let(:time1) { rand(5..10) }
+    let(:width1) { rand(200..300) }
+    let(:name1) { "name-#{unique_id}" }
+    let(:url1) { unique_url }
+    let(:content) { Rails.root.join("spec", "fixtures", "ss", "logo.png").to_s }
+    let!(:file) do
+      tmp_ss_file(SS::UserFile, model: SS::UserFile::FILE_MODEL, user: user, contents: content)
+    end
+
+    before do
+      @save_file_upload_dialog = SS.file_upload_dialog
+      SS.file_upload_dialog = :v2
+
+      ActionMailer::Base.deliveries.clear
+    end
+
+    after do
+      SS.file_upload_dialog = @save_file_upload_dialog
+
+      ActionMailer::Base.deliveries.clear
+    end
+
+    it do
+      login_user user, to: sys_ad_path
+      click_on I18n.t("ss.links.edit")
+      within 'form#item-form' do
+        fill_in "item[time]", with: time1
+        fill_in "item[width]", with: width1
+
+        within "[data-index='new']" do
+          wait_for_cbox_opened { click_on I18n.t("ss.buttons.upload") }
+        end
+      end
+      wait_for_event_fired "turbo:frame-load" do
+        within_dialog do
+          within ".cms-tabs" do
+            click_on I18n.t("ss.buttons.select_from_list")
+          end
+        end
+      end
+      within_dialog do
+        wait_for_event_fired "turbo:frame-load" do
+          within "form.search" do
+            check I18n.t("sns.user_file")
+          end
+        end
+      end
+      within_dialog do
+        expect(page).to have_css('.file-view', text: file.name)
+        wait_for_cbox_closed { click_on file.name }
+      end
+      within "form#item-form" do
+        within "[data-index='new']" do
+          expect(page).to have_css(".humanized-name", text: file.humanized_name)
+          fill_in "item[ad_links][][name]", with: name1
+          fill_in "item[ad_links][][url]", with: url1
+        end
+
+        click_on I18n.t('ss.buttons.save')
+      end
+      wait_for_notice I18n.t('ss.notice.saved')
+
+      Sys::Setting.first.tap do |setting|
+        expect(setting.time).to eq time1
+        expect(setting.width).to eq width1
+
+        setting.ad_links.to_a.tap do |ad_links|
+          expect(ad_links).to have(1).items
+          ad_links[0].tap do |ad_link|
+            expect(ad_link.id).to be_present
+            expect(ad_link.name).to eq name1
+            expect(ad_link.url).to eq url1
+            expect(ad_link.target).to eq "_blank"
+            expect(ad_link.file).to be_present
+            expect(ad_link.state).to eq "show"
+
+            attached_file = ad_link.file
+            expect(attached_file.id).not_to eq file.id
+            expect(attached_file.model).to eq setting.class.name.underscore
+            expect(attached_file.owner_item_type).to eq setting.class.name
+            expect(attached_file.owner_item_id.to_s).to eq setting.id.to_s
+            expect(attached_file.state).to eq "public"
+          end
+        end
+      end
+      SS::File.find(file.id).tap do |after_attached|
+        expect(after_attached.model).to eq file.model
+        expect(after_attached.owner_item_id).to be_blank
+        expect(after_attached.owner_item_type).to be_blank
       end
     end
   end
