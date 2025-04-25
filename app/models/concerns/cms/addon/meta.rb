24b841bde4892056186b0f0224a6070719acf0d5
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-04-25 11:44:26 +0900

    不要コードの削除

--- app/models/concerns/cms/addon/meta.rb
+++ app/models/concerns/cms/addon/meta.rb
@@ -10,8 +10,6 @@ module Cms::Addon
       field :description_setting, type: String
       permit_params :keywords, :description, :summary_html, :description_setting
 
-      after_validation :set_default_description_setting, if: -> { description_setting.blank? }
-
       before_save :set_keywords, if: ->{ @cur_site && @cur_site.auto_keywords_enabled? }
       before_save :set_description, if: ->{ @cur_site && @cur_site.auto_description_enabled? }
       before_save :template_variable_handler_description, if: -> { description_setting_auto? }
