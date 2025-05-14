b36ebea86e439ee2b9065cdebbc46c13538b09d2
itowtips <ito@web-tips.co.jp>
2025-05-14 18:34:07 +0900

    [modify] cms/node : add thumb addon (#5576)
    * [modify] cms/node : add thumb addon
    
    * [modify] cms/layout : add page_thumb.src
    
    * [modify] article/node, event/node, faq/node : thumb addon

--- app/models/cms/node.rb
+++ app/models/cms/node.rb
@@ -21,6 +21,7 @@ class Cms::Node
     include Cms::Model::Node
     include Cms::Addon::NodeSetting
     include Cms::Addon::Meta
+    include Cms::Addon::Thumb
     include Cms::Addon::EditorSetting
     include Cms::Addon::NodeTwitterPostSetting
     include Cms::Addon::NodeLinePostSetting
@@ -41,6 +42,7 @@ class Cms::Node
     include Cms::Model::Node
     include Cms::Addon::NodeSetting
     include Cms::Addon::Meta
+    include Cms::Addon::Thumb
     include Cms::Addon::EditorSetting
     include Cms::Addon::NodeTwitterPostSetting
     include Cms::Addon::NodeLinePostSetting
