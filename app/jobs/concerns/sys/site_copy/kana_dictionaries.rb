fb1a884d4018084a08be5436a340322d7fb27ada
NAKANO Hideo <nakano@web-tips.co.jp>
2025-05-14 16:09:59 +0900

    [fixup] remove permission level completely (#5558) (#5634)
    * [fix] failed copying site with roles having permission_level
    
    * [fix] same codes

--- app/jobs/concerns/sys/site_copy/kana_dictionaries.rb
+++ app/jobs/concerns/sys/site_copy/kana_dictionaries.rb
@@ -7,7 +7,9 @@ module Sys::SiteCopy::KanaDictionaries
       begin
         src_dictionary = Kana::Dictionary.find(src_dictionary_id)
         Rails.logger.debug("#{src_dictionary.name}(#{src_dictionary.id}): 辞書のコピーを開始します。")
-        dest_dictionary = Kana::Dictionary.new src_dictionary.attributes.except("id", "_id", "site_id", "created", "updated")
+        attributes = src_dictionary.attributes.slice(*Kana::Dictionary.fields.keys)
+        attributes = attributes.except("id", "_id", "site_id", "created", "updated")
+        dest_dictionary = Kana::Dictionary.new attributes
         dest_dictionary.cur_site = @dest_site
         dest_dictionary.site_id = @dest_site.id
         dest_dictionary.save!
