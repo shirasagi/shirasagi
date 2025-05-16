96b052a37ec3772ddca547cd9d95ce55b83082d7
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-05-16 18:16:55 +0900

    コメント追加

--- spec/components/cms/syntax_check_detail_box_component_spec.rb
+++ spec/components/cms/syntax_check_detail_box_component_spec.rb
@@ -22,6 +22,7 @@ RSpec.describe Cms::SyntaxCheckDetailBoxComponent, type: :component do
   let(:syntax_checker) { double('SyntaxChecker', errors: errors) }
   let(:empty_syntax_checker) { double('SyntaxChecker', errors: []) }
 
+  # エラーが存在する場合にコンポーネントが表示されることを確認するテスト
   it 'displays when errors are present' do
     render_inline described_class.new(syntax_checker: syntax_checker)
     expect(page).to have_css('#errorSyntaxChecker')
@@ -30,34 +31,40 @@ RSpec.describe Cms::SyntaxCheckDetailBoxComponent, type: :component do
     expect(page).to have_css('.btn-auto-correct', count: 1)
   end
 
+  # エラーが存在しない場合にコンポーネントが表示されないことを確認するテスト
   it 'does not display when there are no errors' do
     render_inline described_class.new(syntax_checker: empty_syntax_checker)
     expect(page).not_to have_css('#errorSyntaxChecker')
   end
 
+  # エラーの詳細情報が表示されることを確認するテスト
   it 'shows details' do
     render_inline described_class.new(syntax_checker: syntax_checker)
     expect(page).to have_content(I18n.t('errors.messages.syntax_check_detail.invalid_order_of_h'))
     expect(page).to have_content('詳細2')
   end
 
+  # 自動修正ボタンがcollectorが存在する場合のみ表示されることを確認するテスト
   it 'shows auto-correct button only when collector is present' do
     render_inline described_class.new(syntax_checker: syntax_checker)
     expect(page).to have_css('.btn-auto-correct', count: 1)
   end
 
+  # エラーコードがcodeタグ内に表示されることを確認するテスト
   it 'displays error code in <code> tag' do
     render_inline described_class.new(syntax_checker: syntax_checker)
     expect(page).to have_css('code', text: 'E001')
     expect(page).to have_css('code', text: 'E002')
   end
 
+  # IDがcolumn-nameクラス付きで表示されることを確認するテスト
   it 'displays id with .column-name class' do
     render_inline described_class.new(syntax_checker: syntax_checker)
     expect(page).to have_css('.column-name', text: 'html')
     expect(page).to have_css('.column-name', text: 'body')
   end
 
+  # 詳細情報が文字列の場合でも正しく表示されることを確認するテスト
   it 'shows detail even if it is a string' do
     errors = [
       {
@@ -73,6 +80,7 @@ RSpec.describe Cms::SyntaxCheckDetailBoxComponent, type: :component do
     expect(page).to have_css('li', text: Array(I18n.t('errors.messages.syntax_check_detail.invalid_order_of_h')).first)
   end
 
+  # 自動修正ボタンが正しい属性を持つことを確認するテスト
   it 'auto-correct button has correct attributes' do
     render_inline described_class.new(syntax_checker: syntax_checker)
     btn = page.find('.btn-auto-correct')
