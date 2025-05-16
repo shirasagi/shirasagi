d5dd4ce9dfe97f1741be41e9df4115e4f5f2c47f
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-05-16 15:54:44 +0900

    auto_correctメソッド追加、動作確認OK

--- app/controllers/cms/layouts_controller.rb
+++ app/controllers/cms/layouts_controller.rb
@@ -1,14 +1,14 @@
 class Cms::LayoutsController < ApplicationController
   include Cms::BaseFilter
   include Cms::CrudFilter
-  include Cms::LayoutsHelper
+
+  helper Cms::LayoutsHelper
 
   model Cms::Layout
 
   navi_view "cms/main/navi"
 
   before_action :set_tree_navi, only: [:index]
-  before_action :syntax_check, only: [:create, :update]
 
   private
 
@@ -21,10 +21,8 @@ class Cms::LayoutsController < ApplicationController
   end
 
   def syntax_check
-    contents = [{ "id" => "html", "content" => [@item.html], "resolve" => "html", "type" => "array" }]
-
+    contents = [{ "id" => "html", "content" => @item.html, "resolve" => "html", "type" => "scalar" }]
     @syntax_checker = Cms::SyntaxChecker.check(cur_site: @cur_site, cur_user: @cur_user, contents: contents)
-
     if @syntax_checker.errors.present?
       @syntax_checker.errors.each do |error|
         @item.errors.add :base, error[:msg]
@@ -32,6 +30,60 @@ class Cms::LayoutsController < ApplicationController
       return false
     end
     true
+  end
+
+  def auto_correct
+    error_index = params[:auto_correct].to_i
+    @item.errors.clear
+
+    # 構文チェックを実行
+    contents = [{ "id" => "html", "content" => @item.html, "resolve" => "html", "type" => "scalar" }]
+    @syntax_checker = Cms::SyntaxChecker.check(cur_site: @cur_site, cur_user: @cur_user, contents: contents)
+
+    before_html = @item.html
+    @syntax_checker.errors.each_with_index do |error, idx|
+      next unless idx == error_index
+      next unless error[:collector].present?
+
+      Rails.logger.debug("[auto_correct] 修正前HTML: #{before_html.inspect}")
+
+      case error[:collector]
+      when "Cms::SyntaxChecker::InterwordSpaceChecker"
+        content_html = error[:code]
+      else
+        content_html = @item.html
+      end
+
+      corrected = Cms::SyntaxChecker.correct(
+        cur_site: @cur_site,
+        cur_user: @cur_user,
+        content: {
+          "content" => content_html,
+          "resolve" => "html",
+          "type" => "scalar"
+        },
+        collector: error[:collector],
+        params: (error[:collector_params] || {}).transform_keys(&:to_s)
+      )
+
+      next unless corrected.respond_to?(:result)
+      corrected_html = corrected.result
+
+      Rails.logger.debug("[auto_correct] 修正後HTML: #{corrected_html.inspect}")
+
+      case error[:collector]
+      when "Cms::SyntaxChecker::InterwordSpaceChecker"
+        @item.html = replace_html(before_html, error[:code], corrected_html)
+      else
+        @item.html = corrected_html
+      end
+    end
+  end
+
+  def replace_html(before_html, error_code, corrected_html)
+    pattern = Regexp.new(Regexp.escape(error_code))
+    replaced_text = before_html.gsub(/#{pattern}/, corrected_html)
+    replaced_text
   end
 
   public
@@ -52,12 +104,25 @@ class Cms::LayoutsController < ApplicationController
   def create
     raise "403" unless @model.allowed?(:create, @cur_user, site: @cur_site, node: @cur_node)
     @item = @model.new get_params
+    if params.key?(:auto_correct)
+      auto_correct
+      result = syntax_check
+      render_create result
+      return
+    end
     render_create @item.valid? && syntax_check && @item.save
   end
 
   def update
     raise "403" unless @model.allowed?(:edit, @cur_user, site: @cur_site, node: @cur_node)
     @item.attributes = get_params
-    render_update @item.valid? && syntax_check && @item.save
+    if params.key?(:auto_correct)
+      auto_correct
+      result = syntax_check
+      render_update result
+      return
+    else
+      render_update @item.valid? && syntax_check && @item.save
+    end
   end
 end
