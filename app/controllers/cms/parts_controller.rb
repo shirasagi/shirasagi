8b94bcee7515495b8cf830895014f4f26a69f1e0
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-05-16 16:06:18 +0900

    モジュールを分割

--- app/controllers/cms/parts_controller.rb
+++ app/controllers/cms/parts_controller.rb
@@ -1,6 +1,7 @@
 class Cms::PartsController < ApplicationController
   include Cms::BaseFilter
   include Cms::PartFilter
+  include Cms::SyntaxCheckable
 
   helper Cms::LayoutsHelper
 
@@ -9,7 +10,6 @@ class Cms::PartsController < ApplicationController
   navi_view "cms/main/navi"
 
   before_action :set_tree_navi, only: [:index]
-  before_action :syntax_check, only: [:create, :update]
 
   private
 
@@ -23,72 +23,6 @@ class Cms::PartsController < ApplicationController
 
   def pre_params
     { route: "cms/free" }
-  end
-
-  def syntax_check
-    contents = [{ "id" => "html", "content" => @item.html, "resolve" => "html", "type" => "scalar" }]
-    @syntax_checker = Cms::SyntaxChecker.check(cur_site: @cur_site, cur_user: @cur_user, contents: contents)
-    if @syntax_checker.errors.present?
-      @syntax_checker.errors.each do |error|
-        @item.errors.add :base, error[:msg]
-      end
-      return false
-    end
-    true
-  end
-
-  def auto_correct
-    error_index = params[:auto_correct].to_i
-    @item.errors.clear
-
-    # 構文チェックを実行
-    contents = [{ "id" => "html", "content" => @item.html, "resolve" => "html", "type" => "scalar" }]
-    @syntax_checker = Cms::SyntaxChecker.check(cur_site: @cur_site, cur_user: @cur_user, contents: contents)
-
-    before_html = @item.html
-    @syntax_checker.errors.each_with_index do |error, idx|
-      next unless idx == error_index
-      next unless error[:collector].present?
-
-      Rails.logger.debug("[auto_correct] 修正前HTML: #{before_html.inspect}")
-
-      case error[:collector]
-      when "Cms::SyntaxChecker::InterwordSpaceChecker"
-        content_html = error[:code]
-      else
-        content_html = @item.html
-      end
-
-      corrected = Cms::SyntaxChecker.correct(
-        cur_site: @cur_site,
-        cur_user: @cur_user,
-        content: {
-          "content" => content_html,
-          "resolve" => "html",
-          "type" => "scalar"
-        },
-        collector: error[:collector],
-        params: (error[:collector_params] || {}).transform_keys(&:to_s)
-      )
-
-      next unless corrected.respond_to?(:result)
-      corrected_html = corrected.result
-
-      Rails.logger.debug("[auto_correct] 修正後HTML: #{corrected_html.inspect}")
-
-      case error[:collector]
-      when "Cms::SyntaxChecker::InterwordSpaceChecker"
-        @item.html = replace_html(before_html, error[:code], corrected_html)
-      else
-        @item.html = corrected_html
-      end
-    end
-  end
-
-  def replace_html(before_html, error_code, corrected_html)
-    pattern = Regexp.new(Regexp.escape(error_code))
-    replaced_text = before_html.gsub(/#{pattern}/, corrected_html)
-    replaced_text
   end
 
   public
