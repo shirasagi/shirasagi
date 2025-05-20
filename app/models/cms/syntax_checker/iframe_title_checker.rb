b538a54e63536382dbdb5f9ba5079384a0f49a43
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-05-20 19:03:59 +0900

    frameタグに対応

--- app/models/cms/syntax_checker/iframe_title_checker.rb
+++ app/models/cms/syntax_checker/iframe_title_checker.rb
@@ -2,17 +2,28 @@ class Cms::SyntaxChecker::IframeTitleChecker
   include Cms::SyntaxChecker::Base
 
   def check(context, id, idx, raw_html, fragment)
-    fragment.css('iframe').each do |iframe_node|
-      title = iframe_node['title']
-      next if title.present?
-
-      context.errors << {
-        id: id,
-        idx: idx,
-        code: Cms::SyntaxChecker::Base.outer_html_summary(iframe_node),
-        msg: I18n.t('errors.messages.set_iframe_title'),
-        detail: I18n.t('errors.messages.syntax_check_detail.set_iframe_title')
-      }
+    # frameset内のframeタグも検出できるように、より詳細なセレクタを使用
+    fragment.css('frame, frameset frame').each do |node|
+      check_node(context, id, idx, node)
     end
+
+    fragment.css('iframe').each do |node|
+      check_node(context, id, idx, node)
+    end
+  end
+
+  private
+
+  def check_node(context, id, idx, node)
+    title = node['title']
+    return if title.present?
+
+    context.errors << {
+      id: id,
+      idx: idx,
+      code: Cms::SyntaxChecker::Base.outer_html_summary(node),
+      msg: I18n.t('errors.messages.set_iframe_title'),
+      detail: I18n.t('errors.messages.syntax_check_detail.set_iframe_title')
+    }
   end
 end
--- spec/models/cms/syntax_checker/iframe_title_checker_spec.rb
+++ spec/models/cms/syntax_checker/iframe_title_checker_spec.rb
@@ -3,35 +3,107 @@ require 'spec_helper'
 describe Cms::SyntaxChecker::IframeTitleChecker, type: :model, dbscope: :example do
   let(:site) { cms_site }
   let(:user) { cms_user }
-  let(:content) { "<iframe src='https://www.youtube.com/embed/example'></iframe>" }
-  let(:context) { Cms::SyntaxChecker::CheckerContext.new(site, user, [content], [], false, 0) }
 
   describe "#check" do
-    it "should detect iframe without title" do
-      checker = described_class.new
-      checker.check(context, 1, 1, content, Nokogiri::HTML5.fragment(content))
-      expect(context.errors).to have(1).items
-      expect(context.errors[0][:msg]).to eq I18n.t('errors.messages.set_iframe_title')
+    context "with iframe" do
+      let(:content) { "<iframe src='https://www.youtube.com/embed/example'></iframe>" }
+      let(:context) { Cms::SyntaxChecker::CheckerContext.new(site, user, [content], [], false, 0) }
+
+      it "should detect iframe without title" do
+        checker = described_class.new
+        checker.check(context, 1, 1, content, Nokogiri::HTML5.parse(content))
+        expect(context.errors).to have(1).items
+        expect(context.errors[0][:msg]).to eq I18n.t('errors.messages.set_iframe_title')
+      end
+
+      it "should not detect iframe with title" do
+        content = "<iframe src='https://www.youtube.com/embed/example' title='動画の説明'></iframe>"
+        context = Cms::SyntaxChecker::CheckerContext.new(site, user, [content], [], false, 0)
+        checker = described_class.new
+        checker.check(context, 1, 1, content, Nokogiri::HTML5.parse(content))
+        expect(context.errors).to be_empty
+      end
+
+      it "should detect multiple iframes without title" do
+        content = <<~HTML
+          <!DOCTYPE html>
+          <html>
+            <head></head>
+            <body>
+              <iframe src='https://www.youtube.com/embed/example1'></iframe>
+              <iframe src='https://www.youtube.com/embed/example2' title='動画の説明'></iframe>
+              <iframe src='https://www.youtube.com/embed/example3'></iframe>
+            </body>
+          </html>
+        HTML
+        context = Cms::SyntaxChecker::CheckerContext.new(site, user, [content], [], false, 0)
+        checker = described_class.new
+        doc = Nokogiri::HTML5.parse(content)
+        checker.check(context, 1, 1, content, doc)
+        expect(context.errors).to have(2).items
+        expect(context.errors[0][:msg]).to eq I18n.t('errors.messages.set_iframe_title')
+        expect(context.errors[1][:msg]).to eq I18n.t('errors.messages.set_iframe_title')
+      end
     end
 
-    it "should not detect iframe with title" do
-      content = "<iframe src='https://www.youtube.com/embed/example' title='動画の説明'></iframe>"
-      checker = described_class.new
-      checker.check(context, 1, 1, content, Nokogiri::HTML5.fragment(content))
-      expect(context.errors).to be_empty
-    end
+    context "with frame (legacy)" do
+      let(:content) do
+        <<~HTML
+          <!DOCTYPE html>
+          <html>
+            <head></head>
+            <frameset>
+              <frame src='example.html'>
+            </frameset>
+          </html>
+        HTML
+      end
+      let(:context) { Cms::SyntaxChecker::CheckerContext.new(site, user, [content], [], false, 0) }
 
-    it "should detect multiple iframes without title" do
-      content = <<~HTML
-        <iframe src='https://www.youtube.com/embed/example1'></iframe>
-        <iframe src='https://www.youtube.com/embed/example2'></iframe>
-        <iframe src='https://www.google.com/maps/embed' title='地図'></iframe>
-      HTML
-      checker = described_class.new
-      checker.check(context, 1, 1, content, Nokogiri::HTML5.fragment(content))
-      expect(context.errors).to have(2).items
-      expect(context.errors[0][:msg]).to eq I18n.t('errors.messages.set_iframe_title')
-      expect(context.errors[1][:msg]).to eq I18n.t('errors.messages.set_iframe_title')
+      it "should detect frame without title" do
+        checker = described_class.new
+        doc = Nokogiri::HTML5.parse(content)
+        checker.check(context, 1, 1, content, doc)
+        expect(context.errors).to have(1).items
+        expect(context.errors[0][:msg]).to eq I18n.t('errors.messages.set_iframe_title')
+      end
+
+      it "should not detect frame with title" do
+        content = <<~HTML
+          <!DOCTYPE html>
+          <html>
+            <head></head>
+            <frameset>
+              <frame src='example.html' title='フレームの説明'>
+            </frameset>
+          </html>
+        HTML
+        context = Cms::SyntaxChecker::CheckerContext.new(site, user, [content], [], false, 0)
+        checker = described_class.new
+        checker.check(context, 1, 1, content, Nokogiri::HTML5.parse(content))
+        expect(context.errors).to be_empty
+      end
+
+      it "should detect multiple frames without title" do
+        content = <<~HTML
+          <!DOCTYPE html>
+          <html>
+            <head></head>
+            <frameset>
+              <frame src='example1.html'>
+              <frame src='example2.html' title='フレームの説明'>
+              <frame src='example3.html'>
+            </frameset>
+          </html>
+        HTML
+        context = Cms::SyntaxChecker::CheckerContext.new(site, user, [content], [], false, 0)
+        checker = described_class.new
+        doc = Nokogiri::HTML5.parse(content)
+        checker.check(context, 1, 1, content, doc)
+        expect(context.errors).to have(2).items
+        expect(context.errors[0][:msg]).to eq I18n.t('errors.messages.set_iframe_title')
+        expect(context.errors[1][:msg]).to eq I18n.t('errors.messages.set_iframe_title')
+      end
     end
   end
 end
