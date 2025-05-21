36614f0e9b923ef354ec62b327abc12341532df8
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-05-21 11:17:37 +0900

    同じエラーが２回出ていた問題を修正

--- app/models/cms/syntax_checker/iframe_title_checker.rb
+++ app/models/cms/syntax_checker/iframe_title_checker.rb
@@ -2,28 +2,18 @@ class Cms::SyntaxChecker::IframeTitleChecker
   include Cms::SyntaxChecker::Base
 
   def check(context, id, idx, raw_html, fragment)
-    # frameset内のframeタグも検出できるように、より詳細なセレクタを使用
-    fragment.css('frame, frameset frame').each do |node|
-      check_node(context, id, idx, node)
+    # iframe, frame, frameset frame をまとめて一度だけチェック
+    fragment.css('iframe, frame, frameset frame').each do |node|
+      title = node['title']
+      next if title.present?
+
+      context.errors << {
+        id: id,
+        idx: idx,
+        code: Cms::SyntaxChecker::Base.outer_html_summary(node),
+        msg: I18n.t('errors.messages.set_iframe_title'),
+        detail: I18n.t('errors.messages.syntax_check_detail.set_iframe_title')
+      }
     end
-
-    fragment.css('iframe').each do |node|
-      check_node(context, id, idx, node)
-    end
-  end
-
-  private
-
-  def check_node(context, id, idx, node)
-    title = node['title']
-    return if title.present?
-
-    context.errors << {
-      id: id,
-      idx: idx,
-      code: Cms::SyntaxChecker::Base.outer_html_summary(node),
-      msg: I18n.t('errors.messages.set_iframe_title'),
-      detail: I18n.t('errors.messages.syntax_check_detail.set_iframe_title')
-    }
   end
 end
