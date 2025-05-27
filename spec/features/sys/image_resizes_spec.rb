2130404df10aa2bdbe09f832eae991b64f4b356e
NAKANO Hideo <nakano@web-tips.co.jp>
2025-05-27 19:20:55 +0900

    [modify^2] able to disable quality options

--- spec/features/sys/image_resizes_spec.rb
+++ spec/features/sys/image_resizes_spec.rb
@@ -15,51 +15,127 @@ describe "sys_image_resizes", type: :feature, dbscope: :example, js: true do
     let(:size2) { rand(1..5) }
     let(:quality2) { rand(80..100) }
 
-    it do
-      expect(SS::ImageResize.all.count).to eq 0
-
-      login_sys_user to: sys_image_resize_path
-      click_on I18n.t("ss.links.edit")
-      within "form#item-form" do
-        select state_label, from: "item[state]"
-        fill_in "item[max_width]", with: max_width
-        fill_in "item[max_height]", with: max_height
-        fill_in "item[in_size_mb]", with: size
-        fill_in "item[quality]", with: quality
-
-        click_on I18n.t("ss.buttons.save")
-      end
-      wait_for_notice I18n.t("ss.notice.saved")
-
-      expect(SS::ImageResize.all.count).to eq 1
-      SS::ImageResize.all.first.tap do |image_resize|
-        expect(image_resize.state).to eq state
-        expect(image_resize.max_width).to eq max_width
-        expect(image_resize.max_height).to eq max_height
-        expect(image_resize.size).to eq size * 1_024 * 1_024
-        expect(image_resize.quality).to eq quality
+    context "when SS.config.ss.quality_option['type'] is disabled" do
+      before do
+        quality_option = @save_ss_quality_option = SS.config.ss.quality_option.dup
+        quality_option['type'] = 'disabled'
+        SS.config.replace_value_at(:ss, :quality_option, quality_option)
       end
 
-      visit sys_image_resize_path
-      click_on I18n.t("ss.links.edit")
-      within "form#item-form" do
-        select state2_label, from: "item[state]"
-        fill_in "item[max_width]", with: max_width2
-        fill_in "item[max_height]", with: max_height2
-        fill_in "item[in_size_mb]", with: size2
-        fill_in "item[quality]", with: quality2
-
-        click_on I18n.t("ss.buttons.save")
+      after do
+        SS.config.replace_value_at(:ss, :quality_option, @save_ss_quality_option)
       end
-      wait_for_notice I18n.t("ss.notice.saved")
 
-      expect(SS::ImageResize.all.count).to eq 1
-      SS::ImageResize.all.first.tap do |image_resize|
-        expect(image_resize.state).to eq state2
-        expect(image_resize.max_width).to eq max_width2
-        expect(image_resize.max_height).to eq max_height2
-        expect(image_resize.size).to eq size2 * 1_024 * 1_024
-        expect(image_resize.quality).to eq quality2
+      it do
+        expect(SS::ImageResize.all.count).to eq 0
+
+        login_sys_user to: sys_image_resize_path
+        click_on I18n.t("ss.links.edit")
+        within "form#item-form" do
+          select state_label, from: "item[state]"
+          fill_in "item[max_width]", with: max_width
+          fill_in "item[max_height]", with: max_height
+          # fill_in "item[in_size_mb]", with: size
+          # fill_in "item[quality]", with: quality
+
+          click_on I18n.t("ss.buttons.save")
+        end
+        wait_for_notice I18n.t("ss.notice.saved")
+
+        expect(SS::ImageResize.all.count).to eq 1
+        SS::ImageResize.all.first.tap do |image_resize|
+          expect(image_resize.state).to eq state
+          expect(image_resize.max_width).to eq max_width
+          expect(image_resize.max_height).to eq max_height
+          # expect(image_resize.size).to eq size * 1_024 * 1_024
+          # expect(image_resize.quality).to eq quality
+          expect(image_resize.size).to be_blank
+          expect(image_resize.quality).to be_blank
+        end
+
+        visit sys_image_resize_path
+        click_on I18n.t("ss.links.edit")
+        within "form#item-form" do
+          select state2_label, from: "item[state]"
+          fill_in "item[max_width]", with: max_width2
+          fill_in "item[max_height]", with: max_height2
+          # fill_in "item[in_size_mb]", with: size2
+          # fill_in "item[quality]", with: quality2
+
+          click_on I18n.t("ss.buttons.save")
+        end
+        wait_for_notice I18n.t("ss.notice.saved")
+
+        expect(SS::ImageResize.all.count).to eq 1
+        SS::ImageResize.all.first.tap do |image_resize|
+          expect(image_resize.state).to eq state2
+          expect(image_resize.max_width).to eq max_width2
+          expect(image_resize.max_height).to eq max_height2
+          # expect(image_resize.size).to eq size2 * 1_024 * 1_024
+          # expect(image_resize.quality).to eq quality2
+          expect(image_resize.size).to be_blank
+          expect(image_resize.quality).to be_blank
+        end
+      end
+    end
+
+    context "when SS.config.ss.quality_option['type'] is custom" do
+      before do
+        quality_option = @save_ss_quality_option = SS.config.ss.quality_option.dup
+        quality_option['type'] = 'custom'
+        SS.config.replace_value_at(:ss, :quality_option, quality_option)
+      end
+
+      after do
+        SS.config.replace_value_at(:ss, :quality_option, @save_ss_quality_option)
+      end
+
+      it do
+        expect(SS::ImageResize.all.count).to eq 0
+
+        login_sys_user to: sys_image_resize_path
+        click_on I18n.t("ss.links.edit")
+        within "form#item-form" do
+          select state_label, from: "item[state]"
+          fill_in "item[max_width]", with: max_width
+          fill_in "item[max_height]", with: max_height
+          fill_in "item[in_size_mb]", with: size
+          fill_in "item[quality]", with: quality
+
+          click_on I18n.t("ss.buttons.save")
+        end
+        wait_for_notice I18n.t("ss.notice.saved")
+
+        expect(SS::ImageResize.all.count).to eq 1
+        SS::ImageResize.all.first.tap do |image_resize|
+          expect(image_resize.state).to eq state
+          expect(image_resize.max_width).to eq max_width
+          expect(image_resize.max_height).to eq max_height
+          expect(image_resize.size).to eq size * 1_024 * 1_024
+          expect(image_resize.quality).to eq quality
+        end
+
+        visit sys_image_resize_path
+        click_on I18n.t("ss.links.edit")
+        within "form#item-form" do
+          select state2_label, from: "item[state]"
+          fill_in "item[max_width]", with: max_width2
+          fill_in "item[max_height]", with: max_height2
+          fill_in "item[in_size_mb]", with: size2
+          fill_in "item[quality]", with: quality2
+
+          click_on I18n.t("ss.buttons.save")
+        end
+        wait_for_notice I18n.t("ss.notice.saved")
+
+        expect(SS::ImageResize.all.count).to eq 1
+        SS::ImageResize.all.first.tap do |image_resize|
+          expect(image_resize.state).to eq state2
+          expect(image_resize.max_width).to eq max_width2
+          expect(image_resize.max_height).to eq max_height2
+          expect(image_resize.size).to eq size2 * 1_024 * 1_024
+          expect(image_resize.quality).to eq quality2
+        end
       end
     end
   end
