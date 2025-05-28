d1a8b3b9fd6e0155d1ff01ce6ca3295cdb9a6a40
Sayoko <79836227+se1987@users.noreply.github.com>
2025-05-28 14:51:54 +0900

    [fix] close permission2 (#5630)
    * add test
    
    * fix test
    
    * リンクチェックを行う処理を追加、エラーメッセージが削除できませんになるので修正必要
    
    * 被リンクがある場合は一括非公開操作ができないよう修正/test pass
    
    * 被リンクのエラー表示処理を追加、被リンクページへのアクセスができるように修正必要
    
    * アクセシビリティエラー有りの表示、差し替えページ有りの表示、被リンクがある場合に関連ページのリンク表示OK
    
    * チェックボックスの表示の制御処理を追加
    
    * チェックされたもののみ非公開にするように修正
    
    * キャンセルボタンの遷移エラーのため、修正必要
    
    * fix spec/features/article/pages/close_permission_spec.rb
    
    * fix spec/features/article/pages/close_permission_spec.rb
    
    * エラー表示確認/被リンクエラー表示OK、アクセシビリティエラーについては別ブランチで修正
    
    * 差し替えページのエラー表示変更
    
    * add en.yml
    
    * fix test
    
    * fix CI
    
    * エラー表示を横並びに表示
    
    ---------
    
    Co-authored-by: 佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>

--- spec/features/article/pages/close_permission_spec.rb
+++ spec/features/article/pages/close_permission_spec.rb
@@ -7,7 +7,6 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
     create :cms_role, cur_site: site, name: "role-#{unique_id}", permissions: minimum_close_permissions
   end
   let!(:user1) { create :cms_test_user, group_ids: admin.group_ids, cms_role_ids: [ minimum_close_role.id ] }
-
   let!(:node) { create :article_node_page, cur_site: site, cur_user: admin, group_ids: admin.group_ids }
   let!(:page1) do
     create :article_page, cur_site: site, cur_user: admin, cur_node: node, group_ids: admin.group_ids, state: "public"
@@ -25,15 +24,16 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
         expect(page).to have_css(".list-item[data-id='#{page1.id}']", text: page1.name)
 
         click_on page1.name
-        wait_for_all_ckeditors_ready
+        # wait_for_all_ckeditors_ready
         expect(page).to have_css("#workflow_route", text: I18n.t("mongoid.attributes.workflow/model/route.my_group"))
 
         click_on I18n.t("ss.links.edit")
-        wait_for_all_ckeditors_ready
+        # wait_for_all_ckeditors_ready
         within "form#item-form" do
           wait_for_cbox_opened { click_on I18n.t("ss.buttons.withdraw") }
         end
         within_cbox do
+          expect(page).to have_content(I18n.t("cms.confirm.close"))
           click_on I18n.t("ss.buttons.ignore_alert")
         end
         wait_for_notice I18n.t("ss.notice.saved")
@@ -58,6 +58,7 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
           first("[type='checkbox']").click
         end
         within ".list-head" do
+          expect(page).to have_content(I18n.t("ss.links.make_them_close"))
           click_on I18n.t("ss.links.make_them_close")
         end
 
@@ -65,6 +66,160 @@ describe "article_pages", type: :feature, dbscope: :example, js: true do
           expect(page).to have_css("[data-id='#{page1.id}'] [type='checkbox']")
           click_on I18n.t("ss.links.make_them_close")
         end
+        wait_for_notice I18n.t("ss.notice.depublished")
+
+        Article::Page.find(page1.id).tap do |after_page|
+          expect(after_page.state).to eq "closed"
+        end
+      end
+    end
+  end
+
+  context "when page is linked from other pages" do
+    let!(:page2) do
+      create :article_page, cur_site: site, cur_user: admin, cur_node: node, group_ids: admin.group_ids, state: "public"
+    end
+    let!(:link_page) do
+      create :article_page, cur_site: site, cur_user: admin, cur_node: node, group_ids: admin.group_ids, state: "public",
+        html: "<a href=\"#{page1.url}\">#{page1.name}</a>"
+    end
+
+    context "with minimum close permissions and contains links in file" do
+      let!(:minimum_close_permissions) do
+        %w(read_private_cms_nodes read_private_article_pages edit_private_article_pages close_private_article_pages)
+      end
+
+      it "cannot close page in edit view without ignore_alert permission when linked from block" do
+        login_user user1
+        visit article_pages_path(site: site, cid: node)
+        expect(page).to have_css(".list-item[data-id='#{page1.id}']", text: page1.name)
+
+        click_on page1.name
+        wait_for_all_ckeditors_ready
+        expect(page).to have_css("#workflow_route", text: I18n.t("mongoid.attributes.workflow/model/route.my_group"))
+
+        click_on I18n.t("ss.links.edit")
+        wait_for_all_ckeditors_ready
+        within "form#item-form" do
+          wait_for_cbox_opened { click_on I18n.t("ss.buttons.withdraw") }
+        end
+
+        within_cbox do
+          expect(page).not_to have_content(I18n.t("ss.buttons.ignore_alert"))
+        end
+
+        Article::Page.find(page1.id).tap do |after_page|
+          expect(after_page.state).to eq "public"
+        end
+      end
+
+      it "cannot close page in edit view without ignore_alert permission" do
+        login_user user1
+        visit article_pages_path(site: site, cid: node)
+        expect(page).to have_css(".list-item[data-id='#{page1.id}']", text: page1.name)
+
+        click_on page1.name
+        wait_for_all_ckeditors_ready
+        expect(page).to have_css("#workflow_route", text: I18n.t("mongoid.attributes.workflow/model/route.my_group"))
+
+        click_on I18n.t("ss.links.edit")
+        wait_for_all_ckeditors_ready
+        within "form#item-form" do
+          wait_for_cbox_opened { click_on I18n.t("ss.buttons.withdraw") }
+        end
+
+        within_cbox do
+          expect(page).not_to have_content(I18n.t("ss.buttons.ignore_alert"))
+        end
+
+        Article::Page.find(page1.id).tap do |after_page|
+          expect(after_page.state).to eq "public"
+        end
+      end
+
+      it "cannot close page in index view" do
+        login_user user1
+        visit article_pages_path(site: site, cid: node)
+        expect(page).to have_css(".list-item[data-id='#{page1.id}']", text: page1.name)
+
+        within ".list-item[data-id='#{page1.id}']" do
+          first("[type='checkbox']").click
+        end
+
+        expect(page).to have_content(I18n.t("ss.links.make_them_close"))
+
+        within ".list-head" do
+          first("button, input[type='button']", text: I18n.t("ss.links.make_them_close")).click
+        end
+
+        within "form" do
+          expect(page).to have_content(I18n.t("ss.confirm.contains_links_in_file_close"))
+          expect(page).to have_no_css("[type='checkbox']")
+        end
+
+        visit article_pages_path(site: site, cid: node)
+        expect(page).to have_css(".list-item[data-id='#{page1.id}']", text: page1.name)
+
+        Article::Page.find(page1.id).tap do |after_page|
+          expect(after_page.state).to eq "public"
+        end
+      end
+    end
+
+    context "with ignore_alert permission and contains links in file" do
+      let!(:minimum_close_permissions) do
+        %w(read_private_cms_nodes read_private_article_pages edit_private_article_pages close_private_article_pages
+           edit_cms_ignore_alert)
+      end
+
+      it "can close page in edit view with ignore_alert permission" do
+        login_user user1
+        visit article_pages_path(site: site, cid: node)
+        expect(page).to have_css(".list-item[data-id='#{page1.id}']", text: page1.name)
+
+        click_on page1.name
+        wait_for_all_ckeditors_ready
+        expect(page).to have_css("#workflow_route", text: I18n.t("mongoid.attributes.workflow/model/route.my_group"))
+
+        click_on I18n.t("ss.links.edit")
+        wait_for_all_ckeditors_ready
+        within "form#item-form" do
+          wait_for_cbox_opened { click_on I18n.t("ss.buttons.withdraw") }
+        end
+        within_cbox do
+          expect(page).to have_content(I18n.t("cms.confirm.close"))
+          expect(page).to have_content(I18n.t("ss.buttons.ignore_alert"))
+        end
+        click_on I18n.t("ss.buttons.ignore_alert")
+        wait_for_notice I18n.t("ss.notice.saved")
+
+        Article::Page.find(page1.id).tap do |after_page|
+          expect(after_page.state).to eq "closed"
+        end
+      end
+
+      it "can close page in index view with ignore_alert permission" do
+        login_user user1
+        visit article_pages_path(site: site, cid: node)
+        expect(page).to have_css(".list-item[data-id='#{page1.id}']", text: page1.name)
+
+        within ".list-item[data-id='#{page1.id}']" do
+          first("[type='checkbox']").click
+        end
+
+        expect(page).to have_content(I18n.t("ss.links.make_them_close"))
+
+        within ".list-head" do
+          first("button, input[type='button']", text: I18n.t("ss.links.make_them_close")).click
+        end
+
+        within "form" do
+          expect(page).to have_content(I18n.t("ss.confirm.contains_links_in_file_ignoring_alert_close"))
+          expect(page).to have_css("[data-id='#{page1.id}'] [type='checkbox']")
+          first("[type='checkbox']").click
+          click_on I18n.t("ss.links.make_them_close")
+        end
+
         wait_for_notice I18n.t("ss.notice.depublished")
 
         Article::Page.find(page1.id).tap do |after_page|
