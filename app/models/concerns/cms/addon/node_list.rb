8f0c0f2a86ab83590da4e07c18c23a85086b9529
itowtips <ito@web-tips.co.jp>
2025-06-05 08:59:43 +0900

    [modify] cms/node2 part : origin_of_page setting (#5640)

--- app/models/concerns/cms/addon/node_list.rb
+++ app/models/concerns/cms/addon/node_list.rb
@@ -12,8 +12,10 @@ module Cms::Addon
 
       field :node_routes, type: SS::Extensions::Words
       field :list_origin, type: String
+      field :origin_of_page, type: String, default: "parent1"
+      field :origin_of_node, type: String, default: "parent1"
       permit_params node_routes: []
-      permit_params :list_origin
+      permit_params :list_origin, :origin_of_page, :origin_of_node
       validates :list_origin, inclusion: { in: %w(deployment content), allow_blank: true }
     end
 
@@ -32,9 +34,55 @@ module Cms::Addon
       { sort.sub(/ .*/, "") => (/-1$/.match?(sort) ? -1 : 1) }
     end
 
+    def select_list_origin(page, node)
+      if list_origin_deployment?
+        return parent
+      end
+
+      if page
+        select_origin_of_page(page)
+      elsif node
+        select_origin_of_node(node)
+      else
+        nil
+      end
+    end
+
+    def select_origin_of_page(page)
+      case origin_of_page
+      when "parent2"
+        (page.parent ? page.parent.parent : false)
+      else #parent1
+        page.parent
+      end
+    end
+
+    def select_origin_of_node(node)
+      case origin_of_node
+      when "parent2"
+        (node.parent ? node.parent.parent : false)
+      when "self_node"
+        node
+      else #parent1
+        node.parent
+      end
+    end
+
     def list_origin_options
       %w(deployment content).map do |v|
         [ I18n.t("cms.options.list_origin.#{v}"), v ]
+      end
+    end
+
+    def origin_of_page_options
+      %w(parent1 parent2).map do |v|
+        [ I18n.t("cms.options.origin_of_page.#{v}"), v ]
+      end
+    end
+
+    def origin_of_node_options
+      %w(parent1 parent2 self_node).map do |v|
+        [ I18n.t("cms.options.origin_of_node.#{v}"), v ]
       end
     end
 
