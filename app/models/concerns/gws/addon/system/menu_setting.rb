80a6cb152c4a01c01cfea3d4956646f667cc5d63
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-06-09 17:24:30 +0900

    メニュー設定の画面にアップロードフィールド、アイコン画像の表示

--- app/models/concerns/gws/addon/system/menu_setting.rb
+++ app/models/concerns/gws/addon/system/menu_setting.rb
@@ -19,7 +19,9 @@ module Gws::Addon::System::MenuSetting
     def define_menu_setting(name, options = {})
       field "menu_#{name}_state", type: String, default: options[:default_state]
       field "menu_#{name}_label", type: String, localize: true
-      permit_params "menu_#{name}_state", "menu_#{name}_label"
+      field "menu_#{name}_icon_image_id", type: BSON::ObjectId
+      belongs_to "menu_#{name}_icon_image", class_name: "SS::File", optional: true
+      permit_params "menu_#{name}_state", "menu_#{name}_label", "menu_#{name}_icon_image_id"
       alias_method("menu_#{name}_state_options", "menu_state_options")
 
       if !options[:define_visible]
