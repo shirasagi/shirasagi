01f68d0fbc7b885d98aa959f71a02bc208a22c17
tany <tanizawa@web-tips.co.jp>
2025-06-20 13:14:54 +0900

    [modify] site_search

--- app/models/cms/node.rb
+++ app/models/cms/node.rb
@@ -147,6 +147,7 @@ class Cms::Node
     include Cms::Addon::Meta
     include Cms::Addon::SiteSearch::Setting
     include Category::Addon::Setting
+    include Cms::Addon::SiteSearch::Group
     include Cms::Addon::Release
     include Cms::Addon::GroupPermission
     include History::Addon::Backup
