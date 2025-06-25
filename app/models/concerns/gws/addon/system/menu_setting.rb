b88a08ddeacea160f79efbc6bf4a90b9f379db40
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-06-25 16:09:16 +0900

    belongs_to_fileメソッドを使用

--- app/models/concerns/gws/addon/system/menu_setting.rb
+++ app/models/concerns/gws/addon/system/menu_setting.rb
@@ -22,11 +22,9 @@ module Gws::Addon::System::MenuSetting
       field "menu_#{name}_state", type: String, default: options[:default_state]
       field "menu_#{name}_label", type: String, localize: true
       field "menu_#{name}_icon_image_id", type: BSON::ObjectId
-      belongs_to "menu_#{name}_icon_image", class_name: "SS::File", optional: true
+      belongs_to_file "menu_#{name}_icon_image", class_name: "SS::File", accepts: SS::File::IMAGE_FILE_EXTENSIONS + [".svg"]
       permit_params "menu_#{name}_state", "menu_#{name}_label", "menu_#{name}_icon_image_id"
       alias_method("menu_#{name}_state_options", "menu_state_options")
-
-      before_save :set_menu_icon_owner
 
       if !options[:define_visible]
         define_method("menu_#{name}_visible?") do
@@ -42,22 +40,5 @@ module Gws::Addon::System::MenuSetting
 
   def menu_visible?(name)
     try("menu_#{name}_state") != 'hide'
-  end
-
-  def set_menu_icon_owner
-    %w(portal notice reminder presence schedule todo affair affair2 daily_report attendance bookmark memo board
-       faq qna workload report workflow2 circular monitor survey share shared_address personal_address
-       discussion contrast elasticsearch workflow conf).each do |name|
-      icon_file = send("menu_#{name}_icon_image")
-      next unless icon_file
-
-      if icon_file.owner_item != self || icon_file.state != "public"
-        icon_file.owner_item = self
-        icon_file.state = "public"
-        icon_file.site_id = icon_file.owner_item_id
-        icon_file.save!
-      end
-      Rails.logger.debug "[set_menu_icon_owner] icon_file: #{icon_file.inspect}"
-    end
   end
 end
