ac28addde457f08a8313f34c16fc98da51dab0e3
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-07-01 16:30:58 +0900

    layout helperを修正、cssの読み込み追加

--- app/helpers/gws/layout_helper.rb
+++ app/helpers/gws/layout_helper.rb
@@ -23,43 +23,18 @@ module Gws::LayoutHelper
     label = @cur_site.send("menu_#{type}_label") || t(label_i18n)
     icon_class = "icon-#{type}"
 
-    svg_path = Rails.root.join("public/assets/img/icons/ic-#{type}.svg")
-    svg_tag = ""
-    if File.exist?(svg_path)
-      svg_content = File.read(svg_path)
-      svg_tag = raw(svg_content)
-    end
-
     if icon_file.present?
       content_tag(:h2) do
         link_to(path, class: "#{icon_class} has-custom-icon") do
           image_tag(icon_file.url, class: "nav-icon-img") + label
         end
       end
-    elsif svg_tag.present?
-      content_tag(:h2) do
-        link_to(path, class: "#{icon_class} has-inline-svg") do
-          svg_tag + label
-        end
-      end
     else
       content_tag(:h2) do
-        link_to(label, path, class: icon_class)
+        link_to(path, class: "ss-icon ss-icon-#{type}") do
+          content_tag(:span, "", class: "ss-icon ss-icon-#{type}") + label
+        end
       end
     end
-  end
-
-  def gws_inline_svg(type, options = {})
-    svg_path = Rails.root.join("public/assets/img/icons/ic-#{type}.svg")
-    return "" unless File.exist?(svg_path)
-    svg_content = File.read(svg_path)
-    doc = Nokogiri::HTML::DocumentFragment.parse(svg_content)
-    svg = doc.at_css("svg")
-    base_class = "icon-#{type} has-inline-svg"
-    if svg
-      svg["class"] = [svg["class"], base_class, options.delete(:class)].compact.join(" ")
-      options.each { |k, v| svg[k.to_s] = v }
-    end
-    raw(doc.to_html)
   end
 end
