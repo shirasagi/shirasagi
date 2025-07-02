ea8388c3c9be061085210c61b6c96bac96fcce6a
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-07-03 08:50:30 +0900

    fix test/test pass

--- spec/features/gws/menu_settings_spec.rb
+++ spec/features/gws/menu_settings_spec.rb
@@ -33,34 +33,34 @@ describe "gws_menu_settings", type: :feature, dbscope: :example, js: true do
         #
         # メニュー設定の詳細画面の初期状態を確認
         #
-        expect(page).to have_css("svg.icon-portal")
-        expect(page).to have_css("svg.icon-notice")
-        expect(page).to have_css("svg.icon-schedule")
-        expect(page).to have_css("svg.icon-todo")
-        expect(page).to have_css("svg.icon-reminder")
-        expect(page).to have_css("svg.icon-presence")
-        expect(page).to have_css("svg.icon-attendance")
-        expect(page).to have_css("svg.icon-affair")
-        expect(page).to have_css("svg.icon-affair2")
-        expect(page).to have_css("svg.icon-daily_report")
-        expect(page).to have_css("svg.icon-bookmark")
-        expect(page).to have_css("svg.icon-memo")
-        expect(page).to have_css("svg.icon-workload")
-        expect(page).to have_css("svg.icon-report")
-        expect(page).to have_css("svg.icon-workflow")
-        expect(page).to have_css("svg.icon-workflow2")
-        expect(page).to have_css("svg.icon-circular")
-        expect(page).to have_css("svg.icon-monitor")
-        expect(page).to have_css("svg.icon-survey")
-        expect(page).to have_css("svg.icon-board")
-        expect(page).to have_css("svg.icon-faq")
-        expect(page).to have_css("svg.icon-qna")
-        expect(page).to have_css("svg.icon-discussion")
-        expect(page).to have_css("svg.icon-share")
-        expect(page).to have_css("svg.icon-shared_address")
-        expect(page).to have_css("svg.icon-personal_address")
-        expect(page).to have_css("svg.icon-elasticsearch")
-        expect(page).to have_css("svg.icon-conf")
+        expect(page).to have_css("span.ss-icon.ss-icon-portal")
+        expect(page).to have_css("span.ss-icon.ss-icon-notice")
+        expect(page).to have_css("span.ss-icon.ss-icon-schedule")
+        expect(page).to have_css("span.ss-icon.ss-icon-todo")
+        expect(page).to have_css("span.ss-icon.ss-icon-reminder")
+        expect(page).to have_css("span.ss-icon.ss-icon-presence")
+        expect(page).to have_css("span.ss-icon.ss-icon-attendance")
+        expect(page).to have_css("span.ss-icon.ss-icon-affair")
+        expect(page).to have_css("span.ss-icon.ss-icon-affair2")
+        expect(page).to have_css("span.ss-icon.ss-icon-daily_report")
+        expect(page).to have_css("span.ss-icon.ss-icon-bookmark")
+        expect(page).to have_css("span.ss-icon.ss-icon-memo")
+        expect(page).to have_css("span.ss-icon.ss-icon-workload")
+        expect(page).to have_css("span.ss-icon.ss-icon-report")
+        expect(page).to have_css("span.ss-icon.ss-icon-workflow")
+        expect(page).to have_css("span.ss-icon.ss-icon-workflow2")
+        expect(page).to have_css("span.ss-icon.ss-icon-circular")
+        expect(page).to have_css("span.ss-icon.ss-icon-monitor")
+        expect(page).to have_css("span.ss-icon.ss-icon-survey")
+        expect(page).to have_css("span.ss-icon.ss-icon-board")
+        expect(page).to have_css("span.ss-icon.ss-icon-faq")
+        expect(page).to have_css("span.ss-icon.ss-icon-qna")
+        expect(page).to have_css("span.ss-icon.ss-icon-discussion")
+        expect(page).to have_css("span.ss-icon.ss-icon-share")
+        expect(page).to have_css("span.ss-icon.ss-icon-shared_address")
+        expect(page).to have_css("span.ss-icon.ss-icon-personal_address")
+        expect(page).to have_css("span.ss-icon.ss-icon-elasticsearch")
+        expect(page).to have_css("span.ss-icon.ss-icon-conf")
 
         within ".nav-menu" do
           click_on I18n.t("ss.links.edit")
@@ -118,7 +118,7 @@ describe "gws_menu_settings", type: :feature, dbscope: :example, js: true do
         visit index_path
         wait_for_js_ready
         within ".main-navi" do
-          expect(page).to have_no_css("svg.icon-portal")
+          expect(page).to have_no_css("span.ss-icon.ss-icon-portal")
           expect(page).to have_css(".icon-portal.has-custom-icon")
           expect(page).to have_css("img.nav-icon-img[src*='logo.png']")
         end
@@ -150,7 +150,7 @@ describe "gws_menu_settings", type: :feature, dbscope: :example, js: true do
         # ポータルとスケジュールのアイコンを変更
         menu_types = [:portal, :schedule]
         menu_types.each do |menu_type|
-          within(all(".addon-gws-system-menu-setting").find { |el| el.has_css?("svg.icon-#{menu_type}") }) do
+          within(all(".addon-gws-system-menu-setting").find { |el| el.has_css?("span.ss-icon.ss-icon-#{menu_type}") }) do
             within find("dt", text: I18n.t("gws.buttons.change_menu_icon")).find(:xpath, "following-sibling::dd") do
               wait_for_cbox_opened do
                 click_on I18n.t("ss.buttons.upload")
@@ -189,11 +189,11 @@ describe "gws_menu_settings", type: :feature, dbscope: :example, js: true do
         wait_for_js_ready
         within ".main-navi" do
           within ".icon-portal" do
-            expect(page).to have_no_css("icon-portal.has-inline-svg")
+            expect(page).to have_no_css("icon-portal.has-font-icon")
             expect(page).to have_css("img.nav-icon-img[src*='logo.png']")
           end
           within ".icon-schedule" do
-            expect(page).to have_no_css("icon-schedule.has-inline-svg")
+            expect(page).to have_no_css("icon-schedule.has-font-icon")
             expect(page).to have_css("img.nav-icon-img[src*='logo.png']")
           end
         end
@@ -273,14 +273,14 @@ describe "gws_menu_settings", type: :feature, dbscope: :example, js: true do
         ensure_addon_opened("#addon-gws-agents-addons-system-menu_setting")
         # デフォルトアイコンに戻っていることを確認
         within all(".addon-gws-system-menu-setting").first do
-          expect(page).to have_css("svg.icon-portal")
+          expect(page).to have_css("span.ss-icon.ss-icon-portal")
           expect(page).to have_no_css("img.icon-portal[src*='logo.png']")
         end
 
         visit index_path
         wait_for_js_ready
         within ".main-navi" do
-          expect(page).to have_css(".icon-portal.has-inline-svg")
+          expect(page).to have_css(".icon-portal.has-font-icon")
           expect(page).to have_no_css(".icon-portal.has-custom-icon")
           expect(page).to have_no_css("img.nav-icon-img[src*='logo.png']")
         end
@@ -359,7 +359,7 @@ describe "gws_menu_settings", type: :feature, dbscope: :example, js: true do
         visit index_path
         wait_for_js_ready
         within ".main-navi" do
-          expect(page).to have_no_css("svg.icon-portal")
+          expect(page).to have_no_css("span.ss-icon.ss-icon-portal")
           expect(page).to have_css(".icon-portal.has-custom-icon")
           expect(page).to have_css("img.nav-icon-img[src*='test_icon.svg']")
         end
@@ -374,7 +374,7 @@ describe "gws_menu_settings", type: :feature, dbscope: :example, js: true do
 
         # user2でもアップロードされたアイコンが表示されることを確認
         within ".main-navi" do
-          expect(page).to have_no_css("svg.icon-portal")
+          expect(page).to have_no_css("span.ss-icon.ss-icon-portal")
           expect(page).to have_css(".icon-portal.has-custom-icon")
           expect(page).to have_css("img.nav-icon-img[src*='test_icon.svg']")
         end
