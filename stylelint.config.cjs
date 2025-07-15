9d010abbc4b66ce51ff020d81823347dfec44cc8
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-07-15 11:35:38 +0900

    stylelint-stylisticの導入、スタイルルールを追加

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
     "stylelint-config-recommended-scss",
     "stylelint-config-property-sort-order-smacss"
   ],
+  "plugins": [
+    "@stylistic/stylelint-plugin"
+  ],
   "ignoreFiles": [
     "app/assets/builds/**/*.css",
     "app/assets/stylesheets/ss/_github-markdown.scss",
@@ -23,6 +26,9 @@ module.exports = {
       // these selectors could be blocked by ad-blockers, so you shouldn't use.
       "/[.#].*(campaign|splash|adsbygoogle|google_ads).*/",
       "/[.#].*ads[-_].*/", "/[.#].*[-_]ads.*/", "/[.#]ads/"
-    ]
+    ],
+    // stylistic rules from @stylistic/stylelint-plugin:
+    "block-opening-brace-space-before": "always",
+    "selector-pseudo-element-colon-notation": "double"
   }
 }
