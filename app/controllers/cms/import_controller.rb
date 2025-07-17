d643e4a2d102c5cee5c74aea8122c638ea21daeb
NAKANO Hideo <nakano@web-tips.co.jp>
2025-07-17 11:16:00 +0900

    [cms] フォルダー取り込みのパンくずを設定 (#5710)

--- app/controllers/cms/import_controller.rb
+++ app/controllers/cms/import_controller.rb
@@ -9,6 +9,10 @@ class Cms::ImportController < ApplicationController
 
   private
 
+  def set_crumbs
+    @crumbs << [t("cms.import_node"), action: :import]
+  end
+
   def fix_params
     { cur_site: @cur_site, cur_user: @cur_user }
   end
