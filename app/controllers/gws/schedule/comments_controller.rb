467f4e48f5e92d123bc7cb60044b08db1187fd11
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2025-07-30 16:57:39 +0900

    [modify] avoid method override "member?" (#5088)
    * [modify] avoid method override "member?"
    
    * [modify] avoid method override "member?"
    
    * [modify] avoid method override "member?"

--- app/controllers/gws/schedule/comments_controller.rb
+++ app/controllers/gws/schedule/comments_controller.rb
@@ -35,7 +35,8 @@ class Gws::Schedule::CommentsController < ApplicationController
   def create
     @item = @model.new get_params
     @item.text_type ||= 'plain'
-    raise "403" unless @cur_schedule.member?(@cur_user) || @cur_schedule.allowed_for_managers?(:edit, @cur_user, site: @cur_site)
+    raise "403" unless @cur_schedule.member_include?(@cur_user) ||
+                       @cur_schedule.allowed_for_managers?(:edit, @cur_user, site: @cur_site)
     # render_create @item.save
     result = @item.save
     if result
