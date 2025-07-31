13f450170512bf4957414722bc69d0c38a16aa26
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-07-31 09:45:58 +0900

    add test/test pass

--- spec/factories/cms/layouts.rb
+++ spec/factories/cms/layouts.rb
@@ -28,4 +28,21 @@ FactoryBot.define do
       HTML
     end
   end
+
+  factory :cms_layout_with_title, class: Cms::Layout, traits: [:cms_layout] do
+    html do
+      <<~HTML
+        <html>
+        <head>
+          <meta charset="UTF-8" />
+          <title><%= @window_name %></title>
+        </head>
+        <body>
+          <h1><%= @window_name %></h1>
+          </ yield />
+        </body>
+        </html>
+      HTML
+    end
+  end
 end
