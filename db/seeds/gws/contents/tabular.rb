c2fa1997355f246cafb3ec20c56ff493e4a367e6
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2025-08-06 09:07:10 +0900

    [modify] gws tabular seed

--- db/seeds/gws/contents/tabular.rb
+++ db/seeds/gws/contents/tabular.rb
@@ -370,42 +370,117 @@ def create_tabular_file(data)
   item
 end
 
-@tabular_files = [
-  create_tabular_file(
-    cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[0], form: @tabular_forms[2],
-    "col_#{@tabular_columns[9].id}": "<p>これはテストです1</p>"
-  ),
-  create_tabular_file(
-    cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[0], form: @tabular_forms[2],
-    "col_#{@tabular_columns[9].id}": "<p>これはテストです2</p>"
-  ),
-  create_tabular_file(
-    cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[1], form: @tabular_forms[3],
-    "col_#{@tabular_columns[10].id}": "2025-07-17",
-    "col_#{@tabular_columns[12].id}": %w(社用),
-    "col_#{@tabular_columns[13].id}": "配達",
-    "col_#{@tabular_columns[14].id}": %w(なし)
-  ),
-  create_tabular_file(
-    cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[1], form: @tabular_forms[3],
-    "col_#{@tabular_columns[10].id}": "2025-08-01",
-    "col_#{@tabular_columns[12].id}": %w(社用),
-    "col_#{@tabular_columns[13].id}": "営業",
-    "col_#{@tabular_columns[14].id}": %w(なし)
-  ),
-  create_tabular_file(
-    cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[1], form: @tabular_forms[6],
-    "col_#{@tabular_columns[23].id}": "ワゴン",
-    "col_#{@tabular_columns[24].id}": 100,
-  ),
-  create_tabular_file(
-    cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[1], form: @tabular_forms[6],
-    "col_#{@tabular_columns[23].id}": "軽自動車",
-    "col_#{@tabular_columns[24].id}": 300,
-  ),
-  create_tabular_file(
-    cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[1], form: @tabular_forms[6],
-    "col_#{@tabular_columns[23].id}": "セダン",
-    "col_#{@tabular_columns[24].id}": 100,
-  )
-]
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[0], form: @tabular_forms[2],
+  "col_#{@tabular_columns[9].id}": "<p>これはテストです1</p>"
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[0], form: @tabular_forms[2],
+  "col_#{@tabular_columns[9].id}": "<p>これはテストです2</p>"
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[1], form: @tabular_forms[3],
+  "col_#{@tabular_columns[10].id}": "2025-07-17",
+  "col_#{@tabular_columns[12].id}": %w(社用),
+  "col_#{@tabular_columns[13].id}": "配達",
+  "col_#{@tabular_columns[14].id}": %w(なし)
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[1], form: @tabular_forms[3],
+  "col_#{@tabular_columns[10].id}": "2025-08-01",
+  "col_#{@tabular_columns[12].id}": %w(社用),
+  "col_#{@tabular_columns[13].id}": "営業",
+  "col_#{@tabular_columns[14].id}": %w(なし)
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[1], form: @tabular_forms[6],
+  "col_#{@tabular_columns[23].id}": "ワゴン",
+  "col_#{@tabular_columns[24].id}": 100,
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[1], form: @tabular_forms[6],
+  "col_#{@tabular_columns[23].id}": "軽自動車",
+  "col_#{@tabular_columns[24].id}": 300,
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[1], form: @tabular_forms[6],
+  "col_#{@tabular_columns[23].id}": "セダン",
+  "col_#{@tabular_columns[24].id}": 100,
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[2], form: @tabular_forms[8],
+  "col_#{@tabular_columns[33].id}": "PC本体",
+  "col_#{@tabular_columns[34].id}": 100,
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[2], form: @tabular_forms[8],
+  "col_#{@tabular_columns[33].id}": "ケーブル類",
+  "col_#{@tabular_columns[34].id}": 200,
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[2], form: @tabular_forms[8],
+  "col_#{@tabular_columns[33].id}": "付属品類",
+  "col_#{@tabular_columns[34].id}": 300,
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[2], form: @tabular_forms[8],
+  "col_#{@tabular_columns[33].id}": "デスク",
+  "col_#{@tabular_columns[34].id}": 400,
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[2], form: @tabular_forms[8],
+  "col_#{@tabular_columns[33].id}": "イス",
+  "col_#{@tabular_columns[34].id}": 500,
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[2], form: @tabular_forms[8],
+  "col_#{@tabular_columns[33].id}": "コピー機",
+  "col_#{@tabular_columns[34].id}": 600,
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[2], form: @tabular_forms[8],
+  "col_#{@tabular_columns[33].id}": "その他",
+  "col_#{@tabular_columns[34].id}": 700,
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[2], form: @tabular_forms[9],
+  "col_#{@tabular_columns[35].id}": "PC",
+  "col_#{@tabular_columns[36].id}": 100,
+)
+@tabular_file_2_9_2 =create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[2], form: @tabular_forms[9],
+  "col_#{@tabular_columns[35].id}": "PC周辺機器",
+  "col_#{@tabular_columns[36].id}": 200,
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[2], form: @tabular_forms[9],
+  "col_#{@tabular_columns[35].id}": "什器",
+  "col_#{@tabular_columns[36].id}": 300,
+)
+@tabular_file_2_9_4 = create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[2], form: @tabular_forms[9],
+  "col_#{@tabular_columns[35].id}": "事務用品",
+  "col_#{@tabular_columns[36].id}": 400,
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[2], form: @tabular_forms[9],
+  "col_#{@tabular_columns[35].id}": "その他",
+  "col_#{@tabular_columns[36].id}": 500,
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[2], form: @tabular_forms[7],
+  "col_#{@tabular_columns[25].id}": "2025-07-16 09:00",
+  "col_#{@tabular_columns[26].id}": "出張のため",
+  "col_#{@tabular_columns[27].id}_ids": [@tabular_file_2_9_2.id],
+  "col_#{@tabular_columns[28].id}": "Wi-Fiルーター",
+  "col_#{@tabular_columns[29].id}": 1,
+  "in_col_#{@tabular_columns[30].id}": sh_upload_file('dummy_slide05.png'),
+  "in_col_#{@tabular_columns[30].id}": sh_upload_file('dummy_slide08.png')
+)
+create_tabular_file(
+  cur_site: @site, cur_user: u('sys'), space: @tabular_spaces[2], form: @tabular_forms[7],
+  "col_#{@tabular_columns[25].id}": "2025-07-23 13:39",
+  "col_#{@tabular_columns[27].id}_ids": [@tabular_file_2_9_4.id],
+  "col_#{@tabular_columns[28].id}": "テプラ",
+  "col_#{@tabular_columns[29].id}": 1
+)
