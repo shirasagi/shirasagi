d85c9f8e4568468ea018a24c711a75e0585f634b
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-08-06 15:52:13 +0900

    定型フォームへの組み込み

--- app/models/concerns/cms/addon/layout_html.rb
+++ app/models/concerns/cms/addon/layout_html.rb
@@ -5,7 +5,16 @@ module Cms::Addon
 
     included do
       field :html, type: String
-      permit_params :html
+      belongs_to :loop_setting, class_name: 'Cms::LoopSetting'
+      permit_params :html, :loop_setting_id
+
+      before_save :set_html
+    end
+
+    def set_html
+      if loop_setting_id.present?
+        self.html = loop_setting.custom_html
+      end
     end
   end
 end
