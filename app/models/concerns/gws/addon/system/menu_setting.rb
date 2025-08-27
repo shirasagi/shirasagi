86342a807b4a108bcc8d50e5b4e673c04556f6de
itowtips <ito@web-tips.co.jp>
2025-08-27 18:27:55 +0900

    [fixup][refactor] gws change menu icon : en locale (#5772)

--- app/models/concerns/gws/addon/system/menu_setting.rb
+++ app/models/concerns/gws/addon/system/menu_setting.rb
@@ -5,41 +5,41 @@ module Gws::Addon::System::MenuSetting
   set_addon_type :organization
 
   included do
-    define_menu_setting('portal', I18n.t('modules.gws/portal'))
-    define_menu_setting('notice', I18n.t('modules.gws/notice'))
-    define_menu_setting('reminder', I18n.t('mongoid.models.gws/reminder'))
-    define_menu_setting('presence', I18n.t('mongoid.models.gws/presence'))
-    define_menu_setting('schedule', I18n.t('modules.gws/schedule'))
-    define_menu_setting('todo', I18n.t('modules.gws/schedule/todo'))
-    define_menu_setting('affair', I18n.t('modules.gws/affair'))
-    define_menu_setting('daily_report', I18n.t('modules.gws/daily_report'))
-    define_menu_setting('attendance', I18n.t('modules.gws/attendance'))
-    define_menu_setting('bookmark', I18n.t('modules.gws/bookmark'))
-    define_menu_setting('memo', I18n.t('modules.gws/memo'))
-    define_menu_setting('board', I18n.t('modules.gws/board'))
-    define_menu_setting('faq', I18n.t('modules.gws/faq'))
-    define_menu_setting('qna', I18n.t('modules.gws/qna'))
-    define_menu_setting('workload', I18n.t('modules.gws/workload'))
-    define_menu_setting('report', I18n.t('modules.gws/report'))
-    define_menu_setting('workflow2', I18n.t('modules.gws/workflow2'))
-    define_menu_setting('circular', I18n.t('modules.gws/circular'))
-    define_menu_setting('monitor', I18n.t('modules.gws/monitor'))
-    define_menu_setting('survey', I18n.t('modules.gws/survey'))
-    define_menu_setting('share', I18n.t('modules.gws/share'))
-    define_menu_setting('shared_address', I18n.t('modules.gws/shared_address'))
-    define_menu_setting('personal_address', I18n.t('modules.gws/personal_address'))
-    define_menu_setting('staff_record', I18n.t('gws/staff_record.staff_records'))
-    define_menu_setting('links', I18n.t('mongoid.models.gws/link'))
-    define_menu_setting('discussion', I18n.t('modules.gws/discussion'))
-    define_menu_setting('tabular', I18n.t('modules.gws/tabular'))
-    define_menu_setting('contrast', I18n.t('mongoid.models.gws/contrast'), default_state: 'hide')
-    define_menu_setting('elasticsearch', I18n.t('modules.gws/elasticsearch'), default_state: 'hide')
-    define_menu_setting('workflow', I18n.t('modules.gws/workflow'), default_state: 'hide')
-    define_menu_setting('conf', I18n.t('gws.site_config'))
+    define_menu_setting('portal', 'modules.gws/portal')
+    define_menu_setting('notice', 'modules.gws/notice')
+    define_menu_setting('reminder', 'mongoid.models.gws/reminder')
+    define_menu_setting('presence', 'mongoid.models.gws/presence')
+    define_menu_setting('schedule', 'modules.gws/schedule')
+    define_menu_setting('todo', 'modules.gws/schedule/todo')
+    define_menu_setting('affair', 'modules.gws/affair')
+    define_menu_setting('daily_report', 'modules.gws/daily_report')
+    define_menu_setting('attendance', 'modules.gws/attendance')
+    define_menu_setting('bookmark', 'modules.gws/bookmark')
+    define_menu_setting('memo', 'modules.gws/memo')
+    define_menu_setting('board','modules.gws/board')
+    define_menu_setting('faq', 'modules.gws/faq')
+    define_menu_setting('qna', 'modules.gws/qna')
+    define_menu_setting('workload', 'modules.gws/workload')
+    define_menu_setting('report', 'modules.gws/report')
+    define_menu_setting('workflow2', 'modules.gws/workflow2')
+    define_menu_setting('circular', 'modules.gws/circular')
+    define_menu_setting('monitor', 'modules.gws/monitor')
+    define_menu_setting('survey', 'modules.gws/survey')
+    define_menu_setting('share', 'modules.gws/share')
+    define_menu_setting('shared_address', 'modules.gws/shared_address')
+    define_menu_setting('personal_address', 'modules.gws/personal_address')
+    define_menu_setting('staff_record', 'gws/staff_record.staff_records')
+    define_menu_setting('links', 'mongoid.models.gws/link')
+    define_menu_setting('discussion', 'modules.gws/discussion')
+    define_menu_setting('tabular', 'modules.gws/tabular')
+    define_menu_setting('contrast', 'mongoid.models.gws/contrast', default_state: 'hide')
+    define_menu_setting('elasticsearch', 'modules.gws/elasticsearch', default_state: 'hide')
+    define_menu_setting('workflow', 'modules.gws/workflow', default_state: 'hide')
+    define_menu_setting('conf', 'gws.site_config')
   end
 
   module ClassMethods
-    def define_menu_setting(name, default_label, options = {})
+    def define_menu_setting(name, i18n_key, options = {})
       field "menu_#{name}_state", type: String, default: options[:default_state]
       field "menu_#{name}_label", type: String, localize: true
       belongs_to_file "menu_#{name}_icon_image", class_name: "SS::File", accepts: SS::File::IMAGE_FILE_EXTENSIONS + [".svg"]
@@ -47,7 +47,7 @@ module Gws::Addon::System::MenuSetting
       alias_method("menu_#{name}_state_options", "menu_state_options")
 
       define_method("menu_#{name}_default_label") do
-        default_label
+        I18n.t(i18n_key)
       end
       define_method("menu_#{name}_effective_label") do
         send("menu_#{name}_label").presence || send("menu_#{name}_default_label")
