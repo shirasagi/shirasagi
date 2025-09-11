3325272cafa8bc860ca9e68c447d601ac7b4dfaa
itowtips <ito@web-tips.co.jp>
2025-09-11 18:39:49 +0900

    [modify] mail_page : add auto_link setting

--- spec/jobs/mail_page/import_job_spec.rb
+++ spec/jobs/mail_page/import_job_spec.rb
@@ -51,8 +51,6 @@ describe MailPage::ImportJob, dbscope: :example do
         expect(page2.group_ids).to eq node2.group_ids
         expect(page2.name).to eq "UTF-8"
         expect(page2.html.split("<br />")).to match_array decoded.split("\n")
-        expect(page2.mail_page_original_mail).to \
-          include("To: =?UTF-8?B?5LyK6Jek5oKg?= <sample@example.jp>", "From: =?UTF-8?B?5LyK6Jek5oKg?= <sample@example.jp>")
         expect(page2.arrival_start_date).to be_present
         expect(page2.arrival_close_date).to eq page2.arrival_start_date.advance(days: node2.arrival_days)
         expect(page2.state).to eq "public"
@@ -61,8 +59,6 @@ describe MailPage::ImportJob, dbscope: :example do
         expect(page3.group_ids).to eq node3.group_ids
         expect(page3.name).to eq "UTF-8"
         expect(page3.html.split("<br />")).to match_array decoded.split("\n")
-        expect(page3.mail_page_original_mail).to \
-          include("To: =?UTF-8?B?5LyK6Jek5oKg?= <sample@example.jp>", "From: =?UTF-8?B?5LyK6Jek5oKg?= <sample@example.jp>")
         expect(page3.arrival_start_date).to be_present
         expect(page3.arrival_close_date).to eq page3.arrival_start_date.advance(days: node3.arrival_days)
         expect(page3.state).to eq "public"
@@ -94,8 +90,6 @@ describe MailPage::ImportJob, dbscope: :example do
         expect(page2.group_ids).to eq node2.group_ids
         expect(page2.name).to eq "ISO-2022-JP"
         expect(page2.html.split("<br />")).to match_array decoded.split("\n")
-        expect(page2.mail_page_original_mail).to \
-          include("To: =?UTF-8?B?5LyK6Jek5oKg?= <sample@example.jp>", "From: =?UTF-8?B?5LyK6Jek5oKg?= <sample@example.jp>")
         expect(page2.arrival_start_date).to be_present
         expect(page2.arrival_close_date).to eq page2.arrival_start_date.advance(days: node2.arrival_days)
         expect(page2.state).to eq "public"
@@ -104,8 +98,6 @@ describe MailPage::ImportJob, dbscope: :example do
         expect(page3.group_ids).to eq node3.group_ids
         expect(page3.name).to eq "ISO-2022-JP"
         expect(page3.html.split("<br />")).to match_array decoded.split("\n")
-        expect(page3.mail_page_original_mail).to \
-          include("To: =?UTF-8?B?5LyK6Jek5oKg?= <sample@example.jp>", "From: =?UTF-8?B?5LyK6Jek5oKg?= <sample@example.jp>")
         expect(page3.arrival_start_date).to be_present
         expect(page3.arrival_close_date).to eq page3.arrival_start_date.advance(days: node3.arrival_days)
         expect(page3.state).to eq "public"
@@ -196,8 +188,6 @@ describe MailPage::ImportJob, dbscope: :example do
       expect(page1.group_ids).to eq node1.group_ids
       expect(page1.name).to eq "防災情報"
       expect(page1.html.split("<br />")).to match_array bosai_decoded.split("\n")
-      expect(page1.mail_page_original_mail).to \
-        include("To: =?UTF-8?B?5LyK6Jek5oKg?= <bosai@example.jp>", "From: =?UTF-8?B?5LyK6Jek5oKg?= <bosai@example.jp>")
       expect(page1.arrival_start_date).to be_present
       expect(page1.arrival_close_date).to eq page1.arrival_start_date.advance(days: node1.arrival_days)
       expect(page1.state).to eq "public"
@@ -206,8 +196,6 @@ describe MailPage::ImportJob, dbscope: :example do
       expect(page2.group_ids).to eq node2.group_ids
       expect(page2.name).to eq "交通情報"
       expect(page2.html.split("<br />")).to match_array kotsu_decoded.split("\n")
-      expect(page2.mail_page_original_mail).to \
-        include("To: =?UTF-8?B?5LyK6Jek5oKg?= <kotsu@example.jp>", "From: =?UTF-8?B?5LyK6Jek5oKg?= <kotsu@example.jp>")
       expect(page2.arrival_start_date).to be_present
       expect(page2.arrival_close_date).to eq page2.arrival_start_date.advance(days: node2.arrival_days)
       expect(page2.state).to eq "public"
@@ -250,6 +238,56 @@ describe MailPage::ImportJob, dbscope: :example do
 
       expect(MailPage::Page.site(site).where(filename: /^#{node1.filename}\//).count).to eq 1
       expect(MailPage::Page.site(site).where(filename: /^#{node2.filename}\//).count).to eq 1
+    end
+  end
+
+
+  context "enable auto_link" do
+    let(:node1) do
+      create :mail_page_node_page, layout: create_cms_layout, filename: "node1", arrival_days: rand(1..5),
+      mail_page_from_conditions: ["sample@example.jp"],
+      mail_page_to_conditions: ["sample@example.jp"],
+      auto_link_state: "enabled"
+    end
+    let(:node2) do
+      create :mail_page_node_page, layout: create_cms_layout, filename: "node2", arrival_days: rand(1..5),
+      mail_page_from_conditions: ["sample@example.jp"],
+      mail_page_to_conditions: ["sample@example.jp"],
+      auto_link_state: "disabled"
+    end
+    let(:mail_eml) do
+      data = Fs.binread("#{Rails.root}/spec/fixtures/mail_page/auto_link/UTF-8.eml")
+      SS::MailHandler.write_eml(data, "mail_page")
+    end
+    let(:html1) do
+      Fs.read("#{Rails.root}/spec/fixtures/mail_page/auto_link/html1")
+    end
+    let(:html2) do
+      Fs.read("#{Rails.root}/spec/fixtures/mail_page/auto_link/html2")
+    end
+
+    before do
+      node1
+      node2
+    end
+
+    it do
+      perform_enqueued_jobs do
+        mail_eml
+        described_class.bind(site_id: site).perform_later(mail_eml)
+      end
+
+      page1 = MailPage::Page.site(site).where(filename: /^#{node1.filename}\//).first
+      expect(page1.layout_id).to eq node1.layout_id
+      expect(page1.group_ids).to eq node1.group_ids
+      expect(page1.name).to eq "熊の目撃情報"
+      expect(page1.html).to eq html1
+
+      page2 = MailPage::Page.site(site).where(filename: /^#{node2.filename}\//).first
+      expect(page2.layout_id).to eq node2.layout_id
+      expect(page2.group_ids).to eq node2.group_ids
+      expect(page2.name).to eq "熊の目撃情報"
+      expect(page2.html).to eq html2
     end
   end
 end
