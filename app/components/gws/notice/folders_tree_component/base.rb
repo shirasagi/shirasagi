de376e6186791102099d8613820f4d2dc3827409
NAKANO Hideo <nakano@web-tips.co.jp>
2025-09-12 15:36:38 +0900

    [gws][notice] 新フォルダーツリーのテスト作成と中間階層が存在しない場合のケースに対応

--- app/components/gws/notice/folders_tree_component/base.rb
+++ app/components/gws/notice/folders_tree_component/base.rb
@@ -6,17 +6,17 @@ module Gws::Notice::FoldersTreeComponent::Base
   include SS::MaterialIconsHelper
 
   TEMPLATE = <<~ERB
-    <turbo-frame id="gws-notices-folder_tree-frame">
+    <turbo-frame id="gws-notice-folder_tree-frame">
       <%= cache_component do %>
         <div class="content-navi-refresh">
           <%= link_to md_icons.outlined("refresh"), gws_notice_frames_folders_trees_path(mode: mode), title: I18n.t("ss.buttons.reload"), data: { turbo: true } %>
         </div>
 
-        <div class="mt-2 mb-2 tree-item">
+        <div class="mt-2 mb-2 gws-notice-folder_tree-menu">
           <%= link_to t('gws/notice.all'), url_for(controller: "/gws/notice/\#{mode.pluralize}", action: :index, folder_id: '-'), data: { turbo: false } %>
         </div>
 
-        <%= render SS::TreeBaseComponent.new(root_nodes: root_nodes, css_class: "gws-notice-folders-tree") %>
+        <%= render SS::TreeBaseComponent.new(root_nodes: root_nodes, css_class: "gws-notice-folder_tree") %>
       <% end %>
     </turbo-frame>
   ERB
@@ -49,23 +49,41 @@ module Gws::Notice::FoldersTreeComponent::Base
 
     folders.to_a.each do |folder|
       folder.site = folder.cur_site = cur_site
-      base_name = ::File.basename(folder.name)
-      wrap = SS::TreeBaseComponent::NodeItem.new(
-        id: folder.id, name: base_name, depth: folder.depth, updated: folder.updated, url: item_url(folder), children: [])
-      parent_map[folder.name] = wrap
+
       if folder.depth == 1
+        wrap = SS::TreeBaseComponent::NodeItem.new(
+          id: folder.id, name: folder.name, depth: folder.depth, updated: folder.updated, url: item_url(folder), children: [])
         @root_nodes << wrap
+        parent_map[folder.name] = wrap
         next
       end
 
-      parent_name = ::File.dirname(folder.name)
-      parent_wrap = parent_map[parent_name]
-      unless parent_wrap
-        Rails.logger.warn { "'#{folder.name}' hasn't parent node" }
-        next
+      name_parts = folder.name.split("/")
+      split_pos = name_parts.length - 1
+      found = false
+      while split_pos > 0
+        parent_name = name_parts[0..(split_pos - 1)].join("/")
+        base_name = name_parts[split_pos..- 1].join("/")
+        parent_wrap = parent_map[parent_name]
+
+        split_pos -= 1
+
+        if parent_wrap
+          wrap = SS::TreeBaseComponent::NodeItem.new(
+            id: folder.id, name: base_name, depth: folder.depth, updated: folder.updated, url: item_url(folder), children: [])
+          parent_wrap.children << wrap
+          parent_map[folder.name] = wrap
+          found = true
+          break
+        end
       end
 
-      parent_wrap.children << wrap
+      unless found
+        wrap = SS::TreeBaseComponent::NodeItem.new(
+          id: folder.id, name: folder.name, depth: folder.depth, updated: folder.updated, url: item_url(folder), children: [])
+        @root_nodes << wrap
+        parent_map[folder.name] = wrap
+      end
     end
 
     @root_nodes
