88b62b823b8cbf69afe7588c9665e493d7148e7e
NAKANO Hideo <nakano@web-tips.co.jp>
2025-10-03 14:43:44 +0900

    [gws] スケジュールの全体タブが遅い問題の修正

--- app/components/gws/group_tree_component.rb
+++ app/components/gws/group_tree_component.rb
@@ -9,9 +9,72 @@ class Gws::GroupTreeComponent < ApplicationComponent
     [ site.id, results["count"], results["max"].to_i ]
   end
 
+  class TreeBuilder
+    include ActiveModel::Model
+
+    attr_accessor :items, :item_url_p
+
+    def call
+      items_array = items.to_a
+      name_node_map = {}
+      items_array.each do |group|
+        group_depth = group.name.count("/")
+        name_node_map[group.name] = new_node_item(group, depth: group_depth)
+      end
+
+      root_nodes = []
+      items_array.each do |group|
+        node = name_node_map[group.name]
+        name_parts = group.name.split("/")
+        group_depth = name_parts.length + 1
+        if group_depth == 1
+          root_nodes << node
+          next
+        end
+
+        split_pos = name_parts.length - 1
+        found = false
+        while split_pos > 0
+          parent_name = name_parts[0..(split_pos - 1)].join("/")
+          base_name = name_parts[split_pos..- 1].join("/")
+          parent_node = name_node_map[parent_name]
+
+          split_pos -= 1
+          next unless parent_node
+
+          node = update_node_item(node, depth: group_depth - split_pos + 1, name: base_name)
+          name_node_map[group.name] = node
+          parent_node.children << node
+          found = true
+
+          break
+        end
+
+        next if found
+
+        node = update_node_item(node, depth: 1, name: group.name)
+        name_node_map[group.name] = node
+        root_nodes << node
+      end
+
+      root_nodes
+    end
+
+    def new_node_item(group, depth:)
+      opens = depth <= 1
+      SS::TreeBaseComponent::NodeItem.new(
+        id: group.id, name: group.name, depth: depth, updated: group.updated,
+        url: item_url_p.call(group), opens: opens, children: [])
+    end
+
+    def update_node_item(node, depth:, name:)
+      opens = depth <= 1
+      node.with(depth: 1, name: name, opens: opens)
+    end
+  end
+
   def root_nodes
-    return @root_nodes if @root_nodes
-    build_tree
+    @root_nodes ||= TreeBuilder.new(items: items, item_url_p: method(:item_url)).call
   end
 
   private
@@ -24,64 +87,6 @@ class Gws::GroupTreeComponent < ApplicationComponent
       criteria = criteria.reorder(order: 1, id: 1)
       criteria
     end
-  end
-
-  def build_tree
-    items_array = items.to_a
-    name_node_map = {}
-    items_array.each do |group|
-      group_depth = group.name.count("/")
-      name_node_map[group.name] = new_node_item(group, depth: group_depth)
-    end
-
-    @root_nodes = []
-    items_array.each do |group|
-      node = name_node_map[group.name]
-      name_parts = group.name.split("/")
-      group_depth = name_parts.length + 1
-      if group_depth == 1
-        @root_nodes << node
-        next
-      end
-
-      split_pos = name_parts.length - 1
-      found = false
-      while split_pos > 0
-        parent_name = name_parts[0..(split_pos - 1)].join("/")
-        base_name = name_parts[split_pos..- 1].join("/")
-        parent_node = name_node_map[parent_name]
-
-        split_pos -= 1
-        next unless parent_node
-
-        node = update_node_item(node, depth: group_depth - split_pos + 1, name: base_name)
-        name_node_map[group.name] = node
-        parent_node.children << node
-        found = true
-
-        break
-      end
-
-      next if found
-
-      node = update_node_item(node, depth: 1, name: group.name)
-      name_node_map[group.name] = node
-      @root_nodes << node
-    end
-
-    @root_nodes
-  end
-
-  def new_node_item(group, depth:)
-    opens = depth <= 1
-    SS::TreeBaseComponent::NodeItem.new(
-      id: group.id, name: group.name, depth: depth, updated: group.updated,
-      url: item_url(group), opens: opens, children: [])
-  end
-
-  def update_node_item(node, depth:, name:)
-    opens = depth <= 1
-    node.with(depth: 1, name: name, opens: opens)
   end
 
   def item_url(group)
--- app/components/gws/schedule/tree_group_component.rb
+++ app/components/gws/schedule/tree_group_component.rb
@@ -1,24 +1,51 @@
 class Gws::Schedule::TreeGroupComponent < ApplicationComponent
+  include ActiveModel::Model
   include SS::CacheableComponent
 
-  self.cache_key = ->{ [ cur_site.id, group_max_updated.to_i, user_max_updated.to_i ] }
+  attr_accessor :cur_site, :cur_user, :cur_group
 
-  def initialize(cur_site:)
-    super()
-    @cur_site = cur_site
+  self.cache_key = ->do
+    group_aggregates = all_groups.aggregates(:updated)
+    user_aggregates = all_users.aggregates(:updated)
+    [ cur_site.id, group_aggregates["count"], group_aggregates["max"].to_i, user_aggregates["count"], user_aggregates["max"].to_i ]
   end
 
-  attr_reader :cur_site
-
-  def groups
-    @groups ||= cur_site.descendants.active.tree_sort(root_name: cur_site.name)
+  def root_nodes
+    @root_nodes ||= Gws::GroupTreeComponent::TreeBuilder.new(items: all_groups, item_url_p: method(:item_url)).call
   end
 
-  def group_max_updated
-    @group_max_updated ||= cur_site.descendants.max(:updated)
+  private
+
+  def all_groups
+    @all_groups ||= begin
+      criteria = Gws::Group.site(cur_site)
+      criteria = criteria.active
+      criteria = criteria.allow(:read, cur_user, site: cur_site)
+      criteria = criteria.reorder(order: 1, id: 1)
+      criteria
+    end
   end
 
-  def user_max_updated
-    @user_max_updated ||= Gws::User.all.site(cur_site).max(:updated)
+  def all_users
+    @all_users ||= Gws::User.all.site(cur_site).active
+  end
+
+  def group_id_user_map
+    @group_id_user_map ||= begin
+      map = {}
+      all_users.to_a.each do |user|
+        user.group_ids.each do |group_id|
+          map[group_id] ||= []
+          map[group_id] << user
+        end
+      end
+      map
+    end
+  end
+
+  def item_url(group)
+    if group_id_user_map[group.id]
+      gws_schedule_group_plans_path(site: cur_site, group: group)
+    end
   end
 end
