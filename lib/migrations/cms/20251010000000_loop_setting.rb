ae0e5310f207d9521111a94a7395095a5d6125da
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-10-10 11:18:03 +0900

    マイグレーションファイル作成,test pass

---
+++ lib/migrations/cms/20251010000000_loop_setting.rb
@@ -0,0 +1,29 @@
+class SS::Migration20251010000000
+  include SS::Migration::Base
+
+  depends_on "20250801000000"
+
+  def change
+    each_loop_setting do |loop_setting|
+      updates = {}
+
+      if loop_setting[:state].blank?
+        updates[:state] = 'public'
+      end
+
+      if loop_setting[:html_format].blank?
+        updates[:html_format] = 'shirasagi'
+      end
+
+      next if updates.blank?
+
+      loop_setting.set(updates)
+    end
+  end
+
+  private
+
+  def each_loop_setting(&block)
+    Cms::LoopSetting.all.each(&block)
+  end
+end
