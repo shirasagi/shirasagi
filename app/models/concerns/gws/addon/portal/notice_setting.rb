eafdbf5fa7c7f5e49b3989dc3097df0f02e25d7e
NAKANO Hideo <nakano@web-tips.co.jp>
2025-10-30 18:52:48 +0900

    [modify][gws][portal] remove unused setting "portal_notice_severity" (#5834)

--- app/models/concerns/gws/addon/portal/notice_setting.rb
+++ app/models/concerns/gws/addon/portal/notice_setting.rb
@@ -5,21 +5,13 @@ module Gws::Addon::Portal::NoticeSetting
   included do
     field :portal_notice_state, type: String, default: "show"
     field :portal_notice_browsed_state, type: String
-    field :portal_notice_severity, type: String
 
-    permit_params :portal_notice_state, :portal_notice_browsed_state, :portal_notice_severity
+    permit_params :portal_notice_state, :portal_notice_browsed_state
 
     before_validation :set_default_notice_setting
 
     validates :portal_notice_state, inclusion: { in: %w(show hide), allow_blank: true }
     validates :portal_notice_browsed_state, inclusion: { in: %w(unread read both), allow_blank: true }
-    validates :portal_notice_severity, inclusion: { in: %w(all high), allow_blank: true }
-  end
-
-  def portal_notice_severity_options
-    %w(all high normal).map do |v|
-      [ I18n.t("gws/notice.options.severity.#{v}"), v ]
-    end
   end
 
   def portal_notice_state_options
@@ -44,9 +36,6 @@ module Gws::Addon::Portal::NoticeSetting
     site = cur_site || site
     return unless site
 
-    if portal_notice_severity.blank?
-      self.portal_notice_severity = site.notice_severity
-    end
     if portal_notice_browsed_state.blank?
       self.portal_notice_browsed_state = site.notice_browsed_state
     end
