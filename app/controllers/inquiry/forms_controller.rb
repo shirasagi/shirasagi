80bbad03e2b6e6af7c13641e41543de11f4c67d0
tany <tanizawa@web-tips.co.jp>
2025-11-20 14:49:04 +0900

    [modify] inquiry: 権限

--- app/controllers/inquiry/forms_controller.rb
+++ app/controllers/inquiry/forms_controller.rb
@@ -1,9 +1,13 @@
 class Inquiry::FormsController < ApplicationController
   include Cms::BaseFilter
 
-  prepend_before_action ->{ redirect_to inquiry_columns_path }, only: :index
-
   def index
-    # redirect
+    if @cur_node.allowed?(:edit, @cur_user, site: @cur_site)
+      redirect_to inquiry_columns_path
+    elsif @cur_node.allowed?(:read, @cur_user, site: @cur_site)
+      redirect_to inquiry_answers_path
+    else
+      raise '403'
+    end
   end
 end
