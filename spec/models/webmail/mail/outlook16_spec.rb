cd8ad5156417f0e2de6553b95a36da9ff5567760
NAKANO Hideo <nakano@web-tips.co.jp>
2025-12-03 17:24:25 +0900

    [add][webmail] Microsoft Outlook 16 support (#5855)
    * [add][webmail] Microsoft Outlook 16 support
    
    * [add][webmail] add spec
    
    * refactor w/ RuboCop
    
    * subject support of Microsoft Outlook 16
    
    * from / to / cc / bcc support of Microsoft Outlook 16
    
    * [fix] nil error
    
    * [fix] spec failures

---
+++ spec/models/webmail/mail/outlook16_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Webmail::Mail, type: :model, dbscope: :example, imap: true do
+  context "outlook16 mail corruption" do
+    let(:user) { webmail_imap }
+    let(:account) { 0 }
+    let(:imap) { user.initialize_imap(account) }
+    let(:mailbox) { "INBOX" }
+    let(:msg) { File.read(Rails.root.join("spec/fixtures/webmail/outlook16.eml")) }
+
+    before do
+      webmail_import_mail(user, msg)
+      Webmail.imap_pool.disconnect_all
+
+      ActionMailer::Base.deliveries.clear
+    end
+
+    after do
+      ActionMailer::Base.deliveries.clear
+      Webmail.imap_pool.disconnect_all
+    end
+
+    it do
+      webmail_imap_login = imap.login
+      expect(webmail_imap_login).to be_truthy
+
+      imap.examine(mailbox)
+      items = imap.mails.mailbox(mailbox).all
+      item = imap.mails.find(items[0].uid, :body)
+      expect(item.display_subject).to include("件名テスト")
+      expect(item.display_sender.name).to eq "差出人テスト"
+      expect(item.display_to.first.name).to eq "宛先テスト"
+      expect(item.text).to include("本文テスト")
+    end
+  end
+end
