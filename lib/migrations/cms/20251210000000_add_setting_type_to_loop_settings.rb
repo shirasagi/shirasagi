ea46ae41bef3579897af8bc52861be63044f1c89
佃さよこ <tsukudasayoko@tsukudasayokonoMacBook-Pro.local>
2025-12-16 15:25:43 +0900

    fix(cms): harden loop HTML loading and migration batching
    - URL-encode loopSettingId and abort stale XHR to avoid race overwrites
    - Allow empty-string HTML to be applied in layout_html editor
    - Batch-read loop settings in migration and skip invalid docs

--- lib/migrations/cms/20251210000000_add_setting_type_to_loop_settings.rb
+++ lib/migrations/cms/20251210000000_add_setting_type_to_loop_settings.rb
@@ -4,23 +4,38 @@ class SS::Migration20251210000000
   depends_on "20251023000000"
 
   def change
-    Cms::LoopSetting.all.find_each(batch_size: 1_000) do |loop_setting|
+    Cms::LoopSetting.all.find_in_batches(batch_size: 1_000) do |loop_settings|
+      batch_ids = loop_settings.map(&:id).compact
+
       # DB に実際に保存されている値を取得（デフォルト値が補完された attributes は使わない）
-      raw = loop_setting.collection.find({ "_id" => loop_setting.id }).first || {}
-      attrs = {}
+      raw_by_id = Cms::LoopSetting.collection.find({ "_id" => { "$in" => batch_ids } }).to_a.index_by { |doc| doc["_id"] }
 
-      # 既にsetting_typeが設定されている場合はスキップ
-      next if raw["setting_type"].present?
+      loop_settings.each do |loop_setting|
+        raw = raw_by_id[loop_setting.id] || {}
+        attrs = {}
 
-      # 「スニペット/」プレフィックスで判定
-      name = raw["name"].to_s.presence || loop_setting.name.to_s
-      if name.start_with?("スニペット/")
-        attrs[:setting_type] = "snippet"
-      else
-        attrs[:setting_type] = "template"
+        if raw.blank?
+          Rails.logger.info("SS::Migration20251210000000 skip Cms::LoopSetting##{loop_setting.id}: raw doc is missing")
+          next
+        end
+
+        # 既にsetting_typeが設定されている場合はスキップ
+        next if raw["setting_type"].present?
+
+        # 「スニペット/」プレフィックスで判定
+        name = raw["name"].to_s
+        if name.blank?
+          Rails.logger.info("SS::Migration20251210000000 skip Cms::LoopSetting##{loop_setting.id}: raw['name'] is blank")
+          next
+        end
+        if name.start_with?("スニペット/")
+          attrs[:setting_type] = "snippet"
+        else
+          attrs[:setting_type] = "template"
+        end
+
+        loop_setting.set(attrs)
       end
-
-      loop_setting.set(attrs)
     end
   end
 end
