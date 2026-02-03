021f75563b943fce212dc834b143f59e43bd77b3
NAKANO Hideo <nakano@web-tips.co.jp>
2026-02-03 15:11:22 +0900

    [modify][cms] フォルダー複製時にタイトルを変更できるようにする修正

--- spec/jobs/cms/node/copy_nodes_job/layout_spec.rb
+++ spec/jobs/cms/node/copy_nodes_job/layout_spec.rb
@@ -4,22 +4,27 @@ describe Cms::Node::CopyNodesJob, dbscope: :example do
   describe "copy layout" do
     let(:site) { cms_site }
     let(:target_node_name) { unique_id }
-    let(:task) { create :copy_nodes_task, target_node_name: target_node_name, site_id: site.id, node_id: node.id }
+    let(:target_node_filename) { unique_id }
+    let(:task) do
+      create(
+        :copy_nodes_task, site_id: site.id, node_id: node.id,
+        target_node_name: target_node_name, target_node_filename: target_node_filename
+      )
+    end
     let!(:node) { create :cms_node, cur_site: site }
-    let!(:layout) { create :cms_layout, filename: "#{node.filename}/layout" }
+    let!(:layout) { create :cms_layout, cur_site: site, cur_node: node, basename: "layout" }
 
     describe "copy layout which is located under a node" do
-
       before do
-        perform_enqueued_jobs do
-          Cms::Node::CopyNodesJob.bind( {site_id: site.id, node_id: node.id} )
-          .perform_now( {target_node_name: target_node_name} )
-        end
+        expect do
+          job = Cms::Node::CopyNodesJob.bind(site_id: site.id, node_id: node.id)
+          job.perform_now(target_node_name: target_node_name, target_node_filename: target_node_filename)
+        end.to output(include(layout.filename)).to_stdout
       end
 
       it "copied layout filename was changed " do
-        copied_layout = Cms::Layout.site(site).where(filename: /^#{target_node_name}\//).first
-        expect(copied_layout.filename).to eq "#{target_node_name}/layout.layout.html"
+        copied_layout = Cms::Layout.site(site).where(filename: /^#{target_node_filename}\//).first
+        expect(copied_layout.filename).to eq "#{target_node_filename}/layout.layout.html"
       end
     end
   end
--- spec/jobs/cms/node/copy_nodes_job/part_spec.rb
+++ spec/jobs/cms/node/copy_nodes_job/part_spec.rb
@@ -4,29 +4,35 @@ describe Cms::Node::CopyNodesJob, dbscope: :example do
   describe "copy part" do
     let(:site) { cms_site }
     let(:target_node_name) { unique_id }
-    let(:task) { create :copy_nodes_task, target_node_name: target_node_name, site_id: site.id, node_id: node.id }
+    let(:target_node_filename) { unique_id }
+    let(:task) do
+      create(
+        :copy_nodes_task, site_id: site.id, node_id: node.id,
+        target_node_name: target_node_name, target_node_filename: target_node_filename
+      )
+    end
     let!(:node) { create :cms_node, cur_site: site }
     let!(:part) do
-      create :article_part_page,
-        cur_site: site,
-        filename: "#{node.filename}/part",
+      create(
+        :article_part_page, cur_site: site, cur_node: node, basename: "part",
         lower_html: "<div class=\"feed\"><a class=\"rss\" href=\"/#{node.filename}/rss.xml\">RSS</a>"
+      )
     end
 
     describe "copy part which is located under a node" do
 
       before do
-        perform_enqueued_jobs do
-          Cms::Node::CopyNodesJob.bind( {site_id: site.id, node_id: node.id} )
-          .perform_now( {target_node_name: target_node_name} )
-        end
+        expect do
+          job_class = Cms::Node::CopyNodesJob.bind(site_id: site.id, node_id: node.id)
+          ss_perform_now(job_class, target_node_name: target_node_name, target_node_filename: target_node_filename)
+        end.to output(include(part.filename)).to_stdout
       end
 
       it "html element which has original node name was overwritten" do
-        copied_part = Cms::Part.site(site).where(filename: /^#{target_node_name}\//).first
-        expect(copied_part.filename).to eq "#{target_node_name}/part.part.html"
+        copied_part = Cms::Part.site(site).where(filename: /^#{target_node_filename}\//).first
+        expect(copied_part.filename).to eq "#{target_node_filename}/part.part.html"
         expect(copied_part.lower_html).not_to include node.filename.to_s
-        expect(copied_part.lower_html).to include target_node_name.to_s
+        expect(copied_part.lower_html).to include target_node_filename.to_s
       end
     end
   end
