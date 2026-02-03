a1901cbff987d9bc3cfa8af58e01ed78cba3a4e7
NAKANO Hideo <nakano@web-tips.co.jp>
2026-02-03 11:45:36 +0900

    [modify] 承認可能ユーザーに絞り込む処理を共通化

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -5,4 +5,11 @@ module Workflow
     now ||= Time.zone.now.change(usec: 0)
     now > content.updated + duration
   end
+
+  def approvable_users(cur_site:, item:, criteria_or_array: nil)
+    criteria_or_array ||= Cms::User.all.site(cur_site)
+    criteria_or_array.select do |user|
+      item.allowed?(:read, user, site: cur_site) && item.allowed?(:approve, user, site: cur_site) && user.enabled?
+    end
+  end
 end
