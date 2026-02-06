9a0d641faf1f3c7280757974df22d89dec7ff0ad
NAKANO Hideo <nakano@web-tips.co.jp>
2026-02-06 13:53:33 +0900

    [modify^2] reduce duplicated codes
    ApplicationHelper#tt と HumanAttributeNmae#tt とを共通化

--- app/models/concerns/ss/human_attribute_name.rb
+++ app/models/concerns/ss/human_attribute_name.rb
@@ -11,25 +11,40 @@ module SS::HumanAttributeName
     end
 
     def tt(key)
-      model_names = ancestors.select { |x| x.respond_to?(:model_name) }
-      msg = ""
-      model_names.each do |model_name|
-        msg = I18n.t("tooltip.#{model_name.model_name.i18n_key}.#{key}", default: "")
+      model_names = ancestors.filter_map { _1.try(:model_name) }
+      scope = model_names.map { "tooltip.#{_1.i18n_key}" }
+      SS::HumanAttributeName.tt(key, scope: scope)
+    end
+  end
+
+  def self.find_tt(key, scope: nil, **opts)
+    msg = nil
+
+    if scope
+      Array(scope).flatten.each do |scope1|
+        msg = I18n.t(key, **opts.merge(default: '', scope: scope1))
         break if msg.present?
       end
-      return msg if msg.blank?
-
-      list = Array(msg)
-        .map { _1.to_s.gsub(/\r\n|\n/, "<br />") }
-        .map { "<li>#{_1}<br /></li>" }
-
-      <<~HTML.html_safe
-        <div class="tooltip">?
-          <ul class="tooltip-content">
-            #{list.join("\n")}
-          </ul>
-        </div>
-      HTML
     end
+
+    msg = I18n.t(key, **opts.merge(default: '', scope: 'tooltip')) if msg.blank?
+    msg
+  end
+
+  def self.tt(key, scope: nil, symbol: nil, html_wrap: true, **opts)
+    msg = self.find_tt(key, scope: scope, **opts)
+    return msg if msg.blank? || !html_wrap
+
+    list = Array(msg)
+      .map { _1.to_s.gsub(/\r\n|\n/, "<br />") }
+      .map { "<li>#{_1}<br /></li>" }
+
+    <<~HTML.html_safe
+      <div class="tooltip">#{symbol || "?"}
+        <ul class="tooltip-content">
+          #{list.join("\n")}
+        </ul>
+      </div>
+    HTML
   end
 end
