8e11e382cad7fe8473ab5e50ad67a18ceddb41a0
NAKANO Hideo <nakano@web-tips.co.jp>
2026-02-09 16:51:38 +0900

    [modify][inquiry] remove {read,edit,delte}_private_inquiry_columns
    このモデルはグループでは管理しない。

--- app/models/inquiry/initializer.rb
+++ app/models/inquiry/initializer.rb
@@ -6,11 +6,8 @@ module Inquiry
     Cms::Part.plugin "inquiry/feedback"
 
     Cms::Role.permission :read_other_inquiry_columns
-    Cms::Role.permission :read_private_inquiry_columns
     Cms::Role.permission :edit_other_inquiry_columns
-    Cms::Role.permission :edit_private_inquiry_columns
     Cms::Role.permission :delete_other_inquiry_columns
-    Cms::Role.permission :delete_private_inquiry_columns
 
     Cms::Role.permission :read_other_inquiry_answers
     Cms::Role.permission :read_private_inquiry_answers
