3741f3f4cab1b3bddec01959ed9478a40eae66a6
NAKANO Hideo <nakano@web-tips.co.jp>
2026-02-12 11:32:34 +0900

    [modify] test new role "feedback manager"

--- spec/models/inquiry/enum_menu_items_spec.rb
+++ spec/models/inquiry/enum_menu_items_spec.rb
@@ -85,6 +85,29 @@ describe Inquiry, dbscope: :example do
 
       it do
         menu_items = Inquiry.enum_menu_items(site, node, user).to_a
+        expect(menu_items.length).to eq 2
+        menu_items[0].tap do |menu_item|
+          expect(menu_item.label).to eq I18n.t("inquiry.answer")
+          expect(menu_item.path).to eq helpers.inquiry_answers_path(site: site, cid: node)
+        end
+        menu_items[1].tap do |menu_item|
+          expect(menu_item.label).to eq I18n.t("inquiry.result")
+          expect(menu_item.path).to eq helpers.inquiry_results_path(site: site, cid: node)
+        end
+      end
+    end
+
+    context "with feedback manager" do
+      let!(:role) do
+        permissions = %w(
+          read_private_cms_nodes edit_private_cms_nodes
+          read_private_inquiry_answers edit_private_inquiry_answers delete_private_inquiry_answers)
+        create :cms_role, cur_site: site, name: unique_id, permissions: permissions
+      end
+      let!(:user) { create :cms_test_user, cur_site: site, cms_role_ids: [ role.id ], group_ids: [ group.id ] }
+
+      it do
+        menu_items = Inquiry.enum_menu_items(site, node, user).to_a
         expect(menu_items.length).to eq 3
         menu_items[0].tap do |menu_item|
           expect(menu_item.label).to eq I18n.t("inquiry.answer")
