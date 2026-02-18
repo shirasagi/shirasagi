681c5aea545d65bc05a2b6f8b4de017d4d424a04
NAKANO Hideo <nakano@web-tips.co.jp>
2026-02-18 10:14:25 +0900

    [fixup][cms] 一時的に cms/contents の :mod パラメータを廃止し、cms/contents の index 以外のルーティングを廃止

--- app/controllers/cms/contents_controller.rb
+++ app/controllers/cms/contents_controller.rb
@@ -18,5 +18,11 @@ class Cms::ContentsController < ApplicationController
     @s = Cms::ShortcutComponent::SearchParams.new
     @s = @s.with(mod: params[:mod]) if params[:mod].present?
     @s = @s.with(keyword: params.dig(:s, :keyword)) if params.dig(:s, :keyword).present?
+
+    respond_to do |format|
+      format.html { render }
+      # 高速化により JSON には応答不能になった
+      format.json { head :not_found }
+    end
   end
 end
