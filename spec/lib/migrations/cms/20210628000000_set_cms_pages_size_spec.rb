94dff94934fcac22a58959f1e440da3685381c70
NAKANO Hideo <nakano@web-tips.co.jp>
2026-02-18 15:29:26 +0900

    [modify] migration "20210628000000 set_cms_pages_size" の高速化 (#5926)
    * [modify] migration "20210628000000 set_cms_pages_size" の高速化
    
    * [fixup][modify] migration "20210628000000 set_cms_pages_size" の高速化

--- spec/lib/migrations/cms/20210628000000_set_cms_pages_size_spec.rb
+++ spec/lib/migrations/cms/20210628000000_set_cms_pages_size_spec.rb
@@ -3,88 +3,102 @@ require Rails.root.join("lib/migrations/cms/20210628000000_set_cms_pages_size.rb
 
 RSpec.describe SS::Migration20210628000000, dbscope: :example do
   context "with html page" do
-    let(:page) { create :cms_page }
+    let!(:site) { cms_site }
+    let!(:user) { cms_user }
     let(:html) { unique_id }
+    let!(:page) { create :cms_page, cur_site: site, cur_user: user, html: html }
 
     before do
-      page.set(html: html)
+      page.unset(:size)
       described_class.new.change
     end
 
     it do
       page.reload
       expect(page.size).to eq html.bytesize
+
+      # マイグレーション結果と page.new_size_input の結果が同じかを確認
+      page.new_size_input
+      expect(page.size).to eq html.bytesize
+    end
+  end
+
+  context "with html and file" do
+    let!(:site) { cms_site }
+    let!(:user) { cms_user }
+    let!(:file) { tmp_ss_file(site: site, user: user, contents: "#{Rails.root}/spec/fixtures/ss/logo.png") }
+    let(:html) { unique_id }
+    let!(:page) { create :cms_page, cur_site: site, cur_user: user, html: html, file_ids: [ file.id ] }
+
+    before do
+      page.unset(:size)
+      described_class.new.change
+    end
+
+    it do
+      page.reload
+      expect(page.size).to eq html.bytesize + file.size
+
+      # マイグレーション結果と page.new_size_input の結果が同じかを確認
+      page.new_size_input
+      expect(page.size).to eq html.bytesize + file.size
     end
   end
 
   context "with form page" do
-    let(:site) { cms_site }
-    let(:node) { create :article_node_page, cur_site: site }
+    let!(:site) { cms_site }
+    let!(:user) { cms_user }
+    let!(:file) { tmp_ss_file(site: site, user: user, contents: "#{Rails.root}/spec/fixtures/ss/logo.png") }
+    let!(:node) { create :article_node_page, cur_site: site, cur_user: user }
 
-    let(:form) { create :cms_form, cur_site: site, state: 'public', sub_type: 'entry', html: nil }
-    let(:page) { create :article_page, cur_site: site, cur_node: node, form: form }
-    let(:column1) do
+    let!(:form) { create :cms_form, cur_site: site, state: 'public', sub_type: 'entry', html: nil }
+    let!(:column1) do
       create(:cms_column_text_field, cur_site: site, name: "column1", cur_form: form, required: "optional", order: 1)
     end
-    let(:column2) do
-      create(:cms_column_text_field, cur_site: site, name: "column2", cur_form: form, required: "optional", order: 2)
+    let!(:column2) do
+      create(:cms_column_free, cur_site: site, name: "column2", cur_form: form, required: "optional", order: 2)
     end
-    let(:column_values) do
-      [
-        column1.value_type.new(column: column1, value: "value1"),
-        column2.value_type.new(column: column2, value: "value2")
-      ]
+    let!(:page) do
+      page = create(
+        :article_page, cur_site: site, cur_user: user, cur_node: node, form: form,
+        column_values: [
+          column1.value_type.new(column: column1, value: "value1"),
+          column2.value_type.new(column: column2, value: "value2", file_ids: [ file.id ])
+        ])
+      # page はメンバー変数などで汚染されている；まっさらなページを取得して操作する。
+      Article::Page.find(page.id)
     end
     let(:rendered_html) do
-      '<div class="ss-alignment ss-alignment-flow">value1</div><div class="ss-alignment ss-alignment-flow">value2</div>'
+      page.render_html
     end
 
     it "unset size and migration" do
-      Article::Page.find(page.id).tap do |work_page|
-        # page はメンバー変数などで汚染されている；まっさらなページを取得して操作する。
-        work_page.column_values = column_values
-        work_page.save!
-      end
-
-      Article::Page.find(page.id).tap do |work_page|
-        # page はメンバー変数などで汚染されている；まっさらなページを取得して操作する。
-        expect(work_page.render_html).to eq rendered_html
-        expect(work_page.size).to eq rendered_html.bytesize
-      end
+      expect(rendered_html.length).to be > 100
 
       page.unset(:size)
       described_class.new.change
 
-      Article::Page.find(page.id).tap do |work_page|
-        # page はメンバー変数などで汚染されている；まっさらなページを取得して操作する。
-        expect(work_page.render_html).to eq rendered_html
-        expect(work_page.size).to eq rendered_html.bytesize
+      Article::Page.find(page.id).tap do |page_after_migration|
+        expect(page_after_migration.size).to eq rendered_html.bytesize + file.size
+
+        # マイグレーション結果と page.new_size_input の結果が同じかを確認
+        page_after_migration.new_size_input
+        expect(page_after_migration.size).to eq rendered_html.bytesize + file.size
       end
     end
 
     it "when site removed" do
-      Article::Page.find(page.id).tap do |work_page|
-        # page はメンバー変数などで汚染されている；まっさらなページを取得して操作する。
-        work_page.column_values = column_values
-        work_page.save!
-      end
+      expect(rendered_html.length).to be > 100
 
-      Article::Page.find(page.id).tap do |work_page|
-        # page はメンバー変数などで汚染されている；まっさらなページを取得して操作する。
-        expect(work_page.render_html).to eq rendered_html
-        expect(work_page.size).to eq rendered_html.bytesize
-      end
-
-      page.unset(:size)
+      page.set(size: 50)
       site.destroy
 
       # Run migration
       described_class.new.change
 
-      Article::Page.find(page.id).tap do |work_page|
-        # page はメンバー変数などで汚染されている；まっさらなページを取得して操作する。
-        expect(work_page.render_html).to eq nil
-        expect(work_page.size).to eq nil
+      Article::Page.find(page.id).tap do |page_after_migration|
+        # size is unable to update because site was deleted
+        expect(page_after_migration.size).to eq 50
       end
     end
   end
