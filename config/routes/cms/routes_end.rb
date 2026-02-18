61b4f08fa7bb2c928d85ac461ab4bf253b9e8106
NAKANO Hideo <nakano@web-tips.co.jp>
2026-02-18 10:14:06 +0900

    [revert][cms] 一時的に cms/contents の :mod パラメータを廃止し、cms/contents の index 以外のルーティングを廃止

--- config/routes/cms/routes_end.rb
+++ config/routes/cms/routes_end.rb
@@ -130,7 +130,7 @@ Rails.application.routes.draw do
       get :verify, on: :member
       post :verify, on: :member
     end
-    resources :contents, only: %i[index]
+    resources :contents, path: "contents/(:mod)", only: %i[index]
 
     resources :nodes, concerns: [:deletion, :command, :change_state, :import] do
       get :routes, on: :collection
