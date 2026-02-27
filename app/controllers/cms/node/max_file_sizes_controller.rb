885356a0537727d11fdf446780a01dd9f9406fd6
NAKANO Hideo <nakano@web-tips.co.jp>
2026-02-27 13:57:14 +0900

    [modify][cms] refactor cms/max_file_sizes

--- app/controllers/cms/node/max_file_sizes_controller.rb
+++ app/controllers/cms/node/max_file_sizes_controller.rb
@@ -2,6 +2,7 @@ class Cms::Node::MaxFileSizesController < ApplicationController
   include Cms::BaseFilter
   include Cms::CrudFilter
 
+  navi_view "cms/node/main/navi"
   model Cms::MaxFileSize
 
   private
@@ -11,7 +12,84 @@ class Cms::Node::MaxFileSizesController < ApplicationController
   end
 
   def set_items
-    @items = @model.site(@cur_site).node(@cur_node).
-      allow(:read, @cur_user, site: @cur_site, node: @cur_node)
+    if @cur_node.allowed?(:read, @cur_user, site: @cur_site)
+      @items = @model.site(@cur_site).node(@cur_node)
+    else
+      @items = @model.none
+    end
+  end
+
+  def set_deletable
+    return @deletable if instance_variable_defined?(:@deletable)
+    @deletable = @cur_node.allowed?(:edit, @cur_user, site: @cur_site)
+  end
+
+  def destroy_items
+    raise "400" if @selected_items.blank?
+    raise "403" unless @cur_node.allowed?(:edit, @cur_user, site: @cur_site)
+
+    entries = @selected_items.entries
+    @items = []
+
+    entries.each do |item|
+      item.cur_user = @cur_user if item.respond_to?(:cur_user)
+      next if item.destroy
+      @items << item
+    end
+    entries.size != @items.size
+  end
+
+  public
+
+  def index
+    raise "403" unless @cur_node.allowed?(:read, @cur_user, site: @cur_site)
+    set_items
+    @items = @items.search(params[:s]).page(params[:page]).per(50)
+    render
+  end
+
+  def show
+    raise "403" unless @cur_node.allowed?(:read, @cur_user, site: @cur_site)
+    render
+  end
+
+  def new
+    raise "403" unless @cur_node.allowed?(:edit, @cur_user, site: @cur_site)
+    @item = @model.new pre_params.merge(fix_params)
+  end
+
+  def create
+    raise "403" unless @cur_node.allowed?(:edit, @cur_user, site: @cur_site)
+    @item = @model.new get_params
+    render_create @item.save
+  end
+
+  def edit
+    raise "403" unless @cur_node.allowed?(:edit, @cur_user, site: @cur_site)
+    if @item.is_a?(Cms::Addon::EditLock)
+      unless @item.acquire_lock
+        redirect_to action: :lock
+        return
+      end
+    end
+    render
+  end
+
+  def update
+    raise "403" unless @cur_node.allowed?(:edit, @cur_user, site: @cur_site)
+    @item.attributes = get_params
+    @item.in_updated = params[:_updated] if @item.respond_to?(:in_updated)
+    render_update @item.update
+  end
+
+  def delete
+    raise "403" unless @cur_node.allowed?(:edit, @cur_user, site: @cur_site)
+    render
+  end
+
+  def destroy
+    raise "403" unless @cur_node.allowed?(:edit, @cur_user, site: @cur_site)
+    @item.cur_user = @cur_user if @item.respond_to?(:cur_user)
+    render_destroy @item.destroy
   end
 end
