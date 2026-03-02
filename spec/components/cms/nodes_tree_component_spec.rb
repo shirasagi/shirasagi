325e998fcf1fc154b238979a9210d49520aa1d33
NAKANO Hideo <nakano@web-tips.co.jp>
2026-03-02 11:31:56 +0900

    [modify][cms] missing folders support to node tree

--- spec/components/cms/nodes_tree_component_spec.rb
+++ spec/components/cms/nodes_tree_component_spec.rb
@@ -11,6 +11,9 @@ describe Cms::NodesTreeComponent, type: :component, dbscope: :example do
     save_perform_caching = described_class.perform_caching
     described_class.perform_caching = perform_caching
 
+    # role をウォーミングアップしておく
+    user.cms_role_permit_any?(site, :read_private_cms_nodes)
+
     with_request_url("/.s#{site.id}/cms/nodes") do
       example.run
     end
@@ -65,7 +68,7 @@ describe Cms::NodesTreeComponent, type: :component, dbscope: :example do
 
         # キャッシュ無効時でも最小限のDBアクセスでキャッシュを利用可能なことを確認
         after_mongodb_count = MongoAccessCounter.succeeded_count
-        expect(after_mongodb_count - before_mongodb_count).to eq 2
+        expect(after_mongodb_count - before_mongodb_count).to eq 1
 
         # キャッシュは無効なので、テスト終了時点でもキャッシュは存在しない
         described_class.new(site: site, user: user, cache_mode: cache_mode).tap do |component|
@@ -91,7 +94,7 @@ describe Cms::NodesTreeComponent, type: :component, dbscope: :example do
 
         # キャッシュ無効時でも最小限のDBアクセスでキャッシュを利用可能なことを確認
         after_mongodb_count = MongoAccessCounter.succeeded_count
-        expect(after_mongodb_count - before_mongodb_count).to eq 2
+        expect(after_mongodb_count - before_mongodb_count).to eq 1
 
         # キャッシュは有効なので、テスト終了時点でキャッシュが存在する
         described_class.new(site: site, user: user, cache_mode: cache_mode).tap do |component|
@@ -142,6 +145,90 @@ describe Cms::NodesTreeComponent, type: :component, dbscope: :example do
             expect(cache_entry.expires_at).to be > before_cache_entry.expires_at
           end
         end
+      end
+    end
+  end
+
+  context "when a user doesn't have read permission to root folder" do
+    let!(:editor_role) do
+      permissions = %w(read_private_cms_nodes read_private_article_pages edit_private_article_pages)
+      create :cms_role, cur_site: site, name: unique_id, permissions: permissions
+    end
+    let!(:group0) { cms_group }
+    let!(:editor_group) { create :cms_group, name: "#{group0.name}/#{unique_id}" }
+    let!(:editor) { create :cms_test_user, cms_role_ids: [ editor_role.id ], group_ids: [ editor_group.id ] }
+    let(:perform_caching) { true }
+
+    let!(:node1) { create :cms_node_node, cur_site: site, group_ids: user.group_ids }
+    let!(:node1_1) { create :article_node_page, cur_site: site, cur_node: node1, group_ids: [ editor_group.id ] }
+
+    before do
+      expect(node1.allowed?(:read, editor, site: site)).to be_falsey
+      expect(node1_1.allowed?(:read, editor, site: site)).to be_truthy
+    end
+
+    it do
+      html = render_inline described_class.new(site: site, user: editor, cache_mode: cache_mode)
+      # puts html
+      html.css(".ss-tree-item[data-node-id='#{node1.id}']").tap do |tree_items|
+        expect(tree_items).to have(1).items
+      end
+      html.css(".ss-tree-item[data-node-id='#{node1_1.id}']").tap do |tree_items|
+        expect(tree_items).to have(1).items
+      end
+      html.css("a[href='/.s#{site.id}/cms#{node1.id}/nodes']").tap do |anchors|
+        expect(anchors).to have(0).items
+      end
+      html.css("a[href='/.s#{site.id}/article#{node1_1.id}/pages']").tap do |anchors|
+        expect(anchors).to have(1).items
+        expect(anchors[0].text.strip).to eq node1_1.name
+      end
+
+      html = render_inline described_class.new(site: site, user: user, cache_mode: cache_mode)
+      # puts html
+      html.css(".ss-tree-item[data-node-id='#{node1.id}']").tap do |tree_items|
+        expect(tree_items).to have(1).items
+      end
+      html.css(".ss-tree-item[data-node-id='#{node1_1.id}']").tap do |tree_items|
+        expect(tree_items).to have(1).items
+      end
+      html.css("a[href='/.s#{site.id}/cms#{node1.id}/nodes']").tap do |anchors|
+        expect(anchors).to have(1).items
+        expect(anchors[0].text.strip).to eq node1.name
+      end
+      html.css("a[href='/.s#{site.id}/article#{node1_1.id}/pages']").tap do |anchors|
+        expect(anchors).to have(1).items
+        expect(anchors[0].text.strip).to eq node1_1.name
+      end
+    end
+  end
+
+  context "when some mid-level folders are missing" do
+    let(:perform_caching) { true }
+
+    let!(:node1) { create :cms_node_node, cur_site: site }
+    let!(:node1_1) { create :article_node_page, cur_site: site, cur_node: node1 }
+
+    before do
+      node1.delete
+      expect { node1_1.reload }.not_to raise_error
+    end
+
+    it do
+      html = render_inline described_class.new(site: site, user: user, cache_mode: cache_mode)
+      # puts html
+      html.css(".ss-tree-item[data-node-id='not_found']").tap do |tree_items|
+        expect(tree_items).to have(1).items
+      end
+      html.css(".ss-tree-item[data-node-id='#{node1_1.id}']").tap do |tree_items|
+        expect(tree_items).to have(1).items
+      end
+      html.css("a[href='/.s#{site.id}/cms#{node1.id}/nodes']").tap do |anchors|
+        expect(anchors).to have(0).items
+      end
+      html.css("a[href='/.s#{site.id}/article#{node1_1.id}/pages']").tap do |anchors|
+        expect(anchors).to have(1).items
+        expect(anchors[0].text.strip).to eq node1_1.name
       end
     end
   end
