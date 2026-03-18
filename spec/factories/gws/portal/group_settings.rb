1aed488d297a139fe0cce53b70689d5521c551ea
NAKANO Hideo <nakano@web-tips.co.jp>
2026-03-18 18:27:26 +0900

    [modify] replace name field with name method responds currently available name

--- spec/factories/gws/portal/group_settings.rb
+++ spec/factories/gws/portal/group_settings.rb
@@ -3,7 +3,10 @@ FactoryBot.define do
     cur_site { gws_site }
     cur_user { gws_user }
 
-    name { cur_user.long_name }
+    name do
+      group = cur_user.groups.first
+      group.organization? ? I18n.t("gws/portal.tabs.root_portal") : group.trailing_name.truncate(20)
+    end
     portal_group { cur_user.groups.first }
   end
 end
