475da7ffec5f6333a73425288915c2785d9d6264
NAKANO Hideo <nakano@web-tips.co.jp>
2026-03-24 18:42:02 +0900

    [add][cms] add more specs about nofollow

--- spec/jobs/cms/check_links_job/basic_spec.rb
+++ spec/jobs/cms/check_links_job/basic_spec.rb
@@ -530,7 +530,7 @@ describe Cms::CheckLinksJob, dbscope: :example do
     end
 
     it do
-      expect { ss_perform_now described_class.bind(site_id: site.id) }.to output(/784 errors/).to_stdout
+      expect { ss_perform_now described_class.bind(site_id: site.id) }.to output(/5 errors/).to_stdout
 
       expect(Job::Log.count).to eq 1
       Job::Log.first.tap do |log|
@@ -555,6 +555,164 @@ describe Cms::CheckLinksJob, dbscope: :example do
       # 外部サイトURLについては転送量を押さえる目的で HEAD でアクセスする
       expect(a_request(:head, "https://www.example.jp/notfound1.html")).to have_been_made.times(1)
       expect(a_request(:head, "https://www.example.jp/notfound2.html")).to have_been_made.times(1)
+
+      expect(SS::Task.all.count).to eq 3
+      Cms::Task.all.where(name: "cms:check_links").first.tap do |task|
+        expect(task.site_id).to eq site.id
+        expect(task.node_id).to be_blank
+        expect(task.state).to eq "completed"
+        expect(task.started.in_time_zone).to be_within(5.minutes).of(Time.zone.now)
+        expect(task.closed.in_time_zone).to be_within(5.minutes).of(Time.zone.now)
+        expect(task.current_count).to eq 7
+
+        url_log_path = task.log_file_path.sub(".log", "") + "-url-log.json.gz"
+        url_logs = Zlib::GzipReader.open(url_log_path) { _1.readlines }.map { JSON.parse(_1.chomp) }
+        # puts url_logs.map { _1["full_url"] }.join("\n")
+        expect(url_logs.length).to eq 25
+
+        next_month_url = "#{calendar.full_url}#{Time.zone.today.next_month.strftime("%Y%m")}/"
+        expect(url_logs.select { _1["full_url"].start_with?(next_month_url) }.map { _1["status"] }.uniq).to eq %w(nofollow)
+
+        prev_month_url = "#{calendar.full_url}#{Time.zone.today.prev_month.strftime("%Y%m")}/"
+        expect(url_logs.select { _1["full_url"].start_with?(prev_month_url) }.map { _1["status"] }.uniq).to eq %w(nofollow)
+
+        event_url = "#{calendar.full_url}#{event_date.strftime("%Y%m%d")}/"
+        expect(url_logs.select { _1["full_url"] == event_url }.map { _1["status"] }.uniq).to eq %w(success)
+
+        expect(url_logs.select { _1["full_url"] == page1.full_url }.map { _1["status"] }.uniq).to eq %w(success)
+
+        list_url = "#{calendar.full_url}#{Time.zone.today.strftime("%Y%m")}/list.html"
+        expect(url_logs.select { _1["full_url"] == list_url }.map { _1["status"] }.uniq).to eq %w(success)
+
+        list_ics_url = "#{calendar.full_url}#{Time.zone.today.strftime("%Y%m")}/list.ics"
+        expect(url_logs.select { _1["full_url"] == list_ics_url }.map { _1["status"] }.uniq).to eq %w(nofollow)
+      end
+    end
+  end
+
+  context "with sns share part" do
+    context "with 'show'" do
+      let!(:part) do
+        share_states = Cms::Part::SnsShare::SERVICES.index_with { "show" }
+        create :cms_part_sns_share, cur_site: site, sns_share_states: share_states
+      end
+      let!(:layout) { create_cms_layout part, cur_site: site }
+      let!(:index) { create :cms_page, cur_site: site, layout: layout, filename: "index.html", state: "public" }
+
+      before do
+        expect { ss_perform_now Cms::Node::GenerateJob.bind(site_id: site.id) }.to output.to_stdout
+        expect { ss_perform_now Cms::Page::GenerateJob.bind(site_id: site.id) }.to output.to_stdout
+        Job::Log.destroy_all
+      end
+
+      it do
+        expect { ss_perform_now described_class.bind(site_id: site.id) }.to output(/0 errors/).to_stdout
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(/INFO -- : .* Started Job/)
+          expect(log.logs).to include(/INFO -- : .* Completed Job/)
+
+          expect(log.logs).to include(include("#{site_url}/"))
+        end
+
+        expect(Cms::CheckLinks::Report.all.count).to eq 1
+        Cms::CheckLinks::Report.all.first.tap do |report|
+          expect(report.site_id).to eq site.id
+          expect(report.name).to include "実行結果"
+          # yield 外のリンク切れからレポートは作成されない
+          expect(report.link_errors.count).to eq 0
+          expect(report.pages.count).to eq 0
+          expect(report.nodes.count).to eq 0
+        end
+
+        expect(ActionMailer::Base.deliveries.length).to eq 0
+
+        expect(SS::Task.all.count).to eq 3
+        Cms::Task.all.where(name: "cms:check_links").first.tap do |task|
+          expect(task.site_id).to eq site.id
+          expect(task.node_id).to be_blank
+          expect(task.state).to eq "completed"
+          expect(task.started.in_time_zone).to be_within(5.minutes).of(Time.zone.now)
+          expect(task.closed.in_time_zone).to be_within(5.minutes).of(Time.zone.now)
+          expect(task.current_count).to eq 1
+
+          url_log_path = task.log_file_path.sub(".log", "") + "-url-log.json.gz"
+          url_logs = Zlib::GzipReader.open(url_log_path) { _1.readlines }.map { JSON.parse(_1.chomp) }
+          # puts url_logs.map { _1["full_url"] }.join("\n")
+          expect(url_logs.length).to eq 6
+
+          statuses = url_logs.select { _1["full_url"].start_with?("https://www.facebook.com/") }.map { _1["status"] }.uniq
+          expect(statuses).to eq %w(nofollow)
+          statuses = url_logs.select { _1["full_url"].start_with?("https://twitter.com/") }.map { _1["status"] }.uniq
+          expect(statuses).to eq %w(nofollow)
+          statuses = url_logs.select { _1["full_url"].start_with?("https://b.hatena.ne.jp/") }.map { _1["status"] }.uniq
+          expect(statuses).to eq %w(nofollow)
+        end
+      end
+    end
+
+    context "with 'link_only'" do
+      let!(:part) do
+        share_states = Cms::Part::SnsShare::SERVICES.index_with { "link_only" }
+        create :cms_part_sns_share, cur_site: site, sns_share_states: share_states
+      end
+      let!(:layout) { create_cms_layout part, cur_site: site }
+      let!(:index) { create :cms_page, cur_site: site, layout: layout, filename: "index.html", state: "public" }
+
+      before do
+        expect { ss_perform_now Cms::Node::GenerateJob.bind(site_id: site.id) }.to output.to_stdout
+        expect { ss_perform_now Cms::Page::GenerateJob.bind(site_id: site.id) }.to output.to_stdout
+        Job::Log.destroy_all
+      end
+
+      it do
+        expect { ss_perform_now described_class.bind(site_id: site.id) }.to output(/0 errors/).to_stdout
+
+        expect(Job::Log.count).to eq 1
+        Job::Log.first.tap do |log|
+          expect(log.logs).to include(/INFO -- : .* Started Job/)
+          expect(log.logs).to include(/INFO -- : .* Completed Job/)
+
+          expect(log.logs).to include(include("#{site_url}/"))
+        end
+
+        expect(Cms::CheckLinks::Report.all.count).to eq 1
+        Cms::CheckLinks::Report.all.first.tap do |report|
+          expect(report.site_id).to eq site.id
+          expect(report.name).to include "実行結果"
+          # yield 外のリンク切れからレポートは作成されない
+          expect(report.link_errors.count).to eq 0
+          expect(report.pages.count).to eq 0
+          expect(report.nodes.count).to eq 0
+        end
+
+        expect(ActionMailer::Base.deliveries.length).to eq 0
+
+        expect(SS::Task.all.count).to eq 3
+        Cms::Task.all.where(name: "cms:check_links").first.tap do |task|
+          expect(task.site_id).to eq site.id
+          expect(task.node_id).to be_blank
+          expect(task.state).to eq "completed"
+          expect(task.started.in_time_zone).to be_within(5.minutes).of(Time.zone.now)
+          expect(task.closed.in_time_zone).to be_within(5.minutes).of(Time.zone.now)
+          expect(task.current_count).to eq 1
+
+          url_log_path = task.log_file_path.sub(".log", "") + "-url-log.json.gz"
+          url_logs = Zlib::GzipReader.open(url_log_path) { _1.readlines }.map { JSON.parse(_1.chomp) }
+          # puts url_logs.map { _1["full_url"] }.join("\n")
+          expect(url_logs.length).to eq 7
+
+          statuses = url_logs.select { _1["full_url"].start_with?("https://www.facebook.com/") }.map { _1["status"] }.uniq
+          expect(statuses).to eq %w(nofollow)
+          statuses = url_logs.select { _1["full_url"].start_with?("https://x.com/") }.map { _1["status"] }.uniq
+          expect(statuses).to eq %w(nofollow)
+          statuses = url_logs.select { _1["full_url"].start_with?("https://b.hatena.ne.jp/") }.map { _1["status"] }.uniq
+          expect(statuses).to eq %w(nofollow)
+          statuses = url_logs.select { _1["full_url"].start_with?("https://line.me/") }.map { _1["status"] }.uniq
+          expect(statuses).to eq %w(nofollow)
+        end
+      end
     end
   end
 end
