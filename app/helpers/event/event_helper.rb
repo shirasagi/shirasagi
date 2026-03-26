444b782034ab24133096971bf6d0ba3ca960564b
NAKANO Hideo <nakano@web-tips.co.jp>
2026-03-26 10:40:24 +0900

    [modify][cms] リンクチェックを軽くするためにカレンダーナビパーツのいくつかのリンクに data-ss-rel="nofollow" を付けた

--- app/helpers/event/event_helper.rb
+++ app/helpers/event/event_helper.rb
@@ -67,19 +67,21 @@ module Event::EventHelper
   end
 
   def link_to_monthly(date, opts = {})
-    year  = date.year
-    month = date.month
     name = opts[:name].present? ? opts[:name] : "#{month}#{t_date('month')}"
-    path = opts[:path].present? ? opts[:path] : @cur_node.try(:url).to_s
     enable = (opts[:enable] != nil) ? opts[:enable] : true
-    display = opts[:display].present? ? opts[:display] : 'index'
-    data = {}
-    if opts[:nofollow]
-      data[:ss_rel] = "nofollow"
-    end
 
     if enable && within_one_year?(date) || opts[:style]
-      link_to name, sprintf("#{path}%04d%02d/#{display}.html", year, month), data: data
+      year  = date.year
+      month = date.month
+      path = opts[:path].present? ? opts[:path] : @cur_node.try(:url).to_s
+      display = opts[:display].present? ? opts[:display] : 'index'
+      path = sprintf("#{path}%04d%02d/#{display}.html", year, month)
+      data = {}
+      if opts[:nofollow]
+        data[:ss_rel] = "nofollow"
+      end
+
+      link_to name, path, data: data
     else
       name
     end
@@ -94,19 +96,21 @@ module Event::EventHelper
   end
 
   def link_to_daily(date, opts = {})
-    year  = date.year
-    month = date.month
-    day   = date.day
     name = opts[:name].present? ? opts[:name] : "#{day}#{t_date('day')}"
-    path = opts[:path].present? ? opts[:path] : @cur_node.try(:url).to_s
     enable = (opts[:enable] != nil) ? opts[:enable] : true
-    data = {}
-    if opts[:nofollow]
-      data[:ss_rel] = "nofollow"
-    end
 
     if enable && within_one_year?(date)
-      link_to name, sprintf("#{path}%04d%02d%02d/", year, month, day), data: data
+      year  = date.year
+      month = date.month
+      day   = date.day
+      path = opts[:path].present? ? opts[:path] : @cur_node.try(:url).to_s
+      path = sprintf("#{path}%04d%02d%02d/", year, month, day)
+      data = {}
+      if opts[:nofollow]
+        data[:ss_rel] = "nofollow"
+      end
+
+      link_to name, path, data: data
     else
       name
     end
