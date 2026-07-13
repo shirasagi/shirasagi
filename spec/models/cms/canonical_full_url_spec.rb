1480e141d775b3dc4d16cefebdbaa0decd0f5da8
NAKANO Hideo <nakano@web-tips.co.jp>
2026-07-13 10:50:15 +0900

    fix spec failures

--- spec/models/cms/canonical_full_url_spec.rb
+++ spec/models/cms/canonical_full_url_spec.rb
@@ -5,11 +5,50 @@ describe Cms, type: :model, dbscope: :example do
 
   describe ".canonical_full_url" do
     it do
-      expect(Cms.canonical_full_url(site, "/news/")).to eq "#{site.full_root_url}news/"
-      expect(Cms.canonical_full_url(site, "/news/index.html")).to eq "#{site.full_root_url}news/"
-      expect(Cms.canonical_full_url(site, "/news")).to eq "#{site.full_root_url}news/"
-      expect(Cms.canonical_full_url(site, "/news/index.p2.html")).to eq "#{site.full_root_url}news/index.p2.html"
-      expect(Cms.canonical_full_url(site, "/docs/926.html")).to eq "#{site.full_root_url}docs/926.html"
+      "/news/".tap do |request_path|
+        request = ActionDispatch::Request.new(
+          "rack.input" => "",
+          "REQUEST_METHOD" => "GET",
+          "PATH_INFO" => request_path
+        )
+        expect(Cms.canonical_full_url(site, request)).to eq "#{site.full_root_url}news/"
+      end
+
+      "/news/index.html".tap do |request_path|
+        request = ActionDispatch::Request.new(
+          "rack.input" => "",
+          "REQUEST_METHOD" => "GET",
+          "PATH_INFO" => request_path
+        )
+        expect(Cms.canonical_full_url(site, request)).to eq "#{site.full_root_url}news/"
+      end
+
+      "/news".tap do |request_path|
+        request = ActionDispatch::Request.new(
+          "rack.input" => "",
+          "REQUEST_METHOD" => "GET",
+          "PATH_INFO" => request_path
+        )
+        expect(Cms.canonical_full_url(site, request)).to eq "#{site.full_root_url}news/"
+      end
+
+      "/news/index.p2.html".tap do |request_path|
+        request = ActionDispatch::Request.new(
+          "rack.input" => "",
+          "REQUEST_METHOD" => "GET",
+          "PATH_INFO" => request_path
+        )
+        expect(Cms.canonical_full_url(site, request)).to eq "#{site.full_root_url}news/index.p2.html"
+      end
+
+      "/docs/926.html".tap do |request_path|
+        request = ActionDispatch::Request.new(
+          "rack.input" => "",
+          "REQUEST_METHOD" => "GET",
+          "PATH_INFO" => request_path
+        )
+        expect(Cms.canonical_full_url(site, request)).to eq "#{site.full_root_url}docs/926.html"
+      end
     end
   end
 end
