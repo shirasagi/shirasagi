1ebacdd4751a0c0fa5fa26d8a2f1db95af169437
NAKANO Hideo <nakano@web-tips.co.jp>
2026-07-24 09:41:39 +0900

    sourceMapFilename の指定をやめてみる

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,6 @@ module.exports = {
   },
   output: {
     filename: "[name].js",
-    sourceMapFilename: "[name].js.map",
     path: path.resolve(__dirname, "app/assets/builds"),
   },
   plugins: [
