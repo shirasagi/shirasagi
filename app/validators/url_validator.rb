8d87cbc618c77ec68b04e0a07361fd9e2a566bc7
NAKANO Hideo <nakano@web-tips.co.jp>
2026-08-06 10:05:48 +0900

    [fix][cms] link url xss (#6173)
    * [add][guide] 再現テスト
    
    * [fix][guide] XSS
    
    * [modify][guide] much safer even if guides already have xss links
    
    * [fix][image_map] xss
    
    * [fix][cms/column/value/file_upload] xss
    
    * 修正内容の点検
    
    * refactor w/ RuboCop

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -3,21 +3,33 @@
 class UrlValidator < ActiveModel::EachValidator
   ALLOWED_SCHEMES = %w(http https).freeze
 
+  class << self
+    def valid?(value, **options)
+      return true if value.blank?
+
+      uri = ::Addressable::URI.parse(value)
+      # 絶対パスの場合の検証
+      if options[:absolute_path] && uri.scheme.blank? && value.start_with?("/")
+        return true
+      end
+
+      # URLスキームの検証
+      allowed_schemes = options[:scheme].try { |scheme| Array[scheme].flatten.map(&:to_s) } || ALLOWED_SCHEMES
+      if !allowed_schemes.include?(uri.scheme)
+        return false
+      end
+
+      true
+    rescue
+      false
+    end
+  end
+
   def validate_each(record, attribute, value)
     return if value.blank?
 
-    uri = ::Addressable::URI.parse(value)
-
-    # 絶対パスの場合の検証
-    if options[:absolute_path] && uri.scheme.blank? && value.start_with?("/")
-      return
-    end
-
-    # URLスキームの検証
-    allowed_schemes = options[:scheme].try { |scheme| Array[scheme].flatten.map(&:to_s) } || ALLOWED_SCHEMES
-    if !allowed_schemes.include?(uri.scheme)
+    unless self.class.valid?(value, **options)
       record.errors.add(attribute, options[:message] || :url)
-      return
     end
   rescue
     record.errors.add(attribute, options[:message] || :url)
--- spec/validators/url_validator_spec.rb
+++ spec/validators/url_validator_spec.rb
@@ -5,6 +5,7 @@ describe UrlValidator, type: :validator do
     let!(:clazz) do
       Struct.new(:url) do
         include ActiveModel::Validations
+
         def self.model_name
           ActiveModel::Name.new(self, nil, "temp")
         end
@@ -45,6 +46,7 @@ describe UrlValidator, type: :validator do
     let!(:clazz) do
       Struct.new(:url) do
         include ActiveModel::Validations
+
         def self.model_name
           ActiveModel::Name.new(self, nil, "temp")
         end
@@ -75,6 +77,7 @@ describe UrlValidator, type: :validator do
     let!(:clazz) do
       Struct.new(:url) do
         include ActiveModel::Validations
+
         def self.model_name
           ActiveModel::Name.new(self, nil, "temp")
         end
@@ -82,7 +85,7 @@ describe UrlValidator, type: :validator do
       end
     end
 
-    context 'with valid absolute path url' do
+    context 'with valid absolute path url #1' do
       subject! { clazz.new("//www.example.jp/docs/3481.html") }
 
       it do
@@ -90,8 +93,36 @@ describe UrlValidator, type: :validator do
       end
     end
 
+    context 'with valid absolute path url #2' do
+      subject! { clazz.new("/docs/3481.html") }
+
+      it do
+        expect(subject).to be_valid
+      end
+    end
+
+    context 'with valid relative path' do
+      subject! { clazz.new("docs/3481.html") }
+
+      it do
+        expect(subject).to be_invalid
+        expect(subject.errors[:url]).to have(1).items
+        expect(subject.errors[:url]).to include(I18n.t("errors.messages.url"))
+      end
+    end
+
     context 'with invalid url' do
       subject! { clazz.new("{{{}}}") }
+
+      it do
+        expect(subject).to be_invalid
+        expect(subject.errors[:url]).to have(1).items
+        expect(subject.errors[:url]).to include(I18n.t("errors.messages.url"))
+      end
+    end
+
+    context 'with xss' do
+      subject! { clazz.new(%(#" onclick="console.log('xss')" data-dummy=")) }
 
       it do
         expect(subject).to be_invalid
