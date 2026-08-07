32bc2b7bb290ff1fe21f5f9990916d115626ef4f
NAKANO Hideo <nakano@web-tips.co.jp>
2026-08-07 11:09:21 +0900

    Cms::ApiFilter の利用か所の点検 #2

--- app/controllers/cms/apis/forms_controller.rb
+++ app/controllers/cms/apis/forms_controller.rb
@@ -28,7 +28,7 @@ class Cms::Apis::FormsController < ApplicationController
   end
 
   def set_items
-    @items ||= @model.site(@cur_site).allow(:read, @cur_user, site: @cur_site)
+    @items ||= @model.site(@cur_site)
   end
 
   public
@@ -39,6 +39,7 @@ class Cms::Apis::FormsController < ApplicationController
 
     set_items
     @items = @items.
+      allow(:read, @cur_user, site: @cur_site).
       search(params[:s]).
       order_by(order: 1, name: 1).
       page(params[:page]).per(50)
