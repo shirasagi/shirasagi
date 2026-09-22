61eb786c16ab71fc26e93dd98f794b05948b0c0a
NAKANO Hideo <nakano@web-tips.co.jp>
2026-09-22 15:16:04 +0900

    非公開スクリプトを削除

--- public/assets/gws/memo/filter.js
+++
@@ -1,6 +0,0 @@
-function Gws_Memo_Filter() {
-}
-
-Gws_Memo_Filter.render = function () {
-  return $("#addon-gws-agents-addons-group_permission").hide();
-};
--- public/assets/gws/memo/folder.js
+++
@@ -1,6 +0,0 @@
-function Gws_Memo_Folder() {
-}
-
-Gws_Memo_Folder.render = function () {
-  return $("#addon-gws-agents-addons-group_permission").hide();
-};
